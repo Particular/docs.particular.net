04dce8fb3eba28e949ec7bacc0433a71594298b1
sfarmar <s.farmar@gmail.com>
2014-01-17 19:30:40 +0000

    adding samples of getting started code first articles

---
+++ Samples/001_OrderingSendOnly/src/Ordering.Server/PlaceOrderHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace Ordering.Server
+{
+    using System;
+    using Messages;
+    using NServiceBus;
+
+    public class PlaceOrderHandler : IHandleMessages<PlaceOrder>
+    {
+        public IBus Bus { get; set; }
+
+        public void Handle(PlaceOrder message)
+        {
+            Console.WriteLine(@"Order for Product:{0} placed with id: {1}", message.Product, message.Id);
+        }
+    }
+}
---
+++ Samples/003_OrderingPubSub/src/Ordering.Client/SendOrder.cs
@@ -0,0 +1,30 @@
+﻿namespace Ordering.Client
+{
+    using System;
+    using Messages;
+    using NServiceBus;
+
+    public class SendOrder : IWantToRunWhenBusStartsAndStops
+    {
+        public IBus Bus { get; set; }
+
+        public void Start()
+        {
+            Console.WriteLine("Press 'Enter' to send a message.To exit, Ctrl + C");
+
+            while (Console.ReadLine() != null)
+            {
+                var id = Guid.NewGuid();
+
+                Bus.Send("Ordering.Server", new PlaceOrder() { Product = "New shoes", Id = id});
+
+                Console.WriteLine("==========================================================================");
+                Console.WriteLine("Send a new PlaceOrder message with id: {0}", id.ToString("N"));
+            }
+        }
+
+        public void Stop()
+        {
+        }
+    }
+}
