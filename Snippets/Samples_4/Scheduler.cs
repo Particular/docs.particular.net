5d64befb41ee116e9c4e9669de39f28289255d01
Simon Cropp <simon.cropp@gmail.com>
2014-07-09 12:11:57 +1000

    clean up snippets

---
+++ Snippets/Samples_4/Scheduler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using NServiceBus;
+
+public class SchedulerSample
+{
+    public class ScheduleMyTasks : IWantToRunWhenBusStartsAndStops
+    {
+        IBus bus;
+
+        public ScheduleMyTasks(IBus bus)
+        {
+            this.bus = bus;
+        }
+
+        public void Start()
+        {
+            // To send a message every 5 minutes
+            Schedule.Every(TimeSpan.FromMinutes(5))
+                .Action(() => bus.SendLocal(new MyMessage()));
+
+            // Name a schedule task and invoke it every 5 minutes
+            Schedule.Every(TimeSpan.FromMinutes(5))
+                .Action("Task name", () => Debug.WriteLine("Task executed"));
+        }
+
+        public void Stop()
+        {
+        }
+    }
+
+    public class MyMessage : IMessage
+    {
+    }
+}
---
+++ Snippets/Snippets_5/Scheduler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using NServiceBus;
+using NServiceBus.ObjectBuilder;
+
+public class SchedulerSample
+{
+    public class ScheduleMyTasks : IWantToRunWhenBusStartsAndStops
+    {
+        IBus bus;
+        Schedule schedule;
+
+        public ScheduleMyTasks(IBus bus, Schedule schedule)
+        {
+            this.bus = bus;
+            this.schedule = schedule;
+        }
+
+        public void Start()
+        {      
+            // To send a message every 5 minutes
+            schedule.Every(TimeSpan.FromMinutes(5), () => bus.SendLocal(new MyMessage()));
+
+            // Name a schedule task and invoke it every 5 minutes
+            schedule.Every(TimeSpan.FromMinutes(5), "Task name", () => Debug.WriteLine("Task executed"));
+
+        }
+
+        public void Stop()
+        {
+        }
+    }
+    public class MyMessage : IMessage
+    {
+    }
+}
