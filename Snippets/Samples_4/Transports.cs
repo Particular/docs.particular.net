351fdb146c902bf7286d78dd549c63d8451ce434
Simon Cropp <simon.cropp@gmail.com>
2014-07-11 14:04:56 +1000

    more samples

---
+++ Snippets/Samples_4/Transports.cs
@@ -0,0 +1,26 @@
+﻿using NServiceBus;
+
+
+public class Transports
+{
+    public void AllTheTransports()
+    {
+
+        // start code ConfigureTransportsV4
+
+        // Configure to use MSMQ 
+        Configure.With().UseTransport<Msmq>();
+
+        // Configure to use AzureStorageQueue
+        Configure.With().UseTransport<AzureStorageQueue>();
+
+        // Configure to use AzureServiceBus
+        Configure.With().UseTransport<AzureServiceBus>();
+
+        // Configure to use SqlServer
+        Configure.With().UseTransport<NServiceBus.SqlServer>();
+
+        // end code ConfigureTransportsV4
+    }
+
+}
