bdecb7129d9462124935ee165ed6bb2fab2bcca9
Simon Cropp <simon.cropp@gmail.com>
2014-08-04 14:30:15 +1000

    DefineCriticalErrorAction

---
+++ Snippets/Samples_4/DefineCriticalErrorAction.cs
@@ -0,0 +1,18 @@
+﻿using NServiceBus;
+
+public class DefineCriticalErrorAction
+{
+    public void Simple()
+    {
+        // start code DefineCriticalErrorActionV4
+
+        var configure = Configure.With()
+            .DefineCriticalErrorAction((s, exception) =>
+            {
+                // custom exception handling
+            });
+
+        // end code DefineCriticalErrorActionV4
+    }
+
+}
---
+++ Snippets/Snippets_5/DefineCriticalErrorAction.cs
@@ -0,0 +1,21 @@
+﻿using NServiceBus;
+
+// ReSharper disable once ConvertToLambdaExpression
+public class DefineCriticalErrorAction
+{
+    public void Simple()
+    {
+        // start code DefineCriticalErrorActionV5
+
+        var configure = Configure.With(builder =>
+        {
+            builder.DefineCriticalErrorAction((s, exception) =>
+            {
+                // custom exception handling
+            });
+        });
+
+        // end code DefineCriticalErrorActionV
+    }
+
+}
