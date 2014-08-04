0b667dcdf5fede8065e0e3ecc360b52b0e9a9ef5
Simon Cropp <simon.cropp@gmail.com>
2014-08-04 20:12:26 +1000

    FileShareDataBus

---
+++ Snippets/Samples_4/FileShareDataBus.cs
@@ -0,0 +1,15 @@
+﻿using NServiceBus;
+
+public class FileShareDataBus
+{
+    public void Simple()
+    {
+        string databusPath = null;
+        // start code FileShareDataBusV4
+
+        var configure = Configure.With()
+            .FileShareDataBus(databusPath);
+
+        // end code FileShareDataBusV4
+    }
+}
---
+++ Snippets/Snippets_5/FileShareDataBus.cs
@@ -0,0 +1,14 @@
+﻿using NServiceBus;
+
+public class FileShareDataBus
+{
+    public void Simple()
+    {
+        string databusPath = null;
+        // start code FileShareDataBusV5
+
+        var configure = Configure.With(b => b.FileShareDataBus(databusPath));
+
+        // end code FileShareDataBusV5
+    }
+}
