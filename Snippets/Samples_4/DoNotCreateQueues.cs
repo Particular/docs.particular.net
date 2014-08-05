f6bd8d14e40c3382f848408922536bbd1b1943cf
Simon Cropp <simon.cropp@gmail.com>
2014-08-06 00:41:34 +1000

    Configure.With().DoNotCreateQueues();

---
+++ Snippets/Samples_4/DoNotCreateQueues.cs
@@ -0,0 +1,13 @@
+﻿using NServiceBus;
+
+
+public class DoNotCreateQueues
+{
+    public void Simple()
+    {
+        // start code DoNotCreateQueuesV4
+        Configure.With().DoNotCreateQueues();
+        // end code DoNotCreateQueuesV4
+    }
+
+}
---
+++ Snippets/Snippets_5/DoNotCreateQueues.cs
@@ -0,0 +1,13 @@
+﻿using NServiceBus;
+
+
+public class DoNotCreateQueues
+{
+    public void Simple()
+    {
+        // start code DoNotCreateQueuesV5
+        Configure.With(builder => builder.DoNotCreateQueues());
+        // end code DoNotCreateQueuesV5
+    }
+
+}
