8e9bdd43937013a68665a10c607d261b6e52369d
Simon Cropp <simon.cropp@gmail.com>
2014-08-05 10:22:55 +1000

    add purge on startup sample

---
+++ Snippets/Samples_4/PurgeOnStartup.cs
@@ -0,0 +1,15 @@
+﻿using NServiceBus;
+
+public class PurgeOnStartup
+{
+    public void Simple()
+    {
+        // start code PurgeOnStartupV4
+
+        Configure.With()
+            .PurgeOnStartup(true);
+
+        // end code PurgeOnStartupV4
+    }
+
+}
---
+++ Snippets/Snippets_5/PurgeOnStartup.cs
@@ -0,0 +1,14 @@
+﻿using NServiceBus;
+
+public class PurgeOnStartup
+{
+    public void Simple()
+    {
+        // start code PurgeOnStartupV5
+
+        Configure.With(builder => builder.PurgeOnStartup(true));
+
+        // end code PurgeOnStartupV5
+    }
+
+}
