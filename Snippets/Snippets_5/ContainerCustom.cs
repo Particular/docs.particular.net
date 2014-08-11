90430aef9bbbfdd887d0ea47b7c28788ee86aa9a
Simon Cropp <simon.cropp@gmail.com>
2014-08-11 17:15:40 +1000

    fix missing save

--- Snippets/Snippets_5/ContainerCustom.cs
+++ Snippets/Snippets_5/ContainerCustom.cs
@@ -7,7 +7,7 @@ using NServiceBus.Settings;
 
 public class ContainerCustom
 {
-    #region CustomContainersV5
+    // startcode CustomContainersV5
 
     public void CustomContainerExtensionUsage()
     {
@@ -22,59 +22,58 @@ public class ContainerCustom
         }
     }
 
-    #endregion
-
-}
-
-public class MyObjectBuilder : IContainer
-{
-    public void Dispose()
+    public class MyObjectBuilder : IContainer
     {
-        throw new NotImplementedException();
-    }
+    // endcode 
 
-    public object Build(Type typeToBuild)
-    {
-        throw new NotImplementedException();
-    }
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
 
-    public IContainer BuildChildContainer()
-    {
-        throw new NotImplementedException();
-    }
+        public object Build(Type typeToBuild)
+        {
+            throw new NotImplementedException();
+        }
 
-    public IEnumerable<object> BuildAll(Type typeToBuild)
-    {
-        throw new NotImplementedException();
-    }
+        public IContainer BuildChildContainer()
+        {
+            throw new NotImplementedException();
+        }
 
-    public void Configure(Type component, DependencyLifecycle dependencyLifecycle)
-    {
-        throw new NotImplementedException();
-    }
+        public IEnumerable<object> BuildAll(Type typeToBuild)
+        {
+            throw new NotImplementedException();
+        }
 
-    public void Configure<T>(Func<T> component, DependencyLifecycle dependencyLifecycle)
-    {
-        throw new NotImplementedException();
-    }
+        public void Configure(Type component, DependencyLifecycle dependencyLifecycle)
+        {
+            throw new NotImplementedException();
+        }
 
-    public void ConfigureProperty(Type component, string property, object value)
-    {
-        throw new NotImplementedException();
-    }
+        public void Configure<T>(Func<T> component, DependencyLifecycle dependencyLifecycle)
+        {
+            throw new NotImplementedException();
+        }
 
-    public void RegisterSingleton(Type lookupType, object instance)
-    {
-        throw new NotImplementedException();
-    }
+        public void ConfigureProperty(Type component, string property, object value)
+        {
+            throw new NotImplementedException();
+        }
 
-    public bool HasComponent(Type componentType)
-    {
-        throw new NotImplementedException();
-    }
+        public void RegisterSingleton(Type lookupType, object instance)
+        {
+            throw new NotImplementedException();
+        }
 
-    public void Release(object instance)
-    {
-        throw new NotImplementedException();
+        public bool HasComponent(Type componentType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Release(object instance)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
