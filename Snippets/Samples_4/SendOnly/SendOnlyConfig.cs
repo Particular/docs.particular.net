9e5c0a8568d9bff2f3457c5b065b6d3ca9bbe683
Andreas Öhlund <andreas.ohlund@particular.net>
2014-08-29 12:59:38 +0200

    Updating to new sendonly api in v5

---
+++ Snippets/Samples_4/SendOnly/SendOnlyConfig.cs
@@ -0,0 +1,22 @@
+﻿using NServiceBus;
+
+public class SendOnlyConfig
+{
+    public SendOnlyConfig()
+    {
+        #region SendOnly-v4
+        var bus = Configure.With()
+                        .DefaultBuilder()
+                        .UseTransport<Msmq>()
+                        .UnicastBus()
+                        .SendOnly();
+        bus.Send(new TestMessage());
+        #endregion
+    }
+
+
+    public class TestMessage
+    {
+    }
+
+}
---
+++ Snippets/Snippets_5/SendOnly/SendOnlyConfig.cs
@@ -0,0 +1,20 @@
+﻿using NServiceBus;
+
+public class SendOnlyConfig
+{
+    public SendOnlyConfig()
+    {
+        #region SendOnly-v5
+        
+        var bus = Bus.CreateSendOnly(new BusConfiguration());
+
+        bus.Send(new TestMessage());
+        #endregion
+    }
+
+
+    public class TestMessage
+    {
+    }
+
+}
