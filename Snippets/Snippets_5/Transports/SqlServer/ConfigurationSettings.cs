3f561875eb9f1afd875d5973c80c0a4a4b0fc34e
SzymonPobiega <szymon.pobiega@gmail.com>
2014-10-09 13:00:07 +0200

    Added snippets for SqlServer transport

---
+++ Snippets/Snippets_5/Transports/SqlServer/ConfigurationSettings.cs
@@ -0,0 +1,97 @@
+﻿namespace MyServer.Transports.SqlServer
+{
+    using NServiceBus;
+
+    public class ConfigurationSettings
+    {
+        void TransactionScope()
+        {
+            var configuration = new BusConfiguration();
+
+
+            #region sqlserver-config-transactionscope
+
+            configuration.UseTransport<SqlServerTransport>();
+
+            #endregion
+        }
+
+        void NativeTransactions()
+        {
+            var configuration = new BusConfiguration();
+
+
+            #region sqlserver-config-native-transactions
+
+            configuration.UseTransport<SqlServerTransport>();
+            configuration.Transactions().DisableDistributedTransactions();
+
+            #endregion
+        }
+
+        void NoTransactions()
+        {
+            var configuration = new BusConfiguration();
+
+            #region sqlserver-config-no-transactions
+
+            configuration.UseTransport<SqlServerTransport>();
+            configuration.Transactions().Disable();
+
+            #endregion
+        }
+
+        void DisableSecondaries()
+        {
+            var configuration = new BusConfiguration();
+
+            #region sqlserver-config-disable-secondaries
+
+            configuration.UseTransport<SqlServerTransport>().DisableCallbackReceiver();
+
+            #endregion
+        }
+
+        void SetSecondaryReceiverConcurrency()
+        {
+            var configuration = new BusConfiguration();
+
+            #region sqlserver-config-set-secondary-concurrency
+
+            configuration.UseTransport<SqlServerTransport>().CallbackReceiverMaxConcurrency(16);
+
+            #endregion
+        }
+
+        void Callbacks()
+        {
+            var configuration = new BusConfiguration();
+
+            var bus = Bus.Create(configuration);
+
+            #region sqlserver-config-callbacks
+
+            var callback = bus.Send(new Request());
+            callback.Register(result => ProcessResponse(result));
+
+            #endregion
+
+            #region sqlserver-config-callbacks-reply
+
+            bus.Return(42);
+
+            #endregion
+        }
+
+
+        void ProcessResponse(CompletionResult returnCode)
+        {
+        }
+
+        private class Request : IMessage
+        {
+        }
+    }
+
+ 
+}
