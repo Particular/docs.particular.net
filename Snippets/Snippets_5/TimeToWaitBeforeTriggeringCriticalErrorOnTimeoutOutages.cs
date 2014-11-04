61ad17d21998dc0120406f6b67ebeb5b9e47ac48
John Simons <john.simons@nservicebus.com>
2014-11-04 11:24:15 +1000

    Added ability to specify time to wait before raising critical time for timeout outages

---
+++ Snippets/Snippets_5/TimeToWaitBeforeTriggeringCriticalErrorOnTimeoutOutages.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NServiceBus;
+
+
+public class TimeToWaitBeforeTriggeringCriticalErrorOnTimeoutOutages
+{
+    public void Simple()
+    {
+        #region TimeToWaitBeforeTriggeringCriticalErrorOnTimeoutOutages-V5
+
+        var configuration = new BusConfiguration();
+
+        configuration.TimeToWaitBeforeTriggeringCriticalErrorOnTimeoutOutages(TimeSpan.FromMinutes(5));
+
+        #endregion
+    }
+
+}
