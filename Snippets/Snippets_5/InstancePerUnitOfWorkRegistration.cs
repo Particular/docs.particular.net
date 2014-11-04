bae8931c58bcc85f206744775a907b65bc2af93c
Simon Cropp <simon.cropp@gmail.com>
2014-11-04 16:12:33 +1100

    add instance of unit per work snippet

---
+++ Snippets/Snippets_5/InstancePerUnitOfWorkRegistration.cs
@@ -0,0 +1,18 @@
+﻿using NServiceBus;
+
+public class InstancePerUnitOfWorkRegistration
+{
+    public void Simple()
+    {
+        #region InstancePerUnitOfWorkRegistrationV5
+
+        var configuration = new BusConfiguration();
+        configuration.RegisterComponents(c => c.ConfigureComponent<MyService>(DependencyLifecycle.InstancePerCall));
+
+        #endregion
+    }
+    public class MyService
+    {
+    }
+}
+
