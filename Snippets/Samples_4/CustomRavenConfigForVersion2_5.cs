61252874711c501c47e7912566c6fdf34aedd260
indualagarsamy <indu.alagarsamy@gmail.com>
2014-11-07 11:58:14 -0800

    NServiceBus version 4 and RavenDB 2.5
    If using NServiceBus v4 endpoints and RavenDB 2.5 server is installed, then the user must install the NServiceBus.RavenDB nuget package and initialize appropriately. Added Guidance. Fixes https://github.com/Particular/docs.particular.net/issues/158

---
+++ Snippets/Samples_4/CustomRavenConfigForVersion2_5.cs
@@ -0,0 +1,40 @@
+﻿using NServiceBus;
+using NServiceBus.RavenDB;
+
+public class UseRavenDBVersion2PersistenceUsingCustomInitialization : IWantCustomInitialization
+{
+    public void Init()
+    {
+
+        #region OldRavenDBPersistenceInitialization
+
+        Configure.With()
+           .DefaultBuilder()
+           .RavenPersistence()
+           .RavenSagaPersister()
+           .RavenSubscriptionStorage()
+           .UseRavenTimeoutPersister()
+           .UseRavenGatewayDeduplication()
+           .UseRavenGatewayPersister();
+
+        #endregion 
+    }
+}
+
+public class UseRavenDBVersion2_5PersistenceUsingCustomInitialization : IWantCustomInitialization
+{
+    public void Init()
+    {
+        #region Version2_5RavenDBPersistenceInitialization
+
+        Configure.With()
+           .DefaultBuilder()
+           .RavenDBStorage() // Need to call this method
+           .UseRavenDBSagaStorage() // Call this method to use Raven saga storage
+           .UseRavenDBSubscriptionStorage() // Call this method to use Raven subscription storage
+           .UseRavenDBTimeoutStorage() // Call this method to use Raven timeout storage
+           .UseRavenDBGatewayDeduplicationStorage() // Call this method to use Raven deduplication storage for the Gateway
+           .UseRavenDBGatewayStorage(); // Call this method to use the  Raven Gateway storage method
+        #endregion
+    }
+}
