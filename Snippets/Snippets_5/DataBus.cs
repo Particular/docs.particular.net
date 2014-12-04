c4d43235b30cf7aeabfa1aa8eab6cdf95ebc0c2f
Mauro Servienti <mauro@topics.it>
2014-12-04 13:52:35 +0100

    fixing links and snippets

--- Snippets/Snippets_5/DataBus.cs
+++ Snippets/Snippets_5/DataBus.cs
@@ -1,4 +1,7 @@
 ﻿using NServiceBus;
+using NServiceBus.DataBus;
+using System;
+using System.IO;
 
 public class DataBus
 {
@@ -12,4 +15,48 @@ public class DataBus
 
         #endregion
     }
+}
+
+namespace CustomDataBusPluginSnippet
+{
+    #region CustomDataBus
+
+    class CustomDataBus : IDataBus
+    {
+        public Stream Get( string key )
+        {
+            return File.OpenRead( "blob.dat" );
+        }
+
+        public string Put( Stream stream, TimeSpan timeToBeReceived )
+        {
+            using( var destination = File.OpenWrite( "blob.dat" ) )
+            {
+                stream.CopyTo( destination );
+            }
+            return "the-key-of-the-stored-file-such-as-the-full-path";
+        }
+
+        public void Start()
+        {
+        }
+    }
+
+    #endregion
+
+    #region PluginCustomDataBusV5 5
+
+
+    public class EndpointConfig : IConfigureThisEndpoint, AsA_Server
+    {
+        public void Customize( BusConfiguration configuration )
+        {
+            //other configuration calls
+
+            configuration.UseDataBus( typeof( CustomDataBus ) );
+        }
+    }
+
+    #endregion
+
 }
--- nservicebus/plugin-custom-databus.md
+++ nservicebus/plugin-custom-databus.md
@@ -5,7 +5,7 @@ tags:
 - DataBus
 ---
 
-NServiceBus endpoints support sending and receiving large chunks of data via the [DataBus feature](/nservicebus/attachments-databus-sample).
+NServiceBus endpoints support sending and receiving large chunks of data via the [DataBus feature](/nservicebus/attachments-databus-sample.md).
 
 NServiceBus by default supports 2 DataBus implementations:
 
@@ -16,7 +16,7 @@ Note: The `Azure DataBus` implementation is part of the Azure transport package.
 
 It is possible to create your own DataBus implementation by implementing the `IDataBus` interface, such as in the following minimalistic sample:
 
-<!-- import CustomDataBusPluginSnippet -->
+<!-- import CustomDataBus -->
 
 To configure the endpoint to use your custom DataBus implementation it is enough to register it at endpoint configuration time, such as in the following sample:
 
