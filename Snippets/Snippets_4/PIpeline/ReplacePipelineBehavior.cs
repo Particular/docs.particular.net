2212ccad3313fa2f92e9d47e1fade958516da1a7
SzymonPobiega <szymon.pobiega@gmail.com>
2014-12-08 13:37:36 +0100

    Added snipet for replacing behavior

---
+++ Snippets/Snippets_4/PIpeline/ReplacePipelineBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NServiceBus.Pipeline;
+using NServiceBus.Pipeline.Contexts;
+using NServiceBus.Unicast.Behaviors;
+
+#region ReplacePipelineStep 4.5
+class ReplaceExistingBehavior : PipelineOverride
+{
+    public override void Override(BehaviorList<HandlerInvocationContext> behaviorList)
+    {
+        behaviorList.Replace<InvokeHandlersBehavior, MyInvokeHandlersBehavior>();
+    }
+
+    //Classes inheriting from PipelineOverride are registered by convention. No need to explicitly register.
+}
+#endregion
+
+public class MyInvokeHandlersBehavior : IBehavior<HandlerInvocationContext>
+{
+    public void Invoke(HandlerInvocationContext context, Action next)
+    {
+        throw new NotImplementedException();
+    }
+}
