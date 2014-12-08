4edc547de74c2b4a9d90ef6765d72d1b8ddae904
SzymonPobiega <szymon.pobiega@gmail.com>
2014-12-08 12:55:48 +0100

    Added snippets for 4.5 pipeline configuration

---
+++ Snippets/Snippets_4/PIpeline/NewPipelineStep.cs
@@ -0,0 +1,14 @@
+﻿using NServiceBus.Pipeline;
+using NServiceBus.Pipeline.Contexts;
+using NServiceBus.Unicast.Behaviors;
+
+#region NewStepInPipeline
+class NewStepInPipeline : PipelineOverride
+{
+    public override void Override(BehaviorList<HandlerInvocationContext> behaviorList)
+    {
+        behaviorList.InsertAfter<InvokeHandlersBehavior, SampleBehavior>();
+    }
+}
+#endregion
+
---
+++ Snippets/Snippets_4/PIpeline/SampleBehavior.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NServiceBus.Pipeline;
+using NServiceBus.Pipeline.Contexts;
+
+#region SamplePipelineBehavior
+
+public class SampleBehavior : IBehavior<HandlerInvocationContext>
+{
+    public void Invoke(HandlerInvocationContext context, Action next)
+    {
+        next();
+    }
+}
+
+#endregion
