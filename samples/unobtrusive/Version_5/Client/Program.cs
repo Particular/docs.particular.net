4b158a3bc08f6025a608b9ab5bd997a7ca1256cc
Simon Cropp <simon.cropp@gmail.com>
2015-01-28 15:21:09 +1100

    add unobtrusive sample

---
+++ samples/unobtrusive/Version_5/Client/Program.cs
@@ -0,0 +1,22 @@
+﻿using NServiceBus;
+
+class Program
+{
+    public static void Main()
+    {
+        var busConfiguration = new BusConfiguration();
+
+        busConfiguration.EnableInstallers();
+        busConfiguration.UsePersistence<InMemoryPersistence>();
+        busConfiguration.UseDataBus<FileShareDataBus>()
+            .BasePath(@"..\..\..\DataBusShare\");
+        busConfiguration.RijndaelEncryptionService();
+
+        busConfiguration.ApplyCustomConventions();
+
+        var bus = Bus.Create(busConfiguration);
+        bus.Start();
+        CommandSender.Start(bus);
+    }
+}
+
---
+++ samples/unobtrusive/Version_5/Server/CommandSender.cs
@@ -0,0 +1,50 @@
+using System;
+using Events;
+using Messages;
+using NServiceBus;
+
+class CommandSender 
+{
+
+    public static void Start(IBus bus)
+    {
+        Console.WriteLine("Press 'E' to publish an event");
+        Console.WriteLine("Press 'D' to send a deferred message");
+        Console.WriteLine("To exit, press Ctrl + C");
+
+        while (true)
+        {
+            var cmd = Console.ReadKey().Key.ToString().ToLower();
+
+            switch (cmd)
+            {
+                case "e":
+                    PublishEvent(bus);
+                    break;
+                case "d":
+                    DeferMessage(bus);
+                    break;
+            }
+        }
+    }
+
+    static void DeferMessage(IBus bus)
+    {
+        bus.Defer(TimeSpan.FromSeconds(10), new DeferredMessage());
+        Console.WriteLine();
+        Console.WriteLine("{0} - {1}", DateTime.Now.ToLongTimeString(), "Sent a message that is deferred for 10 seconds");
+    }
+
+    static void PublishEvent(IBus bus)
+    {
+        var eventId = Guid.NewGuid();
+
+        bus.Publish<IMyEvent>(m =>
+        {
+            m.EventId = eventId;
+        });
+        Console.WriteLine("Event published, id: " + eventId);
+
+    }
+
+}
