76a8443958ead9689d7af013b31ff842bf2f7d5e
SzymonPobiega <szymon.pobiega@gmail.com>
2015-03-20 14:33:52 +0100

    Added V4 snippets to transactions doco

---
+++ Snippets/Snippets_4/Transactions.cs
@@ -0,0 +1,37 @@
+﻿using NServiceBus;
+
+public class Transactions
+{
+    public void Unreliable()
+    {
+        #region TransactionsDisable
+        Configure.Transactions.Disable();
+        #endregion
+    }
+
+    public void TransportTransactions()
+    {
+        #region TransactionsDisableDistributedTransactions
+        Configure.Transactions.Advanced(x => x.DisableDistributedTransactions());
+        #endregion
+
+    }
+
+    public void AmbientTransactions()
+    {
+        #region TransactionsEnable
+        Configure.Transactions.Enable().Advanced(x => x.EnableDistributedTransactions());
+        #endregion
+
+        #region TransactionsDoNotWrapHandlersExecutionInATransactionScope
+        Configure.Transactions.Advanced(x => x.DoNotWrapHandlersExecutionInATransactionScope());
+        #endregion
+    }
+
+    public void TransportTransactionsWithScope()
+    {
+        #region TransactionsWrapHandlersExecutionInATransactionScope
+        Configure.Transactions.Advanced(x => x.DoNotWrapHandlersExecutionInATransactionScope());
+        #endregion
+    }
+}
