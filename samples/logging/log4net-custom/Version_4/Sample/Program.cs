3267312cc8e1a1c7f855bf5ea108514ab15022ae
Simon Cropp <simon.cropp@gmail.com>
2015-03-24 15:18:54 +1100

    add log4net sample v4

--- samples/logging/log4net-custom/Version_4/Sample/Program.cs
+++ samples/logging/log4net-custom/Version_4/Sample/Program.cs
@@ -4,15 +4,12 @@ using log4net.Config;
 using log4net.Core;
 using log4net.Layout;
 using NServiceBus;
-using NServiceBus.Log4Net;
-using NServiceBus.Logging;
+using NServiceBus.Installation.Environments;
 
 class Program
 {
-
     static void Main()
     {
-
         #region ConfigureLog4Net
         PatternLayout layout = new PatternLayout
         {
@@ -33,35 +30,37 @@ class Program
             MaxSizeRollBackups = 10,
             LockingModel = new FileAppender.MinimalLock(),
             StaticLogFileName = false,
-            File = @"nsblog",
+            File = @"nsblog2",
             Layout = layout,
             AppendToFile = true,
-            Threshold = Level.Debug,
+            Threshold = Level.Info,
         };
-        fileAppender.ActivateOptions();
-
+        // Note that no fileAppender.ActivateOptions(); is required since NSB 4 does this internally
         BasicConfigurator.Configure(fileAppender, consoleAppender);
         #endregion
+
+        Configure.Serialization.Json();
+
         #region UseConfig
+        Configure configure = Configure.With();
+        configure.DefineEndpointName("Samples.Logging.Log4NetCustom");
 
-        LogManager.Use<Log4NetFactory>();
-
-        // Then continue with your BusConfiguration
-        BusConfiguration busConfig = new BusConfiguration();
-        busConfig.EndpointName("Samples.Logging.Log4NetCustom");
-
+        //Log4Net will pick up the config set in BasicConfigurator
+        configure.Log4Net(consoleAppender);
+        configure.Log4Net(fileAppender);
         #endregion
+        configure.DefaultBuilder();
+        configure.InMemorySagaPersister();
+        configure.UseInMemoryTimeoutPersister();
+        configure.InMemorySubscriptionStorage();
+        configure.UseTransport<Msmq>();
+        IBus bus = configure.UnicastBus()
+            .CreateBus()
+            .Start(() => Configure.Instance.ForInstallationOn<Windows>().Install());
 
-        busConfig.UseSerialization<JsonSerializer>();
-        busConfig.EnableInstallers();
-        busConfig.UsePersistence<InMemoryPersistence>();
+        bus.SendLocal(new MyMessage());
 
-        using (IStartableBus bus = Bus.Create(busConfig))
-        {
-            bus.Start();
-            bus.SendLocal(new MyMessage());
-            Console.WriteLine("\r\nPress any key to stop program\r\n");
-            Console.ReadKey();
-        }
+        Console.WriteLine("\r\nPress any key to stop program\r\n");
+        Console.ReadKey();
     }
 }
