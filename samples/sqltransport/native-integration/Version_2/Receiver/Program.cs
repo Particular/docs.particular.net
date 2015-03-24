a356e8f9594b3bc7f0e723d0811974e8e5170ddb
Simon Cropp <simon.cropp@gmail.com>
2015-03-25 05:03:54 +1100

    cleanup

--- samples/sqltransport/native-integration/Version_2/Receiver/Program.cs
+++ samples/sqltransport/native-integration/Version_2/Receiver/Program.cs
@@ -24,32 +24,43 @@ class Program
 
             while (Console.ReadKey().ToString() != "q")
             {
-                #region MessagePayload
-                string message = @"{
-                                            $type: 'PlaceOrder',
-                                            OrderId: 'Order from ADO.net sender'
-                                         }";
-                #endregion
-
-                #region SendingUsingAdoNet
-                using (SqlConnection connection = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=samples;Integrated Security=True"))
-                {
-                    connection.Open();
-
-                    using (SqlCommand command = new SqlCommand(@"INSERT INTO [Samples.SqlServer.NativeIntegration] ([Id],[Recoverable],[Headers],[Body]) VALUES (@Id,@Recoverable,@Headers,@Body)", connection))
-                    {
-                        command.CommandType = CommandType.Text;
-
-                        command.Parameters.Add("Id", SqlDbType.UniqueIdentifier).Value = Guid.NewGuid();
-                        command.Parameters.Add("Headers", SqlDbType.VarChar).Value = "";
-                        command.Parameters.Add("Body", SqlDbType.VarBinary).Value = Encoding.UTF8.GetBytes(message);
-                        command.Parameters.Add("Recoverable", SqlDbType.Bit).Value = true;
-
-                        command.ExecuteNonQuery();
-                    }
-                }
-                #endregion
+                PlaceOrder();
             }
         }
+    }
+
+    static void PlaceOrder()
+    {
+        #region MessagePayload
+
+        string message = @"{
+                               $type: 'PlaceOrder',
+                               OrderId: 'Order from ADO.net sender'
+                            }";
+
+        #endregion
+
+        #region SendingUsingAdoNet
+
+        string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=samples;Integrated Security=True";
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            string insertSql = @"INSERT INTO [Samples.SqlServer.NativeIntegration] ([Id],[Recoverable],[Headers],[Body]) VALUES (@Id,@Recoverable,@Headers,@Body)";
+            using (SqlCommand command = new SqlCommand(insertSql, connection))
+            {
+                command.CommandType = CommandType.Text;
+
+                command.Parameters.Add("Id", SqlDbType.UniqueIdentifier).Value = Guid.NewGuid();
+                command.Parameters.Add("Headers", SqlDbType.VarChar).Value = "";
+                command.Parameters.Add("Body", SqlDbType.VarBinary).Value = Encoding.UTF8.GetBytes(message);
+                command.Parameters.Add("Recoverable", SqlDbType.Bit).Value = true;
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        #endregion
     }
 }
