fd899996742a1589a03b59e06fa78a54c8c8c913
Andreas Öhlund <andreas.ohlund@particular.net>
2015-03-26 13:58:24 +0100

    Adding a sample showing UoW using StructureMap
    Fleshed out the walkthrough
    
    Updating after code review from @simoncropp

---
+++ samples/unit-of-work/using-childcontainers/Version_5/Sample/PlaceOrder.cs
@@ -0,0 +1,7 @@
+﻿using NServiceBus;
+
+public class PlaceOrder:IMessage
+{
+    public string OrderNumber { get; set; }
+    public double OrderValue { get; set; }
+}
---
+++ samples/unit-of-work/using-childcontainers/Version_5/Sample/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using NServiceBus;
+using Raven.Client;
+using Raven.Client.Document;
+using StructureMap;
+
+static class Program
+{
+    static void Main()
+    {
+        BusConfiguration configuration = new BusConfiguration();
+        configuration.EndpointName("Samples.UoWWithChildContainers");
+
+        #region ContainerConfiguration
+        Container container = new Container(x =>
+        {
+            x.For<IDocumentStore>().Use(new DocumentStore
+                        {
+                            Url = "http://localhost:32076",
+                            DefaultDatabase = "Samples.UoWWithChildContainers"
+                        }
+                        .Initialize());
+
+            x.For<IDocumentSession>().Use(c => c.GetInstance<IDocumentStore>().OpenSession());
+        });
+
+        configuration.UseContainer<StructureMapBuilder>(c => c.ExistingContainer(container));
+        #endregion
+
+        #region PipelineRegistration
+        configuration.Pipeline.Register<RavenUnitOfWork.Registration>();
+        #endregion
+
+        configuration.UsePersistence<InMemoryPersistence>();
+        configuration.EnableInstallers();
+
+        int orderNumber = 1;
+
+        using (IStartableBus bus = Bus.Create(configuration))
+        {
+            bus.Start();
+
+            Console.Out.WriteLine("Press any key to send a message. Press `q` to quit");
+
+            Console.Out.WriteLine("After storing a few orders you can open a browser and view them at http://localhost:32076/studio/index.html#databases/documents?collection=Orders&database=Samples.UoWWithChildContainers");
+
+            while (Console.ReadKey().ToString() != "q")
+            {
+                bus.SendLocal(new PlaceOrder
+                {
+                    OrderNumber = string.Format("Order-{0}", orderNumber),
+                    OrderValue = 100
+                });
+
+                orderNumber++;
+            }
+        }
+    }
+}
