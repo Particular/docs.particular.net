47afe2da1f2f6abc3a370babc610345dacbfb0f4
danielmarbach <daniel.marbach@particular.net>
2015-04-20 11:35:46 +0200

    Describing outbox ravendb and including how to configure timetokeep

---
+++ Snippets/Snippets_5/Outbox/RavenDB/TimeToKeep.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NServiceBus;
+using NServiceBus.Configuration.AdvanceExtensibility;
+
+public class TimeToKeep
+{
+    public void Customize()
+    {
+        BusConfiguration configuration = null;
+
+        #region OutboxRavendBTimeToKeep
+        configuration.GetSettings().Set("Outbox.TimeToKeepDeduplicationData", TimeSpan.FromDays(7));
+        configuration.GetSettings().Set("Outbox.FrequencyToRunDeduplicationDataCleanup", TimeSpan.FromMinutes(1));
+        #endregion
+    }
+}
