9e29cb0a18e982f5aafb96a7030ab7212fdecc88
Simon Cropp <simon.cropp@gmail.com>
2015-04-28 19:26:11 +1000

    remove noise about persistence

--- samples/show-case/on-premise/Version_5/VideoStore.ECommerce/Global.asax.cs
+++ samples/show-case/on-premise/Version_5/VideoStore.ECommerce/Global.asax.cs
@@ -16,20 +16,14 @@ namespace VideoStore.ECommerce
             configuration.PurgeOnStartup(true);
             configuration.RijndaelEncryptionService();
 
-            // For production use, please select a durable persistence.
-            // To use RavenDB, install-package NServiceBus.RavenDB and then use configuration.UsePersistence<RavenDBPersistence>();
-            // To use SQLServer, install-package NServiceBus.NHibernate and then use configuration.UsePersistence<NHibernatePersistence>();
-            if (Debugger.IsAttached)
-            {
-                configuration.UsePersistence<InMemoryPersistence>();
-            }
+            configuration.UsePersistence<InMemoryPersistence>();
 
             configuration.Conventions()
                 .DefiningCommandsAs(t => t.Namespace != null && t.Namespace.StartsWith("VideoStore") && t.Namespace.EndsWith("Commands"))
                 .DefiningEventsAs(t => t.Namespace != null && t.Namespace.StartsWith("VideoStore") && t.Namespace.EndsWith("Events"))
                 .DefiningMessagesAs(t => t.Namespace != null && t.Namespace.StartsWith("VideoStore") && t.Namespace.EndsWith("RequestResponse"))
                 .DefiningEncryptedPropertiesAs(p => p.Name.StartsWith("Encrypted"));
- 
+
             // In Production, make sure the necessary queues for this endpoint are installed before running the website
             if (Debugger.IsAttached)
             {
