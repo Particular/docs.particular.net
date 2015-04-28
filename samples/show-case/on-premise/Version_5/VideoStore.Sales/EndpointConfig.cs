e8fa3d8e7d33707491fb0b99066d646f6f130df4
Simon Cropp <simon.cropp@gmail.com>
2015-04-28 19:19:05 +1000

    move to shared error config

--- samples/show-case/on-premise/Version_5/VideoStore.Sales/EndpointConfig.cs
+++ samples/show-case/on-premise/Version_5/VideoStore.Sales/EndpointConfig.cs
@@ -1,6 +1,3 @@
-
-using System;
-using System.Diagnostics;
 using NServiceBus;
 
 namespace VideoStore.Sales
@@ -10,13 +7,7 @@ namespace VideoStore.Sales
     {
         public void Customize(BusConfiguration configuration)
         {
-            // For production use, please select a durable persistence.
-            // To use RavenDB, install-package NServiceBus.RavenDB and then use configuration.UsePersistence<RavenDBPersistence>();
-            // To use SQLServer, install-package NServiceBus.NHibernate and then use configuration.UsePersistence<NHibernatePersistence>();
-            if (Debugger.IsAttached)
-            {
-                configuration.UsePersistence<InMemoryPersistence>();
-            }
+            configuration.UsePersistence<InMemoryPersistence>();
             configuration.Conventions()
                 .DefiningCommandsAs(t => t.Namespace != null && t.Namespace.StartsWith("VideoStore") && t.Namespace.EndsWith("Commands"))
                 .DefiningEventsAs(t => t.Namespace != null && t.Namespace.StartsWith("VideoStore") && t.Namespace.EndsWith("Events"))
@@ -27,6 +18,3 @@ namespace VideoStore.Sales
     }
 
 }
-
-
-
