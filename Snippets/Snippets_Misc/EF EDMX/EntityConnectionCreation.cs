057a250e9640b0dbd90aea4c3343f3be42958c34
Mauro Servienti <mauro@topics.it>
2015-05-06 11:45:30 +0200

    moving to snippets

---
+++ Snippets/Snippets_Misc/EF EDMX/EntityConnectionCreation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.EntityClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snippets_Misc.EF_EDMX
+{
+	class EntityConnectionCreation
+	{
+		void Temp()
+		{
+			#region EntityConnectionCreationAndUsage
+
+			var entityBuilder = new EntityConnectionStringBuilder()
+			{
+				Provider = "System.Data.SqlClient",
+				ProviderConnectionString = "the database connection string",
+				Metadata = @"res://*/MySample.csdl|res://*/MySample.ssdl|res://*/MySample.msl"
+			};
+
+			var entityConn = new EntityConnection( entityBuilder.ToString() );
+
+			using( var ctx = new MySampleContainer( entityConn ) )
+			{
+				//use the DbContext as required
+			}
+
+			#endregion
+		}
+	}
+}
---
+++ Snippets/Snippets_Misc/EF EDMX/MySampleContainer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.EntityClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snippets_Misc.EF_EDMX
+{
+	#region DbContextPartialWithEntityConnection
+	partial class MySampleContainer
+	{
+		public MySampleContainer( EntityConnection dbConnection )
+			: base( dbConnection, true ) { }
+	}
+	#endregion
+}
