a516dd112eb4c576164c6de7009cc995535dceb3
Simon Cropp <simon.cropp@gmail.com>
2015-05-07 13:12:32 +1000

    fix throughput doco snippets

---
+++ Snippets/Snippets_3/Transports/Throughtput/Throughtput.cs
@@ -0,0 +1,24 @@
+﻿using NServiceBus.Config;
+using NServiceBus.Config.ConfigurationSource;
+using NServiceBus.Unicast;
+
+public class Throughtput
+{
+
+    #region ThroughtputFromCode
+
+    public class ConfigureEncryption :
+        IProvideConfiguration<MsmqTransportConfig>
+    {
+        public MsmqTransportConfig GetConfiguration()
+        {
+            return new MsmqTransportConfig
+            {
+                NumberOfWorkerThreads = 5
+            };
+        }
+    }
+
+    #endregion
+
+}
---
+++ Snippets/Snippets_4/Transports/Throughtput/Throughtput.cs
@@ -0,0 +1,43 @@
+﻿using NServiceBus.Config;
+using NServiceBus.Config.ConfigurationSource;
+using NServiceBus.Unicast;
+
+public class Throughtput
+{
+
+    #region ThroughtputFromCode
+
+    public class ConfigureEncryption :
+        IProvideConfiguration<TransportConfig>
+    {
+        public TransportConfig GetConfiguration()
+        {
+            return new TransportConfig
+            {
+                MaximumConcurrencyLevel = 5,
+                MaximumMessageThroughputPerSecond = 10
+            };
+        }
+    }
+
+    #endregion
+    public void WithCode()
+    {
+        UnicastBus bus = null;
+
+        #region ChangeThroughtput
+        //bus is an instance of NServiceBus.Unicast.UnicastBus
+        bus.Transport.ChangeMaximumMessageThroughputPerSecond(10);
+        bus.Transport.ChangeMaximumConcurrencyLevel(5);
+        #endregion
+
+        #region ReadThroughtput
+
+        //bus is an instance of NServiceBus.Unicast.UnicastBus
+        int messageThroughputPerSecond = bus.Transport.MaximumMessageThroughputPerSecond;
+        int maximumConcurrencyLevel = bus.Transport.MaximumConcurrencyLevel;
+
+        #endregion
+    }
+
+}
