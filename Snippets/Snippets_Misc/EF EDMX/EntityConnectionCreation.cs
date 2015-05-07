864f430f21f4a231d0b2331490d71f1a318f14dd
Simon Cropp <simon.cropp@gmail.com>
2015-05-07 14:52:47 +1000

    redundant usings

--- Snippets/Snippets_Misc/EF EDMX/EntityConnectionCreation.cs
+++ Snippets/Snippets_Misc/EF EDMX/EntityConnectionCreation.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity.Core.EntityClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.Entity.Core.EntityClient;
 
 namespace Snippets_Misc.EF_EDMX
 {
