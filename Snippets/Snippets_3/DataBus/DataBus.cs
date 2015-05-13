eb85f0e432d17db321ff95609960adade4664c66
Simon Cropp <simon.cropp@gmail.com>
2015-05-13 10:16:02 +1000

    add version 3 databus doco

---
+++ Snippets/Snippets_3/DataBus/DataBus.cs
@@ -0,0 +1,54 @@
+﻿using NServiceBus;
+
+public class DataBus
+{
+    public void FileShareDataBus()
+    {
+        string databusPath = null;
+
+        #region FileShareDataBus
+
+        Configure configure = Configure.With()
+            .FileShareDataBus(databusPath);
+
+        #endregion
+    }
+
+}
+
+namespace DataBusProperties
+{
+    #region MessageWithLargePayload
+
+    public class MessageWithLargePayload
+    {
+        public string SomeProperty { get; set; }
+        public DataBusProperty<byte[]> LargeBlob { get; set; }
+    }
+
+    #endregion
+
+    #region MessageWithLargePayloadUsingConvention
+
+    public class MessageWithLargePayloadUsingConvention
+    {
+        public string SomeProperty { get; set; }
+        public byte[] LargeBlobDataBus { get; set; }
+    }
+
+    #endregion
+
+    public static class MessageConventions
+    {
+        public static void DefineDataBusPropertiesConvention(Configure configuration)
+        {
+            #region DefineMessageWithLargePayloadUsingConvention
+
+            configuration.DefiningDataBusPropertiesAs(p => p.Name.EndsWith("DataBus"));
+
+            #endregion
+
+        }
+    }
+
+}
