a80a71a7438bf70738380e8c704f3ded4baa199d
Andreas Öhlund <andreas.ohlund@particular.net>
2015-05-25 14:56:32 +0200

    Adding doco on what correlation id is
    And how is can be configured when sending messages

---
+++ Snippets/Snippets_5/Correlation/CustomCorrelationId.cs
@@ -0,0 +1,22 @@
+﻿namespace Snippets_6.Correlation
+{
+    using NServiceBus;
+
+    public class CustomCorrelationId
+    {
+        public CustomCorrelationId()
+        {
+            IBus bus = null;
+
+            #region custom-correlationid
+
+            bus.Send("TargetQueue","My custom correlation id", new MyRequest());
+
+            #endregion
+        }
+
+        public class MyRequest
+        {
+        }
+    }
+}
