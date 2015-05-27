4d4d714507a249acaef188b65bb5620448620248
Sean Feldman <feldman.sean@gmail.com>
2015-05-26 23:35:20 -0600

    Fixing time format to ensure it runs on all locales

--- samples/azure/shared-host/Version_5/Shared/LogEntry.cs
+++ samples/azure/shared-host/Version_5/Shared/LogEntry.cs
@@ -6,7 +6,7 @@ class LogEntry : TableEntity
     public LogEntry(string endpoint, string message)
     {
         PartitionKey = endpoint;
-        RowKey = DateTime.Now.ToString();
+        RowKey = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
         Message = message;
     }
 
