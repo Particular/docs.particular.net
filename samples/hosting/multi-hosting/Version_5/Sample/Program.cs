63a27056b484c84a3919ff0ba65f6cb22b20118a
Simon Cropp <simon.cropp@gmail.com>
2015-05-27 18:00:43 +1000

    better multihost

--- samples/hosting/multi-hosting/Version_5/Sample/Program.cs
+++ samples/hosting/multi-hosting/Version_5/Sample/Program.cs
@@ -11,8 +11,23 @@ class Program
         using (IBus bus1 = StartInstance1())
         using (IBus bus2 = StartInstance2())
         {
-            Console.WriteLine("\r\nBus instances created and configured; press any key to stop program\r\n");
-            Console.ReadKey();
+            Console.WriteLine("Press '1' to send a message from Instance1 to Instance2");
+            Console.WriteLine("Press '2' to send a message from Instance2 to Instance1");
+            Console.WriteLine("To exit, press Ctrl + C");
+
+            while (true)
+            {
+                ConsoleKeyInfo key = Console.ReadKey();
+                Console.WriteLine();
+                if (key.Key == ConsoleKey.D1)
+                {
+                    bus1.Send("Samples.MultiHosting.Instance2", new MyMessage());
+                } 
+                else if (key.Key == ConsoleKey.D2)
+                {
+                    bus2.Send("Samples.MultiHosting.Instance1", new MyMessage());
+                }
+            }
         }
 
         #endregion
@@ -24,8 +39,8 @@ class Program
 
         BusConfiguration busConfiguration = new BusConfiguration();
 
-        busConfiguration.EndpointName("Samples.MultiHosting-1");
-        busConfiguration.AssembliesToScan(AllAssemblies.Matching("Instance1."));
+        busConfiguration.EndpointName("Samples.MultiHosting.Instance1");
+        busConfiguration.AssembliesToScan(AllAssemblies.Matching("Instance1.").And("Shared"));
         busConfiguration.UseSerialization<JsonSerializer>();
         busConfiguration.EnableInstallers();
         busConfiguration.UsePersistence<InMemoryPersistence>();
@@ -39,9 +54,9 @@ class Program
     {
         BusConfiguration busConfiguration = new BusConfiguration();
 
-        busConfiguration.EndpointName("Samples.MultiHosting-2");
-        busConfiguration.AssembliesToScan(AllAssemblies.Matching("Instance2."));
-        busConfiguration.UseSerialization<XmlSerializer>();
+        busConfiguration.EndpointName("Samples.MultiHosting.Instance2");
+        busConfiguration.AssembliesToScan(AllAssemblies.Matching("Instance2.").And("Shared"));
+        busConfiguration.UseSerialization<JsonSerializer>();
         busConfiguration.EnableInstallers();
         busConfiguration.UsePersistence<InMemoryPersistence>();
 
