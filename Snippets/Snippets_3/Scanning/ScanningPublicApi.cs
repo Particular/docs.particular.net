8116e23c43553bc68331e369a0e85f2e00c45ca9
Simon Cropp <simon.cropp@gmail.com>
2015-05-29 11:19:50 +1000

    cleanup

--- Snippets/Snippets_3/Scanning/ScanningPublicApi.cs
+++ Snippets/Snippets_3/Scanning/ScanningPublicApi.cs
@@ -1,42 +1,39 @@
-﻿namespace MyServer.Scanning
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using NServiceBus;
+
+public class ScanningPublicApi
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Reflection;
-    using NServiceBus;
-
-    public class ScanningPublicApi
+    public ScanningPublicApi()
     {
-        public ScanningPublicApi()
-        {
-            IEnumerable<Assembly> myListOfAssemblies = null;
+        IEnumerable<Assembly> myListOfAssemblies = null;
 
-            Assembly assembly2 = null;
-            Assembly assembly1 = null;
+        Assembly assembly2 = null;
+        Assembly assembly1 = null;
 
-            IEnumerable<Type> myTypes = null;
+        IEnumerable<Type> myTypes = null;
 
-            #region ScanningDefault
-            Configure.With();
-            #endregion
+        #region ScanningDefault
+        Configure.With();
+        #endregion
 
-            #region ScanningListOfAssemblies
-            Configure.With(myListOfAssemblies);
-            // or
-            Configure.With(assembly1, assembly2);
-            #endregion
+        #region ScanningListOfAssemblies
+        Configure.With(myListOfAssemblies);
+        // or
+        Configure.With(assembly1, assembly2);
+        #endregion
 
-            #region ScanningCustomDirectory
-            Configure.With(@"c:\my-custom-dir");
-            #endregion
+        #region ScanningCustomDirectory
+        Configure.With(@"c:\my-custom-dir");
+        #endregion
 
-            #region ScanningListOfTypes
-            Configure.With(myTypes);
-            #endregion
+        #region ScanningListOfTypes
+        Configure.With(myTypes);
+        #endregion
 
-            #region ScanningExcludeByName
-            Configure.With(AllAssemblies.Except("MyAssembly1.dll").And("MyAssembly2.dll"));
-            #endregion
-        } 
-    }
+        #region ScanningExcludeByName
+        Configure.With(AllAssemblies.Except("MyAssembly1.dll").And("MyAssembly2.dll"));
+        #endregion
+    } 
 }
