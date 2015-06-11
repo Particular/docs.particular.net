8034d6fd117dc04e013fb252bef5574d7a0fe3fd
danielmarbach <daniel.marbach@particular.net>
2015-06-11 08:58:03 +0200

    Simplify

--- samples/saga/ravendb-custom-sagafinder/Version_5/Sample/Program.cs
+++ samples/saga/ravendb-custom-sagafinder/Version_5/Sample/Program.cs
@@ -3,6 +3,7 @@ using NServiceBus;
 using NServiceBus.Persistence;
 using Raven.Client;
 using Raven.Client.Document;
+using Raven.Client.Embedded;
 using Raven.Client.UniqueConstraints;
 
 class Program
@@ -10,36 +11,53 @@ class Program
 
     static void Main()
     {
-        BusConfiguration busConfiguration = new BusConfiguration();
-        busConfiguration.EndpointName("Samples.RavenDBCustomSagaFinder");
-        busConfiguration.UseSerialization<JsonSerializer>();
-        busConfiguration.EnableInstallers();
-
-        #region RavenDBSetup
-
-        IDocumentStore defaultStore = new DocumentStore
+        int port = 32076;
+        using (var documentStore = new EmbeddableDocumentStore
         {
-                                          Url = "http://localhost:32076",
-                                          DefaultDatabase = "Samples.RavenDBCustomSagaFinder"
-                                      }
-            .RegisterListener(new UniqueConstraintsStoreListener())
-            .Initialize();
-
-        busConfiguration.UsePersistence<RavenDBPersistence>()
-            .DoNotSetupDatabasePermissions() //Only required to simplify the sample setup
-            .SetDefaultDocumentStore(defaultStore);
-
-        #endregion
-
-        using (IBus bus = Bus.Create(busConfiguration).Start())
+            DataDirectory = "Data",
+            UseEmbeddedHttpServer = true,
+            Configuration =
+            {
+                PluginsDirectory = Environment.CurrentDirectory,
+                Port = port,
+                HostName = "localhost"
+            }
+        })
         {
-            bus.SendLocal(new StartOrder
-                          {
-                              OrderId = "123"
-                          });
+            documentStore.Initialize();
+            Console.WriteLine("Raven server started on http://localhost:{0}/.", port);
 
-            Console.WriteLine("\r\nPress any key to stop program\r\n");
-            Console.ReadKey();
+            BusConfiguration busConfiguration = new BusConfiguration();
+            busConfiguration.EndpointName("Samples.RavenDBCustomSagaFinder");
+            busConfiguration.UseSerialization<JsonSerializer>();
+            busConfiguration.EnableInstallers();
+
+            #region RavenDBSetup
+
+            IDocumentStore defaultStore = new DocumentStore
+            {
+                Url = "http://localhost:32076",
+                DefaultDatabase = "Samples.RavenDBCustomSagaFinder"
+            }
+                .RegisterListener(new UniqueConstraintsStoreListener())
+                .Initialize();
+
+            busConfiguration.UsePersistence<RavenDBPersistence>()
+                .DoNotSetupDatabasePermissions() //Only required to simplify the sample setup
+                .SetDefaultDocumentStore(defaultStore);
+
+            #endregion
+
+            using (IBus bus = Bus.Create(busConfiguration).Start())
+            {
+                bus.SendLocal(new StartOrder
+                {
+                    OrderId = "123"
+                });
+
+                Console.WriteLine("\r\nPress any key to stop program\r\n");
+                Console.ReadKey();
+            }
         }
     }
 }
