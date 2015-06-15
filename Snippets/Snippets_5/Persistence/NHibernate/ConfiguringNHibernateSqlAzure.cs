dc024a9b8ae055cbd0a3a18e4ab7bc6fbaef73b2
Yves Goeleven <yves@goeleven.com>
2015-06-15 13:10:21 +0200

    sql azure & nhibernate

---
+++ Snippets/Snippets_5/Persistence/NHibernate/ConfiguringNHibernateSqlAzure.cs
@@ -0,0 +1,25 @@
+﻿namespace Snippets5.Persistence.NHibernate
+{
+    using global::NHibernate.Cfg;
+    using NServiceBus;
+    using NServiceBus.Persistence;
+
+    class ConfiguringNHibernateSqlAzure
+    {
+        public void SqlAzureNHibernateDriverConfiguration()
+        {
+            #region SqlAzureNHibernateDriverConfiguration
+
+            BusConfiguration busConfiguration = new BusConfiguration();
+
+            Configuration nhConfiguration = new Configuration();
+            nhConfiguration.Properties["connection.driver_class"] = "NHibernate.SqlAzure.SqlAzureClientDriver, NHibernate.SqlAzure";
+
+            busConfiguration.UsePersistence<NHibernatePersistence>()
+                .UseConfiguration(nhConfiguration);
+
+            #endregion
+        }
+
+    }
+}
