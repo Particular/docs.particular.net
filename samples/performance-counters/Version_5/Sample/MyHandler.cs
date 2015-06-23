14af398f273d9419a13d69beceb1c0b5ebe15840
Simon Cropp <simon.cropp@gmail.com>
2015-06-23 17:43:53 +1000

    add perf counter sample

---
+++ samples/performance-counters/Version_5/Sample/MyHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using NServiceBus;
+using NServiceBus.Logging;
+
+#region handler
+public class MyHandler : IHandleMessages<MyMessage>
+{
+    static ILog logger = LogManager.GetLogger(typeof(MyHandler));
+
+    static Random random = new Random();
+    public void Handle(MyMessage message)
+    {
+        int sleepTime = random.Next(1, 1000);
+        Thread.Sleep(sleepTime);
+        if (sleepTime%2 != 0)
+        {
+            throw new Exception();
+        }
+        logger.InfoFormat("Hello from MyHandler. Slept for {0}ms", sleepTime);
+    }
+}
+#endregion
---
+++ samples/performance-counters/Version_5/Sample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NServiceBus;
+
+class Program
+{
+
+    static void Main()
+    {
+        BusConfiguration busConfiguration = new BusConfiguration();
+        busConfiguration.EndpointName("Samples.PerfCounters");
+        busConfiguration.UseSerialization<JsonSerializer>();
+        busConfiguration.EnableInstallers();
+        busConfiguration.UsePersistence<InMemoryPersistence>();
+
+        #region enable-counters
+        busConfiguration.EnableCriticalTimePerformanceCounter();
+        busConfiguration.EnableSLAPerformanceCounter(TimeSpan.FromSeconds(100));
+        #endregion
+
+        using (IBus bus = Bus.Create(busConfiguration).Start())
+        {
+            Console.WriteLine("Press any key to send 10 messages with random sleep");
+
+            Console.WriteLine("To exit, press Escape");
+            while (Console.ReadKey().Key != ConsoleKey.Escape)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    bus.SendLocal(new MyMessage());
+                }
+            }
+        }
+    }
+}
