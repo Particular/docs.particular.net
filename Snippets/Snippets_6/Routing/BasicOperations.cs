0f62a9622b11345d66df0953a710c817c60d9d40
andreasohlund <andreas.ohlund@particular.net>
2015-07-12 17:21:55 +0200

    Better doco for pub sub
    Including auto subscribe

---
+++ Snippets/Snippets_6/Routing/BasicOperations.cs
@@ -0,0 +1,52 @@
+﻿namespace Snippets6.Routing
+{
+    using NServiceBus;
+
+    public class BasicOperations
+    {
+        public void ConcreteMessage()
+        {
+            IBus bus = null;
+            #region InstancePublish
+            var message = new MyEvent { SomeProperty = "Hello world" };
+
+            bus.Publish(message);
+            #endregion
+
+        }
+
+        public void InterfaceMessage()
+        {
+            IBus bus = null;
+            #region InterfacePublish
+            var message = new MyEvent { SomeProperty = "Hello world" };
+
+            bus.Publish<IMyEvent>(m => { m.SomeProperty = "Hello world"; });
+            #endregion
+
+        }
+
+        
+        public void Subscribe()
+        {
+            IBus bus = null;
+            #region ExplicitSubscribe
+            bus.Subscribe<MyEvent>();
+
+            bus.Unsubscribe<MyEvent>();
+            #endregion
+
+        }
+        
+        class MyEvent
+        {
+            public string SomeProperty { get; set; }
+        }
+
+        class IMyEvent
+        {
+            public string SomeProperty { get; set; }
+        }
+
+    }
+}
