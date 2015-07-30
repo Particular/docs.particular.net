58a03d17f8e4592a32e06cf9447d8e9561b51722
Simon Cropp <simon.cropp@gmail.com>
2015-07-30 12:35:07 +1000

    fix upgrade guide

---
+++ Snippets/Snippets_6/UpgradeGuides/5to6/OutgoingBehavior.cs
@@ -0,0 +1,18 @@
+﻿namespace Snippets6.UpgradeGuide
+{
+    using System;
+    using NServiceBus.Pipeline;
+    using NServiceBus.Pipeline.Contexts;
+    using NServiceBus.TransportDispatch;
+
+    #region 5to6header-outgoing-behavior
+    public class OutgoingBehavior : Behavior<OutgoingContext>
+    {
+        public override void Invoke(OutgoingContext context, Action next)
+        {
+            context.SetHeader("MyCustomHeader", "My custom value");
+            next();
+        }
+    }
+    #endregion
+}
---
+++ Snippets/Snippets_6/UpgradeGuides/5to6/Upgrade.cs
@@ -0,0 +1,16 @@
+﻿namespace Snippets6.UpgradeGuide
+{
+    using NServiceBus;
+
+    public class Upgrade
+    {
+        void StaticHeaders()
+        {
+
+            BusConfiguration busConfiguration = new BusConfiguration();
+            #region 5to6header-static-endpoint
+            busConfiguration.AddHeaderToAllOutgoingMessages("MyGlobalHeader", "some static value");
+            #endregion
+        }
+    }
+}
