378297acad167ed4df5093a220997c91a5145e11
Simon Cropp <simon.cropp@gmail.com>
2015-07-30 13:37:44 +1000

    suppress a snippet

--- Snippets/Snippets_6/Sagas/FindSagas/SagaFinder.cs
+++ Snippets/Snippets_6/Sagas/FindSagas/SagaFinder.cs
@@ -3,7 +3,7 @@ namespace Snippets6.Sagas.FindSagas
 {
     public class SagaFinder
     {
-        #region TODOsaga-finder
+        #TODOregion saga-finder
 
         //todo: update when we have versions of NH + raven for v6 (also remove TODO from region name
         //// NHibernate example:
