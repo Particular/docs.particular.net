c2b4e2679f6aceeeded38e356312e5eb84425b2e
Simon Cropp <simon.cropp@gmail.com>
2015-08-07 10:55:23 +1000

    ad v6 logging sample

--- samples/logging/default/Version_6/Sample/Program.cs
+++ samples/logging/default/Version_6/Sample/Program.cs
@@ -10,8 +10,9 @@ class Program
         #region ConfigureLogging
         BusConfiguration busConfiguration = new BusConfiguration();
         busConfiguration.EndpointName("Samples.Logging.Default");
-        //Note that no config is required in version 5  and higher since logging is enabled by default
+        //Note that no config is required in version 5 and higher since logging is enabled by default
         #endregion
+        busConfiguration.SendFailedMessagesTo("error");
         busConfiguration.UseSerialization<JsonSerializer>();
         busConfiguration.EnableInstallers();
         busConfiguration.UsePersistence<InMemoryPersistence>();
