56ff50ca3628c5a15f055ada361f93b79f3e60b2
Simon Cropp <simon.cropp@gmail.com>
2015-08-21 13:32:40 +1000

    refs

--- samples/scaleout/Version_5/Orders.Handler/EndpointConfig.cs
+++ samples/scaleout/Version_5/Orders.Handler/EndpointConfig.cs
@@ -10,19 +10,4 @@ namespace Orders.Handler
         }
     }
     
-    class ConfiguringTheDistributorWithTheFluentApi : INeedInitialization
-    {
-        public void Customize(BusConfiguration busConfiguration)
-        {
-            // uncomment one of the following lines if you want to use the fluent api instead. Remember to 
-            // remove the "Master" profile from the command line Properties->Debug
-            // busConfiguration.RunMSMQDistributor();
-
-            // or if you want to run the distributor only and no worker
-            // busConfiguration.RunMSMQDistributor(false);
-
-            // or if you want to be a worker, remove the "Worker" profile from the command line Properties -> Debug
-            // busConfiguration.EnlistWithMSMQDistributor(); 
-        }
-    }
 }
