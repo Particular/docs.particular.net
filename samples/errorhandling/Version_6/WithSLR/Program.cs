9be6746b66da37de66005dd4c001e9aa7323084a
Simon Cropp <simon.cropp@gmail.com>
2015-08-25 16:25:33 +1000

    add error handling v6

---
+++ samples/errorhandling/Version_6/WithSLR/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NServiceBus;
+using NServiceBus.Logging;
+
+static class Program
+{
+    static void Main()
+    {
+        LogManager.Use<DefaultFactory>()
+            .Level(LogLevel.Warn);
+
+        BusConfiguration busConfiguration = new BusConfiguration();
+        busConfiguration.EndpointName("Samples.ErrorHandling.WithSLR");
+        busConfiguration.UseSerialization<JsonSerializer>();
+        busConfiguration.UsePersistence<InMemoryPersistence>();
+        busConfiguration.EnableInstallers();
+        using (IBus bus = Bus.Create(busConfiguration).Start())
+        {
+            Console.WriteLine("Press enter to send a message that will throw an exception.");
+            Console.WriteLine("Press any key to exit");
+
+            while (true)
+            {
+                ConsoleKeyInfo key = Console.ReadKey();
+                if (key.Key != ConsoleKey.Enter)
+                {
+                    return;
+                }
+                MyMessage m = new MyMessage
+                {
+                    Id = Guid.NewGuid()
+                };
+                bus.SendLocal(m);
+            }
+        }
+    }
+}
