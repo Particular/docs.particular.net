0264551992d678eb2ecde1c923b4d1054a79cb2f
Simon Cropp <simon.cropp@gmail.com>
2015-08-25 13:36:10 +1000

    add v3 gateway samples

---
+++ samples/gateway/Version_4/Server/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NServiceBus;
+using NServiceBus.Installation.Environments;
+
+class Program
+{
+    static void Main()
+    {
+        Configure.Serialization.Json();
+        Configure configure = Configure.With();
+        configure.Log4Net();
+        configure.DefineEndpointName("Samples.FullDuplex.Server");
+        configure.DefaultBuilder();
+        configure.InMemorySagaPersister();
+        configure.UseInMemoryTimeoutPersister();
+        configure.InMemorySubscriptionStorage();
+        configure.UseTransport<Msmq>();
+
+        using (IStartableBus startableBus = configure.UnicastBus().CreateBus())
+        {
+            IBus bus = startableBus
+                .Start(() => configure.ForInstallationOn<Windows>().Install());
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
