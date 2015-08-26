b779f289e399e4d49914ad9f14b9b54026e1e525
andreasohlund <andreas.ohlund@particular.net>
2015-08-26 09:49:12 +0200

    Adding a v6 version of the mutator sample
    Adding a v6 version of the mutator sample
    
    Kill reduntant app.config

---
+++ samples/encryption/message-body-encryption/Version_6/Endpoint2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NServiceBus;
+
+class Program
+{
+    static void Main()
+    {
+        BusConfiguration busConfiguration = new BusConfiguration();
+        busConfiguration.EndpointName("Samples.MessageBodyEncryption.Endpoint2");
+        busConfiguration.UsePersistence<InMemoryPersistence>();
+        busConfiguration.RegisterMessageEncryptor();
+        IStartableBus startableBus = Bus.Create(busConfiguration);
+        using (startableBus.Start())
+        {
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
