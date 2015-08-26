09792b829241ed51f3fde571b08757306745553a
Pete <peter.giles@particular.net>
2015-08-26 10:06:34 +1000

    Endpoint Named Properly

--- samples/unobtrusive/Version_5/Server/Program.cs
+++ samples/unobtrusive/Version_5/Server/Program.cs
@@ -5,7 +5,7 @@ class Program
     public static void Main()
     {
         BusConfiguration busConfiguration = new BusConfiguration();
-
+        busConfiguration.EndpointName("Samples.Unobtrusive.Server");
         busConfiguration.EnableInstallers();
         busConfiguration.UsePersistence<InMemoryPersistence>();
         busConfiguration.UseDataBus<FileShareDataBus>()
