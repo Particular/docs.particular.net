5cfefd25be60dfc3c3e9cbfd184ee182ae6ab342
Pete <peter.giles@particular.net>
2015-08-27 11:23:27 +1000

    initial
    message handler not working

---
+++ Snippets/Operations/SqlServer/NativeSend.cs
@@ -0,0 +1,75 @@
+﻿
+
+namespace Operations.SqlServer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Data.SqlClient;
+    using System.Text;
+    using System.Transactions;
+    using NServiceBus.Serializers.Json;
+
+    public static class NativeSend
+    {
+
+        static void Usage()
+        {
+            #region sqlserver-nativesend-usage
+
+            SendMessage(
+              connectionString: @"Data Source=.\SQLEXPRESS;Initial Catalog=samples;Integrated Security=True",
+              queuePath: @"Samples.SqlServer.NativeIntegration",
+              messageBody: "{\"Property\":\"PropertyValue\"}",
+              headers: new List<HeaderInfo>
+                {
+                    new HeaderInfo
+                    {
+                        Key = "NServiceBus.EnclosedMessageTypes",
+                        Value = "MyNamespace.MyMessage"
+                    }
+                });
+
+            #endregion
+        }
+
+        #region sqlserver-nativesend
+
+        public static void SendMessage(string connectionString, string queuePath, string messageBody, List<HeaderInfo> headers)
+        {
+            using (var scope = new TransactionScope())
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string insertSql = @"INSERT INTO [" + queuePath + "] ([Id],[Recoverable],[Headers],[Body]) VALUES (@Id,@Recoverable,@Headers,@Body)";
+                    using (SqlCommand command = new SqlCommand(insertSql, connection))
+                    {
+                        command.CommandType = CommandType.Text;
+
+                        command.Parameters.Add("Id", SqlDbType.UniqueIdentifier).Value = Guid.NewGuid();
+                        command.Parameters.Add("Headers", SqlDbType.VarChar).Value = CreateHeaders(headers);
+                        command.Parameters.Add("Body", SqlDbType.VarBinary).Value = Encoding.UTF8.GetBytes(messageBody);
+                        command.Parameters.Add("Recoverable", SqlDbType.Bit).Value = true;
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+                scope.Complete();
+            }
+        }
+
+        static string CreateHeaders(List<HeaderInfo> headerInfos)
+        {
+            return new JsonMessageSerializer(null).SerializeObject(headerInfos);
+        }
+
+        public class HeaderInfo
+        {
+            public string Key { get; set; }
+            public string Value { get; set; }
+        }
+        #endregion
+    }
+}
---
+++ Snippets/Operations/SqlServer/NativeSendTests.cs
@@ -0,0 +1,125 @@
+﻿namespace Operations.SqlServer
+{
+    using System;
+    using System.Threading;
+    using System.Collections.Generic;
+    using System.Data.SqlClient;
+    using NServiceBus;
+    using NServiceBus.Config;
+    using NServiceBus.Config.ConfigurationSource;
+    using NServiceBus.Features;
+    using NUnit.Framework;
+
+    [TestFixture]
+    [Explicit]
+    public class NativeSendTests
+    {
+        string endpointName = "sqlserverNativeSendTests";
+        static string errorQueueName = "sqlserverNativeSendTestsError";
+
+        static string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=samples;Integrated Security=True";
+        static string schema = @"dbo";
+
+        [SetUp]
+        [TearDown]
+        public void Setup()
+        {
+            
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                QueueDeletion.DeleteQueuesForEndpoint(connection,schema, endpointName);
+                QueueDeletion.DeleteQueuesForEndpoint(connection, schema, errorQueueName);
+
+            }
+        }
+
+        [Test]
+        public void Send()
+        {
+            State state = new State();
+            using (IBus bus = StartBus(state))
+            {
+                var headers = new List<NativeSend.HeaderInfo>
+                {
+                    new NativeSend.HeaderInfo
+                    {
+                        Key = "NServiceBus.EnclosedMessageTypes",
+                        Value = "Operations.SqlServer.NativeSendTests+MessageToSend"
+                    }
+                };
+                NativeSend.SendMessage(connectionString, endpointName, @"{""Property"": ""Value"",}", headers);
+                state.ResetEvent.WaitOne();
+            }
+        }
+
+        IBus StartBus(State state)
+        {
+            BusConfiguration config = new BusConfiguration();
+            config.RegisterComponents(c => c.ConfigureComponent(x => state, DependencyLifecycle.SingleInstance));
+            config.EndpointName(endpointName);
+            config.UseSerialization<JsonSerializer>();
+            config.UseTransport<SqlServerTransport>().ConnectionString(connectionString);
+            
+            // Following line solves {"The given key (NServiceBus.LocalAddress) was not present in the dictionary."}
+            Type[] sqlTypes = typeof(SqlServerTransport).Assembly.GetTypes();
+
+            config.TypesToScan(TypeScanner.NestedTypes<NativeSendTests>(sqlTypes));
+
+            config.EnableInstallers();
+            config.UsePersistence<InMemoryPersistence>();
+            config.DisableFeature<SecondLevelRetries>();
+
+            return Bus.Create(config).Start();
+        }
+
+        class MessageHandler : IHandleMessages<MessageToSend>
+        {
+            State state;
+
+            public MessageHandler(State state)
+            {
+                this.state = state;
+            }
+
+       
+            public void Handle(MessageToSend message)
+            {
+                Assert.AreEqual("Value", message.Property);
+                state.ResetEvent.Set();
+            }
+        }
+
+        class State
+        {
+            public ManualResetEvent ResetEvent = new ManualResetEvent(false);
+        }
+
+        class MessageToSend : IMessage
+        {
+            public string Property { get; set; }
+        }
+        class ConfigTransport : IProvideConfiguration<TransportConfig>
+        {
+            public TransportConfig GetConfiguration()
+            {
+                return new TransportConfig
+                {
+                    MaxRetries = 0
+                };
+            }
+        }
+
+        class ConfigErrorQueue : IProvideConfiguration<MessageForwardingInCaseOfFaultConfig>
+        {
+            public MessageForwardingInCaseOfFaultConfig GetConfiguration()
+            {
+                return new MessageForwardingInCaseOfFaultConfig
+                {
+                    ErrorQueue = errorQueueName
+                };
+            }
+        }
+    }
+}
