1b7420f72071876b7d06b26a68c3f04cbc5da4d2
Pete <peter.giles@particular.net>
2015-08-27 14:16:08 +1000

    Sample Message Mutators v6
    Change messages by plugging custom logic in to a couple of interfaces,
    encrypting as required.

---
+++ samples/messagemutators/Version_6/Sample/ValidationMessageMutator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using NServiceBus.Logging;
+using NServiceBus.MessageMutator;
+#region ValidationMessageMutator
+public class ValidationMessageMutator :   IMutateIncomingMessages, IMutateOutgoingMessages
+{
+    static ILog log = LogManager.GetLogger("ValidationMessageMutator");
+
+
+    public void MutateOutgoing(MutateOutgoingMessagesContext context)
+    {
+        object messageInstance = context.MessageInstance;
+        ValidateDataAnnotations(ref messageInstance);
+        context.MessageInstance = messageInstance;
+    }
+
+
+    public object MutateIncoming(object message)
+    {
+        ValidateDataAnnotations(ref message);
+        return message;
+    }
+
+    static void ValidateDataAnnotations(ref object message)
+    {
+        ValidationContext context = new ValidationContext(message, null, null);
+        List<ValidationResult> results = new List<ValidationResult>();
+
+        bool isValid = Validator.TryValidateObject(message, context, results, true);
+
+        if (isValid)
+        {
+            log.Info("Validation succeeded for message: " + message);
+            return;
+        }
+
+        StringBuilder errorMessage = new StringBuilder();
+        string error = string.Format("Validation failed for message {0}, with the following error/s: " + Environment.NewLine,message);
+        errorMessage.Append(error);
+
+        foreach (ValidationResult validationResult in results)
+        {
+            errorMessage.Append(validationResult.ErrorMessage + Environment.NewLine);
+        }
+
+        log.Error(errorMessage.ToString());
+        throw new Exception(errorMessage.ToString());
+    }
+
+
+}
+#endregion
