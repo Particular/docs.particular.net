ee13ddf19ff46c81440be76582c2862d6f983936
Simon Cropp <simon.cropp@gmail.com>
2015-08-27 15:51:33 +1000

    add v6 hosting

---
+++ samples/hosting/multi-hosting/Version_6/Sample/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using NServiceBus;
+
+class Program
+{
+
+    static void Main()
+    {
+        #region multi-hosting
+
+        using (IBus bus1 = StartInstance1())
+        using (IBus bus2 = StartInstance2())
+        {
+            Console.WriteLine("Press '1' to send a message from Instance1 to Instance2");
+            Console.WriteLine("Press '2' to send a message from Instance2 to Instance1");
+            Console.WriteLine("Press any key to exit");
+
+            while (true)
+            {
+                ConsoleKeyInfo key = Console.ReadKey();
+                Console.WriteLine();
+                if (key.Key == ConsoleKey.D1)
+                {
+                    bus1.Send("Samples.MultiHosting.Instance2", new MyMessage());
+                    continue;
+                }
+                if (key.Key == ConsoleKey.D2)
+                {
+                    bus2.Send("Samples.MultiHosting.Instance1", new MyMessage());
+                    continue;
+                }
+                return;
+            }
+        }
+
+        #endregion
+    }
+
+    static IBus StartInstance1()
+    {
+        #region multi-hosting-assembly-scan
+
+        BusConfiguration busConfiguration = new BusConfiguration();
+
+        busConfiguration.EndpointName("Samples.MultiHosting.Instance1");
+        busConfiguration.ExcludeAssemblies("Instance2");
+        busConfiguration.UseSerialization<JsonSerializer>();
+        busConfiguration.EnableInstallers();
+        busConfiguration.UsePersistence<InMemoryPersistence>();
+
+        return Bus.Create(busConfiguration).Start();
+
+        #endregion
+    }
+
+    static IBus StartInstance2()
+    {
+        BusConfiguration busConfiguration = new BusConfiguration();
+
+        busConfiguration.EndpointName("Samples.MultiHosting.Instance2");
+        busConfiguration.ExcludeAssemblies("Instance1");
+        busConfiguration.UseSerialization<JsonSerializer>();
+        busConfiguration.EnableInstallers();
+        busConfiguration.UsePersistence<InMemoryPersistence>();
+
+        return Bus.Create(busConfiguration).Start();
+    }
+}
---
+++ samples/hosting/windows-service/Version_6/Sample/ProgramService.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Diagnostics;
+using System.ServiceProcess;
+using NServiceBus;
+
+class ProgramService : ServiceBase
+{
+    IBus bus;
+
+    #region windowsservice-hosting-main
+
+    static void Main()
+    {
+        using (ProgramService service = new ProgramService())
+        {
+            if (Environment.UserInteractive)
+            {
+                service.OnStart(null);
+
+                Console.WriteLine("Bus created and configured");
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+
+                service.OnStop();
+
+                return;
+            }
+            Run(service);
+        }
+    }
+
+    #endregion
+
+    #region windowsservice-hosting-onstart
+
+    protected override void OnStart(string[] args)
+    {
+        BusConfiguration busConfiguration = new BusConfiguration();
+
+        busConfiguration.EndpointName("Samples.WindowsServiceAndConsole");
+        busConfiguration.UseSerialization<JsonSerializer>();
+
+        if (Environment.UserInteractive && Debugger.IsAttached)
+        {
+            busConfiguration.UsePersistence<InMemoryPersistence>();
+            busConfiguration.EnableInstallers();
+        }
+
+        bus = Bus.Create(busConfiguration).Start();
+    }
+
+    #endregion
+
+    #region windowsservice-hosting-onstop
+
+    protected override void OnStop()
+    {
+        if (bus != null)
+        {
+            bus.Dispose();
+        }
+    }
+
+    #endregion
+
+}
