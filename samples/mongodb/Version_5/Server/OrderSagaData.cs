4dceb3d0f020248d1997eb9df8b4a4e87a2be07f
Simon Cropp <simon.cropp@gmail.com>
2015-08-30 00:38:57 +1000

    add mongodb sample

---
+++ samples/mongodb/Version_5/Server/OrderSagaData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NServiceBus.MongoDB;
+using NServiceBus.Saga;
+
+#region sagadata
+
+public class OrderSagaData : 
+    IHaveDocumentVersion, 
+    IContainSagaData
+{
+    public Guid Id { get; set; }
+    public string Originator { get; set; }
+    public string OriginalMessageId { get; set; }
+
+    [Unique]
+    public Guid OrderId { get; set; }
+
+    public string OrderDescription { get; set; }
+
+
+    public int DocumentVersion { get; set; }
+}
+#endregion
---
+++ samples/mongodb/Version_5/Server/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NServiceBus;
+using NServiceBus.MongoDB;
+
+class Program
+{
+
+    static void Main()
+    {
+        #region mongoDbConfig
+
+        BusConfiguration busConfiguration = new BusConfiguration();
+        busConfiguration.EndpointName("Samples.MongoDB.Server");
+        busConfiguration.UsePersistence<MongoDBPersistence>()
+            .SetDatabaseName("SamplesMongoDBServer")
+            .SetConnectionString("mongodb://localhost:27017");
+
+        #endregion
+        busConfiguration.UseSerialization<JsonSerializer>();
+        busConfiguration.EnableInstallers();
+
+        using (IBus bus = Bus.Create(busConfiguration).Start())
+        {
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
