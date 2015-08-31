7abf5037db8a2c6bf45de0f7356b90e92d01daf5
Simon Cropp <simon.cropp@gmail.com>
2015-09-01 09:30:37 +1000

    better 3 to 4 doco

---
+++ Snippets/Snippets_4/UpgradeGuides/3to4/Upgrade.cs
@@ -0,0 +1,17 @@
+﻿namespace Snippets4.UpgradeGuides._3to4
+{
+    using System.Transactions;
+    using NServiceBus;
+
+    public class Upgrade
+    {
+        public void RevertToSerializable()
+        {
+            #region 3to4RevertToSerializable
+            Configure.Transactions.Advanced(settings =>
+               settings.IsolationLevel(IsolationLevel.Serializable));
+            #endregion
+        }
+
+    }
+}
