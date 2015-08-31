c0f573741a07ab2a61f8b3228d3e77805cad5183
Simon Cropp <simon.cropp@gmail.com>
2015-08-31 13:26:00 +1000

    add v6 castle sample

---
+++ samples/containers/castle/Version_6/Sample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using NServiceBus;
+
+static class Program
+{
+    static void Main()
+    {
+        #region ContainerConfiguration
+        BusConfiguration busConfiguration = new BusConfiguration();
+        busConfiguration.EndpointName("Samples.Castle");
+
+        WindsorContainer container = new WindsorContainer();
+        container.Register(Component.For<MyService>().Instance(new MyService()));
+
+        busConfiguration.UseContainer<WindsorBuilder>(c => c.ExistingContainer(container));
+        #endregion
+
+        busConfiguration.UseSerialization<JsonSerializer>();
+        busConfiguration.UsePersistence<InMemoryPersistence>();
+        busConfiguration.EnableInstallers();
+
+        using (IBus bus = Bus.Create(busConfiguration).Start())
+        {
+            bus.SendLocal(new MyMessage());
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
