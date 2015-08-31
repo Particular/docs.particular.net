5cb9f76980224baa8051b4f4d7e568162a773498
SzymonPobiega <szymon.pobiega@gmail.com>
2015-08-31 13:06:32 +0200

    Added migration doco on queue bindings

---
+++ Snippets/Snippets_6/UpgradeGuides/5to6/QueueBindings.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Snippets6.UpgradeGuides._5to6
+{
+    using NServiceBus.Features;
+    using NServiceBus.Transports;
+
+    #region 5to6queuebindings
+    public class FeatureThatRequiresAQueue : Feature
+    {
+        protected override void Setup(FeatureConfigurationContext context)
+        {
+            context.Settings.Get<QueueBindings>().BindSending("someAddress");
+        }
+    }
+    #endregion
+}
