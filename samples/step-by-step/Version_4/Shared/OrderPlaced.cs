007a8fb0cc8f11e50d95c0d19a4af7d330481de0
Simon Cropp <simon.cropp@gmail.com>
2015-09-01 11:05:39 +1000

    add v4 of step by step

---
+++ samples/step-by-step/Version_4/Shared/OrderPlaced.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using NServiceBus;
+
+#region OrderPlaced
+public class OrderPlaced : IEvent
+{
+    public Guid OrderId { get; set; }
+}
+#endregion
---
+++ samples/step-by-step/Version_4/Subscriber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NServiceBus;
+using NServiceBus.Installation.Environments;
+
+class Program
+{
+    static void Main()
+    {
+        Configure.Serialization.Json();
+        Configure configure = Configure.With();
+        configure.DefineEndpointName("Samples.StepByStep.Subscriber");
+        configure.Log4Net();
+        configure.DefaultBuilder();
+        configure.InMemorySagaPersister();
+        configure.UseInMemoryTimeoutPersister();
+        configure.InMemorySubscriptionStorage();
+        configure.UseTransport<Msmq>();
+        using (IStartableBus startableBus = configure.UnicastBus().CreateBus())
+        {
+            IBus bus = startableBus
+                .Start(() => configure.ForInstallationOn<Windows>().Install());
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+    }
+}
