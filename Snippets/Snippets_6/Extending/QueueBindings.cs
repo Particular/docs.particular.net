945ef9109b3e199d93bc88d8c8ba2435f4ab3841
SzymonPobiega <szymon.pobiega@gmail.com>
2015-09-01 12:26:52 +0200

    Added main doco on queue bindings

---
+++ Snippets/Snippets_6/Extending/QueueBindings.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Snippets6.Extending
+{
+    using NServiceBus.Features;
+    using NServiceBus.Transports;
+
+    #region queuebindings
+    public class FeatureThatRequiresAQueue : Feature
+    {
+        protected override void Setup(FeatureConfigurationContext context)
+        {
+            context.Settings.Get<QueueBindings>().BindSending("someAddress");
+        }
+    }
+    #endregion
+}
