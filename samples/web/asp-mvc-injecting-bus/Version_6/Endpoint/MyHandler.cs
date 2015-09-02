0bbdeedb2443179162e0c7c9d00016aa966c340f
Simon Cropp <simon.cropp@gmail.com>
2015-09-02 14:46:01 +1000

    update injecting bus

---
+++ samples/web/asp-mvc-injecting-bus/Version_6/Endpoint/MyHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using NServiceBus;
+
+public class MyHandler : IHandleMessages<MyMessage>
+{
+    public void Handle(MyMessage message)
+    {
+        Console.WriteLine("Hello from MyHandler");
+    }
+}
---
+++ samples/web/asp-mvc-injecting-bus/Version_6/Endpoint/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NServiceBus;
+
+static class Program
+{
+    static void Main()
+    {
+        BusConfiguration busConfiguration = new BusConfiguration();
+        busConfiguration.EndpointName("Samples.Mvc.Endpoint");
+        busConfiguration.UseSerialization<JsonSerializer>();
+        busConfiguration.UsePersistence<InMemoryPersistence>();
+        busConfiguration.EnableInstallers();
+
+        using (IBus bus = Bus.Create(busConfiguration).Start())
+        {
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
