71b897606843d8d4a7d78cd824ea36fdc4d06cf8
Simon Cropp <simon.cropp@gmail.com>
2015-10-07 13:05:26 +1100

    move over to async

--- samples/serializers/xml/Version_6/Sample/CreateOrderHandler.cs
+++ samples/serializers/xml/Version_6/Sample/CreateOrderHandler.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.Threading.Tasks;
 using NServiceBus;
 using XmlSample;
 
 public class CreateOrderHandler : IHandleMessages<CreateOrder>
 {
-    public void Handle(CreateOrder message)
+    public Task Handle(CreateOrder message)
     {
         Console.WriteLine("Order received");
+        return Task.FromResult(0);
     }
 }
