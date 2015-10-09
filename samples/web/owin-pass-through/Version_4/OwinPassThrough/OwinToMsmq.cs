37650b2fd11a0d64974211026e1b7289b0e88654
Simon Cropp <simon.cropp@gmail.com>
2015-10-09 11:46:04 +1100

    finish owin sample

---
+++ samples/web/owin-pass-through/Version_4/OwinPassThrough/OwinToMsmq.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Threading.Tasks;
+
+namespace OwinPassThrough
+{
+
+    #region OwinToMsmq
+
+    using System.IO;
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
+    public class OwinToMsmq
+    {
+        string queuePath;
+
+        public OwinToMsmq(string queuePath)
+        {
+            this.queuePath = queuePath;
+        }
+
+        public Func<AppFunc, AppFunc> Middleware()
+        {
+            return x => (AppFunc) Invoke;
+        }
+
+        public async Task Invoke(IDictionary<string, object> environment)
+        {
+            using (Stream memoryStream = await RequestAsStream(environment))
+            using (MessageQueue queue = new MessageQueue(queuePath))
+            using (Message message = new Message())
+            {
+                message.BodyStream = memoryStream;
+                IDictionary<string, string[]> requestHeaders = (IDictionary<string, string[]>) environment["owin.RequestHeaders"];
+                string messageType = requestHeaders["MessageType"].Single();
+                message.Extension = MsmqHeaderSerializer.CreateHeaders(messageType);
+                queue.Send(message, MessageQueueTransactionType.Single);
+            }
+        }
+
+        async Task<Stream> RequestAsStream(IDictionary<string, object> environment)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            using (Stream requestStream = (Stream)environment["owin.RequestBody"])
+            {
+                await requestStream.CopyToAsync(memoryStream);
+            }
+            return memoryStream;
+        }
+    }
+
+    #endregion
+}
---
+++ samples/web/owin-pass-through/Version_4/OwinPassThrough/StreamWrapper.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+
+
+class StreamWrapper : Stream
+{
+    Stream stream;
+    int length;
+
+    public StreamWrapper(Stream stream, int length)
+    {
+        this.stream = stream;
+        this.length = length;
+    }
+
+    public override void Flush()
+    {
+        stream.Flush();
+    }
+
+    public override long Seek(long offset, SeekOrigin origin)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void SetLength(long value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        return stream.Read(buffer, offset, count);
+    }
+
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool CanRead
+    {
+        get { return stream.CanRead; }
+    }
+
+    public override bool CanSeek
+    {
+        get { return false; }
+    }
+
+    public override bool CanWrite
+    {
+        get { return stream.CanWrite; }
+    }
+
+    public override long Length
+    {
+        get { return length; }
+    }
+
+    public override long Position
+    {
+        get { return stream.Position; }
+        set
+        {
+            if (value == 0)
+            {
+                return;
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
