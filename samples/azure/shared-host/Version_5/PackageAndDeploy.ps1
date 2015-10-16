741e6f2f13d9c580d6aa76b050142a98d9b3b83e
Sean Feldman <feldman.sean@gmail.com>
2015-10-15 18:57:05 -0600

    fixing PS script to create endpoints container if such is missing (smoothing F5 experience)

--- samples/azure/shared-host/Version_5/PackageAndDeploy.ps1
+++ samples/azure/shared-host/Version_5/PackageAndDeploy.ps1
@@ -34,6 +34,14 @@ Write-Host "Zips are created at $zipsDir location" -ForegroundColor Cyan
 Write-Host "Deploying endpoints..." -ForegroundColor Cyan
 
 $context = New-AzureStorageContext -Local
-ls -path $zipsDir -file -recurse | Set-AzureStorageBlobContent -Container "endpoints" -Context $context -Force
+$containerName = "endpoints"
+$container = Get-AzureStorageContainer -Name $containerName -Context $context -ErrorAction SilentlyContinue 
+if (!$container)
+{
+	Write-Host "Creating '$containerName' container for uploaded endpoints" -ForegroundColor Cyan
+	New-AzureStorageContainer -Name $containerName -Permission Off -Context $context
+}
+
+ls -path $zipsDir -file -recurse | Set-AzureStorageBlobContent -Container $containerName -Context $context -Force
     
 Write-Host "Endpoints deployed to storage account " $context.StorageAccountName -ForegroundColor Cyan
