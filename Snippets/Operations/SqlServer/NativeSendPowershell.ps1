52d5b47caf4f3ba1b5dd213cb1b661caec24b109
Pete <peter.giles@particular.net>
2015-10-21 09:19:00 +1100

    fixed indents

--- Snippets/Operations/SqlServer/NativeSendPowershell.ps1
+++ Snippets/Operations/SqlServer/NativeSendPowershell.ps1
@@ -5,16 +5,16 @@ Add-Type -AssemblyName System.Data
 
 Function SendMessage
 {
-    param(
-        [Parameter(Mandatory=$true)]
-        [string] $connectionString,
-        [Parameter(Mandatory=$true)]
-        [string] $queue,
+	param(
 		[Parameter(Mandatory=$true)]
-        [string] $messageBody,
+		[string] $connectionString,
 		[Parameter(Mandatory=$true)]
-        [object] $headers
-    )
+		[string] $queue,
+		[Parameter(Mandatory=$true)]
+		[string] $messageBody,
+		[Parameter(Mandatory=$true)]
+		[object] $headers
+	)
 
 	$sqlConnection = new-object System.Data.SqlClient.SqlConnection $connectionString
 
@@ -23,12 +23,12 @@ Function SendMessage
 	$sqlCommand = $sqlConnection.CreateCommand()
 
 	$sqlCommand.CommandText = "INSERT INTO [" + $queue + "] ([Id],[Recoverable],[Headers],[Body]) VALUES (@Id, @Recoverable, @Headers, @Body)";
-	
+
 	$sqlCommand.Parameters.Add("Id", [System.Data.SqlDbType]::UniqueIdentifier).Value = [System.Guid]::NewGuid()
 	$serializeHeaders = ConvertTo-Json $headers;
 	$sqlCommand.Parameters.Add("Headers", [System.Data.SqlDbType]::VarChar).Value = $serializeHeaders
 
-    $UTF8 = [System.Text.Encoding]::UTF8
+	$UTF8 = [System.Text.Encoding]::UTF8
 
 	$sqlCommand.Parameters.Add("Body", [System.Data.SqlDbType]::VarBinary).Value = $UTF8.GetBytes("$messageBody")
 	$sqlCommand.Parameters.Add("Recoverable", [System.Data.SqlDbType]::Bit).Value = 1
@@ -41,10 +41,10 @@ Function SendMessage
 Function Usage
 {
 
-		# region sqlserver-powershell-nativesend-usage
+	# region sqlserver-powershell-nativesend-usage
 
-        SendMessage "Data Source=.\SQLEXPRESS;Initial Catalog=samples;Integrated Security=True" "sqlserverNativeSendTests" "{'Property':'Value'}" @{"NServiceBus.EnclosedMessageTypes" = "Operations.SqlServer.NativeSendTests+MessageToSend"}
+	SendMessage "Data Source=.\SQLEXPRESS;Initial Catalog=samples;Integrated Security=True" "sqlserverNativeSendTests" "{'Property':'Value'}" @{"NServiceBus.EnclosedMessageTypes" = "Operations.SqlServer.NativeSendTests+MessageToSend"}
 
-        # endregion
-       
+	# endregion
+
 }
