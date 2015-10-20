6b600cd0646079ece4059125c732266444340ea8
Pete <peter.giles@particular.net>
2015-10-21 09:47:12 +1100

    post-merge trauma fixed

--- Snippets/Operations/SqlServer/NativeSendPowershell.ps1
+++ Snippets/Operations/SqlServer/NativeSendPowershell.ps1
@@ -6,24 +6,13 @@ Add-Type -AssemblyName System.Data
 Function SendMessage
 {
 	param(
-		[Parameter(Mandatory=$true)]
-<<<<<<< HEAD
-		[string] $connectionString,
-		[Parameter(Mandatory=$true)]
-		[string] $queue,
-		[Parameter(Mandatory=$true)]
-		[string] $messageBody,
-		[Parameter(Mandatory=$true)]
-		[object] $headers
-	)
-=======
-        [ValidateNotNullOrEmpty()]
+	    [Parameter(Mandatory=$true)]
+		[ValidateNotNullOrEmpty()]
 		[string] $ConnectionString,
 
 		[Parameter(Mandatory=$true)]
         [ValidateNotNullOrEmpty()] 
 		[string] $Queue,
->>>>>>> 4a73802c7e342ca510e800ac9aab8d6c611f4eea
 
 		[Parameter(Mandatory=$true)]
         [ValidateNotNullOrEmpty()] 
@@ -36,17 +25,7 @@ Function SendMessage
 
 	$UTF8 = [System.Text.Encoding]::UTF8
 
-<<<<<<< HEAD
-	$sqlCommand.CommandText = "INSERT INTO [" + $queue + "] ([Id],[Recoverable],[Headers],[Body]) VALUES (@Id, @Recoverable, @Headers, @Body)";
 
-	$sqlCommand.Parameters.Add("Id", [System.Data.SqlDbType]::UniqueIdentifier).Value = [System.Guid]::NewGuid()
-	$serializeHeaders = ConvertTo-Json $headers;
-	$sqlCommand.Parameters.Add("Headers", [System.Data.SqlDbType]::VarChar).Value = $serializeHeaders
-
-	$UTF8 = [System.Text.Encoding]::UTF8
-
-	$sqlCommand.Parameters.Add("Body", [System.Data.SqlDbType]::VarBinary).Value = $UTF8.GetBytes("$messageBody")
-=======
 	$sqlConnection = New-Object System.Data.SqlClient.SqlConnection($ConnectionString)
 	$sqlConnection.Open()
 	
@@ -55,9 +34,7 @@ Function SendMessage
 	$sqlCommand.Parameters.Add("Id", [System.Data.SqlDbType]::UniqueIdentifier).Value = [System.Guid]::NewGuid()
 	$serializedHeaders = ConvertTo-Json $Headers
 	$sqlCommand.Parameters.Add("Headers", [System.Data.SqlDbType]::VarChar).Value = $serializedHeaders
-	
 	$sqlCommand.Parameters.Add("Body", [System.Data.SqlDbType]::VarBinary).Value = $UTF8.GetBytes($MessageBody)
->>>>>>> 4a73802c7e342ca510e800ac9aab8d6c611f4eea
 	$sqlCommand.Parameters.Add("Recoverable", [System.Data.SqlDbType]::Bit).Value = 1
 	$sqlCommand.ExecuteNonQuery()
 
@@ -67,15 +44,6 @@ Function SendMessage
 Function Usage
 {
 	# region sqlserver-powershell-nativesend-usage
-
-<<<<<<< HEAD
-	# startcode sqlserver-powershell-nativesend-usage
-
-	SendMessage "Data Source=.\SQLEXPRESS;Initial Catalog=samples;Integrated Security=True" "sqlserverNativeSendTests" "{'Property':'Value'}" @{"NServiceBus.EnclosedMessageTypes" = "Operations.SqlServer.NativeSendTests+MessageToSend"}
-
-	# endcode
-
-=======
 
 	SendMessage -ConnectionString "Data Source=.\SQLEXPRESS;Initial Catalog=samples;Integrated Security=True" `
 				-Queue "sqlserverNativeSendTests" `
@@ -96,5 +64,5 @@ Function Usage
 	SendMessage @payload
 	
 	# endregion
->>>>>>> 4a73802c7e342ca510e800ac9aab8d6c611f4eea
+
 }
