1d551217bf11ca6394981902a0d27fb4f82b93f7
Simon Cropp <simon.cropp@gmail.com>
2015-10-28 18:13:00 +1100

    fix build

--- samples/header-manipulation/Version_6/Sample/MyHandler.cs
+++ samples/header-manipulation/Version_6/Sample/MyHandler.cs
@@ -7,19 +7,12 @@ using NServiceBus.Logging;
 
 public class MyHandler : IHandleMessages<MyMessage>
 {
-    IBus bus;
     static ILog logger = LogManager.GetLogger(typeof(MyHandler));
 
-    public MyHandler(IBus bus)
-    {
-        this.bus = bus;
-    }
-
-    public Task Handle(MyMessage message)
+    public Task Handle(MyMessage message, IMessageHandlerContext context)
     {
         logger.Info("Hello from MyHandler");
-        foreach (string line in bus.CurrentMessageContext
-            .Headers.OrderBy(x => x.Key)
+        foreach (string line in context.MessageHeaders.OrderBy(x => x.Key)
             .Select(x => string.Format("Key={0}, Value={1}", x.Key, x.Value)))
         {
             logger.Info(line);
