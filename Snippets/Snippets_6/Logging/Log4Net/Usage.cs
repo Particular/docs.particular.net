35ddc71646e4013c68ee20a43be60476e542270c
Simon Cropp <simon.cropp@gmail.com>
2015-10-30 13:44:33 +1100

    add v6 logging

---
+++ Snippets/Snippets_6/Logging/Log4Net/Usage.cs
@@ -0,0 +1,36 @@
+﻿namespace Snippets6.Logging.Log4Net
+{
+    using log4net.Appender;
+    using log4net.Config;
+    using log4net.Core;
+    using log4net.Layout;
+    using NServiceBus.Log4Net;
+    using NServiceBus.Logging;
+
+    public class Usage
+    {
+        public Usage()
+        {
+            #region Log4NetInCode
+
+            PatternLayout layout = new PatternLayout
+            {
+                ConversionPattern = "%d [%t] %-5p %c [%x] - %m%n"
+            };
+            layout.ActivateOptions();
+            ConsoleAppender consoleAppender = new ConsoleAppender
+            {
+                Threshold = Level.Debug,
+                Layout = layout
+            };
+            // Note that ActivateOptions is required in NSB 5 and above
+            consoleAppender.ActivateOptions();
+
+            BasicConfigurator.Configure(consoleAppender);
+
+            LogManager.Use<Log4NetFactory>();
+
+            #endregion
+        }
+    }
+}
---
+++ Snippets/Snippets_6/Logging/ProvideConfiguration.cs
@@ -0,0 +1,20 @@
+﻿namespace Snippets6.Logging
+{
+    using NServiceBus.Config;
+    using NServiceBus.Config.ConfigurationSource;
+
+    #region LoggingThresholdFromIProvideConfiguration
+
+    public class ProvideConfiguration : IProvideConfiguration<Logging>
+    {
+        public Logging GetConfiguration()
+        {
+            return new Logging
+            {
+                Threshold = "Info"
+            };
+        }
+    }
+
+    #endregion
+}
