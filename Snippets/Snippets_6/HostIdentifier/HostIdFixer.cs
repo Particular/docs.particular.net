e25718ad89f4d29894861ba58c3824747fc1bed2
Simon Cropp <simon.cropp@gmail.com>
2015-11-02 11:40:53 +1100

    add v6 host id

---
+++ Snippets/Snippets_6/HostIdentifier/HostIdFixer.cs
@@ -0,0 +1,29 @@
+﻿namespace Snippets6.HostIdentifier
+{
+    using System;
+    using NServiceBus;
+
+    public class HostIdFixer
+    {
+        public void Start()
+        {
+            #region HostIdFixer
+
+            BusConfiguration busConfiguration = new BusConfiguration();
+            busConfiguration.UniquelyIdentifyRunningInstance()
+                .UsingNames("endpointName", Environment.MachineName);
+            // or
+            Guid hostId = CreateMyUniqueIdThatIsTheSameAcrossRestarts();
+            busConfiguration.UniquelyIdentifyRunningInstance()
+                .UsingCustomIdentifier(hostId);
+            
+            #endregion
+        }
+
+        Guid CreateMyUniqueIdThatIsTheSameAcrossRestarts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+    
