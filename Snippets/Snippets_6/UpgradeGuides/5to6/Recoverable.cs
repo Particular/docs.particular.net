7ea425b28d74d308192e81ff05b70c650c2a0b89
danielmarbach <daniel.marbach@particular.net>
2015-11-05 14:57:24 +0100

    Adding snippets for NonDurable and DiscardIfNotReceivedBefore

---
+++ Snippets/Snippets_6/UpgradeGuides/5to6/Recoverable.cs
@@ -0,0 +1,23 @@
+﻿namespace Snippets6.UpgradeGuides._5to6
+{
+    using NServiceBus;
+    using NServiceBus.DeliveryConstraints;
+    using NServiceBus.TransportDispatch;
+
+    class Recoverable
+    {
+        public Recoverable()
+        {
+            RoutingContext context = null;
+
+            #region SetDeliveryConstraintNonDurable
+            context.AddDeliveryConstraint(new NonDurableDelivery());
+            #endregion
+
+            #region ReadDeliveryConstraintNonDurable
+            NonDurableDelivery constraint;
+            context.TryGetDeliveryConstraint(out constraint);
+            #endregion
+        }
+    }
+}
