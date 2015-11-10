59c70bb1436b663c5f9ad4a54d392ace50f9b6b8
Simon Cropp <simon.cropp@gmail.com>
2015-11-10 16:18:55 +1100

    typos

--- samples/callbacks/Version_3/Sender/ObjectResponseMessageHandler.cs
+++ samples/callbacks/Version_3/Sender/ObjectResponseMessageHandler.cs
@@ -3,7 +3,6 @@ using NServiceBus;
 #region ObjectResponseMessageHandler
 public class ObjectResponseMessageHandler : IHandleMessages<ObjectResponseMessage>
 {
-
     public void Handle(ObjectResponseMessage message)
     {
     }
