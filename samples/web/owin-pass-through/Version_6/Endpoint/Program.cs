600df502d0951d5fcb2b613daa3b0bc65f70d405
Simon Cropp <simon.cropp@gmail.com>
2015-11-12 12:53:46 +1100

    add v6 owin sample

---
+++ samples/web/owin-pass-through/Version_6/Endpoint/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Hosting;
+using NServiceBus;
+using NServiceBus.Logging;
+using Owin;
+
+static class Program
+{
+
+    static void Main()
+    {
+        AsyncMain().GetAwaiter().GetResult();
+    }
+
+    static async Task AsyncMain()
+    {
+        LogManager.Use<DefaultFactory>()
+            .Level(LogLevel.Info);
+#region startbus
+        BusConfiguration busConfiguration = new BusConfiguration();
+        busConfiguration.EndpointName("Samples.OwinPassThrough");
+        busConfiguration.SendFailedMessagesTo("error");
+        busConfiguration.UseSerialization<JsonSerializer>();
+        busConfiguration.UsePersistence<InMemoryPersistence>();
+        busConfiguration.EnableInstallers();
+        using (IBus bus = await Bus.Create(busConfiguration).StartAsync())
+        using (StartOwinHost(bus))
+        {
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+#endregion
+    }
+#region startowin
+    static IDisposable StartOwinHost(IBus bus)
+    {
+        string baseUrl = "http://localhost:12345/";
+        StartOptions startOptions = new StartOptions(baseUrl)
+        {
+            ServerFactory = "Microsoft.Owin.Host.HttpListener",
+        };
+
+        return WebApp.Start(startOptions, builder =>
+        {
+            builder.UseCors(CorsOptions.AllowAll);
+            MapToBus(builder, bus);
+            MapToMsmq(builder);
+        });
+    }
+
+    static void MapToBus(IAppBuilder builder, IBus bus)
+    {
+        OwinToBus owinToBus = new OwinToBus(bus);
+        builder.Map("/to-bus", app => { app.Use(owinToBus.Middleware()); });
+    }
+
+    static void MapToMsmq(IAppBuilder builder)
+    {
+        string queue = Environment.MachineName + @"\private$\Samples.OwinPassThrough";
+        OwinToMsmq owinToMsmq = new OwinToMsmq(queue);
+        builder.Map("/to-msmq", app => { app.Use(owinToMsmq.Middleware()); });
+    }
+#endregion
+
+}
