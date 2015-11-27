7da94e6adcc826e2ded41f44a72810a737160684
danielmarbach <daniel.marbach@particular.net>
2015-11-27 15:47:34 +0100

    FeatureStartupTask snippets update

---
+++ Snippets/Snippets_6/Features/FeatureConfiguration.cs
@@ -0,0 +1,70 @@
+﻿// ReSharper disable UnusedParameter.Local
+namespace Snippets6.Features
+{
+    using System;
+    using System.Threading.Tasks;
+    using NServiceBus;
+    using NServiceBus.Config;
+    using NServiceBus.Features;
+    using NServiceBus.Pipeline;
+
+    class SecondLevelRetries : Feature
+    {
+
+#region FeatureSetup
+        protected override void Setup(FeatureConfigurationContext context)
+        {
+            var retriesConfig = context.Settings.GetConfigSection<SecondLevelRetriesConfig>();
+
+            var retryPolicy = new SecondLevelRetryPolicy(retriesConfig.NumberOfRetries, retriesConfig.TimeIncrease);
+            context.Container.RegisterSingleton(typeof(SecondLevelRetryPolicy), retryPolicy);
+
+            context.Pipeline.Register<SecondLevelRetriesBehavior.Registration>();
+        }
+#endregion
+
+        async Task EndpointConfiguration()
+        {
+#region EnableDisableFeatures
+            var configuration = new BusConfiguration();
+            
+            // enable delayed delivery feature since SLR relies on it
+            configuration.EnableFeature<DelayedDeliveryFeature>();
+
+            // this is not required if the feature uses EnableByDefault()
+            configuration.EnableFeature<SecondLevelRetries>();
+
+            // we can disable features we won't use
+            configuration.DisableFeature<Sagas>();
+
+            var endpoint = await Endpoint.Create(configuration);
+#endregion
+        }
+
+        class SecondLevelRetriesBehavior
+        {
+            public class Registration : RegisterStep
+            {
+                public Registration()
+                    : base(string.Empty, typeof(object), null)
+                {
+                }
+            }
+        }
+
+        class SecondLevelRetryPolicy
+        {
+            public SecondLevelRetryPolicy(int numberOfRetries, TimeSpan timeIncrease)
+            {
+            }
+        }
+
+        class DelayedDeliveryFeature : Feature
+        {
+            protected override void Setup(FeatureConfigurationContext context)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
