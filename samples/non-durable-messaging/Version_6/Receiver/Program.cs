6e558a04a29d7a134eddc640ca1ab636a9b6bfa3
Simon Cropp <simon.cropp@gmail.com>
2015-11-30 12:23:16 +1100

    add v6 non durable

---
+++ samples/non-durable-messaging/Version_6/Receiver/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+static class Program
+{
+    static void Main()
+    {
+        AsyncMain().GetAwaiter().GetResult();
+    }
+
+    static async Task AsyncMain()
+    {
+        BusConfiguration busConfiguration = new BusConfiguration();
+        busConfiguration.Transactions()
+            .Disable();
+        busConfiguration.EndpointName("Samples.MessageDurability.Receiver");
+        busConfiguration.UseSerialization<JsonSerializer>();
+        busConfiguration.EnableInstallers();
+        busConfiguration.UsePersistence<InMemoryPersistence>();
+
+        IEndpointInstance endpoint = await Endpoint.Start(busConfiguration);
+        try
+        {
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+        finally
+        {
+            await endpoint.Stop();
+        }
+    }
+}
