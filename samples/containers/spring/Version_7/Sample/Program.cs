6641ec1645ac59213051f1a1f543a0d334d68950
Simon Cropp <simon.cropp@gmail.com>
2015-12-03 14:31:15 +1100

    add new spring sample

---
+++ samples/containers/spring/Version_7/Sample/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using Spring.Context.Support;
+
+static class Program
+{
+    static void Main()
+    {
+        AsyncMain().GetAwaiter().GetResult();
+    }
+
+    static async Task AsyncMain()
+    {
+        #region ContainerConfiguration
+        BusConfiguration busConfiguration = new BusConfiguration();
+        busConfiguration.EndpointName("Samples.Spring");
+
+        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        applicationContext.ObjectFactory.RegisterSingleton("MyService", new MyService());
+        busConfiguration.UseContainer<SpringBuilder>(c => c.ExistingApplicationContext(applicationContext));
+        #endregion
+        busConfiguration.UseSerialization<JsonSerializer>();
+        busConfiguration.UsePersistence<InMemoryPersistence>();
+
+        busConfiguration.SendFailedMessagesTo("error");
+
+        IEndpointInstance endpoint = await Endpoint.Start(busConfiguration);
+        try
+        {
+            IBusContext busContext = endpoint.CreateBusContext();
+            await busContext.SendLocal(new MyMessage());
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+        finally
+        {
+            await endpoint.Stop();
+        }
+    }
+}
