19159df4ebe714b404f73691ee372bd17e03d68a
Simon Cropp <simon.cropp@gmail.com>
2015-12-15 16:02:18 +1100

    move over to interface context

--- Snippets/Snippets_6/UpgradeGuides/5to6/OutgoingBehavior.cs
+++ Snippets/Snippets_6/UpgradeGuides/5to6/OutgoingBehavior.cs
@@ -6,9 +6,9 @@
     using NServiceBus.Pipeline.OutgoingPipeline;
 
     #region 5to6header-outgoing-behavior
-    public class OutgoingBehavior : Behavior<OutgoingLogicalMessageContext>
+    public class OutgoingBehavior : Behavior<IOutgoingLogicalMessageContext>
     {
-        public override async Task Invoke(OutgoingLogicalMessageContext context, Func<Task> next)
+        public override async Task Invoke(IOutgoingLogicalMessageContext context, Func<Task> next)
         {
             context.Headers["MyCustomHeader"] = "My custom value";
             await next();
