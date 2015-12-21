630cba13845d9921e8d44dccbcd2bdbcc974f33e
andreasohlund <andreas.ohlund@particular.net>
2015-12-21 15:43:46 +0100

    Snippets for upgrade guide

---
+++ Snippets/Snippets_6/UpgradeGuides/5to6/Transactions.cs
@@ -0,0 +1,27 @@
+﻿namespace Snippets6.UpgradeGuides._5to6
+{
+    using NServiceBus;
+    using NServiceBus.Configuration.AdvanceExtensibility;
+    using NServiceBus.ConsistencyGuarantees;
+
+    public class Transactions
+    {
+        public Transactions()
+        {
+            BusConfiguration busConfiguration = new BusConfiguration();
+            #region 5to6IsDtcTransactionsSuppressed
+            if (busConfiguration.GetSettings().GetRequiredTransactionModeForReceives() != TransportTransactionMode.TransactionScope)
+            {
+                //your code here
+            }
+            #endregion
+
+            #region 5to6IsTransactionsEnabled
+            if (busConfiguration.GetSettings().GetRequiredTransactionModeForReceives() != TransportTransactionMode.None)
+            {
+                //your code here
+            }
+            #endregion
+        }
+    }
+}
