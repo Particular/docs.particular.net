2937086829be092a130fbf3ead3f2913b712cd78
Igal Tabachnik <hmemcpy@gmail.com>
2015-12-23 15:28:56 +0200

    Updating samples

---
+++ samples/containers/ninject/Version_6/Sample/MyService.cs
@@ -0,0 +1,11 @@
+﻿using NServiceBus.Logging;
+
+public class MyService
+{
+    static ILog logger = LogManager.GetLogger<MyService>();
+    
+    public void WriteHello()
+    {
+        logger.Info("Hello from MyService.");
+    }
+}
