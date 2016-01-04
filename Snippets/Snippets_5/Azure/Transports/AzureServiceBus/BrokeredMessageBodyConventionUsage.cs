751f533435b3e0dc90482f96138157283588ca26
Sean Feldman <feldman.sean@gmail.com>
2016-01-04 14:35:20 -0700

    Adding doco/snippets for BrokeredMessageBodyConventionUsage

---
+++ Snippets/Snippets_5/Azure/Transports/AzureServiceBus/BrokeredMessageBodyConventionUsage.cs
@@ -0,0 +1,37 @@
+﻿namespace Snippets5.Azure.Transports.AzureServiceBus
+{
+    using System.IO;
+    using Microsoft.ServiceBus.Messaging;
+    using NServiceBus.Azure.Transports.WindowsAzureServiceBus;
+
+    public class BrokeredMessageBodyConventionUsage
+    {
+        public void Snippet()
+        {
+            #region ASB-outgoing-message-convention 6.3
+
+            BrokeredMessageBodyConversion.InjectBody = bytes =>
+            {
+                MemoryStream messageAsStream = new MemoryStream(bytes);
+                return new BrokeredMessage(messageAsStream);
+            };
+
+            #endregion
+
+            #region ASB-incoming-message-convention 6.3
+
+            BrokeredMessageBodyConversion.ExtractBody = brokeredMessage =>
+            {
+                using (var stream = new MemoryStream())
+                using (var body = brokeredMessage.GetBody<Stream>())
+                {
+                    body.CopyTo(stream);
+                    return stream.ToArray();
+                }
+            };
+
+            #endregion
+
+        }
+    }
+}
