59eb4638463c69cbf41eb0b4bc27f88fee1584b3
indualagarsamy <indu.alagarsamy@particular.net>
2016-01-07 15:14:44 -0800

    Added IBus deprecation as the first thing. Moved Message Handlers and IWantToRunWhenBusStartsAndStops also to the top.

---
+++ Snippets/Snippets_6/UpgradeGuides/5to6/StartupInterface.cs
@@ -0,0 +1,25 @@
+﻿namespace Snippets6.Handlers
+{
+    using System.Threading.Tasks;
+    using NServiceBus;
+
+    #region 5to6-IWantToRunWhenBusStartsAndStops
+    public class Bootstrapper : IWantToRunWhenBusStartsAndStops
+    {
+        public Task Start(IBusSession session)
+        {
+            // Do your startup action here. 
+            // Either mark your Start method as async or do the following
+            return Task.FromResult(0);
+
+        }
+
+        public Task Stop(IBusSession session)
+        {
+            // Do your cleanup action here. 
+            // Either mark your Stop method as async or do the following
+            return Task.FromResult(0);
+        }
+    }
+    #endregion    
+}
