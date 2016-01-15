498eb409df1528fceb0ce4c1f5568fe89d9fa418
Sean Feldman <feldman.sean@gmail.com>
2016-01-14 23:59:48 -0700

    core v6 version of the sample

---
+++ samples/azure/native-integration-asb/Version_6/Receiver/NativeMessageHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+
+#region NativeMessageHandler
+
+public class NativeMessageHandler : IHandleMessages<NativeMessage>
+{
+    ILog logger = LogManager.GetLogger<NativeMessageHandler>();
+
+    public Task Handle(NativeMessage message, IMessageHandlerContext context)
+    {
+        logger.InfoFormat("Message content: {0}", message.Content);
+        logger.InfoFormat("Received native message sent on {0} UTC", message.SendOnUtc);
+        return Task.FromResult(0);
+    }
+}
+
+#endregion
