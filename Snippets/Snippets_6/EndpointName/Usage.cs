e414620b2ec3a191775c86f1a089ad58bbec4bc0
John Simons <john.simons@nservicebus.com>
2016-01-29 16:20:49 +1000

    Endpoint name snippets updated to v6 of NServiceBus
    Closes #1066

---
+++ Snippets/Snippets_6/EndpointName/Usage.cs
@@ -0,0 +1,18 @@
+﻿namespace Snippets5.EndpointName
+{
+    using NServiceBus;
+
+    public class Usage
+    {
+        public Usage()
+        {
+            #region EndpointNameCode
+
+            BusConfiguration busConfiguration = new BusConfiguration();
+            busConfiguration.EndpointName("MyEndpoint");
+        
+            #endregion
+        }
+
+    }
+}
