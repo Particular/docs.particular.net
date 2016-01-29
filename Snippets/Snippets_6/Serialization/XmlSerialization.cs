5903730e88345a21ee9b16736dd94044f62e959f
John Simons <john.simons@nservicebus.com>
2016-01-29 15:44:33 +1000

    Added Serialization snippets for v6 core
    Closes #1078

---
+++ Snippets/Snippets_6/Serialization/XmlSerialization.cs
@@ -0,0 +1,17 @@
+﻿namespace Snippets5.Serialization
+{
+    using NServiceBus;
+
+    public class XmlSerialization
+    {
+        public void Simple()
+        {
+            #region XmlSerialization
+
+            BusConfiguration busConfiguration = new BusConfiguration();
+            busConfiguration.UseSerialization<XmlSerializer>();
+
+            #endregion
+        }
+    }
+}
