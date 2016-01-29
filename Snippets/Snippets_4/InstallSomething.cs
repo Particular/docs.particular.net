32dede4419a8e4619e7138238b3fa2db2d8a2dfa
John Simons <john.simons@nservicebus.com>
2016-01-29 15:24:19 +1000

    Installers page clean-up and update to v6
    Closes #1096

---
+++ Snippets/Snippets_4/InstallSomething.cs
@@ -0,0 +1,16 @@
+﻿namespace Snippets4
+{
+    using NServiceBus.Installation;
+
+    #region InstallSomething
+
+    public class MyInstaller : INeedToInstallSomething
+    {
+        public void Install(string identity)
+        {
+            // My code to install something
+        }
+    }
+
+    #endregion
+}
---
+++ Snippets/Snippets_5/InstallSomething.cs
@@ -0,0 +1,17 @@
+﻿namespace Snippets4
+{
+    using NServiceBus;
+    using NServiceBus.Installation;
+
+    #region InstallSomething
+
+    public class MyInstaller : INeedToInstallSomething
+    {
+        public void Install(string identity, Configure config)
+        {
+            // My code to install something
+        }
+    }
+
+    #endregion
+}
