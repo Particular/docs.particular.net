d0409e236d66ada1de5d3306887a900e201f0bb7
John Simons <john.simons@nservicebus.com>
2016-02-02 12:08:27 +1000

    Updated profiles snippets for NServiceBus v6
    Closes #1073

---
+++ Snippets/Snippets_6/Profiles/ConfigureLogging.cs
@@ -0,0 +1,15 @@
+﻿using NServiceBus;
+
+#region configure_logging
+class YourProfileLoggingHandler : NServiceBus.Hosting.Profiles.IConfigureLoggingForProfile<YourProfile>
+{
+    public void Configure(IConfigureThisEndpoint specifier)
+    {
+        // setup your logging infrastructure here
+    }
+}
+#endregion
+
+internal class YourProfile : IProfile
+{
+}
