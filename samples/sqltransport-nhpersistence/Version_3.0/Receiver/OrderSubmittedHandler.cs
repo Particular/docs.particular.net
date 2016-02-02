b56df207d190d835c105a21cf8b71023aeea4830
Simon Cropp <simon.cropp@gmail.com>
2016-02-02 12:15:22 +1100

    use SynchronizedStorageSession

--- samples/sqltransport-nhpersistence/Version_3.0/Receiver/OrderSubmittedHandler.cs
+++ samples/sqltransport-nhpersistence/Version_3.0/Receiver/OrderSubmittedHandler.cs
@@ -1,25 +1,16 @@
 using System;
 using System.Threading.Tasks;
 using NServiceBus;
-using NHibernate;
 
 public class OrderSubmittedHandler : IHandleMessages<OrderSubmitted>
 {
-    ISession session;
-
-    public OrderSubmittedHandler(ISession session)
-    {
-        this.session = session;
-    }
-
-
     public async Task Handle(OrderSubmitted message, IMessageHandlerContext context)
     {
         Console.WriteLine("Order {0} worth {1} submitted", message.OrderId, message.Value);
 
         #region StoreUserData
 
-        session.Save(new Order
+        context.SynchronizedStorageSession.Session().Save(new Order
         {
             OrderId = message.OrderId,
             Value = message.Value
