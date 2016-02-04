1a4149c6f8eb7cb9e4692682d88f195cbd95b1c2
WilliamBZA <william.brander@gmail.com>
2016-02-04 12:12:12 +0200

    Updated sample project to include v6 sample

---
+++ samples/containers/ninject/Version_6/Sample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Ninject;
+using NServiceBus;
+
+public class Program
+{
+    static void Main()
+    {
+        new Program().Start().Wait();
+    }
+
+    async Task Start()
+    {
+        #region ContainerConfiguration
+        BusConfiguration busConfiguration = new BusConfiguration();
+        busConfiguration.EndpointName("Samples.Ninject");
+
+        StandardKernel kernel = new StandardKernel();
+        kernel.Bind<MyService>().ToConstant(new MyService());
+        busConfiguration.UseContainer<NinjectBuilder>(c => c.ExistingKernel(kernel));
+        #endregion
+        busConfiguration.UseSerialization<JsonSerializer>();
+        busConfiguration.UsePersistence<InMemoryPersistence>();
+        busConfiguration.EnableInstallers();
+
+
+        IEndpointInstance endpointInstance = await Endpoint.Start(busConfiguration);
+        await endpointInstance.SendLocal(new MyMessage());
+
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop();
+    }
+}
