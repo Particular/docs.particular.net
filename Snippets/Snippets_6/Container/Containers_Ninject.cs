929eb01643b3454ef1be0e5087fe7c2d585ca83a
WilliamBZA <william.brander@gmail.com>
2016-02-05 08:17:12 +0200

    Updated snippets to reflect v6

--- Snippets/Snippets_6/Container/Containers_Ninject.cs
+++ Snippets/Snippets_6/Container/Containers_Ninject.cs
@@ -28,6 +28,36 @@
             #endregion
         }
 
+        public void UseUnitOfWorkScope()
+        {
+            #region NinjectUnitOfWork [4.0,6.0]
+
+            StandardKernel kernel = new StandardKernel();
+
+            kernel.Bind<MyService>().ToSelf().InUnitOfWorkScope();
+
+            #endregion
+        }
+
+        public void UseConditionalBinding()
+        {
+            #region NinjectConditionalBindings [4.0,6.0]
+
+            StandardKernel kernel = new StandardKernel();
+
+            // always create a new instance when not processing a message
+            kernel.Bind<MyService>().ToSelf()
+                .WhenNotInUnitOfWork()
+                .InTransientScope();
+
+            // always use the same instance when processing messages
+            kernel.Bind<MyService>().ToSelf()
+                .WhenInUnitOfWork()
+                .InSingletonScope();
+
+            #endregion
+        }
+
         class MyService
         {
         }
