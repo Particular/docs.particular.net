a0d47806c0b4778ee6d31a37ce7250146b0f5ed8
Mat McLoughlin <mathew.mcloughlin@gmail.com>
2016-02-09 09:53:29 +0000

    Updating scaleout docs with new distributor config

---
+++ samples/scaleout/Version_6/Worker.Handlers/ProcessOrderCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using NServiceBus;
+using System;
+using System.Reflection;
+
+#region WorkerHandler
+
+public class ProcessOrderCommandHandler : IHandleMessages<PlaceOrder>
+{
+    IBus bus;
+
+    public ProcessOrderCommandHandler(IBus bus)
+    {
+        this.bus = bus;
+    }
+
+    public void Handle(PlaceOrder placeOrder)
+    {
+        // Process Order...
+        Console.WriteLine("Processing received order....");
+
+        OrderPlaced message = new OrderPlaced
+        {
+            OrderId = placeOrder.OrderId,
+            WorkerName = Assembly.GetEntryAssembly().GetName().Name
+        };
+        bus.Reply(message);
+        Console.WriteLine("Sent Order placed event for orderId [{0}].", placeOrder.OrderId);
+    }
+}
+
+#endregion
