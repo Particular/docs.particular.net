e063913da53a8427364093fb4cb1c4efc34caf6f
SzymonPobiega <szymon.pobiega@gmail.com>
2016-02-11 19:12:21 +0100

    Added code to Distributor.

--- samples/scaleout/distributor-upgrade/Version_6/Distributor/Program.cs
+++ samples/scaleout/distributor-upgrade/Version_6/Distributor/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NServiceBus;
 
 namespace Distributor
 {
@@ -10,6 +11,17 @@ namespace Distributor
     {
         static void Main(string[] args)
         {
+            BusConfiguration busConfiguration = new BusConfiguration();
+            busConfiguration.EndpointName("Samples.Scaleout.Distributor");
+            busConfiguration.RunMSMQDistributor(withWorker: false);
+            busConfiguration.UseSerialization<JsonSerializer>();
+            busConfiguration.UsePersistence<InMemoryPersistence>();
+            busConfiguration.EnableInstallers();
+            using (IBus bus = Bus.Create(busConfiguration).Start())
+            {
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+            }
         }
     }
 }
