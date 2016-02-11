0fe0408d663d1e38efe747bf66896372afddf713
SzymonPobiega <szymon.pobiega@gmail.com>
2016-02-11 19:06:31 +0100

    Initial state.

---
+++ samples/scaleout/distributor-upgrade/Version_6/Worker2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NServiceBus;
+
+class Program
+{
+
+    static void Main()
+    {
+        BusConfiguration busConfiguration = new BusConfiguration();
+        busConfiguration.EndpointName("Samples.Scaleout.Slave1");
+        busConfiguration.EnlistWithMSMQDistributor();
+        busConfiguration.UseSerialization<JsonSerializer>();
+        busConfiguration.UsePersistence<InMemoryPersistence>();
+        busConfiguration.EnableInstallers();
+        using (IBus bus = Bus.Create(busConfiguration).Start())
+        {
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
