d163dd392aeeecab918dbecfb24b26d82b232a45
Weronika Łabaj <weronika.labaj@particular.net>
2016-02-11 16:06:38 +0100

    copy v2 project as v3

---
+++ samples/outbox/sqltransport-nhpersistence-ef/Version_3/Receiver/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NServiceBus;
+using NServiceBus.Transports.SQLServer;
+using NServiceBus.Persistence;
+
+class Program
+{
+    static void Main()
+    {
+        using (ReceiverDataContext ctx = new ReceiverDataContext())
+        {
+            ctx.Database.Initialize(true);
+        }
+
+        Configuration hibernateConfig = new Configuration();
+        hibernateConfig.DataBaseIntegration(x =>
+        {
+            x.ConnectionStringName = "NServiceBus/Persistence";
+            x.Dialect<MsSql2012Dialect>();
+        });
+
+        hibernateConfig.SetProperty("default_schema", "receiver");
+
+        BusConfiguration busConfiguration = new BusConfiguration();
+        
+        #region ReceiverConfiguration
+
+        busConfiguration
+            .UseTransport<SqlServerTransport>()
+            .UseSpecificConnectionInformation(
+                EndpointConnectionInfo.For("sender").UseSchema("sender"))
+            .DefaultSchema("receiver");
+
+        busConfiguration
+            .UsePersistence<NHibernatePersistence>()
+            .RegisterManagedSessionInTheContainer();
+
+        busConfiguration.EnableOutbox();
+
+        #endregion
+
+        using (Bus.Create(busConfiguration).Start())
+        {
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ samples/outbox/sqltransport-nhpersistence-ef/Version_3/Sender/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NServiceBus;
+using NServiceBus.Transports.SQLServer;
+
+class Program
+{
+    static void Main()
+    {
+        const string letters = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+        Random random = new Random();
+
+        Configuration hibernateConfig = new Configuration();
+        hibernateConfig.DataBaseIntegration(x =>
+        {
+            x.ConnectionStringName = "NServiceBus/Persistence";
+            x.Dialect<MsSql2012Dialect>();
+        });
+
+        hibernateConfig.SetProperty("default_schema", "sender");
+
+        BusConfiguration busConfiguration = new BusConfiguration();
+
+        #region SenderConfiguration
+
+        busConfiguration
+            .UseTransport<SqlServerTransport>()
+            .UseSpecificConnectionInformation(
+                EndpointConnectionInfo.For("receiver").UseSchema("receiver"))
+            .DefaultSchema("sender");
+
+        busConfiguration
+            .UsePersistence<NHibernatePersistence>();
+
+        busConfiguration.EnableOutbox();
+
+        #endregion
+
+        using (IBus bus = Bus.Create(busConfiguration).Start())
+        {
+            Console.WriteLine("Press enter to send a message");
+            Console.WriteLine("Press any key to exit");
+
+            while (true)
+            {
+                ConsoleKeyInfo key = Console.ReadKey();
+                Console.WriteLine();
+
+                if (key.Key != ConsoleKey.Enter)
+                {
+                    return;
+                }
+                string orderId = new string(Enumerable.Range(0, 4).Select(x => letters[random.Next(letters.Length)]).ToArray());
+                bus.Publish(new OrderSubmitted
+                {
+                    OrderId = orderId,
+                    Value = random.Next(100)
+                });
+
+            }
+        }
+    }
+}
