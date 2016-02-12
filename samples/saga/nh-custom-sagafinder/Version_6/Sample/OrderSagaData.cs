e6e686d784905bd69f9ad819f17532a634f490c7
Simon Cropp <simon.cropp@gmail.com>
2016-02-12 11:57:02 +1100

    add NH saga finder sample

---
+++ samples/saga/nh-custom-sagafinder/Version_6/Sample/OrderSagaData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using NServiceBus;
+
+public class OrderSagaData : IContainSagaData
+{
+	public virtual Guid Id { get; set; }
+	public virtual string Originator { get; set; }
+	public virtual string OriginalMessageId { get; set; }
+	public virtual string OrderId { get; set; }
+	public virtual string PaymentTransactionId { get; set; }
+}
---
+++ samples/saga/nh-custom-sagafinder/Version_6/Sample/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Persistence;
+
+class Program
+{
+
+    static void Main()
+    {
+        AsyncMain().GetAwaiter().GetResult();
+    }
+
+    static async Task AsyncMain()
+    {
+        EndpointConfiguration endpointConfiguration = new EndpointConfiguration();
+        endpointConfiguration.EndpointName("Samples.NHibernateCustomSagaFinder");
+        endpointConfiguration.UseSerialization<JsonSerializer>();
+        endpointConfiguration.EnableInstallers();
+
+        #region NHibernateSetup
+
+        endpointConfiguration.UsePersistence<NHibernatePersistence>()
+            .ConnectionString(@"Data Source=.\SqlExpress;Initial Catalog=NHCustomSagaFinder;Integrated Security=True");
+
+        #endregion
+
+        IEndpointInstance endpoint = await Endpoint.Start(endpointConfiguration);
+        try
+        {
+            await endpoint.SendLocal(new StartOrder
+                          {
+                              OrderId = "123"
+                          });
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+        finally
+        {
+            await endpoint.Stop();
+        }
+    }
+}
