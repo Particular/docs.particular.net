e590d6ce10ecf26941e95bb73a3acfd303ecfcda
Simon Cropp <simon.cropp@gmail.com>
2016-02-15 13:41:55 +1100

    remove redundant settings

--- Snippets/Snippets_6/Transports/Throughput/ProvideConfiguration.cs
+++ Snippets/Snippets_6/Transports/Throughput/ProvideConfiguration.cs
@@ -1,6 +1,5 @@
 ﻿namespace Snippets6.Transports.Throughput
 {
-    using System.Configuration;
     using NServiceBus;
 
     class ConcurrencyConfiguration
@@ -13,18 +12,6 @@
             configuration.LimitMessageProcessingConcurrencyTo(5);
             #endregion
         }
-
-        void ConfigureFromAppConfig()
-        {
-            #region TuningFromAppConfig
-            EndpointConfiguration configuration = new EndpointConfiguration();
-
-            // add your custom key to the appSettings section like this:
-            // <appSettings>
-            //    <add key="MaxConcurrency" value="5"/>
-            // </appSettings>
-            configuration.LimitMessageProcessingConcurrencyTo(int.Parse(ConfigurationManager.AppSettings["MaxConcurrency"]));
-            #endregion
-        }
+        
     }
 }
