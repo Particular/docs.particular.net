f0a78ddf954fa93df93f77f4308c539da4081d5b
Simon Cropp <simon.cropp@gmail.com>
2016-02-16 14:49:23 +1100

    fix broken snippet

--- samples/pubsub/Version_5/MyPublisher/AuthorizeSubscriptions.cs
+++ samples/pubsub/Version_5/MyPublisher/AuthorizeSubscriptions.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using NServiceBus;
 
-#region subscriptionAuthorizaton
+#region SubscriptionAuthorizer
 class AuthorizeSubscriptions : IAuthorizeSubscriptions
 {
 
