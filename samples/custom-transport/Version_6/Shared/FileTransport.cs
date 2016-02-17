7b7fe789a84cfb303ae1fe8a42d1fbf2c24e4349
Simon Cropp <simon.cropp@gmail.com>
2016-02-17 19:11:18 +1100

    update file transport to new TransportDefinition

--- samples/custom-transport/Version_6/Shared/FileTransport.cs
+++ samples/custom-transport/Version_6/Shared/FileTransport.cs
@@ -12,70 +12,38 @@ using NServiceBus.Transports;
 #region TransportDefinition
 public class FileTransport : TransportDefinition
 {
-    /// <summary>
-    /// Used by implementations to control if a connection string is necessary.
-    /// </summary>
     public override bool RequiresConnectionString => false;
 
-    /// <summary>
-    /// Gets an example connection string to use when reporting lack of configured connection string to the user.
-    /// </summary>
+    protected override TransportInfrastructure Initialize(SettingsHolder settings, string connectionString)
+    {
+        return new FileTransportInfrastructure();
+    }
+
     public override string ExampleConnectionStringForErrorMessage { get; } = "";
+}
 
-    /// <summary>
-    /// Gets the highest supported transaction mode for the this transport.
-    /// </summary>
-    public override TransportTransactionMode GetSupportedTransactionMode() => TransportTransactionMode.SendsAtomicWithReceive;
+public class FileTransportInfrastructure : TransportInfrastructure
+{
+    public override TransportReceiveInfrastructure ConfigureReceiveInfrastructure()
+    {
+        return new TransportReceiveInfrastructure(() => new FileTransportMessagePump(), () => new FileTransportQueueCreator(), () => Task.FromResult(StartupCheckResult.Success));
+    }
 
-    /// <summary>
-    /// Will be called if the transport has indicated that it has native support for pub sub.
-    /// Creates a transport address for the input queue defined by a logical address.
-    /// </summary>
-    public override IManageSubscriptions GetSubscriptionManager()
+    public override TransportSendInfrastructure ConfigureSendInfrastructure()
+    {
+        return new TransportSendInfrastructure(() => new Dispatcher(), () => Task.FromResult(StartupCheckResult.Success));
+    }
+
+    public override TransportSubscriptionInfrastructure ConfigureSubscriptionInfrastructure()
     {
         throw new NotImplementedException();
     }
 
-    /// <summary>
-    /// Returns the discriminator for this endpoint instance.
-    /// </summary>
-    public override EndpointInstance BindToLocalEndpoint(EndpointInstance instance, ReadOnlySettings settings)
+    public override EndpointInstance BindToLocalEndpoint(EndpointInstance instance)
     {
         return instance;
     }
 
-    /// <summary>
-    /// Configures transport for receiving.
-    /// </summary>
-    protected override TransportReceivingConfigurationResult ConfigureForReceiving(TransportReceivingConfigurationContext context)
-    {
-        return new TransportReceivingConfigurationResult(() => new FileTransportMessagePump(), () => new FileTransportQueueCreator(), () => Task.FromResult(StartupCheckResult.Success));
-    }
-
-    /// <summary>
-    /// Configures transport for sending.
-    /// </summary>
-    protected override TransportSendingConfigurationResult ConfigureForSending(TransportSendingConfigurationContext context)
-    {
-        return new TransportSendingConfigurationResult(() => new Dispatcher(), () => Task.FromResult(StartupCheckResult.Success));
-    }
-
-    /// <summary>
-    /// Returns the list of supported delivery constraints for this transport.
-    /// </summary>
-    public override IEnumerable<Type> GetSupportedDeliveryConstraints()
-    {
-        return new List<Type>
-            {
-                typeof(DiscardIfNotReceivedBefore)
-            };
-    }
-
-    /// <summary>
-    /// Converts a given logical address to the transport address.
-    /// </summary>
-    /// <param name="logicalAddress">The logical address.</param>
-    /// <returns>The transport address.</returns>
     public override string ToTransportAddress(LogicalAddress logicalAddress)
     {
         return Path.Combine(logicalAddress.EndpointInstance.Endpoint.ToString(),
@@ -83,12 +51,13 @@ public class FileTransport : TransportDefinition
             logicalAddress.Qualifier ?? "");
     }
 
-    /// <summary>
-    /// Returns the outbound routing policy selected for the transport.
-    /// </summary>
-    public override OutboundRoutingPolicy GetOutboundRoutingPolicy(ReadOnlySettings settings)
+    public override IEnumerable<Type> DeliveryConstraints
     {
-        return new OutboundRoutingPolicy(OutboundRoutingType.Unicast, OutboundRoutingType.Unicast, OutboundRoutingType.Unicast);
+        get { yield return typeof(DiscardIfNotReceivedBefore);}
     }
+
+    public override TransportTransactionMode TransactionMode => TransportTransactionMode.SendsAtomicWithReceive;
+
+    public override OutboundRoutingPolicy OutboundRoutingPolicy => new OutboundRoutingPolicy(OutboundRoutingType.Unicast, OutboundRoutingType.Unicast, OutboundRoutingType.Unicast);
 }
 #endregion
