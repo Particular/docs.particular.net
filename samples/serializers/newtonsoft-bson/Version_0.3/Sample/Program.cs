4090f7110038b301e3eb7523dccd5c9268324b09
Simon Cropp <simon.cropp@gmail.com>
2016-02-24 21:17:30 +1100

    bump refs

--- samples/serializers/newtonsoft-bson/Version_0.3/Sample/Program.cs
+++ samples/serializers/newtonsoft-bson/Version_0.3/Sample/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Text;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Bson;
 using NServiceBus;
 using NServiceBus.Serialization;
--- samples/serializers/newtonsoft/Version_0.3/Sample/Program.cs
+++ samples/serializers/newtonsoft/Version_0.3/Sample/Program.cs
@@ -30,6 +30,7 @@ static class Program
         #endregion
         endpointConfiguration.UsePersistence<InMemoryPersistence>();
         endpointConfiguration.EnableInstallers();
+        endpointConfiguration.SendFailedMessagesTo("error");
 
         IEndpointInstance endpoint = await Endpoint.Start(endpointConfiguration);
         try
