7f0cd22551a83bf9adc1358c605461e6fd13b527
Daniel Marbach <daniel.marbach@particular.net>
2016-02-29 14:20:52 +0100

    Snippet for accessing message context information

---
+++ Snippets/Snippets_6/UpgradeGuides/5to6/MessageContext.cs
@@ -0,0 +1,21 @@
+﻿namespace Snippets6.UpgradeGuides._5to6
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using NServiceBus;
+    using Snippets6.Handlers;
+
+    public class MessageContext : IHandleMessages<MyMessage>
+    {
+        #region 5to6-messagecontext
+
+        public async Task Handle(MyMessage message, IMessageHandlerContext context)
+        {
+            string messageId = context.MessageId;
+            string replyToAddress = context.ReplyToAddress;
+            IReadOnlyDictionary<string, string> headers = context.MessageHeaders;
+        }
+
+        #endregion
+    }
+}
