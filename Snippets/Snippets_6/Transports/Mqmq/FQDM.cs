28a2eae3d57b491b006004124d0db3f58fb2ea6e
Jan Ove Skogheim <janovesk@janovesk.com>
2016-03-03 11:39:15 +0100

    Documentation on how to use FQDM with NServiceBus

---
+++ Snippets/Snippets_6/Transports/Mqmq/FQDM.cs
@@ -0,0 +1,16 @@
+﻿namespace Snippets6.Transports.MSMQ
+{
+    using NServiceBus.Support;
+
+    public class FQDMTest
+    {
+        void FQDM()
+        {
+            #region MsmqMachineNameFQDN [4,)
+
+            RuntimeEnvironment.MachineNameAction = () => System.Net.Dns.GetHostEntry(Environment.MachineName).HostName;
+
+            #endregion
+        }       
+    }
+}
