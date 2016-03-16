7a43c3290283f3ad83eb6ca7870772ae9b0793ad
Hadi Eskandari <h.eskandari@gmail.com>
2016-03-16 11:43:04 +1100

    Added NHibernate v6 to v7 upgrade snippet

---
+++ Snippets/Snippets_6/Persistence/NHibernate/UpgradeGuides/6to7/AccessingSession.cs
@@ -0,0 +1,32 @@
+﻿namespace Snippets6.Persistence.NHibernate
+{
+    using System.Threading.Tasks;
+    using global::NHibernate;
+    using NServiceBus;
+
+    public class AccessingSession
+    {
+        public class OrderMessage : IMessage
+        {
+        }
+
+        public class Order
+        {
+        }
+
+        public class ViaContext
+        {
+            #region NHibernateAccessingSessionUpgrade6To7
+            public class OrderHandler : IHandleMessages<OrderMessage>
+            {
+                public Task Handle(OrderMessage message, IMessageHandlerContext context)
+                {
+                    ISession nhibernateSession = context.SynchronizedStorageSession.Session();
+                    nhibernateSession.Save(new Order());
+                    return Task.FromResult(0);
+                }
+            }
+            #endregion
+        }
+    }
+}
