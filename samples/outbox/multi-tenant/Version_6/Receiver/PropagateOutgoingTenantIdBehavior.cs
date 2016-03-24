91d74811551225329d69952ba5e54d245353030c
Hadi Eskandari <h.eskandari@gmail.com>
2016-03-24 20:59:18 +1100

    Revert "Removing redundant snippet"
    This reverts commit 950b831499778e214d426991bee28dde6b688298.

--- samples/outbox/multi-tenant/Version_6/Receiver/PropagateOutgoingTenantIdBehavior.cs
+++ samples/outbox/multi-tenant/Version_6/Receiver/PropagateOutgoingTenantIdBehavior.cs
@@ -6,12 +6,16 @@ internal class PropagateOutgoingTenantIdBehavior : Behavior<IOutgoingLogicalMess
 {
     public override async Task Invoke(IOutgoingLogicalMessageContext context, Func<Task> next)
     {
+        #region PropagateTenantIdOutgoing
+
         string tenant;
         if (context.Extensions.TryGet("TenantId", out tenant))
         {
             context.Headers["TenantId"] = tenant;
         }
         await next().ConfigureAwait(false);
+
+        #endregion
     }
 
     public class Registration : RegisterStep
