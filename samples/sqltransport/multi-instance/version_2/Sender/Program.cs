e16b5f96e93a0a958943d151c422f12b65eee96b
Weronika Łabaj <weronika.labaj@particular.net>
2016-03-29 16:46:25 +0200

    Code sample for sql server multi-instance v2

---
+++ samples/sqltransport/multi-instance/version_2/Sender/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Messages;
+using NServiceBus;
+using NServiceBus.Config;
+using NServiceBus.Config.ConfigurationSource;
+
+public class Program
+{
+    static void Main()
+    {
+        Console.Title = "Samples.SqlServer.MultiInstanceSender";
+
+        #region SenderConfiguration
+
+        BusConfiguration busConfiguration = new BusConfiguration();
+        busConfiguration.EndpointName("Samples.SqlServer.MultiInstanceSender");
+        busConfiguration.UseTransport<SqlServerTransport>()
+            .UseSpecificConnectionInformation(ConnectionProvider.GetConnecton)
+            .ConnectionString(ConnectionProvider.SenderConnectionString);
+        busConfiguration.UseSerialization<JsonSerializer>();
+        busConfiguration.UsePersistence<InMemoryPersistence>();
+
+        #endregion
+
+        using (IBus bus = Bus.Create(busConfiguration).Start())
+        {
+            Console.WriteLine("Press <enter> to send a message");
+            Console.WriteLine("Press any other key to exit");
+
+            while (true)
+            {
+                if (Console.ReadKey().Key != ConsoleKey.Enter)
+                {
+                    return;
+                }
+
+                #region SendMessage
+
+                ClientOrder order = new ClientOrder
+                {
+                    OrderId = Guid.NewGuid()
+                };
+
+                bus.Send("Samples.SqlServer.MultiInstanceReceiver", order);
+
+                #endregion
+
+                Console.WriteLine("ClientOrder message sent with ID {0}", order.OrderId);
+            }
+        }
+    }
+
+    class ProvideConfiguration : IProvideConfiguration<MessageForwardingInCaseOfFaultConfig>
+    {
+        public MessageForwardingInCaseOfFaultConfig GetConfiguration()
+        {
+            return new MessageForwardingInCaseOfFaultConfig
+            {
+                ErrorQueue = "error"
+            };
+        }
+    }
+}
