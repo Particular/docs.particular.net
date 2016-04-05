2069465b3ad496c669b4b65c4e50e33bcc418619
Simon Cropp <simon.cropp@gmail.com>
2016-04-05 16:12:38 +1000

    add asb 7 snippets

---
+++ Snippets/Snippets_6/Azure/Transports/AzureServiceBus/Usage.cs
@@ -0,0 +1,28 @@
+﻿namespace Snippets6.Azure.Transports.AzureServiceBus
+{
+    using NServiceBus;
+
+    class Usage
+    {
+        Usage(EndpointConfiguration endpointConfiguration)
+        {
+            #region AzureServiceBusTransportWithAzure 7
+
+            endpointConfiguration.UseTransport<AzureServiceBusTransport>();
+
+            #endregion
+        }
+
+        #region AzureServiceBusTransportWithAzureHost 7
+
+        public class EndpointConfig : IConfigureThisEndpoint
+        {
+            public void Customize(EndpointConfiguration endpointConfiguration)
+            {
+                endpointConfiguration.UseTransport<AzureServiceBusTransport>();
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ nservicebus/upgrades/asb-6to7.md
@@ -0,0 +1,15 @@
+---
+title: Upgrade Azure Service Bus Transport Version 6 to 7
+summary: Instructions on how to upgrade Azure Service Bus Transport Version 6 to 7.
+reviewed: 2016-04-05
+tags:
+ - upgrade
+ - migration
+related:
+- nservicebus/azure/azure-servicebus-transport
+- nservicebus/upgrades/5to6
+---
+
+
+## Azure Service Bus Transport
+
