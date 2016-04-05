32024f03d98d4c7e5a15d2cfa3211deb6893d608
Simon Cropp <simon.cropp@gmail.com>
2016-04-05 15:41:33 +1000

    add asq 7 doco
    placeholder for upgrade guide
    port doco

---
+++ Snippets/Snippets_6/Azure/Transports/AzureStorageQueues/Usage.cs
@@ -0,0 +1,28 @@
+﻿namespace Snippets5.Azure.Transports.AzureStorageQueues
+{
+    using NServiceBus;
+
+    class Usage
+    {
+        Usage(EndpointConfiguration endpointConfiguration)
+        {
+            #region AzureStorageQueueTransportWithAzure 7
+
+            endpointConfiguration.UseTransport<AzureStorageQueueTransport>();
+
+            #endregion
+        }
+
+        #region AzureStorageQueueTransportWithAzureHost 7
+
+        public class EndpointConfig : IConfigureThisEndpoint
+        {
+            public void Customize(EndpointConfiguration endpointConfiguration)
+            {
+                endpointConfiguration.UseTransport<AzureStorageQueueTransport>();
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ nservicebus/upgrades/asq-6to7.md
@@ -0,0 +1,15 @@
+---
+title: Upgrade Azure Storage Queues Transport Version 6 to 7
+summary: Instructions on how to upgrade Azure Storage Queues Transport Version 6 to 7.
+reviewed: 2016-04-05
+tags:
+ - upgrade
+ - migration
+related:
+- nservicebus/azure/azure-storage-queues-transport
+- nservicebus/upgrades/5to6
+---
+
+
+## Azure Storage Queues Transport
+
