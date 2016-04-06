67e908d0d83bdd370a6d3f29d749fda831b6361b
Sean Feldman <feldman.sean@gmail.com>
2016-04-06 17:29:17 -0600

    Specify connection string for persistence
    Specify serialization for transport

--- samples/azure/shared-host/Version_6/Receiver/EndpointConfig.cs
+++ samples/azure/shared-host/Version_6/Receiver/EndpointConfig.cs
@@ -1,13 +1,16 @@
 ﻿using NServiceBus;
+using NServiceBus.Azure.Transports.WindowsAzureStorageQueues;
 using NServiceBus.Logging;
 
 public class EndpointConfig : IConfigureThisEndpoint
 {
     public void Customize(EndpointConfiguration configuration)
     {
-        configuration.UsePersistence<AzureStoragePersistence>();
-        configuration.UseTransport<AzureStorageQueueTransport>()
+        configuration.UsePersistence<AzureStoragePersistence>()
             .ConnectionString("UseDevelopmentStorage=true");
+        configuration.UseTransport<AzureStorageQueueTransport>()
+            .ConnectionString("UseDevelopmentStorage=true")
+            .SerializeMessageWrapperWith(definition => MessageWrapperSerializer.Json.Value);
         configuration.SendFailedMessagesTo("error");
         LogManager.Use<DefaultFactory>().Level(LogLevel.Debug);
 
