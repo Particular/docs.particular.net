3ac028a019c2fce35b279ca588bc81318c788dca
Andreas Öhlund <andreas.ohlund@particular.net>
2016-04-07 04:44:11 +0200

    Upgrade guide for iconfigurecomponents no longer registered in DI

---
+++ Snippets/Snippets_6/UpgradeGuides/5to6/IConfigureComponentsNotInjected.cs
@@ -0,0 +1,25 @@
+﻿namespace Snippets5.UpgradeGuides._5to6
+{
+    using NServiceBus;
+    using NServiceBus.Features;
+
+    class IConfigureComponentsNotInjected
+    {
+        public IConfigureComponentsNotInjected()
+        {
+            FeatureConfigurationContext context = null;
+
+            #region 5to6-IConfigureComponentsNotInjected
+            context.Container.ConfigureComponent(b => new MyDependency(context.Container), DependencyLifecycle.InstancePerCall);
+            #endregion
+        }
+
+        class MyDependency
+        {
+            public MyDependency(object container)
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+    }
+}
