e97664ba415c3728172d3579cb492b3b8a96af82
Szymon Pobiega <szymon.pobiega@gmail.com>
2016-04-08 11:03:14 +0200

    Replaces the static field with AsyncLocal and simplifies the connection mgmt (#1398)
    * Replaces the static field with AsyncLocal and simplifies the connection management.
    
    * Reverted stupid change in app.config

--- samples/outbox/multi-tenant/Version_6/Receiver/MultiTenantConnectionProvider.cs
+++ samples/outbox/multi-tenant/Version_6/Receiver/MultiTenantConnectionProvider.cs
@@ -1,30 +1,23 @@
-﻿using System;
-using System.Configuration;
-using System.Data;
+﻿using System.Data;
 using NHibernate.Connection;
-using Receiver;
 
 class MultiTenantConnectionProvider : DriverConnectionProvider
 {
     public override IDbConnection GetConnection()
     {
-        string defaultConnectionString = ConfigurationManager.ConnectionStrings["NServiceBus/Persistence"]
-            .ConnectionString;
-
         #region GetConnectionFromContext
 
-        Lazy<IDbConnection> lazy;
-        PipelineExecution pipelineExecutor = PipelineExecution.Instance;
-        string key = $"LazySqlConnection-{defaultConnectionString}";
-        if (pipelineExecutor.CurrentContext != null && pipelineExecutor.CurrentContext.Extensions.TryGet(key, out lazy))
+        string connectionString = ExtractTenantConnectionStringBehavior.ConnectionStringHolder.Value;
+        if (connectionString != null)
         {
             IDbConnection connection = Driver.CreateConnection();
-            connection.ConnectionString = lazy.Value.ConnectionString;
+            connection.ConnectionString = connectionString;
             connection.Open();
             return connection;
         }
-        return base.GetConnection();
 
         #endregion
+
+        return base.GetConnection();
     }
 }
