7aadf9c2f6e58b08942995248b28a1dddddbc4c2
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2016-04-18 15:59:16 +0200

    added snippets related to 'SingleNamespacePartitioning'

---
+++ Snippets/ASB/ASB_7/MultipleNamespaces.cs
@@ -0,0 +1,37 @@
+﻿namespace ASB_7
+{
+    using NServiceBus;
+    using NServiceBus.AzureServiceBus.Addressing;
+
+    class MultipleNamespaces
+    {
+        private readonly EndpointConfiguration endpointConfiguration;
+
+        public MultipleNamespaces(EndpointConfiguration endpointConfiguration)
+        {
+            this.endpointConfiguration = endpointConfiguration;
+        }
+
+        public void SingleNamespaceStrategyWithAddNamespace()
+        {
+            #region single_namespace_partitioning_strategy_with_add_namespace
+
+            endpointConfiguration.UseTransport<AzureServiceBusTransport>()
+                .NamespacePartitioning().UseStrategy<SingleNamespacePartitioning>()
+                   .AddNamespace("namespace", "Endpoint=sb://namespace.servicebus.windows.net;SharedAccessKeyName=[shared access key name];SharedAccessKey=[shared access key]");
+
+            #endregion
+        }
+
+        public void SingleNamespaceStrategyWithDefaultConnectionString()
+        {
+            #region single_namespace_partitioning_strategy_with_default_connection_string
+
+            endpointConfiguration.UseTransport<AzureServiceBusTransport>()
+                .ConnectionString("Endpoint=sb://namespace.servicebus.windows.net;SharedAccessKeyName=[shared access key name];SharedAccessKey=[shared access key]")
+                .NamespacePartitioning().UseStrategy<SingleNamespacePartitioning>();
+
+            #endregion
+        }
+    }
+}
---
+++ nservicebus/azure-servicebus/multiple-namespaces-support.md
@@ -0,0 +1,14 @@
+---
+title: Multiple namespace support
+summary: How to configure transport to support different partitioning strategies 
+component: ASB
+tags:
+- Cloud
+- Azure
+- Transports 
+reviewed: 2016-04-18
+---
+
+snippet: single_namespace_partitioning_strategy_with_add_namespace
+
+snippet: single_namespace_partitioning_strategy_with_default_connection_string
