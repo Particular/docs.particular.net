5db6a36f50dfbc026a8534e1976bfdb98985fc65
Bob Langley <bob.langley@particular.net>
2016-04-19 17:31:25 -0700

    Update Gateway usage snippets for recoverability settings

--- Snippets/Gateway/Gateway_2/Usage.cs
+++ Snippets/Gateway/Gateway_2/Usage.cs
@@ -1,5 +1,6 @@
 ﻿namespace Gateway_2
 {
+    using System;
     using NServiceBus;
     using NServiceBus.Features;
 
@@ -8,7 +9,31 @@
         Usage(EndpointConfiguration endpointConfiguration, IEndpointInstance endpoint)
         {
             #region GatewayConfiguration
+
+            endpointConfiguration.SendFailedMessagesTo("error");
             endpointConfiguration.EnableFeature<Gateway>();
+
+            #endregion
+
+            #region GatewayDefaultRetryPolicyConfiguration
+
+            endpointConfiguration.Gateway().Retries(numberOfRetries: 5, timeIncrease: TimeSpan.FromMinutes(1));
+
+            #endregion
+
+            #region GatewayCustomRetryPolicyConfiguration
+
+            endpointConfiguration.Gateway().CustomRetryPolicy(message =>
+            {
+                var currentRetry = message.GetCurrentRetry();
+                return currentRetry > 4 ? TimeSpan.Zero : TimeSpan.FromSeconds(currentRetry * 60);
+            });
+
+            #endregion
+
+            #region GatewayDisableRetriesConfiguration
+
+            endpointConfiguration.Gateway().DisableRetries();
 
             #endregion
 
