036f161fd4852b90054849c0056a478b3fb06962
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2016-04-26 15:59:39 +0200

    added snippets and description about how to enable logical name feature

---
+++ Snippets/ASB/ASB_7/KeepConnectionStringSafe.cs
@@ -0,0 +1,30 @@
+﻿namespace ASB_7
+{
+    using NServiceBus;
+    using NServiceBus.AzureServiceBus.Addressing;
+
+    public class KeepConnectionStringSafe
+    {
+        public KeepConnectionStringSafe(EndpointConfiguration endpointConfiguration)
+        {
+            #region enable_use_namespace_name_instead_of_connection_string
+
+            endpointConfiguration.UseTransport<AzureServiceBusTransport>()
+                .UseNamespaceNamesInsteadOfConnectionStrings();
+
+            #endregion
+        }
+
+        public void MapLogicalNameToConnectionString(EndpointConfiguration endpointConfiguration)
+        {
+            #region map_logical_name_to_connection_string
+
+            endpointConfiguration.UseTransport<AzureServiceBusTransport>()
+                .NamespacePartitioning()
+                .UseStrategy<SingleNamespacePartitioning>()
+                    .AddNamespace("namespaceName", "Endpoint=sb://namespace.servicebus.windows.net;SharedAccessKeyName=[shared access key name];SharedAccessKey=[shared access key]");
+
+            #endregion
+        }
+    }
+}
--- nservicebus/azure-service-bus/keep-connectionstring-safe.md
+++ nservicebus/azure-service-bus/keep-connectionstring-safe.md
@@ -11,3 +11,13 @@ reviewed: 2016-04-26
 ---
 
 include: intro-keep-connection-string-safe
+
+To enable this behavior, apply the following configuration:
+
+snippet: enable_use_namespace_name_instead_of_connection_string
+
+Using `AddNamespace(string name, string connectionString)` configuration API is possible to register namespaces mapping logical names with connection strings, as shown below for `SingleNamespacePartitioning` strategy:
+
+snippet: map_logical_name_to_connection_string 
+
+For detailed explanation about all supported partitioning strategies and how to configure namespaces mapping for them see related [article](multiple-namespaces-support.md)
