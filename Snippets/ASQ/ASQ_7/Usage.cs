79ff84e9c09449a925497a87b14a3e9d24a3e217
Simon Cropp <simon.cropp@gmail.com>
2016-04-27 11:12:42 +1000

    move over to timespan api

--- Snippets/ASQ/ASQ_7/Usage.cs
+++ Snippets/ASQ/ASQ_7/Usage.cs
@@ -22,9 +22,9 @@ namespace ASQ_7
             var transport = endpointConfiguration.UseTransport<AzureStorageQueueTransport>();
             transport.ConnectionString("azure-storage-connection-string");
             transport.BatchSize(20);
-            transport.MaximumWaitTimeWhenIdle(1000);
+            transport.MaximumWaitTimeWhenIdle(TimeSpan.FromSeconds(1));
             transport.DegreeOfReceiveParallelism(16);
-            transport.PeekInterval(100);
+            transport.PeekInterval(TimeSpan.FromMilliseconds(100));
             transport.MessageInvisibleTime(TimeSpan.FromSeconds(30));
 
             #endregion
