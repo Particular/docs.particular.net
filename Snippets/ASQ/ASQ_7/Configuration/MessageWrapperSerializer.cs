d057cebef67d48a23941849624cfc7f9e3edd5ea
Tim Bussmann <tim.bussmann@particular.net>
2016-04-29 13:17:38 +0200

    changed config snippet

--- Snippets/ASQ/ASQ_7/Configuration/MessageWrapperSerializer.cs
+++ Snippets/ASQ/ASQ_7/Configuration/MessageWrapperSerializer.cs
@@ -4,33 +4,30 @@
 
     public class MessageWrapperSerializer
     {
-        #region SerializerAndMessageWrapperSerializer
-        public void ConfigureEndpointA()
+        
+
+        public void ConfigureEndpoints()
         {
+            #region SerializerAndMessageWrapperSerializer
             var endpointA = new EndpointConfiguration("Endpoint-A");
             // serialize the messages using the XML serializer:
             endpointA.UseSerialization<XmlSerializer>();
             // enables handling messages from Endpoint-B
             endpointA.AddDeserializer<JsonSerializer>();
-
             endpointA.UseTransport<AzureStorageQueueTransport>()
                 .ConnectionStringName("ASQ-ConnectionString")
                 // wrap messages in JSON to maintain compatibility with existing messages
                 .SerializeMessageWrapperWith<JsonSerializer>();
-        }
 
-        public void ConfigureEndpointB()
-        {
             var endpointB = new EndpointConfiguration("Endpoint-B");
             // serialize the messages using the JSON serializer:
             endpointB.UseSerialization<JsonSerializer>();
             // enables handling messages from Endpoint-A
             endpointB.AddDeserializer<XmlSerializer>();
-
             // wraps messages in JSON since this is the configured message serializer
             endpointB.UseTransport<AzureStorageQueueTransport>()
                 .ConnectionStringName("ASQ-ConnectionString");
+            #endregion
         }
-        #endregion
     }
 }
