0abe76d542584af7079429d07d38c6278a04ec5d
Marcin Hoppe <marcin.hoppe@particular.net>
2016-05-24 15:23:51 +0200

    Update the Azure shared host sample to changes in the host API

--- samples/azure/shared-host/CloudServicesHost_7/HostWorker/EndpointConfig.cs
+++ samples/azure/shared-host/CloudServicesHost_7/HostWorker/EndpointConfig.cs
@@ -1,11 +1,27 @@
-﻿using NServiceBus;
+﻿using Microsoft.WindowsAzure.ServiceRuntime;
+using NServiceBus;
 
 #region AzureSharedHosting_HostConfiguration
 
-public class EndpointConfig : IConfigureThisEndpoint, AsA_Host
+public class EndpointConfig : IConfigureThisEndpoint, IConfigureThisHost
 {
     public void Customize(EndpointConfiguration configuration)
     {
+    }
+
+    public HostingSettings Configure()
+    {
+        var connectionString = RoleEnvironment.GetConfigurationSettingValue("HostWorker.ConnectionString");
+        var autoUpdate = bool.Parse(RoleEnvironment.GetConfigurationSettingValue("HostWorker.AutoUpdate"));
+        var updateInterval = int.Parse(RoleEnvironment.GetConfigurationSettingValue("HostWorker.UpdateInterval"));
+        var container = RoleEnvironment.GetConfigurationSettingValue("HostWorker.Container");
+
+        return new HostingSettings(connectionString)
+        {
+            AutoUpdate = autoUpdate,
+            UpdateInterval = updateInterval,
+            Container = container
+        };
     }
 }
 
