075404791c2ed220bb07bc44d2e56733135e1d59
indualagarsamy <indu.alagarsamy@particular.net>
2016-05-24 18:31:14 -0700

    Added snippets for the Saga to the upgrade guide.

---
+++ Snippets/Core/Core_5/UpgradeGuides/5to6/Saga.cs
@@ -0,0 +1,35 @@
+﻿namespace Core5.UpgradeGuides._5to6
+{
+    using Core5.Sagas.SimpleSaga;
+    using NServiceBus;
+    using NServiceBus.Saga;
+
+
+    public class OrderSaga : Saga<OrderSagaData>,
+                                IAmStartedByMessages<StartOrder>,
+                                IHandleMessages<CompleteOrder>
+    {
+        protected override void ConfigureHowToFindSaga(SagaPropertyMapper<OrderSagaData> mapper)
+        {
+            mapper.ConfigureMapping<StartOrder>(message => message.OrderId)
+                    .ToSaga(sagaData => sagaData.OrderId);
+
+            mapper.ConfigureMapping<CompleteOrder>(message => message.OrderId)
+                    .ToSaga(sagaData => sagaData.OrderId);
+        }
+
+        #region 5to6-NoSagaDataCorrelationNeeded
+        public void Handle(StartOrder message)
+        {
+            Data.OrderId = message.OrderId;
+            // The processing logic for the StartOrder message
+        }
+        #endregion
+
+        public void Handle(CompleteOrder message)
+        {
+            // code to handle order completion
+            MarkAsComplete();
+        }
+    }
+}
---
+++ Snippets/Core/Core_6/UpgradeGuides/5to6/Saga.cs
@@ -0,0 +1,37 @@
+﻿namespace Core6.UpgradeGuides._5to6
+{
+    using System.Threading.Tasks;
+    using Core6.Sagas.SimpleSaga;
+    using NServiceBus;
+
+#region 5to6-SagaDefinition
+    public class OrderSaga : Saga<OrderSagaData>,
+                          IAmStartedByMessages<StartOrder>,
+                          IHandleMessages<CompleteOrder>
+#endregion
+    {
+        #region 5to6-ConfigureHowToFindSaga
+        protected override void ConfigureHowToFindSaga(SagaPropertyMapper<OrderSagaData> mapper)
+        {
+            mapper.ConfigureMapping<StartOrder>(message => message.OrderId)
+                    .ToSaga(sagaData => sagaData.OrderId);
+
+            mapper.ConfigureMapping<CompleteOrder>(message => message.OrderId)
+                    .ToSaga(sagaData => sagaData.OrderId);
+        }
+        #endregion
+
+        #region 5to6-NoSagaDataCorrelationNeeded
+        public async Task Handle(StartOrder message, IMessageHandlerContext context)
+        {
+            // The processing logic for the StartOrder message
+        }
+        #endregion
+
+        public async Task Handle(CompleteOrder message, IMessageHandlerContext context)
+        {
+            // code to handle order completion
+            MarkAsComplete();
+        }
+    }
+}
