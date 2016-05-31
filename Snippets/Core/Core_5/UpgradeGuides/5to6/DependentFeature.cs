06aad7bfda315d202174e37690cebf79adf6e384
Simon Cropp <simon.cropp@gmail.com>
2016-05-31 16:12:43 +1000

    add Feature Dependencies to upgrade guide

---
+++ Snippets/Core/Core_5/UpgradeGuides/5to6/DependentFeature.cs
@@ -0,0 +1,21 @@
+﻿namespace Core5.UpgradeGuides._5to6
+{
+    using NServiceBus.Features;
+
+    #region 5to6-DependentFeature
+
+    public class DependentFeature : Feature
+    {
+        public DependentFeature()
+        {
+            DependsOn("DependencyB");
+            DependsOnAtLeastOne("DependencyC", "DependencyD");
+        }
+
+        #endregion
+
+        protected override void Setup(FeatureConfigurationContext context)
+        {
+        }
+    }
+}
