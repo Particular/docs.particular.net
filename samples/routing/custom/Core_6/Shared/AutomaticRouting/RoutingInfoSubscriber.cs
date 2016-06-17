5573f03adcf8df1d995ebe0f3c721ea09afa08da
Simon Cropp <simon.cropp@gmail.com>
2016-06-17 20:37:49 +1000

    cleanup

--- samples/routing/custom/Core_6/Shared/AutomaticRouting/RoutingInfoSubscriber.cs
+++ samples/routing/custom/Core_6/Shared/AutomaticRouting/RoutingInfoSubscriber.cs
@@ -12,11 +12,11 @@ using NServiceBus.Routing.MessageDrivenSubscriptions;
 
 class RoutingInfoSubscriber : FeatureStartupTask
 {
-    static readonly ILog Logger = LogManager.GetLogger<RoutingInfoSubscriber>();
+    static ILog log = LogManager.GetLogger<RoutingInfoSubscriber>();
 
     UnicastRoutingTable routingTable;
     EndpointInstances endpointInstances;
-    readonly IReadOnlyCollection<Type> messageTypesHandledByThisEndpoint;
+    IReadOnlyCollection<Type> messageTypesHandledByThisEndpoint;
     Publishers publishers;
     TimeSpan sweepPeriod;
     TimeSpan heartbeatTimeout;
@@ -39,11 +39,11 @@ class RoutingInfoSubscriber : FeatureStartupTask
 
     protected override Task OnStart(IMessageSession context)
     {
-        this.messageSession = context;
+        messageSession = context;
 
         #region AddDynamic
         routingTable.AddDynamic((list, bag) => FindEndpoint(list));
-        endpointInstances.AddDynamic(FindInstances);
+        endpointInstances.AddDynamic(FindInstancesTask);
         publishers.AddDynamic(FindPublisher);
         #endregion
 
@@ -53,7 +53,7 @@ class RoutingInfoSubscriber : FeatureStartupTask
             {
                 if (!info.Value.Sweep(DateTime.UtcNow, heartbeatTimeout))
                 {
-                    Logger.Info($"Instance {info.Key} deactivated (heartbeat timeout).");
+                    log.Info($"Instance {info.Key} deactivated (heartbeat timeout).");
                 }
             }
         }, null, sweepPeriod + sweepPeriod, sweepPeriod);
@@ -70,22 +70,22 @@ class RoutingInfoSubscriber : FeatureStartupTask
         return Task.FromResult(0);
     }
 
-
-    public async Task OnRemoved(Entry e)
+    public async Task OnRemoved(Entry entry)
     {
-        var deserializedData = JsonConvert.DeserializeObject<RoutingInfo>(e.Data);
+        var deserializedData = JsonConvert.DeserializeObject<RoutingInfo>(entry.Data);
         var instanceName = new EndpointInstance(deserializedData.EndpointName, deserializedData.Discriminator, deserializedData.InstanceProperties);
 
-        Logger.Info($"Instance {instanceName} removed from routing tables.");
+        log.Info($"Instance {instanceName} removed from routing tables.");
 
-        await UpdateCaches(instanceName, new Type[0], new Type[0]).ConfigureAwait(false);
+        await UpdateCaches(instanceName, new Type[0], new Type[0])
+            .ConfigureAwait(false);
 
         instanceInformation.Remove(instanceName);
     }
 
-    public Task OnChanged(Entry e)
+    public Task OnChanged(Entry entry)
     {
-        var deserializedData = JsonConvert.DeserializeObject<RoutingInfo>(e.Data);
+        var deserializedData = JsonConvert.DeserializeObject<RoutingInfo>(entry.Data);
         var instanceName = new EndpointInstance(deserializedData.EndpointName, deserializedData.Discriminator, deserializedData.InstanceProperties);
 
         var handledTypes =
@@ -109,12 +109,12 @@ class RoutingInfoSubscriber : FeatureStartupTask
         if (deserializedData.Active)
         {
             instanceInfo.Activate(deserializedData.Timestamp);
-            Logger.Debug($"Instance {instanceName} active (heartbeat).");
+            log.Debug($"Instance {instanceName} active (heartbeat).");
         }
         else
         {
             instanceInfo.Deactivate();
-            Logger.Info($"Instance {instanceName} deactivated.");
+            log.Info($"Instance {instanceName} deactivated.");
         }
         return UpdateCaches(instanceName, handledTypes, publishedTypes);
     }
@@ -133,7 +133,8 @@ class RoutingInfoSubscriber : FeatureStartupTask
 
         foreach (var type in toSubscribe.Intersect(messageTypesHandledByThisEndpoint))
         {
-            await messageSession.Subscribe(type).ConfigureAwait(false);
+            await messageSession.Subscribe(type)
+                .ConfigureAwait(false);
         }
     }
 
@@ -141,13 +142,13 @@ class RoutingInfoSubscriber : FeatureStartupTask
     {
         foreach (var addedType in newPublisherMap.Keys.Except(publisherMap.Keys))
         {
-            Logger.Info($"Added {newPublisherMap[addedType]} as publisher of {addedType}.");
+            log.Info($"Added {newPublisherMap[addedType]} as publisher of {addedType}.");
             yield return addedType;
         }
 
         foreach (var removedType in publisherMap.Keys.Except(newPublisherMap.Keys))
         {
-            Logger.Info($"Removed {publisherMap[removedType]} as publisher of {removedType}.");
+            log.Info($"Removed {publisherMap[removedType]} as publisher of {removedType}.");
         }
     }
 
@@ -155,12 +156,12 @@ class RoutingInfoSubscriber : FeatureStartupTask
     {
         foreach (var addedEndpoint in newInstanceMap.Keys.Except(instanceMap.Keys))
         {
-            Logger.Info($"Added endpoint {addedEndpoint} with instances {FormatSet(newInstanceMap[addedEndpoint])}");
+            log.Info($"Added endpoint {addedEndpoint} with instances {FormatSet(newInstanceMap[addedEndpoint])}");
         }
 
         foreach (var removedEndpoint in instanceMap.Keys.Except(newInstanceMap.Keys))
         {
-            Logger.Info($"Removed endpoint {removedEndpoint} with instances {FormatSet(instanceMap[removedEndpoint])}");
+            log.Info($"Removed endpoint {removedEndpoint} with instances {FormatSet(instanceMap[removedEndpoint])}");
         }
 
         foreach (var existingEndpoint in instanceMap.Keys.Intersect(newInstanceMap.Keys))
@@ -169,11 +170,11 @@ class RoutingInfoSubscriber : FeatureStartupTask
             var removedInstances = instanceMap[existingEndpoint].Except(newInstanceMap[existingEndpoint]).ToArray();
             if (addedInstances.Any())
             {
-                Logger.Info($"Added instances {FormatSet(addedInstances)} to endpoint {existingEndpoint}");
+                log.Info($"Added instances {FormatSet(addedInstances)} to endpoint {existingEndpoint}");
             }
             if (removedInstances.Any())
             {
-                Logger.Info($"Removed instances {FormatSet(removedInstances)} from endpoint {existingEndpoint}");
+                log.Info($"Removed instances {FormatSet(removedInstances)} from endpoint {existingEndpoint}");
             }
         }
     }
@@ -182,25 +183,27 @@ class RoutingInfoSubscriber : FeatureStartupTask
     {
         foreach (var addedType in newEndpointMap.Keys.Except(endpointMap.Keys))
         {
-            Logger.Info($"Added route for {addedType.Name} to {FormatSet(newEndpointMap[addedType])}");
+            log.Info($"Added route for {addedType.Name} to {FormatSet(newEndpointMap[addedType])}");
         }
 
         foreach (var removedType in endpointMap.Keys.Except(newEndpointMap.Keys))
         {
-            Logger.Info($"Removed route for {removedType.Name} to {FormatSet(newEndpointMap[removedType])}");
+            log.Info($"Removed route for {removedType.Name} to {FormatSet(newEndpointMap[removedType])}");
         }
 
         foreach (var existingType in endpointMap.Keys.Intersect(newEndpointMap.Keys))
         {
-            var addedEndpoints = newEndpointMap[existingType].Except(endpointMap[existingType]).ToArray();
-            var removedEndpoints = endpointMap[existingType].Except(newEndpointMap[existingType]).ToArray();
+            var newSet = newEndpointMap[existingType];
+            var currentSet = endpointMap[existingType];
+            var addedEndpoints = newSet.Except(currentSet).ToArray();
+            var removedEndpoints = currentSet.Except(newSet).ToArray();
             if (addedEndpoints.Any())
             {
-                Logger.Info($"Added route for {existingType.Name} to {FormatSet(addedEndpoints)}");
+                log.Info($"Added route for {existingType.Name} to {FormatSet(addedEndpoints)}");
             }
             if (removedEndpoints.Any())
             {
-                Logger.Info($"Removed route for {existingType.Name} to {FormatSet(removedEndpoints)}");
+                log.Info($"Removed route for {existingType.Name} to {FormatSet(removedEndpoints)}");
             }
         }
     }
@@ -213,9 +216,10 @@ class RoutingInfoSubscriber : FeatureStartupTask
     static Dictionary<Type, string> BuildNewPublisherMap(EndpointInstance endpointInstance, Type[] publishedTypes, Dictionary<Type, string> publisherMap)
     {
         var newPublisherMap = new Dictionary<Type, string>();
+        var endpointName = endpointInstance.Endpoint.ToString();
         foreach (var pair in publisherMap)
         {
-            if (pair.Value != endpointInstance.Endpoint.ToString())
+            if (pair.Value != endpointName)
             {
                 newPublisherMap[pair.Key] = pair.Value;
             }
@@ -224,12 +228,11 @@ class RoutingInfoSubscriber : FeatureStartupTask
         {
             if (newPublisherMap.ContainsKey(publishedType))
             {
-                Logger.Warn($"More than one endpoint advertises publishing of {publishedType}: {newPublisherMap[publishedType]}, {endpointInstance.Endpoint}");
+                log.Warn($"More than one endpoint advertises publishing of {publishedType}: {newPublisherMap[publishedType]}, {endpointInstance.Endpoint}");
             }
             else
             {
-                newPublisherMap[publishedType] = endpointInstance.Endpoint.ToString();
-                //Subscribe
+                newPublisherMap[publishedType] = endpointName;
             }
         }
         return newPublisherMap;
@@ -244,10 +247,11 @@ class RoutingInfoSubscriber : FeatureStartupTask
             newInstanceMap[pair.Key] = new HashSet<EndpointInstance>(otherInstances);
         }
         HashSet<EndpointInstance> endpointEntry;
-        if (!newInstanceMap.TryGetValue(instanceName.Endpoint.ToString(), out endpointEntry))
+        var endpointName = instanceName.Endpoint.ToString();
+        if (!newInstanceMap.TryGetValue(endpointName, out endpointEntry))
         {
             endpointEntry = new HashSet<EndpointInstance>();
-            newInstanceMap[instanceName.Endpoint.ToString()] = endpointEntry;
+            newInstanceMap[endpointName] = endpointEntry;
         }
         endpointEntry.Add(instanceName);
         return newInstanceMap;
@@ -303,20 +307,25 @@ class RoutingInfoSubscriber : FeatureStartupTask
     #endregion
 
     #region FindInstance
-    Task<IEnumerable<EndpointInstance>> FindInstances(EndpointName endpointName)
+    Task<IEnumerable<EndpointInstance>> FindInstancesTask(EndpointName endpointName)
+    {
+        return Task.FromResult(FindInstances(endpointName));
+    }
+
+    IEnumerable<EndpointInstance> FindInstances(EndpointName endpointName)
     {
         HashSet<EndpointInstance> instances;
-        if (instanceMap.TryGetValue(endpointName.ToString(), out instances))
+        if (!instanceMap.TryGetValue(endpointName.ToString(), out instances))
         {
-            var activeInstances = instances.Where(i => instanceInformation[i].State == InstanceState.Active).ToArray();
-            if (activeInstances.Any())
-            {
-                return Task.FromResult((IEnumerable<EndpointInstance>)activeInstances);
-            }
-            Logger.Info($"No active instances of endpoint {endpointName} detected. Trying to route to the inactive ones.");
-            return Task.FromResult((IEnumerable<EndpointInstance>)instances);
+            return Enumerable.Empty<EndpointInstance>();
         }
-        return Task.FromResult(Enumerable.Empty<EndpointInstance>());
+        var activeInstances = instances.Where(i => instanceInformation[i].State == InstanceState.Active).ToArray();
+        if (activeInstances.Any())
+        {
+            return activeInstances;
+        }
+        log.Info($"No active instances of endpoint {endpointName} detected. Trying to route to the inactive ones.");
+        return instances;
     }
     #endregion
 }
