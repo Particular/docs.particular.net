206be2083713ce5e81e22e14a614327dc11c6c07
Tim Bussmann <timbussmann@users.noreply.github.com>
2016-06-23 04:15:35 +0200

    update doco recoverability API (#1608)

---
+++ Snippets/Core/Core_6/Errors/FirstLevel/FlrCodeFirstConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace Core6.Errors.FirstLevel
+{
+    using NServiceBus;
+
+    public class FlrCodeFirstConfiguration
+    {
+        void ConfigureFlr(EndpointConfiguration endpointConfiguration)
+        {
+            #region FlrCodeFirstConfiguration
+            endpointConfiguration.FirstLevelRetries().NumberOfRetries(3);
+            #endregion
+        }
+    }
+}
---
+++ Snippets/Core/Core_6/UpgradeGuides/5to6/Recoverability.cs
@@ -0,0 +1,26 @@
+﻿namespace Core6.UpgradeGuides._5to6
+{
+    using System;
+    using NServiceBus;
+
+    public class Recoverability
+    {
+        void ConfigureRetries(EndpointConfiguration endpointConfiguration)
+        {
+            #region 5to6-RecoverabilityCodeFirstApi
+            endpointConfiguration.FirstLevelRetries().NumberOfRetries(3);
+            endpointConfiguration.SecondLevelRetries()
+                .NumberOfRetries(5)
+                .TimeIncrease(TimeSpan.FromSeconds(30));
+            #endregion
+        }
+
+        void DisableRetries(EndpointConfiguration endpointConfiguration)
+        {
+            #region 5to6-RecoverabilityDisableRetries
+            endpointConfiguration.FirstLevelRetries().Disable();
+            endpointConfiguration.SecondLevelRetries().Disable();
+            #endregion
+        }
+    }
+}
