45fb30b9983a197fbd70319b7edc4fa6eb642acd
Simon Cropp <simon.cropp@gmail.com>
2016-06-27 14:55:32 +1000

    fix error

--- samples/sqltransport/multi-instance/SqlServer_2/EndpointConnectionLookup/ConnectionProvider.cs
+++ samples/sqltransport/multi-instance/SqlServer_2/EndpointConnectionLookup/ConnectionProvider.cs
@@ -1,18 +1,25 @@
 ﻿using NServiceBus.Transports.SQLServer;
 
 #region ConnectionProvider
-public class ConnectionProvider
+public static class ConnectionProvider
 {
     public const string ReceiverConnectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=ReceiverCatalog;Integrated Security=True";
     public const string SenderConnectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=SenderCatalog;Integrated Security=True";
 
     public static ConnectionInfo GetConnection(string transportAddress)
     {
-        var connectionString = transportAddress.StartsWith("Samples.SqlServer.MultiInstanceSender")
-                                                ? SenderConnectionString
-                                                : ReceiverConnectionString;
+        var connectionString = GetConnectionString(transportAddress);
         return ConnectionInfo.Create()
                 .UseConnectionString(connectionString);
+    }
+
+    static string GetConnectionString(string transportAddress)
+    {
+        if (transportAddress.StartsWith("Samples.SqlServer.MultiInstanceSender"))
+        {
+            return SenderConnectionString;
+        }
+        return ReceiverConnectionString;
     }
 }
 #endregion
--- samples/sqltransport/multi-instance/SqlServer_3/EndpointConnectionLookup/ConnectionProvider.cs
+++ samples/sqltransport/multi-instance/SqlServer_3/EndpointConnectionLookup/ConnectionProvider.cs
@@ -2,20 +2,26 @@
 using System.Threading.Tasks;
 
 #region ConnectionProvider
-public class ConnectionProvider
+public static class ConnectionProvider
 {
-    string ReceiverConnectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=ReceiverCatalog;Integrated Security=True";
-    string SenderConnectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=SenderCatalog;Integrated Security=True";
-
     public static async Task<SqlConnection> GetConnection(string transportAddress)
     {
-        var connectionString = transportAddress.StartsWith("Samples.SqlServer.MultiInstanceSender")
-                                                ? SenderConnectionString
-                                                : ReceiverConnectionString;
+        var connectionString = GetConnectionString(transportAddress);
         var connection = new SqlConnection(connectionString);
         await connection.OpenAsync()
             .ConfigureAwait(false);
         return connection;
+    }
+
+    static string GetConnectionString(string transportAddress)
+    {
+        var receiverConnectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=ReceiverCatalog;Integrated Security=True";
+        var senderConnectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=SenderCatalog;Integrated Security=True";
+        if (transportAddress.StartsWith("Samples.SqlServer.MultiInstanceSender"))
+        {
+            return senderConnectionString;
+        }
+        return receiverConnectionString;
     }
 }
 #endregion
