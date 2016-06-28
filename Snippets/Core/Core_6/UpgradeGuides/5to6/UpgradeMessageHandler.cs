9bc043c7eb03f36073ea56b2e1f204b4113238e7
Daniel Marbach <daniel.marbach@particular.net>
2016-06-28 14:21:01 +0200

    Show how to migrate existing handlers

--- Snippets/Core/Core_6/UpgradeGuides/5to6/UpgradeMessageHandler.cs
+++ Snippets/Core/Core_6/UpgradeGuides/5to6/UpgradeMessageHandler.cs
@@ -18,7 +18,7 @@
     {
         public Task Handle(MyMessage message, IMessageHandlerContext context)
         {
-            // when no asynchronous code is executed in a handler Task.FromResult(0) can be returned
+            // when no asynchronous code is executed in a handler Task.FromResult(0) or Task.CompletedTask can be returned
             SomeLibrary.SomeMethod(message.Data);
             return Task.FromResult(0);
         }
@@ -37,4 +37,88 @@
         }
     }
     #endregion
+
+
+    #region 5to6-handler-migration-beginning
+    public class MigrationBeginning : IHandleMessagesFromPreviousVersions<MyMessage>
+    {
+        public IBus Bus { get; set; }
+
+        public void Handle(MyMessage message)
+        {
+            Bus.Send(new MyOtherMessage());
+            Bus.Publish(new MyEvent());
+        }
+    }
+    #endregion
+
+    #region 5to6-handler-migration-step1
+    public class MigrationStep1 : IHandleMessages<MyMessage>
+    {
+        public IBus Bus { get; set; }
+
+        public void Handle(MyMessage message)
+        {
+            Bus.Send(new MyOtherMessage());
+            Bus.Publish(new MyEvent());
+        }
+
+        public async Task Handle(MyMessage message, IMessageHandlerContext context)
+        {
+        }
+    }
+    #endregion
+
+    #region 5to6-handler-migration-step2
+    public class MigrationStep2 : IHandleMessages<MyMessage>
+    {
+        public IBus context { get; set; }
+
+        public void Handle(MyMessage message)
+        {
+            context.Send(new MyOtherMessage());
+            context.Publish(new MyEvent());
+        }
+
+        public async Task Handle(MyMessage message, IMessageHandlerContext context)
+        {
+        }
+    }
+    #endregion
+
+#pragma warning disable 4014
+    #region 5to6-handler-migration-step3
+    public class MigrationStep3 : IHandleMessages<MyMessage>
+    {
+        public async Task Handle(MyMessage message, IMessageHandlerContext context)
+        {
+            // CS4014: Consider applying the 'await' operator to the result of the call.
+            context.Send(new MyOtherMessage());
+            context.Publish(new MyEvent());
+        }
+    }
+    #endregion
+#pragma warning restore 4014
+
+    #region 5to6-handler-migration-step4
+    public class MigrationStep4 : IHandleMessages<MyMessage>
+    {
+        public async Task Handle(MyMessage message, IMessageHandlerContext context)
+        {
+            context.Send(new MyOtherMessage());
+            await context.Publish(new MyEvent()).ConfigureAwait(false);
+        }
+    }
+    #endregion
+
+    public interface IHandleMessagesFromPreviousVersions<in TMessage>
+    {
+        void Handle(TMessage message);
+    }
+
+    public interface IBus
+    {
+        void Send(object message);
+        void Publish(object message);
+    }
 }
