069c37e6ed08db6aa9bb9e1b1329d659371674a7
Mike Minutillo <michael.minutillo@gmail.com>
2016-07-07 14:43:00 +0800

    Add snippets

---
+++ Snippets/Core/Core_3/MessageIdentity/MessageIdFromMessageMutator.cs
@@ -0,0 +1,29 @@
+﻿namespace Core3.MessageIdentity
+{
+    using NServiceBus;
+    using NServiceBus.MessageMutator;
+
+    #region MessageId-Mutator
+    public class MessageIdFromMessageMutator : IMutateOutgoingMessages
+    {
+        public MessageIdFromMessageMutator(IBus bus)
+        {
+            this.bus = bus;
+        }
+
+        public object MutateOutgoing(object message)
+        {
+            bus.OutgoingHeaders["NServiceBus.MessageId"] = GenerateIdForMessage(message);
+
+            return message;
+        }
+
+        string GenerateIdForMessage(object message)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        IBus bus;
+    }
+    #endregion
+}
---
+++ Snippets/Core/Core_4/MessageIdentity/MessageIdFromMessageMutator.cs
@@ -0,0 +1,29 @@
+﻿namespace Core4.MessageIdentity
+{
+    using NServiceBus;
+    using NServiceBus.MessageMutator;
+
+    #region MessageId-Mutator
+    public class MessageIdFromMessageMutator : IMutateOutgoingMessages
+    {
+        public MessageIdFromMessageMutator(IBus bus)
+        {
+            this.bus = bus;
+        }
+
+        public object MutateOutgoing(object message)
+        {
+            bus.SetMessageHeader(message, "NServiceBus.MessageId", GenerateIdForMessage(message));
+
+            return message;
+        }
+
+        string GenerateIdForMessage(object message)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        IBus bus;
+    }
+    #endregion
+}
