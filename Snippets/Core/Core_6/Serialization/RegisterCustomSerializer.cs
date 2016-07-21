686e41240e4e204442aa8c98f20c28611b6be4b3
Szymon Kulec <scooletz@gmail.com>
2016-07-21 17:36:41 +0200

    PR remarks: proper settings provided

--- Snippets/Core/Core_6/Serialization/RegisterCustomSerializer.cs
+++ Snippets/Core/Core_6/Serialization/RegisterCustomSerializer.cs
@@ -4,6 +4,7 @@
     using System.Collections.Generic;
     using System.IO;
     using NServiceBus;
+    using NServiceBus.Configuration.AdvanceExtensibility;
     using NServiceBus.MessageInterfaces;
     using NServiceBus.Serialization;
     using NServiceBus.Settings;
@@ -19,44 +20,54 @@
 
             #endregion
         }
-
-        #region CustomSerializer
-
-        class MyCustomSerializerDefinition :
-            SerializationDefinition
-        {
-            public override Func<IMessageMapper, IMessageSerializer> Configure(ReadOnlySettings settings)
-            {
-                return mapper => new MyCustomSerializer(settings.GetOrDefault<string>("MyCustomSerializer.Settings"));
-            }
-        }
-
-        class MyCustomSerializer :
-            IMessageSerializer
-        {
-            public MyCustomSerializer(string settingsValue)
-            {
-                // Add code initializing serializer on the basis of settingsValue
-            }
-
-            public void Serialize(object message, Stream stream)
-            {
-                // Add code to serialize message
-                throw new NotImplementedException();
-            }
-
-            public object[] Deserialize(Stream stream, IList<Type> messageTypes = null)
-            {
-                // Add code to deserialize message
-                throw new NotImplementedException();
-            }
-
-            public string ContentType
-            {
-                get { throw new NotImplementedException(); }
-            }
-        }
-
-        #endregion
     }
+
+    #region CustomSerializer
+
+    class MyCustomSerializerDefinition :
+        SerializationDefinition
+    {
+        public const string Key = "MyCustomSerializer.Settings";
+
+        public override Func<IMessageMapper, IMessageSerializer> Configure(ReadOnlySettings settings)
+        {
+            return mapper => new MyCustomSerializer(settings.GetOrDefault<string>(Key));
+        }
+    }
+
+    class MyCustomSerializer :
+        IMessageSerializer
+    {
+        public MyCustomSerializer(string settingsValue)
+        {
+            // Add code initializing serializer on the basis of settingsValue
+        }
+
+        public void Serialize(object message, Stream stream)
+        {
+            // Add code to serialize message
+            throw new NotImplementedException();
+        }
+
+        public object[] Deserialize(Stream stream, IList<Type> messageTypes = null)
+        {
+            // Add code to deserialize message
+            throw new NotImplementedException();
+        }
+
+        public string ContentType
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+
+    static class MyCustomSerializerConfigurationExtensions
+    {
+        public static void Settings(this SerializationExtensions<MyCustomSerializerDefinition> config, string value)
+        {
+            config.GetSettings().Set(MyCustomSerializerDefinition.Key, value);
+        }
+    }
+
+    #endregion
 }
