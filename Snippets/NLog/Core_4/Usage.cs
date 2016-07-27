a938c9ae34f1f5e260c08e5a39a8fb2bbac1036e
Simon Cropp <simon.cropp@gmail.com>
2016-07-27 12:29:59 +1000

    add v4 nlog usage

---
+++ Snippets/NLog/Core_4/Usage.cs
@@ -0,0 +1,27 @@
+﻿using global::NLog;
+using global::NLog.Config;
+using global::NLog.Targets;
+using NServiceBus;
+
+class Usage
+{
+    Usage()
+    {
+        #region NLogInCode
+
+        var config = new LoggingConfiguration();
+
+        var consoleTarget = new ColoredConsoleTarget
+        {
+            Layout = "${level}|${logger}|${message}${onexception:${newline}${exception:format=tostring}}"
+        };
+        config.AddTarget("console", consoleTarget);
+        config.LoggingRules.Add(new LoggingRule("*", LogLevel.Debug, consoleTarget));
+
+        LogManager.Configuration = config;
+
+        SetLoggingLibrary.NLog();
+
+        #endregion
+    }
+}
