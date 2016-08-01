f2a091d442bb301fb931996692e458194b23c0ec
Weronika Łabaj <weronika.labaj@particular.net>
2016-08-01 14:28:14 +0200

    Missed changes in 2 files

--- Snippets/Core/Core_6/Routing/RoutingAPIs.cs
+++ Snippets/Core/Core_6/Routing/RoutingAPIs.cs
@@ -171,46 +171,6 @@ namespace Core6.Routing
             throw new NotImplementedException();
         }
 
-        void InstanceMappingFileConfig(EndpointConfiguration endpointConfiguration)
-        {
-            #region InstanceMappingFile-Config
-
-            var transport = endpointConfiguration.UseTransport<MsmqTransport>();
-            var routing = transport.Routing();
-            var routingTable = routing.InstanceMappingFile();
-            routingTable.FilePath(@"C:\Routes.xml");
-            routing.RouteToEndpoint(typeof(AcceptOrder), "Sales");
-            routing.RouteToEndpoint(typeof(SendOrder), "Shipping");
-
-            #endregion
-        }
-
-        public void InstanceMappingFileRefreshInterval(EndpointConfiguration endpointConfiguration)
-        {
-            #region InstanceMappingFile-RefreshInterval
-
-
-            var transport = endpointConfiguration.UseTransport<MsmqTransport>();
-            var routing = transport.Routing();
-            var routingTable = routing.InstanceMappingFile();
-            var fileRoutingTable = routingTable.FilePath(@"C:\Routes.xml");
-            fileRoutingTable.RefreshInterval(TimeSpan.FromSeconds(45));
-
-            #endregion
-        }
-
-        public void InstanceMappingFilePath(EndpointConfiguration endpointConfiguration)
-        {
-            #region InstanceMappingFile-FilePath
-
-            var transport = endpointConfiguration.UseTransport<MsmqTransport>();
-            var routing = transport.Routing();
-            var routingTable = routing.InstanceMappingFile();
-            routingTable.FilePath(@"C:\Routes.xml");
-
-            #endregion
-        }
-
         interface IUseCustomDistributionStrategy
         {
         }
