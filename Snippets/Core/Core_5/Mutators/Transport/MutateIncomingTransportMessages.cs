551b4ea7ef7f05bb5bce54ae3ac45e46e65aa362
Simon Cropp <simon.cropp@gmail.com>
2016-08-24 08:04:21 +1000

    Update MutateIncomingTransportMessages.cs

--- Snippets/Core/Core_5/Mutators/Transport/MutateIncomingTransportMessages.cs
+++ Snippets/Core/Core_5/Mutators/Transport/MutateIncomingTransportMessages.cs
@@ -15,7 +15,6 @@
             // optionally replace the Body
             transportMessage.Body = ServiceThatChangesBody.Mutate(transportMessage.Body);
 
-
             // the incoming headers
             var headers = transportMessage.Headers;
 
