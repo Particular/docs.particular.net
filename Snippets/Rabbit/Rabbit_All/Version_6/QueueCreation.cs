d79e9b8f15458187c9a573140ff6ec7e0401fc9d
Tomasz Masternak <tomasz.masternak@particular.net>
2016-08-26 10:56:29 +0200

    updated rabittmq integration scripts for ceating/deleteing endpoint queues

---
+++ Snippets/Rabbit/Rabbit_All/Version_6/QueueCreation.cs
@@ -0,0 +1,57 @@
+﻿namespace Rabbit_All.Version_6
+{
+    using System;
+    using RabbitMQ.Client;
+
+    public static class QueueCreation
+    {
+        #region rabbit-create-queues
+
+        public static void CreateQueuesForEndpoint(string uri, string endpointName, bool durableMessages, bool createExchanges)
+        {
+            // main queue
+            CreateQueue(uri, endpointName, durableMessages, createExchanges);
+
+            // callback queue
+            CreateQueue(uri, $"{endpointName}.{Environment.MachineName}", durableMessages, createExchanges);
+
+            // timeout queue
+            CreateQueue(uri, $"{endpointName}.Timeouts", durableMessages, createExchanges);
+
+            // timeout dispatcher queue
+            CreateQueue(uri, $"{endpointName}.TimeoutsDispatcher", durableMessages, createExchanges);
+        }
+
+        public static void CreateQueue(string uri, string queueName, bool durableMessages, bool createExchange)
+        {
+            var connectionFactory = new ConnectionFactory
+            {
+                Uri = uri,
+            };
+
+            using (var connection = connectionFactory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: queueName,
+                    durable: durableMessages,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                if (createExchange)
+                {
+                    CreateExchange(channel, queueName, durableMessages);
+                }
+            }
+        }
+
+        static void CreateExchange(IModel channel, string queueName, bool durableMessages)
+        {
+            channel.ExchangeDeclare(queueName, ExchangeType.Fanout, durableMessages);
+            channel.QueueBind(queueName, queueName, string.Empty);
+        }
+
+        #endregion
+    }
+}
---
+++ Snippets/Rabbit/Rabbit_All/Version_6/QueueDeletion.cs
@@ -0,0 +1,43 @@
+﻿namespace Rabbit_All.Version_6
+{
+    using System;
+    using RabbitMQ.Client;
+
+    public class QueueDeletion
+    {
+        #region rabbit-delete-queues
+
+        public static void DeleteQueue(string uri, string queueName)
+        {
+            var connectionFactory = new ConnectionFactory
+            {
+                Uri = uri,
+            };
+
+            using (var connection = connectionFactory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueUnbind(queueName, queueName, string.Empty, null);
+                channel.ExchangeDelete(queueName);
+                channel.QueueDelete(queueName);
+            }
+        }
+
+        public static void DeleteQueuesForEndpoint(string uri, string endpointName)
+        {
+            // main queue
+            DeleteQueue(uri, endpointName);
+
+            // callback queue
+            DeleteQueue(uri, $"{endpointName}.{Environment.MachineName}");
+
+            // timeout queue
+            DeleteQueue(uri, $"{endpointName}.Timeouts");
+
+            // timeout dispatcher queue
+            DeleteQueue(uri, $"{endpointName}.TimeoutsDispatcher");
+        }
+
+        #endregion
+    }
+}
