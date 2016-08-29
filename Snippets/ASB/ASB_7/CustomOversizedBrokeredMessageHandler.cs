5a638eadd48c4e5afb394a50a3ba5be564f0aae0
Yves Goeleven <yves@goeleven.com>
2016-08-29 14:50:51 +0200

    useful & e.g.

--- Snippets/ASB/ASB_7/CustomOversizedBrokeredMessageHandler.cs
+++ Snippets/ASB/ASB_7/CustomOversizedBrokeredMessageHandler.cs
@@ -7,7 +7,7 @@ internal class CustomOversizedBrokeredMessageHandler : IHandleOversizedBrokeredM
 {
     public Task Handle(BrokeredMessage brokeredMessage)
     {
-        // do something usefull with the brokered message, f.e. store it in blob storage
+        // do something usefull with the brokered message, e.g. store it in blob storage
         return Task.FromResult(true);
     }
 }
