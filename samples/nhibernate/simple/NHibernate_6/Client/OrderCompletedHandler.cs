a0f5f5c32899203b4d9925694a912957ca742c98
Hadi Eskandari <h.eskandari@gmail.com>
2016-08-29 19:48:53 +1000

    Adding client subscription

---
+++ samples/nhibernate/simple/NHibernate_6/Client/OrderCompletedHandler.cs
@@ -0,0 +1,13 @@
+﻿using NServiceBus;
+using NServiceBus.Logging;
+
+public class OrderCompletedHandler :
+    IHandleMessages<OrderCompleted>
+{
+    static ILog log = LogManager.GetLogger<OrderCompletedHandler>();
+
+    public void Handle(OrderCompleted message)
+    {
+        log.Info($"Received OrderCompleted for OrderId {message.OrderId}");
+    }
+}
