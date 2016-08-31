bc9d5659da8e8fe94d9a94c9f8401d0dd6126612
Simon Cropp <simon.cropp@gmail.com>
2016-08-31 15:50:24 +1000

    remove SqlServer_1.
    it only had one snippet in it

--- Snippets/SqlServer/SqlServer_2/UpgradeGuides/MultiInstance.cs
+++ Snippets/SqlServer/SqlServer_2/UpgradeGuides/MultiInstance.cs
@@ -1,38 +1,39 @@
 ﻿    using NServiceBus;
     using NServiceBus.Transports.SQLServer;
 
-    class SqlServer
+class SqlServer
+{
+    void MultiInstance(BusConfiguration busConfiguration)
     {
-        void MultiInstance(BusConfiguration busConfiguration)
-        {
-            #region sqlserver-multiinstance-upgrade [2.1,3.0)
+        #region sqlserver-multiinstance-upgrade [2.1,3.0)
 
-            var transport = busConfiguration.UseTransport<SqlServerTransport>();
-            // Option 1
-            transport
-                .UseSpecificConnectionInformation(
-                    EndpointConnectionInfo.For("RemoteEndpoint")
-                        .UseConnectionString("SomeConnectionString"),
-                    EndpointConnectionInfo.For("AnotherEndpoint")
-                        .UseConnectionString("SomeOtherConnectionString"));
+        var transport = busConfiguration.UseTransport<SqlServerTransport>();
+        // Option 1
+        transport
+            .UseSpecificConnectionInformation(
+                EndpointConnectionInfo.For("RemoteEndpoint")
+                    .UseConnectionString("SomeConnectionString"),
+                EndpointConnectionInfo.For("AnotherEndpoint")
+                    .UseConnectionString("SomeOtherConnectionString"));
 
-            // Option 2
-            transport
-                .UseSpecificConnectionInformation(x =>
+        // Option 2
+        transport
+            .UseSpecificConnectionInformation(
+                connectionInformationProvider: x =>
                 {
                     if (x == "RemoteEndpoint")
                     {
-                        var connectionInfo = ConnectionInfo.Create();
-                        return connectionInfo.UseConnectionString("SomeConnectionString");
+                        var connection = ConnectionInfo.Create();
+                        return connection.UseConnectionString("SomeConnectionString");
                     }
                     if (x == "AnotherEndpoint")
                     {
-                        var connectionInfo = ConnectionInfo.Create();
-                        return connectionInfo.UseConnectionString("SomeOtherConnectionString");
+                        var connection = ConnectionInfo.Create();
+                        return connection.UseConnectionString("SomeOtherConnectionString");
                     }
                     return null;
                 });
 
-            #endregion
-        }
+        #endregion
     }
+}
