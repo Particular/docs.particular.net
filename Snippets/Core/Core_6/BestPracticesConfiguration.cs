eb8ce0e5ae930981f5ad22a530a25e806e8419b4
Simon Cropp <simon.cropp@gmail.com>
2016-08-31 18:17:12 +1000

    trnsport agnostic

--- Snippets/Core/Core_6/BestPracticesConfiguration.cs
+++ Snippets/Core/Core_6/BestPracticesConfiguration.cs
@@ -2,31 +2,48 @@
 {
     using System.Threading.Tasks;
     using NServiceBus;
+    using NServiceBus.Settings;
+    using NServiceBus.Transport;
 
     class BestPracticesConfiguration
     {
         void DisableFeature(EndpointConfiguration endpointConfiguration)
         {
             #region DisableBestPracticeEnforcementPerEndpoint
-            var routing = endpointConfiguration.UseTransport<MsmqTransport>().Routing();
+
+            var transport = endpointConfiguration.UseTransport<TransportName>();
+            var routing = transport.Routing();
             routing.DoNotEnforceBestPractices();
+
             #endregion
         }
 
         async Task DisablePerMessage(IPipelineContext context)
         {
             #region DisableBestPracticeEnforcementPerMessage
+
             var options = new SendOptions();
 
             options.DoNotEnforceBestPractices();
 
             await context.Send(new MyEvent(), options)
                 .ConfigureAwait(false);
+
             #endregion
         }
 
         class MyEvent
         {
+        }
+
+        class TransportName : TransportDefinition
+        {
+            public override TransportInfrastructure Initialize(SettingsHolder settings, string connectionString)
+            {
+                throw new System.NotImplementedException();
+            }
+
+            public override string ExampleConnectionStringForErrorMessage { get; }
         }
     }
 }
