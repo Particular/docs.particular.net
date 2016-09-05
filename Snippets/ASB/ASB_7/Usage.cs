ffa737b9bdf301290906971aa7c766fd29354be5
Yves Goeleven <yves@goeleven.com>
2016-09-05 14:21:14 +0200

    removed unneccesary using

--- Snippets/ASB/ASB_7/Usage.cs
+++ Snippets/ASB/ASB_7/Usage.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Reflection;
 using Microsoft.ServiceBus.Messaging;
 using NServiceBus;
-using NServiceBus.Pipeline;
 using NServiceBus.Transport.AzureServiceBus;
 
 class Usage
