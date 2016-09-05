97aa6cfb7c8cb74456b8c0641f5fbb170e082e03
Simon Cropp <simon.cropp@gmail.com>
2016-09-05 14:17:55 +1000

    redundant code

--- Snippets/ServiceControlContracts/ServiceControlContracts_1/ServiceControlEventsConfig.cs
+++ Snippets/ServiceControlContracts/ServiceControlContracts_1/ServiceControlEventsConfig.cs
@@ -8,7 +8,6 @@
         {
             #region ServiceControlEventsConfig
 
-            busConfiguration.UseSerialization<JsonSerializer>();
             var conventions = busConfiguration.Conventions();
             conventions.DefiningEventsAs(type =>
             {
