4a983014c4312a4bd7c7c2b1bafcf05be547de26
Yves Goeleven <yves@goeleven.com>
2016-09-09 13:41:14 +0200

    split subscriber

---
+++ samples/azure/custom-partitioning-asb/ASB_7/Subscriber1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+class Program
+{
+    static void Main()
+    {
+        MainAsync().GetAwaiter().GetResult();
+    }
+
+    static async Task MainAsync()
+    {
+        Console.Title = "Samples.ASB.Partitioning.Subscriber";
+        var endpointConfiguration = new EndpointConfiguration("Samples.ASB.Partitioning.Subscriber");
+        var transport = endpointConfiguration.UseTransport<AzureServiceBusTransport>();
+        var connectionString = Environment.GetEnvironmentVariable("AzureServiceBus.ConnectionString1");
+        if (string.IsNullOrWhiteSpace(connectionString))
+        {
+            throw new Exception($"Could not read the 'AzureServiceBus.ConnectionString1' environment variable. Check the sample prerequisites.");
+        }
+        transport.ConnectionString(connectionString);
+        var topology = transport.UseTopology<ForwardingTopology>();
+        
+        endpointConfiguration.SendFailedMessagesTo("error");
+        endpointConfiguration.UseSerialization<JsonSerializer>();
+        endpointConfiguration.EnableInstallers();
+        endpointConfiguration.UsePersistence<InMemoryPersistence>();
+        endpointConfiguration.Recoverability().Delayed(settings => settings.NumberOfRetries(0));
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        try
+        {
+            Console.WriteLine("Subscriber is ready to receive events on namespace1");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+        finally
+        {
+            await endpointInstance.Stop()
+                .ConfigureAwait(false);
+        }
+    }
+}
--- samples/azure/custom-partitioning-asb/ASB_7/Subscriber2/Program.cs
+++ samples/azure/custom-partitioning-asb/ASB_7/Subscriber2/Program.cs
@@ -12,14 +12,13 @@ class Program
     static async Task MainAsync()
     {
         Console.Title = "Samples.ASB.Partitioning.Subscriber";
-        Console.WriteLine("Which connectionstring shall I subscribe to [1, 2]:");
-        var instance = Console.ReadLine();
+
         var endpointConfiguration = new EndpointConfiguration("Samples.ASB.Partitioning.Subscriber");
         var transport = endpointConfiguration.UseTransport<AzureServiceBusTransport>();
-        var connectionString = Environment.GetEnvironmentVariable("AzureServiceBus.ConnectionString" + instance);
+        var connectionString = Environment.GetEnvironmentVariable("AzureServiceBus.ConnectionString2");
         if (string.IsNullOrWhiteSpace(connectionString))
         {
-            throw new Exception($"Could not read the 'AzureServiceBus.ConnectionString{instance}' environment variable. Check the sample prerequisites.");
+            throw new Exception($"Could not read the 'AzureServiceBus.ConnectionString2' environment variable. Check the sample prerequisites.");
         }
         transport.ConnectionString(connectionString);
         var topology = transport.UseTopology<ForwardingTopology>();
@@ -34,7 +33,7 @@ class Program
             .ConfigureAwait(false);
         try
         {
-            Console.WriteLine("Subscriber is ready to receive events on namespace" + instance);
+            Console.WriteLine("Subscriber is ready to receive events on namespace2");
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
         }
