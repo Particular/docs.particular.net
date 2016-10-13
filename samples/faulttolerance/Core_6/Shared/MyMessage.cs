c09032138989b7cd559e83d2b0ef40ea2e6103cf
Simon Cropp <simon.cropp@gmail.com>
2016-10-13 15:03:23 +1100

    redundant write line

--- samples/faulttolerance/Core_6/Shared/MyMessage.cs
+++ samples/faulttolerance/Core_6/Shared/MyMessage.cs
@@ -1,14 +1,8 @@
 ﻿using System;
-using System.Diagnostics;
 using NServiceBus;
 
 public class MyMessage :
     IMessage
 {
     public Guid Id { get; set; }
-
-    public MyMessage()
-    {
-        Debug.WriteLine("sdfsdf");
-    }
 }
