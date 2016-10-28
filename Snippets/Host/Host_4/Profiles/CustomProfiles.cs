2f07e2162627d3b7e346fd140924c8e35352d8e3
Simon Cropp <simon.cropp@gmail.com>
2016-10-28 15:45:24 +1100

    add missing snippet

---
+++ Snippets/Host/Host_4/Profiles/CustomProfiles.cs
@@ -0,0 +1,30 @@
+﻿using NServiceBus.Hosting.Profiles;
+// ReSharper disable RedundantNameQualifier
+#region profile_behavior
+class LiteEmailBehavior :
+    IHandleProfile<NServiceBus.Lite>
+{
+    public void ProfileActivated()
+    {
+        // set the NullEmailSender in the container
+    }
+}
+
+class IntegrationEmailBehavior :
+    IHandleProfile<NServiceBus.Integration>
+{
+    public void ProfileActivated()
+    {
+        // set the FileEmailSender in the container
+    }
+}
+
+class ProductionEmailBehavior :
+    IHandleProfile<NServiceBus.Production>
+{
+    public void ProfileActivated()
+    {
+        // set the SmtpEmailSender in the container
+    }
+}
+#endregion
