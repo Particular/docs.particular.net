8c51446a7204e8ce9d543aca6693aa8987106017
WilliamBZA <william.brander@gmail.com>
2016-10-31 09:14:13 +0200

    Add snippet for using an existing container

--- Snippets/SimpleInjector/SimpleInjector_1/Usage.cs
+++ Snippets/SimpleInjector/SimpleInjector_1/Usage.cs
@@ -1,4 +1,6 @@
 ﻿using NServiceBus;
+using SimpleInjector;
+using SimpleInjector.Extensions.ExecutionContextScoping;
 
 class Usage
 {
@@ -9,5 +11,24 @@ class Usage
         endpointConfiguration.UseContainer<SimpleInjectorBuilder>();
 
         #endregion
+
+        #region simpleinjector_Existing
+
+        var container = new Container();
+        container.Options.DefaultScopedLifestyle = new ExecutionContextScopeLifestyle();
+
+        container.Register(() => new MyService { Property = "Created outside" }, Lifestyle.Scoped);
+
+        endpointConfiguration.UseContainer<SimpleInjectorBuilder>(customizations =>
+        {
+            customizations.UseExistingContainer(container);
+        });
+
+        #endregion
+    }
+
+    class MyService
+    {
+        public string Property { get; set; }
     }
 }
