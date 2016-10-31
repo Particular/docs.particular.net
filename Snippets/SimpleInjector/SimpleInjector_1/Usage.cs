3744ccb8c4813c81e1e449c613f447af2f5e6cd0
Simon Cropp <simon.cropp@gmail.com>
2016-10-31 11:16:24 +1100

    add simpleinjector usage

---
+++ Snippets/SimpleInjector/SimpleInjector_1/Usage.cs
@@ -0,0 +1,13 @@
+﻿using NServiceBus;
+
+class Usage
+{
+    Usage(EndpointConfiguration endpointConfiguration)
+    {
+        #region Spring
+
+        endpointConfiguration.UseContainer<SimpleInjectorBuilder>();
+
+        #endregion
+    }
+}
---
+++ samples/containers/simpleinjector/SimpleInjector_1/Sample/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+static class Program
+{
+    static void Main()
+    {
+        AsyncMain().GetAwaiter().GetResult();
+    }
+
+    static async Task AsyncMain()
+    {
+        Console.Title = "Samples.SimpleInjector";
+
+        #region ContainerConfiguration
+
+        var endpointConfiguration = new EndpointConfiguration("Samples.SimpleInjector");
+
+        endpointConfiguration.RegisterComponents(
+            registration: components =>
+            {
+                components.RegisterSingleton(new MyService());
+            });
+        endpointConfiguration.UseContainer<SimpleInjectorBuilder>();
+
+        #endregion
+
+        endpointConfiguration.UseSerialization<JsonSerializer>();
+        endpointConfiguration.UsePersistence<InMemoryPersistence>();
+
+        endpointConfiguration.SendFailedMessagesTo("error");
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        try
+        {
+            var myMessage = new MyMessage();
+            await endpointInstance.SendLocal(myMessage)
+                .ConfigureAwait(false);
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+        finally
+        {
+            await endpointInstance.Stop()
+                .ConfigureAwait(false);
+        }
+    }
+}
