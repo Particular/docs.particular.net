e0540e979f08ac6e083ab23484b326be7393a5cb
Mike Minutillo <michael.minutillo@gmail.com>
2016-11-02 13:22:18 +0800

    Switch to using Azure Storage Queues

--- samples/show-case/cloud-azure/Core_6/Store.Shared/CommonConfiguration.cs
+++ samples/show-case/cloud-azure/Core_6/Store.Shared/CommonConfiguration.cs
@@ -4,7 +4,7 @@ public static class CommonConfiguration
 {
     public static void ApplyCommonConfiguration(this EndpointConfiguration endpointConfiguration)
     {
-        endpointConfiguration.UseTransport<MsmqTransport>();
+        endpointConfiguration.UseTransport<AzureStorageQueueTransport>().ConnectionString("UseDevelopmentStorage=true");
         endpointConfiguration.UsePersistence<InMemoryPersistence>();
         endpointConfiguration.RijndaelEncryptionService();
         endpointConfiguration.AuditProcessedMessagesTo("audit");
