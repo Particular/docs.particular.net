44eede93f3ca93fa817daaaf18956d1cf45a6b97
Simon Cropp <simon.cropp@gmail.com>
2016-11-15 14:25:36 +1100

    fix HB versions

--- Snippets/Heartbeats/Heartbeats6_2/Heartbeats.cs
+++ Snippets/Heartbeats/Heartbeats6_2/Heartbeats.cs
@@ -4,37 +4,43 @@ using ServiceControl.Features;
 
 class MyClass
 {
-    public void Foo()
+    public void Configure_ServiceControl()
     {
         #region Heartbeats_Configure_ServiceControl
 
         var endpointConfiguration = new EndpointConfiguration("myendpoint");
-        endpointConfiguration.HeartbeatPlugin("ServiceControl_Queue");
+        endpointConfiguration.HeartbeatPlugin(
+            serviceControlQueue: "ServiceControl_Queue");
 
         #endregion
     }
 
-    public void Foo2()
+    public void Interval()
     {
         #region Heartbeats_interval
 
         var endpointConfiguration = new EndpointConfiguration("myendpoint");
-        endpointConfiguration.HeartbeatPlugin("ServiceControl_Queue", frequency: TimeSpan.FromMinutes(2));
+        endpointConfiguration.HeartbeatPlugin(
+            serviceControlQueue: "ServiceControl_Queue",
+            frequency: TimeSpan.FromMinutes(2));
 
         #endregion
     }
 
-    public void Foo3()
+    public void Ttl()
     {
         #region Heartbeats_ttl
 
         var endpointConfiguration = new EndpointConfiguration("myendpoint");
-        endpointConfiguration.HeartbeatPlugin("ServiceControl_Queue", TimeSpan.FromSeconds(30), TimeSpan.FromMinutes(3));
+        endpointConfiguration.HeartbeatPlugin(
+            serviceControlQueue: "ServiceControl_Queue",
+            frequency: TimeSpan.FromSeconds(30),
+            timeToLive: TimeSpan.FromMinutes(3));
 
         #endregion
     }
 
-    public void Foo4()
+    public void Disable()
     {
         #region Heartbeats_disable
 
