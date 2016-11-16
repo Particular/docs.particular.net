d50c7af5828532153ef979a7b38c0e56f6ae5d5d
Simon Cropp <simon.cropp@gmail.com>
2016-11-16 17:28:09 +1100

    update to NSB common logging 4

---
+++ Snippets/CommonLogging/CommonLogging_4/Usage.cs
@@ -0,0 +1,16 @@
+﻿using Common.Logging;
+using Common.Logging.Simple;
+using NServiceBus;
+
+class Usage
+{
+    Usage()
+    {
+        #region CommonLoggingInCode
+
+        LogManager.Adapter = new ConsoleOutLoggerFactoryAdapter();
+        NServiceBus.Logging.LogManager.Use<CommonLoggingFactory>();
+
+        #endregion
+    }
+}
---
+++ samples/logging/commonlogging/CommonLogging_4/Sample/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using Common.Logging;
+using NServiceBus;
+using Common.Logging.Simple;
+// ReSharper disable RedundantNameQualifier
+
+class Program
+{
+
+    static void Main()
+    {
+        AsyncMain().GetAwaiter().GetResult();
+    }
+
+    static async Task AsyncMain()
+    {
+        Console.Title = "Samples.Logging.CommonLogging";
+
+        #region ConfigureLogging
+
+        Common.Logging.LogManager.Adapter = new ConsoleOutLoggerFactoryAdapter
+        {
+            Level = LogLevel.Info
+        };
+
+        NServiceBus.Logging.LogManager.Use<CommonLoggingFactory>();
+
+        var endpointConfiguration = new EndpointConfiguration("Samples.Logging.CommonLogging");
+
+        #endregion
+
+        endpointConfiguration.UseSerialization<JsonSerializer>();
+        endpointConfiguration.EnableInstallers();
+        endpointConfiguration.UsePersistence<InMemoryPersistence>();
+        endpointConfiguration.SendFailedMessagesTo("error");
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        try
+        {
+            var myMessage = new MyMessage();
+            await endpointInstance.SendLocal(myMessage)
+                .ConfigureAwait(false);
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+        finally
+        {
+            await endpointInstance.Stop()
+                .ConfigureAwait(false);
+        }
+    }
+}
