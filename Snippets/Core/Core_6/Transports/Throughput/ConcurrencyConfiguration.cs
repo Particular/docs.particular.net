39d3fe1dda6b13320bd746aef6ae0942945906c0
Tim Bussmann <timbussmann@users.noreply.github.com>
2016-11-22 22:41:18 +0100

    TimeoutManager concurrency setting (#2246)
    * document TimeoutManager concurrency config
    
    * update to latest unstable package
    
    * fix broken reference

--- Snippets/Core/Core_6/Transports/Throughput/ConcurrencyConfiguration.cs
+++ Snippets/Core/Core_6/Transports/Throughput/ConcurrencyConfiguration.cs
@@ -9,6 +9,12 @@
             #region TuningFromCode
             endpointConfiguration.LimitMessageProcessingConcurrencyTo(5);
             #endregion
+
+            #region TuningTimeoutManagerConcurrency 6.1
+
+            var timeoutManager = endpointConfiguration.TimeoutManager();
+            timeoutManager.LimitMessageProcessingConcurrencyTo(4);
+            #endregion
         }
 
     }
