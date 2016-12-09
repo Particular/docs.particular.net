b317716b2169285e51e41764e8cf483b42f480e9
Simon Cropp <simon.cropp@gmail.com>
2016-12-09 11:17:06 +1100

    fix raw doco

---
+++ Snippets/RawMessaging/RawMessaging_0/Usage.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Extensibility;
+using NServiceBus.Raw;
+using NServiceBus.Routing;
+using NServiceBus.Transport;
+
+public class Usage
+{
+    static async Task Start()
+    {
+        #region Configuration
+
+        var senderConfig = RawEndpointConfiguration.Create(
+            endpointName: "EndpointName", 
+            onMessage: OnMessage, 
+            poisonMessageQueue: "error");
+        senderConfig.UseTransport<MsmqTransport>();
+
+        var sender = await RawEndpoint.Start(senderConfig)
+            .ConfigureAwait(false);
+
+        #endregion
+
+        #region Sending
+
+        var body = Serialize();
+        var headers = new Dictionary<string, string>
+        {
+            ["SomeHeader"] = "SomeValue"
+        };
+        var request = new OutgoingMessage(
+            messageId: Guid.NewGuid().ToString(),
+            headers: headers, 
+            body: body);
+
+        var operation = new TransportOperation(
+            request,
+            new UnicastAddressTag("Receiver"));
+
+        await sender.SendRaw(
+                outgoingMessages: new TransportOperations(operation),
+                transaction: new TransportTransaction(),
+                context: new ContextBag())
+            .ConfigureAwait(false);
+
+        #endregion
+    }
+
+    #region Receiving
+
+    static Task OnMessage(MessageContext context, IDispatchMessages dispatcher)
+    {
+        var message = Deserialize(context.Body);
+
+        Console.WriteLine(message);
+        // Can use dispatcher to send messages from here
+
+        return Task.CompletedTask;
+    }
+
+    #endregion
+
+    static byte[] Serialize()
+    {
+        throw new NotImplementedException();
+    }
+
+    static string Deserialize(byte[] body)
+    {
+        throw new NotImplementedException();
+    }
+}
