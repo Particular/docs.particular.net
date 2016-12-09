c5623a152fdd77e88ba807cc6172949db2fe5270
Simon Cropp <simon.cropp@gmail.com>
2016-12-09 18:49:32 +1100

    add routing slips

---
+++ samples/routing-slips/MessageRouting_2/Messages/SequentialProcess.cs
@@ -0,0 +1,10 @@
+﻿using NServiceBus;
+#region multi-message
+public class SequentialProcess :
+    ICommand
+{
+    public string StepAInfo { get; set; }
+    public string StepBInfo { get; set; }
+    public string StepCInfo { get; set; }
+}
+#endregion
---
+++ samples/routing-slips/MessageRouting_2/Sender/Program.cs
@@ -0,0 +1,97 @@
+using NServiceBus.Logging;
+using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.MessageRouting.RoutingSlips;
+
+class Program
+{
+
+    static ILog log = LogManager.GetLogger(typeof(Program));
+
+    static void Main()
+    {
+        RunBus().GetAwaiter().GetResult();
+    }
+
+    static async Task RunBus()
+    {
+        Console.Title = "Samples.RoutingSlips.Sender";
+        var endpointConfiguration = new EndpointConfiguration("Samples.RoutingSlips.Sender");
+
+        endpointConfiguration.UseTransport<MsmqTransport>();
+        endpointConfiguration.SendFailedMessagesTo("error");
+        endpointConfiguration.UsePersistence<InMemoryPersistence>();
+
+        #region enableRoutingSlips
+        endpointConfiguration.EnableFeature<RoutingSlips>();
+        #endregion
+
+        var endpoint = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+
+
+        Console.WriteLine("Press Enter to send a message");
+        Console.WriteLine("Press any other key to exit");
+        #region alternate
+        var toggle = false;
+        while (true)
+        {
+            var key = Console.ReadKey();
+            if (key.Key != ConsoleKey.Enter)
+            {
+                break;
+            }
+            if (toggle)
+            {
+                await SendToABC(endpoint);
+            }
+            else
+            {
+                await SendToAC(endpoint);
+            }
+
+            toggle = !toggle;
+        }
+        #endregion
+        await endpoint.Stop()
+            .ConfigureAwait(false);
+    }
+    #region SendAC
+    static Task SendToAC(IEndpointInstance endpoint)
+    {
+        var sequentialProcess = new SequentialProcess
+        {
+            StepAInfo = "Foo",
+            StepCInfo = "Baz",
+        };
+
+        log.Info("Sending message for step A, C");
+        return endpoint.Route(sequentialProcess, Guid.NewGuid(),
+            "Samples.RoutingSlips.StepA",
+            "Samples.RoutingSlips.StepC",
+            "Samples.RoutingSlips.ResultHost");
+    }
+    #endregion
+
+    #region SendABC
+    static Task SendToABC(IEndpointInstance endpoint)
+    {
+        #region multi-message-Send
+        var sequentialProcess = new SequentialProcess
+        {
+            StepAInfo = "Foo",
+            StepBInfo = "Bar",
+            StepCInfo = "Baz",
+        };
+        #endregion
+
+        log.Info("Sending message for step A, B, C");
+        return endpoint.Route(sequentialProcess, Guid.NewGuid(),
+            "Samples.RoutingSlips.StepA",
+            "Samples.RoutingSlips.StepB",
+            "Samples.RoutingSlips.StepC",
+            "Samples.RoutingSlips.ResultHost");
+    }
+    #endregion
+}
