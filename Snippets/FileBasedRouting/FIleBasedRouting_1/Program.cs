456100ae55e0f33579d2ea6df3ba04d9062a2626
SzymonPobiega <szymon.pobiega@gmail.com>
2016-12-16 13:25:03 +0100

    Added file-based-routing doco

---
+++ Snippets/FileBasedRouting/FIleBasedRouting_1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.FileBasedRouting;
+using NServiceBus.Settings;
+using NServiceBus.Transport;
+
+namespace FIleBasedRouting_1
+{
+    class Snippets
+    {
+        public void Enable(EndpointConfiguration config)
+        {
+            #region Enable
+
+            var routing = config.UseTransport<MyTransport>().Routing();
+            routing.UseFileBasedRouting();
+
+            #endregion
+        }
+
+        public void EnableCustomPath(EndpointConfiguration config)
+        {
+            #region EnableCustomPath
+
+            var routing = config.UseTransport<MyTransport>().Routing();
+            routing.UseFileBasedRouting(@"C:\routingFile.xml");
+
+            #endregion
+        }
+    }
+
+    class MyTransport : TransportDefinition
+    {
+        public override TransportInfrastructure Initialize(SettingsHolder settings, string connectionString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ExampleConnectionStringForErrorMessage { get; }
+    }
+}
---
+++ samples/routing/file-based-routing/FileBasedRouting_1/Sales/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.FileBasedRouting;
+
+class Program
+{
+    static void Main()
+    {
+        AsyncMain().GetAwaiter().GetResult();
+    }
+
+    static async Task AsyncMain()
+    {
+        Console.Title = "Samples.FileBasedRouting.Sales";
+        var endpointConfiguration = new EndpointConfiguration("Samples.FileBasedRouting.Sales");
+        endpointConfiguration.UseSerialization<JsonSerializer>();
+        endpointConfiguration.UsePersistence<InMemoryPersistence>();
+        endpointConfiguration.EnableInstallers();
+        endpointConfiguration.SendFailedMessagesTo("error");
+
+        var transport = endpointConfiguration.UseTransport<MsmqTransport>();
+        var routing = transport.Routing();
+        transport.Routing().UseFileBasedRouting(@"..\..\..\endpoints.xml");
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
