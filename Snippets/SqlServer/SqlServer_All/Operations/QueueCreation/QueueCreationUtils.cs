652dfd895cbd3ff32e66be5bca4e1a7222e95ffa
Simon Cropp <simon.cropp@gmail.com>
2016-12-29 22:45:49 +1100

    fix indentation

--- Snippets/SqlServer/SqlServer_All/Operations/QueueCreation/QueueCreationUtils.cs
+++ Snippets/SqlServer/SqlServer_All/Operations/QueueCreation/QueueCreationUtils.cs
@@ -12,28 +12,28 @@ namespace SqlServer_All.Operations.QueueCreation
             var sql = $@"IF NOT  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[{schema}].[{queueName}]') AND type in (N'U'))
                 BEGIN
                 CREATE TABLE [{schema}].[{queueName}](
-	                [Id] [uniqueidentifier] NOT NULL,
-	                [CorrelationId] [varchar](255),
-	                [ReplyToAddress] [varchar](255),
-	                [Recoverable] [bit] NOT NULL,
-	                [Expires] [datetime],
-	                [Headers] [varchar](max) NOT NULL,
-	                [Body] [varbinary](max),
-	                [RowVersion] [bigint] IDENTITY(1,1) NOT NULL
+                    [Id] [uniqueidentifier] NOT NULL,
+                    [CorrelationId] [varchar](255),
+                    [ReplyToAddress] [varchar](255),
+                    [Recoverable] [bit] NOT NULL,
+                    [Expires] [datetime],
+                    [Headers] [varchar](max) NOT NULL,
+                    [Body] [varbinary](max),
+                    [RowVersion] [bigint] IDENTITY(1,1) NOT NULL
                 ) ON [PRIMARY];
                 CREATE CLUSTERED INDEX [Index_RowVersion] ON [{schema}].[{queueName}]
                 (
-	                [RowVersion] ASC
+                    [RowVersion] ASC
                 ) ON [PRIMARY]
-		CREATE NONCLUSTERED INDEX [Index_Expires] ON [{schema}].[{queueName}]
-	        (
-			[Expires] ASC
-	        )
-	    	INCLUDE
-	    	(
-			[Id],
-			[RowVersion]
-		)
+        CREATE NONCLUSTERED INDEX [Index_Expires] ON [{schema}].[{queueName}]
+            (
+            [Expires] ASC
+            )
+            INCLUDE
+            (
+            [Id],
+            [RowVersion]
+        )
                 END";
             using (var command = new SqlCommand(sql, connection))
             {
