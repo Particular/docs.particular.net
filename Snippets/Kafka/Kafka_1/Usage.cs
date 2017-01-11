f3b65c14c9d83449fb8e7f3e39fff1a23a0cb806
Simon Cropp <simon.cropp@gmail.com>
2017-01-11 08:20:08 +0000

    add kafka

---
+++ Snippets/Kafka/Kafka_1/Usage.cs
@@ -0,0 +1,15 @@
+﻿using NServiceBus;
+using NServiceBus.Transport.Kafka;
+
+class Usage
+{
+    void KafkaTransport(EndpointConfiguration endpointConfiguration)
+    {
+        #region KafkaTransport
+
+        var transport = endpointConfiguration.UseTransport<KafkaTransport>();
+        transport.ConnectionString("127.0.0.1:9092");
+        
+        #endregion
+    }
+}
---
+++ nservicebus/kafka/index.md
@@ -0,0 +1,12 @@
+---
+title: Kafka Transport
+reviewed: 2017-01-10
+component: Kafka
+---
+
+A transport built on top of [Apache Kafka](https://kafka.apache.org/).
+
+
+## Usage
+
+snippet: KafkaTransport
