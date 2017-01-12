606c9807cd20e2c04106248340955c7708286d80
Simon Cropp <simon.cropp@gmail.com>
2017-01-12 09:47:13 +0000

    fix tabs

--- samples/sqltransport/native-integration/SqlServer_3/Receiver/Scripts.sql
+++ samples/sqltransport/native-integration/SqlServer_3/Receiver/Scripts.sql
@@ -2,17 +2,17 @@
 -- startcode SendFromTSQL
 -- TSql that can be pasted into Sql Server Query analyzer to send straight from the DB
 INSERT INTO [Samples.SqlServer.NativeIntegration] (Id , Recoverable, Headers, Body)
-VALUES	(CONVERT(UNIQUEIDENTIFIER, HASHBYTES('MD5','MyUniqueId')),
-		'true',
-		'',
-		CONVERT(varbinary(255), '{ $type: "PlaceOrder",OrderId: "Order from TSQL sender"}'))
+VALUES (CONVERT(UNIQUEIDENTIFIER, HASHBYTES('MD5','MyUniqueId')),
+    'true',
+    '',
+    CONVERT(varbinary(255), '{ $type: "PlaceOrder",OrderId: "Order from TSQL sender"}'))
 -- endcode
 
 -- startcode CreateLegacyTable
 -- Create a "legacy" Orders table
 CREATE TABLE Orders(
-	Id int IDENTITY(1,1) NOT NULL,
-	OrderValue money NOT NULL
+  Id int IDENTITY(1,1) NOT NULL,
+  OrderValue money NOT NULL
 )
 GO
 -- endcode
@@ -21,18 +21,18 @@ GO
 -- startcode CreateTrigger
 -- Create a trigger to push a message out for each new order
 CREATE TRIGGER OrderAcceptedTrigger
-	ON  Orders
-	AFTER INSERT
+  ON Orders
+  AFTER INSERT
 AS
 BEGIN
-	SET NOCOUNT ON;
+  SET NOCOUNT ON;
 
-	INSERT INTO [Samples.SqlServer.NativeIntegration] (Id, Recoverable, Headers ,Body)
-	SELECT CONVERT(UNIQUEIDENTIFIER, HASHBYTES('MD5',CONVERT(VARCHAR(255),i.Id))) as Id,
-	'true' as Recoverable,
-	'' as Headers,
-	CONVERT(varbinary(255), '{ $type: "LegacyOrderDetected",OrderId: "Hello from legacy Order ' + CONVERT(VARCHAR(255),i.Id) + '"}') as Body
-	FROM inserted i
+  INSERT INTO [Samples.SqlServer.NativeIntegration] (Id, Recoverable, Headers ,Body)
+  SELECT CONVERT(UNIQUEIDENTIFIER, HASHBYTES('MD5',CONVERT(VARCHAR(255),i.Id))) as Id,
+  'true' as Recoverable,
+  '' as Headers,
+  CONVERT(varbinary(255), '{ $type: "LegacyOrderDetected",OrderId: "Hello from legacy Order ' + CONVERT(VARCHAR(255),i.Id) + '"}') as Body
+  FROM inserted i
 END
 GO
 -- endcode
