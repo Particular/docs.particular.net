e7a6d33939f31432f5274984bc4c356011dbd19d
Simon Cropp <simon.cropp@gmail.com>
2017-01-16 04:06:03 +1100

    add kafka sample

---
+++ samples/kafka/Kafka_1/Sender/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Transport.Kafka;
+
+class Program
+{
+    static void Main()
+    {
+        AsyncMain().GetAwaiter().GetResult();
+    }
+
+    static async Task AsyncMain()
+    {
+        Console.Title = "Samples.Kafka.SimpleSender";
+        var endpointConfiguration = new EndpointConfiguration("Samples.Kafka.SimpleSender");
+        endpointConfiguration.SendFailedMessagesTo("error");
+        endpointConfiguration.UsePersistence<InMemoryPersistence>();
+        endpointConfiguration.EnableInstallers();
+
+        #region TransportConfiguration
+
+        var transport = endpointConfiguration.UseTransport<KafkaTransport>();
+        transport.ConnectionString("127.0.0.1:9092");
+        #endregion
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        await endpointInstance.Send("Samples.Kafka.SimpleReceiver", new MyMessage())
+            .ConfigureAwait(false);
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+
+}
---
+++ samples/kafka/sample.md
@@ -0,0 +1,20 @@
+---
+title: Simple Kafka Transport Usage
+summary: A simple send and receive scenario with the Kafka Transport .
+reviewed: 2017-01-16
+component: Kafka
+related:
+- nservicebus/kafka
+---
+
+
+## Prerequisites
+
+
+
+## Running the project
+
+
+## Code Walk-through
+
+
