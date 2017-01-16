9f3a6f2f5c198fadcc278b172a5d6b53579f32b2
Simon Cropp <simon.cropp@gmail.com>
2017-01-17 08:36:15 +1100

    WIP: add kafka sample (#2377)
    * add kafka sample
    
    * kafka readme

---
+++ samples/kafka/Kafka_1/Receiver/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Transport.Kafka;
+
+class Program
+{
+    static void Main()
+    {
+        AsyncMain().GetAwaiter().GetResult();
+    }
+
+    static async Task AsyncMain()
+    {
+        Console.Title = "Samples.Kafka.SimpleReceiver";
+        var endpointConfiguration = new EndpointConfiguration("Samples.Kafka.SimpleReceiver");
+        endpointConfiguration.SendFailedMessagesTo("error");
+        var transport = endpointConfiguration.UseTransport<KafkaTransport>();
+        transport.ConnectionString("127.0.0.1:9092");
+        endpointConfiguration.UsePersistence<InMemoryPersistence>();
+        endpointConfiguration.EnableInstallers();
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        try
+        {
+            Console.WriteLine("Press any key to exit");
+            Console.WriteLine("Waiting for message from the Sender");
+            Console.ReadKey();
+        }
+        finally
+        {
+            await endpointInstance.Stop()
+                .ConfigureAwait(false);
+        }
+    }
+
+}
---
+++ samples/kafka/sample.md
@@ -0,0 +1,33 @@
+---
+title: Simple Kafka Transport Usage
+summary: A simple send and receive scenario with the Kafka Transport
+reviewed: 2017-01-16
+component: Kafka
+related:
+- nservicebus/kafka
+---
+
+
+## Prerequisites
+
+Ensure an instance of Kafka is installed and accessible in '127.0.0.1:9092'. 
+
+Useful links:
+
+ * [Setting Up and Running Apache Kafka on Windows OS](https://dzone.com/articles/running-apache-kafka-on-windows-os)
+ * [Kafka Tool: GUI for managing and using Kafka clusters](http://www.kafkatool.com/)
+
+
+## Running the project
+
+ 1. Start both the Sender and Receiver projects.
+ 1. At startup Sender will send a message to Receiver.
+ 1. Receiver will handle the message.
+
+
+## Code Walk-through
+
+
+### Configure the Kafka
+
+snippet: TransportConfiguration
