9f396f1ba1db82de202f7057f35f140224024034
danielmarbach <daniel.marbach@openplace.net>
2017-01-18 11:05:22 +0100

    Enable installers based on cmd args or machine name convention

--- Snippets/Core/Core_6/Installers.cs
+++ Snippets/Core/Core_6/Installers.cs
@@ -2,6 +2,7 @@
 {
     using System.Threading.Tasks;
     using NServiceBus;
+    using NServiceBus.Support;
 
     class ForInstallationOnReplacement
     {
@@ -17,6 +18,39 @@
 
             #endregion
         }
+    }
 
+    class SwitchInstallersWithCommandline
+    {
+        static EndpointConfiguration endpointConfiguration = new EndpointConfiguration("someEndpoint");
+
+        #region InstallersRunWhenNecessaryCommandLine
+
+        public static void Main(string[] args)
+        {
+            var runInstallers = args.Length == 1 && args[0] == "/runInstallers";
+
+            if (runInstallers)
+            {
+                endpointConfiguration.EnableInstallers();
+            }
+        }
+
+        #endregion
+    }
+
+    class SwitchInstallersByMachineNameConvention
+    {
+        async Task Simple(EndpointConfiguration endpointConfiguration)
+        {
+            #region InstallersRunWhenNecessaryMachineNameConvention
+
+            if (!RuntimeEnvironment.MachineName.EndsWith("-PROD"))
+            {
+                endpointConfiguration.EnableInstallers();
+            }
+
+            #endregion
+        }
     }
 }
