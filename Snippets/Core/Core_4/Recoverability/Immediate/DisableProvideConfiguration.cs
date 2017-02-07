87c67489d79a6b59f8d9644b1e7fc6dd115672ea
Simon Cropp <simon.cropp@gmail.com>
2017-02-07 16:50:25 +1100

    missing snippet

---
+++ Snippets/Core/Core_4/Recoverability/Immediate/DisableProvideConfiguration.cs
@@ -0,0 +1,21 @@
+﻿namespace Core4.Recoverability.Immediate
+{
+    using NServiceBus.Config;
+    using NServiceBus.Config.ConfigurationSource;
+
+    #region disableImmediateRetriesProvideConfiguration
+
+    class DisableProvideConfiguration :
+        IProvideConfiguration<TransportConfig>
+    {
+        public TransportConfig GetConfiguration()
+        {
+            return new TransportConfig
+            {
+                MaxRetries = 0
+            };
+        }
+    }
+
+    #endregion
+}
