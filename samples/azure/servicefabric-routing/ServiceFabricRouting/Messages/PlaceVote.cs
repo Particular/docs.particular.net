896fae705d7af158dfd6a3b8ec73350b780ac6fb
danielmarbach <daniel.marbach@openplace.net>
2017-02-13 11:37:20 +0100

    Solution draft

---
+++ samples/azure/servicefabric-routing/ServiceFabricRouting/Messages/PlaceVote.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NServiceBus;
+
+namespace Messages
+{
+    public class PlaceVote : ICommand
+    {
+        public string ZipCode { get; set; }
+
+        public string Candidate { get; set; }
+    }
+}
---
+++ samples/azure/servicefabric-routing/ServiceFabricRouting/ZipCodeVoteCount/EndpointCommunicationListener.cs
@@ -0,0 +1,57 @@
+﻿using System.Fabric;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using NServiceBus;
+
+namespace ZipCodeVoteCount
+{
+    public class EndpointCommunicationListener : ICommunicationListener
+    {
+        private StatefulServiceContext context;
+        private IEndpointInstance endpointInstance;
+
+        public EndpointCommunicationListener(StatefulServiceContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<string> OpenAsync(CancellationToken cancellationToken)
+        {
+            Int64RangePartitionInformation rangePartitionInformation;
+            using (var client = new FabricClient())
+            {
+                var servicePartitionList = await client.QueryManager.GetPartitionListAsync(context.ServiceName, context.PartitionId).ConfigureAwait(false);
+                rangePartitionInformation = servicePartitionList.Select(x => x.PartitionInformation).Cast<Int64RangePartitionInformation>().Single(p => p.Id == context.PartitionId);
+            }
+            
+            var endpointConfiguration = new EndpointConfiguration(context.ServiceTypeName);
+            endpointConfiguration.MakeInstanceUniquelyAddressable(rangePartitionInformation.HighKey.ToString());
+
+            endpointConfiguration.SendFailedMessagesTo("error");
+            endpointConfiguration.AuditProcessedMessagesTo("audit");
+            endpointConfiguration.UseSerialization<JsonSerializer>();
+            endpointConfiguration.EnableInstallers();
+            endpointConfiguration.UsePersistence<InMemoryPersistence>();
+            // endpointConfiguration.RegisterComponents(components => components.RegisterSingleton(context));
+            var transportConfig = endpointConfiguration.UseTransport<AzureServiceBusTransport>();
+            transportConfig.ConnectionString("");
+            transportConfig.UseForwardingTopology();
+
+            endpointInstance = await Endpoint.Start(endpointConfiguration).ConfigureAwait(false);
+            return null;
+        }
+
+        public Task CloseAsync(CancellationToken cancellationToken)
+        {
+            return endpointInstance.Stop();
+        }
+
+        public void Abort()
+        {
+            // Fire & Forget Close
+            CloseAsync(CancellationToken.None);
+        }
+    }
+}
