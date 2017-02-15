d4e50d766c97184555c6c16f659db46b47b5a6d7
Szymon Kulec <scooletz@gmail.com>
2017-02-15 20:36:05 +0100

    LocalPartitionDistributionStrategy now selects an address from all receiver addresses

--- samples/azure/azure-service-fabric-routing/Core_6/Contracts/LocalPartitionAwareDistributionStrategy.cs
+++ samples/azure/azure-service-fabric-routing/Core_6/Contracts/LocalPartitionAwareDistributionStrategy.cs
@@ -8,11 +8,13 @@ namespace Contracts
     public class LocalPartitionAwareDistributionStrategy : DistributionStrategy
     {
         readonly string _partitionId;
+        LogicalAddress _logicalAddress;
 
         public LocalPartitionAwareDistributionStrategy(string partitionId, string endpoint, DistributionStrategyScope scope)
             : base(endpoint, scope)
         {
             _partitionId = partitionId;
+            _logicalAddress = LogicalAddress.CreateRemoteAddress(new EndpointInstance(endpoint, _partitionId));
         }
 
         public override string SelectReceiver(string[] receiverAddresses)
@@ -23,9 +25,7 @@ namespace Contracts
         public override string SelectDestination(DistributionContext context)
         {
             context.Headers[PartitionHeaders.PartitionKey] = _partitionId;
-
-            //var logicalAddress = LogicalAddress.CreateRemoteAddress(new EndpointInstance(Endpoint, _partitionId));
-            return context.ReceiverAddresses.First();//.Single(a => a == logicalAddress.ToString());
+            return context.ReceiverAddresses.Single(a => a == _logicalAddress.ToString());
         }
     }
 }
