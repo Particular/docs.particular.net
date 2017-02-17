9dcc85343c84c25f8521fdcd81a4d0d556027dc4
Bob Langley <bob.langley@particular.net>
2017-02-16 17:40:06 -0800

    Add option to trust replies without header and context mapping

--- samples/azure/azure-service-fabric-routing/Core_6/Contracts/DistributeMessagesBasedOnPayload.cs
+++ samples/azure/azure-service-fabric-routing/Core_6/Contracts/DistributeMessagesBasedOnPayload.cs
@@ -1,22 +1,24 @@
-using System;
-using System.Threading.Tasks;
-using NServiceBus;
-using NServiceBus.Pipeline;
-
 namespace Contracts
 {
+    using System;
+    using System.Threading.Tasks;
+    using NServiceBus;
+    using NServiceBus.Pipeline;
+
     class DistributeMessagesBasedOnPayload : IBehavior<IIncomingLogicalMessageContext, IIncomingLogicalMessageContext>
     {
         private readonly string localPartitionKey;
         private readonly Forwarder forwarder;
         private readonly Func<object, string> mapper;
+        private readonly bool trustReplies;
         private readonly Action<string> logger;
 
-        public DistributeMessagesBasedOnPayload(string localPartitionKey, Forwarder forwarder, Func<object, string> mapper, Action<string> logger)
+        public DistributeMessagesBasedOnPayload(string localPartitionKey, Forwarder forwarder, Func<object, string> mapper, bool trustReplies, Action<string> logger)
         {
             this.localPartitionKey = localPartitionKey;
             this.forwarder = forwarder;
             this.mapper = mapper;
+            this.trustReplies = trustReplies;
             this.logger = logger;
         }
 
@@ -30,6 +32,12 @@ namespace Contracts
 
                 if (string.IsNullOrWhiteSpace(messagePartitionKey))
                 {
+                    if (trustReplies && IsReply(context))
+                    {
+                        await next(context).ConfigureAwait(false);
+                        return;
+                    }
+
                     throw new Exception($"Could not map a partition key for message of type {context.Headers[Headers.EnclosedMessageTypes]}"); //Will be replaced by unrecoverable exception part of Core PR 4479
                 }
             }
@@ -47,6 +55,23 @@ namespace Contracts
             context.Headers[PartitionHeaders.PartitionKey] = messagePartitionKey;
 
             await forwarder.Forward(context, messagePartitionKey).ConfigureAwait(false);
+        }
+
+        static bool IsReply(IIncomingLogicalMessageContext context)
+        {
+            string intentStr;
+
+            if (context.MessageHeaders.TryGetValue(Headers.MessageIntent, out intentStr))
+            {
+                MessageIntentEnum intent;
+                if (Enum.TryParse(intentStr, out intent))
+                {
+                    return intent == MessageIntentEnum.Reply;
+                }
+                return false;
+            }
+
+            return false;
         }
     }
 }
--- samples/azure/azure-service-fabric-routing/Core_6/Contracts/EndpointConfigurationExtensions.cs
+++ samples/azure/azure-service-fabric-routing/Core_6/Contracts/EndpointConfigurationExtensions.cs
@@ -7,12 +7,22 @@ namespace Contracts
 {
     public static class EndpointConfigurationExtensions
     {
-        public static void EnableReceiverSideDistribution(this EndpointConfiguration configuration, HashSet<string> discriminators, Func<object,string> mapper, Action<string> logger = null)
+        public static ReceiverSideDistributionOptions EnableReceiverSideDistribution(this EndpointConfiguration configuration, HashSet<string> discriminators, Func<object,string> mapper, Action<string> logger = null)
         {
+            var options = new ReceiverSideDistributionOptions();
+            configuration.GetSettings().Set("ReceiverSideDistribution.Options", options);
+
             configuration.GetSettings().Set("ReceiverSideDistribution.Discriminators", discriminators);
             configuration.GetSettings().Set("ReceiverSideDistribution.Mapper", mapper);
             configuration.GetSettings().Set("ReceiverSideDistribution.Logger", logger);
             configuration.EnableFeature<ReceiverSideDistribution>();
+
+            return options;
+        }
+
+        public static void TrustIncomingRepliesWithoutHeaderOrContext(this ReceiverSideDistributionOptions configuration)
+        {
+            configuration.TrustReplies = true;
         }
     }
 }
