eb93254008ec3c641aebfc8091176aa2e750052d
Simon Cropp <simon.cropp@gmail.com>
2017-02-17 13:19:54 +1100

    Update CustomEventListener.cs

--- samples/logging/eventsourcelogging/EventSourceLogging_1/Sample/CustomEventListener.cs
+++ samples/logging/eventsourcelogging/EventSourceLogging_1/Sample/CustomEventListener.cs
@@ -5,13 +5,11 @@ using System.Linq;
 #region EventListener
 class CustomEventListener :
     EventListener
-{
-    
+{    
     protected override void OnEventWritten(EventWrittenEventArgs data)
     {
         var message = string.Format(data.Message, data.Payload?.ToArray() ?? new object[0]);
         Console.WriteLine($"{data.EventId} {data.Channel} {data.Level} {message}");
     }
-
 }
 #endregion
