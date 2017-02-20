37189fac5dbb94de2b6b520333e20a40218829d8
Yves Goeleven <yves@goeleven.com>
2017-02-20 13:41:45 +0100

    removed old code & left over file references that don't exist anymore after merge

--- samples/azure/azure-service-fabric-routing/Core_6/CandidateVoteCount/EndpointCommunicationListener.cs
+++ samples/azure/azure-service-fabric-routing/Core_6/CandidateVoteCount/EndpointCommunicationListener.cs
@@ -107,51 +107,6 @@ namespace CandidateVoteCount
             var distributionConfig = transportConfig.Routing().RegisterPartitionedDestinationEndpoint("ZipCodeVoteCount", new[] { "33000", "66000", "99000" });
             distributionConfig.AddPartitionMappingForMessageType<TrackZipCode>(message => convertStringZipCodeToHighKey(message.GetType(), message.ZipCode));
 
-
-            //transportConfig.Routing().RouteToEndpoint(typeof(TrackZipCode), "ZipCodeVoteCount"); //TODO: Is this really necessary if we are replacing it later?
-
-            //Func<object, string> mapper = message =>
-            //{
-            //    var trackZipCode = message as TrackZipCode;
-            //    if (trackZipCode != null)
-            //    {
-            //        var zipCodeAsNumber = Convert.ToInt32(trackZipCode.ZipCode);
-            //        // 00000..33000 => 33000 34000..66000 => 66000 67000..99000 => 99000
-            //        if (zipCodeAsNumber >= 0 && zipCodeAsNumber <= 33000)
-            //        {
-            //            return "33000";
-            //        }
-
-            //        if (zipCodeAsNumber > 33000 && zipCodeAsNumber <= 66000)
-            //        {
-            //            return "66000";
-            //        }
-
-            //        if (zipCodeAsNumber > 66000 && zipCodeAsNumber <= 99000)
-            //        {
-            //            return "99000";
-            //        }
-
-            //        throw new Exception($"Invalid zip code '{zipCodeAsNumber}' for message of type '{message.GetType()}'.");
-            //    }
-
-            //    throw new Exception($"No partition mapping is found for message type '{message.GetType()}'.");
-            //};
-
-            //policy.SetDistributionStrategy(new PartitionAwareDistributionStrategy("ZipCodeVoteCount", mapper, DistributionStrategyScope.Send, localPartitionKey));
-
-            //using (var client = new FabricClient())
-            //{
-            //    var servicePartitionList = await client.QueryManager.GetPartitionListAsync(new Uri("fabric:/ServiceFabricRouting/ZipCodeVoteCount")).ConfigureAwait(false);
-            //    var partitions = new List<EndpointInstance>();
-            //    foreach (var partition in servicePartitionList)
-            //    {
-            //        partitions.Add(new EndpointInstance("ZipCodeVoteCount", partition.PartitionInformation.Kind == ServicePartitionKind.Int64Range ? ((Int64RangePartitionInformation)partition.PartitionInformation).HighKey.ToString() : ((NamedPartitionInformation)partition.PartitionInformation).Name));
-            //    }
-
-            //    instances.AddOrReplaceInstances("ZipCodeVoteCount", partitions);
-            //}
-
             #endregion
 
             endpointInstance = await Endpoint.Start(endpointConfiguration).ConfigureAwait(false);
