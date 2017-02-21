44a1ee71109864cf2bcdafc3d8cca3659f75f64c
indualagarsamy <indu.alagarsamy@particular.net>
2017-02-20 20:18:05 -0800

    Added a v5 version of the audit filtering pipeline sample

---
+++ samples/pipeline/audit-filtering/Core_5/AuditFilter/DoNotAuditThisMessageHandler.cs
@@ -0,0 +1,13 @@
+﻿using NServiceBus;
+using NServiceBus.Logging;
+
+public class DoNotAuditThisMessageHandler :
+    IHandleMessages<DoNotAuditThisMessage>
+{
+    static ILog log = LogManager.GetLogger<DoNotAuditThisMessageHandler>();
+
+    public void Handle(DoNotAuditThisMessage message)
+    {
+        log.Info($"Handling {message.GetType().Name}");
+    }
+}
---
+++ samples/pipeline/audit-filtering/Core_5/AuditFilter/InitializeAuditRulesBehavior.cs
@@ -0,0 +1,11 @@
+﻿using NServiceBus;
+using NServiceBus.Pipeline;
+
+class InitializeNewAuditBehavior : INeedInitialization
+{
+    public void Customize(BusConfiguration configuration)
+    {
+        // Replace the existing auditing step with our new one in the Pipeline
+        configuration.Pipeline.Replace(WellKnownStep.AuditProcessedMessage, typeof(AuditRulesBehavior), "Replaces existing audit behavior with the new behavior that filters certain messages");
+    }
+}
