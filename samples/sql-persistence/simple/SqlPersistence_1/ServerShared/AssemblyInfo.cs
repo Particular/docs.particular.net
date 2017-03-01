91547952f9b612a9564e6a1fb9c166aaff9ef623
Simon Cropp <simon.cropp@gmail.com>
2017-03-02 08:39:36 +1100

    fix attribute

--- samples/sql-persistence/simple/SqlPersistence_1/ServerShared/AssemblyInfo.cs
+++ samples/sql-persistence/simple/SqlPersistence_1/ServerShared/AssemblyInfo.cs
@@ -3,6 +3,8 @@
 #region SqlPersistenceSettings 1.1
 
 [assembly: SqlPersistenceSettings(
+    msSqlServerScripts: true,
+    mySqlScripts: true,
     scriptPromotionPath: "$(SolutionDir)PromotedSqlScripts")]
 
 #endregion
