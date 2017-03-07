53a52a855460f78727663ff24009f6eb6da12df4
Tim Bussmann <tim.bussmann@particular.net>
2017-03-07 08:19:12 +0100

    use 6.2 mutator registration API

--- Snippets/Core/Core_6/Mutators/MutatorRegistration.cs
+++ Snippets/Core/Core_6/Mutators/MutatorRegistration.cs
@@ -1,6 +1,8 @@
 ﻿namespace Core6.Mutators
 {
+    using System.Threading.Tasks;
     using NServiceBus;
+    using NServiceBus.MessageMutator;
 
     class MutatorRegistration
     {
@@ -8,17 +10,26 @@
         {
             #region MutatorRegistration
 
-            //TODO
-            endpointConfiguration.RegisterComponents(
-                registration: components =>
-                {
-                    components.ConfigureComponent<MyMutator>(DependencyLifecycle.InstancePerCall);
-                });
+            endpointConfiguration.RegisterMessageMutator(new MyIncomingMessageMutator());
+            endpointConfiguration.RegisterMessageMutator(new MyOutgoingTransportMessageMutator());
+
             #endregion
         }
 
-        public class MyMutator
+        public class MyIncomingMessageMutator : IMutateIncomingMessages
         {
+            public Task MutateIncoming(MutateIncomingMessageContext context)
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+
+        public class MyOutgoingTransportMessageMutator : IMutateOutgoingTransportMessages
+        {
+            public Task MutateOutgoing(MutateOutgoingTransportMessageContext context)
+            {
+                throw new System.NotImplementedException();
+            }
         }
     }
 }
