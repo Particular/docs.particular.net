fc12a0453d2fbb0372364774398d9df9b7b35515
Simon Cropp <simon.cropp@gmail.com>
2017-03-17 11:10:07 +1100

    redundant ref

--- Snippets/Core/Core_6/Scanning/ScanningPublicApi.cs
+++ Snippets/Core/Core_6/Scanning/ScanningPublicApi.cs
@@ -4,7 +4,6 @@
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
-    using System.Reflection;
     using System.Text.RegularExpressions;
     using NServiceBus;
 
@@ -28,6 +27,7 @@
 
             #endregion
         }
+
         void ScanningExcludeByWildcard(EndpointConfiguration endpointConfiguration)
         {
             #region ScanningAssebliesWildcard
