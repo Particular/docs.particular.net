da5998568529d859159b4e1716f87afb047752da
Yves Goeleven <yves@goeleven.com>
2017-03-20 10:57:37 +0100

    outbox snippet

---
+++ Snippets/ServiceFabricPersistence/ServiceFabricPersistence_1/OutboxConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NServiceBus;
+
+class OutboxConfiguration
+{
+    void OutboxConfigurationUsage(EndpointConfiguration endpointConfiguration)
+    {
+        #region ServiceFabricPersistenceOutboxConfiguration
+
+        var outboxSettings = endpointConfiguration.EnableOutbox();
+        outboxSettings.SetTimeToKeepDeduplicationData(TimeSpan.FromDays(1));
+        outboxSettings.SetFrequencyToRunDeduplicationDataCleanup(TimeSpan.FromMinutes(1));
+
+        #endregion
+    }
+}
