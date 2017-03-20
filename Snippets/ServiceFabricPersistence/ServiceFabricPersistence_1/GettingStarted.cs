7bfdb47e2aeac266836fac29b7da5534f09076ef
Sean Feldman <feldman.sean@gmail.com>
2017-03-20 17:24:50 -0600

    review + custom transaction in a handler

--- Snippets/ServiceFabricPersistence/ServiceFabricPersistence_1/GettingStarted.cs
+++ Snippets/ServiceFabricPersistence/ServiceFabricPersistence_1/GettingStarted.cs
@@ -31,4 +31,21 @@ class GettingStarted
         }
     }
     #endregion
+
+    class CustomTransactionHandler : IHandleMessages<Message>
+    {
+        #region CustomTransaction
+        public async Task Handle(Message message, IMessageHandlerContext context)
+        {
+            var session = context.SynchronizedStorageSession.ServiceFabricSession();
+            var stateManager = session.StateManager;
+            using (var transaction = stateManager.CreateTransaction())
+            {
+                var dictionary = await stateManager.GetOrAddAsync<IReliableDictionary<string, string>>(transaction, "specialCollection");
+                await dictionary.AddOrUpdateAsync(transaction, "key", _ => "value", (_, __) => "value");
+                await transaction.CommitAsync();
+            }
+        }
+        #endregion
+    }
 }
