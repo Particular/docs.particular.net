1acc05c73b089d3b73c453223bb3963a0e6a7bde
Tomasz Masternak <tomasz.masternak@gmail.com>
2017-03-20 10:33:26 +0100

    transaction sharing description

--- Snippets/ServiceFabricPersistence/ServiceFabricPersistence_1/GettingStarted.cs
+++ Snippets/ServiceFabricPersistence/ServiceFabricPersistence_1/GettingStarted.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Data.Collections;
 using NServiceBus;
 using NServiceBus.Persistence.ServiceFabric;
 
@@ -18,4 +19,22 @@ class GettingStarted
 
         #endregion
     }
+
+    class Message : IMessage { }
+
+    #region ServiceFabricPersistenceSynchronizedSession
+    class Handler : IHandleMessages<Message>
+    {
+        public async Task Handle(Message message, IMessageHandlerContext context)
+        {
+            var session = context.SynchronizedStorageSession.ServiceFabricSession();
+            var stateManager = session.StateManager;
+            var transaction = session.Transaction;
+
+            var dictionary = await stateManager.GetOrAddAsync<IReliableDictionary<string, string>>(transaction, "state");
+
+            await dictionary.AddOrUpdateAsync(transaction, "key", _ => "value", (_, __) => "value");
+        }
+    }
+    #endregion
 }
