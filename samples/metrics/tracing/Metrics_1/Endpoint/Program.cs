601a378b35a21042694b9e45456abfeb307ed23f
Mike Minutillo <michael.minutillo@gmail.com>
2017-03-24 11:49:29 +0800

    Add basic tracing sample

---
+++ samples/metrics/tracing/Metrics_1/Endpoint/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        AsyncMain().GetAwaiter().GetResult();
+    }
+
+    static async Task AsyncMain()
+    {
+        Console.Title = "Samples.Metrics.Tracing.Endpoint";
+        var endpointConfig = new EndpointConfiguration("Samples.Metrics.Tracing.Endpoint");
+        endpointConfig.SendFailedMessagesTo("error");
+        endpointConfig.UsePersistence<InMemoryPersistence>();
+        endpointConfig.EnableInstallers();
+
+        #region EnableMetricTracing
+
+        var metricsOptions = endpointConfig.EnableMetrics();
+        metricsOptions.EnableMetricTracing(TimeSpan.FromSeconds(5));
+
+        #endregion
+
+        var endpointInstance = await Endpoint.Start(endpointConfig).ConfigureAwait(false);
+
+        try
+        {
+            Console.WriteLine("Endpoint Started. Press any key to send a message");
+            Console.WriteLine("Press [ESC] to quit");
+
+            while (true)
+            {
+                var key = Console.ReadKey(true);
+                if (key.Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+
+                await endpointInstance.SendLocal(new SomeCommand()).ConfigureAwait(false);
+            }
+        }
+        finally
+        {
+            await endpointInstance.Stop().ConfigureAwait(false);
+        }
+    }
+}
---
+++ samples/metrics/tracing/Metrics_1/Endpoint/SomeCommandHandler.cs
@@ -0,0 +1,7 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+
+class SomeCommandHandler : IHandleMessages<SomeCommand>
+{
+    public Task Handle(SomeCommand message, IMessageHandlerContext context) => Task.CompletedTask;
+}
