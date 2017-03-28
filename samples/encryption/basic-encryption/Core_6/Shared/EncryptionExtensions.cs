1f9e7eb99818c256da86db44b8e02d0d111033b6
Simon Cropp <simon.cropp@gmail.com>
2017-03-28 18:41:56 +1100

    fix obsoletes

--- samples/encryption/basic-encryption/Core_6/Shared/EncryptionExtensions.cs
+++ samples/encryption/basic-encryption/Core_6/Shared/EncryptionExtensions.cs
@@ -8,7 +8,12 @@ public static class EncryptionExtensions
     public static void ConfigurationEncryption(this EndpointConfiguration endpointConfiguration)
     {
         var encryptionKey = Convert.FromBase64String("gdDbqRpqdRbTs3mhdZh9qCaDaxJXl+e6");
+#pragma warning disable 618
+        // Property Encryption has been moved to an external nuget package: NServiceBus.Encryption.MessageProperty 
+        // Old Encryption APIs marked obsolete in 6.2 of NServiceBus
+#pragma warning disable 618
         endpointConfiguration.RijndaelEncryptionService("2015-10", encryptionKey);
+#pragma warning restore 618
     }
 }
 
