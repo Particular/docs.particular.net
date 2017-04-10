372db38d9569ef1a972be9e9fe52edec487bda5e
Marcin Hoppe <marcin.hoppe@particular.net>
2017-04-10 12:59:55 +0200

    Add Oracle endpoint to simple sample for SQL persister

---
+++ samples/sql-persistence/simple/SqlPersistence_2/EndpointOracle/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Persistence.Sql;
+using Oracle.ManagedDataAccess.Client;
+
+internal class Program
+{
+    static void Main()
+    {
+        AsyncMain().GetAwaiter().GetResult();
+    }
+
+    static async Task AsyncMain()
+    {
+        Console.Title = "EndpointOracle";
+
+        #region OracleConfig
+
+        var endpointConfiguration = new EndpointConfiguration("EndpointOracle");
+
+        var persistence = endpointConfiguration.UsePersistence<SqlPersistence>();
+        var password = Environment.GetEnvironmentVariable("OraclePassword");
+        if (string.IsNullOrWhiteSpace(password))
+        {
+            throw new Exception("Could not extract 'OraclePassword' from Environment variables.");
+        }
+        var username = Environment.GetEnvironmentVariable("OracleUserName");
+        if (string.IsNullOrWhiteSpace(username))
+        {
+            throw new Exception("Could not extract 'OracleUserName' from Environment variables.");
+        }
+        var connection = $"Data Source=(DESCRIPTION=(ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT = 1521)))(CONNECT_DATA = (SERVER = DEDICATED)(SERVICE_NAME = XE)));User Id={username}; Password={password}; Enlist=dynamic";
+        persistence.SqlVariant(SqlVariant.Oracle);
+        persistence.ConnectionBuilder(
+            connectionBuilder: () =>
+            {
+                return new OracleConnection(connection);
+            });
+        var subscriptions = persistence.SubscriptionSettings();
+        subscriptions.CacheFor(TimeSpan.FromMinutes(1));
+
+        #endregion
+
+        endpointConfiguration.SendFailedMessagesTo("error");
+        endpointConfiguration.UseSerialization<JsonSerializer>();
+        endpointConfiguration.EnableInstallers();
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
