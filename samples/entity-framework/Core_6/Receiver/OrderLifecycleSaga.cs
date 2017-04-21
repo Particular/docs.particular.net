7dfe30fc37c07eecf1077ea48bc24b54abb23b71
Szymon Pobiega <szymon.pobiega@gmail.com>
2017-04-21 16:13:31 +0200

    Added entity framework UoW sample (#2457)
    * Added entity framework UoW sample
    
    * Cleaned up the code.
    
    * Simplified the code.
    
    * Fixed snippets
    
    * Fixed typo
    
    * Removed redundant snippets.
    
    * Added some EF tags.
    
    * Removed the tags.
    
    * review

---
+++ samples/entity-framework/Core_6/Receiver/OrderLifecycleSaga.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+
+public class OrderLifecycleSaga :
+    Saga<OrderLifecycleSagaData>,
+    IAmStartedByMessages<OrderSubmitted>,
+    IHandleTimeouts<OrderTimeout>
+{
+    protected override void ConfigureHowToFindSaga(SagaPropertyMapper<OrderLifecycleSagaData> mapper)
+    {
+        mapper.ConfigureMapping<OrderSubmitted>(m => m.OrderId)
+            .ToSaga(s => s.OrderId);
+    }
+
+    public async Task Handle(OrderSubmitted message, IMessageHandlerContext context)
+    {
+        var orderTimeout = new OrderTimeout();
+        await RequestTimeout(context, TimeSpan.FromSeconds(5), orderTimeout)
+            .ConfigureAwait(false);
+
+        var orderAccepted = new OrderAccepted
+        {
+            OrderId = message.OrderId,
+        };
+        await context.Reply(orderAccepted)
+            .ConfigureAwait(false);
+    }
+
+    public Task Timeout(OrderTimeout state, IMessageHandlerContext context)
+    {
+        return context.SendLocal(new CompleteOrder
+        {
+            OrderId = Data.OrderId
+        });
+    }
+}
