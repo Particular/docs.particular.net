a4e2fd969098a14e4637306440e8577471df9b7b
Simon Cropp <simon.cropp@gmail.com>
2017-05-01 13:43:09 +1000

    add placeholders to learning persistence

---
+++ Snippets/LearningPersistence/Core_6.3/Usage.cs
@@ -0,0 +1,25 @@
+﻿namespace LearningPersistence
+{
+    using NServiceBus;
+
+    class Usage
+    {
+        Usage(EndpointConfiguration endpointConfiguration)
+        {
+            #region LearningPersistence
+
+            endpointConfiguration.UsePersistence<LearningPersistence>();
+
+            #endregion
+        }
+        void SagaStorageDirectory(EndpointConfiguration endpointConfiguration)
+        {
+            #region LearningPersistenceSagaStorageDirectory
+
+            var persistence = endpointConfiguration.UsePersistence<LearningPersistence>();
+            persistence.SagaStorageDirectory("PathToStoreSagas");
+
+            #endregion
+        }
+    }
+}
---
+++ nservicebus/learning-persistence/index.md
@@ -0,0 +1,11 @@
+---
+title: Learning Persistence
+component: LearningPersistence
+related:
+reviewed: 2017-05-01
+---
+
+
+snippet: LearningPersistence
+
+snippet: LearningPersistenceSagaStorageDirectory
