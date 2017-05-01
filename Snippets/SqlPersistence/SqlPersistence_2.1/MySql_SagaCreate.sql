6535252bf8cbbf08729e3e5b8cb2291fe1eb40b4
Simon Cropp <simon.cropp@gmail.com>
2017-05-01 10:44:22 +1000

    add missing snippet

--- Snippets/SqlPersistence/SqlPersistence_2.1/MySql_SagaCreate.sql
+++ Snippets/SqlPersistence/SqlPersistence_2.1/MySql_SagaCreate.sql
@@ -169,7 +169,7 @@ deallocate prepare script;
 
 /* PurgeObsoleteProperties */
 
-select concat('alter table ', @tableName, ' drop column ', column_name, ';')
+select concat('alter table ', table_name, ' drop column ', column_name, ';')
 from information_schema.columns
 where
     table_schema = database() and
---
+++ Snippets/SqlPersistence/SqlPersistence_2.1/Oracle_SagaSelect.sql
@@ -0,0 +1,11 @@
+startcode Oracle_SagaSelectSql
+
+select
+    Id,
+    SagaTypeVersion,
+    Concurrency,
+    Metadata,
+    Data
+from "ENDPOINTNAME_SAGANAME"
+
+endcode
