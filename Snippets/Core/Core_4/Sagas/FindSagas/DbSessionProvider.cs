8d54bcc62f9cd1012d6aff0581438c7c7784c57f
Simon Cropp <simon.cropp@gmail.com>
2017-05-08 14:14:21 +1000

    dont use dynamic

--- Snippets/Core/Core_4/Sagas/FindSagas/DbSessionProvider.cs
+++ Snippets/Core/Core_4/Sagas/FindSagas/DbSessionProvider.cs
@@ -1,10 +1,18 @@
 ﻿namespace Core4.Sagas.FindSagas
 {
     using System;
-
+    
     public class DbSessionProvider
     {
-        public dynamic GetDbSession()
+        public DbSession GetDbSession()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class DbSession
+    {
+        public MySagaData GetSagaFromDB(string messageSomeId, string messageSomeData)
         {
             throw new NotImplementedException();
         }
