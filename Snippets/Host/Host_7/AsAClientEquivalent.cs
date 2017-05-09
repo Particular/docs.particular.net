842a5239ef4fe4c4aeaa685cfc78b96e687c4854
sfarmar <s.farmar@gmail.com>
2017-05-09 20:15:58 +0100

    Added AsAClientEquivalent snippet

---
+++ Snippets/Host/Host_7/AsAClientEquivalent.cs
@@ -0,0 +1,21 @@
+﻿using NServiceBus;
+using NServiceBus.Features;
+
+public class AsAClientEquivalent
+{
+    public AsAClientEquivalent()
+    {
+        #region AsAClientEquivalent
+
+        var endpointConfiguration = new EndpointConfiguration("MyEndpointName");
+        endpointConfiguration.PurgeOnStartup(true);
+
+        var transport = endpointConfiguration.UseTransport<MsmqTransport>();
+        transport.Transactions(TransportTransactionMode.None);
+
+        endpointConfiguration.Recoverability().Delayed(cfg => cfg.NumberOfRetries(0));
+        endpointConfiguration.DisableFeature<TimeoutManager>();
+
+        #endregion
+    }
+}
