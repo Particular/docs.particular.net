deddd8ebfefddec8da5a5b90ad3503a5b15b5c46
Simon Cropp <simon.cropp@gmail.com>
2017-05-17 14:22:22 +1000

    move store to learning

--- samples/show-case/on-premise/Core_6/Store.Shared/CommonConfiguration.cs
+++ samples/show-case/on-premise/Core_6/Store.Shared/CommonConfiguration.cs
@@ -6,16 +6,13 @@ public static class CommonConfiguration
 {
     public static void ApplyCommonConfiguration(this EndpointConfiguration endpointConfiguration)
     {
-        endpointConfiguration.UseTransport<MsmqTransport>();
-        endpointConfiguration.UsePersistence<InMemoryPersistence>();
+        endpointConfiguration.UseTransport<LearningTransport>();
+        endpointConfiguration.UsePersistence<LearningPersistence>();
         var defaultKey = "2015-10";
         var ascii = Encoding.ASCII;
         var encryptionService = new RijndaelEncryptionService(
             encryptionKeyIdentifier: defaultKey,
             key: ascii.GetBytes("gdDbqRpqdRbTs3mhdZh9qCaDaxJXl+e6"));
         endpointConfiguration.EnableMessagePropertyEncryption(encryptionService);
-        endpointConfiguration.AuditProcessedMessagesTo("audit");
-        endpointConfiguration.SendFailedMessagesTo("error");
-        endpointConfiguration.EnableInstallers();
     }
 }
