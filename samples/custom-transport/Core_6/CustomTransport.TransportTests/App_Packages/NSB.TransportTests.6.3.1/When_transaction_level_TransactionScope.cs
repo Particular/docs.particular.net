e47456e650f23162e288b7f113f94600f8f334ad
Tim Bussmann <tim.bussmann@particular.net>
2017-05-23 16:08:29 +0200

    include transport tests in custom transport sample

---
+++ samples/custom-transport/Core_6/CustomTransport.TransportTests/App_Packages/NSB.TransportTests.6.3.1/When_transaction_level_TransactionScope.cs
@@ -0,0 +1,39 @@
+﻿namespace NServiceBus.TransportTests
+{
+    using System;
+    using System.Threading.Tasks;
+    using System.Transactions;
+    using NUnit.Framework;
+    using Transport;
+
+    public class When_transaction_level_TransactionScope : NServiceBusTransportTest
+    {
+        //[TestCase(TransportTransactionMode.None)] -- not relevant
+        //[TestCase(TransportTransactionMode.ReceiveOnly)] -- not relevant
+        //[TestCase(TransportTransactionMode.SendsAtomicWithReceive)] -- not relevant
+        [TestCase(TransportTransactionMode.TransactionScope)]
+        public async Task Should_have_active_transaction(TransportTransactionMode transactionMode)
+        {
+            var messageHandled = new TaskCompletionSource<Tuple<Transaction, Transaction>>();
+
+            await StartPump(
+                context =>
+                {
+                    var currentTransaction = Transaction.Current;
+                    var contextTransaction = context.TransportTransaction.Get<Transaction>();
+                    messageHandled.SetResult(new Tuple<Transaction, Transaction>(currentTransaction, contextTransaction));
+
+                    return Task.FromResult(0);
+                },
+                errorContext => Task.FromResult(ErrorHandleResult.Handled),
+                transactionMode);
+            await SendMessage(InputQueueName);
+
+            var transactions = await messageHandled.Task;
+
+            Assert.That(transactions.Item1, Is.Not.Null);
+            Assert.That(transactions.Item2, Is.Not.Null);
+            Assert.AreSame(transactions.Item1, transactions.Item2);
+        }
+    }
+}
---
+++ samples/custom-transport/Core_6/CustomTransport.TransportTests/ConfigureFileTransportInfrastructure.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Settings;
+using NServiceBus.TransportTests;
+
+public class ConfigureFileTransportInfrastructure : IConfigureTransportInfrastructure
+{
+    public TransportConfigurationResult Configure(SettingsHolder settings, TransportTransactionMode transactionMode)
+    {
+        return new TransportConfigurationResult
+        {
+            PurgeInputQueueOnStartup = true,
+            TransportInfrastructure = new FileTransportInfrastructure()
+        };
+    }
+
+    public Task Cleanup()
+    {
+        return Task.CompletedTask;
+    }
+}
