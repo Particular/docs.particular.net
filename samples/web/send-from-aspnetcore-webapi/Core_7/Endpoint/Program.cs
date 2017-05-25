aadc87ef9f1590a64d4a3be5dcdf6758c8649d23
Simon Cropp <simon.cropp@gmail.com>
2017-05-25 14:03:19 +1000

    missing update

---
+++ samples/web/send-from-aspnetcore-webapi/Core_7/Endpoint/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+static class Program
+{
+    static void Main()
+    {
+        AsyncMain().GetAwaiter().GetResult();
+    }
+
+    static async Task AsyncMain()
+    {
+        Console.Title = "Samples.ASPNETCore.Endpoint";
+        var endpointConfiguration = new EndpointConfiguration("Samples.ASPNETCore.Endpoint");
+        endpointConfiguration.UsePersistence<LearningPersistence>();
+        endpointConfiguration.UseTransport<LearningTransport>();
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
