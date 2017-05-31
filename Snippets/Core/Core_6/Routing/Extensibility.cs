c3d531ae1f0d26200bf7294af3597e3969619120
SzymonPobiega <szymon.pobiega@gmail.com>
2017-05-31 12:17:37 +0200

    Moving DistributionStrategies doco to msmq transport.

--- Snippets/Core/Core_6/Routing/Extensibility.cs
+++ Snippets/Core/Core_6/Routing/Extensibility.cs
@@ -141,36 +141,6 @@
             #endregion
         }
 
-        void CustomDistributionStrategy(EndpointConfiguration endpointConfiguration)
-        {
-            #region RoutingExtensibility-Distribution
-
-            var transport = endpointConfiguration.UseTransport<MsmqTransport>();
-            var routing = transport.Routing();
-            routing.SetMessageDistributionStrategy(new RandomStrategy("Sales", DistributionStrategyScope.Send));
-
-            #endregion
-        }
-
-        #region RoutingExtensibility-DistributionStrategy
-
-        class RandomStrategy :
-            DistributionStrategy
-        {
-            static Random random = new Random();
-
-            public RandomStrategy(string endpoint, DistributionStrategyScope scope) : base(endpoint, scope)
-            {
-            }
-
-            public override string SelectReceiver(string[] receiverAddresses)
-            {
-                return receiverAddresses[random.Next(receiverAddresses.Length)];
-            }
-        }
-
-        #endregion
-
         class MyCommand :
             ICommand
         {
@@ -180,34 +150,5 @@
             IEvent
         {
         }
-    }
-
-    public class Extensibility_62
-    {
-        #region RoutingExtensibility-DistributionStrategy [6.2,)
-
-        class RandomStrategy :
-            DistributionStrategy
-        {
-            static Random random = new Random();
-
-            public RandomStrategy(string endpoint, DistributionStrategyScope scope) : base(endpoint, scope)
-            {
-            }
-
-            // Method will not be called since SelectDestination doesn't call base.SelectDestination
-            public override string SelectReceiver(string[] receiverAddresses)
-            {
-                throw new NotImplementedException();
-            }
-
-            public override string SelectDestination(DistributionContext context)
-            {
-                // access to headers, payload...
-                return context.ReceiverAddresses[random.Next(context.ReceiverAddresses.Length)];
-            }
-        }
-
-        #endregion
     }
 }
---
+++ Snippets/MsmqTransport/Core_6/DistributionStrategies.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using NServiceBus;
+using NServiceBus.Routing;
+
+public class DistributionStrategies
+{
+    void CustomDistributionStrategy(EndpointConfiguration endpointConfiguration)
+    {
+        #region RoutingExtensibility-Distribution
+
+        var transport = endpointConfiguration.UseTransport<MsmqTransport>();
+        var routing = transport.Routing();
+        routing.SetMessageDistributionStrategy(new RandomStrategy("Sales", DistributionStrategyScope.Send));
+
+        #endregion
+    }
+
+    #region RoutingExtensibility-DistributionStrategy
+
+    class RandomStrategy :
+        DistributionStrategy
+    {
+        static Random random = new Random();
+
+        public RandomStrategy(string endpoint, DistributionStrategyScope scope) : base(endpoint, scope)
+        {
+        }
+
+        public override string SelectReceiver(string[] receiverAddresses)
+        {
+            return receiverAddresses[random.Next(receiverAddresses.Length)];
+        }
+    }
+
+    #endregion
+
+    class Version_6_2
+    {
+        #region RoutingExtensibility-DistributionStrategy [6.2,)
+
+        class RandomStrategy :
+            DistributionStrategy
+        {
+            static Random random = new Random();
+
+            public RandomStrategy(string endpoint, DistributionStrategyScope scope) : base(endpoint, scope)
+            {
+            }
+
+            // Method will not be called since SelectDestination doesn't call base.SelectDestination
+            public override string SelectReceiver(string[] receiverAddresses)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override string SelectDestination(DistributionContext context)
+            {
+                // access to headers, payload...
+                return context.ReceiverAddresses[random.Next(context.ReceiverAddresses.Length)];
+            }
+        }
+        
+        #endregion
+    }
+}
