8cbf4be0fee7b4012c7a47b350da9e8f9611fce2
andreasohlund <andreas.ohlund@particular.net>
2017-05-31 14:15:14 +0200

    Add external msmq snippets

---
+++ Snippets/MsmqTransport/Core_7/RoutingAPIs.cs
@@ -0,0 +1,34 @@
+﻿// ReSharper disable UnusedParameter.Local
+
+namespace Core6.Routing
+{
+    using NServiceBus;
+
+    class RoutingAPIs
+    {
+        void MapMessagesToLogicalEndpoints(EndpointConfiguration endpointConfiguration)
+        {
+            #region Routing-MapMessagesToLogicalEndpoints
+
+            var transport = endpointConfiguration.UseTransport<MsmqTransport>();
+            var routing = transport.Routing();
+
+            routing.RouteToEndpoint(
+                messageType: typeof(AcceptOrder),
+                destination: "Sales");
+            routing.RouteToEndpoint(
+                messageType: typeof(SendOrder),
+                destination: "Shipping");
+
+            #endregion
+        }
+
+        class AcceptOrder
+        {
+        }
+
+        class SendOrder
+        {
+        }
+    }
+}
