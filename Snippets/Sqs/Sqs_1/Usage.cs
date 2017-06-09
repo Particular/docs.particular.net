83e158145d5ae08b533d26c105977d2ba0cc24ce
Adrian Hofman <adrian@adrianhofman.com>
2017-06-09 21:12:23 +1000

    First pass snippets for SqsTransport

---
+++ Snippets/Sqs/Sqs_1/Usage.cs
@@ -0,0 +1,14 @@
+﻿using NServiceBus;
+
+class Usage
+{
+    Usage(BusConfiguration busConfiguration)
+    {
+        #region SqsTransport
+
+        var transport = busConfiguration.UseTransport<SqsTransport>();
+        transport.ConnectionString("Region=ap-southeast-2;S3BucketForLargeMessages=myBucketName;S3KeyPrefix=my/key/prefix;");
+        
+        #endregion
+    }
+}
---
+++ Snippets/Sqs/Sqs_2/Usage.cs
@@ -0,0 +1,15 @@
+﻿using NServiceBus;
+
+class Usage
+{
+    Usage(EndpointConfiguration endpointConfiguration)
+    {
+        #region SqsTransport
+
+        endpointConfiguration.UseTransport<SqsTransport>()
+                .Region("ap-southeast-2")
+                .S3BucketForLargeMessages("myBucketName", "my/key/prefix");
+        
+        #endregion
+    }
+}
