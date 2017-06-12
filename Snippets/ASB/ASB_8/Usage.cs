18f607e489b6cb961670004946a458139b1204a2
Sean Feldman <feldman.sean@gmail.com>
2017-06-12 16:47:38 -0600

    compiling against v8 alpha on myget

--- Snippets/ASB/ASB_8/Usage.cs
+++ Snippets/ASB/ASB_8/Usage.cs
@@ -4,19 +4,14 @@ class Usage
 {
     void TopologySelectionUpgradeGuide(EndpointConfiguration endpointConfiguration)
     {
-        // TODO: once ASB v8 is there, the project needs to reference v8.
-        // This snippet would need to be commented out.
-
-#pragma warning disable 618
         #region 7to8-number-of-entities-bundle
 
         var transport = endpointConfiguration.UseTransport<AzureServiceBusTransport>();
         var forwardingTopology = transport.UseForwardingTopology();
 
-        forwardingTopology.BundlePrefix("my-prefix");
-        forwardingTopology.NumberOfEntitiesInBundle(3);
+        // forwardingTopology.BundlePrefix("my-prefix");
+        // forwardingTopology.NumberOfEntitiesInBundle(3);
         
         #endregion
-#pragma warning restore 618
     }
 }
