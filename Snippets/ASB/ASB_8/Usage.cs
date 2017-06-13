c5748cc343ad2fe55a296604c95c55a7a8a4c6f8
Sean Feldman <feldman.sean@gmail.com>
2017-06-12 21:47:16 -0600

    Proper snippet for the upgrade guide w/o commented out code + range clarified

--- Snippets/ASB/ASB_8/Usage.cs
+++ Snippets/ASB/ASB_8/Usage.cs
@@ -4,14 +4,5 @@ class Usage
 {
     void TopologySelectionUpgradeGuide(EndpointConfiguration endpointConfiguration)
     {
-        #region 7to8-number-of-entities-bundle
-
-        var transport = endpointConfiguration.UseTransport<AzureServiceBusTransport>();
-        var forwardingTopology = transport.UseForwardingTopology();
-
-        // forwardingTopology.BundlePrefix("my-prefix");
-        // forwardingTopology.NumberOfEntitiesInBundle(3);
-        
-        #endregion
     }
 }
