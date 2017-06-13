dca7c4bd2418ca18fc5c18f66057029477500537
Sean Feldman <feldman.sean@gmail.com>
2017-06-13 08:06:57 -0600

    asb doco requires snippets from older version to exist in v8

--- Snippets/ASB/ASB_8/Usage.cs
+++ Snippets/ASB/ASB_8/Usage.cs
@@ -2,7 +2,23 @@
 
 class Usage
 {
-    void TopologySelectionUpgradeGuide(EndpointConfiguration endpointConfiguration)
+    Usage(EndpointConfiguration endpointConfiguration)
     {
+        #region AzureServiceBusTransportWithAzure
+
+        var transport = endpointConfiguration.UseTransport<AzureServiceBusTransport>();
+        transport.ConnectionString("Endpoint=sb://[NAMESPACE].servicebus.windows.net/;SharedAccessKeyName=[KEYNAME];SharedAccessKey=[KEY]");
+
+        #endregion
+    }
+
+    void SettingConnectionString(EndpointConfiguration endpointConfiguration)
+    {
+        #region setting_asb_connection_string
+
+        var transport = endpointConfiguration.UseTransport<AzureServiceBusTransport>();
+        transport.ConnectionString("Endpoint=sb://[NAMESPACE].servicebus.windows.net/;SharedAccessKeyName=[KEYNAME];SharedAccessKey=[KEY]");
+
+        #endregion
     }
 }
