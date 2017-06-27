ccbb082bd41c4d9bd54daf1b7c55e043bd965494
Mauro Servienti <mauro@topics.it>
2017-06-27 19:59:48 +0200

    Support for non versioned subscription (#2867)
    * [WIP] Support for non versioned subscription
    
    * remove the usage of "your"
    
    * Create version-compatibility_non-versioned-subscriptions_raven_[4.2,].partial.md
    
    * Reference to "non-versioned-subscriptions" partial
    
    * Rename version-compatibility_non-versioned-subscriptions_raven_[4.2,].partial.md to subscription-versioning_non-versioned-subscriptions_raven_[4.2,].partial.md
    
    * fix file name and spelling
    
    * Add snippet for non versioned subscriptions
    
    * typo
    
    * Update subscription-versioning_non-versioned-subscriptions_raven_[4,].partial.md
    
    * Upgraded toNServiceBus.RavenDB 4.2.0

---
+++ Snippets/Raven/Raven_4/UnversionedSubscriptions.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Raven_4
+{
+    using NServiceBus;
+
+    class UnversionedSubscriptions
+    {
+        UnversionedSubscriptions(EndpointConfiguration endpointConfiguration)
+        {
+            #region DisableSubscriptionVersioning 4.2
+            var persistence = endpointConfiguration.UsePersistence<RavenDBPersistence>();
+            persistence.DisableSubscriptionVersioning();
+            #endregion
+        }
+    }
+}
