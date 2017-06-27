73fe4ded213689885878e1b6b033b73f45b92c59
Marcin Hoppe <marcin.hoppe@particular.net>
2017-06-27 12:37:32 +0200

    Fix sample

--- samples/outbox/sqltransport-sqlpersistence-ef/SqlServer_3/Receiver/ReceiverDataContext.cs
+++ samples/outbox/sqltransport-sqlpersistence-ef/SqlServer_3/Receiver/ReceiverDataContext.cs
@@ -31,14 +31,4 @@ public class ReceiverDataContext :
     #endregion
 
     public DbSet<Order> Orders { get; set; }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
-        
-        var orders = modelBuilder.Entity<Order>();
-        orders.ToTable("Orders");
-        orders.HasKey(x => x.OrderId);
-        orders.Property(x => x.Value);
-    }
 }
