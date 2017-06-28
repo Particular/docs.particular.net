e0115f176982bbd22b5bdcc74ec804f7d61723d0
Weronika Łabaj <weronika.labaj@particular.net>
2017-06-28 12:39:28 +0200

    no message

---
+++ Snippets/MsmqTransport/MsmqTransport_1/ServiceControlRemoteQueues.cs
@@ -0,0 +1,26 @@
+﻿
+using NServiceBus;
+
+class ServiceControlRemoteQueues
+{
+    ServiceControlRemoteQueues(EndpointConfiguration endpointConfiguration)
+    {
+        # region ConfigMsmqErrorWithCode
+
+        endpointConfiguration.SendFailedMessagesTo("targetErrorQueue@machinename");
+
+        # endregion
+    }
+}
+
+class CustomConfigurationSource
+{
+    CustomConfigurationSource(EndpointConfiguration endpointConfiguration)
+    {
+        #region UseCustomConfigurationSourceForErrorQueueRemoateMachineConfig
+
+        endpointConfiguration.CustomConfigurationSource(new ConfigurationSource());
+
+        #endregion
+    }
+}
