18687aa8d8d145d2dcf65f98e5fbca37927c38dc
danielmarbach <daniel.marbach@openplace.net>
2017-06-29 20:58:39 +0200

    Using minutes instead of hours to align with attributes

--- Snippets/Core/Core_5/DiscardingOldMessages.cs
+++ Snippets/Core/Core_5/DiscardingOldMessages.cs
@@ -26,7 +26,7 @@
                 {
                     if (type == typeof(MyMessage))
                     {
-                        return TimeSpan.FromHours(1);
+                        return TimeSpan.FromMinutes(1);
                     }
                     return TimeSpan.MaxValue;
                 });
