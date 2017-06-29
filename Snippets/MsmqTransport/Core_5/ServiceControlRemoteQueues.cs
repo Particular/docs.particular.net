2b9538b9ef23dd9d9d3da8ef50a0b39053b75804
WilliamBZA <WilliamBZA@users.noreply.github.com>
2017-06-29 21:52:18 +0200

    Add machine name to queue name examples (#2745)
    * Add msmq specific error config samples for SC
    
    * fix related
    
    * update
    
    * add menu entry
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * test
    
    * Revert "test"
    
    This reverts commit 544fdb757c913890f6fb58dcde6916c054a10b77.
    
    * test2
    
    * no message
    
    * fix dodgy regions
    
    * fix file paths

---
+++ Snippets/MsmqTransport/Core_5/ServiceControlRemoteQueues.cs
@@ -0,0 +1,55 @@
+﻿
+using NServiceBus;
+using NServiceBus.Config;
+using NServiceBus.Config.ConfigurationSource;
+
+
+
+#region ErrorQueueRemoteMachineConfigurationProvider
+
+class ProvideConfiguration : IProvideConfiguration<MessageForwardingInCaseOfFaultConfig>
+{
+    public MessageForwardingInCaseOfFaultConfig GetConfiguration()
+    {
+        return new MessageForwardingInCaseOfFaultConfig
+        {
+            ErrorQueue = "error@machinename"
+        };
+    }
+}
+
+#endregion
+
+#region ErrorQueueRemoteMachineConfigurationSource
+public class ConfigurationSource : IConfigurationSource
+{
+    public T GetConfiguration<T>() where T : class, new()
+    {
+        if (typeof(T) == typeof(MessageForwardingInCaseOfFaultConfig))
+        {
+            var config = new MessageForwardingInCaseOfFaultConfig
+            {
+                ErrorQueue = "error@machinename"
+            };
+
+            return config as T;
+        }
+
+        // Respect app.config for other sections not defined in this method
+        return System.Configuration.ConfigurationManager.GetSection(typeof(T).Name) as T;
+    }
+}
+
+#endregion
+
+class CustomConfigurationSource
+{
+    CustomConfigurationSource(BusConfiguration busConfiguration)
+    {
+        #region UseCustomConfigurationSourceForErrorQueueRemoateMachineConfig
+
+        busConfiguration.CustomConfigurationSource(new ConfigurationSource());
+
+        #endregion
+    }
+}
---
+++ Snippets/MsmqTransport/Core_6/ServiceControlRemoteQueues.cs
@@ -0,0 +1,66 @@
+﻿
+using NServiceBus;
+using NServiceBus.Config;
+using NServiceBus.Config.ConfigurationSource;
+
+class ServiceControlRemoteQueues
+{
+    ServiceControlRemoteQueues(EndpointConfiguration endpointConfiguration)
+    {
+        #region ConfigMsmqErrorWithCode
+
+        endpointConfiguration.SendFailedMessagesTo("targetErrorQueue@machinename");
+
+        #endregion
+    }
+}
+
+
+#region ErrorQueueRemoteMachineConfigurationProvider
+
+class ProvideConfiguration : IProvideConfiguration<MessageForwardingInCaseOfFaultConfig>
+{
+    public MessageForwardingInCaseOfFaultConfig GetConfiguration()
+    {
+        return new MessageForwardingInCaseOfFaultConfig
+        {
+            ErrorQueue = "error@machinename"
+        };
+    }
+}
+
+#endregion
+
+#region ErrorQueueRemoteMachineConfigurationSource
+public class ConfigurationSource : IConfigurationSource
+{
+    public T GetConfiguration<T>() where T : class, new()
+    {
+        if (typeof(T) == typeof(MessageForwardingInCaseOfFaultConfig))
+        {
+            var config = new MessageForwardingInCaseOfFaultConfig
+            {
+                ErrorQueue = "error@machinename"
+            };
+
+            return config as T;
+        }
+
+        // Respect app.config for other sections not defined in this method
+        return System.Configuration.ConfigurationManager.GetSection(typeof(T).Name) as T;
+    }
+}
+
+#endregion
+
+class CustomConfigurationSource
+{
+    CustomConfigurationSource(EndpointConfiguration endpointConfiguration)
+    {
+        #region UseCustomConfigurationSourceForErrorQueueRemoateMachineConfig
+
+        endpointConfiguration.CustomConfigurationSource(new ConfigurationSource());
+
+        #endregion
+    }
+}
