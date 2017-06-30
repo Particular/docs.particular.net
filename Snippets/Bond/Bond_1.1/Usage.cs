32da6797a114067d517a468683e96caa161471b2
Simon Cropp <simon.cropp@gmail.com>
2017-06-30 15:38:29 +1000

    update bond

---
+++ Snippets/Bond/Bond_1.1/Usage.cs
@@ -0,0 +1,54 @@
+﻿using Bond;
+using Bond.IO.Unsafe;
+using Bond.Protocols;
+using NServiceBus;
+using NServiceBus.Bond;
+
+class Usage
+{
+    Usage(EndpointConfiguration endpointConfiguration)
+    {
+        #region BondSerialization
+
+        endpointConfiguration.UseSerialization<BondSerializer>();
+
+        #endregion
+    }
+
+    void SerializationDelegates(EndpointConfiguration endpointConfiguration)
+    {
+        #region BondSerializationDelegates
+
+        var serialization = endpointConfiguration.UseSerialization<BondSerializer>();
+        serialization.SerializationDelegates(
+            sertializationDelegatesBuilder: messageType =>
+            {
+                return new SerializationDelegates(
+                    serialize: (buffer, message) =>
+                    {
+                        var writer = new CompactBinaryWriter<OutputBuffer>(buffer);
+                        var serializer = new Serializer<CompactBinaryWriter<OutputBuffer>>(messageType);
+                        serializer.Serialize(message, writer);
+                    },
+                    deserialize: buffer =>
+                    {
+                        var reader = new CompactBinaryReader<InputBuffer>(buffer);
+                        var deserializer = new Deserializer<CompactBinaryReader<InputBuffer>>(messageType);
+                        return deserializer.Deserialize(reader);
+                    });
+            });
+
+        #endregion
+    }
+
+    void ContentTypeKey(EndpointConfiguration endpointConfiguration)
+    {
+        #region BondContentTypeKey
+
+        var serialization = endpointConfiguration.UseSerialization<BondSerializer>();
+        serialization.ContentTypeKey("custom-key");
+
+        #endregion
+    }
+
+}
---
+++ samples/serializers/bond/Bond_1.1/Sample/CreateOrder.cs
@@ -0,0 +1,18 @@
+﻿using Bond;
+using NServiceBus;
+
+#region message
+[Schema]
+public class CreateOrder :
+    IMessage
+{
+    [Id(0)]
+    public int OrderId { get; set; }
+
+    [Id(1)]
+    public long Date { get; set; }
+
+    [Id(2)]
+    public int CustomerId { get; set; }
+}
+#endregion
