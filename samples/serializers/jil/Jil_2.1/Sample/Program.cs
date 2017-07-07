f8fcd400aae3d53791803c793822d2270c158009
Simon Cropp <simon.cropp@gmail.com>
2017-07-07 10:05:26 +1000

    add jil 2.1

---
+++ samples/serializers/jil/Jil_2.1/Sample/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Jil;
+using NServiceBus;
+using NServiceBus.Jil;
+
+static class Program
+{
+    static void Main()
+    {
+        AsyncMain().GetAwaiter().GetResult();
+    }
+
+    static async Task AsyncMain()
+    {
+        Console.Title = "Samples.Serialization.Jil";
+
+        #region config
+
+        var endpointConfiguration = new EndpointConfiguration("Samples.Serialization.Jil");
+        endpointConfiguration.UseSerialization<JilSerializer>()
+            .Options(
+                new Options(
+                    prettyPrint: true,
+                    excludeNulls: true,
+                    includeInherited: true));
+        // register the mutator so the the message on the wire is written
+        endpointConfiguration.RegisterComponents(components =>
+        {
+            components.ConfigureComponent<MessageBodyWriter>(DependencyLifecycle.InstancePerCall);
+        });
+
+        #endregion
+
+        endpointConfiguration.UsePersistence<LearningPersistence>();
+        endpointConfiguration.UseTransport<LearningTransport>();
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        #region message
+
+        var message = new CreateOrder
+        {
+            OrderId = 9,
+            Date = DateTime.Now,
+            CustomerId = 12,
+            OrderItems = new List<OrderItem>
+            {
+                new OrderItem
+                {
+                    ItemId = 6,
+                    Quantity = 2
+                },
+                new OrderItem
+                {
+                    ItemId = 5,
+                    Quantity = 4
+                },
+            }
+        };
+        await endpointInstance.SendLocal(message)
+            .ConfigureAwait(false);
+
+        #endregion
+
+        Console.WriteLine("Order Sent");
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
