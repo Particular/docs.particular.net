5163dd52429fb6b22f136825015f37b042075450
Weronika Łabaj <weralabaj@users.noreply.github.com>
2017-07-10 09:35:12 +0200

    spelling

--- Snippets/SqlServer/SqlServer_3.1/DelayedDelivery.cs
+++ Snippets/SqlServer/SqlServer_3.1/DelayedDelivery.cs
@@ -9,31 +9,31 @@ class DelayedDelivery
         #region EnableNativeDelayedDelivery
 
         var transport = endpointConfiguration.UseTransport<SqlServerTransport>();
-        var delayeeDeliverySettings = transport.UseNativeDelayedDelivery();
+        var delayedDeliverySettings = transport.UseNativeDelayedDelivery();
 
         #endregion
 
         #region DelayedDeliveryTableSuffix
 
-        delayeeDeliverySettings.TableSuffix("DelayedMessages");
+        delayedDeliverySettings.TableSuffix("DelayedMessages");
 
         #endregion
 
         #region DelayedDeliveryProcessingInterval
 
-        delayeeDeliverySettings.ProcessingInterval(TimeSpan.FromSeconds(15));
+        delayedDeliverySettings.ProcessingInterval(TimeSpan.FromSeconds(15));
 
         #endregion
 
         #region DelayedDeliveryBatchSize
 
-        delayeeDeliverySettings.BatchSize(50);
+        delayedDeliverySettings.BatchSize(50);
 
         #endregion
 
         #region DelayedDeliveryDisableTM
 
-        delayeeDeliverySettings.DisableTimeoutManagerCompatibility();
+        delayedDeliverySettings.DisableTimeoutManagerCompatibility();
 
         #endregion
     }
