94322ac6ba6421e160cb20e6e9ef5338249a395f
Weronika Łabaj <weronika.labaj@particular.net>
2017-07-11 11:04:57 +0200

    fix

---
+++ Snippets/SqlTransport/SqlTransport_3.1/NativeTimeouts.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Data.SqlClient;
+using NServiceBus;
+using NServiceBus.Transport.SQLServer;
+
+class NativeTimeouts
+{
+    void OtherEndpointConnectionParamsPull(EndpointConfiguration endpointConfiguration)
+    {
+        #region sqlserver-multidb-other-endpoint-connection
+
+        var transport = endpointConfiguration.UseTransport<SqlServerTransport>();
+        transport.EnableLegacyMultiInstanceMode(async address =>
+        {
+            var connectionString = address.Equals("RemoteEndpoint") ? "SomeConnectionString" : "SomeOtherConnectionString";
+            var connection = new SqlConnection(connectionString);
+            await connection.OpenAsync()
+                .ConfigureAwait(false);
+            return connection;
+        });
+
+        #endregion
+    }
+}
+
