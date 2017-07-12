4203ec150ed826645f31662b0b2793d67bc348b1
Simon Cropp <simon.cropp@gmail.com>
2017-07-12 21:37:21 +1000

    update zf

---
+++ Snippets/ZeroFormatter/ZeroFormatter_1.1/Usage.cs
@@ -0,0 +1,26 @@
+﻿using NServiceBus;
+using NServiceBus.ZeroFormatter;
+
+class Usage
+{
+    Usage(EndpointConfiguration endpointConfiguration)
+    {
+        #region ZeroFormatterSerialization
+
+        endpointConfiguration.UseSerialization<ZeroSerializer>();
+
+        #endregion
+    }
+
+
+    void ContentTypeKey(EndpointConfiguration endpointConfiguration)
+    {
+        #region ZeroFormatterContentTypeKey
+
+        var serialization = endpointConfiguration.UseSerialization<ZeroSerializer>();
+        serialization.ContentTypeKey("custom-key");
+
+        #endregion
+    }
+
+}
---
+++ samples/serializers/zeroformatter/ZeroFormatter_1.1/Sample/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.ZeroFormatter;
+
+static class Program
+{
+    static void Main()
+    {
+        AsyncMain().GetAwaiter().GetResult();
+    }
+
+    static async Task AsyncMain()
+    {
+        Console.Title = "Samples.Serialization.ZeroFormatter";
+
+        #region config
+
+        var endpointConfiguration = new EndpointConfiguration("Samples.Serialization.ZeroFormatter");
+        endpointConfiguration.UseSerialization<ZeroSerializer>();
+
+        #endregion
+
+        endpointConfiguration.UsePersistence<LearningPersistence>();
+        endpointConfiguration.UseTransport<LearningTransport>();
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        #region messagesend
+
+        var message = new CreateOrder
+        {
+            OrderId = 9,
+            Date = DateTime.Now.Ticks,
+            CustomerId = 12,
+        };
+        await endpointInstance.SendLocal(message)
+            .ConfigureAwait(false);
+
+        #endregion
+
+        Console.WriteLine("Order Sent");
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
