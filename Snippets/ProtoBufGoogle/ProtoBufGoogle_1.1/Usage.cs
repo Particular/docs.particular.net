9b6f8e3e851b329289dddfdbb4511d99c92cff71
Simon Cropp <simon.cropp@gmail.com>
2017-07-12 12:04:07 +1000

    update protobuf

---
+++ Snippets/ProtoBufGoogle/ProtoBufGoogle_1.1/Usage.cs
@@ -0,0 +1,25 @@
+﻿using NServiceBus;
+using NServiceBus.ProtoBufGoogle;
+
+class Usage
+{
+    Usage(EndpointConfiguration endpointConfiguration)
+    {
+        #region ProtoBufSerialization
+
+        endpointConfiguration.UseSerialization<ProtoBufGoogleSerializer>();
+
+        #endregion
+    }
+
+    void ContentTypeKey(EndpointConfiguration endpointConfiguration)
+    {
+        #region ProtoBufContentTypeKey
+
+        var serialization = endpointConfiguration.UseSerialization<ProtoBufGoogleSerializer>();
+        serialization.ContentTypeKey("custom-key");
+
+        #endregion
+    }
+
+}
---
+++ samples/serializers/protobufgoogle/ProtoBufGoogle_1.1/Sample/CreateOrder.partial.cs
@@ -0,0 +1,5 @@
+#region partial
+public partial class CreateOrder : NServiceBus.IMessage
+{
+}
+#endregion
