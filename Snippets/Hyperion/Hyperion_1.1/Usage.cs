4f65d05089ad21c28655fb1a487d8fa2324ee6da
Simon Cropp <simon.cropp@gmail.com>
2017-07-12 13:01:24 +1000

    update hyperion

---
+++ Snippets/Hyperion/Hyperion_1.1/Usage.cs
@@ -0,0 +1,38 @@
+﻿using NServiceBus;
+using NServiceBus.Hyperion;
+using Hyperion;
+
+class Usage
+{
+    Usage(EndpointConfiguration endpointConfiguration)
+    {
+        #region HyperionSerialization
+
+        endpointConfiguration.UseSerialization<HyperionSerializer>();
+
+        #endregion
+    }
+
+    void CustomSettings(EndpointConfiguration endpointConfiguration)
+    {
+        #region HyperionCustomSettings
+
+        var options = new SerializerOptions(
+            preserveObjectReferences: true);
+        var serialization = endpointConfiguration.UseSerialization<HyperionSerializer>();
+        serialization.Options(options);
+
+        #endregion
+    }
+
+    void ContentTypeKey(EndpointConfiguration endpointConfiguration)
+    {
+        #region HyperionContentTypeKey
+
+        var serialization = endpointConfiguration.UseSerialization<HyperionSerializer>();
+        serialization.ContentTypeKey("custom-key");
+
+        #endregion
+    }
+
+}
---
+++ samples/serializers/hyperion/Hyperion_1.1/Sample/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Hyperion;
+
+static class Program
+{
+    static void Main()
+    {
+        AsyncMain().GetAwaiter().GetResult();
+    }
+
+    static async Task AsyncMain()
+    {
+        Console.Title = "Samples.Serialization.Hyperion";
+
+        #region config
+
+        var endpointConfiguration = new EndpointConfiguration("Samples.Serialization.Hyperion");
+        endpointConfiguration.UseSerialization<HyperionSerializer>();
+
+        #endregion
+
+        endpointConfiguration.UsePersistence<LearningPersistence>();
+        endpointConfiguration.UseTransport<LearningTransport>();
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        #region message
+
+        var message = new CreateOrder
+        {
+            OrderId = 9,
+            Date = DateTime.Now,
+            CustomerId = 12,
+            OrderItems = new List<OrderItem>
+            {
+                new OrderItem
+                {
+                    ItemId = 6,
+                    Quantity = 2
+                },
+                new OrderItem
+                {
+                    ItemId = 5,
+                    Quantity = 4
+                },
+            }
+        };
+        await endpointInstance.SendLocal(message)
+            .ConfigureAwait(false);
+
+        #endregion
+
+        Console.WriteLine("Order Sent");
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
