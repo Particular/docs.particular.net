540a7e2725193b83aa2e48cf0ed8fd13eeb70ac7
Simon Cropp <simon.cropp@gmail.com>
2017-07-12 13:44:37 +1000

    update bond

---
+++ Snippets/Bond/Bond_1.3/Usage.cs
@@ -0,0 +1,53 @@
+﻿using Bond;
+using Bond.IO.Unsafe;
+using Bond.Protocols;
+using NServiceBus;
+using NServiceBus.Bond;
+
+class Usage
+{
+    Usage(EndpointConfiguration endpointConfiguration)
+    {
+        #region BondSerialization
+
+        endpointConfiguration.UseSerialization<BondSerializer>();
+
+        #endregion
+    }
+
+    void SerializationDelegates(EndpointConfiguration endpointConfiguration)
+    {
+        #region BondSerializationDelegates
+
+        var serialization = endpointConfiguration.UseSerialization<BondSerializer>();
+        serialization.SerializationDelegates(
+            sertializationDelegatesBuilder: messageType =>
+            {
+                var item = SerializerCache.GetSerializer(messageType);
+                return new SerializationDelegates(
+                    serialize: (buffer, message) =>
+                    {
+                        var writer = new CompactBinaryWriter<OutputBuffer>(buffer);
+                        item.Serializer.Serialize(message, writer);
+                    },
+                    deserialize: buffer =>
+                    {
+                        var reader = new CompactBinaryReader<InputBuffer>(buffer);
+                        return item.Deserializer.Deserialize(reader);
+                    });
+            });
+
+        #endregion
+    }
+
+    void ContentTypeKey(EndpointConfiguration endpointConfiguration)
+    {
+        #region BondContentTypeKey
+
+        var serialization = endpointConfiguration.UseSerialization<BondSerializer>();
+        serialization.ContentTypeKey("custom-key");
+
+        #endregion
+    }
+
+}
