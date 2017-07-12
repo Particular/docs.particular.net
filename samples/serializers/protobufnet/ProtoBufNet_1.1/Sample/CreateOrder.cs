4177af56808b620205cb2cac68f235bf6413af8f
Simon Cropp <simon.cropp@gmail.com>
2017-07-12 20:28:11 +1000

    update protobuf

---
+++ samples/serializers/protobufnet/ProtoBufNet_1.1/Sample/CreateOrder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using NServiceBus;
+using ProtoBuf;
+
+#region message
+[ProtoContract]
+public class CreateOrder :
+    IMessage
+{
+    [ProtoMember(1)]
+    public int OrderId;
+
+    [ProtoMember(2)]
+    public DateTime Date;
+
+    [ProtoMember(3)]
+    public int CustomerId;
+
+    [ProtoMember(4)]
+    public List<OrderItem> OrderItems;
+}
+#endregion
---
+++ samples/serializers/protobufnet/ProtoBufNet_1.1/Sample/OrderItem.cs
@@ -0,0 +1,12 @@
+﻿
+using ProtoBuf;
+
+[ProtoContract]
+public class OrderItem
+{
+    [ProtoMember(1)]
+    public int ItemId;
+
+    [ProtoMember(2)]
+    public int Quantity;
+}
