a92c6316653475d2a1694b1baf27ff9e796c042a
Szymon Kulec <scooletz@gmail.com>
2017-07-25 10:06:33 +0200

    NSB.Metrics bump up to 1.1 (#2986)
    * Initial commit for bumping up metrics to 1.1
    
    * Metrics-Enable added
    
    * lingo update
    
    * Showing how to observe probes.
    
    * Dropping unneeded refs
    
    * WARN for obsoleted methods
    
    * Snippet version added
    
    * Version removed from the sample.
    
    * Metrics 1.1 snippets brought again.
    
    * beta no more

---
+++ Snippets/Metrics/Metrics_1.1/Configuration.cs
@@ -0,0 +1,71 @@
+﻿namespace Metrics_1
+{
+    using System;
+    using System.Threading.Tasks;
+    using NServiceBus;
+    using NServiceBus.Logging;
+
+    public class Configuration
+    {
+        public void ConfigureEndpoint(EndpointConfiguration endpointConfiguration)
+        {
+            #region Metrics-Enable
+
+            var metricsOptions = endpointConfiguration.EnableMetrics();
+
+            #endregion
+
+            #region Metrics-Log
+
+            metricsOptions.EnableLogTracing(TimeSpan.FromMinutes(5));
+
+            #endregion
+
+            #region Metrics-Log-Info
+
+            metricsOptions.EnableLogTracing(TimeSpan.FromMinutes(5), LogLevel.Info);
+
+            #endregion
+
+            #region Metrics-Tracing
+
+            metricsOptions.EnableMetricTracing(TimeSpan.FromSeconds(5));
+
+            #endregion
+
+            #region Metrics-Custom-Function
+
+            metricsOptions.EnableCustomReport(
+                func: data =>
+                {
+                    // process metrics
+                    return Task.CompletedTask;
+                },
+                interval: TimeSpan.FromSeconds(5));
+
+            #endregion
+
+            #region Metrics-Observers
+
+            metricsOptions.RegisterObservers(ctx =>
+            {
+                foreach (var duration in ctx.Durations)
+                {
+                    duration.Register(durationLength =>
+                    {
+                        Console.WriteLine($"Duration '{duration.Name}' value observed: '{durationLength}'");
+                    });
+                }
+                foreach (var signal in ctx.Signals)
+                {
+                    signal.Register(() =>
+                    {
+                        Console.WriteLine($"'{signal.Name}' occurred.");
+                    });
+                }
+            });
+
+            #endregion
+        }
+    }
+}
