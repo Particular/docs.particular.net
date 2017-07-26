d25029e57b707101b728a44e2c5f1681807c91f5
Ramon Smits <ramon.smits@gmail.com>
2017-07-26 14:19:49 +0200

    Local import of folder structure containing the sample readme and solution.

---
+++ samples/application-insight/Metrics_1/Endpoint/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+class Program
+{
+    static void Main()
+    {
+        AsyncMain().GetAwaiter().GetResult();
+    }
+
+    static async Task AsyncMain()
+    {
+        Console.Title = "Samples.Metrics.Tracing.Endpoint";
+        var endpointConfiguration = new EndpointConfiguration("Samples.Metrics.Tracing.Endpoint");
+        endpointConfiguration.UsePersistence<LearningPersistence>();
+        endpointConfiguration.UseTransport<LearningTransport>();
+
+        #region EnableMetricTracing
+
+        var metricsOptions = endpointConfiguration.EnableMetrics();
+        metricsOptions.EnableMetricTracing(TimeSpan.FromSeconds(5));
+
+        #endregion
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+
+        try
+        {
+            Console.WriteLine("Endpoint started. Press 'enter' to send a message");
+            Console.WriteLine("Press any other key to quit");
+
+            while (true)
+            {
+                var key = Console.ReadKey(true);
+                if (key.Key != ConsoleKey.Enter)
+                {
+                    break;
+                }
+
+                await endpointInstance.SendLocal(new SomeCommand())
+                    .ConfigureAwait(false);
+            }
+        }
+        finally
+        {
+            await endpointInstance.Stop()
+                .ConfigureAwait(false);
+        }
+    }
+}
---
+++ samples/application-insight/index.md
@@ -0,0 +1,30 @@
+---
+title:Report metrics to Azure Application Insight
+component: Metrics
+---
+
+Azure Appliccation Insights provides alot of monitoring and alerting capabilities to use to monitoring the health of your NServiceBus endpoints. The sampe demonstrates how you are able to report metric data to Azure Application Insight so that you are able to.
+
+This sample will report the following metrics to Azure Application Insight:
+
+- Fetched messages per second
+- Failed messages per second
+- Succesful messages per second
+- Average message processing time
+- Averare critical time
+
+For a detailed explanation of these metrics please read [Metrics - Metrics captured](/nservicebus/operations/metrics#metrics-captured).
+
+
+## Setup Azure Application Insight
+
+
+## Use NServiceBus.Metrics to capture metric values
+
+
+## Send captured metric data to Application Insight
+
+
+
+
+
