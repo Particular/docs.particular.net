171ab97eb1fa37ab5942fcd6f83df0a017affdd6
Szymon Pobiega <szymon.pobiega@gmail.com>
2017-07-26 03:00:34 +0200

    [WIP] ASB transport adapter sample (#2965)
    * Simple case with single namespace for both endpoints.
    
    * for sample to work, use the default SC queue name
    
    * Switch body type of from byte[] to Stream
    
    * revert SC-side input queue to what it was
    
    * snippet for advanced features enabled on the endpoints
    
    * initial pass on the MD for ASB
    
    * Can't reuse "how it works" section as it was SQL specific. duplicating content.
    
    * tweaked documentation a bit more
    
    * remove SQL references
    
    * Enabled cross-namespace
    
    * Some minor corrections to sample.md
    
    * Updated to latest transport adapter.
    
    * cleanup
    
    * project renamed
    
    * feature name fix
    
    * rephrasing
    
    * expanding arguments names for clarity
    
    * retry and error are headers
    
    Calling a spade a spade.
    
    * move adapter specific headers into constants class and change their name to distinguish from core headers
    
    * visualize when failure simulation is on
    
    * replacing SVG image with mermaid graph
    
    * minor cleanups

---
+++ samples/servicecontrol/adapter-asb-multi-namespace/SCTransportAdapter_1/Shared/ConsoleHelper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public static class ConsoleHelper
+{
+    public static void ToggleTitle()
+    {
+        var parts = Console.Title.Split(' ');
+        Console.Title = parts.Length == 1 ? parts[0] + "  Failure simulation: ON" : parts[0];
+    }
+}
