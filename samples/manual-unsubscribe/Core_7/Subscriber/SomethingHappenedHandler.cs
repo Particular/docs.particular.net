5779ab0cfaaf43ed6d7e60cafaec6ab995aeeb71
Mauro Servienti <mauro.servienti@gmail.com>
2017-07-26 10:03:46 +0200

    manual unsubscribe sample (#2907)
    * MSMQ manual unsubscribe sample
    
    * remove the release configs from solution and projects
    
    * extra ;
    
    * remove redundant files
    
    * remove not needed namespaces
    
    * remove agrs from mains
    
    * cleanup
    
    * Add Sample.md draft and define regions for snippets
    
    * adding ASQ to the related list
    
    * add missing r# settings
    
    * cleanup
    
    * move manual unsubscribe
    
    * cleanup project
    
    * add diagrams
    
    * typo
    
    * add v7
    
    * devops
    
    * cleanup
    
    * split unsubscribe
    
    * add note about originator
    
    * typo
    
    * redundant text

---
+++ samples/manual-unsubscribe/Core_7/Subscriber/SomethingHappenedHandler.cs
@@ -0,0 +1,18 @@
+﻿using NServiceBus;
+using System.Threading.Tasks;
+using NServiceBus.Logging;
+
+#region event-handler
+
+class SomethingHappenedHandler :
+    IHandleMessages<SomethingHappened>
+{
+    static ILog log = LogManager.GetLogger<SomethingHappenedHandler>();
+    public Task Handle(SomethingHappened message, IMessageHandlerContext context)
+    {
+        log.Info("Subscriber has received SomethingHappened event.");
+        return Task.CompletedTask;
+    }
+}
+
+#endregion
