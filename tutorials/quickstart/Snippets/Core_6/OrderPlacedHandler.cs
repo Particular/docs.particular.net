1b98a05c8b1562ab11f914cdcff18be2f8e7d485
David Boike <david.boike@gmail.com>
2017-07-27 15:24:21 -0500

    Remove Before/Completed directories until hintpath issue can be figured out

--- tutorials/quickstart/Snippets/Core_6/OrderPlacedHandler.cs
+++ tutorials/quickstart/Snippets/Core_6/OrderPlacedHandler.cs
@@ -1,9 +1,10 @@
 ﻿using System.Threading.Tasks;
-using Messages;
 using NServiceBus;
 using NServiceBus.Logging;
 
-namespace Billing
+#region OrderPlacedHandler
+
+namespace Shipping
 {
     public class OrderPlacedHandler :
         IHandleMessages<OrderPlaced>
@@ -12,8 +13,16 @@ namespace Billing
 
         public Task Handle(OrderPlaced message, IMessageHandlerContext context)
         {
-            log.Info($"Billing has received OrderPlaced, OrderId = {message.OrderId}");
+            log.Info($"Shipping has received OrderPlaced, OrderId = {message.OrderId}");
             return Task.CompletedTask;
         }
     }
+}
+
+#endregion
+
+public class OrderPlaced 
+    : IEvent
+{
+    public string OrderId { get; set; }
 }
