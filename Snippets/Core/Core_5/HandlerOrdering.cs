1ae067a063b74c7da741fb80d50c4dd535acdaba
Simon Cropp <simon.cropp@gmail.com>
2017-07-30 20:38:13 +1000

    smoke test protobuf-net

--- Snippets/Core/Core_5/HandlerOrdering.cs
+++ Snippets/Core/Core_5/HandlerOrdering.cs
@@ -30,7 +30,10 @@
         {
             public void SpecifyOrder(Order order)
             {
-                order.Specify(typeof(HandlerB), typeof(HandlerA), typeof(HandlerC));
+                order.Specify(
+                    typeof(HandlerB),
+                    typeof(HandlerA),
+                    typeof(HandlerC));
             }
         }
         #endregion
