508ee37e7bbd6f90acf965d1444a93a16b533469
Simon Cropp <simon.cropp@gmail.com>
2017-08-02 12:15:31 +1000

    add v5 rabbit

---
+++ Snippets/Rabbit/Rabbit_5/UpgradeGuides/3to4/Upgrade.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using NServiceBus;
+
+partial class Upgrade
+{
+    void PrefetchMultiplier(EndpointConfiguration endpointConfiguration)
+    {
+        #region 3to4rabbitmq-config-prefetch-multiplier
+
+        endpointConfiguration.LimitMessageProcessingConcurrencyTo(10);
+        var transport = endpointConfiguration.UseTransport<RabbitMQTransport>();
+        // Prefetch count set to 10 * 4 = 40
+        transport.PrefetchMultiplier(4);
+
+        #endregion
+    }
+
+    void PrefetchCount(EndpointConfiguration endpointConfiguration)
+    {
+        #region 3to4rabbitmq-config-prefetch-count
+
+        var transport = endpointConfiguration.UseTransport<RabbitMQTransport>();
+        transport.PrefetchCount(100);
+
+        #endregion
+    }
+
+    void CallbackReceiverMaxConcurrency(EndpointConfiguration endpointConfiguration)
+    {
+        #region 3to4rabbitmq-config-callbackreceiver-thread-count
+
+        endpointConfiguration.LimitMessageProcessingConcurrencyTo(10);
+
+        #endregion
+    }
+
+    void UseCustomCircuitBreakerSettings(EndpointConfiguration endpointConfiguration)
+    {
+        #region 3to4rabbitmq-custom-breaker-settings-time-to-wait-before-triggering
+
+        var transport = endpointConfiguration.UseTransport<RabbitMQTransport>();
+        transport.TimeToWaitBeforeTriggeringCircuitBreaker(TimeSpan.FromMinutes(2));
+
+        #endregion
+    }
+
+    void UsePublisherConfirms(EndpointConfiguration endpointConfiguration)
+    {
+        #region 3to4rabbitmq-use-publisher-confirms
+
+        var transport = endpointConfiguration.UseTransport<RabbitMQTransport>();
+        transport.UsePublisherConfirms(true);
+
+        #endregion
+    }
+}
---
+++ samples/rabbitmq/simple/Rabbit_5/Sample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+class Program
+{
+    static void Main()
+    {
+        AsyncMain().GetAwaiter().GetResult();
+    }
+
+    static async Task AsyncMain()
+    {
+        Console.Title = "Samples.RabbitMQ.Simple";
+        #region ConfigureRabbit
+
+        var endpointConfiguration = new EndpointConfiguration("Samples.RabbitMQ.Simple");
+        var transport = endpointConfiguration.UseTransport<RabbitMQTransport>();
+        transport.ConnectionString("host=localhost");
+
+        #endregion
+        endpointConfiguration.SendFailedMessagesTo("error");
+        endpointConfiguration.EnableInstallers();
+        endpointConfiguration.UsePersistence<InMemoryPersistence>();
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        var myMessage = new MyMessage();
+        await endpointInstance.SendLocal(myMessage)
+            .ConfigureAwait(false);
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
