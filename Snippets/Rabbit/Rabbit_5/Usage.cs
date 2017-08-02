32c929aad4c3599d2890bdd7d0c3b951e5030c52
Simon Cropp <simon.cropp@gmail.com>
2017-08-02 12:44:12 +1000

    redundant upgrades

--- Snippets/Rabbit/Rabbit_5/Usage.cs
+++ Snippets/Rabbit/Rabbit_5/Usage.cs
@@ -85,21 +85,9 @@ class Usage
         throw new NotImplementedException();
     }
 
-#pragma warning disable CS0618
-    void UseRoutingTopology4_0(EndpointConfiguration endpointConfiguration)
-    {
-        #region rabbitmq-config-useroutingtopology [4.0,4.1)
-
-        var transport = endpointConfiguration.UseTransport<RabbitMQTransport>();
-        transport.UseRoutingTopology<MyRoutingTopology>();
-
-        #endregion
-    }
-#pragma warning restore CS0618
-
     void UseRoutingTopology4_1(EndpointConfiguration endpointConfiguration)
     {
-        #region rabbitmq-config-useroutingtopologyDelegate 4.1
+        #region rabbitmq-config-useroutingtopologyDelegate
 
         var transport = endpointConfiguration.UseTransport<RabbitMQTransport>();
         transport.UseRoutingTopology(
