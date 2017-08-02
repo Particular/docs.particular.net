1cb46c155f729b1b0e17f24cff2f5b08711220b6
Ramon Smits <ramon.smits@gmail.com>
2017-08-02 11:19:27 +0200

    Logging a warning in case a probe signal or duration is unknown.

--- samples/prometheus-grafana/Metrics_1/Endpoint/PrometheusFeature.cs
+++ samples/prometheus-grafana/Metrics_1/Endpoint/PrometheusFeature.cs
@@ -1,21 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using NServiceBus;
 using NServiceBus.Features;
+using NServiceBus.Logging;
 using Prometheus;
 
 class PrometheusFeature : Feature
 {
+    #region name-mapping
     Dictionary<string, string> nameMapping = new Dictionary<string, string>
     {
         // https://prometheus.io/docs/practices/naming/
-        {"# of msgs successfully processed / sec", "nservicebus_success_total"},
+        { "# of msgs successfully processed / sec", "nservicebus_success_total"},
         {"# of msgs pulled from the input queue /sec", "nservicebus_fetched_total"},
         {"# of msgs failures / sec", "nservicebus_failure_total"},
         {"Critical Time", "nservicebus_criticaltime_seconds"},
         {"Processing Time", "nservicebus_processingtime_seconds"},
     };
+    #endregion
 
     public PrometheusFeature()
     {
@@ -48,15 +50,29 @@ class PrometheusFeature : Feature
     {
         #region observers-registration
 
+        var log = LogManager.GetLogger(nameof(PrometheusFeature));
         foreach (var duration in context.Durations)
         {
-            var summary = Prometheus.Metrics.CreateSummary(nameMapping[duration.Name], duration.Description, "endpoint");
-            duration.Register(observer: durationLength => summary.Labels(endpoint).Observe(durationLength.TotalSeconds));
+            if (!nameMapping.ContainsKey(duration.Name))
+            {
+                log.WarnFormat("Unsupported duration probe {0}", duration.Name);
+                continue;
+            }
+            var prometheusName = nameMapping[duration.Name];
+            var summary = Prometheus.Metrics.CreateSummary(prometheusName, duration.Description, "endpoint");
+            duration.Register(observer: durationLength => summary.Labels(endpoint)
+                .Observe(durationLength.TotalSeconds));
         }
 
         foreach (var signal in context.Signals)
         {
-            var counter = Prometheus.Metrics.CreateCounter(nameMapping[signal.Name], signal.Description, "endpoint");
+            if (!nameMapping.ContainsKey(signal.Name))
+            {
+                log.WarnFormat("Unsupported signal probe {0}", signal.Name);
+                continue;
+            }
+            var prometheusName = nameMapping[signal.Name];
+            var counter = Prometheus.Metrics.CreateCounter(prometheusName, signal.Description, "endpoint");
             signal.Register(observer: () => counter.Labels(endpoint).Inc());
         }
 
