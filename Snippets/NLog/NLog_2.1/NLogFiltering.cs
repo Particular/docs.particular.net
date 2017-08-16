c472bd91aab231b6f34fef6c18cdb1cd47c3847e
Simon Cropp <simon.cropp@gmail.com>
2017-08-16 11:17:08 +1000

    add nlog for core v7

---
+++ Snippets/NLog/NLog_2.1/NLogFiltering.cs
@@ -0,0 +1,24 @@
+﻿using global::NLog;
+using global::NLog.Config;
+using global::NLog.Targets;
+using NServiceBus;
+
+public class NLogFiltering
+{
+    public NLogFiltering()
+    {
+        #region NLogFiltering
+
+        var config = new LoggingConfiguration();
+
+        var target = new ColoredConsoleTarget();
+        config.AddTarget("console", target);
+        config.LoggingRules.Add(new LoggingRule("MyNamespace.*", LogLevel.Debug, target));
+
+        LogManager.Configuration = config;
+
+        NServiceBus.Logging.LogManager.Use<NLogFactory>();
+
+        #endregion
+    }
+}
---
+++ Snippets/NLog/NLog_2.1/Usage.cs
@@ -0,0 +1,27 @@
+﻿using NLog;
+using NLog.Config;
+using NLog.Targets;
+using NServiceBus;
+
+class Usage
+{
+    Usage()
+    {
+        #region NLogInCode
+
+        var config = new LoggingConfiguration();
+
+        var consoleTarget = new ColoredConsoleTarget
+        {
+            Layout = "${level}|${logger}|${message}${onexception:${newline}${exception:format=tostring}}"
+        };
+        config.AddTarget("console", consoleTarget);
+        config.LoggingRules.Add(new LoggingRule("*", LogLevel.Debug, consoleTarget));
+
+        LogManager.Configuration = config;
+
+        NServiceBus.Logging.LogManager.Use<NLogFactory>();
+
+        #endregion
+    }
+}
