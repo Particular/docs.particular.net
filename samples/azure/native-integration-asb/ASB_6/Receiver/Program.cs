cd9c1cd4e9bd9d88804747a7fbd149a521656c43
Sean Feldman <feldman.sean@gmail.com>
2017-08-16 18:01:53 -0600

    native-integration-asb NSB 5 sample converted

--- samples/azure/native-integration-asb/ASB_6/Receiver/Program.cs
+++ samples/azure/native-integration-asb/ASB_6/Receiver/Program.cs
@@ -7,6 +7,8 @@ class Program
 {
     static void Main()
     {
+		AppContext.SetSwitch("Switch.System.IdentityModel.DisableMultipleDNSEntriesInSANCertificate", true);
+		
         Console.Title = "Samples.ASB.NativeIntegration";
         var busConfiguration = new BusConfiguration();
 
