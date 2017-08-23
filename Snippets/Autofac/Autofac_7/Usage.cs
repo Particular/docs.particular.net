ec0185b03b3a7477639c83d9c783dfafeb61e96f
Simon Cropp <simon.cropp@gmail.com>
2017-08-23 14:06:02 +1000

    add autofac v7

---
+++ Snippets/Autofac/Autofac_7/Usage.cs
@@ -0,0 +1,35 @@
+﻿using Autofac;
+using NServiceBus;
+
+class Usage
+{
+    Usage(EndpointConfiguration endpointConfiguration)
+    {
+        #region Autofac
+
+        endpointConfiguration.UseContainer<AutofacBuilder>();
+
+        #endregion
+    }
+
+    void Existing(EndpointConfiguration endpointConfiguration)
+    {
+        #region Autofac_Existing
+
+        var builder = new ContainerBuilder();
+        builder.RegisterInstance(new MyService());
+        var container = builder.Build();
+        endpointConfiguration.UseContainer<AutofacBuilder>(
+            customizations: customizations =>
+            {
+                customizations.ExistingLifetimeScope(container);
+            });
+
+        #endregion
+    }
+
+    class MyService
+    {
+    }
+
+}
