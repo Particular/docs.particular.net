429a9ccebde24b8d60f20e87a7e932f34536781c
Simon Cropp <simon.cropp@gmail.com>
2017-08-23 13:41:37 +1000

    add msmqpersistence nsb v7

---
+++ Snippets/MsmqPersistence/MsmqTransport_1/Usage.cs
@@ -0,0 +1,49 @@
+﻿namespace Core6.Persistence.Msmq
+{
+    using NServiceBus;
+    using NServiceBus.Features;
+    using NServiceBus.Persistence;
+
+    class Usage
+    {
+        void Configuration(EndpointConfiguration endpointConfiguration)
+        {
+            #region ConfiguringMsmqPersistence
+
+            endpointConfiguration.UsePersistence<MsmqPersistence>();
+
+            #endregion
+        }
+
+        void WithDisabledTimeoutManager(EndpointConfiguration endpointConfiguration)
+        {
+            #region DisablingTimeoutManagerForMsmqPersistence
+
+            endpointConfiguration.DisableFeature<TimeoutManager>();
+            endpointConfiguration.UsePersistence<MsmqPersistence>();
+
+            #endregion
+        }
+
+        void WithOtherPersisters(EndpointConfiguration endpointConfiguration)
+        {
+            #region MsmqPersistenceWithOtherPersisters
+
+            endpointConfiguration.UsePersistence<InMemoryPersistence, StorageType.Sagas>();
+            endpointConfiguration.UsePersistence<InMemoryPersistence, StorageType.Timeouts>();
+            endpointConfiguration.UsePersistence<MsmqPersistence, StorageType.Subscriptions>();
+
+            #endregion
+        }
+
+        void OverrideSubscriptionQueue(EndpointConfiguration endpointConfiguration)
+        {
+            #region MsmqSubscriptionCode
+
+            var persistence = endpointConfiguration.UsePersistence<MsmqPersistence>();
+            persistence.SubscriptionQueue("YourEndpointName.Subscriptions");
+
+            #endregion
+        }
+    }
+}
