0b26699f68307240e014feaacb7632efcc142107
Simon Cropp <simon.cropp@gmail.com>
2017-08-24 14:29:12 +1000

    gateway to NSB v7

---
+++ Snippets/Gateway/Gateway_3/Sites/ProvideConfiguration.cs
@@ -0,0 +1,35 @@
+﻿namespace Gateway_2.Sites
+{
+    using NServiceBus.Config;
+    using NServiceBus.Config.ConfigurationSource;
+
+    #region GatewaySitesConfigurationProvider
+
+    class ProvideConfiguration :
+        IProvideConfiguration<GatewayConfig>
+    {
+        public GatewayConfig GetConfiguration()
+        {
+            return new GatewayConfig
+            {
+                Sites = new SiteCollection
+                {
+                    new SiteConfig
+                    {
+                        Key = "SiteA",
+                        Address = "http://SiteA.mycorp.com/",
+                        ChannelType = "Http"
+                    },
+                    new SiteConfig
+                    {
+                        Key = "SiteB",
+                        Address = "http://SiteB.mycorp.com/",
+                        ChannelType = "Http"
+                    }
+                }
+            };
+        }
+    }
+
+    #endregion
+}
