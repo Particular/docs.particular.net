7593ba53971a5a149fa6c9e573d481cb0bb52400
Simon Cropp <simon.cropp@gmail.com>
2017-08-24 10:58:48 +1000

    add more container snippets

---
+++ Snippets/StructureMap/StructureMap_7/Usage.cs
@@ -0,0 +1,37 @@
+﻿using NServiceBus;
+using StructureMap;
+
+class Usage
+{
+    Usage(EndpointConfiguration endpointConfiguration)
+    {
+        #region StructureMap
+
+        endpointConfiguration.UseContainer<StructureMapBuilder>();
+
+        #endregion
+    }
+
+    void Existing(EndpointConfiguration endpointConfiguration)
+    {
+        #region StructureMap_Existing
+
+        var container = new Container(
+            action: expression =>
+            {
+                expression.For<MyService>()
+                    .Use(new MyService());
+            });
+        endpointConfiguration.UseContainer<StructureMapBuilder>(
+            customizations: customizations =>
+            {
+                customizations.ExistingContainer(container);
+            });
+
+        #endregion
+    }
+
+    class MyService
+    {
+    }
+}
---
+++ Snippets/Unity/Unity_8/Usage.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Practices.Unity;
+using NServiceBus;
+
+class Usage
+{
+    Usage(EndpointConfiguration endpointConfiguration)
+    {
+        #region Unity
+
+        endpointConfiguration.UseContainer<UnityBuilder>();
+
+        #endregion
+    }
+
+    void Existing(EndpointConfiguration endpointConfiguration)
+    {
+        #region Unity_Existing
+
+        var container = new UnityContainer();
+        container.RegisterInstance(new MyService());
+        endpointConfiguration.UseContainer<UnityBuilder>(
+            customizations: customizations =>
+            {
+                customizations.UseExistingContainer(container);
+            });
+
+        #endregion
+    }
+
+    class MyService
+    {
+    }
+}
