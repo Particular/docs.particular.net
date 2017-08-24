fe2a2f2a7bf19a6040471033ffc7e80885d0f455
Simon Cropp <simon.cropp@gmail.com>
2017-08-24 12:09:52 +1000

    add castle v7

---
+++ Snippets/Castle/Castle_7/Usage.cs
@@ -0,0 +1,36 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using NServiceBus;
+
+class Usage
+{
+    Usage(EndpointConfiguration endpointConfiguration)
+    {
+        #region CastleWindsor
+
+        endpointConfiguration.UseContainer<WindsorBuilder>();
+
+        #endregion
+    }
+
+    void Existing(EndpointConfiguration endpointConfiguration)
+    {
+        #region CastleWindsor_Existing
+
+        var container = new WindsorContainer();
+        var registration = Component.For<MyService>()
+            .Instance(new MyService());
+        container.Register(registration);
+        endpointConfiguration.UseContainer<WindsorBuilder>(
+            customizations: customizations =>
+            {
+                customizations.ExistingContainer(container);
+            });
+
+        #endregion
+    }
+
+    class MyService
+    {
+    }
+}
