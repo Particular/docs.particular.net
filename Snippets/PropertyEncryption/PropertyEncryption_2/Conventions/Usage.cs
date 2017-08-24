d2e2008fc03f21f38701f60b445b72f52c20aba9
Simon Cropp <simon.cropp@gmail.com>
2017-08-24 13:05:18 +1000

    add prop encryption v2

---
+++ Snippets/PropertyEncryption/PropertyEncryption_2/Conventions/Usage.cs
@@ -0,0 +1,29 @@
+﻿namespace Core6.Encryption.Conventions
+{
+    using System.Text;
+    using NServiceBus;
+    using NServiceBus.Encryption.MessageProperty;
+
+    class Usage
+    {
+        Usage(EndpointConfiguration endpointConfiguration)
+        {
+            #region DefiningEncryptedPropertiesAs
+
+            var ascii = Encoding.ASCII;
+            var encryptionService = new RijndaelEncryptionService(
+                encryptionKeyIdentifier: "2015-10",
+                key: ascii.GetBytes("gdDbqRpqdRbTs3mhdZh9qCaDaxJXl+e6"));
+
+            endpointConfiguration.EnableMessagePropertyEncryption(
+                encryptionService: encryptionService,
+                encryptedPropertyConvention: propertyInfo =>
+                {
+                    return propertyInfo.Name.EndsWith("EncryptedProperty");
+                }
+            );
+
+            #endregion
+        }
+    }
+}
---
+++ samples/encryption/encryption-conventions/PropertyEncryption_2/Shared/MessageWithSecretData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using NServiceBus;
+
+#region Message
+
+// Note all the properties to encrypt start with "Encrypted"
+public class MessageWithSecretData :
+    IMessage
+{
+    public string EncryptedSecret { get; set; }
+    public MySecretSubProperty SubProperty { get; set; }
+    public List<CreditCardDetails> CreditCards { get; set; }
+}
+
+public class MySecretSubProperty
+{
+    public string EncryptedSecret { get; set; }
+}
+
+public class CreditCardDetails
+{
+    public DateTime ValidTo { get; set; }
+    public string EncryptedNumber { get; set; }
+}
+
+#endregion
