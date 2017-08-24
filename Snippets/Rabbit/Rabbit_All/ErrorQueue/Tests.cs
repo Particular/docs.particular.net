f78339bc07736124a5e436474526a267151d0319
Adam Ralph <adam@adamralph.com>
2017-08-24 01:51:51 +0100

    Fix rabbitmq snippets tests (#3124)
    * fix Snippets/Rabbit/Rabbit_All/ErrorQueue/Tests.cs
    
    * fixed Snippets/Rabbit/Rabbit_All/NativeSend/Tests.cs

--- Snippets/Rabbit/Rabbit_All/ErrorQueue/Tests.cs
+++ Snippets/Rabbit/Rabbit_All/ErrorQueue/Tests.cs
@@ -1,5 +1,9 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Framing;
 
 namespace Rabbit_All.ErrorQueue
 {
@@ -10,6 +14,9 @@ namespace Rabbit_All.ErrorQueue
         [Test]
         public void ReturnMessageToSourceQueue()
         {
+            var sourceQueue = "return-message-to-source-queue";
+            var errorQueue = "return-message-to-source-queue-error";
+
             var connectionFactory = new ConnectionFactory
             {
                 HostName = "localhost",
@@ -18,12 +25,34 @@ namespace Rabbit_All.ErrorQueue
                 Password = "guest",
             };
 
+            var messageId = Guid.NewGuid().ToString();
+
             using (var connection = connectionFactory.CreateConnection())
             {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(sourceQueue, true, false, false, null);
+                    channel.QueueDeclare(errorQueue, true, false, false, null);
+
+                    channel.BasicPublish(
+                        string.Empty,
+                        errorQueue,
+                        false,
+                        new BasicProperties
+                        {
+                            MessageId = messageId,
+                            Headers = new Dictionary<string, object>
+                            {
+                                { "NServiceBus.FailedQ", Encoding.UTF8.GetBytes(sourceQueue) },
+                            },
+                        },
+                        new byte[0]);
+                }
+
                 ErrorQueue.ReturnMessageToSourceQueue(
                     connection: connection,
-                    errorQueueName: "error",
-                    messageId: "26afdee3-ac53-4990-95ef-a61300ca15c8");
+                    errorQueueName: errorQueue,
+                    messageId: messageId);
             }
         }
     }
