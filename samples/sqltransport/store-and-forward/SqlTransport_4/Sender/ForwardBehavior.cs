f91892c72eb0aa7b1fad503608a72e9dbd38bfe6
Simon Cropp <simon.cropp@gmail.com>
2017-08-24 13:25:32 +1000

    store and forward to nsb v7

---
+++ samples/sqltransport/store-and-forward/SqlTransport_4/Sender/ForwardBehavior.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NServiceBus.Extensibility;
+using NServiceBus.ObjectBuilder;
+using NServiceBus.Pipeline;
+using NServiceBus.Routing;
+using NServiceBus.Transport;
+
+public class ForwardBehavior :
+    ForkConnector<IIncomingPhysicalMessageContext, IDispatchContext>
+{
+    public override Task Invoke(IIncomingPhysicalMessageContext context, Func<Task> next, Func<IDispatchContext, Task> fork)
+    {
+        #region ForwardBehavior
+        var message = context.Message;
+        var headers = message.Headers;
+        var body = message.Body;
+        if (headers.TryGetValue("$.store-and-forward.destination", out var destination))
+        {
+            var operation = new TransportOperation(
+                message: new OutgoingMessage(context.MessageId, headers, body),
+                addressTag: new UnicastAddressTag(destination));
+            return fork(new DispatchContext(operation, context));
+
+        }
+        if (headers.TryGetValue("$.store-and-forward.eventtype", out var eventtype))
+        {
+            var messageType = Type.GetType(eventtype, true);
+            var operation = new TransportOperation(
+                message: new OutgoingMessage(context.MessageId, headers, body),
+                addressTag: new MulticastAddressTag(messageType));
+            return fork(new DispatchContext(operation, context));
+        }
+        return next();
+        #endregion
+    }
+
+    class DispatchContext :
+        ContextBag,
+        IDispatchContext
+    {
+        TransportOperation operation;
+        public ContextBag Extensions => this;
+        public IBuilder Builder => Get<IBuilder>();
+
+        public DispatchContext(TransportOperation operation, IBehaviorContext parentContext)
+            : base(parentContext?.Extensions)
+        {
+            this.operation = operation;
+        }
+
+        public IEnumerable<TransportOperation> Operations
+        {
+            get
+            {
+                yield return operation;
+            }
+        }
+    }
+}
