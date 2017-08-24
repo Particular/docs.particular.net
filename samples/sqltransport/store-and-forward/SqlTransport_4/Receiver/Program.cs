13cf2ad1dc280a9d4df37769e6d27fe5a746f0bc
Simon Cropp <simon.cropp@gmail.com>
2017-08-24 13:29:23 +1000

    Revert "Revert "store and forward to nsb v7""
    This reverts commit 479ecca18b14f0ff5e8a345c3419c9572f521976.

---
+++ samples/sqltransport/store-and-forward/SqlTransport_4/Receiver/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Transport.SQLServer;
+#pragma warning disable 618
+
+class Program
+{
+    static void Main()
+    {
+        AsyncMain().GetAwaiter().GetResult();
+    }
+
+    static async Task AsyncMain()
+    {
+        Console.Title = "Samples.SqlServer.StoreAndForwardReceiver";
+        var endpointConfiguration = new EndpointConfiguration("Samples.SqlServer.StoreAndForwardReceiver");
+        endpointConfiguration.SendFailedMessagesTo("error");
+        endpointConfiguration.EnableInstallers();
+
+        #region ReceiverConfiguration
+
+        var transport = endpointConfiguration.UseTransport<SqlServerTransport>();
+        var routing = transport.Routing();
+        routing.RegisterPublisher(
+            eventType: typeof(OrderSubmitted),
+            publisherEndpoint: "Samples.SqlServer.StoreAndForwardSender");
+
+        transport.EnableLegacyMultiInstanceMode(async address =>
+        {
+            string connectionString;
+            if (address.StartsWith("Samples.SqlServer.StoreAndForwardReceiver") ||
+                address == "error")
+            {
+                connectionString = Connections.ReceiverConnectionString;
+            }
+            else
+            {
+                connectionString = Connections.SenderConnectionString;
+            }
+
+            var connection = new SqlConnection(connectionString);
+            await connection.OpenAsync()
+                .ConfigureAwait(false);
+            return connection;
+        });
+
+
+        #endregion
+
+        endpointConfiguration.UsePersistence<InMemoryPersistence>();
+        var recoverability = endpointConfiguration.Recoverability();
+        recoverability.Delayed(
+            customizations: delayed =>
+            {
+                delayed.NumberOfRetries(0);
+            });
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        Console.WriteLine("Press any key to exit");
+        Console.WriteLine("Waiting for Order messages from the Sender");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+
+}
