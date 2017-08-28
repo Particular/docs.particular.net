d471a1fa318fad3f05b65caee29bc5f1313345d8
Simon Cropp <simon.cropp@gmail.com>
2017-08-28 21:20:01 +1000

    perf counters to NSB v7

---
+++ Snippets/PerfCounters/PerfCounters_2/PerformanceMonitoring.cs
@@ -0,0 +1,32 @@
+﻿namespace PerfCounters
+{
+    using System;
+    using NServiceBus;
+
+    class PerformanceMonitoring
+    {
+        void EnablingCriticalTime(EndpointConfiguration endpointConfiguration)
+        {
+            #region enable-criticaltime
+
+            var performanceCounters = endpointConfiguration.EnableWindowsPerformanceCounters();
+
+            #endregion
+
+            #region update-counters-every
+
+            performanceCounters.UpdateCounterEvery(TimeSpan.FromSeconds(2));
+
+            #endregion
+        }
+        void EnablingSla(EndpointConfiguration endpointConfiguration)
+        {
+            #region enable-sla
+
+            var performanceCounters = endpointConfiguration.EnableWindowsPerformanceCounters();
+            performanceCounters.EnableSLAPerformanceCounters(TimeSpan.FromMinutes(3));
+
+            #endregion
+        }
+    }
+}
---
+++ samples/performance-counters/PerfCounters_2/Sample/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+class Program
+{
+
+    static void Main()
+    {
+        AsyncMain().GetAwaiter().GetResult();
+    }
+
+    static async Task AsyncMain()
+    {
+        Console.Title = "Samples.PerfCounters";
+        var endpointConfiguration = new EndpointConfiguration("Samples.PerfCounters");
+        endpointConfiguration.EnableInstallers();
+        endpointConfiguration.UsePersistence<InMemoryPersistence>();
+        endpointConfiguration.SendFailedMessagesTo("error");
+
+        #region enable-counters
+        var performanceCounters = endpointConfiguration.EnableWindowsPerformanceCounters();
+        performanceCounters.EnableSLAPerformanceCounters(TimeSpan.FromSeconds(100));
+        performanceCounters.UpdateCounterEvery(TimeSpan.FromSeconds(2));
+        #endregion
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        Console.WriteLine("Press enter to send 10 messages with random sleep");
+        Console.WriteLine("Press any key to exit");
+
+        while (true)
+        {
+            var key = Console.ReadKey();
+            Console.WriteLine();
+
+            if (key.Key != ConsoleKey.Enter)
+            {
+                break;
+            }
+            for (var i = 0; i < 10; i++)
+            {
+                var myMessage = new MyMessage();
+                await endpointInstance.SendLocal(myMessage)
+                    .ConfigureAwait(false);
+            }
+        }
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
