adabb50f7dac15c27a13f4cf0ae6812ff6011d69
Simon Cropp <simon.cropp@gmail.com>
2017-08-30 06:49:52 +1000

    WIP: Blob storage data bus to nsb v7 (#3146)
    * add asb databus v2
    
    * add netcore sample
    
    * missing pre
    
    * Removed setting
    
    * Revert "Removed setting"
    
    This reverts commit aa55ec596ce50fb652dee4b1b284e2505646ee69.
    
    * remove redundant json serialization

---
+++ samples/azure/blob-storage-databus/ABSDataBus_2/Receiver/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+class Program
+{
+    static void Main()
+    {
+        AsyncMain().GetAwaiter().GetResult();
+    }
+
+    static async Task AsyncMain()
+    {
+        Console.Title = "Samples.AzureBlobStorageDataBus.Receiver";
+        var endpointConfiguration = new EndpointConfiguration("Samples.AzureBlobStorageDataBus.Receiver");
+        var dataBus = endpointConfiguration.UseDataBus<AzureDataBus>();
+        dataBus.ConnectionString("UseDevelopmentStorage=true");
+        endpointConfiguration.UseTransport<LearningTransport>();
+        endpointConfiguration.UsePersistence<LearningPersistence>();
+        endpointConfiguration.EnableInstallers();
+        endpointConfiguration.SendFailedMessagesTo("error");
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
