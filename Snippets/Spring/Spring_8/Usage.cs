e657bb768d1529c785254cc268cefc6922f36bfa
Simon Cropp <simon.cropp@gmail.com>
2017-08-29 11:24:32 +1000

    add spring for nsb v7

---
+++ Snippets/Spring/Spring_8/Usage.cs
@@ -0,0 +1,34 @@
+﻿using NServiceBus;
+using Spring.Context.Support;
+
+class Usage
+{
+    Usage(EndpointConfiguration endpointConfiguration)
+    {
+        #region Spring
+
+        endpointConfiguration.UseContainer<SpringBuilder>();
+
+        #endregion
+    }
+
+    void Existing(EndpointConfiguration endpointConfiguration)
+    {
+        #region Spring_Existing
+
+        var applicationContext = new GenericApplicationContext();
+        applicationContext.ObjectFactory
+            .RegisterSingleton("MyService", new MyService());
+        endpointConfiguration.UseContainer<SpringBuilder>(
+            customizations: customizations =>
+            {
+                customizations.ExistingApplicationContext(applicationContext);
+            });
+
+        #endregion
+    }
+
+    class MyService
+    {
+    }
+}
