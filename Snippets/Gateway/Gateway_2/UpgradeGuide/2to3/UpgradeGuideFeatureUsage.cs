f80c29e7e78e4c827cc5064ede8e98a76251fc2e
Weronika Łabaj <weronika.labaj@particular.net>
2017-09-04 09:02:05 +0200

    fix upgrade guide snippets

---
+++ Snippets/Gateway/Gateway_2/UpgradeGuide/2to3/UpgradeGuideFeatureUsage.cs
@@ -0,0 +1,17 @@
+﻿namespace Gateway_2.UpgradeGuide._2to3
+{
+    using NServiceBus;
+    using NServiceBus.Features;
+
+    class UpgradeGuideFeatureUsage
+    {
+        void EnableGatewayBefore(EndpointConfiguration endpointConfiguration)
+        {
+            #region 2to3EnableGatewayBefore
+
+            endpointConfiguration.EnableFeature<Gateway>();
+
+            #endregion
+        }
+    }
+}
---
+++ Snippets/Gateway/Gateway_3/UpgradeGuide/2to3/UpgradeGuideFeatureUsage.cs
@@ -0,0 +1,16 @@
+﻿namespace Gateway_3.UpgradeGuide._2to3
+{
+    using NServiceBus;
+
+    class UpgradeGuideFeatureUsage
+    {
+        void EnableGatewayAfter(EndpointConfiguration endpointConfiguration)
+        {
+            #region 2to3EnableGatewayAfter
+
+            endpointConfiguration.Gateway();
+
+            #endregion
+        }
+    }
+}
