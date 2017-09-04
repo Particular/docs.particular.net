4d440475c9b407e7f3ad5204e5b7adf98c5f34da
Simon Cropp <simon.cropp@gmail.com>
2017-09-05 06:35:17 +1000

    whitespace

--- Snippets/NHibernate/NHibernate_7/UpgradeGuides/MissingUniqueConstraintOnSagaDataTables/unique_constraints.sql
+++ Snippets/NHibernate/NHibernate_7/UpgradeGuides/MissingUniqueConstraintOnSagaDataTables/unique_constraints.sql
@@ -15,7 +15,6 @@ declare @sagaDataTableName nvarchar(max) = '...'
 declare @correlationPropertyColumnName nvarchar(max) = '...'
 declare @sql nvarchar(max)
 
-
 select @sql = 'alter table ' + @sagaDataTableName + ' add unique nonclustered ( ' + @correlationPropertyColumnName + ' asc )with (pad_index = off, statistics_norecompute = off, sort_in_tempdb = off, ignore_dup_key = off, online = off, allow_row_locks = on, allow_page_locks = on)'
 exec sp_executeSQL @sql
 endcode
