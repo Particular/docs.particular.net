05767d8da69befc308fc69777645d6f3da347199
Simon Cropp <simon.cropp@gmail.com>
2017-09-04 10:15:09 +1000

    transport bridge to NSB v7

---
+++ Snippets/Bridge/Bridge_2/API.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Bridge;
+using NServiceBus.Settings;
+using NServiceBus.Transport;
+
+public class API
+{
+    public void Connector()
+    {
+        #region connector
+
+        var endpointConfiguration = new EndpointConfiguration("MyEndpoint");
+        var transport = endpointConfiguration.UseTransport<MsmqTransport>();
+        var routing = transport.Routing();
+        var bridge = routing.ConnectToBridge("LeftBank");
+
+        bridge.RouteToEndpoint(typeof(MyMessage), "Receiver");
+        bridge.RegisterPublisher(typeof(MyEvent), "Publisher");
+
+        #endregion
+    }
+
+    public async Task BridgeConfig()
+    {
+        #region bridge
+
+        var bridgeConfiguration = Bridge
+            .Between<MsmqTransport>(endpointName: "LeftBank")
+            .And<RabbitMQTransport>(
+                endpointName: "RightBank",
+                customization: transportExtensions =>
+                {
+                    transportExtensions.ConnectionString("host=localhost");
+                });
+
+        #endregion
+
+        #region lifecycle
+
+        var bridge = bridgeConfiguration.Create();
+
+        await bridge.Start().ConfigureAwait(false);
+
+        await bridge.Stop().ConfigureAwait(false);
+
+        #endregion
+    }
+
+    public class RabbitMQTransport : TransportDefinition
+    {
+        public override TransportInfrastructure Initialize(SettingsHolder settings, string connectionString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ExampleConnectionStringForErrorMessage { get; }
+    }
+
+    public class MyEvent
+    {
+    }
+
+    public class MyMessage
+    {
+    }
+}
