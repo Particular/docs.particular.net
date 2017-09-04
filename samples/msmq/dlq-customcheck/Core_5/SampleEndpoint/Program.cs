c7682fdbfabb9a0217fa8aaefde4cdb0b539ebae
Mike Minutillo <michael.minutillo@gmail.com>
2017-09-04 09:43:30 +0800

    MSMQ DLQ Custom Check (#3164)
    * MSMQ DLQ Custom Check
    
    * cleanup
    
    * use code for error queue

---
+++ samples/msmq/dlq-customcheck/Core_5/SampleEndpoint/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NServiceBus;
+
+class Program
+{
+    static void Main()
+    {
+        var busConfiguration = new BusConfiguration();
+        busConfiguration.EndpointName("SampleEndpoint");
+        busConfiguration.UseTransport<MsmqTransport>();
+        busConfiguration.UsePersistence<InMemoryPersistence>();
+
+        using (Bus.Create(busConfiguration).Start())
+        {
+            Console.WriteLine("Bus started");
+            Console.ReadLine();
+        }
+    }
+}
