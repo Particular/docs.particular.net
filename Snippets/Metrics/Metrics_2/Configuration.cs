bf67497aacd4f342deb220067d0e55d9e4a7f0c6
Simon Cropp <simon.cropp@gmail.com>
2017-09-05 23:12:47 +1000

    WIP: metrics to nsb v7 (#3149)
    * metrics to nsb v7
    
    * update to v2 apis

---
+++ Snippets/Metrics/Metrics_2/Configuration.cs
@@ -0,0 +1,39 @@
+﻿namespace Metrics_1
+{
+    using System;
+    using NServiceBus;
+
+    public class Configuration
+    {
+        public void ConfigureEndpoint(EndpointConfiguration endpointConfiguration)
+        {
+            #region Metrics-Enable
+
+            var metricsOptions = endpointConfiguration.EnableMetrics();
+
+            #endregion
+
+            #region Metrics-Observers
+
+            metricsOptions.RegisterObservers(context =>
+            {
+                foreach (var duration in context.Durations)
+                {
+                    duration.Register((ref DurationEvent durationEvent) =>
+                    {
+                        Console.WriteLine($"Duration '{duration.Name}' value observed: '{durationEvent.Duration}'");
+                    });
+                }
+                foreach (var signal in context.Signals)
+                {
+                    signal.Register((ref SignalEvent signalEvent) =>
+                    {
+                        Console.WriteLine($"Signal '{signal.Name}' value observed: '{signalEvent.MessageType}'");
+                    });
+                }
+            });
+
+            #endregion
+        }
+    }
+}
