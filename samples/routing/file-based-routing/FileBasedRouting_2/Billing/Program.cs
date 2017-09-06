60ec65692334aa347ba9ec605f4baf04af2e84f7
Simon Cropp <simon.cropp@gmail.com>
2017-09-07 07:14:38 +1000

    update filebased routing (#3179)

---
+++ samples/routing/file-based-routing/FileBasedRouting_2/Billing/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.FileBasedRouting;
+
+class Program
+{
+
+    static void Main()
+    {
+        AsyncMain().GetAwaiter().GetResult();
+    }
+
+    static async Task AsyncMain()
+    {
+        Console.Title = "Samples.FileBasedRouting.Billing";
+        var endpointConfiguration = new EndpointConfiguration("Samples.FileBasedRouting.Billing");
+        endpointConfiguration.UsePersistence<InMemoryPersistence>();
+        endpointConfiguration.SendFailedMessagesTo("Samples.FileBasedRouting.Error");
+        var transport = endpointConfiguration.UseTransport<MsmqTransport>();
+        var routing = transport.Routing();
+        routing.UseFileBasedRouting(@"..\..\..\..\endpoints.xml");
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
