30660e0efa77cb967d9687b00550620ddb4df643
Donald Belcham <donald.belcham@particular.net>
2017-09-07 10:42:27 -0300

    Update simple saga sample

--- samples/saga/simple/Core_7/Sample/Program.cs
+++ samples/saga/simple/Core_7/Sample/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using System.Threading;
 using System.Threading.Tasks;
 using NServiceBus;
 
@@ -7,6 +9,10 @@ class Program
 
     static void Main()
     {
+        //required to prevent possible occurrence of .NET Core issue https://github.com/dotnet/coreclr/issues/12668
+        Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+        Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
         AsyncMain().GetAwaiter().GetResult();
     }
 
