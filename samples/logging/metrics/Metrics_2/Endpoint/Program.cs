221ebf37b7e54c8ec709abcc7dc359647ee8e50c
Donald Belcham <donald.belcham@particular.net>
2017-09-07 10:00:12 -0300

    Fix comment spelling

--- samples/logging/metrics/Metrics_2/Endpoint/Program.cs
+++ samples/logging/metrics/Metrics_2/Endpoint/Program.cs
@@ -9,7 +9,7 @@ class Program
 {
     static void Main()
     {
-        //required to prevent possible occurence of .NET Core issue https://github.com/dotnet/coreclr/issues/12668
+        //required to prevent possible occurrence of .NET Core issue https://github.com/dotnet/coreclr/issues/12668
         Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
         Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
 
