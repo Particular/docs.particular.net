ff83064a53f5298b096fd0557617b7b1a9edb9b2
Simon Cropp <simon.cropp@gmail.com>
2017-09-11 23:39:48 +1000

    add mslogging

---
+++ Snippets/MsLogging/MsLogging_1/Usage.cs
@@ -0,0 +1,21 @@
+﻿using NServiceBus;
+using Microsoft.Extensions.Logging;
+using NServiceBus.Logging;
+
+class Usage
+{
+    Usage()
+    {
+        #region MsLoggingInCode
+
+        using (var loggerFactory = new LoggerFactory())
+        {
+            loggerFactory.AddConsole();
+            var logFactory = LogManager.Use<MicrosoftLogFactory>();
+            logFactory.UseMsFactory(loggerFactory);
+
+            #endregion
+        }
+    }
+
+}
---
+++ nservicebus/logging/microsoft.md
@@ -0,0 +1,17 @@
+---
+title:  Microsoft Logging
+summary: Logging to Microsoft.Extensions.Logging
+reviewed: 2017-09-11
+component: MsLogging
+tags:
+ - Logging
+related:
+ - nservicebus/logging
+---
+
+Support for writing all NServiceBus log entries to  [Microsoft.Extensions.Logging](https://github.com/aspnet/Logging).
+
+
+## Usage
+
+snippet: MsLoggingInCode
