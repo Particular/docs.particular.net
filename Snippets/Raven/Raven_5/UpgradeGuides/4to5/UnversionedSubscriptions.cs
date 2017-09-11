2c693b4d2667a6f2ea24d0c96f29d020899e9455
David Boike <david.boike@gmail.com>
2017-09-11 11:37:57 -0500

    RavenDB 4to5 upgrade guide (#3208)
    * RavenDB 4to5 upgrade guide
    
    * Fix URL

---
+++ Snippets/Raven/Raven_5/UpgradeGuides/4to5/UnversionedSubscriptions.cs
@@ -0,0 +1,24 @@
+﻿using NServiceBus;
+
+class UnversionedSubscriptions
+{
+    void UseUnversionedSubscriptions(EndpointConfiguration endpointConfiguration)
+    {
+        #region 4to5-DisableSubscriptionVersioning
+        var persistence = endpointConfiguration.UsePersistence<RavenDBPersistence>();
+        persistence.DisableSubscriptionVersioning();
+        #endregion
+    }
+
+#pragma warning disable CS0618 // Type or member is obsolete
+
+    void LegacySubscriptionVersioning(EndpointConfiguration endpointConfiguration)
+    {
+        #region 4to5-LegacySubscriptionVersioning
+        var persistence = endpointConfiguration.UsePersistence<RavenDBPersistence>();
+        persistence.UseLegacyVersionedSubscriptions();
+        #endregion
+    }
+
+#pragma warning restore CS0618 // Type or member is obsolete
+}
