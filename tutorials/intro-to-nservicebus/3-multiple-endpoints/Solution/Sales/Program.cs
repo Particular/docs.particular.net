56fb400e68671ca7524b14460d81bc34b88e9e86
Simon Cropp <simon.cropp@gmail.com>
2017-09-17 21:02:58 +1000

    async main tutorials

--- tutorials/intro-to-nservicebus/3-multiple-endpoints/Solution/Sales/Program.cs
+++ tutorials/intro-to-nservicebus/3-multiple-endpoints/Solution/Sales/Program.cs
@@ -8,12 +8,7 @@ namespace Sales
 
     class Program
     {
-        static void Main()
-        {
-            AsyncMain().GetAwaiter().GetResult();
-        }
-
-        static async Task AsyncMain()
+        static async Task Main()
         {
             Console.Title = "Sales";
 
