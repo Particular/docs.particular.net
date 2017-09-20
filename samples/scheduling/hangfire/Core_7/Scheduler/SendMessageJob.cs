74a922dc4d8ed313e61172ad8b3bf3b6f17dfcfa
Jan Ove Skogheim <janovesk@janovesk.com>
2017-09-20 11:16:09 +0200

    Add v7 Hangfire sample

---
+++ samples/scheduling/hangfire/Core_7/Scheduler/SendMessageJob.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+
+namespace Scheduler
+{
+    public class SendMessageJob
+    {
+        public static Task Run()
+        {
+            return EndpointHelper.Instance.Send("Samples.HangfireScheduler.Receiver", new MyMessage());
+        }
+    }
+}
