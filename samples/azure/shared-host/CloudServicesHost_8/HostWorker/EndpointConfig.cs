43bd41aadfa976a345fa812dc662b6bf7db08a0d
danielmarbach <daniel.marbach@openplace.net>
2017-09-20 16:06:33 +0200

    CloudServiceHost 8 sample

---
+++ samples/azure/shared-host/CloudServicesHost_8/HostWorker/EndpointConfig.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.WindowsAzure.ServiceRuntime;
+using NServiceBus;
+
+#pragma warning disable 618
+
+#region AzureSharedHosting_HostConfiguration
+
+public class EndpointConfig :
+    IConfigureThisHost
+{
+    public void Customize(EndpointConfiguration endpointConfiguration)
+    {
+    }
+
+    public HostingSettings Configure()
+    {
+        var connectionString = RoleEnvironment.GetConfigurationSettingValue("HostWorker.ConnectionString");
+        var autoUpdate = bool.Parse(RoleEnvironment.GetConfigurationSettingValue("HostWorker.AutoUpdate"));
+        var updateInterval = int.Parse(RoleEnvironment.GetConfigurationSettingValue("HostWorker.UpdateInterval"));
+        var container = RoleEnvironment.GetConfigurationSettingValue("HostWorker.Container");
+
+        return new HostingSettings(connectionString)
+        {
+            AutoUpdate = autoUpdate,
+            UpdateInterval = updateInterval,
+            Container = container
+        };
+    }
+}
+
+#endregion
+
+#pragma warning restore 618
---
+++ samples/azure/shared-host/CloudServicesHost_8/Sender/PongHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+
+#region AzureMultiHost_PongHandler
+
+public class PongHandler :
+    IHandleMessages<Pong>
+{
+    public Task Handle(Pong message, IMessageHandlerContext context)
+    {
+        VerificationLogger.Write("Sender", "Got Pong from Receiver");
+        return Task.CompletedTask;
+    }
+}
+
+#endregion
