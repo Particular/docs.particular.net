cb29ac82c342094bc690a81559cde04831771fb9
Bob Langley <bobiii@thelangleys.org>
2017-09-22 10:22:58 -0700

    Create Upgrade7to8.cs

---
+++ Snippets/ASB/ASB_8/Upgrade7to8.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Reflection;
+using NServiceBus;
+
+class Upgrade7to8
+{
+    void Serializer(EndpointConfiguration endpointConfiguration)
+    {
+        #region 7to8_asb-backwardscompatible-serializer
+
+        endpointConfiguration.UseSerialization<NewtonsoftSerializer>();
+
+        #endregion
+    }
+}
