0b77e28433200faaf1417ddeccf656f3598e4a68
SzymonPobiega <szymon.pobiega@gmail.com>
2017-09-25 10:20:36 +0200

    Added/removed snippets

---
+++ Snippets/SqlTransport/SqlTransport_4/UpgradeGuides/31to4/LegacyMultiInstance.cs
@@ -0,0 +1,20 @@
+﻿using NServiceBus;
+using NServiceBus.Transport.SQLServer;
+
+class LegacyMultiInstance
+{
+#pragma warning disable 0618
+
+    void ConfigureMultiCatalog(EndpointConfiguration endpointConfiguration)
+    {
+        #region 31to4-multi-catalog
+
+        var transport = endpointConfiguration.UseTransport<SqlServerTransport>();
+        transport.ConnectionString("Data Source=SQL; Database=ThisEndpoint; Integrated Security=True");
+        transport.UseCatalogForEndpoint("RemoteEndpoint", "RemoteEndpoint");
+
+        #endregion
+    }
+
+#pragma warning restore 0618
+}
