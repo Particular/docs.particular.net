51bc1bb4fcf673435bdc0fa4e99dcc1554ebf41f
Szymon Kulec <scooletz@gmail.com>
2017-09-27 14:03:01 +0200

    Metrics 3.0 snippets

---
+++ Snippets/Metrics/Metrics_3/Configuration.cs
@@ -0,0 +1,42 @@
+﻿namespace Metrics_1
+{
+    using System;
+    using NServiceBus;
+
+    public class Configuration
+    {
+        public void ConfigureEndpoint(EndpointConfiguration endpointConfiguration)
+        {
+            #region Metrics-Enable
+
+            var metrics = endpointConfiguration.EnableMetrics();
+
+            #endregion
+
+            #region Metrics-Observers
+
+            metrics.RegisterObservers(
+                register: context =>
+                {
+                    foreach (var duration in context.Durations)
+                    {
+                        duration.Register(
+                            observer: (ref DurationEvent @event) =>
+                            {
+                                Console.WriteLine($"Duration: '{duration.Name}'. Value: '{@event.Duration}'");
+                            });
+                    }
+                    foreach (var signal in context.Signals)
+                    {
+                        signal.Register(
+                            observer: (ref SignalEvent @event) =>
+                            {
+                                Console.WriteLine($"Signal: '{signal.Name}'. Type: '{@event.MessageType}'");
+                            });
+                    }
+                });
+
+            #endregion
+        }
+    }
+}
