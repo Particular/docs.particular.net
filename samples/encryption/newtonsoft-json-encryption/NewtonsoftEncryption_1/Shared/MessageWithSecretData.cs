5152e016f6b6beb68db936bd01a5c72d85cc2af5
Simon Cropp <simon.cropp@gmail.com>
2017-09-27 23:07:04 +1000

    add newtonsoft encryption

---
+++ samples/encryption/newtonsoft-json-encryption/NewtonsoftEncryption_1/Shared/MessageWithSecretData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using NServiceBus;
+using Newtonsoft.Json.Encryption;
+
+#region Message
+
+public class MessageWithSecretData :
+    IMessage
+{
+    [Encrypt]
+    public string Secret { get; set; }
+    public MySecretSubProperty SubProperty { get; set; }
+    public List<CreditCardDetails> CreditCards { get; set; }
+}
+
+public class MySecretSubProperty
+{
+    [Encrypt]
+    public string Secret { get; set; }
+}
+
+public class CreditCardDetails
+{
+    public DateTime ValidTo { get; set; }
+    [Encrypt]
+    public string Number { get; set; }
+}
+
+#endregion
---
+++ samples/encryption/newtonsoft-json-encryption/sample.md
@@ -0,0 +1,10 @@
+---
+title: Newtonsoft Json Encryption
+summary: Encrypting specific parts of a message using the message property encryption.
+reviewed: 2017-09-27
+component: NewtonsoftEncryption
+tags:
+- Encryption
+---
+
+include: encryption-sample
