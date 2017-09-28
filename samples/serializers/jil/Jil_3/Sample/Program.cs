1c44352d9495f987bb00fe74971c380294e4ea9c
andreasohlund <andreas.ohlund@particular.net>
2017-09-28 10:42:21 +0200

    Fix mutator registrations in JIL samples

--- samples/serializers/jil/Jil_3/Sample/Program.cs
+++ samples/serializers/jil/Jil_3/Sample/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Jil;
 using NServiceBus;
 using NServiceBus.Jil;
+using NServiceBus.MessageMutator;
 
 static class Program
 {
@@ -27,14 +28,10 @@ static class Program
                     excludeNulls: true,
                     includeInherited: true));
         // register the mutator so the the message on the wire is written
-        endpointConfiguration.RegisterComponents(components =>
-        {
-            components.ConfigureComponent<MessageBodyWriter>(DependencyLifecycle.InstancePerCall);
-        });
+        endpointConfiguration.RegisterMessageMutator(new MessageBodyWriter());
 
         #endregion
 
-        endpointConfiguration.UsePersistence<LearningPersistence>();
         endpointConfiguration.UseTransport<LearningTransport>();
 
         var endpointInstance = await Endpoint.Start(endpointConfiguration)
