3155b3476147371830e30420e6aa6c8b729e775a
Mike Minutillo <michael.minutillo@gmail.com>
2017-10-03 14:41:41 +0800

    Add snippets and solution placeholder

---
+++ tutorials/monitoring/Snippets/Core_6/EndpointBuilder.cs
@@ -0,0 +1,30 @@
+﻿namespace Core_6
+{
+    using System;
+    using NServiceBus;
+
+    class EndpointBuilder
+    {
+        public EndpointConfiguration BuildEndpoint()
+        {
+            var endpointConfiguration = new EndpointConfiguration("MonitoredEndpoint");
+
+            #region enable-metrics
+
+            var metrics = endpointConfiguration.EnableMetrics();
+
+            #endregion
+
+            #region send-metric-data-to-servicecontrol
+
+            metrics.SendMetricDataToServiceControl(
+                serviceControlMetricsAddress: "Particular.Monitoring", 
+                interval: TimeSpan.FromSeconds(10)
+            );
+
+            #endregion
+
+            return endpointConfiguration;
+        }
+    }
+}
