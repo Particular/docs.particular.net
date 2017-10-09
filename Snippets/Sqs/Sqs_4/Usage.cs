392e6b51a60ebf1f0b7edf72c878c649d75bd70e
Simon Cropp <simon.cropp@gmail.com>
2017-10-09 16:52:05 +1100

    typo

--- Snippets/Sqs/Sqs_4/Usage.cs
+++ Snippets/Sqs/Sqs_4/Usage.cs
@@ -55,7 +55,7 @@ class Usage
 
     void Region(EndpointConfiguration endpointConfiguration)
     {
-        #region QueueNamePrefix
+        #region Region
 
         var transport = endpointConfiguration.UseTransport<SqsTransport>();
         transport.Region("ap-southeast-2");
