3208b0863682a53503c20b2b2e2801e73ec25979
andreasohlund <andreas.ohlund@particular.net>
2017-10-16 21:26:36 +0200

    Snippets

---
+++ Snippets/Core/Core_7/Hosting/StartUpDiagnostics.cs
@@ -0,0 +1,30 @@
+﻿namespace Core7
+{
+    using System.Threading.Tasks;
+    using NServiceBus;
+
+    public class StartUpDiagnostics
+    {
+        void SetDiagnosticsPath(EndpointConfiguration endpointConfiguration)
+        {
+            #region SetDiagnosticsPath
+
+            endpointConfiguration.SetDiagnosticsPath("myCustomPath");
+
+            #endregion
+        }
+
+        void CustomDiagnosticsWriter(EndpointConfiguration endpointConfiguration)
+        {
+            #region CustomDiagnosticsWriter
+
+            endpointConfiguration.CustomDiagnosticsWriter(diagnostics =>
+            {
+                //custom logic to write data
+                return Task.CompletedTask;
+            });
+
+            #endregion
+        }
+    }
+}
--- nservicebus/hosting/index_diagnostics_core_[7,).partial.md
+++ nservicebus/hosting/index_diagnostics_core_[7,).partial.md
@@ -2,15 +2,15 @@
 
 To make troubleshooting easier diagnostics is collected during enpoint startup and written to the host output directory.
 
-NOTE: This is usually X but on webapps `app_data` is used instead.
+NOTE: Output directory is by default `AppDomain.CurrentDomain.BaseDirectory` excpet for webapps where `App_Data` is used instead.
 
 To change the output path use:
 
-snippet: CustomDiagnosticsPath
+snippet: SetDiagnosticsPath
 
 To take full controll of how diagnostics are written use:
 
-snippet: CustomDiagnosticsWriter.
+snippet: CustomDiagnosticsWriter
 
 ### Adding startup diagnostics sections
 
