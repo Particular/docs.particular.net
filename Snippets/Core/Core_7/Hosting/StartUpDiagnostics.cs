208acf3f6f1f6ef97624eb02e817aadb1c57f54b
Weronika Łabaj <weronika.labaj@particular.net>
2017-10-17 13:14:18 +0200

    snippet cleanup

--- Snippets/Core/Core_7/Hosting/StartUpDiagnostics.cs
+++ Snippets/Core/Core_7/Hosting/StartUpDiagnostics.cs
@@ -1,44 +1,41 @@
-﻿namespace Core7
+﻿
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Settings;
+
+public class StartUpDiagnostics
 {
-    using System.Linq.Expressions;
-    using System.Threading.Tasks;
-    using NServiceBus;
-    using NServiceBus.Settings;
-
-    public class StartUpDiagnostics
+    void SetDiagnosticsPath(EndpointConfiguration endpointConfiguration)
     {
-        void SetDiagnosticsPath(EndpointConfiguration endpointConfiguration)
+        #region SetDiagnosticsPath
+
+        endpointConfiguration.SetDiagnosticsPath("myCustomPath");
+
+        #endregion
+    }
+
+    void CustomDiagnosticsWriter(EndpointConfiguration endpointConfiguration)
+    {
+        #region CustomDiagnosticsWriter
+
+        endpointConfiguration.CustomDiagnosticsWriter(diagnostics =>
         {
-            #region SetDiagnosticsPath
-
-            endpointConfiguration.SetDiagnosticsPath("myCustomPath");
-
-            #endregion
-        }
-
-        void CustomDiagnosticsWriter(EndpointConfiguration endpointConfiguration)
-        {
-            #region CustomDiagnosticsWriter
-
-            endpointConfiguration.CustomDiagnosticsWriter(diagnostics =>
-            {
                 //custom logic to write data
                 return Task.CompletedTask;
-            });
+        });
 
-            #endregion
-        }
-        void CustomDiagnosticsSection(ReadOnlySettings settings)
+        #endregion
+    }
+    void CustomDiagnosticsSection(ReadOnlySettings settings)
+    {
+        #region CustomDiagnosticsSection
+
+        settings.AddStartupDiagnosticsSection("MySection", new
         {
-            #region CustomDiagnosticsSection
+            SomeSetting = "some data",
+            SomeOtherSetting = 10
+        });
 
-            settings.AddStartupDiagnosticsSection("MySection", new
-            {
-                SomeSetting = "some data",
-                SomeOtherSetting = 10
-            });
-
-            #endregion
-        }
+        #endregion
     }
 }
