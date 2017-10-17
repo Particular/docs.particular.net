7fefbe9e459907b0e6f843df2e37607eb12d0dce
Sean Feldman <feldman.sean@gmail.com>
2017-10-17 15:10:38 -0600

    ASB V7 sample

---
+++ samples/azure/azure-service-bus-msmq-bridge/ASB_7/AsbEndpoint/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.Azure.ServiceBus.Subscriber";
+        var endpointConfiguration = new EndpointConfiguration("Samples.Azure.ServiceBus.Subscriber");
+        endpointConfiguration.SendFailedMessagesTo("error");
+        endpointConfiguration.EnableInstallers();
+        endpointConfiguration.UsePersistence<InMemoryPersistence>();
+        endpointConfiguration.UseSerialization<JsonSerializer>();
+        endpointConfiguration.AddDeserializer<XmlSerializer>();
+        var transport = endpointConfiguration.UseTransport<AzureServiceBusTransport>();
+        var connectionString = Environment.GetEnvironmentVariable("AzureServiceBus.ConnectionString");
+        if (string.IsNullOrWhiteSpace(connectionString))
+        {
+            throw new Exception("Could not read the 'AzureServiceBus.ConnectionString' environment variable. Check the sample prerequisites.");
+        }
+        transport.ConnectionString(connectionString);
+        transport.UseForwardingTopology();
+        var recoverability = endpointConfiguration.Recoverability();
+        recoverability.DisableLegacyRetriesSatellite();
+
+        #region connect-msmq-side-of-bridge
+
+        var routing = transport.Routing();
+        var bridge = routing.ConnectToBridge("Bridge-Subscriber");
+
+        #endregion
+
+        #region subscribe-to-event-via-bridge
+
+        bridge.RegisterPublisher(typeof(MyEvent), "Samples.Azure.ServiceBus.Publisher");
+
+        #endregion
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
---
+++ samples/azure/azure-service-bus-msmq-bridge/ASB_7/MsmqEndpoint/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.Azure.ServiceBus.Publisher";
+
+        var endpointConfiguration = new EndpointConfiguration("Samples.Azure.ServiceBus.Publisher");
+        endpointConfiguration.SendFailedMessagesTo("error");
+        endpointConfiguration.EnableInstallers();
+        endpointConfiguration.UsePersistence<InMemoryPersistence>();
+        endpointConfiguration.UseSerialization<XmlSerializer>();
+        var recoverability = endpointConfiguration.Recoverability();
+        recoverability.DisableLegacyRetriesSatellite();
+        var transport = endpointConfiguration.UseTransport<MsmqTransport>();
+
+        #region connect-msmq-side-of-bridge
+
+        var routing = transport.Routing();
+        var bridge = routing.ConnectToBridge("Bridge-Publisher");
+
+        #endregion
+
+        #region route-command-via-bridge
+
+        bridge.RouteToEndpoint(typeof(MyCommand), "Samples.Azure.ServiceBus.Subscriber");
+
+        #endregion
+        
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+
+        Console.WriteLine("Press 'c' to send a command");
+        Console.WriteLine("Press 'e' to publish an event");
+        Console.WriteLine("Press any other key to exit");
+        var @continue = true;
+
+        while (@continue)
+        {
+            var key = Console.ReadKey().Key;
+            switch (key)
+            {
+                case ConsoleKey.C:
+                    await endpointInstance.Send(new MyCommand { Property = "command from MSMQ endpoint"}).ConfigureAwait(false);
+                    Console.WriteLine("\nCommand sent");
+                    break;
+
+                case ConsoleKey.E:
+                    await endpointInstance.Publish<MyEvent>(@event => { @event.Property = "event from MSMQ endpoint"; }).ConfigureAwait(false);
+                    Console.WriteLine("\nEvent sent");
+                    break;
+
+                default:
+                    @continue = false;
+                    break;
+            }
+        }
+
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
