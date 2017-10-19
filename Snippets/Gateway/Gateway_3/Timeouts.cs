29729346287c79d23de3741e21102e14d5732df3
andreasohlund <andreas.ohlund@particular.net>
2017-10-19 13:50:26 +0200

    Add snippet for v3 timeout api

---
+++ Snippets/Gateway/Gateway_3/Timeouts.cs
@@ -0,0 +1,22 @@
+﻿namespace Gateway_2
+{
+    using System;
+    using NServiceBus;
+
+    public class Timeouts
+    {
+        //customGatewayTimeout
+        public Timeouts()
+        {
+            var endpointConfiguration = new EndpointConfiguration("MyEndpoint");
+
+            #region CustomGatewayTimeout
+
+            var gatewayConfig = endpointConfiguration.Gateway();
+
+            gatewayConfig.TransactionTimeout(TimeSpan.FromSeconds(40));
+
+            #endregion
+        }
+    }
+}
--- nservicebus/gateway/multi-site-deployments_timeouts_gateway_[3,].partial.md
+++ nservicebus/gateway/multi-site-deployments_timeouts_gateway_[3,].partial.md
@@ -2,4 +2,4 @@
 
 Transmitting messages to remote sites will by default use the default transaction timeout of the underlying transport. A custom timeout value can be configured using the following api:
 
-snippet: customGatewayTimeout
+snippet: CustomGatewayTimeout
