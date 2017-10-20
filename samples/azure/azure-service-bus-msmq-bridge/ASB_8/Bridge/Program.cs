03dcae9a35c85db7f956ec3fe59edc124e2dde82
Sean Feldman <feldman.sean@gmail.com>
2017-10-20 11:50:53 -0600

    core v7 / asb v8 sample

---
+++ samples/azure/azure-service-bus-msmq-bridge/ASB_8/Bridge/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Bridge;
+using NServiceBus.Configuration.AdvancedExtensibility;
+using NServiceBus.Serialization;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.Title = "Samples.Azure.ServiceBus.Bridge";
+
+        var connectionString = Environment.GetEnvironmentVariable("AzureServiceBus.ConnectionString");
+        if (string.IsNullOrWhiteSpace(connectionString))
+        {
+            throw new Exception("Could not read the 'AzureServiceBus.ConnectionString' environment variable. Check the sample prerequisites.");
+        }
+
+        // TODO: ASB requires serializer to be registered.
+        // Currently, there's no way to specify serialization for the bridged endpoints
+        // endpointConfiguration.UseSerialization<T>();
+
+        #region bridge-general-configuration
+
+        var bridgeConfiguration = Bridge
+            .Between<MsmqTransport>("Bridge-MSMQ")
+            .And<AzureServiceBusTransport>("Bridge-ASB", transport =>
+            {
+                transport.ConnectionString(connectionString);
+                transport.UseForwardingTopology();
+                var settings = transport.GetSettings();
+                var serializer = Tuple.Create(new NewtonsoftSerializer() as SerializationDefinition, settings);
+                settings.Set("MainSerializer", serializer);
+            });
+
+        bridgeConfiguration.AutoCreateQueues();
+        bridgeConfiguration.UseSubscriptionPersistece<InMemoryPersistence>((configuration, persistence) => { });
+
+        #endregion
+
+        #region suppress-transaction-scope-for-asb
+
+        // TODO: Bridge 2.x doesn't support this yet
+
+//        bridgeConfiguration.InterceptForwarding(async (queue, message, forward) =>
+//        {
+//            using (new TransactionScope(TransactionScopeOption.Suppress, TransactionScopeAsyncFlowOption.Enabled))
+//            {
+//                await forward().ConfigureAwait(false);
+//            }
+//        });
+
+        #endregion
+
+        #region bridge-execution
+
+        var bridge = bridgeConfiguration.Create();
+
+        await bridge.Start().ConfigureAwait(false);
+
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+
+        await bridge.Stop().ConfigureAwait(false);
+
+
+        #endregion
+    }
+}
---
+++ samples/azure/azure-service-bus-msmq-bridge/ASB_8/MsmqEndpoint/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.Azure.ServiceBus.MsmqEndpoint";
+
+        var endpointConfiguration = new EndpointConfiguration("Samples.Azure.ServiceBus.MsmqEndpoint");
+        endpointConfiguration.SendFailedMessagesTo("error");
+        endpointConfiguration.EnableInstallers();
+        endpointConfiguration.UsePersistence<InMemoryPersistence>();
+        endpointConfiguration.UseSerialization<XmlSerializer>();
+        endpointConfiguration.AddDeserializer<NewtonsoftSerializer>();
+        var transport = endpointConfiguration.UseTransport<MsmqTransport>();
+
+        #region connect-msmq-side-of-bridge
+
+        var routing = transport.Routing();
+        var bridge = routing.ConnectToBridge("Bridge-MSMQ");
+
+        #endregion
+
+        #region route-command-via-bridge
+
+        bridge.RouteToEndpoint(typeof(MyCommand), "Samples.Azure.ServiceBus.AsbEndpoint");
+
+        #endregion
+        
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
