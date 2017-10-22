ee2ad8e6764349985e59824b7869ac8959681667
Simon Cropp <simon.cropp@gmail.com>
2017-10-23 10:04:49 +1100

    add StartUpDiagnosticsWriter

---
+++ Snippets/Core/Core_7/Hosting/StartUpDiagnosticsWriter.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Common;
+using Core7.Headers.Writers;
+using NServiceBus;
+using NUnit.Framework;
+
+[TestFixture]
+public class StartUpDiagnosticsWriter
+{
+    [Test]
+    public async Task Write()
+    {
+        var endpointConfiguration = new EndpointConfiguration("StartUpDiagnosticsWriter");
+        var typesToScan = TypeScanner.NestedTypes<HeaderWriterSend>();
+        endpointConfiguration.SetTypesToScan(typesToScan);
+        endpointConfiguration.UseTransport<LearningTransport>();
+        string diagnostics = null;
+        endpointConfiguration.CustomDiagnosticsWriter(x =>
+        {
+             diagnostics = x;
+            return Task.CompletedTask;
+        });
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        SnippetLogger.Write(diagnostics);
+        await endpointInstance.Stop();
+    }
+}
