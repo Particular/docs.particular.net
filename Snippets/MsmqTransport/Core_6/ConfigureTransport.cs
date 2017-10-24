a5bdf523bfba8ee80fc6c8a8392d3aac33968f62
indualagarsamy <indu.alagarsamy@particular.net>
2017-10-23 18:16:29 -0700

    setting minimum version for v6 snippet

--- Snippets/MsmqTransport/Core_6/ConfigureTransport.cs
+++ Snippets/MsmqTransport/Core_6/ConfigureTransport.cs
@@ -3,8 +3,8 @@ class ConfigureTransport
 {
     ConfigureTransport(EndpointConfiguration endpointConfiguration)
     {
-        #region msmq-config-basic
-        
+        #region msmq-config-basic [6.0,)
+
         var transport = endpointConfiguration.UseTransport<MsmqTransport>();
         transport.ConnectionString("deadLetter=false;");
 
