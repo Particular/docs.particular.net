3536868b6ed2d70c39b22ddefc7a1ce07f35c81e
Simon Cropp <simon.cropp@gmail.com>
2017-10-24 11:50:41 +1100

    add trans sql 3.1

---
+++ samples/sql-persistence/transitioning-correlation-ids/SqlPersistence_3.1/Phase1/Shared/OrderSaga.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+using NServiceBus.Persistence.Sql;
+
+#region sagaPhase1
+
+public class OrderSaga :
+    SqlSaga<OrderSagaData>,
+    IAmStartedByMessages<StartOrder>
+{
+    static ILog log = LogManager.GetLogger<OrderSaga>();
+
+    protected override void ConfigureMapping(IMessagePropertyMapper mapper)
+    {
+        mapper.ConfigureMapping<StartOrder>(_ => _.OrderNumber);
+    }
+
+    protected override string CorrelationPropertyName => nameof(OrderSagaData.OrderNumber);
+
+    public Task Handle(StartOrder message, IMessageHandlerContext context)
+    {
+        Data.OrderNumber = message.OrderNumber;
+        log.Info($"Received StartOrder message. Data.OrderNumber={Data.OrderNumber}");
+        return Task.CompletedTask;
+    }
+}
+
+#endregion
