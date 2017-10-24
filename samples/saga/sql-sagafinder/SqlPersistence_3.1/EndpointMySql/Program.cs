baf68bf437659de7c106ea53cd487ca95ca5495c
Simon Cropp <simon.cropp@gmail.com>
2017-10-24 14:17:38 +1100

    add finder

---
+++ samples/saga/sql-sagafinder/SqlPersistence_3.1/EndpointMySql/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using NServiceBus;
+using NServiceBus.Persistence.Sql;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.SqlSagaFinder.MySql";
+        var endpointConfiguration = new EndpointConfiguration("Samples.SqlSagaFinder.MySql");
+        endpointConfiguration.UseTransport<LearningTransport>();
+        endpointConfiguration.SendFailedMessagesTo("error");
+        endpointConfiguration.EnableInstallers();
+        #region MySqlConfig
+
+        var persistence = endpointConfiguration.UsePersistence<SqlPersistence>();
+        var password = Environment.GetEnvironmentVariable("MySqlPassword");
+        if (string.IsNullOrWhiteSpace(password))
+        {
+            throw new Exception("Could not extract 'MySqlPassword' from Environment variables.");
+        }
+        var username = Environment.GetEnvironmentVariable("MySqlUserName");
+        if (string.IsNullOrWhiteSpace(username))
+        {
+            throw new Exception("Could not extract 'MySqlUserName' from Environment variables.");
+        }
+        var connection = $"server=localhost;user={username};database=sqlpersistencesample;port=3306;password={password};AllowUserVariables=True;AutoEnlist=false";
+        persistence.SqlDialect<SqlDialect.MySql>();
+        persistence.ConnectionBuilder(
+            connectionBuilder: () =>
+            {
+                return new MySqlConnection(connection);
+            });
+        var subscriptions = persistence.SubscriptionSettings();
+        subscriptions.CacheFor(TimeSpan.FromMinutes(1));
+
+        #endregion
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        var startOrder = new StartOrder
+        {
+            OrderId = "123"
+        };
+        await endpointInstance.SendLocal(startOrder)
+            .ConfigureAwait(false);
+
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
---
+++ samples/saga/sql-sagafinder/SqlPersistence_3.1/EndpointPostgreSql/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading.Tasks;
+using Npgsql;
+using NpgsqlTypes;
+using NServiceBus;
+using NServiceBus.Persistence.Sql;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.SqlSagaFinder.PostgreSql";
+        var endpointConfiguration = new EndpointConfiguration("Samples.SqlSagaFinder.PostgreSql");
+        endpointConfiguration.UseTransport<LearningTransport>();
+        endpointConfiguration.SendFailedMessagesTo("error");
+        endpointConfiguration.EnableInstallers();
+        #region PostgreSqlConfig
+
+        var persistence = endpointConfiguration.UsePersistence<SqlPersistence>();
+        var password = Environment.GetEnvironmentVariable("PostgreSqlPassword");
+        if (string.IsNullOrWhiteSpace(password))
+        {
+            throw new Exception("Could not extract 'PostgreSqlPassword' from Environment variables.");
+        }
+        var username = Environment.GetEnvironmentVariable("PostgreSqlUserName");
+        if (string.IsNullOrWhiteSpace(username))
+        {
+            throw new Exception("Could not extract 'PostgreSqlUserName' from Environment variables.");
+        }
+        var connection = $"Host=localhost;Username={username};Password={password};Database=NsbSamplesSqlSagaFinder";
+        persistence.TablePrefix("Finder");
+        var dialect = persistence.SqlDialect<SqlDialect.PostgreSql>();
+        dialect.JsonBParameterModifier(
+            modifier: parameter =>
+            {
+                var npgsqlParameter = (NpgsqlParameter)parameter;
+                npgsqlParameter.NpgsqlDbType = NpgsqlDbType.Jsonb;
+            });
+        persistence.ConnectionBuilder(
+            connectionBuilder: () =>
+            {
+                return new NpgsqlConnection(connection);
+            });
+
+        var subscriptions = persistence.SubscriptionSettings();
+        subscriptions.CacheFor(TimeSpan.FromMinutes(1));
+
+        #endregion
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        var startOrder = new StartOrder
+        {
+            OrderId = "123"
+        };
+        await endpointInstance.SendLocal(startOrder)
+            .ConfigureAwait(false);
+
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
