538a1eb1fbd9c7027b35e12e2d0ef83c6ec50ca8
danielmarbach <daniel.marbach@openplace.net>
2017-10-27 09:48:40 +0200

    Show usage in snippets

---
+++ Snippets/UniformSession/UniformSession_2/Usage.cs
@@ -0,0 +1,85 @@
+﻿namespace UniformSession_2
+{
+    using System;
+    using System.Threading.Tasks;
+    using NServiceBus;
+    using NServiceBus.UniformSession;
+
+    #region uniformsession-usage
+    class ComponentReused
+    {
+        IUniformSession session;
+
+        public ComponentReused(IUniformSession session)
+        {
+            this.session = session;
+        }
+
+        public async Task Do()
+        {
+            await session.Send(new MyMessage())
+                .ConfigureAwait(false);
+        }
+    }
+
+    [Route("api/[controller]")]
+    class MyController : Controller
+    {
+        ComponentReused component;
+
+        public MyController(ComponentReused component)
+        {
+            this.component = component;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] Input input)
+        {
+            await component.Do()
+                .ConfigureAwait(false);
+
+            return Ok();
+        }
+    }
+
+    class MyHandler : IHandleMessages<MyCommand>
+    {
+        ComponentReused component;
+
+        public MyHandler(ComponentReused component)
+        {
+            this.component = component;
+        }
+        public async Task Handle(MyCommand message, IMessageHandlerContext context)
+        {
+            await component.Do()
+                .ConfigureAwait(false);
+        }
+    }
+
+    #endregion
+
+    class MyMessage { }
+    class MyCommand { }
+
+    class Controller
+    {
+        public IActionResult Ok()
+        {
+            return default;
+        }
+    }
+    class HttpPostAttribute : Attribute { }
+    class FromBodyAttribute : Attribute { }
+    class RouteAttribute : Attribute
+    {
+        string v;
+
+        public RouteAttribute(string v)
+        {
+            this.v = v;
+        }
+    }
+    class Input { }
+    interface IActionResult { }
+}
