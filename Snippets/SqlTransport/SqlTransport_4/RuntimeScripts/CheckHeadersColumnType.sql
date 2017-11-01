738882d50179b385d7c0123c44a789bdbb3777b4
Simon Cropp <simon.cropp@gmail.com>
2017-11-01 14:10:14 +1100

    re gen sql transport scripts

--- Snippets/SqlTransport/SqlTransport_4/RuntimeScripts/CheckHeadersColumnType.sql
+++ Snippets/SqlTransport/SqlTransport_4/RuntimeScripts/CheckHeadersColumnType.sql
@@ -3,6 +3,6 @@ startcode CheckHeadersColumnTypeSql
 SELECT t.name
 FROM sys.columns c
 INNER JOIN sys.types t ON c.system_type_id = t.system_type_id
-WHERE c.object_id = OBJECT_ID('{0}') 
+WHERE c.object_id = OBJECT_ID('{0}')
     AND c.name = 'Headers'
 endcode
