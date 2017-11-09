37ae13f2ad26ec0cc100192d19cae87899228095
Bob Langley <bobiii@thelangleys.org>
2017-11-08 19:45:37 -0800

    Change SagaAudit deprecation packages
    Create nservicebus.sagaaudit.md

---
+++ Snippets/SagaAudit/SagaAudit_1/SagaAudit.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using NServiceBus;
+
+class MyClass
+{
+    public void Enable()
+    {
+        #region SagaAudit_Enable
+
+        var endpointConfiguration = new BusConfiguration();
+        endpointConfiguration.AuditSagaStateChanges(
+            serviceControlQueue: "ServiceControl_Queue");
+
+        #endregion
+    }
+
+    public void CustomSerialization()
+    {
+        #region SagaAudit_CustomSerialization
+
+        var endpointConfiguration = new BusConfiguration();
+        endpointConfiguration.AuditSagaStateChanges(
+            serviceControlQueue: "ServiceControl_Queue",
+            customSagaEntitySerialization: saga =>
+            {
+                var data = (MySagaData) saga;
+                var dictionary = new Dictionary<string, string>
+                {
+                    ["prop1"] = data.Name,
+                    ["prop2"] = data.Address
+                };
+                return dictionary;
+            });
+
+        #endregion
+    }
+
+    class MySagaData
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ nservicebus/upgrades/nservicebus.sagaaudit.md
@@ -0,0 +1,29 @@
+---
+title: Upgrade from ServiceControl.Plugin.NsbX.SagaAudit to NServiceBus.SagaAudit
+summary: Instructions on how to upgrade SagaAudit Plugins to the new NServiceBus.SagaAudit package
+reviewed: 2017-11-08
+component: ServiceControl
+related:
+ - servicecontrol/plugins
+ - servicecontrol/plugins/saga-audit
+isUpgradeGuide: true
+upgradeGuideCoreVersions:
+ - 5
+ - 6
+ - 7
+---
+
+
+## Connecting to ServiceControl
+
+The NserviceBus.SagaAudit package replaces the **ServiceControl.Plugin.Nsb5.SagaAudit** and **ServiceControl.Plugin.Nsb6.SagaAudit** packages. It also introduces the NServiceBus v7 compatible version SagaAudit plugin.
+
+To update, remove the deprecated package and install the NServiceBus.SagaAudit package.
+
+## Configuration
+
+The location of the ServiceControl queue must be specified via extensions to the endpoint configuration.
+
+#### SagaAudit
+
+TODO: Place snippet here
