09384292fa4e3c7607765d436d1b1f75f77e869e
Mike Minutillo <mike.minutillo@particular.net>
2017-11-10 10:27:19 +0800

    Add override to upgrade guide

---
+++ Snippets/Testing/Testing_6/UpgradeGuides/6to7/WhenHandling.cs
@@ -0,0 +1,46 @@
+﻿namespace Testing_6.UpgradeGuides._6to7
+{
+    using System.Threading.Tasks;
+    using NServiceBus;
+    using NServiceBus.Testing;
+    using Testing_6.Saga;
+
+    class WhenHandling
+    {
+        public void Run()
+        {
+            #region 6to7-WhenHandling
+
+            var message = new CompleteSagaMessage();
+
+            Test.Saga<MySaga>()
+                .WhenHandling<CompleteSagaMessage>(msg => { /* msg created for you */ })
+                .AssertSagaCompletionIs(true);
+
+            #endregion
+
+        }
+
+        class MySaga : NServiceBus.Saga<MySagaData>, 
+            IAmStartedByMessages<CompleteSagaMessage>
+        {
+            public Task Handle(CompleteSagaMessage message, IMessageHandlerContext context)
+            {
+                return Task.CompletedTask;
+            }
+
+            protected override void ConfigureHowToFindSaga(SagaPropertyMapper<Testing_6.Saga.MySagaData> mapper)
+            {
+            }
+
+            class MySagaData : ContainSagaData
+            {
+
+            }
+        }
+
+        class CompleteSagaMessage
+        {
+        }
+    }
+}
---
+++ Snippets/Testing/Testing_7/UpgradeGuides/6to7/WhenHandling.cs
@@ -0,0 +1,25 @@
+﻿namespace Testing_7.UpgradeGuides._6to7
+{
+    using NServiceBus.Testing;
+    using Testing_6.Saga;
+
+    class WhenHandling
+    {
+        public void Run()
+        {
+            #region 6to7-WhenHandling
+
+            var message = new CompleteSagaMessage();
+
+            Test.Saga<MySaga>()
+                .ExpectSagaCompleted()
+                .WhenHandling(message);
+            
+            #endregion
+        }
+
+        class CompleteSagaMessage
+        {
+        }
+    }
+}
