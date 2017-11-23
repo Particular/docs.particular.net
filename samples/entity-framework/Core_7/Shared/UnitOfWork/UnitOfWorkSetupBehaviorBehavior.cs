2703bc16b8e492c37f310c21b49a3a5a482490de
SzymonPobiega <szymon.pobiega@gmail.com>
2017-11-23 08:57:18 +0100

    EF UoW support for SQLP

---
+++ samples/entity-framework/Core_7/Shared/UnitOfWork/UnitOfWorkSetupBehaviorBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus.Persistence;
+using NServiceBus.Pipeline;
+
+#region SetupBehavior
+
+public class UnitOfWorkSetupBehaviorBehavior
+    : Behavior<IIncomingLogicalMessageContext>
+{
+    Func<SynchronizedStorageSession, ReceiverDataContext> contextFactory;
+
+    public UnitOfWorkSetupBehaviorBehavior(Func<SynchronizedStorageSession, ReceiverDataContext> contextFactory)
+    {
+        this.contextFactory = contextFactory;
+    }
+
+    public override async Task Invoke(
+        IIncomingLogicalMessageContext context, Func<Task> next)
+    {
+        var uow = new EntityFrameworkUnitOfWork(contextFactory);
+        context.Extensions.Set(uow);
+        await next().ConfigureAwait(false);
+        context.Extensions.Remove<EntityFrameworkUnitOfWork>();
+    }
+}
+
+#endregion
