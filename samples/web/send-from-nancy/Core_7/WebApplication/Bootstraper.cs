f2f1ed4b44d11d5db5dddf2700dc91403943dadd
michal0x17 <michal0x17@gmail.com>
2017-12-09 10:10:36 +0100

    Add version 7 sample

---
+++ samples/web/send-from-nancy/Core_7/WebApplication/Bootstraper.cs
@@ -0,0 +1,44 @@
+﻿using Nancy;
+using Nancy.TinyIoc;
+using NServiceBus;
+
+namespace WebApplication
+{
+    public class Bootstraper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureApplicationContainer(TinyIoCContainer container)
+        {
+            base.ConfigureApplicationContainer(container);
+
+            #region EndpointConfiguration
+
+            var endpointConfiguration = new EndpointConfiguration("Samples.Nancy.Sender");
+            var transport = endpointConfiguration.UseTransport<LearningTransport>();
+            endpointConfiguration.UsePersistence<LearningPersistence>();
+            endpointConfiguration.SendOnly();
+
+            #endregion
+
+            #region Routing
+
+            var routing = transport.Routing();
+            routing.RouteToEndpoint(
+                assembly: typeof(MyMessage).Assembly,
+                destination: "Samples.Nancy.Endpoint");
+
+            #endregion
+
+            #region EndpointStart
+
+            var endpointInstance = Endpoint.Start(endpointConfiguration).GetAwaiter().GetResult();
+
+            #endregion
+
+            #region ServiceRegistration
+
+            container.Register<IMessageSession>(endpointInstance);
+
+            #endregion
+        }
+    }
+}
