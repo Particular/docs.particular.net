6344d1d7179d72d3060ac1a499c27508eab249a9
Yves Goeleven <yves@goeleven.com>
2017-12-13 18:49:41 +0100

    updated cloud showcase code (#3514)

---
+++ samples/show-case/cloud-azure/Core_7/Store.Messages/Events/OrderCancelled.cs
@@ -0,0 +1,11 @@
+﻿namespace Store.Messages.Events
+{
+    using NServiceBus;
+
+    public class OrderCancelled :
+        IEvent
+    {
+        public int OrderNumber { get; set; }
+        public string ClientId { get; set; }
+    }
+}
