80e7b47ac3c5dec42080cf359e93f038c9bbc400
Yves Goeleven <yves@goeleven.com>
2017-12-13 14:23:18 +0100

    updated cloud showcase code

---
+++ samples/show-case/cloud-azure/Core_7/Store.CustomerRelations/OrderAcceptedHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+using Store.Messages.Events;
+
+class OrderAcceptedHandler :
+    IHandleMessages<OrderAccepted>
+{
+    static ILog log = LogManager.GetLogger<OrderAcceptedHandler>();
+
+    public Task Handle(OrderAccepted message, IMessageHandlerContext context)
+    {
+        if (DebugFlagMutator.Debug)
+        {
+            Debugger.Break();
+        }
+
+        log.Info($"Customer: {message.ClientId} is now a preferred customer publishing for other service concerns");
+
+        // publish this event as an asynchronous event
+        var clientBecamePreferred = new ClientBecamePreferred
+        {
+            ClientId = message.ClientId,
+            PreferredStatusExpiresOn = DateTime.Now.AddMonths(2)
+        };
+        return context.Publish(clientBecamePreferred);
+    }
+}
---
+++ samples/show-case/cloud-azure/Core_7/Store.ECommerce/Handlers/OrderPlacedHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using NServiceBus;
+using Store.Messages.Events;
+
+public class OrderPlacedHandler :
+    IHandleMessages<OrderPlaced>
+{
+    public Task Handle(OrderPlaced message, IMessageHandlerContext context)
+    {
+        var hubContext = GlobalHost.ConnectionManager.GetHubContext<OrdersHub>();
+        hubContext.Clients.Client(message.ClientId)
+            .orderReceived(new
+            {
+                message.OrderNumber,
+                message.ProductIds
+            });
+        return Task.CompletedTask;
+    }
+}
