e1e32e4f30f19b07170fe93eb503508ba2da89e9
Mauro Servienti <mauro.servienti@gmail.com>
2018-01-03 15:28:30 +0100

    upgraded to Gateway 3 beta and Core 7 beta

---
+++ samples/websocket-gateway/Gateway_3/SiteA/Program.cs
@@ -0,0 +1,36 @@
+﻿using NServiceBus;
+using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "SiteA";
+
+        #region WebSocketGateway-EndpointConfig-SiteA
+
+        var config = new EndpointConfiguration("Custom Gateway - SiteA");
+        config.UseTransport<LearningTransport>();
+        // NOTE: The LearningPersistence does not support the gateway
+        config.UsePersistence<InMemoryPersistence>();
+
+        var gatewaySettings = config.Gateway();
+        gatewaySettings.ChannelFactories(
+            s => new WebSocketChannelSender(),
+            s => new WebSocketChannelReceiver()
+        );
+
+        #endregion
+
+        var endpoint = await Endpoint.Start(config).ConfigureAwait(false);
+
+        await endpoint.SendToSites(new[] { "SiteB" }, new SomeMessage { Contents = "Hello, World!" }).ConfigureAwait(false);
+
+        Console.WriteLine("Started SiteA. Sent message to SiteB");
+
+        Console.ReadLine();
+
+        await endpoint.Stop().ConfigureAwait(false);
+    }
+}
---
+++ samples/websocket-gateway/Gateway_3/SiteB/Program.cs
@@ -0,0 +1,36 @@
+﻿using NServiceBus;
+using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "SiteB";
+
+        #region WebSocketGateway-EndpointConfig-SiteB
+
+        var config = new EndpointConfiguration("Custom Gateway - SiteB");
+        var transport = config.UseTransport<LearningTransport>();
+        var routing = transport.Routing();
+        routing.RouteToEndpoint(typeof(SomeMessage), "Custom Gateway - SiteB");
+        // NOTE: The LearningPersistence does not support the gateway
+        config.UsePersistence<InMemoryPersistence>();
+
+        var gatewaySettings = config.Gateway();
+        gatewaySettings.ChannelFactories(
+            s => new WebSocketChannelSender(),
+            s => new WebSocketChannelReceiver()
+        );
+
+        #endregion
+
+        var endpoint = await Endpoint.Start(config).ConfigureAwait(false);
+
+        Console.WriteLine("Started SiteB");
+
+        Console.ReadLine();
+
+        await endpoint.Stop().ConfigureAwait(false);
+    }
+}
