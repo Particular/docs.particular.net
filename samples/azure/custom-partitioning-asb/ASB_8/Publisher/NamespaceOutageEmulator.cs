4351390ba2a07cdad5fe81940bcfa543f21cd783
Sean Feldman <feldman.sean@gmail.com>
2018-01-04 15:22:13 -0700

    Transport V8 needs to take into consideration bundle with 2 topics, but not fail if there's only 1 (default)

--- samples/azure/custom-partitioning-asb/ASB_8/Publisher/NamespaceOutageEmulator.cs
+++ samples/azure/custom-partitioning-asb/ASB_8/Publisher/NamespaceOutageEmulator.cs
@@ -15,17 +15,26 @@ class NamespaceOutageEmulator
     public async Task Toggle()
     {
         var namespaceManager = NamespaceManager.CreateFromConnectionString(connectionString);
+
         var topic1 = await namespaceManager.GetTopicAsync("bundle-1")
             .ConfigureAwait(false);
-        var topic2 = await namespaceManager.GetTopicAsync("bundle-2")
-            .ConfigureAwait(false);
-
-        topic1.Status = topic2.Status = NewStatus(topic1.Status);
-
+        var newStatus = NewStatus(topic1.Status);
+        topic1.Status = newStatus;
         await namespaceManager.UpdateTopicAsync(topic1)
             .ConfigureAwait(false);
-        await namespaceManager.UpdateTopicAsync(topic2)
-            .ConfigureAwait(false);
+
+        try
+        {
+            var topic2 = await namespaceManager.GetTopicAsync("bundle-2")
+                .ConfigureAwait(false);
+            topic2.Status = newStatus;
+            await namespaceManager.UpdateTopicAsync(topic2)
+                .ConfigureAwait(false);
+
+        }
+        catch (MessagingEntityNotFoundException)
+        {
+        }
 
         var parts = Console.Title.Split(new [] {" ("}, StringSplitOptions.RemoveEmptyEntries);
         Console.Title = parts[0] + (topic1.Status == EntityStatus.Active ? "" : " (OUTAGE: Namespace with connection string 'AzureServiceBus.ConnectionString.1' is not available)");
@@ -33,23 +42,37 @@ class NamespaceOutageEmulator
     public static async Task EnsureNoOutageIsEmulated(string connectionString)
     {
         var namespaceManager = NamespaceManager.CreateFromConnectionString(connectionString);
-        var topic1 = await namespaceManager.GetTopicAsync("bundle-1")
-            .ConfigureAwait(false);
-        var topic2 = await namespaceManager.GetTopicAsync("bundle-2")
-            .ConfigureAwait(false);
 
-        if (topic1.Status != EntityStatus.Active)
+        try
         {
-            topic1.Status = EntityStatus.Active;
-            await namespaceManager.UpdateTopicAsync(topic1)
+            var topic1 = await namespaceManager.GetTopicAsync("bundle-1")
                 .ConfigureAwait(false);
+
+            if (topic1.Status != EntityStatus.Active)
+            {
+                topic1.Status = EntityStatus.Active;
+                await namespaceManager.UpdateTopicAsync(topic1)
+                    .ConfigureAwait(false);
+            }
+        }
+        catch (MessagingEntityNotFoundException)
+        {
         }
 
-        if (topic2.Status != EntityStatus.Active)
+        try
         {
-            topic2.Status = EntityStatus.Active;
-            await namespaceManager.UpdateTopicAsync(topic2)
+            var topic2 = await namespaceManager.GetTopicAsync("bundle-2")
                 .ConfigureAwait(false);
+        
+            if (topic2.Status != EntityStatus.Active)
+            {
+                topic2.Status = EntityStatus.Active;
+                await namespaceManager.UpdateTopicAsync(topic2)
+                    .ConfigureAwait(false);
+            }
+        }
+        catch (MessagingEntityNotFoundException)
+        {
         }
     }
 
