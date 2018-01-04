13d9ec6ea8a107077e223b55f391b3c3d5d3a13d
Mauro Servienti <mauro.servienti@gmail.com>
2018-01-04 14:56:24 +0100

    DeadLetter Queue CustomCheck sample for Core V7

---
+++ samples/msmq/dlq-customcheck/Core_7/SampleEndpoint/Program.cs
@@ -0,0 +1,37 @@
+﻿using NServiceBus;
+using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        MsmqUtils.SetUpDummyQueue();
+        var endpointConfiguration = new EndpointConfiguration("SampleEndpoint");
+        endpointConfiguration.UseTransport<MsmqTransport>();
+        endpointConfiguration.UsePersistence<InMemoryPersistence>();
+        endpointConfiguration.SendFailedMessagesTo("error");
+
+        #region configure-custom-checks
+
+        endpointConfiguration.ReportCustomChecksTo("Particular.ServiceControl");
+
+        #endregion
+
+        var endpoint = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+
+        Console.WriteLine("Endpoint Started");
+        Console.WriteLine("Press [d] to send a message to the Dead Letter Queue");
+        Console.WriteLine("Press any other key to exit");
+
+        while (Console.ReadKey(true).Key == ConsoleKey.D)
+        {
+            MsmqUtils.SendMessageToDeadLetterQueue(DateTime.UtcNow.ToShortTimeString());
+            Console.WriteLine("Sent message to Dead Letter Queue");
+        }
+
+        await endpoint.Stop()
+            .ConfigureAwait(false);
+    }
+}
