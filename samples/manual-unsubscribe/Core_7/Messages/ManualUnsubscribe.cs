9f2bc992e5422460e01aa48e7d2d92e8865cc611
Mauro Servienti <mauro.servienti@gmail.com>
2018-01-04 11:50:08 +0100

    manual unsubscribe V7 sample (full framework only)

---
+++ samples/manual-unsubscribe/Core_7/Messages/ManualUnsubscribe.cs
@@ -0,0 +1,10 @@
+﻿using NServiceBus;
+
+#region unsubscribe-message
+public class ManualUnsubscribe :
+    IMessage
+{
+    public string MessageTypeName { get; set; }
+    public string SubscriberEndpoint { get; set; }
+}
+#endregion
