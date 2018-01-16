b5d873797e4c1b99f093ab55bd00ec7a81aae4c2
Ramon Smits <ramon.smits@gmail.com>
2018-01-16 16:35:07 +0100

    Missing snippet for [1.2.0, 2.0.0).

--- Snippets/MetricsServiceControl/MetricsServiceControl_1.2/Configuration.cs
+++ Snippets/MetricsServiceControl/MetricsServiceControl_1.2/Configuration.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
 using NServiceBus;
 using NServiceBus.Metrics.ServiceControl;
 
@@ -20,6 +22,20 @@ class Configuration
 
         busConfiguration.SetServiceControlMetricsMessageTTBR(TimeSpan.FromHours(1));
 
+        #endregion
+    }
+    void SendMetricDataToServiceControlHostId(BusConfiguration busConfiguration)
+    {
+        #region SendMetricDataToServiceControlHostId
+        const string SERVICE_CONTROL_METRICS_ADDRESS = "particular.monitoring";
+
+        var endpointName = "MyEndpoint";
+        var machineName = $"{Dns.GetHostName()}.{IPGlobalProperties.GetIPGlobalProperties().DomainName}";
+        var instanceIdentifier = $"{endpointName}@{machineName}";
+
+        busConfiguration.SendMetricDataToServiceControl(
+            serviceControlMetricsAddress: SERVICE_CONTROL_METRICS_ADDRESS,
+            instanceId: instanceIdentifier);
         #endregion
     }
 }
