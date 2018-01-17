cbf84e10c11ee067f126bd7e7a76edc2ec988581
David Boike <david.boike@gmail.com>
2018-01-17 13:43:33 -0600

    V4 - Remove Raven.Database package

--- samples/ravendb/simple/Raven_4/Server/Program.cs
+++ samples/ravendb/simple/Raven_4/Server/Program.cs
@@ -8,38 +8,36 @@ class Program
     static async Task Main()
     {
         Console.Title = "Samples.RavenDB.Server";
-        using (new RavenHost())
+        #region Config
+
+        var endpointConfiguration = new EndpointConfiguration("Samples.RavenDB.Server");
+        using (var documentStore = new DocumentStore
         {
-            #region Config
+            Url = "http://localhost:8080",
+            DefaultDatabase = "RavenSimpleSample",
+            EnlistInDistributedTransactions = false
+        })
+        {
+            documentStore.Initialize();
 
-            var endpointConfiguration = new EndpointConfiguration("Samples.RavenDB.Server");
-            using (var documentStore = new DocumentStore
-            {
-                Url = "http://localhost:32076",
-                DefaultDatabase = "RavenSampleData"
-            })
-            {
-                documentStore.Initialize();
+            var persistence = endpointConfiguration.UsePersistence<RavenDBPersistence>();
+            // Only required to simplify the sample setup
+            persistence.DoNotSetupDatabasePermissions();
+            persistence.SetDefaultDocumentStore(documentStore);
 
-                var persistence = endpointConfiguration.UsePersistence<RavenDBPersistence>();
-                // Only required to simplify the sample setup
-                persistence.DoNotSetupDatabasePermissions();
-                persistence.SetDefaultDocumentStore(documentStore);
+            #endregion
 
-                #endregion
+            endpointConfiguration.UseTransport<LearningTransport>();
+            endpointConfiguration.EnableInstallers();
 
-                endpointConfiguration.UseTransport<LearningTransport>();
-                endpointConfiguration.EnableInstallers();
+            var endpointInstance = await Endpoint.Start(endpointConfiguration)
+                .ConfigureAwait(false);
 
-                var endpointInstance = await Endpoint.Start(endpointConfiguration)
-                    .ConfigureAwait(false);
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
 
-                Console.WriteLine("Press any key to exit");
-                Console.ReadKey();
-
-                await endpointInstance.Stop()
-                    .ConfigureAwait(false);
-            }
+            await endpointInstance.Stop()
+                .ConfigureAwait(false);
         }
     }
 }
