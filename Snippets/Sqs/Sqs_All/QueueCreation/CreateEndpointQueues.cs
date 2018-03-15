b1a6e084607356236cd6aa3b02aa0b8b25959809
danielmarbach <daniel.marbach@openplace.net>
2018-03-15 17:30:14 +0100

    Endpoints Queue Creation takes delay delivery method into account

--- Snippets/Sqs/Sqs_All/QueueCreation/CreateEndpointQueues.cs
+++ Snippets/Sqs/Sqs_All/QueueCreation/CreateEndpointQueues.cs
@@ -22,22 +22,36 @@
 
         #region sqs-create-queues-for-endpoint
 
-        public static async Task CreateQueuesForEndpoint(string endpointName, TimeSpan? maxTimeToLive = null, string queueNamePrefix = null, bool includeRetries = false)
+        public static async Task CreateQueuesForEndpoint(string endpointName, TimeSpan? maxTimeToLive = null, string queueNamePrefix = null, bool includeRetries = false, string delayedDeliveryMethod = "Native")
         {
-            // timeout dispatcher queue
-            // This queue is created first because it has the longest name. 
-            // If the endpointName and queueNamePrefix are too long this call will throw and no queues will be created. 
-            // In this event, a shorter value for endpointName or queueNamePrefix should be used.
-            await QueueCreationUtils.CreateQueue($"{endpointName}.TimeoutsDispatcher", maxTimeToLive, queueNamePrefix)
-                .ConfigureAwait(false);
+            switch (delayedDeliveryMethod)
+            {
+                case "TimeoutManager":
+
+                    // timeout dispatcher queue
+                    // This queue is created first because it has the longest name. 
+                    // If the endpointName and queueNamePrefix are too long this call will throw and no queues will be created. 
+                    // In this event, a shorter value for endpointName or queueNamePrefix should be used.
+                    await QueueCreationUtils.CreateQueue($"{endpointName}.TimeoutsDispatcher", maxTimeToLive, queueNamePrefix)
+                        .ConfigureAwait(false);
+
+                    // timeout queue
+                    await QueueCreationUtils.CreateQueue($"{endpointName}.Timeouts", maxTimeToLive, queueNamePrefix)
+                        .ConfigureAwait(false);
+
+                    break;
+                case "UnrestrictedDelayedDelivery":
+                    
+                    await QueueCreationUtils.CreateQueue($"{endpointName}-delay.fifo", maxTimeToLive, queueNamePrefix)
+                        .ConfigureAwait(false);
+
+                    break;
+            }
 
             // main queue
             await QueueCreationUtils.CreateQueue(endpointName, maxTimeToLive, queueNamePrefix)
                 .ConfigureAwait(false);
 
-            // timeout queue
-            await QueueCreationUtils.CreateQueue($"{endpointName}.Timeouts", maxTimeToLive, queueNamePrefix)
-                .ConfigureAwait(false);
             
             // retries queue
             if (includeRetries)
