050407e2acfc5f8275a91cd5499db64a3723101e
danielmarbach <daniel.marbach@openplace.net>
2018-03-15 18:02:34 +0100

    Added parameter to powershell script and let QueueNameHelper deal with fifo queues

--- Snippets/Sqs/Sqs_All/QueueCreation/QueueCreation.ps1
+++ Snippets/Sqs/Sqs_All/QueueCreation/QueueCreation.ps1
@@ -35,7 +35,11 @@ param(
 [string] $QueueNamePrefix,
 
 [Parameter(HelpMessage="Only required for NSB Versions 5 and below")]
-[Switch] $IncludeRetries
+[Switch] $IncludeRetries,
+
+[Parameter(HelpMessage="'TimeoutManager' for timeout manager queues (V1), 'Native' for V2-V3, 'UnrestrictedDelayedDelivery' for unrestricted delayed delivery (V4 and higher)", Mandatory=$false)]
+[ValidateNotNullOrEmpty()]
+[string] $DelayedDeliveryMethod = "Native"
 )
 
 # timeout dispatcher queue
@@ -101,6 +105,7 @@ return $false
 
 Add-Type @'
     using System;
+    using System.Text;
 
     public static class QueueNameHelper
     {
@@ -113,26 +118,29 @@ Add-Type @'
 
             var s = queueNamePrefix + destination;
 
-            // SQS queue names can only have alphanumeric characters, hyphens and underscores.
-            // Any other characters will be replaced with a hyphen.
-            for (var i = 0; i < s.Length; ++i)
-            {
-                var c = s[i];
-                if (!char.IsLetterOrDigit(c)
-                    && c != '-'
-                    && c != '_')
-                {
-                    s = s.Replace(c, '-');
-                }
-            }
-
             if (s.Length > 80)
             {
                 throw new ArgumentException(
                     string.Format("Address {0} with configured prefix {1} is longer than 80 characters and therefore cannot be used to create an SQS queue. Use a shorter endpoint name or a shorter queue name prefix.", destination, queueNamePrefix));
             }
 
-            return s;
+            // SQS queue names can only have alphanumeric characters, hyphens and underscores.
+            // Any other characters will be replaced with a hyphen.
+            var skipCharacters = s.EndsWith(".fifo") ? 5 : 0;
+            var queueNameBuilder = new StringBuilder(s);
+
+            for (var i = 0; i < queueNameBuilder.Length - skipCharacters; ++i)
+            {
+                var c = queueNameBuilder[i];
+                if (!char.IsLetterOrDigit(c)
+                    && c != '-'
+                    && c != '_')
+                {
+                    queueNameBuilder[i] = '-';
+                }
+            }
+
+            return queueNameBuilder.ToString();
         }
     }
 '@
