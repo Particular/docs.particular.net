811676bdfa234b25231729ba1123fb5d2cac16a9
danielmarbach <daniel.marbach@openplace.net>
2018-03-15 17:30:47 +0100

    Client factory should use standard ctor of SDK types

--- Snippets/Sqs/Sqs_All/ClientFactory.cs
+++ Snippets/Sqs/Sqs_All/ClientFactory.cs
@@ -1,8 +1,5 @@
 ﻿namespace SqsAll
 {
-    using System;
-    using System.Linq;
-    using Amazon;
     using Amazon.CloudFormation;
     using Amazon.S3;
     using Amazon.SQS;
@@ -11,45 +8,17 @@
     {
         public static IAmazonSQS CreateSqsClient()
         {
-            var config = new AmazonSQSConfig
-            {
-                RegionEndpoint = GetRegion()
-            };
-
-
-            return new AmazonSQSClient(config);
+            return new AmazonSQSClient();
         }
 
         public static IAmazonS3 CreateS3Client()
         {
-            var config = new AmazonS3Config
-            {
-                RegionEndpoint = GetRegion()
-            };
-
-            return new AmazonS3Client(config);
+            return new AmazonS3Client();
         }
 
         public static IAmazonCloudFormation CreateCloudFormationClient()
         {
-            var config = new AmazonCloudFormationConfig
-            {
-                RegionEndpoint = GetRegion()
-            };
-
-            return new AmazonCloudFormationClient(config);
-        }
-
-        static RegionEndpoint GetRegion()
-        {
-            var region = EnvironmentHelper.GetEnvironmentVariable(SqsTransportConfigurationExtensions.RegionEnvironmentVariableName) ?? "ap-southeast-2";
-            var awsRegion = RegionEndpoint.EnumerableAllRegions.SingleOrDefault(x => x.SystemName == region);
-
-            if (awsRegion == null)
-            {
-                throw new ArgumentException($"Unknown region: \"{region}\"");
-            }
-            return awsRegion;
+            return new AmazonCloudFormationClient();
         }
     }
 }
