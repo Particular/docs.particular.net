386395dde393fd6f8faf49456320ba427f719452
danielmarbach <daniel.marbach@openplace.net>
2018-03-19 08:32:53 +0100

    Remove native deferral and region (use default client ctor instead)

--- Snippets/Sqs/Sqs_All/SqsTransportConfigurationExtensions.cs
+++ Snippets/Sqs/Sqs_All/SqsTransportConfigurationExtensions.cs
@@ -1,22 +1,17 @@
 ﻿namespace SqsAll
 {
-    using Amazon;
     using Amazon.SQS;
     using NServiceBus;
 
     public static class SqsTransportConfigurationExtensions
     {
-        internal const string RegionEnvironmentVariableName = "NServiceBus_AmazonSQS_Region";
         const string S3BucketEnvironmentVariableName = "NServiceBus_AmazonSQS_S3Bucket";
-        const string NativeDeferralEnvironmentVariableName = "NServiceBus_AmazonSQS_NativeDeferral";
 
         public static string S3BucketName => EnvironmentHelper.GetEnvironmentVariable(S3BucketEnvironmentVariableName);
 
         public static void ConfigureSqsTransport(this TransportExtensions<SqsTransport> transportConfiguration, string queueNamePrefix = null)
         {
-            var region = EnvironmentHelper.GetEnvironmentVariable(RegionEnvironmentVariableName);
-
-            transportConfiguration.ClientFactory(() => new AmazonSQSClient(new AmazonSQSConfig { RegionEndpoint = RegionEndpoint.GetBySystemName(region) }));
+            transportConfiguration.ClientFactory(() => new AmazonSQSClient());
             if (queueNamePrefix != null)
             {
                 transportConfiguration.QueueNamePrefix(queueNamePrefix);
@@ -27,15 +22,6 @@
             if (!string.IsNullOrEmpty(S3BucketName))
             {
                 transportConfiguration.S3(S3BucketName, "test");
-            }
-
-            var nativeDeferralRaw = EnvironmentHelper.GetEnvironmentVariable(NativeDeferralEnvironmentVariableName);
-            var validValue = bool.TryParse(nativeDeferralRaw, out var nativeDeferral);
-            if (validValue && nativeDeferral)
-            {
-#pragma warning disable 618
-                transportConfiguration.NativeDeferral();
-#pragma warning restore 618
             }
         }
     }
