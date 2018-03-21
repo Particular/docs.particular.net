cb7267ba7389fbde4a8cae2d38fc90642c9ffb86
Hadi Eskandari <h.eskandari@gmail.com>
2018-03-21 16:53:30 +1100

    Port of quickstart for VS2015

---
+++ tutorials/quickstart/VS2015Solution/Sales/PlaceOrderHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using Messages;
+using NServiceBus;
+using NServiceBus.Logging;
+
+namespace Sales
+{
+    public class PlaceOrderHandler :
+        IHandleMessages<PlaceOrder>
+    {
+        static ILog log = LogManager.GetLogger<PlaceOrderHandler>();
+        static Random random = new Random();
+
+        public Task Handle(PlaceOrder message, IMessageHandlerContext context)
+        {
+            log.Info($"Received PlaceOrder, OrderId = {message.OrderId}");
+
+            // This is normally where some business logic would occur
+
+            #region ThrowTransientException
+            // Uncomment to test throwing transient exceptions
+            //if (random.Next(0, 5) == 0)
+            //{
+            //    throw new Exception("Oops");
+            //}
+            #endregion
+
+            // Uncomment to test throwing fatal exceptions
+            //throw new Exception("BOOM");
+
+            var orderPlaced = new OrderPlaced
+            {
+                OrderId = message.OrderId
+            };
+            
+            log.Info($"Publishing OrderPlaced, OrderId = {message.OrderId}");
+            
+            return context.Publish(orderPlaced);
+        }
+    }
+}
