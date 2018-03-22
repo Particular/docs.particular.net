07331eb4fab1dfc7b11526ccd4d77bd6a9557bf7
Hadi Eskandari <H.Eskandari@Gmail.com>
2018-03-23 08:21:36 +1100

    Port of quickstart for VS2015 (#3678)
    * Port of quickstart for VS2015
    
    * Remove cruft

---
+++ tutorials/quickstart/VS2015Solution/Billing/Program.cs
@@ -0,0 +1,32 @@
+﻿using NServiceBus;
+using System;
+using System.Threading.Tasks;
+
+namespace Billing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AsyncMain().GetAwaiter().GetResult();
+        }
+
+        static async Task AsyncMain()
+        {
+            Console.Title = "Billing";
+
+            var endpointConfiguration = new EndpointConfiguration("Billing");
+
+            endpointConfiguration.UseTransport<LearningTransport>();
+
+            var endpointInstance = await Endpoint.Start(endpointConfiguration)
+                .ConfigureAwait(false);
+
+            Console.WriteLine("Press Enter to exit.");
+            Console.ReadLine();
+
+            await endpointInstance.Stop()
+                .ConfigureAwait(false);
+        }
+    }
+}
