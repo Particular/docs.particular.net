18031ec74af8066d707cecf8dc905ddb49a9bcc1
Tim <bussmann.t@gmail.com>
2018-04-05 16:05:29 +0200

    remove routing sample files

--- samples/routing/custom/Core_6/Shared/AutomaticRoutingConst.cs
+++
@@ -1,9 +0,0 @@
-﻿public static class AutomaticRoutingConst
-{
-    static AutomaticRoutingConst()
-    {
-        SqlHelper.EnsureDatabaseExists(ConnectionString);
-    }
-
-    public static string ConnectionString = @"Data Source=.\SqlExpress;Database=NsbSamplesAutomaticRouting;Integrated Security=True";
-}
--- samples/routing/custom/Core_7/Shared/ICircuitBreaker.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using System.Threading.Tasks;
-
-interface ICircuitBreaker
-{
-    void Success();
-    Task Failure(Exception exception);
-}
