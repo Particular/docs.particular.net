812dc0752708560bfb8281ed08521dfb515d0c13
David Boike <david.boike@gmail.com>
2018-04-24 09:35:20 -0500

    Integrity tests for samples and snippets. (#3738)
    * Prototype test
    
    * TestRunner
    
    * Different API
    
    * Cleanup

---
+++ tests/IntegrityTests/ProjectFrameworks.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using NUnit.Framework;
+
+namespace IntegrityTests
+{
+    public class ProjectFrameworks
+    {
+       [Test]
+        public void DoNotUseTargetFrameworksPlural()
+        {
+            new TestRunner("*.csproj", "Project files should not be multi-targeted with <TargetFrameworks> element")
+                .Run(projectFilePath =>
+                {
+                    var xdoc = XDocument.Load(projectFilePath);
+                    if (xdoc.Root.Attribute("xmlns") != null)
+                    {
+                        return true;
+                    }
+
+                    var firstTargetFrameworksElement = xdoc.XPathSelectElement("/Project/PropertyGroup/TargetFrameworks");
+                    return firstTargetFrameworksElement == null;
+                });
+        }
+
+        [Test]
+        public void EnsureSingleTargetFramework()
+        {
+            new TestRunner("*.csproj", "Project files should only contain a single <TargetFramework> element")
+                .Run(projectFilePath =>
+                {
+                    var xdoc = XDocument.Load(projectFilePath);
+                    if (xdoc.Root.Attribute("xmlns") != null)
+                    {
+                        return true;
+                    }
+
+                    var targetFrameworkElements = xdoc.XPathSelectElements("/Project/PropertyGroup/TargetFramework");
+
+                    // Project may have zero of these if it has TargetFrameworks, but then it fails DoNotUseTargetFrameworksPlural
+                    // Projects with no target framework will not build at all
+                    return targetFrameworkElements.Count() <= 1;
+                });
+        }
+    }
+}
---
+++ tests/IntegrityTests/TestRunner.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Linq;
+using NUnit.Framework;
+
+namespace IntegrityTests
+{
+    public class TestRunner
+    {
+        private string glob;
+        private string errorMessage;
+
+        public TestRunner(string glob, string errorMessage)
+        {
+            this.glob = glob;
+            this.errorMessage = errorMessage;
+        }
+
+        public void Run(Func<string, bool> testDelegate)
+        {
+            var badProjects = new List<string>();
+
+            foreach (var rootPath in TestSetup.RootDirectories)
+            {
+                var projectFiles = Directory.GetFiles(rootPath, glob, SearchOption.AllDirectories);
+
+                foreach (var projectFilePath in projectFiles)
+                {
+                    bool success = testDelegate(projectFilePath);
+
+                    if (!success)
+                    {
+                        badProjects.Add(projectFilePath);
+                    }
+                }
+            }
+
+            if (badProjects.Count > 0)
+            {
+                Assert.Fail($"{errorMessage}:\r\n  > {string.Join("\r\n  > ", badProjects)}");
+            }
+        }
+    }
+}
