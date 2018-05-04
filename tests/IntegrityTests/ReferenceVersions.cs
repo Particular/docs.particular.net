110b8176106044c00dd9a634e3c8b0ee044a999d
David Boike <david.boike@gmail.com>
2018-05-04 10:35:12 -0500

    Ensure project references have lower bound to satisfy NU1604: Project dependency (whatever) does not contain an inclusive lower bound. Include a lower bound in the dependency version to ensure consistent restore results.

---
+++ tests/IntegrityTests/ReferenceVersions.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using NUnit.Framework;
+
+namespace IntegrityTests
+{
+    public class ReferenceVersions
+    {
+       [Test]
+        public void MustScopeReferencesToAMajorVersion()
+        {
+            new TestRunner("*.csproj", "Package References cannot have Version=\"*\" or restore will sometimes fail and yield old, incorrect, or mismatched versions.")
+                .Run(projectFilePath =>
+                {
+                    var xdoc = XDocument.Load(projectFilePath);
+                    if (xdoc.Root.Attribute("xmlns") != null)
+                    {
+                        // Ignore pre-VS2017 projects
+                        return true;
+                    }
+
+                    var badPackageRefs = xdoc.XPathSelectElements("/Project/ItemGroup/PackageReference[@Version='*']");
+                    return !badPackageRefs.Any();
+                });
+        }
+    }
+}
