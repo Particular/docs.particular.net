0a8e3f4d2e6e686f56a80c0136665b7326925162
David Boike <david.boike@gmail.com>
2018-05-04 13:39:51 -0500

    Ensure project references have lower bound to satisfy NU1604 error (#3761)
    * Ensure project references have lower bound to satisfy NU1604: Project dependency (whatever) does not contain an inclusive lower bound. Include a lower bound in the dependency version to ensure consistent restore results.
    
    * I hate XML namespaces

---
+++ tests/IntegrityTests/ReferenceVersions.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using NUnit.Framework;
+
+namespace IntegrityTests
+{
+    public class ReferenceVersions
+    {
+       [Test]
+        public void MustScopeReferencesToAMajorVersion()
+        {
+            new TestRunner("*.csproj", "Package References cannot have Version=\"*\" or restore will sometimes fail and yield old, incorrect, or mismatched versions.")
+                .Run(projectFilePath =>
+                {
+                    var xdoc = XDocument.Load(projectFilePath);
+                    var nsMgr = new XmlNamespaceManager(new NameTable());
+                    var query = "/Project/ItemGroup/PackageReference[@Version='*']";
+                    var xmlnsAtt = xdoc.Root.Attribute("xmlns");
+
+                    if (xmlnsAtt != null)
+                    {
+                        query = "/x:Project/x:ItemGroup/x:PackageReference[@Version='*']";
+                        var xmlns = xmlnsAtt.Value;
+                        nsMgr.AddNamespace("x", xmlns);
+                    }
+
+                    var badPackageRefs = xdoc.XPathSelectElements(query, nsMgr);
+                    return !badPackageRefs.Any();
+                });
+        }
+    }
+}
