a2cc771cb27c73db6e26d048d4515cebf7f71ecf
Weronika Łabaj <weralabaj@users.noreply.github.com>
2018-05-17 08:31:04 +0200

    Add websocket gateway 3 sample (#3771)
    * upgraded to Gateway 3 beta and Core 7 beta
    
    * add prerelease tag
    
    * Fixes wildcard depnedencies
    
    * move gateway config from app.config file to code
    
    * expand snippets to include new code config
    
    * clarify code comments
    
    * fix snippets/partials
    
    * update to rtm wildcards, fix integrity test

---
+++ samples/websocket-gateway/Gateway_3/SiteA/Program.cs
@@ -0,0 +1,46 @@
+﻿using NServiceBus;
+using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "SiteA";
+
+        #region WebSocketGateway-EndpointConfig-SiteA
+
+        var config = new EndpointConfiguration("Custom Gateway - SiteA");
+        config.UseTransport<LearningTransport>();
+        // NOTE: Using InMemoryPersistence since the LearningPersistence does not support the gateway
+        config.UsePersistence<InMemoryPersistence>();
+
+        var gatewaySettings = config.Gateway();
+        gatewaySettings.ChannelFactories(
+            s => new WebSocketChannelSender(),
+            s => new WebSocketChannelReceiver()
+        );
+
+        gatewaySettings.AddReceiveChannel(
+            address: "ws://localhost:33334/SiteA",
+            type: "WebSocket",
+            isDefault: true);
+
+        gatewaySettings.AddSite(
+            siteKey: "SiteB",
+            address: "ws://localhost:33335/SiteB",
+            type: "WebSocket");
+
+        #endregion
+
+        var endpoint = await Endpoint.Start(config).ConfigureAwait(false);
+
+        await endpoint.SendToSites(new[] { "SiteB" }, new SomeMessage { Contents = "Hello, World!" }).ConfigureAwait(false);
+
+        Console.WriteLine("Started SiteA. Sent message to SiteB");
+
+        Console.ReadLine();
+
+        await endpoint.Stop().ConfigureAwait(false);
+    }
+}
