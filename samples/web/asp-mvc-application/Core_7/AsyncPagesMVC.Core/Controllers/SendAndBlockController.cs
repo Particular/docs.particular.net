11efce6315ddf4463b7e987b30a43aeaff76fe13
Michał Wójcik <wojcik.mike@gmail.com>
2018-05-22 16:35:00 +0200

    Asp mvc application netcore (#3791)
    * Adding sample for Asp mvc on netcore
    
    * Fixing the dependencies
    
    * Cleanup
    
    * Enable c# 7 and 7.1 for R#

---
+++ samples/web/asp-mvc-application/Core_7/AsyncPagesMVC.Core/Controllers/SendAndBlockController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using NServiceBus;
+
+public class SendAndBlockController :
+    Controller
+{
+    IEndpointInstance endpoint;
+
+    public SendAndBlockController(IEndpointInstance endpoint)
+    {
+        this.endpoint = endpoint;
+    }
+
+    [HttpGet]
+    public ActionResult Index()
+    {
+        ViewBag.Title = "SendAndBlock";
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Index(string textField)
+    {
+        ViewBag.Title = "SendAndBlock";
+
+        if (!int.TryParse(textField, out var number))
+        {
+            return View();
+        }
+
+        #region SendAndBlockController
+
+        var command = new Command
+        {
+            Id = number
+        };
+
+
+        var sendOptions = new SendOptions();
+        sendOptions.SetDestination("Samples.Mvc.Server");
+        var status = endpoint.Request<ErrorCodes>(command, sendOptions).GetAwaiter().GetResult();
+
+        return IndexCompleted(Enum.GetName(typeof(ErrorCodes), status));
+
+        #endregion
+    }
+
+    public ActionResult IndexCompleted(string errorCode)
+    {
+        ViewBag.Title = "SendAsync";
+        ViewBag.ResponseText = errorCode;
+        return View("Index");
+    }
+}
