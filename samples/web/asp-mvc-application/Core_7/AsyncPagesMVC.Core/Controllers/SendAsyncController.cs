b95fc4f928e0ce3c8bb3795efc18adb29bffb524
Michał Wójcik <wojcik.mike@gmail.com>
2018-05-22 11:59:48 +0200

    Adding sample for Asp mvc on netcore

---
+++ samples/web/asp-mvc-application/Core_7/AsyncPagesMVC.Core/Controllers/SendAsyncController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NServiceBus;
+
+public class SendAsyncController :
+    Controller
+{
+    IEndpointInstance endpoint;
+
+    public SendAsyncController(IEndpointInstance endpoint)
+    {
+        this.endpoint = endpoint;
+    }
+
+    [HttpGet]
+    public ActionResult Index()
+    {
+        ViewBag.Title = "SendAsync";
+        return View("Index");
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Index(string textField)
+    {
+        if (!int.TryParse(textField, out var number))
+        {
+            return View("Index");
+        }
+        #region AsyncController
+        var command = new Command
+                        {
+                            Id = number
+                        };
+
+        var sendOptions = new SendOptions();
+        sendOptions.SetDestination("Samples.Mvc.Server");
+
+        var status = await endpoint.Request<ErrorCodes>(command, sendOptions)
+            .ConfigureAwait(false);
+
+        return IndexCompleted(Enum.GetName(typeof(ErrorCodes), status));
+
+        #endregion
+    }
+
+    public ActionResult IndexCompleted(string errorCode)
+    {
+        ViewBag.Title = "SendAsync";
+        ViewBag.ResponseText = errorCode;
+        return View("Index");
+    }
+}
