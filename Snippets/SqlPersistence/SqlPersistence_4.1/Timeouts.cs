c59ef6d0f8968e95f7b87a3c5df2a7009f98c9fb
SzymonPobiega <szymon.pobiega@gmail.com>
2018-05-23 14:46:41 +0200

    Add snipprt for timeouts.

---
+++ Snippets/SqlPersistence/SqlPersistence_4.1/Timeouts.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.SqlClient;
+using NServiceBus;
+using NServiceBus.Persistence.Sql;
+
+class Timeouts
+{
+    void Connection(EndpointConfiguration endpointConfiguration)
+    {
+        #region SqlPersistenceTimeoutConnection
+
+        var connection = @"Data Source=.\SqlExpress;Initial Catalog=timeouts;Integrated Security=True";
+        var persistence = endpointConfiguration.UsePersistence<SqlPersistence>();
+        //TODO: uncomment after updating dependency to 4.1
+        //var timeouts = persistence.TimeoutSettings();
+        //timeouts.ConnectionBuilder(
+        //    connectionBuilder: () =>
+        //    {
+        //        return new SqlConnection(connection);
+        //    });
+
+        #endregion
+    }
+}
