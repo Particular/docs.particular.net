17c83ffb0131ec60212cf5b6826cdec03979aaed
Michał Wójcik <wojcik.mike@gmail.com>
2018-05-23 15:55:38 +0200

    Adding sample for sending from mvc in netcore

---
+++ samples/web/send-from-mvc-controller/Core_7/WebApplication.Core/Controllers/DefaultController.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NServiceBus;
+
+namespace WebApplication.Core.Controllers
+{
+    public class DefaultController : Controller
+    {
+        IEndpointInstance endpoint;
+
+        public DefaultController(IEndpointInstance endpoint)
+        {
+            this.endpoint = endpoint;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public async Task<ActionResult> Send()
+        {
+            await endpoint.Send("Samples.Mvc.Endpoint", new MyMessage())
+                .ConfigureAwait(false);
+            return RedirectToAction("Index", "Default");
+        }
+
+    }
+}
---
+++ samples/web/send-from-mvc-controller/Core_7/WebApplication.Core/Startup.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NServiceBus;
+
+namespace WebApplication.Core
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            var builder = new ContainerBuilder();
+
+            builder.Populate(services);
+
+            IEndpointInstance endpoint = null;
+            builder.Register(c => endpoint)
+                .As<IEndpointInstance>()
+                .SingleInstance();
+
+            var container = builder.Build();
+
+            var endpointConfiguration = new EndpointConfiguration("Samples.MvcInjection.WebApplication");
+            
+            endpointConfiguration.UseContainer<AutofacBuilder>(
+                customizations: customizations =>
+                {
+                    customizations.ExistingLifetimeScope(container);
+                });
+            endpointConfiguration.UsePersistence<LearningPersistence>();
+            endpointConfiguration.UseTransport<LearningTransport>();
+
+            endpoint = Endpoint.Start(endpointConfiguration).GetAwaiter().GetResult();
+
+            return new AutofacServiceProvider(container);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Default}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
