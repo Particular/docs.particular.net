527d8697fa7121337bf02034b456d1e08cf1f26b
SzymonPobiega <szymon.pobiega@gmail.com>
2018-05-23 14:44:51 +0200

    Move the OutboxSettings snippet to correct project. Add doco for separate subscription connection.

---
+++ Snippets/SqlPersistence/SqlPersistence_4.1/Subscriptions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.SqlClient;
+using NServiceBus;
+using NServiceBus.Persistence.Sql;
+
+class Subscriptions
+{
+    void Connection(EndpointConfiguration endpointConfiguration)
+    {
+        #region SqlPersistenceSubscriptionConnection
+
+        var connection = @"Data Source=.\SqlExpress;Initial Catalog=subscriptions;Integrated Security=True";
+        var persistence = endpointConfiguration.UsePersistence<SqlPersistence>();
+        var subscriptions = persistence.SubscriptionSettings();
+        //TODO: uncomment after updating dependency to 4.1
+        //subscriptions.ConnectionBuilder(
+        //    connectionBuilder: () =>
+        //    {
+        //        return new SqlConnection(connection);
+        //    });
+
+        #endregion
+    }
+}
---
+++ persistence/sql/subscriptions_connection_sqlpersistence_[4.1,).partial.md
@@ -0,0 +1,5 @@
+## Connection
+
+Subscription persister can be configured to use a connection builder e.g. when subscription tables need to be created in a different database.
+
+snippet: SqlPersistenceSubscriptionConnection  
