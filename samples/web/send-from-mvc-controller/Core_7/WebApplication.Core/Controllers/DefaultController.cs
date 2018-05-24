cd0882de8d2e0bcc777d8ab551911b9badd55ea5
andreasohlund <andreas.ohlund@particular.net>
2018-05-24 09:37:24 +0200

    Cleanup

--- samples/web/send-from-mvc-controller/Core_7/WebApplication.Core/Controllers/DefaultController.cs
+++ samples/web/send-from-mvc-controller/Core_7/WebApplication.Core/Controllers/DefaultController.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using NServiceBus;
