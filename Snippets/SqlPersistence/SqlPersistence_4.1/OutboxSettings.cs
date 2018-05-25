2748f81d469eba6abf96f85225e37accf2f31091
Weronika Łabaj <weronika.labaj@particular.net>
2018-05-25 13:19:01 +0200

    no message

--- Snippets/SqlPersistence/SqlPersistence_4.1/OutboxSettings.cs
+++ Snippets/SqlPersistence/SqlPersistence_4.1/OutboxSettings.cs
@@ -1,32 +1,29 @@
-﻿namespace SqlPersistence_4
+﻿using System;
+using NServiceBus;
+using NServiceBus.InMemory.Outbox;
+
+class OutboxSettings
 {
-    using System;
-    using NServiceBus;
-    using NServiceBus.InMemory.Outbox;
-
-    class OutboxSettings
+    void OutboxSettingsEx1(EndpointConfiguration endpointConfiguration)
     {
-        void OutboxSettingsEx1(EndpointConfiguration endpointConfiguration)
-        {
-            #region SqlPersistenceOutboxSettings
+        #region SqlPersistenceOutboxSettings
 
-            var outboxSettings = endpointConfiguration.EnableOutbox();
+        var outboxSettings = endpointConfiguration.EnableOutbox();
 
-            outboxSettings.KeepDeduplicationDataFor(TimeSpan.FromDays(6));
-            outboxSettings.TimeToKeepDeduplicationData(TimeSpan.FromMinutes(15));
+        outboxSettings.KeepDeduplicationDataFor(TimeSpan.FromDays(6));
+        outboxSettings.TimeToKeepDeduplicationData(TimeSpan.FromMinutes(15));
 
-            #endregion
-        }
+        #endregion
+    }
 
-        void OutboxSettingsEx2(EndpointConfiguration endpointConfiguration)
-        {
-            #region SqlPersistenceOutboxDisableCleanup
+    void OutboxSettingsEx2(EndpointConfiguration endpointConfiguration)
+    {
+        #region SqlPersistenceOutboxDisableCleanup
 
-            var outboxSettings = endpointConfiguration.EnableOutbox();
+        var outboxSettings = endpointConfiguration.EnableOutbox();
 
-            outboxSettings.DisableCleanup();
+        outboxSettings.DisableCleanup();
 
-            #endregion
-        }
+        #endregion
     }
 }
