a206cc5d02e18d0ec3750f2d25d199b73d9a7946
Mauro Servienti <mauro.servienti@gmail.com>
2018-05-30 18:01:55 +0200

    Lesson 1 after solution

---
+++ tutorials/nservicebus-sagas/1-getting-started/Solution/Sales/PlaceOrderHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using Messages;
+using NServiceBus;
+using NServiceBus.Logging;
+
+#pragma warning disable 162
+
+namespace Sales
+{
+    using System;
+
+    public class PlaceOrderHandler :
+        IHandleMessages<PlaceOrder>
+    {
+        static ILog log = LogManager.GetLogger<PlaceOrderHandler>();
+        static Random random = new Random();
+
+        public Task Handle(PlaceOrder message, IMessageHandlerContext context)
+        {
+            log.Info($"Received PlaceOrder, OrderId = {message.OrderId}");
+
+            // This is normally where some business logic would occur
+
+            // Uncomment to test throwing a systemic exception
+            //throw new Exception("BOOM");
+
+            // Uncomment to test throwing a transient exception
+            //if (random.Next(0, 5) == 0)
+            //{
+            //    throw new Exception("Oops");
+            //}
+
+            var orderPlaced = new OrderPlaced
+            {
+                OrderId = message.OrderId
+            };
+            return context.Publish(orderPlaced);
+        }
+    }
+}
