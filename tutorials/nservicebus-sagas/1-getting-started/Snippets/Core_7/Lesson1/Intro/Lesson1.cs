0b19d6db984cb82654bdad737d2bbef333e0bfed
Mauro Servienti <mauro.servienti@gmail.com>
2018-05-30 18:01:27 +0200

    Lesson 1 snippets

---
+++ tutorials/nservicebus-sagas/1-getting-started/Snippets/Core_7/Lesson1/Intro/Lesson1.cs
@@ -0,0 +1,31 @@
+﻿namespace Core_7.Lesson1.Intro
+{
+    using NServiceBus;
+    using NServiceBus.Logging;
+    using System.Threading.Tasks;
+
+#pragma warning disable 1998
+
+    #region EmptyShippingPolicy
+    public class ShippingPolicy :
+        IHandleMessages<OrderPlaced>,
+        IHandleMessages<OrderBilled>
+    {
+        static ILog log = LogManager.GetLogger<ShippingPolicy>();
+
+        public Task Handle(OrderPlaced message, IMessageHandlerContext context)
+        {
+            log.Info($"Received OrderPlaced, OrderId = {message.OrderId}");
+            return Task.CompletedTask;
+        }
+
+        public Task Handle(OrderBilled message, IMessageHandlerContext context)
+        {
+            log.Info($"Received OrderBilled, OrderId = {message.OrderId}");
+            return Task.CompletedTask;
+        }
+    }
+    #endregion
+
+#pragma warning restore 1998
+}
