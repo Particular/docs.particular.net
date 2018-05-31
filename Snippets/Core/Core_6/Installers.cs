a674fcbcd56821e49945b20f215585b9c2eb2b37
Ramon Smits <ramon.smits@gmail.com>
2018-05-31 13:31:20 +0200

    Update Installers.cs

--- Snippets/Core/Core_6/Installers.cs
+++ Snippets/Core/Core_6/Installers.cs
@@ -34,7 +34,7 @@
             if (runInstallers)
             {
                 endpointConfiguration.EnableInstallers();
-                // this will run the installers
+                // this will run the installers but not start the instance!
                 await Endpoint.Create(endpointConfiguration)
                     .ConfigureAwait(false);
                 return; // Exit application
