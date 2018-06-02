7dbdc31881c721ab5db3d97137a34e7399a924eb
Simon Cropp <simon.cropp@gmail.com>
2018-06-02 19:01:52 +1000

    add validation

---
+++ Snippets/DataAnnotations/DataAnnotations_1/MyMessage.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+using NServiceBus;
+
+#region DataAnnotations_message
+public class MyMessage : IMessage
+{
+    [Required]
+    public string Content { get; set; }
+}
+#endregion
---
+++ Snippets/FluentValidation/FluentValidation_1/MyMessage.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using FluentValidation;
+using NServiceBus;
+
+#region FluentValidation_message
+public class MyMessage :
+    IMessage
+{
+    [Required]
+    public string Content { get; set; }
+}
+
+public class MyMessageValidator :
+    AbstractValidator<MyMessage>
+{
+    public MyMessageValidator()
+    {
+        RuleFor(_ => _.Content)
+            .NotEmpty();
+    }
+}
+#endregion
