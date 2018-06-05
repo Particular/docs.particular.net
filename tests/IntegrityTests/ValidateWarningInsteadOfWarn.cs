a2322c47ad1a0dcd691c78c7313352d7a2fe949c
danielmarbach <daniel.marbach@openplace.net>
2018-06-05 14:23:12 +0200

    IntegrityTests which fails when WARN used instead of WARNING

---
+++ tests/IntegrityTests/ValidateWarningInsteadOfWarn.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace IntegrityTests
+{
+    public class ValidateWarningInsteadOfWarn
+    {
+        [Test]
+        public void VerifyWarningIsUsedInsteadOfWarn()
+        {
+            new TestRunner("*.md", "Found WARN instead of WARNING.")
+                .Run(path => !File.ReadLines(path).Any(l => l.Contains("WARN:")));
+        }
+    }
+}
