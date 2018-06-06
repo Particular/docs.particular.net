9d7e69bc39977fb2a5ca5fafe8f0f0c0cd952835
Simon Cropp <simon.cropp@gmail.com>
2018-06-07 07:25:43 +1000

    start claims doco

---
+++ Snippets/SqlHttpPassthrough/SqlHttpPassthrough_1/AppendClaimsToMessageHeaders.cs
@@ -0,0 +1,47 @@
+﻿using System.Data.SqlClient;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using NServiceBus.SqlServer.HttpPassthrough;
+using NServiceBus.Transport.SqlServerNative;
+
+public class AppendClaimsToMessageHeaders
+{
+
+    public void WithPrefix(IServiceCollection services)
+    {
+        #region AppendClaimsToMessageHeaders_WithPrefix
+
+        var configuration = new PassthroughConfiguration(
+            connectionFunc: OpenConnection,
+            callback: Callback);
+        configuration.AppendClaimsToMessageHeaders(headerPrefix: "Claim.");
+        services.AddSqlHttpPassthrough(configuration);
+
+        #endregion
+    }
+
+    public void Default(IServiceCollection services)
+    {
+        #region AppendClaimsToMessageHeaders
+
+        var configuration = new PassthroughConfiguration(
+            connectionFunc: OpenConnection,
+            callback: Callback);
+        configuration.AppendClaimsToMessageHeaders();
+        services.AddSqlHttpPassthrough(configuration);
+
+        #endregion
+    }
+
+    Task<Table> Callback(HttpContext httpContext, PassthroughMessage passthroughMessage)
+    {
+        return null;
+    }
+
+    Task<SqlConnection> OpenConnection(CancellationToken cancellation)
+    {
+        return null;
+    }
+}
