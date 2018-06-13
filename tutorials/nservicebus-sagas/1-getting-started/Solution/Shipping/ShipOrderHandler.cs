5badf2cec97128b5f90019eb2c2f2f00674e4a03
Mauro Servienti <mauro.servienti@gmail.com>
2018-06-13 07:32:26 +0200

    Add missing ShipOrderHandler to final solution

---
+++ tutorials/nservicebus-sagas/1-getting-started/Solution/Shipping/ShipOrderHandler.cs
@@ -0,0 +1,18 @@
+﻿using Messages;
+using NServiceBus;
+using NServiceBus.Logging;
+using System.Threading.Tasks;
+
+namespace Shipping
+{
+    class ShipOrderHandler : IHandleMessages<ShipOrder>
+    {
+        static ILog log = LogManager.GetLogger<ShipOrderHandler>();
+
+        public Task Handle(ShipOrder message, IMessageHandlerContext context)
+        {
+            log.Info($"Order [{message.OrderId}] - Succesfully shipped.");
+            return Task.CompletedTask;
+        }
+    }
+}
