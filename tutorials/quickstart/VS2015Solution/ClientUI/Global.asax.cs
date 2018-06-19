b9b605846d0224fee8dbc67d794aa036b130efc6
Yves Goeleven <yves@goeleven.com>
2018-06-19 16:48:48 +0200

    vs2015 solution clientUI project converted to web

---
+++ tutorials/quickstart/VS2015Solution/ClientUI/Global.asax.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace ClientUI
+{
+    using System.Threading.Tasks;
+    using ClientUI.Controllers;
+    using Messages;
+    using NServiceBus;
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        IEndpointInstance _endpointInstance;
+
+        protected void Application_Start()
+        {
+            AsyncStart().GetAwaiter().GetResult();
+        }
+        
+        async Task AsyncStart()
+        {
+            var endpointConfiguration = new EndpointConfiguration("ClientUI");
+
+            var transport = endpointConfiguration.UseTransport<LearningTransport>();
+
+            var routing = transport.Routing();
+            routing.RouteToEndpoint(typeof(PlaceOrder), "Sales");
+
+            _endpointInstance = await Endpoint.Start(endpointConfiguration)
+                .ConfigureAwait(false);
+
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ControllerBuilder.Current.SetControllerFactory(new InjectEndpointInstanceIntoController(_endpointInstance));
+        }
+
+        protected void Application_End()
+        {
+            _endpointInstance?.Stop().GetAwaiter().GetResult();
+        }
+    }
+}
