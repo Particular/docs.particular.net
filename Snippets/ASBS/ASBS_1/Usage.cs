714dd166f4e9d53984715411270092ac2e1545a7
Sean Feldman <feldman.sean@gmail.com>
2018-06-21 11:03:25 -0600

    Add ASBS v1 snippet and prerelease marker

---
+++ Snippets/ASBS/ASBS_1/Usage.cs
@@ -0,0 +1,14 @@
+﻿using NServiceBus;
+
+class Usage
+{
+    Usage(EndpointConfiguration endpointConfiguration)
+    {
+        #region azure-service-bus-for-dotnet-standard
+
+        var transport = endpointConfiguration.UseTransport<AzureServiceBusTransport>();
+        transport.ConnectionString("Endpoint=sb://[NAMESPACE].servicebus.windows.net/;SharedAccessKeyName=[KEYNAME];SharedAccessKey=[KEY]");
+
+        #endregion
+    }
+}
