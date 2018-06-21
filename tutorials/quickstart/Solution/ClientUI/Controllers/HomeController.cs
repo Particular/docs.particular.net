5b568011c04a97c2866dee7e7b3cabcddbc25469
Yves Goeleven <yves@goeleven.com>
2018-06-21 13:15:23 +0200

    converted console app to web ui

---
+++ tutorials/quickstart/Solution/ClientUI/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Web.Mvc;
+
+namespace ClientUI.Controllers
+{
+    using System;
+    using System.Threading.Tasks;
+    using Messages;
+    using NServiceBus;
+
+    public class HomeController : Controller
+    {
+        IEndpointInstance _endpointInstance;
+
+        public HomeController(IEndpointInstance endpointInstance)
+        {
+            _endpointInstance = endpointInstance;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PlaceOrder()
+        {
+            var command = new PlaceOrder
+            {
+                OrderId = Guid.NewGuid().ToString()
+            };
+
+            // Send the command
+            await _endpointInstance.Send(command)
+                .ConfigureAwait(false);
+
+            return View();
+        }
+    }
+}
