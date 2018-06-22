d79a82786c19034fe8e3306c373934fd1728086b
Adam Ralph <adam@adamralph.com>
2018-06-22 16:27:06 +0200

    add RabbitMQ delayed delivery sample

---
+++ samples/rabbitmq/delayed-delivery/Rabbit_5/Sample/MyHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+
+public class MyHandler :
+    IHandleMessages<MyMessage>
+{
+    static readonly ILog log = LogManager.GetLogger<MyHandler>();
+
+    public Task Handle(MyMessage message, IMessageHandlerContext context)
+    {
+        log.Info("Hello from MyHandler");
+        return Task.CompletedTask;
+    }
+}
---
+++ samples/rabbitmq/delayed-delivery/Rabbit_5/Sample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+
+class Program
+{
+    static readonly ILog log = LogManager.GetLogger<Program>();
+
+    static async Task Main()
+    {
+        Console.Title = "Samples.RabbitMQ.DelayedDelivery";
+
+        var endpointConfiguration = new EndpointConfiguration("Samples.RabbitMQ.DelayedDelivery");
+
+        var transport = endpointConfiguration.UseTransport<RabbitMQTransport>();
+        transport.UseConventionalRoutingTopology();
+        transport.ConnectionString("host=localhost");
+
+        endpointConfiguration.EnableInstallers();
+        endpointConfiguration.UsePersistence<InMemoryPersistence>();
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration);
+
+        var sendOptions = new SendOptions();
+        sendOptions.RouteToThisEndpoint();
+        sendOptions.DelayDeliveryWith(TimeSpan.FromSeconds(7));
+
+        log.Info("Sending message to be delivered in 7 seconds...");
+        await endpointInstance.Send(new MyMessage(), sendOptions);
+
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+
+        await endpointInstance.Stop();
+    }
+}
