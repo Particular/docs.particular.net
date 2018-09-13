eada1e3b9d4750580df7f48c19ca23881599cc5b
Andreas Öhlund <andreas.ohlund@particular.net>
2018-09-13 13:14:08 +0200

    Add log testing snippet

---
+++ Snippets/Testing/Testing_7/Handlers/LoggingTest.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using NServiceBus.Logging;
+using NServiceBus.Testing;
+using NUnit.Framework;
+
+[TestFixture]
+public class LoggingTests
+{
+    #region LoggerTesting
+    [Test]
+    public async Task ShouldLogCorrectly()
+    {
+        var logStatements = new StringBuilder();
+
+        LogManager.Use<TestingLoggerFactory>()
+            .WriteTo(new StringWriter(logStatements));
+
+        var handler = new MyHandlerWithLogging();
+
+        await handler.Handle(new MyRequest(), new TestableMessageHandlerContext())
+            .ConfigureAwait(false);
+
+        StringAssert.Contains("Some log message", logStatements.ToString());
+    }
+    #endregion
+}
