068632a20e0e505740abc4f362d9839e60c8cf7d
seanfarmar <s.farmar@gmail.com>
2018-09-18 19:15:48 +0100

    first draft - ASB to ASBS Bridge

---
+++ samples/azure/asb-asbs-bridge/ASB_8/Bridge/Program.cs
@@ -0,0 +1,78 @@
+﻿extern alias TransportASBS;
+using NServiceBus;
+using System;
+using System.Threading.Tasks;
+using System.Transactions;
+
+using NServiceBus.Bridge;
+using NServiceBus.Configuration.AdvancedExtensibility;
+using NServiceBus.Serialization;
+using NServiceBus.Settings;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.Title = "Samples.Azure.ServiceBus.Bridge";
+
+        var connectionString = Environment.GetEnvironmentVariable("AzureServiceBus.ConnectionString");
+        if (string.IsNullOrWhiteSpace(connectionString))
+        {
+            throw new Exception("Could not read the 'AzureServiceBus.ConnectionString' environment variable. Check the sample prerequisites.");
+        }
+
+        // TODO: ASB requires serializer to be registered.
+        // Currently, there's no way to specify serialization for the bridged endpoints
+        // endpointConfiguration.UseSerialization<T>();
+
+        #region bridge-general-configuration
+
+        var bridgeConfiguration = Bridge
+            .Between<TransportASBS::NServiceBus.AzureServiceBusTransport>("Bridge-ASBS", transport =>
+            {
+                transport.ConnectionString(connectionString);
+                var settings = transport.GetSettings();
+                var serializer = Tuple.Create(new NewtonsoftSerializer() as SerializationDefinition, new SettingsHolder());
+                settings.Set("MainSerializer", serializer);
+            })
+            .And<NServiceBus.AzureServiceBusTransport>("Bridge-ASB", transport =>
+            {
+                transport.ConnectionString(connectionString);
+                transport.UseForwardingTopology();
+                var settings = transport.GetSettings();
+                var serializer = Tuple.Create(new NewtonsoftSerializer() as SerializationDefinition, new SettingsHolder());
+                settings.Set("MainSerializer", serializer);
+            });
+
+        bridgeConfiguration.AutoCreateQueues();
+        bridgeConfiguration.UseSubscriptionPersistence(new InMemorySubscriptionStorage());
+
+        #endregion
+
+        #region suppress-transaction-scope-for-asb
+
+        bridgeConfiguration.InterceptForwarding(async (queue, message, dispatch, forward) =>
+        {
+            using (new TransactionScope(TransactionScopeOption.Suppress, TransactionScopeAsyncFlowOption.Enabled))
+            {
+                await forward(dispatch).ConfigureAwait(false);
+            }
+        });
+
+        #endregion
+
+        #region bridge-execution
+
+        var bridge = bridgeConfiguration.Create();
+
+        await bridge.Start().ConfigureAwait(false);
+
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+
+        await bridge.Stop().ConfigureAwait(false);
+
+
+        #endregion
+    }
+}
