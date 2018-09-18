1a719eff3ef9fdb8585992eb4db3dd0b1c89d53d
seanfarmar <s.farmar@gmail.com>
2018-09-18 19:19:11 +0100

    first draft - ASB and ASBS side by side

---
+++ samples/azure/asb-and-asbs-side-by-side/ASBAndASBSSideBySide/ASBAndASBSSideBySide/SideBySide.cs
@@ -0,0 +1,21 @@
+﻿namespace ASBAndASBSSideBySide
+{
+    extern alias TransportASBS; //  Edit The Project And See The Reference 
+    using NServiceBus;
+
+    public class SideBySide
+    {
+        public SideBySide()
+        {
+            var configurationASB = new EndpointConfiguration("ASB");
+            var transportASB = configurationASB.UseTransport<NServiceBus.AzureServiceBusTransport>();
+
+            transportASB.Queues().EnableBatchedOperations(true);
+
+            var configurationASBS = new EndpointConfiguration("ASBS");
+            var transportASBS = configurationASBS.UseTransport<TransportASBS::NServiceBus.AzureServiceBusTransport>();
+
+            transportASBS.PrefetchMultiplier(1);
+        }
+    }
+}
---
+++ samples/azure/asb-and-asbs-side-by-side/sample.md
@@ -0,0 +1,24 @@
+---
+title: ASBS to ASB Side By Side
+reviewed: 2017-10-17
+component: ASB, ASBS
+related:
+- transports/azure-service-bus
+- nservicebus/bridge
+---
+
+
+TBD...
+
+NOTE:
+```
+  <Target Name="ChangeAliasesOfNugetRefs" BeforeTargets="FindReferenceAssembliesForReferences;ResolveReferences">
+    <ItemGroup>
+      <ReferencePath Condition="'%(FileName)' == 'NServiceBus.Transport.AzureServiceBus'">
+        <Aliases>TransportASBS</Aliases>
+      </ReferencePath>
+    </ItemGroup>
+  </Target>
+ ```
+
+
