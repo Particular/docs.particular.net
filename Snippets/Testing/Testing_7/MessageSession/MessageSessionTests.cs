b3bd0f8de2aa6cc8a38572b996db50917885ab67
Andreas Öhlund <andreas.ohlund@particular.net>
2018-09-19 15:45:47 +0200

    Add section on testing IMessageSession

---
+++ Snippets/Testing/Testing_7/MessageSession/MessageSessionTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Testing;
+using NUnit.Framework;
+
+[TestFixture]
+public class MessageSessionTests
+{
+    #region TestMessageSessionSend
+    [Test]
+    public async Task ShouldLogCorrectly()
+    {
+        var testableSession = new TestableMessageSession();
+
+        var someThingThatUseTheMessageSession = new SomeThingThatUseTheMessageSession(testableSession);
+
+        await someThingThatUseTheMessageSession.DoSomething();
+
+        Assert.AreEqual(1, testableSession.SentMessages.Length);
+        Assert.IsInstanceOf<MyResponse>(testableSession.SentMessages[0].Message);
+    }
+    #endregion
+
+    class SomeThingThatUseTheMessageSession
+    {
+        IMessageSession session;
+
+        public SomeThingThatUseTheMessageSession(IMessageSession session)
+        {
+            this.session = session;
+        }
+
+        public Task DoSomething()
+        {
+            return session.Send(new MyMessage());
+        }
+    }
+    class MyMessage : IMessage
+    {
+    }
+}
--- nservicebus/testing/index_content_testing_(,6).partial.md
+++ nservicebus/testing/index_content_testing_(,6).partial.md
@@ -1,4 +1,3 @@
-
 ### Arrange-Act-Assert style
 
 Arrange-Act-Assert style testing is preferred but not supported in this version. For more information about the fluent style refer to [Fluent style testing](/nservicebus/testing/fluent.md).
