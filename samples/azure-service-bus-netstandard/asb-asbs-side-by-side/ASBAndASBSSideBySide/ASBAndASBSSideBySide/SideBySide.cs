8d7d7c414ef95270fe3254435ab0c4bc6575b3f9
Mauro Servienti <mauro.servienti@gmail.com>
2018-09-21 15:26:44 +0200

    swap transport alias from ASBS to ASB

--- samples/azure-service-bus-netstandard/asb-asbs-side-by-side/ASBAndASBSSideBySide/ASBAndASBSSideBySide/SideBySide.cs
+++ samples/azure-service-bus-netstandard/asb-asbs-side-by-side/ASBAndASBSSideBySide/ASBAndASBSSideBySide/SideBySide.cs
@@ -1,21 +1,29 @@
 ﻿namespace ASBAndASBSSideBySide
 {
-    extern alias TransportASBS; //  Edit The Project And See The Reference 
+    //  Edit The Project And See The Reference 
+    #region transport-asb-alias-definition
+    extern alias TransportASB;
     using NServiceBus;
+    using TransportASB::NServiceBus;
+    #endregion
 
     public class SideBySide
     {
         public SideBySide()
         {
+            #region transport-asb-alias-usage
+
             var configurationASB = new EndpointConfiguration("ASB");
-            var transportASB = configurationASB.UseTransport<NServiceBus.AzureServiceBusTransport>();
+            var transportASB = configurationASB.UseTransport<TransportASB::NServiceBus.AzureServiceBusTransport>();
 
             transportASB.Queues().EnableBatchedOperations(true);
 
             var configurationASBS = new EndpointConfiguration("ASBS");
-            var transportASBS = configurationASBS.UseTransport<TransportASBS::NServiceBus.AzureServiceBusTransport>();
+            var transportASBS = configurationASBS.UseTransport<NServiceBus.AzureServiceBusTransport>();
 
-            // transportASBS.PrefetchMultiplier(1);
+            transportASBS.PrefetchMultiplier(1);
+
+            #endregion
         }
     }
 }
