7a2ee30404dc26bfd362324971b4e8a00ca7fc16
Mauro Servienti <mauro.servienti@gmail.com>
2018-09-21 15:39:10 +0200

    code reorg and snippets definition

--- samples/azure-service-bus-netstandard/asb-asbs-side-by-side/ASBAndASBSSideBySide/ASBAndASBSSideBySide/SideBySide.cs
+++ samples/azure-service-bus-netstandard/asb-asbs-side-by-side/ASBAndASBSSideBySide/ASBAndASBSSideBySide/SideBySide.cs
@@ -3,18 +3,22 @@
     //  Edit The Project And See The Reference 
     #region transport-asb-alias-definition
     extern alias TransportASB;
+    #endregion
+
     using NServiceBus;
     using TransportASB::NServiceBus;
-    #endregion
 
     public class SideBySide
     {
         public SideBySide()
         {
+            var configurationASB = new EndpointConfiguration("ASB");
+
             #region transport-asb-alias-usage
 
-            var configurationASB = new EndpointConfiguration("ASB");
             var transportASB = configurationASB.UseTransport<TransportASB::NServiceBus.AzureServiceBusTransport>();
+
+            #endregion
 
             transportASB.Queues().EnableBatchedOperations(true);
 
@@ -22,8 +26,6 @@
             var transportASBS = configurationASBS.UseTransport<NServiceBus.AzureServiceBusTransport>();
 
             transportASBS.PrefetchMultiplier(1);
-
-            #endregion
         }
     }
 }
