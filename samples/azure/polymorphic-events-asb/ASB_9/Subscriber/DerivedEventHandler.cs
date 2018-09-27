c84a3050c969f6d5627698f4fe5a04b8643129db
Sean Feldman <feldman.sean@gmail.com>
2018-09-27 15:34:15 -0600

    polymorphic-events-asb sample

---
+++ samples/azure/polymorphic-events-asb/ASB_9/Subscriber/DerivedEventHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Events;
+using NServiceBus;
+using NServiceBus.Logging;
+
+public class DerivedEventHandler :
+    IHandleMessages<DerivedEvent>
+{
+    static ILog log = LogManager.GetLogger<DerivedEventHandler>();
+
+    public Task Handle(DerivedEvent message, IMessageHandlerContext context)
+    {
+        log.Info($"Received DerivedEvent. EventId: {message.EventId} Data: {message.Data}");
+        return Task.CompletedTask;
+    }
+}
