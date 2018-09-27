e9f43986585b98f7d1cf1818b9f2caa21c1f7a77
Sean Feldman <feldman.sean@gmail.com>
2018-09-27 15:24:01 -0600

    native-integration-asb sample

---
+++ samples/azure/native-integration-asb/ASB_9/Shared/NativeMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NServiceBus;
+
+#region NativeMessage
+
+public class NativeMessage :
+    IMessage
+{
+    public string Content { get; set; }
+    public DateTime SendOnUtc { get; set; }
+}
+
+#endregion
