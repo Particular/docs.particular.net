f99c3374f6413f565704c6c50b6bf7fec7951818
Sean Feldman <SeanFeldman@users.noreply.github.com>
2018-09-27 15:46:55 -0600

    ASB v9 doco (#4029)
    * Upgrade guide for ASB 8 to 9 + update to client side batching
    
    Update upgrade guide
    
    * aure-service-bus sample
    
    * azure-service-bus-long-running sample
    
    * azure-service-bus-msmq-bridge sample
    
    * custom-partitioning-asb sample
    
    * custom-sanitization-asb sample
    
    * native-integration-asb sample
    
    * performance-tuning-asb sample
    
    * polymorphic-events-asb sample
    
    * asbs-asb-side-by-side sample

---
+++ samples/azure/custom-sanitization-asb/ASB_9/Shared/SomeEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Shared.Messages.In.A.Deep.Nested.Namespace.Nested.Events
+{
+    using NServiceBus;
+
+    #region SomeEvent
+
+    public class SomeEvent :
+        IEvent
+    {
+        public Guid EventId { get; set; }
+    }
+
+    #endregion
+}
