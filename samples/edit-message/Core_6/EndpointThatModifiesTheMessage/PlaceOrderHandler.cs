8f4f15a221b6b2693785477ff5e15ff60a34fd7a
Michał Wójcik <wojcik.mike@gmail.com>
2018-10-23 12:22:28 +0200

    Adding v6 version of the sample

---
+++ samples/edit-message/Core_6/EndpointThatModifiesTheMessage/PlaceOrderHandler.cs
@@ -0,0 +1,21 @@
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+
+public class PlaceOrderHandler :
+    IHandleMessages<PlaceOrder>
+{
+    static ILog log = LogManager.GetLogger<PlaceOrderHandler>();
+
+    public Task Handle(PlaceOrder message, IMessageHandlerContext context)
+    {
+        log.Info($"EndpointThatModifyTheMessage has received PlaceOrder command with OrderId {message.OrderId} and UserId {message.UserId}.");
+        var properUserId = 1;
+        log.Info($"The message will be modified. Old value UserId:{message.UserId} => UserId:{properUserId} and send to it's originating endpoint");
+        message.UserId = properUserId;
+
+        var options = new SendOptions();
+        options.SetDestination("Samples.EditMessage.Subscriber");
+        return context.Send(message, options);
+    }
+}
---
+++ samples/edit-message/Core_6/EndpointThatModifiesTheMessage/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Features;
+
+static class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.EditMessage.EndpointThatModifyTheMessage";
+        var endpointConfiguration = new EndpointConfiguration("Samples.EditMessage.EndpointThatModifyTheMessage");
+        endpointConfiguration.UsePersistence<InMemoryPersistence>();
+        endpointConfiguration.DisableFeature<TimeoutManager>();
+        var transport = endpointConfiguration.UseTransport<MsmqTransport>();
+
+        endpointConfiguration.SendFailedMessagesTo("error");
+        endpointConfiguration.AuditProcessedMessagesTo("audit");
+        endpointConfiguration.EnableInstallers();
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
