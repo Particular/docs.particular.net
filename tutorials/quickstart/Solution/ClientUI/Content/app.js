96c373cf5eb682f6b659cbfe29a0e22dbf53588c
David Boike <david.boike@gmail.com>
2018-11-13 15:33:42 -0600

    Separate app.js

---
+++ tutorials/quickstart/Solution/ClientUI/Content/app.js
@@ -0,0 +1,12 @@
+﻿(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-20451773-2');
+ga('set', 'allowAdFeatures', false);
+ga('set', 'transport', 'beacon');
+ga('send', 'event', 'QuickStart', 'Running', 'VS2017');
+window.sentMessage = function() {
+    ga('send', 'event', 'QuickStart', 'SentMessage', 'VS2017');
+};
+
