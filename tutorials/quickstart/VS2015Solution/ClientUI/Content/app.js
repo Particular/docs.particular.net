9aac994263d915fbfb48a6fc2ce780be056e69cd
David Boike <david.boike@gmail.com>
2018-11-16 08:46:45 -0600

    add trial form to Quick Start tutorial (#4121)
    https://github.com/Particular/DeveloperEducation/issues/2451

---
+++ tutorials/quickstart/VS2015Solution/ClientUI/Content/app.js
@@ -0,0 +1,112 @@
+﻿(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-20451773-2');
+ga('set', 'allowAdFeatures', false);
+ga('set', 'transport', 'beacon');
+
+(function ($, window) {
+    'use strict';
+    window.SOLUTION_VERSION = 'VS2017';
+
+    $(function () {
+
+        var gaEvt = function (category, action, label, callback) {
+            var cb = callback || $.noop;
+            window.ga('send', 'event', category, action, label, { hitCallback: cb });
+            if (!ga.create) {
+                cb();
+            }
+        };
+
+        gaEvt('QuickStart', 'Running', SOLUTION_VERSION);
+        window.sentMessage = function () {
+            gaEvt('QuickStart', 'SentMessage', SOLUTION_VERSION);
+        };
+
+        var form = $('#license-form'),
+            firstname = $('#firstname'),
+            lastname = $('#lastname'),
+            email = $('#email');
+
+        if (!form.length) {
+            return;
+        }
+
+        var isEmail = function (email) {
+            var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+            return regex.test(email);
+        };
+
+        gaEvt('QuickStart', 'DisplayedLicenseForm', SOLUTION_VERSION);
+
+        $("#submit-license").click(function (e) {
+
+            e.preventDefault();
+            var formOK = true;
+
+            var check = function (sel, additionalFn) {
+                if (sel.val() === "") {
+                    sel.attr('placeholder', 'Field is missing');
+                    formOK = false;
+                } else {
+                    sel.removeAttr('placeholder');
+                    additionalFn && additionalFn(sel);
+                }
+            };
+
+            check(firstname);
+            check(lastname);
+            check(email, function (sel) {
+                if (!isEmail(sel.val())) {
+                    sel.val("");
+                    sel.attr("placeholder", "Email isn't valid");
+                    formOK = false;
+                } else {
+                    sel.removeAttr("placeholder");
+                }
+            });
+
+            if (!formOK) {
+                return;
+            }
+
+            var postData = {
+                FirstName: firstname.val(),
+                LastName: lastname.val(),
+                Email: email.val(),
+                templateid: "FreeLicense"
+            };
+
+            if (window.NSB_VERSION && !!window.NSB_VERSION.match(/^\d+\.\d+\.\d+$/)) {
+                postData.NServiceBusVersion = window.NSB_VERSION;
+            }
+
+            gaEvt('QuickStart', 'SubmitLicenseForm', SOLUTION_VERSION);
+
+            $.ajax({
+                url: "https://api.particular.net/platform/licensetool/requestextension",
+                type: "POST",
+                data: postData,
+                complete: function (xhr, textStatus) {
+                    if (textStatus === "success") {
+
+                        form.append($('<input/>', { type: 'hidden', id: 'LeadCategory', name: 'LeadCategory', value: 'Download' }))
+                            .append($('<input/>', { type: 'hidden', id: 'LeadSource', name: 'LeadSource', value: 'QuickStartTutorial' }))
+                            .append($('<input/>', { type: 'hidden', name: 'version', value: 'V7' }));
+
+                        gaEvt('Action Performed', 'Trial Extension', 'Day 14', function () {
+                            form.submit();
+                        });
+                    }
+                    else {
+                        alert("Unable to send request due to exception. Please contact Particular at support@particular.net");
+                    }
+                }
+            });
+        });
+
+    });
+
+}(jQuery, window));
---
+++ tutorials/quickstart/VS2015Solution/ClientUI/GetNServiceBusInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Web.Mvc;
+using NServiceBus;
+
+public static class GetNServiceBusInfo
+{
+    public static MvcHtmlString OutputNServiceBusInfo(this HtmlHelper _)
+    {
+        var nsbAssembly = typeof(IEndpointInstance).Assembly;
+        var att = nsbAssembly.GetCustomAttributes(typeof(AssemblyFileVersionAttribute)).OfType<AssemblyFileVersionAttribute>().First();
+        var v = new Version(att.Version);
+        var script = $"<script>window.NSB_VERSION = '{v.Major}.{v.Minor}.{v.Build}';</script>";
+        return new MvcHtmlString(script);
+    }
+}
