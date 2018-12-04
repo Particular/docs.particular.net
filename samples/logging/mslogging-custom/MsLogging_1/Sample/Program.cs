8b67c9e82236fac41e4e38be0c31710e068d96cd
David Boike <david.boike@gmail.com>
2018-12-04 17:20:27 -0600

    Fix sample to work with 2.2

--- samples/logging/mslogging-custom/MsLogging_1/Sample/Program.cs
+++ samples/logging/mslogging-custom/MsLogging_1/Sample/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.ComponentModel.Design;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 using NServiceBus;
 using Microsoft.Extensions.Logging;
 using NServiceBus.Logging;
+using ILoggerFactory = Microsoft.Extensions.Logging.ILoggerFactory;
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
 
 class Program
 {
@@ -11,9 +15,18 @@ class Program
         Console.Title = "Samples.Logging.MsLoggingCustom";
 
         #region MsLoggingInCode
-        using (var loggerFactory = new LoggerFactory())
+
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddLogging(loggingBuilder =>
         {
-            loggerFactory.AddConsole(Microsoft.Extensions.Logging.LogLevel.Information);
+            loggingBuilder.AddFilter(level => level >= LogLevel.Information);
+            loggingBuilder.AddConsole();
+        });
+
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+
+        using (var loggerFactory = serviceProvider.GetService<ILoggerFactory>())
+        {
             var logFactory = LogManager.Use<MicrosoftLogFactory>();
             logFactory.UseMsFactory(loggerFactory);
 
