96a34ce6b39713855a24e8c341dc19563af2fe4a
Jens Pettersson <jens.pettersson@live.se>
2018-12-21 13:18:01 +0100

    Changed preprocessor directive to NETCOREAPP2_1 as the project target framework is netcoreapp2.1

--- samples/hosting/multi-hosting/Core_7/Sample/Program.cs
+++ samples/hosting/multi-hosting/Core_7/Sample/Program.cs
@@ -67,7 +67,7 @@ class Program
         // Exclude Instance2.dll and, by inference, include all other assemblies
         var scanner = endpointConfiguration.AssemblyScanner();
 
-#if NETCOREAPP2_0
+#if NETCOREAPP2_1
         scanner.ExcludeAssemblies("Instance2.Core");
 #else
         scanner.ExcludeAssemblies("Instance2");
@@ -85,7 +85,7 @@ class Program
     {
         var endpointConfiguration = new EndpointConfiguration("Samples.MultiHosting.Instance2");
         var scanner = endpointConfiguration.AssemblyScanner();
-#if NETCOREAPP2_0
+#if NETCOREAPP2_1
         scanner.ExcludeAssemblies("Instance1.Core");
 #else
         scanner.ExcludeAssemblies("Instance1");
