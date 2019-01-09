58240deaa96901a0001a04c2987360fd05b25785
Szymon Pobiega <szymon.pobiega@gmail.com>
2019-01-09 15:18:45 +0100

    Fix malformed messages sample (#4209)
    * Repair malformed message sample.
    
    * Remove invalid word
    
    * net core version
    
    * Update samples/servicecontrol/fix-messages/sample.md
    
    Co-Authored-By: SzymonPobiega <szymon.pobiega@gmail.com>
    
    * Add sample #2 for "Fixing messages" samples
    
    * Incorporate feedback and move to the Pipelines samples

---
+++ samples/pipeline/fix-messages-using-behavior/Core_7/Shared/SimpleMessage.cs
@@ -0,0 +1,7 @@
+﻿using NServiceBus;
+
+public class SimpleMessage :
+    IMessage
+{
+    public string Id { get; set; }
+}
---
+++ samples/servicecontrol/fix-messages/Core_7/MessageRepairingEndpoint/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "MessageRepairingEndpoint";
+        var endpointConfiguration = new EndpointConfiguration("FixMalformedMessages.MessageRepairingEndpoint");
+        endpointConfiguration.UseTransport<LearningTransport>();
+        endpointConfiguration.EnableInstallers();
+        endpointConfiguration.SendFailedMessagesTo("error");
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+
+        Console.WriteLine("Press 'Enter' to finish.");
+        Console.ReadLine();
+
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
