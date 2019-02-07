3176a0183e2c056cc72ec477ac7c7cee933b7347
Tim <bussmann.t@gmail.com>
2019-02-07 16:42:07 +0100

    add platform launcher to error handling sample

---
+++ samples/errorhandling/Core_7/PlatformLauncher/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PlatformLauncher.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "PlatformLauncher";
+            Particular.PlatformLauncher.Launch();
+        }
+    }
+}
