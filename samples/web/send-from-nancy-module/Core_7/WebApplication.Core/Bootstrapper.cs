4b45b50f66e36a8dadb32758da07f18e0c3869a4
michal0x17 <michal0x17@gmail.com>
2019-02-23 14:15:17 +0100

    add web project

---
+++ samples/web/send-from-nancy-module/Core_7/WebApplication.Core/Bootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using Nancy;
+using Nancy.TinyIoc;
+using NServiceBus;
+
+public class Bootstrapper :
+    DefaultNancyBootstrapper
+{
+    private readonly IMessageSession endpointInstance;
+
+	public Bootstrapper(IMessageSession messageSession)
+	{
+		this.endpointInstance = messageSession;
+	}	
+	
+	protected override void ConfigureApplicationContainer(TinyIoCContainer container)
+    {
+        base.ConfigureApplicationContainer(container);
+
+        // Register endpoint instance
+        container.Register<IMessageSession>(endpointInstance);
+    }
+}
