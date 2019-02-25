46cd635ec95996be6003bdf20c265f51e1d98b6e
Andreas Öhlund <andreas.ohlund@particular.net>
2019-02-25 15:37:44 +0100

    Update Program.cs

--- samples/web/send-from-nancy-module/Core_7/WebApplication.Core/Program.cs
+++ samples/web/send-from-nancy-module/Core_7/WebApplication.Core/Program.cs
@@ -7,12 +7,12 @@ namespace WebApplication.Core
     {
         public static void Main(string[] args)
         {
-			BuildWebHost(args).Run();
+            BuildWebHost(args).Run();
         }
-		
+
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .Build();		
+                .Build();
     }
 }
