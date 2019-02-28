6cfb0c349c3ea81402685dc3ce7326d800b7bd10
Bob Langley <bob.langley@particular.net>
2019-02-28 10:44:23 -0800

    Add example Sha1 deterministic Guid generator

---
+++ Snippets/Core/Core_All/Sha1DeterministicHash.cs
@@ -0,0 +1,28 @@
+﻿namespace Common
+{
+    #region SHA1DeterministicGuid
+
+    using System;
+    using System.Security.Cryptography;
+    using System.Text;
+
+    static class Sha1DeterministicGuid
+    {
+        public static Guid Create(params object[] data)
+        {
+            var inputBytes = Encoding.UTF8.GetBytes(string.Concat(data));
+
+            using (var provider = new SHA1Managed())
+            {
+                var guidBytes = new byte[16];
+
+                var hashBytes = provider.ComputeHash(inputBytes);
+
+                Array.ConstrainedCopy(hashBytes, 0, guidBytes, 0, 16);
+
+                return new Guid(guidBytes);
+            }
+        }
+    }
+    #endregion
+}
