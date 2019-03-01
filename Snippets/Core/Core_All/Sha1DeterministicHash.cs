63612108cc42796e72beaf7b82d631ac14e85326
David Boike <david.boike@gmail.com>
2019-03-01 13:12:26 -0600

    SHA1.Create()

--- Snippets/Core/Core_All/Sha1DeterministicHash.cs
+++ Snippets/Core/Core_All/Sha1DeterministicHash.cs
@@ -12,11 +12,12 @@
         {
             var inputBytes = Encoding.UTF8.GetBytes(string.Concat(data));
 
-            using (var provider = new SHA1Managed())
+            // Will create a FIPS-compliant provider when run on a FIPS-enabled system
+            using (var sha1 = SHA1.Create())
             {
                 var guidBytes = new byte[16];
 
-                var hashBytes = provider.ComputeHash(inputBytes);
+                var hashBytes = sha1.ComputeHash(inputBytes);
 
                 Array.ConstrainedCopy(hashBytes, 0, guidBytes, 0, 16);
 
