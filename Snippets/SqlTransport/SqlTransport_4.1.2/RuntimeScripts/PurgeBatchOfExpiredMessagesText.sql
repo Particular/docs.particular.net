dd592068e997859bc57b9921cc35f7448daa7e4c
Kyle Baley <kyle@baley.org>
2019-03-18 16:36:29 -0500

    Switch to SQL transport 4.2

--- Snippets/SqlTransport/SqlTransport_4.1.2/RuntimeScripts/PurgeBatchOfExpiredMessagesText.sql
+++
@@ -1,8 +0,0 @@
-startcode PurgeBatchOfExpiredMessagesTextSql
-
-DELETE FROM {0}
-WHERE RowVersion
-    IN (SELECT TOP (@BatchSize) RowVersion
-        FROM {0} WITH (NOLOCK)
-        WHERE Expires < GETUTCDATE())
-endcode
