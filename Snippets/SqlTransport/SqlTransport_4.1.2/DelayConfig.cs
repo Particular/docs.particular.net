7e588cbeb460446cf91b45ac6937e27f8ce1d452
Kyle Baley <kyle@baley.org>
2019-03-18 15:50:49 -0500

    Update SQL used to peek messages

---
+++ Snippets/SqlTransport/SqlTransport_4.1.2/DelayConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NServiceBus;
+using NServiceBus.Transport.SQLServer;
+
+class DelayConfig
+{
+    void ConfigurePeekDelay(EndpointConfiguration endpointConfiguration)
+    {
+        #region sqlserver-config-delay
+
+        var transport = endpointConfiguration.UseTransport<SqlServerTransport>();
+        transport.WithPeekDelay(TimeSpan.FromSeconds(5));
+
+        #endregion
+    }
+}
---
+++ Snippets/SqlTransport/SqlTransport_4.1.2/MultiSchema.cs
@@ -0,0 +1,57 @@
+﻿using NServiceBus;
+using NServiceBus.Transport.SQLServer;
+
+class MultiSchema
+{
+    void NonStandardSchema(EndpointConfiguration endpointConfiguration)
+    {
+        #region sqlserver-non-standard-schema
+
+        var transport = endpointConfiguration.UseTransport<SqlServerTransport>();
+        transport.DefaultSchema("myschema");
+
+        #endregion
+    }
+
+    void OtherEndpointConnectionParamsPull(EndpointConfiguration endpointConfiguration, IMessageSession messageSession)
+    {
+        #region sqlserver-multischema-config-for-queue
+
+        var transport = endpointConfiguration.UseTransport<SqlServerTransport>();
+
+        transport.UseSchemaForQueue(queueName: "myqueue", schema: "my_schema");
+        transport.UseSchemaForQueue(queueName: "myerror", schema: "sc");
+
+        #endregion
+
+        #region sqlserver-multischema-config-for-queue-send
+
+        messageSession.Send("myqueue", new MyMessage());
+
+        #endregion
+
+        #region sqlserver-multischema-config-for-queue-error
+
+        endpointConfiguration.SendFailedMessagesTo("myerror");
+
+        #endregion
+    }
+
+    void ConfigureCustomSchemaForEndpoint(EndpointConfiguration endpointConfiguration)
+    {
+        #region sqlserver-multischema-config-for-endpoint
+
+        var transport = endpointConfiguration.UseTransport<SqlServerTransport>();
+
+        var routing = transport.Routing();
+        routing.RouteToEndpoint(typeof(MyMessage), "MyEndpoint");
+
+        transport.UseSchemaForEndpoint(endpointName: "MyEndpoint", schema: "my_schema");
+
+        #endregion
+    }
+
+    class MyMessage
+    {
+    }
+}
