18ccb8ff2555f4f54f1dcab1c2f7a46f59cc9746
Andreas Öhlund <andreas.ohlund@particular.net>
2019-03-21 14:23:18 +0100

    Add snippet

---
+++ Snippets/Rabbit/Rabbit_5/AccessToBasicDeliveryEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus.Pipeline;
+using RabbitMQ.Client.Events;
+
+#region rabbitmq-config-access-to-event-args
+class AccessToBasicDeliveryEventArgs : Behavior<IIncomingContext>
+{
+    public override Task Invoke(IIncomingContext context, Func<Task> next)
+    {
+        var userIdOnBroker = context.Extensions.Get<BasicDeliverEventArgs>().BasicProperties.UserId;
+
+        //do something useful
+
+        return next();
+    }
+}
+#endregion
+
