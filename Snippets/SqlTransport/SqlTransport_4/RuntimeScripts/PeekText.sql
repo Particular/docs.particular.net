de0fa1e1daf44d00255b3af51a9ecbdc994e8ad4
Kyle Baley <kyle@baley.org>
2019-03-27 15:17:37 -0500

    Add separate SQL for SQL transport 4.1.1

--- Snippets/SqlTransport/SqlTransport_4/RuntimeScripts/PeekText.sql
+++ Snippets/SqlTransport/SqlTransport_4/RuntimeScripts/PeekText.sql
@@ -3,9 +3,3 @@ startcode PeekTextSql
 SELECT count(*) Id
 FROM (SELECT TOP {1} * FROM {0} WITH (READPAST)) as count_table;
 endcode
-
-startcode PeekTextSql411
-
-SELECT count(*) Id
-FROM {0} WITH (READPAST);
-endcode
