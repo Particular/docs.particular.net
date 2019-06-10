fa79987be4333dc81c9afab6a279bdf3e40dde20
David Boike <david.boike@gmail.com>
2019-06-10 14:17:41 -0500

    Adding the 6 version of Raven simple sample

---
+++ samples/ravendb/simple/Raven_6/Server/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using Raven.Client.Documents;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.RavenDB.Server";
+
+        #region Config
+
+        var endpointConfiguration = new EndpointConfiguration("Samples.RavenDB.Server");
+        using (var documentStore = new DocumentStore
+        {
+            Urls = new [] {"http://localhost:8080" },
+            Database = "RavenSimpleSample",
+        })
+        {
+            documentStore.Initialize();
+
+            var persistence = endpointConfiguration.UsePersistence<RavenDBPersistence>();
+            // Only required to simplify the sample setup
+            persistence.DoNotSetupDatabasePermissions();
+            persistence.SetDefaultDocumentStore(documentStore);
+
+            #endregion
+
+            var transport = endpointConfiguration.UseTransport<LearningTransport>();
+            transport.Transactions(TransportTransactionMode.SendsAtomicWithReceive);
+            endpointConfiguration.EnableInstallers();
+
+            var endpointInstance = await Endpoint.Start(endpointConfiguration)
+                .ConfigureAwait(false);
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+            await endpointInstance.Stop()
+                .ConfigureAwait(false);
+        }
+    }
+}
