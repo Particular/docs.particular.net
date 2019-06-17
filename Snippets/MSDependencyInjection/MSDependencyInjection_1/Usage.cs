67e844e175f0d8bf9cb99361169e84ab4bbd5b01
Mauro Servienti <mauro.servienti@gmail.com>
2019-06-17 18:17:28 +0200

    Snippet to configure a custom shared container

--- Snippets/MSDependencyInjection/MSDependencyInjection_1/Usage.cs
+++ Snippets/MSDependencyInjection/MSDependencyInjection_1/Usage.cs
@@ -4,7 +4,8 @@ using NServiceBus;
 class Usage
 {
     EndpointConfiguration endpointConfiguration = new EndpointConfiguration("endpoint");
-
+    IMessageSession endpoint = null;
+    
     #region msdependencyinjection
     public void ConfigureServices(IServiceCollection services)
     {
@@ -14,6 +15,28 @@ class Usage
         {
             customizations.ExistingServices(services);
         });
+    }
+    #endregion
+    
+    #region msdependencyinjectionaspnetcore
+    public IServiceProvider ConfigureServices(IServiceCollection services)
+    {
+        // register any services to IServiceCollection
+        
+        UpdateableServiceProvider container = null;
+        endpointConfiguration.UseContainer<ServicesBuilder>(customizations =>
+        {
+            customizations.ExistingServices(services);
+            customizations.ServiceProviderFactory(sc => 
+            {
+                container = new UpdateableServiceProvider(sc);
+                return container;
+            });
+        });
+
+        endpoint = Endpoint.Start(endpointConfiguration).GetAwaiter().GetResult();
+
+        return container;
     }
     #endregion
 }
