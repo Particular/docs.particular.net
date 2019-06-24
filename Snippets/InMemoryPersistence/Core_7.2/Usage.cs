179a3e13ba29f1855a8deaca5b61da1f37b9065b
Ramon Smits <ramon.smits@gmail.com>
2019-06-24 13:49:27 +0200

    content verification
    System.Exception : Some snippets exist that were not used in markdown. Or, the snippet may be used in some versions, but not all, in which case you need to remove the snippet from the version in which it's not needed. These are the unused snippets and the versions in which they weren't used: configuringinmemory (>= 7.2.0 && < 8.0.0)

--- Snippets/InMemoryPersistence/Core_7.2/Usage.cs
+++ Snippets/InMemoryPersistence/Core_7.2/Usage.cs
@@ -6,16 +6,6 @@
     {
         Usage(EndpointConfiguration endpointConfiguration)
         {
-            #region ConfiguringInMemory
-
-            endpointConfiguration.UsePersistence<InMemoryPersistence, StorageType.Sagas>();
-            endpointConfiguration.UsePersistence<InMemoryPersistence, StorageType.Subscriptions>();
-            endpointConfiguration.UsePersistence<InMemoryPersistence, StorageType.Timeouts>();
-            endpointConfiguration.UsePersistence<InMemoryPersistence, StorageType.Outbox>();
-            endpointConfiguration.UsePersistence<InMemoryPersistence, StorageType.GatewayDeduplication>();
-
-            #endregion
-
             #region GatewayDeduplicationCacheSize
 
             var persistence = endpointConfiguration.UsePersistence<InMemoryPersistence, StorageType.GatewayDeduplication>();
