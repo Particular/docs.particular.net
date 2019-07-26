0580faf8db537a0b75a7dab82d6b8d597b422a9f
David Boike <david.boike@gmail.com>
2019-07-26 14:06:22 -0500

    Rabbit Outbox sample code

---
+++ samples/outbox/rabbit/Core_7/Sample/MyHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+
+public class MyHandler :
+    IHandleMessages<MyMessage>
+{
+    static ILog log = LogManager.GetLogger<MyHandler>();
+
+    #region Handler
+    public async Task Handle(MyMessage message, IMessageHandlerContext context)
+    {
+        log.Info($"Processing MessageId {context.MessageId}");
+
+        var sqlPersistenceSession = context.SynchronizedStorageSession.SqlPersistenceSession();
+
+        using (var command = sqlPersistenceSession.Connection.CreateCommand())
+        {
+            command.CommandText = $"insert into BusinessObject (MessageId) values ('{context.MessageId}')";
+            command.Transaction = sqlPersistenceSession.Transaction;
+            await command.ExecuteNonQueryAsync();
+        }
+    }
+    #endregion
+}
