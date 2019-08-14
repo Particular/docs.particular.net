4874b78f12308044633cfde46686021608e6a5db
David Boike <david.boike@gmail.com>
2019-08-14 16:16:40 -0500

    Check Sdk attribute instead

--- tests/IntegrityTests/ProjectLangVersion.cs
+++ tests/IntegrityTests/ProjectLangVersion.cs
@@ -1,25 +1,26 @@
-﻿using System.Linq;
-using System.Xml.Linq;
+﻿using System.Xml.Linq;
 using System.Xml.XPath;
 using NUnit.Framework;
 
 namespace IntegrityTests
 {
-    using System;
-
     public class ProjectLangVersion
     {
        [Test]
         public void ShouldUseLangVersionLatest()
         {
+            // Also reflected in https://docs.particular.net/samples/#technology-choices-c-language-level
+            // And in /tools/projectStandards.linq
+
             new TestRunner("*.csproj", "VS2017 format project files must specify LangVersion=7.1 - This is the default in VS2019 but the property is needed to run correctly in VS2017.")
                 .IgnoreSnippets()
                 .Run(projectFilePath =>
                 {
                     var xdoc = XDocument.Load(projectFilePath);
 
-                    // Ignore VS2015 style projects
-                    if (xdoc.Root.Attribute("xmlns") != null)
+                    // Ignore non-Sdk (VS2015 and previous) style projects
+                    var sdk = xdoc.Root.Attribute("Sdk");
+                    if (sdk == null)
                     {
                         return true;
                     }
