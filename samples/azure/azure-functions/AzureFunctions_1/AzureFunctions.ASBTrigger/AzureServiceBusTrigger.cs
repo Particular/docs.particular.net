1651acc7bb60805dda6126718f48116ac5be8664
Tim Bussmann <timbussmann@users.noreply.github.com>
2019-08-21 16:20:13 +0200

    Add demo solution

---
+++ samples/azure/azure-functions/AzureFunctions_1/AzureFunctions.ASBTrigger/AzureServiceBusTrigger.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+using NServiceBus.AzureFunctions.AzureServiceBus;
+using NServiceBus.Serverless;
+
+namespace AzureFunctions.ASBTrigger
+{
+    public class AzureServiceBusTrigger
+    {
+        [FunctionName(nameof(AzureServiceBusTrigger))]
+        public static async Task Run(
+            [ServiceBusTrigger(queueName: "ASBTriggerQueue", Connection = "ASBConnectionString")]
+            Message message,
+            string messageId,
+            ILogger log,
+            ExecutionContext context)
+        {
+            log.LogInformation($"C# ServiceBus queue trigger function processed message: {messageId}");
+
+            //with caching
+            serverlessEndpoint = serverlessEndpoint ?? new ServerlessEndpoint(() =>
+            {
+                var config = new ConfigurationBuilder()
+                    .SetBasePath(context.FunctionAppDirectory)
+                    .AddJsonFile("local.settings.json", optional: false)
+                    .Build();
+
+                var azureServiceBusTriggerEndpoint = new AzureServiceBusTriggerEndpoint(context.FunctionName);
+                //TODO: package conflicts with json serializer with functions
+                azureServiceBusTriggerEndpoint.UseSerialization<NewtonsoftSerializer>();
+                var transport = azureServiceBusTriggerEndpoint.UseTransportForDispatch<AzureServiceBusTransport>();
+                var connectionString = config.GetValue<string>("Values:ASBConnectionString");
+                transport.ConnectionString(connectionString);
+                transport.Routing().RouteToEndpoint(typeof(ASQMessage), "ASQTriggerQueue");
+
+                return azureServiceBusTriggerEndpoint;
+            });
+
+            await serverlessEndpoint.Process(message);
+        }
+
+        static ServerlessEndpoint serverlessEndpoint;
+    }
+}
