79e7834e084c93c2eed01c96973b8581c4a82b1d
danielmarbach <daniel.marbach@openplace.net>
2019-08-30 12:34:32 +0200

    Remove noise and update to new alpha

--- samples/aws/lambda/sqs/SQSLambda_0.1/AwsLambda.SQSTrigger/Function.cs
+++ samples/aws/lambda/sqs/SQSLambda_0.1/AwsLambda.SQSTrigger/Function.cs
@@ -13,23 +13,10 @@ namespace AwsLambda.SQSTrigger
 {
     public class Function
     {
-        /// <summary>
-        /// Default constructor. This constructor is used by Lambda to construct the instance. When invoked in a Lambda environment
-        /// the AWS credentials will come from the IAM role associated with the function and the AWS region will be set to the
-        /// region the Lambda function is executed in.
-        /// </summary>
         public Function()
         {
         }
 
-
-        /// <summary>
-        /// This method is called for every Lambda invocation. This method takes in an SQS event object and can be used
-        /// to respond to SQS messages.
-        /// </summary>
-        /// <param name="evnt"></param>
-        /// <param name="context"></param>
-        /// <returns></returns>
         #region FunctionHandler
         public async Task FunctionHandler(SQSEvent evnt, ILambdaContext context)
         {
@@ -46,7 +33,7 @@ namespace AwsLambda.SQSTrigger
 
         private static readonly AwsLambdaSQSEndpoint serverlessEndpoint = new AwsLambdaSQSEndpoint(context =>
         {
-            var endpointConfiguration = new SQSTriggeredEndpointConfiguration("AwsLambdaSQSTrigger");
+            var endpointConfiguration = new AwsLambdaSQSEndpointConfiguration("AwsLambdaSQSTrigger");
             endpointConfiguration.UseSerialization<NewtonsoftSerializer>();
 
             var transport = endpointConfiguration.Transport;
