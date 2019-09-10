9b0f138a11aeb9216576702c05cbdb63d922306b
danielmarbach <daniel.marbach@openplace.net>
2019-09-10 12:35:56 +0200

    Scope logging

--- Snippets/Testing/Testing_7/Logging/LoggingTest.cs
+++ Snippets/Testing/Testing_7/Logging/LoggingTest.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using NServiceBus.Logging;
@@ -51,3 +52,40 @@ public class LoggingTests
 
     #endregion
 }
+
+#region LoggerTestingAmbient [7.2,8.0)
+[TestFixture]
+public class LoggingTestsAmbient
+{
+
+
+    StringBuilder logStatements;
+    IDisposable scope;
+    
+    [SetUp]
+    public void SetUp()
+    {
+        logStatements = new StringBuilder();
+        
+        scope = LogManager.Use<TestingLoggerFactory>()
+            .BeginScope(new StringWriter(logStatements));
+    }
+    
+    [TearDown]
+    public void Teardown()
+    {
+        scope.Dispose();
+    }
+    
+    [Test]
+    public async Task ShouldLogCorrectly()
+    {
+        var handler = new MyHandlerWithLogging();
+
+        await handler.Handle(new MyRequest(), new TestableMessageHandlerContext())
+            .ConfigureAwait(false);
+
+        StringAssert.Contains("Some log message", logStatements.ToString());
+    }
+}
+#endregion
