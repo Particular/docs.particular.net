f87792b326994d295aadd824935f9f5e66c509e3
Brandon Ording <bording@gmail.com>
2019-09-13 13:20:18 -0400

    Use ASP.NET Core App metapackage

--- samples/web/send-from-mvc-controller/Core_7/WebApplication.Core/Startup.cs
+++ samples/web/send-from-mvc-controller/Core_7/WebApplication.Core/Startup.cs
@@ -54,7 +54,6 @@ namespace WebApplication.Core
 
             if (env.IsDevelopment())
             {
-                app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
             }
 
