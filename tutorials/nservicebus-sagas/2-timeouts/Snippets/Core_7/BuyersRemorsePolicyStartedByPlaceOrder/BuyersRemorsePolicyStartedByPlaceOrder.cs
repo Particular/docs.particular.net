377181e214535d3e8f725a48f59946a0a39329b9
mauroservienti <mauro.servienti@gmail.com>
2019-10-30 18:19:28 +0100

    snippet: BuyersRemorsePolicyStartedByPlaceOrder

---
+++ tutorials/nservicebus-sagas/2-timeouts/Snippets/Core_7/BuyersRemorsePolicyStartedByPlaceOrder/BuyersRemorsePolicyStartedByPlaceOrder.cs
@@ -0,0 +1,43 @@
+﻿namespace Core_7.BuyersRemorsePolicyStartedByPlaceOrder
+{
+    using NServiceBus;
+    using NServiceBus.Logging;
+    using System.Threading.Tasks;
+
+    #region BuyersRemorsePolicyStartedByPlaceOrder
+
+    class BuyersRemorsePolicy : Saga<BuyersRemorseState>,
+        IAmStartedByMessages<PlaceOrder>
+    {
+        static ILog log = LogManager.GetLogger<BuyersRemorsePolicy>();
+
+        public Task Handle(PlaceOrder message, IMessageHandlerContext context)
+        {
+            log.Info($"Received PlaceOrder, OrderId = {message.OrderId}");
+
+            Data.OrderId = message.OrderId;
+            Data.CustomerId = message.CustomerId;
+
+            return Task.CompletedTask;
+        }
+
+        protected override void ConfigureHowToFindSaga(SagaPropertyMapper<BuyersRemorseState> mapper)
+        {
+            // TO BE IMPLEMENTED
+        }
+    }
+
+    #endregion
+
+    internal class PlaceOrder
+    {
+        public string OrderId { get; set; }
+        public string CustomerId { get; set; }
+    }
+
+    internal class BuyersRemorseState : ContainSagaData
+    {
+        public string OrderId { get; set; }
+        public string CustomerId { get; set; }
+    }
+}
