3e904e58048654bcaa36f5543fa748c5551375eb
mauroservienti <mauro.servienti@gmail.com>
2019-10-31 10:23:08 +0100

    snippet: BuyersRemorseTimeoutRequest

---
+++ tutorials/nservicebus-sagas/2-timeouts/Snippets/Core_7/BuyersRemorseTimeoutRequest/BuyersRemorseTimeoutRequest.cs
@@ -0,0 +1,48 @@
+﻿namespace Core_7.BuyersRemorseTimeoutRequest
+{
+    using NServiceBus;
+    using NServiceBus.Logging;
+    using System;
+    using System.Threading.Tasks;
+
+    class BuyersRemorsePolicy : Saga<BuyersRemorseState>
+    {
+        static ILog log = LogManager.GetLogger<BuyersRemorsePolicy>();
+
+        protected override void ConfigureHowToFindSaga(SagaPropertyMapper<BuyersRemorseState> mapper)
+        {
+            
+        }
+
+        #region BuyersRemorseTimeoutRequest
+
+        public async Task Handle(PlaceOrder message, IMessageHandlerContext context)
+        {
+            log.Info($"Received PlaceOrder, OrderId = {message.OrderId}");
+            Data.OrderId = message.OrderId;
+            Data.CustomerId = message.CustomerId;
+
+            log.Info($"Starting cool down period for order #{Data.OrderId}.");
+            await RequestTimeout(context, TimeSpan.FromSeconds(20), new BuyersRemorseIsOver());
+        }
+
+        #endregion
+    }
+
+    internal class BuyersRemorseIsOver
+    {
+
+    }
+
+    public class BuyersRemorseState : ContainSagaData
+    {
+        public string OrderId { get; set; }
+        public string CustomerId { get; set; }
+    }
+
+    internal class PlaceOrder
+    {
+        public string OrderId { get; set; }
+        public string CustomerId { get; set; }
+    }
+}
