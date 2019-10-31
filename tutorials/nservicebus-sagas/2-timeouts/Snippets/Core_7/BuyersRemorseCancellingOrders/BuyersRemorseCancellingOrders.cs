8dc61188f2535e857d0ad768e966996e5af6c854
mauroservienti <mauro.servienti@gmail.com>
2019-10-31 10:55:46 +0100

    snippet: BuyersRemorseCancellingOrders

---
+++ tutorials/nservicebus-sagas/2-timeouts/Snippets/Core_7/BuyersRemorseCancellingOrders/BuyersRemorseCancellingOrders.cs
@@ -0,0 +1,38 @@
+﻿namespace Core_7.BuyersRemorseCancellingOrders
+{
+    using NServiceBus;
+    using NServiceBus.Logging;
+    using System.Threading.Tasks;
+
+    class UICommands
+    {
+        static ILog log = LogManager.GetLogger<UICommands>();
+
+        async Task Execute(IMessageSession endpointInstance, string orderId, string[] parts)
+        {
+            #region BuyersRemorseCancellingOrders
+            switch (parts[0].ToLowerInvariant())
+            {
+                case "place":
+                    // ... 
+                    break;
+                case "cancel":
+                    {
+                        var command = new CancelOrder
+                        {
+                            OrderId = orderId
+                        };
+                        await endpointInstance.Send(command)
+                            .ConfigureAwait(false);
+                        log.Info($"Sent a correlated message to {orderId}");
+                        break;
+                    }
+            }
+            #endregion
+        }
+    }
+    internal class CancelOrder
+    {
+        public string OrderId { get; set; }
+    }
+}
