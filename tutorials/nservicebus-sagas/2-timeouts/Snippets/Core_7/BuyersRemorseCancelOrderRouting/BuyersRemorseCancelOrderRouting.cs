cdacc753dcb03752d011e9b818295c2392d21752
mauroservienti <mauro.servienti@gmail.com>
2019-10-31 10:49:54 +0100

    snippet: BuyersRemorseCancelOrderRouting

---
+++ tutorials/nservicebus-sagas/2-timeouts/Snippets/Core_7/BuyersRemorseCancelOrderRouting/BuyersRemorseCancelOrderRouting.cs
@@ -0,0 +1,21 @@
+﻿namespace Core_7.BuyersRemorseCancelOrderRouting
+{
+    using NServiceBus;
+
+    class EndpointConfig
+    {
+        public EndpointConfig()
+        {
+            var endpointConfiguration = new EndpointConfiguration("Fake");
+            var transport = endpointConfiguration.UseTransport<LearningTransport>();
+            var routing = transport.Routing();
+            #region BuyersRemorseCancelOrderRouting
+            routing.RouteToEndpoint(typeof(CancelOrder), "Sales");
+            #endregion
+        }
+    }
+
+    internal class CancelOrder
+    {
+    }
+}
