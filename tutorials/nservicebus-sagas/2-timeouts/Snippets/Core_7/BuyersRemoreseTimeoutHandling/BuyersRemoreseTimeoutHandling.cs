f199c7941effa3ecc6d512c4fcf6443a61efd120
mauroservienti <mauro.servienti@gmail.com>
2019-10-31 10:32:10 +0100

    snippet: BuyersRemoreseTimeoutHandling

---
+++ tutorials/nservicebus-sagas/2-timeouts/Snippets/Core_7/BuyersRemoreseTimeoutHandling/BuyersRemoreseTimeoutHandling.cs
@@ -0,0 +1,58 @@
+﻿namespace Core_7.BuyersRemoreseTimeoutHandling
+{
+    using NServiceBus;
+    using NServiceBus.Logging;
+    using System.Threading.Tasks;
+
+    #region BuyersRemoreseTimeoutHandling
+
+    class BuyersRemorsePolicy : Saga<BuyersRemorseState>,
+        IHandleTimeouts<BuyersRemorseIsOver>
+    {
+        static ILog log = LogManager.GetLogger<BuyersRemorsePolicy>();
+
+        protected override void ConfigureHowToFindSaga(SagaPropertyMapper<BuyersRemorseState> mapper)
+        {
+            //Omitted for clarity
+        }
+
+        public async Task Timeout(BuyersRemorseIsOver timeout, IMessageHandlerContext context)
+        {
+            log.Info($"Cooling down period for order #{Data.OrderId} has elapsed.");
+
+            var orderPlaced = new OrderPlaced
+            {
+                CustomerId = Data.CustomerId,
+                OrderId = Data.OrderId
+            };
+
+            // TODO: Save order state in database?
+
+            await context.Publish(orderPlaced);
+
+            MarkAsComplete();
+        }
+    }
+
+    #endregion
+
+    internal class OrderPlaced
+    {
+        public object CustomerId { get; set; }
+        public object OrderId { get; set; }
+    }
+
+    internal class BuyersRemorseIsOver
+    {
+    }
+
+    internal class PlaceOrder
+    {
+    }
+
+    internal class BuyersRemorseState : ContainSagaData
+    {
+        public object OrderId { get; set; }
+        public object CustomerId { get; set; }
+    }
+}
