7a95695a02528458fe47def1cb989dafbbdca478
mauroservienti <mauro.servienti@gmail.com>
2019-10-31 10:28:16 +0100

    snippet: BuyersRemoreseTimeoutClassDefinition

---
+++ tutorials/nservicebus-sagas/2-timeouts/Snippets/Core_7/BuyersRemoreseTimeoutClassDefinition/BuyersRemoreseTimeoutClassDefinition.cs
@@ -0,0 +1,32 @@
+﻿namespace Core_7.BuersRemoreseTimeoutClassDefinition
+{
+    #region BuyersRemoreseTimeoutClassDefinition
+
+    class BuyersRemorsePolicy : Saga<BuyersRemorseState>,
+        IAmStartedByMessages<PlaceOrder>
+    {
+        // ...
+    }
+
+    class BuyersRemorseIsOver
+    {
+    }
+
+    #endregion
+
+    internal class Saga<T>
+    {
+    }
+
+    internal class BuyersRemorseState
+    {
+    }
+
+    internal class PlaceOrder
+    {
+    }
+
+    internal interface IAmStartedByMessages<T>
+    {
+    }
+}
