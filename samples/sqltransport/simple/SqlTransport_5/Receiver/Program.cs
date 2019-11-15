cbec7def74c256f69436715f2dbec7cac3ca22c7
Mike Minutillo <mike.minutillo@particular.net>
2019-11-15 14:26:09 +0800

    Simple Sample

---
+++ samples/sqltransport/simple/SqlTransport_5/Receiver/Program.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Transport.SQLServer;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.SqlServer.SimpleReceiver";
+        var endpointConfiguration = new EndpointConfiguration("Samples.SqlServer.SimpleReceiver");
+        endpointConfiguration.SendFailedMessagesTo("error");
+        var transport = endpointConfiguration.UseTransport<SqlServerTransport>();
+        var connection = @"Data Source=.\SqlExpress;Database=SqlServerSimple;Integrated Security=True;Max Pool Size=100";
+        transport.ConnectionString(connection);
+        transport.Transactions(TransportTransactionMode.SendsAtomicWithReceive);
+        transport.NativeDelayedDelivery().DisableTimeoutManagerCompatibility();
+
+        endpointConfiguration.UsePersistence<InMemoryPersistence>();
+        endpointConfiguration.EnableInstallers();
+
+        SqlHelper.EnsureDatabaseExists(connection);
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        Console.WriteLine("Press any key to exit");
+        Console.WriteLine("Waiting for message from the Sender");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
