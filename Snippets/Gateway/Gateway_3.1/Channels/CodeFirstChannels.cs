c9bceeb0b178a7df02f463c5ccc818bf87bf94c2
Ramon Smits <ramon.smits@gmail.com>
2019-12-09 15:44:28 +0100

    CLoned 3.0 as 3.1

---
+++ Snippets/Gateway/Gateway_3.1/Channels/CodeFirstChannels.cs
@@ -0,0 +1,18 @@
+﻿using NServiceBus;
+
+public class CodeFirstChannels
+{
+    public CodeFirstChannels()
+    {
+        var endpointConfiguration = new EndpointConfiguration("MyEndpoint");
+
+        #region CodeFirstChannels
+
+        var gatewayConfig = endpointConfiguration.Gateway();
+
+        gatewayConfig.AddReceiveChannel("http://Headquarter.mycorp.com/", isDefault: true);
+        gatewayConfig.AddReceiveChannel("http://Headquarter.myotherdomain.com/", maxConcurrency: 10);
+
+        #endregion
+    }
+}
---
+++ Snippets/Gateway/Gateway_3.1/Usage.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Gateway;
+
+class Usage
+{
+    void GatewayConfiguration(EndpointConfiguration endpointConfiguration)
+    {
+        #region GatewayConfiguration
+
+        endpointConfiguration.Gateway();
+
+        #endregion
+    }
+
+    void GatewayDefaultRetryPolicyConfiguration(EndpointConfiguration endpointConfiguration)
+    {
+        #region GatewayDefaultRetryPolicyConfiguration
+
+        var gateway = endpointConfiguration.Gateway();
+        gateway.Retries(5, TimeSpan.FromMinutes(1));
+
+        #endregion
+    }
+
+    void GatewayCustomRetryPolicyConfiguration(EndpointConfiguration endpointConfiguration)
+    {
+        #region GatewayCustomRetryPolicyConfiguration
+
+        var gateway = endpointConfiguration.Gateway();
+        gateway.CustomRetryPolicy(
+            customRetryPolicy: (message, exception, currentRetry) =>
+            {
+                if (currentRetry > 4)
+                {
+                    return TimeSpan.MinValue;
+                }
+                return TimeSpan.FromSeconds(currentRetry*60);
+            });
+
+        #endregion
+    }
+
+    void GatewayDisableRetriesConfiguration(EndpointConfiguration endpointConfiguration)
+    {
+        #region GatewayDisableRetriesConfiguration
+
+        var gateway = endpointConfiguration.Gateway();
+        gateway.DisableRetries();
+
+        #endregion
+    }
+
+    async Task SendToSites(IEndpointInstance endpoint)
+    {
+        #region SendToSites
+
+        await endpoint.SendToSites(new[]
+        {
+            "SiteA",
+            "SiteB"
+        }, new MyMessage())
+        .ConfigureAwait(false);
+
+        #endregion
+    }
+
+    void TransactionTimeout(EndpointConfiguration endpointConfiguration)
+    {
+        #region GatewayCustomTransactionTimeout
+
+        var gateway = endpointConfiguration.Gateway();
+        gateway.TransactionTimeout(TimeSpan.FromSeconds(40));
+
+        #endregion
+    }
+
+    #region HeaderMutator
+
+    public class AddRequiredHeadersForGatewayBackwardsCompatibility : IMutateOutgoingTransportMessages
+    {
+        public Task MutateOutgoing(MutateOutgoingTransportMessageContext context)
+        {
+            var headers = context.OutgoingHeaders;
+            headers.Add(Headers.TimeToBeReceived, TimeSpan.MaxValue.ToString());
+            headers.Add(Headers.NonDurableMessage, false.ToString());
+
+            return Task.CompletedTask;
+        }
+    }
+
+    #endregion
+
+    class CustomChannelReceiver :
+        IChannelReceiver
+    {
+        public void Start(string address, int maxConcurrency, Func<DataReceivedOnChannelArgs, Task> dataReceivedOnChannel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class CustomChannelSender :
+        IChannelSender
+    {
+        public Task Send(string remoteAddress, IDictionary<string, string> headers, Stream data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
