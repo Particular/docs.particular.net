65bc79a386464d1f3281ffe57ea98986472f563b
Panos Pnevmatikatos <pnevmap@gmail.com>
2019-12-19 11:38:32 +0000

    Add Datadog metrics sample project (#4605)
    Co-Authored-By: Ramon Smits <ramon.smits@gmail.com>

---
+++ samples/logging/datadog/Core_7/Endpoint/DatadogFeature.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Features;
+using StatsdClient;
+
+class DatadogFeature : Feature
+{
+    MetricsOptions _metricsOptions;
+
+    Dictionary<string, string> _nameMapping = new Dictionary<string, string>
+    {
+        {"# of msgs successfully processed / sec", "Success"},
+        {"# of msgs pulled from the input queue /sec", "Fetched"},
+        {"# of msgs failures / sec", "Failures"},
+        {"Critical Time", "Critical Time"},
+        {"Processing Time", "Processing Time"},
+        {"Retries", "Retries"},
+    };
+
+    public DatadogFeature()
+    {
+        Defaults(settings =>
+        {
+            _metricsOptions = settings.EnableMetrics();
+        });
+        EnableByDefault();
+    }
+
+    protected override void Setup(FeatureConfigurationContext context)
+    {
+        #region setup-datadog-client
+
+        var dogstatsdConfig = new StatsdConfig
+        {
+            StatsdServerName = "127.0.0.1",
+            StatsdPort = 8125,
+        }; //Datadog agent default address, port
+
+        DogStatsd.Configure(dogstatsdConfig);
+
+        #endregion
+
+        #region datadog-enable-nsb-metrics
+
+        _metricsOptions.RegisterObservers(register: probeContext =>
+            {
+                foreach (var duration in probeContext.Durations)
+                {
+                    if (!_nameMapping.ContainsKey(duration.Name))
+                    {
+                        continue;
+                    }
+                    duration.Register((ref DurationEvent @event) =>
+                    {
+                        var statName = ComposeStatName(duration.Name, @event.MessageType);
+                        DogStatsd.Timer(statName, @event.Duration.TotalMilliseconds);
+                    });
+                }
+
+                foreach (var signal in probeContext.Signals)
+                {
+                    if (!_nameMapping.ContainsKey(signal.Name))
+                    {
+                        continue;
+                    }
+                    signal.Register((ref SignalEvent @event) =>
+                    {
+                        var statName = ComposeStatName(signal.Name, @event.MessageType);
+                        DogStatsd.Increment(statName);
+                    });
+                }
+            });
+
+        #endregion
+    }
+
+    private string ComposeStatName(string eventName, string messageType)
+    {
+        _nameMapping.TryGetValue(eventName, out var mappedName);
+        return $"{messageType.Split(',')[0]}-{mappedName}";
+    }
+}
---
+++ samples/logging/datadog/sample.md
@@ -0,0 +1,50 @@
+---
+title: Capture and visualize metrics using Datadog
+summary: Illustrates how to capture, store and visualize NServiceBus metrics in Datadog.
+component: Metrics
+---
+
+## Introduction
+
+This sample demonstrates how to capture, store and visualize NServiceBus metrics in Datadog, a monitoring solution for storing application performance data.
+
+This sample reports the following metrics to Datadog:
+
+ * Fetched messages per second 
+ * Failed messages per second
+ * Successful messages per second
+ * Critical time in seconds
+ * Processing time seconds
+ * Retries
+
+
+For a detailed explanation of these metrics refer to the [metrics captured section in the metrics documentation](/monitoring/metrics/definitions.md) section.
+
+
+## Prerequisites
+
+To run this sample, [create a Datadog account](https://app.datadoghq.com/signup), then download and run the Datadog agent.
+See the [Introduction to Datadog](https://docs.datadoghq.com/getting_started/) guide for information on how to get started with Datadog metrics.
+
+
+## Code overview
+
+The sample simulates messages load with a random 10% failure rate using the `LoadSimulator` class: LoadSimulator
+
+## Capturing metric values
+
+Setup Datadog client. Let' s assume the Datadog agent runs on default address and port.
+
+snippet: setup-datadog-client
+
+Custom observers need to be registered for the metric probes provided via `NServiceBus.Metrics`. This is configured in the `DatadogFeature`. The registered observers convert NServiceBus.Metric *Signals* to Datadog *Count* and NServiceBus.Metric *Durations* to Datadog *Timer* 
+
+snippet: datadog-enable-nsb-metrics
+
+## Dashboard
+![metrics summary](metrics-summary.png)
+![dashboard](dashboard.png)
+
+### Create
+
+Dashboards can be created by using Datadog.
