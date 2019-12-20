3cddf8c1a9801e0108312b6396c95f72e9c47236
danielmarbach <daniel.marbach@openplace.net>
2019-12-20 11:59:40 +0100

    Proper error samples

--- Snippets/Core/Core_7/Headers/Writers/HeaderWriterError.cs
+++ Snippets/Core/Core_7/Headers/Writers/HeaderWriterError.cs
@@ -26,13 +26,20 @@ namespace Core7.Headers.Writers
         [Test]
         public async Task Write()
         {
+            var errorIngestion = new EndpointConfiguration("error");
+            errorIngestion.SetTypesToScan(TypeScanner.NestedTypes<ErrorMutator>());
+            errorIngestion.EnableInstallers();
+            errorIngestion.UseTransport<LearningTransport>();
+            errorIngestion.Pipeline.Register(typeof(ErrorMutator),"Capture headers on failed messages");
+            await Endpoint.Start(errorIngestion);
+            
             var endpointConfiguration = new EndpointConfiguration(endpointName);
             endpointConfiguration.SendFailedMessagesTo("error");
             var typesToScan = TypeScanner.NestedTypes<HeaderWriterError>();
             endpointConfiguration.SetTypesToScan(typesToScan);
             endpointConfiguration.EnableInstallers();
             endpointConfiguration.UseTransport<LearningTransport>();
-            endpointConfiguration.Pipeline.Register(typeof(Mutator),"Capture headers on failed messages");
+            endpointConfiguration.Pipeline.Register(typeof(Mutator),"Capture headers on sent messages");
 
             var recoverability = endpointConfiguration.Recoverability();
             recoverability.Immediate(settings => settings.NumberOfRetries(1));
@@ -53,6 +60,22 @@ namespace Core7.Headers.Writers
             IMessage
         {
         }
+        
+        class ErrorMutator : Behavior<IIncomingPhysicalMessageContext>
+        {
+            public override Task Invoke(IIncomingPhysicalMessageContext context, Func<Task> next)
+            {
+                var headers = context.MessageHeaders;
+                var headerText = HeaderWriter.ToFriendlyString<HeaderWriterError>(headers);
+                headerText = BehaviorCleaner.CleanStackTrace(headerText);
+                headerText = StackTraceCleaner.CleanStackTrace(headerText);
+                SnippetLogger.Write(
+                    text: headerText,
+                    suffix: "Error");
+                ManualResetEvent.Set();
+                return Task.CompletedTask;
+            }
+        }
 
 
         class MessageHandler :
@@ -67,22 +90,6 @@ namespace Core7.Headers.Writers
 
         class Mutator : Behavior<IIncomingLogicalMessageContext>
         {
-            public Mutator(Notifications busNotifications)
-            {
-                var errorsNotifications = busNotifications.Errors;
-                errorsNotifications.MessageSentToErrorQueue += (sender, retry) =>
-                {
-                    var headers = retry.Headers;
-                    var headerText = HeaderWriter.ToFriendlyString<HeaderWriterError>(headers);
-                    headerText = BehaviorCleaner.CleanStackTrace(headerText);
-                    headerText = StackTraceCleaner.CleanStackTrace(headerText);
-                    SnippetLogger.Write(
-                        text: headerText,
-                        suffix: "Error");
-                    ManualResetEvent.Set();
-                };
-            }
-
             static bool hasCapturedMessage;
 
             public override Task Invoke(IIncomingLogicalMessageContext context, Func<Task> next)
