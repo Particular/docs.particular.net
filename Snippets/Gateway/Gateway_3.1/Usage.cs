0836f557842c53af42a8a3b36ed4e8cbe8f1ffe4
Ramon Smits <ramon.smits@gmail.com>
2019-12-20 14:46:12 +0100

    Removed snippets from 3.0

--- Snippets/Gateway/Gateway_3.1/Usage.cs
+++ Snippets/Gateway/Gateway_3.1/Usage.cs
@@ -1,100 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Threading.Tasks;
-using NServiceBus;
+﻿using NServiceBus;
 using NServiceBus.Gateway;
-using NServiceBus.MessageMutator;
 
 class Usage
 {
-    void GatewayConfiguration(EndpointConfiguration endpointConfiguration)
-    {
-        #region GatewayConfiguration
-
-        endpointConfiguration.Gateway();
-
-        #endregion
-    }
-
-    void GatewayDefaultRetryPolicyConfiguration(EndpointConfiguration endpointConfiguration)
-    {
-        #region GatewayDefaultRetryPolicyConfiguration
-
-        var gateway = endpointConfiguration.Gateway();
-        gateway.Retries(5, TimeSpan.FromMinutes(1));
-
-        #endregion
-    }
-
-    void GatewayCustomRetryPolicyConfiguration(EndpointConfiguration endpointConfiguration)
-    {
-        #region GatewayCustomRetryPolicyConfiguration
-
-        var gateway = endpointConfiguration.Gateway();
-        gateway.CustomRetryPolicy(
-            customRetryPolicy: (message, exception, currentRetry) =>
-            {
-                if (currentRetry > 4)
-                {
-                    return TimeSpan.MinValue;
-                }
-                return TimeSpan.FromSeconds(currentRetry*60);
-            });
-
-        #endregion
-    }
-
-    void GatewayDisableRetriesConfiguration(EndpointConfiguration endpointConfiguration)
-    {
-        #region GatewayDisableRetriesConfiguration
-
-        var gateway = endpointConfiguration.Gateway();
-        gateway.DisableRetries();
-
-        #endregion
-    }
-
-    async Task SendToSites(IEndpointInstance endpoint)
-    {
-        #region SendToSites
-
-        await endpoint.SendToSites(new[]
-        {
-            "SiteA",
-            "SiteB"
-        }, new MyMessage())
-        .ConfigureAwait(false);
-
-        #endregion
-    }
-
-    void TransactionTimeout(EndpointConfiguration endpointConfiguration)
-    {
-        #region GatewayCustomTransactionTimeout
-
-        var gateway = endpointConfiguration.Gateway();
-        gateway.TransactionTimeout(TimeSpan.FromSeconds(40));
-
-        #endregion
-    }
-
-    #region HeaderMutator
-
-    public class AddRequiredHeadersForGatewayBackwardsCompatibility : IMutateOutgoingTransportMessages
-    {
-        public Task MutateOutgoing(MutateOutgoingTransportMessageContext context)
-        {
-            var headers = context.OutgoingHeaders;
-            headers.Add(Headers.TimeToBeReceived, TimeSpan.MaxValue.ToString());
-            headers.Add(Headers.NonDurableMessage, false.ToString());
-
-            return Task.CompletedTask;
-        }
-    }
-
-    #endregion
-
     public void InMemoryDeduplicationConfigurationCacheSize(EndpointConfiguration endpointConfiguration)
     {
         #region InMemoryDeduplicationConfigurationCacheSize
@@ -108,28 +16,4 @@ class Usage
 
         #endregion
     }
-
-    class CustomChannelReceiver :
-        IChannelReceiver
-    {
-        public void Start(string address, int maxConcurrency, Func<DataReceivedOnChannelArgs, Task> dataReceivedOnChannel)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task Stop()
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-    class CustomChannelSender :
-        IChannelSender
-    {
-        public Task Send(string remoteAddress, IDictionary<string, string> headers, Stream data)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
 }
