b18121674ab9babab13c9329832adcf55e981848
danielmarbach <daniel.marbach@openplace.net>
2020-01-27 15:19:15 +0100

    Remove invalid words from snippet

--- Snippets/Host/Host_8/EndpointConfig.cs
+++ Snippets/Host/Host_8/EndpointConfig.cs
@@ -11,11 +11,11 @@
             //TODO: NServiceBus provides multiple durable storage options, including SQL Server, RavenDB, and Azure Storage Persistence.
             // Refer to the documentation for more details on specific options.
             endpointConfiguration.UsePersistence<LearningPersistence>();
-            // NServiceBus will move messages that fail repeatedly to a separate "error" queue. We recommend
-            // that you start with a shared error queue for all your endpoints for easy integration with ServiceControl.
+            // NServiceBus will move messages that fail repeatedly to a separate "error" queue. It is recommended
+            // to start with a shared error queue for all endpoints for easy integration with ServiceControl.
             endpointConfiguration.SendFailedMessagesTo("error");
-            // NServiceBus will store a copy of each successfully process message in a separate "audit" queue. We recommend
-            // that you start with a shared audit queue for all your endpoints for easy integration with ServiceControl.
+            // NServiceBus will store a copy of each successfully process message in a separate "audit" queue. It is recommended
+            // to start with a shared audit queue for all endpoints for easy integration with ServiceControl.
             endpointConfiguration.AuditProcessedMessagesTo("audit");
         }
     }
