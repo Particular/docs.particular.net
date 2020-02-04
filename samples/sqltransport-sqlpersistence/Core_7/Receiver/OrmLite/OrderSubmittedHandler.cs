c80d8ea4f81fc93487884b7c4217c24a77689fc5
danielmarbach <daniel.marbach@openplace.net>
2020-02-04 16:15:27 +0100

    Comment out OrmLite stuff for analysis by brandon

--- samples/sqltransport-sqlpersistence/Core_7/Receiver/OrmLite/OrderSubmittedHandler.cs
+++ samples/sqltransport-sqlpersistence/Core_7/Receiver/OrmLite/OrderSubmittedHandler.cs
@@ -2,20 +2,19 @@ using System.Threading.Tasks;
 using NServiceBus;
 using NServiceBus.Logging;
 using ServiceStack.OrmLite;
-using ServiceStack.OrmLite.SqlServer;
 
 namespace OrmLite
 {
-
-    public class OrderSubmittedHandler :
-        IHandleMessages<OrderSubmitted>
+    public class OrderSubmittedHandler/* :
+        IHandleMessages<OrderSubmitted>*/
     {
         static ILog log = LogManager.GetLogger<OrderSubmittedHandler>();
 
-        static OrderSubmittedHandler()
-        {
-            OrmLiteConfig.DialectProvider = new SqlServer2016OrmLiteDialectProvider();
-        }
+        // TODO: Fix
+        //static OrderSubmittedHandler()
+        //{
+        //    OrmLiteConfig.DialectProvider = new SqlServer2016OrmLiteDialectProvider();
+        //}
 
         public Task Handle(OrderSubmitted message, IMessageHandlerContext context)
         {
@@ -30,9 +29,7 @@ namespace OrmLite
                 Id = $"OrmLite-{message.OrderId}",
                 Value = message.Value,
             };
-            return OrmLiteWriteApiAsync.UpdateAsync(
-                dbConn: session.Connection,
-                obj: order,
+            return session.Connection.UpdateAsync(obj: order,
                 commandFilter: command =>
                 {
                     command.Transaction = session.Transaction;
