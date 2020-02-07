44ca61798ce3be38946d32f19c95f07302502653
mauroservienti <mauro.servienti@gmail.com>
2020-02-07 09:51:54 +0100

    Add RavenDB Gateway snippets

---
+++ Snippets/GatewayRavenDB/GatewayRavenDB_1/Usage.cs
@@ -0,0 +1,28 @@
+﻿using NServiceBus;
+using NServiceBus.Gateway.RavenDB;
+using Raven.Client.Documents;
+
+class Usage
+{
+
+    public void StandardUsage(EndpointConfiguration endpointConfiguration, string connectionString)
+    {
+        #region DefaultUsage
+        var gatewayConfiguration = new RavenGatewayDeduplicationConfiguration((builder, _) =>
+        {
+            var documentStore = new DocumentStore
+            {
+                Urls = new[] { "database-server-url" },
+                Database = "default-database-name"
+            };
+
+            documentStore.Initialize();
+
+            return documentStore;
+        });
+
+        var gatewaySettings = endpointConfiguration.Gateway(gatewayConfiguration);
+        #endregion
+    }
+}
+
