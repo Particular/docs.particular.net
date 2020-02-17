f85dda8640db41b6c89afc2d24994df00a21dc3f
Tim Bussmann <timbussmann@users.noreply.github.com>
2020-02-17 12:48:25 +0100

    add sample code

---
+++ samples/dependency-injection/Extensions.DependencyInjection/Extensions.DependencyInjection_1/Sample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using NServiceBus;
+
+static class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.NServiceBus.Extensions.DependencyInjection";
+
+        #region ContainerConfiguration
+
+        var endpointConfiguration = new EndpointConfiguration("Samples.NServiceBus.Extensions.DependencyInjection");
+
+        var containerSettings = endpointConfiguration.UseContainer(new DefaultServiceProviderFactory());
+        containerSettings.ServiceCollection.AddSingleton(new MyService());
+
+        #endregion
+
+        endpointConfiguration.UsePersistence<LearningPersistence>();
+        endpointConfiguration.UseTransport<LearningTransport>();
+
+        var endpoint = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+
+        var myMessage = new MyMessage();
+        await endpoint.SendLocal(myMessage)
+            .ConfigureAwait(false);
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpoint.Stop()
+            .ConfigureAwait(false);
+    }
+}
---
+++ samples/dependency-injection/Extensions.DependencyInjection/sample.md
@@ -0,0 +1,12 @@
+---
+title: NServiceBus.Extensions.DependencyInjection Usage
+component: Extensions.DependencyInjection
+reviewed: 2019-09-10
+tags:
+ - dependency injection
+related:
+ - nservicebus/dependency-injection
+ - nservicebus/dependency-injection/nservicebus-dependencyinjection
+---
+
+TODO
