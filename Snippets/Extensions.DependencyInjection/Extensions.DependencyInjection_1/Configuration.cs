9546c730d20c307776f656d922b5820f2a46a0e7
Tim Bussmann <timbussmann@users.noreply.github.com>
2020-02-20 15:48:20 +0100

    NServiceBus.Extensions.DependencyInjection documentation (#4691)
    * add new NServiceBus.Extensions.DependencyInjection component to docs
    
    * add doco page for the nservicebus.extensions.dependencyinjection package
    
    * add code snippets
    
    * mention Autofac package name
    
    * add section about property injection
    
    * add sample code
    
    * link sample
    
    * add sample text
    
    * fix typo
    
    * add prerelease markers
    
    * fix whitespace?
    
    * remove persistence configuration
    
    * Apply suggestions from code review
    
    Co-Authored-By: Sean Feldman <SeanFeldman@users.noreply.github.com>
    
    * add nuget alias
    
    * rename folder
    
    * rename file to match file specified in components
    
    * fix broken link
    
    * fixing more links
    
    * Update samples/dependency-injection/extensions-dependency-injection/sample.md
    
    Co-Authored-By: Andreas Öhlund <andreas.ohlund@particular.net>
    
    * remove forbidden words
    
    * move prerelease file
    
    Co-authored-by: Sean Feldman <SeanFeldman@users.noreply.github.com>
    Co-authored-by: Andreas Öhlund <andreas.ohlund@particular.net>

---
+++ Snippets/Extensions.DependencyInjection/Extensions.DependencyInjection_1/Configuration.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using NServiceBus;
+
+namespace Extensions.DependencyInjection_1
+{
+    class Configuration
+    {
+        void AccessServiceCollection(EndpointConfiguration endpointConfiguration)
+        {
+            #region settings-servicecollection
+
+            var containerSettings = endpointConfiguration.UseContainer(new DefaultServiceProviderFactory());
+            containerSettings.ServiceCollection.AddSingleton<MyService>();
+
+            #endregion
+        }
+
+        void CustomizeContainer(EndpointConfiguration endpointConfiguration)
+        {
+            #region settings-configurecontainer
+
+            // use Autofac:
+            var containerSettings = endpointConfiguration.UseContainer(new AutofacServiceProviderFactory());
+            containerSettings.ConfigureContainer(containerBuilder =>
+            {
+                // access Autofac native APIs here:
+                containerBuilder.RegisterType<MyService>().AsSelf().SingleInstance();
+                containerBuilder.RegisterAssemblyModules(Assembly.GetCallingAssembly());
+            });
+
+            #endregion
+        }
+
+        class MyService
+        {
+        }
+    }
+}
