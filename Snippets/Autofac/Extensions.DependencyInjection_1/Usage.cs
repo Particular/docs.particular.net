bc0280b9e8702f515bc7c60a35d4947a3365433c
Andreas Öhlund <andreas.ohlund@particular.net>
2020-02-25 16:21:54 +0100

    Add fake extension snippets

---
+++ Snippets/Autofac/Extensions.DependencyInjection_1/Usage.cs
@@ -0,0 +1,17 @@
+﻿class Usage
+{
+    Usage()
+    {
+        #region Autofac
+        //not used
+        #endregion
+
+    }
+
+    void Existing()
+    {
+        #region Autofac_Existing
+        //not used
+        #endregion
+    }
+}
