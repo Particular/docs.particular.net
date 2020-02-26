d26318b01303f1ab77565489a7533f8c24c5c0f7
Andreas Öhlund <andreas.ohlund@particular.net>
2020-02-26 09:20:54 +0100

    Add unity 10 snippets

---
+++ Snippets/Unity/Unity_10/Usage.cs
@@ -0,0 +1,37 @@
+﻿using NServiceBus;
+using Unity;
+
+class Usage
+{
+    Usage(EndpointConfiguration endpointConfiguration)
+    {
+#pragma warning disable CS0618 // Type or member is obsolete
+        #region Unity
+
+        endpointConfiguration.UseContainer<UnityBuilder>();
+
+        #endregion
+#pragma warning restore CS0618 // Type or member is obsolete
+    }
+
+    void Existing(EndpointConfiguration endpointConfiguration)
+    {
+#pragma warning disable CS0618 // Type or member is obsolete
+        #region Unity_Existing
+
+        var container = new UnityContainer();
+        container.RegisterInstance(new MyService());
+        endpointConfiguration.UseContainer<UnityBuilder>(
+            customizations: customizations =>
+            {
+                customizations.UseExistingContainer(container);
+            });
+
+        #endregion
+#pragma warning restore CS0618 // Type or member is obsolete
+    }
+
+    class MyService
+    {
+    }
+}
