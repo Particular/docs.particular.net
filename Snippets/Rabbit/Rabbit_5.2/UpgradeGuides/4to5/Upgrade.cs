a04b28c658355002941fc5ab2bd8c2150fe5d065
Sean Feldman <feldman.sean@gmail.com>
2020-04-15 11:35:06 -0600

    Adding 5.2 snippets project

---
+++ Snippets/Rabbit/Rabbit_5.2/UpgradeGuides/4to5/Upgrade.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+using NServiceBus;
+
+partial class Upgrade
+{
+    void UseDirectRoutingTopologyWithCustomConventions(EndpointConfiguration endpointConfiguration)
+    {
+        #region 4to5usedirectroutingtopology
+
+        var transport = endpointConfiguration.UseTransport<RabbitMQTransport>();
+        transport.UseDirectRoutingTopology(
+            routingKeyConvention: type => "myroutingkey",
+            exchangeNameConvention: () => "MyTopic");
+
+        #endregion
+    }
+}
