5100dae3e0570dbd3af7886039945d94893264fa
Sean Feldman <feldman.sean@gmail.com>
2020-04-15 15:37:41 -0600

    ASBS 1.5 snippets

---
+++ Snippets/ASBS/ASBS_1.5/AccessToNativeMessage.cs
@@ -0,0 +1,31 @@
+﻿namespace ASBS_1
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.ServiceBus;
+    using NServiceBus.Pipeline;
+
+    public class AccessToNativeMessage
+    {
+        #region access-native-message
+
+        class DoNotAttemptMessageProcessingIfMessageIsNotLocked : Behavior<ITransportReceiveContext>
+        {
+            public override Task Invoke(ITransportReceiveContext context, Func<Task> next)
+            {
+                var lockedUntilUtc = context.Extensions.Get<Message>().SystemProperties.LockedUntilUtc;
+
+                if (lockedUntilUtc <= DateTime.UtcNow)
+                {
+                    return next();
+                }
+
+                context.AbortReceiveOperation();
+
+                return Task.CompletedTask;
+            }
+        }
+
+        #endregion
+    }
+}
