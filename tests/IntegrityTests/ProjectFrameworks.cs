6dd6e907c2a99f83730a1f448b2aebf37067ae15
David Boike <david.boike@gmail.com>
2020-05-11 12:11:55 -0500

    Integrity tests settle for net48, add 3 tests

--- tests/IntegrityTests/ProjectFrameworks.cs
+++ tests/IntegrityTests/ProjectFrameworks.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
 using System.Xml.Linq;
 using System.Xml.XPath;
 using NUnit.Framework;
@@ -21,7 +25,7 @@ namespace IntegrityTests
                     }
 
                     var firstTargetFrameworksElement = xdoc.XPathSelectElement("/Project/PropertyGroup/TargetFrameworks");
-                    if(firstTargetFrameworksElement == null)
+                    if (firstTargetFrameworksElement == null)
                     {
                         return true;
                     }
@@ -29,6 +33,58 @@ namespace IntegrityTests
                     var tfmList = firstTargetFrameworksElement.Value.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
 
                     return tfmList.Length > 1;
+                });
+        }
+
+        static readonly string[] sdkProjectAllowedTfmList = new[] { "netcoreapp3.1", "netcoreapp2.1", "net48", "netstandard2.0" };
+        static readonly string[] nonSdkProjectAllowedFrameworkList = new[] { "v4.8" };
+
+        [Test]
+        public void RestrictTargetFrameworks()
+        {
+            var tfmString = string.Join(", ", sdkProjectAllowedTfmList);
+
+            new TestRunner("*.csproj", "Allowed target frameworks are: " + tfmString)
+                //.IgnoreSnippets()
+                .Run(projectFilePath =>
+                {
+                    var xdoc = XDocument.Load(projectFilePath);
+                    var xmlnsNode = xdoc.Root.Attribute("xmlns");
+                    if (xmlnsNode != null)
+                    {
+                        var mgr = new XmlNamespaceManager(new NameTable());
+                        mgr.AddNamespace("x", xmlnsNode.Value);
+
+                        foreach(var node in xdoc.XPathSelectElements("/x:Project/x:PropertyGroup/x:TargetFramework", mgr))
+                        {
+                            if(!nonSdkProjectAllowedFrameworkList.Contains(node.Value))
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        foreach (var node in xdoc.XPathSelectElements("/Project/PropertyGroup/TargetFramework"))
+                        {
+                            if (!sdkProjectAllowedTfmList.Contains(node.Value))
+                            {
+                                return false;
+                            }
+                        }
+                        foreach (var node in xdoc.XPathSelectElements("/Project/PropertyGroup/TargetFrameworks"))
+                        {
+                            foreach(var tfm in node.Value.Split(';'))
+                            {
+                                if (!sdkProjectAllowedTfmList.Contains(tfm))
+                                {
+                                    return false;
+                                }
+                            }
+                        }
+                    }
+
+                    return true;
                 });
         }
     }
---
+++ tests/IntegrityTests/SolutionStructure.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using NUnit.Framework;
+
+namespace IntegrityTests
+{
+    public class SolutionStructure
+    {
+        [Test]
+        public void OneSolutionPerDirectory()
+        {
+            var directoriesWithMoreThanOneSolution = Directory.GetFiles(TestSetup.DocsRootPath, "*.sln", SearchOption.AllDirectories)
+                .Select(slnPath => Path.GetDirectoryName(slnPath))
+                .GroupBy(dirPath => dirPath, StringComparer.OrdinalIgnoreCase)
+                .Where(group => group.Count() > 1)
+                .Select(group => group.Key)
+                .ToArray();
+
+            var errMsg = $"Only one solution file allowed per directory. These have multiple:" + Environment.NewLine
+                + string.Join(Environment.NewLine, directoriesWithMoreThanOneSolution);
+
+            Assert.AreEqual(0, directoriesWithMoreThanOneSolution.Length, errMsg);
+        }
+
+        [Test]
+        public void OneProjectPerDirectory()
+        {
+            var directoriesWithMoreThanOneProject = Directory.GetFiles(TestSetup.DocsRootPath, "*.*proj", SearchOption.AllDirectories)
+                .Select(projPath => Path.GetDirectoryName(projPath))
+                .GroupBy(dirPath => dirPath, StringComparer.OrdinalIgnoreCase)
+                .Where(group => group.Count() > 1)
+                .Select(group => group.Key)
+                .ToArray();
+
+            var errMsg = $"Only one project file allowed per directory. These have multiple:" + Environment.NewLine
+                + string.Join(Environment.NewLine, directoriesWithMoreThanOneProject);
+
+            Assert.AreEqual(0, directoriesWithMoreThanOneProject.Length, errMsg);
+        }
+    }
+}
