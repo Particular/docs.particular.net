f5d462628b8efd753c2389eb9913a3c4339f45e3
David Boike <david.boike@gmail.com>
2020-05-15 09:50:31 -0500

    Powershell to build samples & snippets

---
+++ tools/build-samples-and-snippets.ps1
@@ -0,0 +1,90 @@
+# Assumes working directory is Docs repository root folder
+
+function CombinePaths()
+{
+    param
+    (
+        [string] $first,
+        [string] $second
+    )
+    $combined = [System.IO.Path]::Combine($first, $second)
+    $fullPath = [System.IO.Path]::GetFullPath($combined)
+    return $fullPath
+}
+
+# Assumes that the current working directory is the root of the docs repo
+function Get-BuildSolutions
+{
+    # Retrieves the current branch name
+    $branch = git rev-parse --abbrev-ref HEAD
+
+    if($branch -eq "master")
+    {
+        # For master branch, build every solution file in repo
+        $result = Get-ChildItem -Filter *.sln -Recurse | sort LastWriteTime -Descending
+        return $result
+    }
+
+    # Fetch to update origin/master to the proper position
+    $ignoreGitFetchOutput = git fetch origin master
+
+    # `origin/master...HEAD` references commit where master & current branch diverged
+    # Just comparing to master will grab changes that occurred in master as well
+    $changes = git diff origin/master...HEAD --name-only
+    $result = @()
+    
+    foreach($change in $changes)
+    {
+        $file = CombinePaths $pwd.Path $change
+        $dir = CombinePaths $file ".." -Resolve
+
+        while($dir.Length -gt $pwd.Path.Length)
+        {
+            # Tracing up from a removed file may result in a directory that no longer exists
+            if(Test-Path $dir)
+            {
+                $dirSolutions = Get-ChildItem -Path $dir -Filter *.sln
+                if($dirSolutions.Length -gt 0)
+                {
+                    # May be separate netfx/netcore solutions in same directory
+                    foreach($sln in $dirSolutions)
+                    {
+                        $path = CombinePaths $dir $sln.Name
+                        # Convert path string to FileInfo-like item
+                        $item = Get-Item $path
+                        $result = $result + $item
+                    }
+                    
+                    # Found solution(s) for change, no need to keep navigating up
+                    break;
+                }
+            }
+            $dir = CombinePaths $dir ".."
+        }
+    }
+
+    return $result | Sort-Object | Get-Unique
+}
+ 
+$samples = Get-BuildSolutions
+foreach($sample in $samples) {
+ 
+    Set-Location -Path $sample.Directory.FullName
+    Get-ChildItem -inc bin,obj -rec | Remove-Item -rec -force
+    ("##teamcity[blockOpened name='Build Sample - {0}']" -f $sample.FullName)
+ 
+    dotnet nuget restore $sample.Name
+ 
+    $buildoutput =  (. msbuild $sample.Name /nr:true /verbosity:normal) 
+    $errors = $buildoutput | Select-String -pattern "^Build FAILED"
+ 
+    if ($errors) {
+       $buildoutput
+       ("##teamcity[buildProblem description='Build Failed - {0}']" -f $sample.FullName)
+    }
+    else {
+        "Build succeeded."
+    }
+ 
+    ("##teamcity[blockClosed name='Build Sample - {0}']" -f $sample.Name)
+}
