fc8fed1522b56735f5c28f017a5c9d0ac6e503c8
Tomek Masternak <tomasz.masternak@particular.net>
2020-06-23 12:22:40 +0200

    usecustomsqlconnection api

--- Snippets/SqlTransport/SqlTransport_6/CustomConnectionAndTransaction.cs
+++ Snippets/SqlTransport/SqlTransport_6/CustomConnectionAndTransaction.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Data.SqlClient;
 using System.Threading.Tasks;
+using System.Transactions;
 using NServiceBus;
 using NServiceBus.Transport.SqlServer;
 
@@ -35,6 +36,33 @@ class CustomConnectionAndTransaction
 
         #endregion
 
+        #region UseCustomSqlConnection
+
+        using (var scope = new TransactionScope(TransactionScopeOption.RequiresNew, TransactionScopeAsyncFlowOption.Enabled))
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                var sqlCommand = new SqlCommand(commandText, connection);
+
+                //Exectute SQL statement
+                sqlCommand.ExecuteNonQuery();
+
+                var options = new SendOptions();
+
+                options.UseCustomSqlConnection(connection);
+
+                //Send bunch of messages using the same transaction
+                await session.Send(new Message(), options);
+                await session.Send(new Message(), options);
+                await session.Send(new Message(), options);
+            }
+
+            scope.Complete();
+        }
+
+        #endregion
     }
 
     class Message { };
