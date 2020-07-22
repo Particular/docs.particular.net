b6ff4004ee8d3ca530882453f3fb524852b4f360
Mike Minutillo <mike.minutillo@particular.net>
2020-07-22 16:34:16 +0800

    Create message forwarding sample

---
+++ samples/routing/message-forwarding/Core_7/NServiceBus.MessageForwarding/ForwardProcessedMessagesBehavior.cs
@@ -0,0 +1,39 @@
+﻿using NServiceBus.Pipeline;
+using NServiceBus.Routing;
+using NServiceBus.Transport;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+#region forward-processed-messages-behavior
+class ForwardProcessedMessagesBehavior : ForkConnector<IIncomingPhysicalMessageContext, IRoutingContext>
+{
+    private string forwardingAddress;
+
+    public ForwardProcessedMessagesBehavior(string forwardingAddress)
+    {
+        this.forwardingAddress = forwardingAddress;
+    }
+
+    public override async Task Invoke(IIncomingPhysicalMessageContext context, Func<Task> next, Func<IRoutingContext, Task> fork)
+    {
+        var messageToForward = new OutgoingMessage(
+            context.Message.MessageId,
+            new Dictionary<string, string>(context.Message.Headers),
+            context.Message.Body);
+
+        await next()
+            .ConfigureAwait(false);
+
+        var forwardingRoutingStrategy = new UnicastRoutingStrategy(forwardingAddress);
+
+        var routingContext = new ForwardedRoutingContext(
+            messageToForward,
+            forwardingRoutingStrategy,
+            context);
+
+        await fork(routingContext)
+            .ConfigureAwait(false);
+    }
+}
+#endregion
---
+++ samples/routing/message-forwarding/Core_7/UpgradedDestination/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "UpgradedDestination";
+        var config = new EndpointConfiguration("UpgradedDestination");
+        config.UseTransport<LearningTransport>();
+
+        var endpoint = await Endpoint.Start(config)
+            .ConfigureAwait(false);
+
+        Console.WriteLine("Endpoint Started. Press any key to exit");
+
+        Console.ReadKey();
+
+        await endpoint.Stop()
+            .ConfigureAwait(false);
+    }
+}
