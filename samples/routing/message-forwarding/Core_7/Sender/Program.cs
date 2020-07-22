b99c7c3ae354475875479d5e758b852c521e5936
Mike Minutillo <mike.minutillo@particular.net>
2020-07-22 16:34:16 +0800

    Create message forwarding sample

---
+++ samples/routing/message-forwarding/Core_7/Sender/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using Messages;
+using NServiceBus;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Sender";
+
+        #region route-message-to-original-destination
+
+        var config = new EndpointConfiguration("Sender");
+        var transport = config.UseTransport<LearningTransport>();
+        var routing = transport.Routing();
+
+        routing.RouteToEndpoint(typeof(ImportantMessage), "OriginalDestination");
+
+        #endregion
+
+        var endpoint = await Endpoint.Start(config)
+            .ConfigureAwait(false);
+
+        Console.WriteLine("Endpoint Started. Press s to send a very important message. Any other key to exit");
+
+        var i = 0;
+
+        while (Console.ReadKey(true).Key == ConsoleKey.S)
+        {
+            var message = new ImportantMessage
+            {
+                Text = $"Hello there: {i++}"
+            };
+            await endpoint.Send(message)
+                .ConfigureAwait(false);
+        }
+
+        await endpoint.Stop()
+            .ConfigureAwait(false);
+    }
+}
