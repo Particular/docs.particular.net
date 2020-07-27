978f135ab23cf91eca16ca4b822de3424c1552c0
mauroservienti <mauro.servienti@gmail.com>
2020-07-27 11:47:26 +0200

    basic pessimistic lock doco

---
+++ Snippets/Raven/Raven_6.4/SagasConfiguration.cs
@@ -0,0 +1,16 @@
+﻿namespace Raven_6
+{
+    using NServiceBus;
+
+    class SagasConfiguration
+    {
+        SagasConfiguration(EndpointConfiguration endpointConfiguration)
+        {
+            #region sagas-pessimistic-lock
+            var sagasConfig = endpointConfiguration.UsePersistence<RavenDBPersistence>()
+                .Sagas();
+            sagasConfig.UsePessimisticLocking();
+            #endregion
+        }
+    }
+}
---
+++ persistence/ravendb/index_pessimistic-locking_raven_[6.4,).partial.md
@@ -0,0 +1,5 @@
+## Sagas pessimistic lock
+
+Starting with version 6.4, it's possible to configure sagas persistence to use pessimistic locking instead of the default optimisitc concurrency control.
+
+snippet: sagas-pessimistic-lock
