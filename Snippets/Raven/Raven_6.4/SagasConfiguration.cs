999923006ecd534d1bd7d4838678e637bf1dee9b
Mauro Servienti <mauro.servienti@gmail.com>
2020-07-29 15:34:45 +0200

    RavenDB pessimistic lock documentation (#4939)
    * basic pessimistic lock doco
    
    * Add pessimistic lock to main Raven page
    
    * details configuration options
    
    * Pessimistic lease lock configuration documentation per API and additional explanation on pessimistic lease lock behavior.
    
    * Use final bits
    
    Co-authored-by: Ramon Smits <ramon.smits@gmail.com>

---
+++ Snippets/Raven/Raven_6.4/SagasConfiguration.cs
@@ -0,0 +1,41 @@
+﻿namespace Raven_6
+{
+    using System;
+    using NServiceBus;
+
+    class SagasConfiguration
+    {
+        void UsePersistence(EndpointConfiguration endpointConfiguration)
+        {
+            #region ravendb-sagas-pessimistic-lock
+            var sagasConfig = endpointConfiguration.UsePersistence<RavenDBPersistence>()
+                .Sagas();
+            sagasConfig.UsePessimisticLocking();
+            #endregion
+        }
+        void SetPessimisticLeaseLockAcquisitionMaximumRefreshDelay(EndpointConfiguration endpointConfiguration)
+        {
+            #region ravendb-sagas-pessimisticLeaseLockAcquisitionMaximumRefreshDelay
+            var sagasConfig = endpointConfiguration.UsePersistence<RavenDBPersistence>()
+                .Sagas();
+            sagasConfig.SetPessimisticLeaseLockAcquisitionMaximumRefreshDelay(TimeSpan.FromMilliseconds(500));
+            #endregion
+        }
+        void SetPessimisticLeaseLockAcquisitionTimeout(EndpointConfiguration endpointConfiguration)
+        {
+            #region ravendb-sagas-setPessimisticLeaseLockAcquisitionTimeout
+            var sagasConfig = endpointConfiguration.UsePersistence<RavenDBPersistence>()
+                .Sagas();
+            sagasConfig.SetPessimisticLeaseLockAcquisitionTimeout(TimeSpan.FromSeconds(15));
+            #endregion
+        }
+        void SetPessimisticLeaseLockTime(EndpointConfiguration endpointConfiguration)
+        {
+            #region ravendb-sagas-setPessimisticLeaseLockTime
+            var sagasConfig = endpointConfiguration.UsePersistence<RavenDBPersistence>()
+                .Sagas();
+            sagasConfig.SetPessimisticLeaseLockTime(TimeSpan.FromMinutes(2));
+            #endregion
+        }
+    }
+}
