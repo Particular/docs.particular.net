1104d64af1c72d9af5a15d7c2b5841ff4a403ec9
Hadi Eskandari <h.eskandari@gmail.com>
2020-08-10 18:03:28 +1000

    new multi-hosting sample with generic host

---
+++ samples/hosting/generic-multi-hosting/Core_7/Sample.MultiHosting/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.Title = "Samples.MultiHosting";
+        #region multi-hosting
+
+        var endpointOneBuilder = ConfigureEndpointOne(Host.CreateDefaultBuilder(args)).Build();
+        var endpointTwoBuilder = ConfigureEndpointTwo(Host.CreateDefaultBuilder(args)).Build();
+        
+        var endpointOneTask = endpointOneBuilder.RunAsync();
+        var endpointTwoTask = endpointTwoBuilder.RunAsync();
+        
+        await Task.WhenAll(endpointOneTask, endpointTwoTask);
+
+        #endregion
+    }
+    
+    private static IHostBuilder ConfigureEndpointOne(IHostBuilder builder)
+    {
+        builder.UseConsoleLifetime();
+        builder.ConfigureLogging((ctx, logging) =>
+        {
+            logging.AddConfiguration(ctx.Configuration.GetSection("Logging"));
+            logging.AddEventLog();
+            logging.AddConsole();
+        });
+
+        #region endpoint-one-use-nservicebus
+
+        builder.UseNServiceBus(ctx =>
+        {
+            var endpointConfiguration = new EndpointConfiguration("Sample.MultiHosting.Instance1");
+            var scanner = endpointConfiguration.AssemblyScanner();
+            scanner.ExcludeAssemblies("Sample.MultiHosting.Instance2");
+            
+            endpointConfiguration.UseTransport<LearningTransport>();
+            endpointConfiguration.DefineCriticalErrorAction(OnCriticalError);
+
+            return endpointConfiguration;
+        });
+
+        #endregion
+
+        return builder;
+    }
+    
+    private static IHostBuilder ConfigureEndpointTwo(IHostBuilder builder)
+    {
+        builder.UseConsoleLifetime();
+        builder.ConfigureLogging((ctx, logging) =>
+        {
+            logging.AddConfiguration(ctx.Configuration.GetSection("Logging"));
+            logging.AddEventLog();
+            logging.AddConsole();
+        });
+
+        #region endpoint-one-use-nservicebus
+
+        builder.UseNServiceBus(ctx =>
+        {
+            var endpointConfiguration = new EndpointConfiguration("Sample.MultiHosting.Instance2");
+            var scanner = endpointConfiguration.AssemblyScanner();
+            scanner.ExcludeAssemblies("Sample.MultiHosting.Instance1");
+            
+            endpointConfiguration.UseTransport<LearningTransport>();
+            endpointConfiguration.DefineCriticalErrorAction(OnCriticalError);
+
+            return endpointConfiguration;
+        });
+
+        #endregion
+
+        return builder;
+    }
+
+    private static async Task OnCriticalError(ICriticalErrorContext context)
+    {
+        var fatalMessage = "The following critical error was " +
+                           $"encountered: {Environment.NewLine}{context.Error}{Environment.NewLine}Process is shutting down. " +
+                           $"StackTrace: {Environment.NewLine}{context.Exception.StackTrace}";
+        
+        EventLog.WriteEntry(".NET Runtime", fatalMessage, EventLogEntryType.Error);
+
+        try
+        {
+            await context.Stop().ConfigureAwait(false);
+        }
+        finally
+        {
+            Environment.FailFast(fatalMessage, context.Exception);
+        }
+    }
+}
---
+++ samples/hosting/generic-multi-hosting/sample.md
@@ -0,0 +1,15 @@
+---
+title: Generic Host multiple endpoint hosting
+summary: Hosting multiple endpoints in a generic host process.
+reviewed: 2020-08-10
+component: Core
+related:
+- nservicebus/hosting
+- nservicebus/hosting/multi-hosting
+---
+
+## Code walk-through
+
+This sample shows how to host multiple endpoints in one Generic Host process by using multiple `IHostBuilder` instances. When started the application creates two host builder instances, each configured for a different endpoint that could be using a different configurations:
+
+One important thing to keep in mind is that [dependency injection](/nservicebus/dependency-injection/) is used internally to register components, handlers, and sagas. Dependency injection is automatically configured during endpoint start up to scan all the assemblies found in the directory where the program is executed from. In order to ensure that each endpoint instance registers only its own components, it is important to specify an assembly scan policy using [one of the supported approaches](/nservicebus/hosting/assembly-scanning.md).
