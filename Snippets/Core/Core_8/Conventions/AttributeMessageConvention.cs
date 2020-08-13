575d4f1507bd477bb7ad8070178ba20d0d9f6455
Hadi Eskandari <h.eskandari@gmail.com>
2020-08-13 12:36:38 +1000

    copying the snippets to v8 solution

---
+++ Snippets/Core/Core_8/Conventions/AttributeMessageConvention.cs
@@ -0,0 +1,27 @@
+﻿namespace Core7.Conventions
+{
+    using System;
+    using NServiceBus;
+
+    #region message-conventions-class
+
+    class MyNamespaceMessageConvention : IMessageConvention
+    {
+        public bool IsMessageType(Type type) => type.Namespace == "MyNamespace.Messages";
+        public bool IsCommandType(Type type) => type.Namespace == "MyNamespace.Messages.Events";
+        public bool IsEventType(Type type) => type.Namespace == "MyNamespace.Messages.Commands";
+        public string Name { get; } = "MyNamespace message convention";
+    }
+
+    #endregion
+
+    public class ApplyConventions
+    {
+        public static void ApplyConventionsClass(EndpointConfiguration endpointConfiguration)
+        {
+            #region message-conventions-class-installation
+            endpointConfiguration.Conventions().Add(new MyNamespaceMessageConvention());
+            #endregion
+        }
+    }
+}
