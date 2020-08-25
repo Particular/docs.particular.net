effd5c33d1a00f72f53c0c44d4f757656ef97698
Tomek Masternak <tomasz.masternak@particular.net>
2020-08-25 13:55:10 +0200

    setting up snippets and samples

---
+++ Snippets/SQSLambda/SQSLambda_0/Usage.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+
+class Usage
+{
+    #region endpoint-configuration
+
+    static readonly AwsLambdaSQSEndpoint lambdaEndpoint = new AwsLambdaSQSEndpoint(context =>
+    {
+        var endpointConfiguration = new AwsLambdaSQSEndpointConfiguration("AwsLambdaSQSTrigger");
+        endpointConfiguration.UseSerialization<NewtonsoftSerializer>();
+
+        var advanced = endpointConfiguration.AdvancedConfiguration;
+        advanced.SendFailedMessagesTo("ErrorAwsLambdaSQSTrigger");
+
+        // shows how to write diagnostics to file
+        advanced.CustomDiagnosticsWriter(diagnostics =>
+        {
+            context.Logger.LogLine(diagnostics);
+            return Task.CompletedTask;
+        });
+
+        return endpointConfiguration;
+    });
+
+    #endregion
+}
