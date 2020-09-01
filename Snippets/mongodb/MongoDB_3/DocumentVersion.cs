9d2037859c54b1e6405e484a97e3b9d26580a2c6
Tim Bussmann <timbussmann@users.noreply.github.com>
2020-09-01 19:56:32 +0200

    add NServiceBus.Storage.MongoDB 3 snippets

---
+++ Snippets/mongodb/MongoDB_3/DocumentVersion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MongoDB_2
+{
+    class DocumentVersion
+    {
+        async Task UpdateWithVersion(IMongoCollection<BsonDocument> collection, string versionFieldName, UpdateDefinitionBuilder<BsonDocument> updateBuilder, int currentVersion, Guid documentId)
+        {
+            #region MongoDBUpdateWithVersion
+
+            UpdateDefinition<BsonDocument> updateDefinition = updateBuilder.Inc(versionFieldName, 1);
+
+            //Define other update operations on the document
+
+            BsonDocument modifiedDocument = await collection.FindOneAndUpdateAsync<BsonDocument>(
+                filter: document => document["_id"] == documentId && document["_version"] == currentVersion,
+                update: updateDefinition,
+                options: new FindOneAndUpdateOptions<BsonDocument, BsonDocument> { IsUpsert = false, ReturnDocument = ReturnDocument.After });
+
+            if (modifiedDocument == null)
+            {
+                //The document was not updated because the version was already incremented.
+            }
+
+            #endregion
+        }
+    }
+}
---
+++ Snippets/mongodb/MongoDB_3/SharedTransaction.cs
@@ -0,0 +1,27 @@
+﻿namespace MongoDB_2
+{
+    using System.Threading.Tasks;
+    using NServiceBus;
+
+    class SharedTransaction : IHandleMessages<MyMessage>
+    {
+        #region MongoDBHandlerSharedTransaction
+
+        public Task Handle(MyMessage message, IMessageHandlerContext context)
+        {
+            var session = context.SynchronizedStorageSession.GetClientSession();
+            var collection = session.Client.GetDatabase("mydatabase").GetCollection<MyBusinessObject>("mycollection");
+            return collection.InsertOneAsync(session, new MyBusinessObject());
+        }
+
+        #endregion
+    }
+
+    class MyMessage
+    {
+    }
+
+    class MyBusinessObject
+    {
+    }
+}
