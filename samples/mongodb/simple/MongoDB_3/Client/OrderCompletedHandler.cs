6520119725ea90a7059f87fcbc3584eb539d3dcc
Tim Bussmann <timbussmann@users.noreply.github.com>
2020-09-01 20:04:50 +0200

    update MongoDB sample to NServiceBus.Persistence.MongoDB 3

---
+++ samples/mongodb/simple/MongoDB_3/Client/OrderCompletedHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+
+public class OrderCompletedHandler : IHandleMessages<OrderCompleted>
+{
+    static readonly ILog log = LogManager.GetLogger<OrderCompletedHandler>();
+
+    public Task Handle(OrderCompleted message, IMessageHandlerContext context)
+    {
+        log.Info($"Received OrderCompleted for OrderId {message.OrderId}");
+        return Task.CompletedTask;
+    }
+}
---
+++ samples/mongodb/simple/MongoDB_3/Server/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.MongoDB.Server";
+
+        #region mongoDbConfig
+
+        var endpointConfiguration = new EndpointConfiguration("Samples.MongoDB.Server");
+        var persistence = endpointConfiguration.UsePersistence<MongoPersistence>();
+        persistence.DatabaseName("Samples_MongoDB_Server");
+
+        #endregion
+
+        endpointConfiguration.EnableInstallers();
+        endpointConfiguration.UseTransport<LearningTransport>();
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
