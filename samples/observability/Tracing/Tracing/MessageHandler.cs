ba05baaded602e00c466f8b515cc123c9862b38d
Tomek Masternak <tomasz.masternak@particular.net>
2020-09-03 14:29:40 +0200

    initial code commit

---
+++ samples/observability/Tracing/Tracing/MessageHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+namespace Tracing
+{
+    public class MessageHandler : IHandleMessages<InitialCommand>, IHandleMessages<FollowupEvent>
+    {
+        public async Task Handle(InitialCommand message, IMessageHandlerContext context)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(2));
+
+            await context.Publish<FollowupEvent>();
+        }
+
+        public async Task Handle(FollowupEvent message, IMessageHandlerContext context)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1));
+        }
+    }
+}
---
+++ samples/observability/Tracing/Tracing/Messages.cs
@@ -0,0 +1,12 @@
+﻿using NServiceBus;
+
+namespace Tracing
+{
+    public class InitialCommand : ICommand
+    {
+    }
+
+    public class FollowupEvent : IEvent
+    {
+    }
+}
