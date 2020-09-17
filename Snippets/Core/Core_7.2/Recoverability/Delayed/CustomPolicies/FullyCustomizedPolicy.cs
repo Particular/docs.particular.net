5f48d1b7c44773cf299d8c67e8f6d54a4a2cf62b
Matthew Hermann <46518526+matthewhermann@users.noreply.github.com>
2020-09-17 17:04:33 +1000

    Corrected the description of how many times Delayed Retries are performed and updated snippets to match

--- Snippets/Core/Core_7.2/Recoverability/Delayed/CustomPolicies/FullyCustomizedPolicy.cs
+++ Snippets/Core/Core_7.2/Recoverability/Delayed/CustomPolicies/FullyCustomizedPolicy.cs
@@ -53,7 +53,7 @@
             // override delayed retry decision for custom exception
             // i.e. MyOtherBusinessException should do fixed backoff of 5 seconds
             if (context.Exception is MyOtherBusinessException &&
-                context.DelayedDeliveriesPerformed <= config.Delayed.MaxNumberOfRetries)
+                context.DelayedDeliveriesPerformed < config.Delayed.MaxNumberOfRetries)
             {
                 return RecoverabilityAction.DelayedRetry(TimeSpan.FromSeconds(5));
             }
