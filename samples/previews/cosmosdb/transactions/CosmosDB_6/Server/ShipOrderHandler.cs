ea1c620b40e570a52358be00d2b9a8aecaed30a3
danielmarbach <daniel.marbach@openplace.net>
2020-09-22 15:19:36 +0200

    Advanced transaction sample

---
+++ samples/previews/cosmosdb/transactions/CosmosDB_6/Server/ShipOrderHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+
+#region UseHeader
+public class ShipOrderHandler :
+    IHandleMessages<ShipOrder>
+{
+    public Task Handle(ShipOrder message, IMessageHandlerContext context)
+    {
+        Log.Info($"Order Shipped. OrderId {message.OrderId}");
+
+        var options = new PublishOptions();
+        options.SetHeader("Sample.CosmosDB.Transaction.OrderId", message.OrderId.ToString());
+
+        return context.Publish(new OrderShipped { OrderId = message.OrderId, ShippingDate = DateTime.Today }, options);
+    }
+
+    static ILog Log = LogManager.GetLogger<ShipOrderHandler>();
+}
+#endregion
