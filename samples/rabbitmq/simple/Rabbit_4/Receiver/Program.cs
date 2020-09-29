e1c960f6b401637831fc5878f9895d678f4a00dd
Tim Bussmann <timbussmann@users.noreply.github.com>
2020-09-29 09:58:27 +0200

    backport sample changes to v4 version

---
+++ samples/rabbitmq/simple/Rabbit_4/Receiver/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Receiver
+{
+    using System.Threading.Tasks;
+    using NServiceBus;
+
+    class Program
+    {
+        static async Task Main()
+        {
+            Console.Title = "Samples.RabbitMQ.SimpleReceiver";
+            var endpointConfiguration = new EndpointConfiguration("Samples.RabbitMQ.SimpleReceiver");
+            var transport = endpointConfiguration.UseTransport<RabbitMQTransport>();
+            transport.ConnectionString("host=localhost");
+            transport.DelayedDelivery().DisableTimeoutManager();
+            endpointConfiguration.EnableInstallers();
+
+            var endpointInstance = await Endpoint.Start(endpointConfiguration)
+                .ConfigureAwait(false);
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+            await endpointInstance.Stop()
+                .ConfigureAwait(false);
+        }
+    }
+}
