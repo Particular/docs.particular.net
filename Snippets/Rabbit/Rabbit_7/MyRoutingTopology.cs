fb9978a5835be1ab89a84b3eaa20c15727fc55d3
Andreas Öhlund <andreas.ohlund@particular.net>
2020-10-16 11:47:17 +0200

    New rabbit api

---
+++ Snippets/Rabbit/Rabbit_7/MyRoutingTopology.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using NServiceBus.Transport;
+using NServiceBus.Transport.RabbitMQ;
+using RabbitMQ.Client;
+
+class MyRoutingTopology :
+    IRoutingTopology
+{
+    public MyRoutingTopology(bool createDurableExchangesAndQueues)
+    {
+    }
+
+    public void SetupSubscription(IModel channel, Type type, string subscriberName)
+    {
+    }
+
+    public void TeardownSubscription(IModel channel, Type type, string subscriberName)
+    {
+    }
+
+    public void Publish(IModel channel, Type type, OutgoingMessage message, IBasicProperties properties)
+    {
+    }
+
+    public void Send(IModel channel, string address, OutgoingMessage message, IBasicProperties properties)
+    {
+    }
+
+    public void RawSendInCaseOfFailure(IModel channel, string address, ReadOnlyMemory<byte> body, IBasicProperties properties)
+    {
+    }
+
+    public void Initialize(IModel channel, IEnumerable<string> receivingAddresses, IEnumerable<string> sendingAddresses)
+    {
+    }
+
+    public void BindToDelayInfrastructure(IModel channel, string address, string deliveryExchange, string routingKey)
+    {
+    }
+}
--- transports/rabbitmq/index.md
+++ transports/rabbitmq/index.md
@@ -57,3 +57,11 @@ partial: topology
  * Requires careful consideration for duplicate messages, e.g. using the [outbox](/nservicebus/outbox/) feature or making all endpoints idempotent.
  * Many organizations don't have the same level of expertise with RabbitMQ as with other technologies, such as SQL Server, so it may require additional training.
  * May require covering additional costs of [commercial RabbitMQ license and support](https://www.rabbitmq.com/services.html).
+
+## Controlling delivery mode
+
+In AMQP [the `delivery_mode`](https://www.rabbitmq.com/amqp-0-9-1-reference.html) controls how the broker treats the message from a durability standpoint. NServiceBus will default to `persistent` in order to prevent message loss. To optimize for higher throughput this can be changed to `non-persistent`.
+
+DANGER: Any failure in transmission or issues in the broker will result in the message being lost
+
+partial: nonpersistent
