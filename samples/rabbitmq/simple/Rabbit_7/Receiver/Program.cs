547d24cfa45f5f372031bc23a9bb19baeee2b4c2
Ramon Smits <ramon.smits@gmail.com>
2020-10-21 16:32:41 +0200

    RabbmitMQ v7 sample (#5077)

---
+++ samples/rabbitmq/simple/Rabbit_7/Receiver/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Receiver
+{
+    using System.Threading.Tasks;
+    using NServiceBus;
+
+    class Program
+    {
+        static async Task Main()
+        {
+            Console.Title = "Samples.RabbitMQ.SimpleReceiver";
+            var endpointConfiguration = new EndpointConfiguration("Samples.RabbitMQ.SimpleReceiver");
+            var transport = endpointConfiguration.UseTransport<RabbitMQTransport>();
+            transport.UseConventionalRoutingTopology();
+            transport.ConnectionString("host=localhost");
+            endpointConfiguration.EnableInstallers();
+
+            var endpointInstance = await Endpoint.Start(endpointConfiguration)
+                .ConfigureAwait(false);
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+            await endpointInstance.Stop()
+                .ConfigureAwait(false);
+        }
+    }
+}
