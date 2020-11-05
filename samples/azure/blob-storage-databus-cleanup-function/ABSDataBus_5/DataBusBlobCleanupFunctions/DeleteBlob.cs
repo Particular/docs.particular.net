23592f4849eafe3b636edfe42f29e15928cc6506
Sean Feldman <feldman.sean@gmail.com>
2020-11-05 00:51:24 -0700

    Add azure databus v5 sample

---
+++ samples/azure/blob-storage-databus-cleanup-function/ABSDataBus_5/DataBusBlobCleanupFunctions/DeleteBlob.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Storage.Blob;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Extensions.Logging;
+
+public static class DeleteBlobFunction
+{
+    static DeleteBlobFunction()
+    {
+        var storageConnectionString = Environment.GetEnvironmentVariable("DataBusStorageAccount");
+        var cloudStorageAccount = Microsoft.Azure.Storage.CloudStorageAccount.Parse(storageConnectionString);
+        cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
+    }
+
+    #region DeleteBlobFunction
+
+    [FunctionName("DeleteBlob")]
+    public static async Task DeleteBlob([ActivityTrigger] DataBusBlobData blobData, ILogger log)
+    {
+        var blob = await cloudBlobClient.GetBlobReferenceFromServerAsync(new Uri(blobData.Path));
+        log.LogInformation($"Deleting blob at {blobData.Path}");
+        await blob.DeleteIfExistsAsync();
+    }
+
+    #endregion
+
+    static CloudBlobClient cloudBlobClient;
+}
