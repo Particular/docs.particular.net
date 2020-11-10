50f2c896797fe5ed305412c7781a21bb6e36f14b
Sean Feldman <feldman.sean@gmail.com>
2020-11-09 23:25:52 -0700

    Update static sample with a custom component passed via DI container

---
+++ samples/previews/azure-functions/service-bus/ASBFunctions_0/AzureFunctions.ASBTrigger.Static/CustomComponent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class CustomComponent
+{
+    readonly string initialValue;
+
+    public CustomComponent(string initialValue)
+    {
+        this.initialValue = $"{initialValue} {DateTime.UtcNow:O}";
+    }
+
+    public string GetValue() => initialValue;
+}
