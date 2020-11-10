0e5ebeae2af82af2b16a32e6d8a7b0586122889d
Sean Feldman <feldman.sean@gmail.com>
2020-11-09 23:50:51 -0700

    Add FunctionHostBuilder based sample with a custom component passed via DI container

---
+++ samples/previews/azure-functions/service-bus/ASBFunctions_0/AzureFunctions.ASBTrigger.FunctionsHostBuilder/AzureServiceBusTriggerFunction.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using NServiceBus;
+
+public class AzureServiceBusTriggerFunction
+{
+    internal const string EndpointName = "ASBTriggerQueue";
+    readonly FunctionEndpoint endpoint;
+
+    #region EndpointInjection
+
+    public AzureServiceBusTriggerFunction(FunctionEndpoint endpoint)
+    {
+        this.endpoint = endpoint;
+    }
+
+    #endregion
+
+    #region InjectedFunction
+
+    [FunctionName(EndpointName)]
+    public async Task Run(
+        [ServiceBusTrigger(queueName: EndpointName)]
+        Message message,
+        ILogger logger,
+        ExecutionContext executionContext)
+    {
+        await endpoint.Process(message, executionContext, logger);
+    }
+
+    #endregion
+}
---
+++ samples/previews/azure-functions/service-bus/ASBFunctions_0/AzureFunctions.ASBTrigger.FunctionsHostBuilder/Startup.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NServiceBus;
+
+#region configuration-with-function-host-builder
+
+[assembly: FunctionsStartup(typeof(Startup))]
+
+public class Startup : FunctionsStartup
+{
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+        var services = builder.Services;
+
+        // register custom service in the container
+        services.AddSingleton(_ =>
+        {
+            var configurationRoot = builder.GetContext().Configuration;
+            var customComponentInitializationValue = configurationRoot.GetValue<string>("CustomComponentValue");
+
+            return new CustomComponent(customComponentInitializationValue);
+        });
+
+        builder.UseNServiceBus(() =>
+        {
+            var configuration = new ServiceBusTriggeredEndpointConfiguration(AzureServiceBusTriggerFunction.EndpointName);
+
+            // optional: log startup diagnostics using Functions provided logger
+            configuration.LogDiagnostics();
+
+            return configuration;
+        });
+    }
+}
+
+#endregion
