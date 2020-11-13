48cbeb43c189b13d8a887384ff423877ca90a7f5
danielmarbach <daniel.marbach@openplace.net>
2020-11-13 13:41:08 +0100

    Dynamic table sample

---
+++ samples/azure/azure-table/table/ASTP_3/Client/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.AzureTable.Table.Client";
+        var endpointConfiguration = new EndpointConfiguration("Samples.AzureTable.Table.Client");
+        endpointConfiguration.UsePersistence<LearningPersistence>();
+        endpointConfiguration.UseTransport<LearningTransport>();
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+
+        Console.WriteLine("Press 'S' to send a StartOrder message to the server endpoint");
+
+        Console.WriteLine("Press any other key to exit");
+
+        while (true)
+        {
+            var key = Console.ReadKey();
+            Console.WriteLine();
+
+            var orderId = Guid.NewGuid();
+            var startOrder = new StartOrder
+            {
+                OrderId = orderId
+            };
+            if (key.Key == ConsoleKey.S)
+            {
+                await endpointInstance.Send("Samples.AzureTable.Table.Server", startOrder)
+                    .ConfigureAwait(false);
+                Console.WriteLine($"StartOrder Message sent to Server with OrderId {orderId}");
+                continue;
+            }
+            break;
+        }
+
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
