e567f9479f5b0297187b9ddc908be19ffd44a228
danielmarbach <daniel.marbach@openplace.net>
2020-11-16 09:28:48 +0100

    Saga transaction sample using IProvidePartitionKeyFromSagaId

---
+++ samples/azure/azure-table/saga-transactions/ASTP_3/Server/OrderIdAsPartitionKeyBehavior.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+using NServiceBus.Persistence.AzureTable;
+using NServiceBus.Pipeline;
+using NServiceBus.Sagas;
+
+#region BehaviorUsingIProvidePartitionKeyFromSagaId
+
+class OrderIdAsPartitionKeyBehavior : Behavior<IIncomingLogicalMessageContext>
+{
+    public OrderIdAsPartitionKeyBehavior(IProvidePartitionKeyFromSagaId partitionKeyFromSagaId)
+    {
+        partitionKeyFromSagaId1 = partitionKeyFromSagaId;
+    }
+
+    public override async Task Invoke(IIncomingLogicalMessageContext context, Func<Task> next)
+    {
+        var correlationProperty = SagaCorrelationProperty.None;
+        if (context.Message.Instance is IProvideOrderId provideOrderId)
+        {
+            var partitionKeyValue = provideOrderId.OrderId;
+            correlationProperty = new SagaCorrelationProperty("OrderId", partitionKeyValue);
+        }
+
+        await partitionKeyFromSagaId1.SetPartitionKey<OrderSagaData>(context, correlationProperty);
+
+        if (context.Headers.TryGetValue(Headers.SagaId, out var sagaIdHeader))
+        {
+            Log.Info($"Saga Id Header: {sagaIdHeader}");
+        }
+
+        if (context.Extensions.TryGet<TableInformation>(out var tableInformation))
+        {
+            Log.Info($"Table Information: {tableInformation.TableName}");
+        }
+
+        Log.Info($"Found partition key '{context.Extensions.Get<TableEntityPartitionKey>().PartitionKey}' from '{nameof(IProvideOrderId)}'");
+
+        await next().ConfigureAwait(false);
+    }
+
+    public class Registration : RegisterStep
+    {
+        public Registration() :
+            base(nameof(OrderIdAsPartitionKeyBehavior),
+                typeof(OrderIdAsPartitionKeyBehavior),
+                "Determines the PartitionKey from the logical message",
+                b => new OrderIdAsPartitionKeyBehavior(b.Build<IProvidePartitionKeyFromSagaId>()))
+        {
+            InsertBefore(nameof(LogicalOutboxBehavior));
+        }
+    }
+
+    IProvidePartitionKeyFromSagaId partitionKeyFromSagaId1;
+    static readonly ILog Log = LogManager.GetLogger<OrderIdAsPartitionKeyBehavior>();
+}
+#endregion
---
+++ samples/azure/azure-table/saga-transactions/ASTP_3/Server/OrderShippingInformation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Azure.Cosmos.Table;
+
+public class OrderShippingInformation : TableEntity
+{
+    [IgnoreProperty]
+    public Guid Id
+    {
+        get => Guid.Parse(RowKey);
+        set => RowKey = value.ToString();
+    }
+
+    public Guid OrderId { get; set; }
+
+    public DateTimeOffset ShippedAt { get; set; }
+}
