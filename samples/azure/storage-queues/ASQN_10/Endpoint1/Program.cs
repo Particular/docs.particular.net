ae20fc00475070d583e1107fabc8cee0edd4bb6f
Sean Feldman <feldman.sean@gmail.com>
2020-11-16 16:35:06 -0700

    Add ASQ v10 sample

---
+++ samples/azure/storage-queues/ASQN_10/Endpoint1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Features;
+using AzureStorageQueueTransport = NServiceBus.AzureStorageQueueTransport;
+
+class Program
+{
+    static async Task Main()
+    {
+        var endpointName = "Samples-Azure-StorageQueues-Endpoint1";
+        var endpointConfiguration = new EndpointConfiguration(endpointName);
+
+        Console.Title = endpointName;
+
+        #region config
+
+        var transport = endpointConfiguration.UseTransport<AzureStorageQueueTransport>();
+        transport.ConnectionString("UseDevelopmentStorage=true");
+
+        #endregion
+
+        transport.DisablePublishing();
+        endpointConfiguration.UsePersistence<LearningPersistence>();
+        endpointConfiguration.UseSerialization<NewtonsoftSerializer>();
+        endpointConfiguration.EnableInstallers();
+        endpointConfiguration.SendFailedMessagesTo("error");
+        endpointConfiguration.DisableFeature<TimeoutManager>();
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        Console.WriteLine("Press 'enter' to send a message");
+        Console.WriteLine("Press any other key to exit");
+
+        while (true)
+        {
+            var key = Console.ReadKey();
+            Console.WriteLine();
+
+            if (key.Key != ConsoleKey.Enter)
+            {
+                break;
+            }
+
+            var message = new Message1
+            {
+                Property = "Hello from Endpoint1"
+            };
+            await endpointInstance.Send("Samples-Azure-StorageQueues-Endpoint2", message)
+                .ConfigureAwait(false);
+            Console.WriteLine("Message1 sent");
+        }
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
