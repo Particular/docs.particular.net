6820fffaa74831d1685b60eb9a42366247da2148
Sean Feldman <feldman.sean@gmail.com>
2020-11-16 16:53:53 -0700

    Update docs and snippets for ASQ 10/Core 8

---
+++ Snippets/ASQ/ASQN_10/CustomEnvelopeUnwrapper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using NServiceBus;
+using NServiceBus.Azure.Transports.WindowsAzureStorageQueues;
+
+class CustomEnvelopeUnwrapper
+{
+
+    CustomEnvelopeUnwrapper(EndpointConfiguration endpointConfiguration)
+    {
+        #region CustomEnvelopeUnwrapper
+
+        var transport = endpointConfiguration.UseTransport<AzureStorageQueueTransport>();
+
+        transport.UnwrapMessagesWith(queueMessage =>
+        {
+            using (var stream = new MemoryStream(Convert.FromBase64String(queueMessage.MessageText)))
+            using (var streamReader = new StreamReader(stream))
+            using (var textReader = new JsonTextReader(streamReader))
+            {
+                //try deserialize to a NServiceBus envelope first
+                var wrapper = jsonSerializer.Deserialize<MessageWrapper>(textReader);
+
+                if (wrapper.Id != null)
+                {
+                    //this was a envelope message
+                    return wrapper;
+                }
+
+                //this was a native message just return the body as is with no headers
+                return new MessageWrapper
+                {
+                    Id = queueMessage.MessageId,
+                    Headers = new Dictionary<string, string>(),
+                    Body = Convert.FromBase64String(queueMessage.MessageText)
+                };
+            }
+        });
+
+        #endregion
+    }
+
+    Newtonsoft.Json.JsonSerializer jsonSerializer = Newtonsoft.Json.JsonSerializer.Create();
+
+}
---
+++ Snippets/ASQ/ASQN_10/Usage.cs
@@ -0,0 +1,161 @@
+﻿// ReSharper disable SuggestVarOrType_Elsewhere
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Storage.Queues;
+using Microsoft.Azure.Cosmos.Table;
+using NServiceBus;
+
+[SuppressMessage("ReSharper", "UnusedMember.Local")]
+class Usage
+{
+    void UseTransport(EndpointConfiguration endpointConfiguration)
+    {
+        #region AzureStorageQueueTransportWithAzure
+
+        var transport = endpointConfiguration.UseTransport<AzureStorageQueueTransport>();
+        transport.ConnectionString("DefaultEndpointsProtocol=https;AccountName=[ACCOUNT];AccountKey=[KEY];");
+
+        #endregion
+    }
+
+    void CodeOnly(EndpointConfiguration endpointConfiguration)
+    {
+        var queueServiceClient = new QueueServiceClient("connectionString", new QueueClientOptions());
+        var blobServiceClient = new BlobServiceClient("connectionString", new BlobClientOptions());
+        var cloudStorageAccount = CloudStorageAccount.Parse("connectionString");
+        var cloudTableClient = cloudStorageAccount.CreateCloudTableClient();
+
+        #region AzureStorageQueueConfigCodeOnly
+
+        var transport = endpointConfiguration.UseTransport<AzureStorageQueueTransport>();
+        transport.ConnectionString("DefaultEndpointsProtocol=https;AccountName=[ACCOUNT];AccountKey=[KEY];");
+        transport.BatchSize(20);
+        transport.MaximumWaitTimeWhenIdle(TimeSpan.FromSeconds(1));
+        transport.DegreeOfReceiveParallelism(16);
+        transport.PeekInterval(TimeSpan.FromMilliseconds(100));
+        transport.MessageInvisibleTime(TimeSpan.FromSeconds(30));
+        transport.UseQueueServiceClient(queueServiceClient);
+        transport.UseBlobServiceClient(blobServiceClient);
+        transport.UseCloudTableClient(cloudTableClient);
+
+        #endregion
+    }
+
+    async Task SendToMulitpleAccountUsingConnectionSTring(IEndpointInstance endpointInstance)
+    {
+        #region storage_account_routing_send_options_full_connectionstring
+
+        await endpointInstance.Send(
+            destination: "sales@accountAlias",
+            message: new MyMessage());
+
+        #endregion
+    }
+
+    async Task SendToMulitpleAccountUsingAlias(IEndpointInstance endpointInstance)
+    {
+        #region storage_account_routing_send_options_alias
+
+        await endpointInstance.Send(
+            destination: "sales@accountAlias",
+            message: new MyMessage());
+
+        #endregion
+    }
+
+    void RegisterEndpoint(EndpointConfiguration configuration)
+    {
+        #region storage_account_routing_registered_endpoint
+
+        var transportConfig = configuration.UseTransport<AzureStorageQueueTransport>();
+        var routing = transportConfig
+                            .ConnectionString("connectionString")
+                            .AccountRouting();
+        var anotherAccount = routing.AddAccount("AnotherAccountName","anotherConnectionString");
+        anotherAccount.RegisteredEndpoints.Add("Receiver");
+
+        transportConfig.Routing().RouteToEndpoint(typeof(MyMessage), "Receiver");
+
+        #endregion
+    }
+
+    async Task SendToMulitpleAccountUsingRegisterdEndpoint(IEndpointInstance endpointInstance)
+    {
+        #region storage_account_routing_send_registered_endpoint
+
+        await endpointInstance.Send(message: new MyMessage());
+
+        #endregion
+    }
+
+    void RegisterPublisher(EndpointConfiguration configuration)
+    {
+        #region storage_account_routing_registered_publisher
+
+        var transportConfig = configuration.UseTransport<AzureStorageQueueTransport>();
+        var routing = transportConfig
+                            .ConnectionString("anotherConnectionString")
+                            .AccountRouting();
+        var anotherAccount = routing.AddAccount("PublisherAccountName", "connectionString");
+        anotherAccount.RegisteredEndpoints.Add("Publisher");
+
+        transportConfig.Routing().RegisterPublisher(typeof(MyEvent), "Publisher");
+
+        #endregion
+    }
+
+    void MultipleAccountAliasesInsteadOfConnectionStrings1(EndpointConfiguration endpointConfiguration)
+    {
+        #region AzureStorageQueueUseMultipleAccountAliasesInsteadOfConnectionStrings1
+
+        var transport = endpointConfiguration.UseTransport<AzureStorageQueueTransport>();
+        transport.ConnectionString("account_A_connection_string");
+        transport.DefaultAccountAlias("account_A");
+        var accountRouting = transport.AccountRouting();
+        accountRouting.AddAccount("account_B", "account_B_connection_string");
+
+        #endregion
+    }
+
+    void SetSerialization(EndpointConfiguration endpointConfiguration)
+    {
+        #region AzureStorageQueueSerialization
+
+        endpointConfiguration.UseSerialization<NewtonsoftSerializer>();
+
+        #endregion
+    }
+
+    // Reference NServiceBus.Host instead of NServiceBus.Hosting.Azure as the usage is the same
+    // This prevents Azure storage library conflicts as the libraries are being updated
+
+    #region AzureStorageQueueTransportWithAzureHost
+
+    public class EndpointConfig :
+        IConfigureThisEndpoint
+    {
+        public void Customize(EndpointConfiguration endpointConfiguration)
+        {
+            var transport = endpointConfiguration.UseTransport<AzureStorageQueueTransport>();
+            transport.ConnectionString("DefaultEndpointsProtocol=https;AccountName=[ACCOUNT];AccountKey=[KEY];");
+        }
+    }
+
+    #endregion
+    // to avoid host ref
+    internal interface IConfigureThisEndpoint
+    {
+    }
+
+    public class MyMessage :
+        ICommand
+    { }
+
+    public class MyEvent :
+        IEvent
+    { }
+
+}
