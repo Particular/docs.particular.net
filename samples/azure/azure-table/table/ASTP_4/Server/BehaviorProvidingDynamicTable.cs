732c57c40452a21276c753d9ffe918bd9edf4c40
danielmarbach <daniel.marbach@openplace.net>
2020-11-24 10:27:52 +0100

    ASP v4 samples

---
+++ samples/azure/azure-table/table/ASTP_4/Server/BehaviorProvidingDynamicTable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+using NServiceBus.Pipeline;
+
+#region BehaviorAddingTableInfo
+
+class BehaviorProvidingDynamicTable : Behavior<IIncomingLogicalMessageContext>
+{
+    public override Task Invoke(IIncomingLogicalMessageContext context, Func<Task> next)
+    {
+        if (context.Message.Instance is ShipOrder ||
+            context.Headers.TryGetValue(Headers.SagaType, out var sagaTypeHeader) && sagaTypeHeader.Contains(nameof(ShipOrderSaga)))
+        {
+            Log.Info($"Message '{context.Message.MessageType.FullName}' destined to be handled by '{nameof(ShipOrderSaga)}' will use 'ShipOrderSagaData' table.");
+
+            context.Extensions.Set(new TableInformation("ShipOrderSagaData"));
+        }
+        else
+        {
+            Log.Info($"Message '{context.Message.MessageType.FullName}' destined to be handled by '{nameof(OrderSaga)}' will the default table.");
+        }
+
+        return next();
+    }
+
+    static readonly ILog Log = LogManager.GetLogger<BehaviorProvidingDynamicTable>();
+}
+#endregion
---
+++ samples/azure/azure-table/transactions/ASTP_4/Server/OrderShipped.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using NServiceBus;
+
+public class OrderShipped : IEvent
+{
+    public Guid OrderId { get; set; }
+    public DateTimeOffset ShippingDate { get; set; }
+}
