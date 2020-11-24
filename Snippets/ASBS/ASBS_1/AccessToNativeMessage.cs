4add5db4457b3e0cfd1456ea651b1186e2121e26
Sean Feldman <feldman.sean@gmail.com>
2020-11-24 00:16:10 -0700

    Update ASB doco for native message access

--- Snippets/ASBS/ASBS_1/AccessToNativeMessage.cs
+++ Snippets/ASBS/ASBS_1/AccessToNativeMessage.cs
@@ -3,11 +3,12 @@
     using System;
     using System.Threading.Tasks;
     using Microsoft.Azure.ServiceBus;
+    using NServiceBus;
     using NServiceBus.Pipeline;
 
     public class AccessToNativeMessage
     {
-        #region access-native-message
+        #region access-native-incoming-message 1.4
 
         class DoNotAttemptMessageProcessingIfMessageIsNotLocked : Behavior<ITransportReceiveContext>
         {
@@ -27,5 +28,71 @@
         }
 
         #endregion
+
+        class AccessOutgoingNativeMessage
+        {
+            async Task AccessNativeOutgoingMessageFromHandler(IMessageHandlerContext context)
+            {
+                #region access-native-outgoing-message-from-handler 1.7
+                // send a command
+                var sendOptions = new SendOptions();
+                sendOptions.CustomizeNativeMessage(context, m => m.Label = "custom-label");
+                await context.Send(new MyCommand(), sendOptions).ConfigureAwait(false);
+
+                // publish an event
+                var publishOptions = new PublishOptions();
+                publishOptions.CustomizeNativeMessage(context, m => m.Label = "custom-label");
+                await context.Publish(new MyEvent(), publishOptions).ConfigureAwait(false);
+                #endregion
+            }
+
+            async Task AccessNativeOutgoingMessageWithMessageSession(IEndpointInstance messageSession)
+            {
+                #region access-native-outgoing-message-with-messagesession 1.7
+                // send a command
+                var sendOptions = new SendOptions();
+                sendOptions.CustomizeNativeMessage(m => m.Label = "custom-label");
+                await messageSession.Send(new MyCommand(), sendOptions).ConfigureAwait(false);
+
+                // publish an event
+                var publishOptions = new PublishOptions();
+                publishOptions.CustomizeNativeMessage(m => m.Label = "custom-label");
+                await messageSession.Publish(new MyEvent(), publishOptions).ConfigureAwait(false);
+                #endregion
+            }
+
+            #region access-native-outgoing-message-from-physical-behavior 1.7
+            public class PhysicalBehavior : Behavior<IIncomingPhysicalMessageContext>
+            {
+                public override async Task Invoke(IIncomingPhysicalMessageContext context, Func<Task> next)
+                {
+                    var sendOptions = new SendOptions();
+                    sendOptions.CustomizeNativeMessage(context, m => m.Label = "custom-label");
+
+                    await context.Send(new MyCommand(), sendOptions);
+
+                    await next();
+                }
+            }
+            #endregion
+
+            #region access-native-outgoing-message-from-logical-behavior 1.7
+            public class LogicalBehavior : Behavior<IIncomingLogicalMessageContext>
+            {
+                public override async Task Invoke(IIncomingLogicalMessageContext context, Func<Task> next)
+                {
+                    var publishOptions = new PublishOptions();
+                    publishOptions.CustomizeNativeMessage(context, m => m.Label = "custom-label");
+
+                    await context.Publish(new MyEvent(), publishOptions);
+
+                    await next();
+                }
+            }
+            #endregion
+
+            class MyCommand { }
+            class MyEvent { }
+        }
     }
 }
--- transports/azure-service-bus/native-message-access.md
+++ transports/azure-service-bus/native-message-access.md
@@ -1,16 +1,38 @@
 ---
 title: Native message access
 component: ASBS
-versions: '[1.4.0,]'
-reviewed: 2019-12-02
+versions: '[1.4.0,)'
+reviewed: 2020-11-23
 ---
 
-This document describes how to access native message information with Azure Service Bus transport version 1.4.0 and above.
+This document describes how to access native message information with Azure Service Bus transport.
+- Incoming message access is available from version 1.4.0 and above.
+- Outgoing message access is available from version 1.7.0 and above.
 
-## Access to the native Azure Service Bus message details
+## Access to the native Azure Service Bus incoming message
 
-It can sometimes be useful to access the native Service Bus message from behaviors and handlers. When a message is received, the transport adds the native Service Bus [`Message`](https://docs.microsoft.com/en-us/dotnet/api/microsoft.azure.servicebus.message) to the message processing context. Use the code below to access the message details from a [pipeline behavior](/nservicebus/pipeline/manipulate-with-behaviors.md):
+It can sometimes be useful to access the native Service Bus incoming message from behaviors and handlers. When a message is received, the transport adds the native Service Bus [`Message`](https://docs.microsoft.com/en-us/dotnet/api/microsoft.azure.servicebus.message) to the message processing context. Use the code below to access the message details from a [pipeline behavior](/nservicebus/pipeline/manipulate-with-behaviors.md):
 
-snippet: access-native-message
+snippet: access-native-incoming-message
 
 The behavior above uses the native message's `LockedUntilUtc` system property to determine wherever the message has lost its lock as a result of aggressive prefetching and slow processing. The message with the lost lock will not go through recoverability, skipping attempted processing that otherwise would be guaranteed to fail due to the message's lost lock.
+
+## Access to the native Azure Service Bus outgoing message
+
+It can also be useful to access the native Service Bus outgoing message from behaviors and handlers for customizations. 
+
+Customizing an outgoing message from a message handler:
+
+snippet: access-native-outgoing-message-from-handler
+
+Customizing an outgoing message using `IMessageSession`:
+
+snippet: access-native-outgoing-message-with-messagesession
+
+Customizing an outgoing message from a physical behavior:
+
+snippet: access-native-outgoing-message-from-physical-behavior
+
+Customizing an outgoing message from a logical behavior:
+
+snippet: access-native-outgoing-message-from-logical-behavior
