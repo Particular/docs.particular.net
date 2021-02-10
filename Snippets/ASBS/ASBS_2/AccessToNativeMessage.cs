a661c69fea4a7cd847d740cf3859a84c4d9d84fa
Tim Bussmann <timbussmann@users.noreply.github.com>
2021-02-10 15:26:36 +0100

    add v2 snippets

---
+++ Snippets/ASBS/ASBS_2/AccessToNativeMessage.cs
@@ -0,0 +1,29 @@
+﻿namespace ASBS_1
+{
+    using System.Threading.Tasks;
+    using NServiceBus;
+
+    public class AccessToNativeMessage
+    {
+        class AccessOutgoingNativeMessage
+        {
+            async Task AccessNativeOutgoingMessageFromHandler(IMessageHandlerContext context)
+            {
+                #region access-native-outgoing-message
+                // send a command
+                var sendOptions = new SendOptions();
+                sendOptions.CustomizeNativeMessage(m => m.Label = "custom-label");
+                await context.Send(new MyCommand(), sendOptions).ConfigureAwait(false);
+
+                // publish an event
+                var publishOptions = new PublishOptions();
+                publishOptions.CustomizeNativeMessage(m => m.Label = "custom-label");
+                await context.Publish(new MyEvent(), publishOptions).ConfigureAwait(false);
+                #endregion
+            }
+
+            class MyCommand { }
+            class MyEvent { }
+        }
+    }
+}
