c655a3dad762c41ca070ff975e558c7771457965
Tomek Masternak <tomasz.masternak@particular.net>
2021-02-15 12:41:02 +0100

    sqlpurge sample

---
+++ samples/sqltransport/startup-purge-behavior/SqlTransport_7/Receiver/TruncateTableStartupFeature.cs
@@ -0,0 +1,20 @@
+﻿using NServiceBus.Features;
+using NServiceBus.Transport;
+
+#region TruncateTableAtStartup
+
+public class TruncateTableStartupFeature : Feature
+{
+    public TruncateTableStartupFeature()
+    {
+        EnableByDefault();
+    }
+
+    protected override void Setup(FeatureConfigurationContext context)
+    {
+        var connectionString = @"Data Source=.\sqlsexpress;Database=SQLServerTruncate;Integrated Security=True;Max Pool Size=100";
+        SqlHelper.TruncateMessageTable(connectionString, "Samples.SqlServer.TruncateReceiver");
+    }
+}
+
+#endregion
