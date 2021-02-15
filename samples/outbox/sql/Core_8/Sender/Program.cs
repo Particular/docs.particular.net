71d08ca48e3fc7dde4708ec2afb2ef24041bf14e
Tomek Masternak <tomasz.masternak@particular.net>
2021-02-15 11:03:28 +0100

    sqloutbox sample

---
+++ samples/outbox/sql/Core_8/Sender/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Transport.SqlServer;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.SqlOutbox.Sender";
+        var random = new Random();
+
+        var endpointConfiguration = new EndpointConfiguration("Samples.SqlOutbox.Sender");
+        endpointConfiguration.EnableInstallers();
+        endpointConfiguration.SendFailedMessagesTo("error");
+
+        #region SenderConfiguration
+
+        var connection = @"Data Source=.\SqlExpress;Database=NsbSamplesSqlOutbox;Integrated Security=True;Max Pool Size=100";
+
+        var transport = new SqlServerTransport(connection)
+        {
+            DefaultSchema = "sender"
+        };
+        transport.SchemaAndCatalog.UseSchemaForQueue("error", "dbo");
+        transport.SchemaAndCatalog.UseSchemaForQueue("audit", "dbo");
+
+        endpointConfiguration.UseTransport(transport);
+
+        var persistence = endpointConfiguration.UsePersistence<SqlPersistence>();
+        persistence.ConnectionBuilder(
+            connectionBuilder: () =>
+            {
+                return new SqlConnection(connection);
+            });
+        var dialect = persistence.SqlDialect<SqlDialect.MsSqlServer>();
+        dialect.Schema("sender");
+        persistence.TablePrefix("");
+
+        transport.Subscriptions.DisableCaching = true;
+        transport.Subscriptions.SubscriptionTableName = new SubscriptionTableName(
+            table: "Subscriptions", 
+            schema: "dbo");
+
+        endpointConfiguration.EnableOutbox();
+
+        #endregion
+
+        SqlHelper.CreateSchema(connection, "sender");
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+
+        Console.WriteLine("Press enter to send a message");
+        Console.WriteLine("Press any key to exit");
+
+        while (true)
+        {
+            var key = Console.ReadKey();
+            Console.WriteLine();
+
+            if (key.Key != ConsoleKey.Enter)
+            {
+                break;
+            }
+            var orderSubmitted = new OrderSubmitted
+            {
+                OrderId = Guid.NewGuid(),
+                Value = random.Next(100)
+            };
+            await endpointInstance.Publish(orderSubmitted)
+                .ConfigureAwait(false);
+        }
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
