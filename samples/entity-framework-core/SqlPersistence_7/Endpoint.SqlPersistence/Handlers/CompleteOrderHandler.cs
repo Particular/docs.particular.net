42edea3509a9efacbfbe1d929bc85f398d87af13
Tomek Masternak <tomasz.masternak@particular.net>
2021-02-15 10:46:08 +0100

    entityframework core sample

---
+++ samples/entity-framework-core/SqlPersistence_7/Endpoint.SqlPersistence/Handlers/CompleteOrderHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NServiceBus;
+using NServiceBus.Logging;
+
+public class CompleteOrderHandler
+    : IHandleMessages<CompleteOrder>
+{
+    static ILog log = LogManager.GetLogger<CompleteOrderHandler>();
+    ReceiverDataContext dataContext;
+
+    public CompleteOrderHandler(ReceiverDataContext dataContext)
+    {
+        this.dataContext = dataContext;
+    }
+
+    public async Task Handle(CompleteOrder message, IMessageHandlerContext context)
+    {
+        var order = await dataContext.Orders.FindAsync(message.OrderId)
+            .ConfigureAwait(false);
+        var shipment = await dataContext.Shipments.FirstAsync(x => x.Order.OrderId == order.OrderId)
+            .ConfigureAwait(false);
+
+        log.Info($"Completing order {order.OrderId} worth {order.Value} by shipping to {shipment.Location}.");
+    }
+}
---
+++ samples/entity-framework-core/SqlPersistence_7/Endpoint.SqlPersistence/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using NServiceBus;
+using NServiceBus.Persistence.Sql;
+
+class Program
+{
+    static async Task Main()
+    {
+        var connection = @"server=(local)\sqlexpress;database=nservicebus;Integrated Security=True;Max Pool Size=100";
+        Console.Title = "Samples.EntityFrameworkUnitOfWork.SQL";
+
+        using (var receiverDataContext = new ReceiverDataContext(new DbContextOptionsBuilder<ReceiverDataContext>()
+            .UseSqlServer(new SqlConnection(connection))
+            .Options))
+        {
+            await receiverDataContext.Database.EnsureCreatedAsync().ConfigureAwait(false);
+        }
+
+        var endpointConfiguration = new EndpointConfiguration("Samples.EntityFrameworkUnitOfWork.SQL");
+        endpointConfiguration.EnableInstallers();
+        endpointConfiguration.SendFailedMessagesTo("error");
+        endpointConfiguration.ExecuteTheseHandlersFirst(typeof(CreateOrderHandler), typeof(OrderLifecycleSaga), typeof(CreateShipmentHandler));
+
+        endpointConfiguration.UseTransport(new SqlServerTransport(connection)
+        {
+            Subscriptions = { DisableCaching = true}
+        });
+
+        var persistence = endpointConfiguration.UsePersistence<SqlPersistence>();
+        persistence.ConnectionBuilder(() => new SqlConnection(connection));
+        var dialect = persistence.SqlDialect<SqlDialect.MsSqlServer>();
+
+        #region UnitOfWork_SQL
+
+        endpointConfiguration.RegisterComponents(c =>
+        {
+            c.AddScoped(b =>
+            {
+                var session = b.GetRequiredService<ISqlStorageSession>();
+
+                var context = new ReceiverDataContext(new DbContextOptionsBuilder<ReceiverDataContext>()
+                    .UseSqlServer(session.Connection)
+                    .Options);
+
+                //Use the same underlying ADO.NET transaction
+                context.Database.UseTransaction(session.Transaction);
+
+                //Ensure context is flushed before the transaction is committed
+                session.OnSaveChanges(s => context.SaveChangesAsync());
+
+                return context;
+            });
+        });
+
+        #endregion
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+
+        await Sender.Start(endpointInstance);
+    }
+}
