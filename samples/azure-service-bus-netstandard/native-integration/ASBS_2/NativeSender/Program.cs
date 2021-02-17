0c9f0323780a78c0578aa27a9b8c2dd4cfa431b8
Tim Bussmann <timbussmann@users.noreply.github.com>
2021-02-17 11:12:14 +0100

    v2 version of ASBS native-integration sample

---
+++ samples/azure-service-bus-netstandard/native-integration/ASBS_2/NativeSender/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.ASB.NativeIntegration.Sender";
+        var connectionString = Environment.GetEnvironmentVariable("AzureServiceBus_ConnectionString");
+        if (string.IsNullOrWhiteSpace(connectionString))
+        {
+            throw new Exception("Could not read the 'AzureServiceBus_ConnectionString' environment variable. Check the sample prerequisites.");
+        }
+
+        var queueClient = new QueueClient(connectionString, "Samples.ASB.NativeIntegration");
+
+        #region SerializedMessage
+
+        var nativeMessage = new NativeMessage
+        {
+            Content = "Hello from native sender",
+            SentOnUtc = DateTime.UtcNow
+        };
+
+        var json = JsonConvert.SerializeObject(nativeMessage);
+
+        #endregion
+
+        var bytes = Encoding.UTF8.GetBytes(json);
+
+        var message = new Message(bytes)
+        {
+            MessageId = Guid.NewGuid().ToString(),
+
+            UserProperties =
+            {
+                #region NecessaryHeaders
+                ["NServiceBus.EnclosedMessageTypes"] = typeof(NativeMessage).FullName
+                #endregion
+            }
+        };
+
+        await queueClient.SendAsync(message)
+            .ConfigureAwait(false);
+
+        Console.WriteLine("Native message sent");
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+    }
+}
---
+++ samples/azure-service-bus-netstandard/native-integration/ASBS_2/Receiver/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.ASB.NativeIntegration";
+
+        #region EndpointName
+
+        var endpointConfiguration = new EndpointConfiguration("Samples.ASB.NativeIntegration");
+
+        #endregion
+
+        endpointConfiguration.SendFailedMessagesTo("error");
+        endpointConfiguration.EnableInstallers();
+        endpointConfiguration.UseSerialization<NewtonsoftSerializer>();
+        endpointConfiguration.Conventions().DefiningMessagesAs(type => type.Name == "NativeMessage");
+
+
+        var connectionString = Environment.GetEnvironmentVariable("AzureServiceBus_ConnectionString");
+        if (string.IsNullOrWhiteSpace(connectionString))
+        {
+            throw new Exception("Could not read the 'AzureServiceBus_ConnectionString' environment variable. Check the sample prerequisites.");
+        }
+
+        var transport = new AzureServiceBusTransport(connectionString);
+        endpointConfiguration.UseTransport(transport);
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
