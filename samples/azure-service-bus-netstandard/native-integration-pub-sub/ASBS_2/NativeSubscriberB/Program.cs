ee054dfb75175e35a879364dc7bb92422f100d58
Tim Bussmann <timbussmann@users.noreply.github.com>
2021-02-17 11:23:21 +0100

    version 2 of native-intergration-pub-sub ASBS sample

---
+++ samples/azure-service-bus-netstandard/native-integration-pub-sub/ASBS_2/NativeSubscriberB/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using NativeSender;
+
+class Program
+{
+    static string EnclosedMessageTypesHeader = "NServiceBus.EnclosedMessageTypes";
+
+    static string ConnectionString
+    {
+        get
+        {
+            var connectionString = Environment.GetEnvironmentVariable("AzureServiceBus_ConnectionString");
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new Exception("Could not read the 'AzureServiceBus_ConnectionString' environment variable. Check the sample prerequisites.");
+            }
+
+            return connectionString;
+        }
+    }
+
+    static async Task Main()
+    {
+        var subscriptionName = "Samples.ASB.NativeIntegration.NativeSubscriberB";
+
+        Console.Title = subscriptionName;
+
+        await TopologyManager.CreateSubscription(ConnectionString, subscriptionName, "all-events", new TrueFilter());
+
+        var subscription = new SubscriptionClient(ConnectionString, "bundle-1", subscriptionName);
+
+        subscription.RegisterMessageHandler((m, _) =>
+        {
+            var messageType = (string) m.UserProperties[EnclosedMessageTypesHeader];
+            var bodyJson = Encoding.UTF8.GetString(m.Body);
+
+            Console.WriteLine($"Received: {messageType}");
+            Console.WriteLine(bodyJson);
+
+            return Task.CompletedTask;
+        }, e => Task.CompletedTask);
+
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+    }
+}
