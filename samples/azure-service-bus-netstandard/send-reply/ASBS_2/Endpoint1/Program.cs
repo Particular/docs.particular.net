45ab1fdbcd402a3548f1c17d30050ecf0fbacc55
Tim Bussmann <timbussmann@users.noreply.github.com>
2021-02-17 14:41:41 +0100

    ASBS v2 samples (#5220)
    * add v2 version for ASBS send-reply sample
    
    * v2 version of ASBS native-integration sample
    
    * version 2 of native-intergration-pub-sub ASBS sample
    
    * add prerelease markers
    
    * align multi-target frameworks for shared project

---
+++ samples/azure-service-bus-netstandard/send-reply/ASBS_2/Endpoint1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.ASBS.SendReply.Endpoint1";
+
+        #region config
+
+        var endpointConfiguration = new EndpointConfiguration("Samples.ASBS.SendReply.Endpoint1");
+        endpointConfiguration.SendFailedMessagesTo("error");
+        endpointConfiguration.EnableInstallers();
+
+        // var transport = endpointConfiguration.UseTransport<AzureServiceBusTransport>();
+        var connectionString = Environment.GetEnvironmentVariable("AzureServiceBus_ConnectionString");
+        if (string.IsNullOrWhiteSpace(connectionString))
+        {
+            throw new Exception("Could not read the 'AzureServiceBus_ConnectionString' environment variable. Check the sample prerequisites.");
+        }
+
+        var transport = new AzureServiceBusTransport(connectionString);
+        endpointConfiguration.UseTransport(transport);
+
+        #endregion
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        Console.WriteLine("Press 'enter' to send a message");
+        Console.WriteLine("Press any other key to exit");
+
+        while (true)
+        {
+            var key = Console.ReadKey();
+            Console.WriteLine();
+
+            if (key.Key != ConsoleKey.Enter)
+            {
+                break;
+            }
+
+            var orderId = Guid.NewGuid();
+            var message = new Message1
+            {
+                Property = "Hello from Endpoint1"
+            };
+            await endpointInstance.Send("Samples.ASBS.SendReply.Endpoint2", message)
+                .ConfigureAwait(false);
+            Console.WriteLine("Message1 sent");
+        }
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
