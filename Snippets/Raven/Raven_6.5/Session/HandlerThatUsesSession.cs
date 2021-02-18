30dd1426d8ea2ed8ca5dc0e591d8584dbd8e9eec
lailabougria <laila.bougria@particular.net>
2021-02-18 08:33:48 +0100

    Add doco and snippets for raven 6.5

---
+++ Snippets/Raven/Raven_6.5/Session/HandlerThatUsesSession.cs
@@ -0,0 +1,27 @@
+﻿namespace Raven_6.Session
+{
+    using System.Threading.Tasks;
+    using NServiceBus;
+    using Raven.Client.Documents.Session;
+
+    #region ravendb-persistence-shared-session-for-handler
+
+    public class HandlerThatUsesSession : IHandleMessages<MyMessage>
+    {
+        readonly IAsyncDocumentSession ravenSession;
+
+        public HandlerThatUsesSession(IAsyncDocumentSession ravenSession)
+        {
+            this.ravenSession = ravenSession;
+        }
+
+        public Task Handle(MyMessage message, IMessageHandlerContext context)
+        {
+            var document = new MyDocument();
+            return ravenSession.StoreAsync(document);
+        }
+    }
+
+    #endregion
+
+}
---
+++ Snippets/Raven/Raven_6.5/Session/SagaThatUsesSession.cs
@@ -0,0 +1,38 @@
+﻿namespace Raven_6.Session
+{
+    using System.Threading.Tasks;
+    using NServiceBus;
+    using Raven.Client.Documents.Session;
+
+    #region ravendb-persistence-shared-session-for-saga
+
+    public class SagaThatUsesSession :
+        Saga<SagaThatUsesSession.SagaData>,
+        IHandleMessages<MyMessage>
+    {
+        readonly IAsyncDocumentSession ravenSession;
+
+        public SagaThatUsesSession(IAsyncDocumentSession ravenSession)
+        {
+            this.ravenSession = ravenSession;
+        }
+
+        public Task Handle(MyMessage message, IMessageHandlerContext context)
+        {
+            var document = new MyDocument();
+            return ravenSession.StoreAsync(document);
+        }
+
+        #endregion
+
+        protected override void ConfigureHowToFindSaga(SagaPropertyMapper<SagaData> mapper)
+        {
+        }
+
+        public class SagaData :
+            ContainSagaData
+        {
+        }
+    }
+
+}
