37dd8cfb29a78b830262dad9915e725caddbd8f8
Szymon Pobiega <szymon.pobiega@gmail.com>
2021-03-10 11:13:51 +0100

    Upgrade RabbitMQ samples to V8 of core

---
+++ samples/rabbitmq/native-integration/Rabbit_7/Receiver/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.RabbitMQ.NativeIntegration.Receiver";
+
+        #region ConfigureRabbitQueueName
+        var endpointConfiguration = new EndpointConfiguration("Samples.RabbitMQ.NativeIntegration");
+        var transport = new RabbitMQTransport(Topology.Conventional, "host=localhost");
+        endpointConfiguration.UseTransport(transport);
+        #endregion
+
+        endpointConfiguration.EnableInstallers();
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
