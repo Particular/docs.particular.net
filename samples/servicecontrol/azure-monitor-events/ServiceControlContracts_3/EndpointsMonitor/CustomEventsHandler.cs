f4ea6292c6cf1eedf505b956cbb1c247512b6d17
Tomek Masternak <tomasz.masternak@particular.net>
2021-04-16 11:48:29 +0200

    app insights integration sample

---
+++ samples/servicecontrol/azure-monitor-events/ServiceControlContracts_3/EndpointsMonitor/CustomEventsHandler.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.ApplicationInsights;
+using NServiceBus;
+using NServiceBus.Logging;
+using ServiceControl.Contracts;
+
+#region AzureMonitorConnectorEventsHandlers
+
+public class CustomEventsHandler :
+    IHandleMessages<MessageFailed>,
+    IHandleMessages<HeartbeatStopped>,
+    IHandleMessages<HeartbeatRestored>,
+    IHandleMessages<FailedMessagesArchived>,
+    IHandleMessages<FailedMessagesUnArchived>,
+    IHandleMessages<MessageFailureResolvedByRetry>,
+    IHandleMessages<MessageFailureResolvedManually>
+{
+    readonly TelemetryClient telemetryClient;
+    static ILog log = LogManager.GetLogger<CustomEventsHandler>();
+
+    public CustomEventsHandler(TelemetryClient telemetryClient)
+    {
+        this.telemetryClient = telemetryClient;
+    }
+
+    public Task Handle(MessageFailed message, IMessageHandlerContext context)
+    {
+        telemetryClient.TrackEvent("Message Failed", new Dictionary<string, string>
+        {
+            {"MessageId", message.FailedMessageId},
+            {"DetailsUrl", $"http://localhost:49205/#/failed-messages/message/{message.FailedMessageId}"}
+        });
+
+        log.Error($"Received ServiceControl 'MessageFailed' event for a {message.MessageType} with ID {message.FailedMessageId}.");
+        return Task.CompletedTask;
+    }
+
+    public Task Handle(HeartbeatStopped message, IMessageHandlerContext context)
+    {
+        telemetryClient.TrackEvent("Heartbeat Stopped", new Dictionary<string, string>
+        {
+            {"MessageId", message.EndpointName},
+            {"DetailsUrl", $"http://localhost:49205/#/endpoints/"}
+        });
+
+        log.Warn($"Heartbeats from {message.EndpointName} have stopped.");
+        return Task.CompletedTask;
+    }
+
+    public Task Handle(HeartbeatRestored message, IMessageHandlerContext context)
+    {
+        log.Info($"Heartbeats from {message.EndpointName} have been restored.");
+        return Task.CompletedTask;
+    }
+
+    public Task Handle(FailedMessagesArchived message, IMessageHandlerContext context)
+    {
+        log.Error($"Received ServiceControl 'FailedMessageArchived' with ID {message.FailedMessagesIds.FirstOrDefault()}.");
+        return Task.CompletedTask;
+    }
+    
+    public Task Handle(FailedMessagesUnArchived message, IMessageHandlerContext context)
+    {
+        log.Error($"Received ServiceControl 'FailedMessagesUnArchived' MessagesCount: {message.FailedMessagesIds.Length}.");
+        return Task.CompletedTask;
+    }   
+    
+    public Task Handle(MessageFailureResolvedByRetry message, IMessageHandlerContext context)
+    {
+        log.Error($"Received ServiceControl 'MessageFailureResolvedByRetry' with ID {message.FailedMessageId}.");
+        return Task.CompletedTask;
+    }
+
+    public Task Handle(MessageFailureResolvedManually message, IMessageHandlerContext context)
+    {
+        log.Error($"Received ServiceControl 'MessageFailureResolvedManually'  with ID {message.FailedMessageId}.");
+        return Task.CompletedTask;
+    }
+}
+
+#endregion
---
+++ samples/servicecontrol/azure-monitor-events/ServiceControlContracts_3/NServiceBusEndpoint/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "NServiceBusEndpoint";
+        var endpointConfiguration = new EndpointConfiguration("NServiceBusEndpoint");
+        endpointConfiguration.UseTransport<LearningTransport>();
+        endpointConfiguration.UseSerialization<NewtonsoftSerializer>();
+        endpointConfiguration.EnableInstallers();
+        endpointConfiguration.SendFailedMessagesTo("error");
+
+        #region DisableRetries
+
+        var recoverability = endpointConfiguration.Recoverability();
+
+        recoverability.Delayed(
+            customizations: retriesSettings =>
+            {
+                retriesSettings.NumberOfRetries(0);
+            });
+        recoverability.Immediate(
+            customizations: retriesSettings =>
+            {
+                retriesSettings.NumberOfRetries(0);
+            });
+
+        #endregion
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        Console.WriteLine("Press 'Enter' to send a new message. Press any other key to finish.");
+        while (true)
+        {
+            var key = Console.ReadKey();
+
+            if (key.Key != ConsoleKey.Enter)
+            {
+                break;
+            }
+
+            var guid = Guid.NewGuid();
+
+            var simpleMessage = new SimpleMessage
+            {
+                Id = guid
+            };
+            await endpointInstance.Send("NServiceBusEndpoint", simpleMessage)
+                .ConfigureAwait(false);
+            Console.WriteLine($"Sent a new message with Id = {guid}.");
+
+            Console.WriteLine("Press 'Enter' to send a new message. Press any other key to finish.");
+        }
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
