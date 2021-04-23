d946243e71e9a5075497c529908e8d2d49e2eeaa
Ramon Smits <ramon.smits@gmail.com>
2021-04-23 13:39:54 +0200

    Improvements related to hosting ServiceControl in Docker (#5293)
    Removed "containers-" prefix from batch files.
    Removed unused ports during maintenance.
    Added maintenance mode
    Increase memory to  8192, as the default 1000 isn't enough for SC under load
    Renamed env file to `.env` as also used in docker documentation.

--- samples/platformtools-docker-compose/ServiceControl_4/update.cmd
+++ samples/platformtools-docker-compose/ServiceControl_4/update.cmd
@@ -5,4 +5,4 @@ docker pull particular/servicecontrol.azureservicebus.monitoring-windows:4
 docker pull particular/servicecontrol.azureservicebus.audit.init-windows:4
 docker pull particular/servicecontrol.azureservicebus.audit-windows:4
 docker pull particular/servicepulse-windows:1
-docker-compose --detach %*
+docker-compose up --detach %*
