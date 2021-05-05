078d840cc88a4b8c3ade311cfc0dddfb57bb3037
Andreas Öhlund <andreas.ohlund@particular.net>
2021-05-05 15:19:02 +0200

    Clustered RabbitMQ sample (#5321)
    * First draft
    
    * use cluster mode
    
    * Disable delayed retries
    
    * Add loop
    
    * Flesh out sample instructions
    
    * Apply suggestions from code review
    
    Co-authored-by: Tim Bussmann <timbussmann@users.noreply.github.com>
    Co-authored-by: WilliamBZA <WilliamBZA@users.noreply.github.com>
    
    * Fix link
    
    * Fix link
    
    * Remove dotsettings
    
    Co-authored-by: Tim Bussmann <timbussmann@users.noreply.github.com>
    Co-authored-by: WilliamBZA <WilliamBZA@users.noreply.github.com>

---
+++ samples/rabbitmq/cluster/Rabbit_8/MyEndpoint/MyCommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+
+public class MyCommandHandler : IHandleMessages<MyCommand>
+{
+    static ILog log = LogManager.GetLogger<MyCommandHandler>();
+
+    public Task Handle(MyCommand commandMessage, IMessageHandlerContext context)
+    {
+        log.Info($"Hello from {nameof(MyCommandHandler)}");
+        return Task.CompletedTask;
+    }
+}
