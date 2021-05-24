b7f84af7fd2ced8b79f2a4abf1dc8a95853aa72a
Dan Kent <dan.kent@particular.net>
2021-05-24 17:02:13 -0400

    Added the ProgramMain region back for snippet link

--- tutorials/quickstart/Solution/Platform/Program.cs
+++ tutorials/quickstart/Solution/Platform/Program.cs
@@ -4,10 +4,14 @@ namespace Platform
 {
     class Program
     {
+        #region PlatformMain
+
         static void Main()
         {
             Console.Title = "Particular Service Platform Launcher";
             // Particular.PlatformLauncher.Launch();
         }
+
+        #endregion
     }
 }
