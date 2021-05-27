d32a65d873447a86425f4e7f8ae60bad00141831
Andreas Öhlund <andreas.ohlund@particular.net>
2021-05-27 18:10:32 +0200

    Add snippets for extensions.hosting v2

---
+++ Snippets/Extensions.Hosting/Extensions.Hosting_2/Configuration.cs
@@ -0,0 +1,27 @@
+﻿namespace Extensions.Hosting_1
+{
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Hosting;
+    using NServiceBus;
+
+    class Configuration
+    {
+        async Task ConfigureHost()
+        {
+            #region extensions-host-configuration
+
+            var host = Host.CreateDefaultBuilder()
+                .UseNServiceBus(hostBuilderContext =>
+                {
+                    var endpointConfiguration = new EndpointConfiguration("MyEndpoint");
+                    // configure endpoint here
+                    return endpointConfiguration;
+                })
+                .Build();
+
+            await host.RunAsync();
+
+            #endregion
+        }
+    }
+}
