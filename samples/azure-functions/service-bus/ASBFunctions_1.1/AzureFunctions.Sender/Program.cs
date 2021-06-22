1c167b0779da885b7d284f52903ed3aa3432d092
Sean Feldman <SeanFeldman@users.noreply.github.com>
2021-06-22 14:46:06 -0600

    Azure Functions (in-proc) 1.1.0 update (#5373)
    Co-authored-by: Brandon Ording <bording@gmail.com>

---
+++ samples/azure-functions/service-bus/ASBFunctions_1.1/AzureFunctions.Sender/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Configuration;
+using NServiceBus;
+using System.Threading.Tasks;
+using System;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.Title = "AzureFunctions.Sender";
+
+        Console.WriteLine("Press [ENTER] to send a message to the Azure ServiceBus trigger queue.");
+        Console.WriteLine("Press [Esc] to exit.");
+        while (true)
+        {
+            var key = Console.ReadKey();
+            Console.WriteLine();
+            switch (key.Key)
+            {
+                case ConsoleKey.Enter:
+                    await SendASBMessage();
+                    Console.WriteLine("Message sent.");
+                    break;
+                case ConsoleKey.Escape:
+                    await (asbEndpoint?.Stop() ?? Task.CompletedTask);
+                    return;
+            }
+        }
+    }
+
+    private static IEndpointInstance asbEndpoint;
+
+    static async Task SendASBMessage()
+    {
+        if (asbEndpoint == null)
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("local.settings.json", optional: false)
+                .Build();
+
+            var endpointConfiguration = new EndpointConfiguration("FunctionsASBSender");
+            endpointConfiguration.SendOnly();
+            endpointConfiguration.UseSerialization<NewtonsoftSerializer>();
+
+            var transport = endpointConfiguration.UseTransport<AzureServiceBusTransport>();
+            transport.ConnectionString(config.GetValue<string>("Values:AzureWebJobsServiceBus"));
+
+            asbEndpoint = await Endpoint.Start(endpointConfiguration);
+        }
+
+        await asbEndpoint.Send("ASBTriggerQueue", new TriggerMessage());
+    }
+}
---
+++ samples/azure-functions/service-bus/sample_trigger-function_ASBFunctions_[,1.0].partial.md
@@ -0,0 +1,7 @@
+`IFunctionEndpoint` is then injected into the function class:
+
+snippet: endpoint-injection
+
+And is invoked in the following manner:
+
+snippet: injected-function
