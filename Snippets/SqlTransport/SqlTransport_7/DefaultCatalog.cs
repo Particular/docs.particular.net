5c0132a125c622a59c3ba111b30369895fc395fd
Dan Kent <83468000+kentdr@users.noreply.github.com>
2021-06-22 13:05:32 -0400

    Added 7.0 snippet for Default Catalog.

---
+++ Snippets/SqlTransport/SqlTransport_7/DefaultCatalog.cs
@@ -0,0 +1,19 @@
+﻿using NServiceBus;
+
+namespace SqlTransport_6._3
+{
+    class DefaultCatalog
+    {
+        void OverwriteDefaultCatalog()
+        {
+            #region sqlserver-default-catalog
+
+            var transport = new SqlServerTransport("connectionString")
+            {
+               DefaultCatalog = "mycatalog"
+            };
+
+            #endregion
+        }
+    }
+}
