99aca8c080f4af14d6833d3d46aca29f8f6ecc5d
David Boike <david.boike@gmail.com>
2021-06-23 08:07:43 -0500

    Updated table creation scripts

---
+++ Snippets/SqlTransport/SqlTransport_6.3/RuntimeScripts/CreateSubscriptionTableText.sql
@@ -0,0 +1,34 @@
+startcode CreateSubscriptionTableTextSql
+
+
+IF EXISTS (
+    SELECT *
+    FROM {1}.sys.objects
+    WHERE object_id = OBJECT_ID(N'{0}')
+        AND type in (N'U'))
+RETURN
+
+EXEC sp_getapplock @Resource = '{0}_lock', @LockMode = 'Exclusive'
+
+IF EXISTS (
+    SELECT *
+    FROM {1}.sys.objects
+    WHERE object_id = OBJECT_ID(N'{0}')
+        AND type in (N'U'))
+BEGIN
+    EXEC sp_releaseapplock @Resource = '{0}_lock'
+    RETURN
+END
+
+CREATE TABLE {0} (
+    QueueAddress NVARCHAR(200) NOT NULL,
+    Endpoint NVARCHAR(200),
+    Topic NVARCHAR(200) NOT NULL,
+    PRIMARY KEY CLUSTERED
+    (
+        Endpoint,
+        Topic
+    )
+)
+EXEC sp_releaseapplock @Resource = '{0}_lock'
+endcode
