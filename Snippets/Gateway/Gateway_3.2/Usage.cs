1107f7e25d698089179fbe2a2208e13931b8b89a
David Boike <david.boike@gmail.com>
2021-06-28 15:46:40 -0500

    Document Gateway SetRepyToUri() (#5381)

---
+++ Snippets/Gateway/Gateway_3.2/Usage.cs
@@ -0,0 +1,20 @@
+﻿using NServiceBus;
+using NServiceBus.Gateway;
+
+class Usage
+{
+    void ReplyUri(EndpointConfiguration endpointConfiguration)
+    {
+        #region SetReplyToUri
+
+        var gatewaySettings = endpointConfiguration.Gateway();
+
+        // Local HTTP binding address uses wilcard domain
+        gatewaySettings.AddReceiveChannel("http://+:12345/MyEndpoint/");
+
+        // Set the reply-to URI as the public address of a load balancer or proxy
+        gatewaySettings.SetReplyToUri("http://my-public-domain.com:54321/MyEndpoint/");
+
+        #endregion
+    }
+}
