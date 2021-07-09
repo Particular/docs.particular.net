bc4d697c2ad3872b4e681c12c884c499cd95d15d
Szymon Pobiega <szymon.pobiega@gmail.com>
2021-07-09 14:20:59 +0200

    Explain raw messaging using NSB V8

---
+++ Snippets/RawMessaging/Core_8/Usage.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Routing;
+using NServiceBus.Transport;
+
+public class Usage
+{
+    static async Task Start()
+    {
+        #region Configuration
+
+        var transport = new LearningTransport();
+
+        var hostSettings = new HostSettings(
+            name: "MyRawEndpoint",
+            hostDisplayName: "My Raw Endpoint",
+            startupDiagnostic: new StartupDiagnosticEntries(),
+            criticalErrorAction: (message, exception, token) =>
+            {
+                Console.WriteLine("Critical error: " + exception);
+            },
+            setupInfrastructure: true);
+
+        var infrastructure = await transport.Initialize(hostSettings, new[]
+            {
+                new ReceiveSettings(
+                    id: "1",
+                    receiveAddress: "MyQueue",
+                    usePublishSubscribe: false,
+                    purgeOnStartup: false,
+                    errorQueue: "error")
+            }, new string[0])
+            .ConfigureAwait(false);
+
+        var sender = infrastructure.Dispatcher;
+
+        #endregion
+
+        #region Sending
+
+        var body = Serialize();
+        var headers = new Dictionary<string, string>
+        {
+            ["SomeHeader"] = "SomeValue"
+        };
+        var request = new OutgoingMessage(
+            messageId: Guid.NewGuid().ToString(),
+            headers: headers,
+            body: body);
+
+        var operation = new TransportOperation(
+            request,
+            new UnicastAddressTag("Receiver"));
+
+        await sender.Dispatch(
+                outgoingMessages: new TransportOperations(operation),
+                transaction: new TransportTransaction())
+            .ConfigureAwait(false);
+
+        #endregion
+
+        #region Receiving
+
+        var receiver = infrastructure.Receivers["1"];
+        await receiver.Initialize(new PushRuntimeSettings(8),
+            onMessage: (context, token) =>
+            {
+                var message = Deserialize(context.Body);
+                Console.WriteLine(message);
+                return Task.CompletedTask;
+            },
+            onError: (context, token) => Task.FromResult(ErrorHandleResult.RetryRequired));
+
+        await receiver.StartReceive().ConfigureAwait(false);
+
+        #endregion
+
+        #region Shutdown
+
+        await receiver.StopReceive().ConfigureAwait(false);
+        await infrastructure.Shutdown().ConfigureAwait(false);
+
+        #endregion
+    }
+
+
+    static byte[] Serialize()
+    {
+        throw new NotImplementedException();
+    }
+
+    static string Deserialize(byte[] body)
+    {
+        throw new NotImplementedException();
+    }
+}
--- nservicebus/rawmessaging/index.md
+++ nservicebus/rawmessaging/index.md
@@ -2,30 +2,7 @@
 title: Raw messaging using NServiceBus
 summary: How to send and receive raw messages using NServiceBus transport infrastructure
 component: RawMessaging
-reviewed: 2020-07-22
+reviewed: 2021-07-09
 ---
 
-`NServiceBus.Raw` allows sending and receiving raw messages using the [NServiceBus transport infrastructure](/transports/). It is flexible in terms of message manipulation, therefore it is a good fit for integrations with 3rd party systems, building gateways and bridges.
-
-
-## Configuration
-
-Configuration of raw endpoints is similar to the standard NServiceBus [endpoint configuration](/nservicebus/endpoints/specify-endpoint-name.md):
-
-snippet: Configuration
-
-
-## Sending
-
-The following code sends a message to another endpoint:
-
-snippet: Sending
-
-
-## Receiving
-
-The following code implements the callback invoked when a message arrives at a raw endpoint:
-
-snippet: Receiving
-
-Notice the method gets a `dispatcher` object which can be used to send messages. The transport transaction object can be passed from the receiving context to the dispatcher, in order to ensure transactions span both send and receive. Make sure that the underlying transport infrastructure supports the [`SendsAtomicWithReceive` mode](/transports/transactions.md#transactions-transport-transaction-sends-atomic-with-receive) when using this option.
+partial: content
