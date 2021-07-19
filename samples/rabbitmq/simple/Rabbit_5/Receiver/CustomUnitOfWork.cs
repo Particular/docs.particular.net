ec926e2af82e47c6d589806bcd31361b961b6b00
williambza <william.brander@gmail.com>
2021-07-19 13:40:24 +0200

    Add net6 as a build target for samples

---
+++ samples/rabbitmq/simple/Rabbit_5/Receiver/CustomUnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data;
+using System.Reflection;
+using System.Threading.Tasks;
+using NServiceBus.UnitOfWork;
+
+namespace Receiver
+{
+    /// <summary>
+    /// Custom implementation of Unit of Work for NServiceBus endpoint.
+    /// </summary>
+    public class CustomUnitOfWork : IManageUnitsOfWork
+    {
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="CustomUnitOfWork"/> class.
+        /// </summary>
+        /// <param name="transaction">Db transaction</param>
+        public CustomUnitOfWork()
+        {
+        }
+
+        /// <inheritdoc />
+        public Task Begin()
+        {
+
+            Console.WriteLine("Transaction Started.");
+            return Task.CompletedTask;
+        }
+
+        /// <inheritdoc />
+        public Task End(Exception exception = null)
+        {
+                return Task.CompletedTask;
+        }
+    }
+}
