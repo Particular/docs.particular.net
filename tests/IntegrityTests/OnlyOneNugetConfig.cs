33cb3eeed1e22ccab7dd7973c7b974a0d3c5746e
David Boike <david.boike@gmail.com>
2021-07-19 14:46:39 -0500

    Fix setup-dotnet usages & nuget.config integration test (#5403)
    * Fix setup-dotnet usages
    
    * Update build-projects.yml
    
    * add temp whitespace
    
    * Update setup-dotnet action
    
    * Revert "add temp whitespace"
    
    This reverts commit a185b8b6020b09c276c0994b8ef4c507bb82bbd1.
    
    * Fix nuget.config situation
    
    * Updates to workflows

---
+++ tests/IntegrityTests/OnlyOneNugetConfig.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace IntegrityTests
+{
+    public class OnlyOneNugetConfig
+    {
+        [Test]
+        public void VerifyProjectsDontHaveExtraNugetConfigs()
+        {
+            var rootPath = Path.Combine(TestSetup.DocsRootPath, "nuget.config");
+
+            new TestRunner("nuget.config", "Projects in this repo should not have their own nuget.config entries, they use the one at the repo root. The DocsEngine inserts the nuget.config file into generated zip downloads")
+                .Run(path =>
+                {
+                    return string.Equals(rootPath, path, StringComparison.OrdinalIgnoreCase);
+                });
+        }
+    }
+}
