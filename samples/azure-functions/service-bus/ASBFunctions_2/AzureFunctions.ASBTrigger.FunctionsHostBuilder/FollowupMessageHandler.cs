f0e173c612eb7ff8e0be03a5db4b214dcafd8aab
Mike Minutillo <mike.minutillo@particular.net>
2021-08-25 10:39:09 +0800

    Update sample to v2

---
+++ samples/azure-functions/service-bus/ASBFunctions_2/AzureFunctions.ASBTrigger.FunctionsHostBuilder/FollowupMessageHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+
+#region FollowupMessageHandler
+
+public class FollowupMessageHandler : IHandleMessages<FollowupMessage>
+{
+    static readonly ILog Log = LogManager.GetLogger<FollowupMessageHandler>();
+    readonly CustomComponent customComponent;
+
+    public FollowupMessageHandler(CustomComponent customComponent)
+    {
+        this.customComponent = customComponent;
+    }
+
+    public Task Handle(FollowupMessage message, IMessageHandlerContext context)
+    {
+        Log.Warn($"Handling {nameof(FollowupMessage)} in {nameof(FollowupMessageHandler)}.");
+        Log.Warn($"Custom component returned: {customComponent.GetValue()}");
+
+        return Task.CompletedTask;
+    }
+}
+
+#endregion
