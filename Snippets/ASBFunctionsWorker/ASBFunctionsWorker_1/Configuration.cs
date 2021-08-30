f245b1776c49856b897ecd38d92011f856e5eb5b
Tim Bussmann <timbussmann@users.noreply.github.com>
2021-08-30 16:46:15 +0200

    add snippets project

---
+++ Snippets/ASBFunctionsWorker/ASBFunctionsWorker_1/Configuration.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Hosting;
+using NServiceBus;
+
+#region configuration
+[assembly: NServiceBusTriggerFunction("WorkerDemoEndpoint")]
+
+public class Program
+{
+    public static void Main()
+    {
+        var host = new HostBuilder()
+            .ConfigureFunctionsWorkerDefaults()
+            .UseNServiceBus()
+            .Build();
+
+        host.Run();
+    }
+}
+#endregion configuration
+
+class EnableDiagnostics
+{
+    #region enable-diagnostics
+    public static void Main()
+    {
+        var host = new HostBuilder()
+            .ConfigureFunctionsWorkerDefaults()
+            .UseNServiceBus(configuration =>
+            {
+                configuration.LogDiagnostics();
+            })
+            .Build();
+
+        host.Run();
+    }
+    #endregion
+}
+
+class ConfigureErrorQueue
+{
+    #region configure-error-queue
+    public static void Main()
+    {
+        var host = new HostBuilder()
+            .ConfigureFunctionsWorkerDefaults()
+            .UseNServiceBus(configuration =>
+            {
+                // Change the error queue name:
+                configuration.AdvancedConfiguration.SendFailedMessagesTo("my-custom-error-queue");
+
+                // Or disable the error queue to let ASB native dead-lettering handle repeated failures:
+                configuration.DoNotSendMessagesToErrorQueue();
+            })
+            .Build();
+
+        host.Run();
+    }
+    #endregion
+}
