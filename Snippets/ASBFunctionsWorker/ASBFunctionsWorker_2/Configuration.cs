e777c06b02a612d805c53d3ca797694b66ce5c0a
Tim Bussmann <timbussmann@users.noreply.github.com>
2021-09-01 14:52:53 +0200

    add version 2 snippets (#5456)

---
+++ Snippets/ASBFunctionsWorker/ASBFunctionsWorker_2/Configuration.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Hosting;
+using NServiceBus;
+
+#region asb-function-isolated-configuration
+[assembly: NServiceBusTriggerFunction("WorkerDemoEndpoint")]
+
+public class Program
+{
+    public static void Main()
+    {
+        var host = new HostBuilder()
+            .ConfigureFunctionsWorkerDefaults()
+            .UseNServiceBus()
+            .Build();
+
+        host.Run();
+    }
+}
+#endregion asb-function-isolated-configuration
+
+class EnableDiagnostics
+{
+    #region asb-function-isolated-enable-diagnostics
+    public static void Main()
+    {
+        var host = new HostBuilder()
+            .ConfigureFunctionsWorkerDefaults()
+            .UseNServiceBus(configuration =>
+            {
+                configuration.LogDiagnostics();
+            })
+            .Build();
+
+        host.Run();
+    }
+    #endregion
+}
+
+class ConfigureErrorQueue
+{
+    #region asb-function-isolated-configure-error-queue
+    public static void Main()
+    {
+        var host = new HostBuilder()
+            .ConfigureFunctionsWorkerDefaults()
+            .UseNServiceBus(configuration =>
+            {
+                // Change the error queue name:
+                configuration.AdvancedConfiguration.SendFailedMessagesTo("my-custom-error-queue");
+
+                // Or disable the error queue to let ASB native dead-lettering handle repeated failures:
+                configuration.DoNotSendMessagesToErrorQueue();
+            })
+            .Build();
+
+        host.Run();
+    }
+    #endregion
+}
