60d3a984261648fd97d16ef2db3552449ddb4627
williambza <william.brander@gmail.com>
2021-09-07 15:18:53 +0200

    Add Core_8 for instance mapping sample

---
+++ samples/routing/instance-mapping-file/Core_8/Billing/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.InstanceMappingFile.Billing";
+        var endpointConfiguration = new EndpointConfiguration("Samples.InstanceMappingFile.Billing");
+        endpointConfiguration.UsePersistence<NonDurablePersistence>();
+        endpointConfiguration.EnableInstallers();
+        endpointConfiguration.SendFailedMessagesTo("error");
+
+        var routing = endpointConfiguration.UseTransport(new MsmqTransport());
+        var routingTable = routing.InstanceMappingFile();
+        routingTable.FilePath(@"..\..\..\..\instance-mapping.xml");
+        routing.RegisterPublisher(typeof(OrderAccepted), "Samples.InstanceMappingFile.Sales");
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
