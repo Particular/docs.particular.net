325ba67dfbf9a9e4e9392d23acffc7e004a89322
Szymon Pobiega <szymon.pobiega@gmail.com>
2021-09-09 10:49:56 +0200

    Update asp-web-application sample to latest core alpha

---
+++ samples/web/asp-web-application/Core_8/WebApplication/Pages/Index.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NServiceBus;
+
+namespace WebApplication.Core.Pages
+{
+    [IgnoreAntiforgeryToken]
+    public class IndexModel : PageModel
+    {
+        IMessageSession messageSession;
+
+        public string ResponseText { get; set; }
+
+        public IndexModel(IMessageSession messageSession)
+        {
+            this.messageSession = messageSession;
+        }
+
+        public async Task<IActionResult> OnPostAsync(string textField)
+        {
+            if (string.IsNullOrWhiteSpace(textField))
+            {
+                return Page();
+            }
+
+            #region ActionHandling
+
+            if (!int.TryParse(textField, out var number))
+            {
+                return Page();
+            }
+            var command = new Command
+            {
+                Id = number
+            };
+
+            var sendOptions = new SendOptions();
+            sendOptions.SetDestination("Samples.AsyncPages.Server");
+
+            var code = await messageSession.Request<ErrorCodes>(command, sendOptions);
+            ResponseText = Enum.GetName(typeof(ErrorCodes), code);
+
+            return Page();
+            #endregion
+        }
+    }
+}
---
+++ samples/web/asp-web-application/Core_8/WebApplication/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using NServiceBus;
+
+namespace WebApplication.Core
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Build().Run();
+        }
+
+        public static IHostBuilder BuildWebHost(string[] args) =>
+        #region ApplicationStart
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(builder => builder.UseStartup<Startup>())
+                .UseNServiceBus(context =>
+                {
+                    var endpointConfiguration = new EndpointConfiguration("Samples.AsyncPages.WebApplication");
+                    endpointConfiguration.MakeInstanceUniquelyAddressable("1");
+                    endpointConfiguration.EnableCallbacks();
+                    endpointConfiguration.UseTransport(new LearningTransport());
+                    return endpointConfiguration;
+                });
+        #endregion
+    }
+}
