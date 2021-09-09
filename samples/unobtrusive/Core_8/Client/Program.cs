d56de0a6674c694ea30d890332926769a5df4329
Szymon Pobiega <szymon.pobiega@gmail.com>
2021-09-09 12:11:16 +0200

    V8 version of Unobtrusive sample

---
+++ samples/unobtrusive/Core_8/Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.Unobtrusive.Client";
+        var endpointConfiguration = new EndpointConfiguration("Samples.Unobtrusive.Client");
+        endpointConfiguration.UseTransport(new LearningTransport());
+        var dataBus = endpointConfiguration.UseDataBus<FileShareDataBus>();
+        dataBus.BasePath(@"..\..\..\..\DataBusShare\");
+
+        endpointConfiguration.ApplyCustomConventions();
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        await CommandSender.Start(endpointInstance)
+            .ConfigureAwait(false);
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
+
