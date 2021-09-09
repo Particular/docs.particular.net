62b0caeaeb4c8d686e8d81678b5d24ec4fa185ca
Szymon Pobiega <szymon.pobiega@gmail.com>
2021-09-09 12:11:43 +0200

    V8 version of versioning sample

---
+++ samples/versioning/Core_8/V1Subscriber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.Versioning.V1Subscriber";
+        var endpointConfiguration = new EndpointConfiguration("Samples.Versioning.V1Subscriber");
+        endpointConfiguration.UsePersistence<NonDurablePersistence>();
+        var transport = endpointConfiguration.UseTransport(new MsmqTransport());
+        endpointConfiguration.SendFailedMessagesTo("error");
+        endpointConfiguration.EnableInstallers();
+
+        #region V1SubscriberMapping
+
+        transport.RegisterPublisher(
+            assembly: typeof(V1.Messages.ISomethingHappened).Assembly,
+            publisherEndpoint: "Samples.Versioning.V2Publisher");
+
+        #endregion
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
---
+++ samples/versioning/Core_8/V2.Messages/ISomethingHappened.cs
@@ -0,0 +1,11 @@
+﻿#region V2Message
+namespace V2.Messages
+{
+    public interface ISomethingHappened :
+        V1.Messages.ISomethingHappened
+    {
+        string MoreInfo { get; set; }
+    }
+}
+
+#endregion
