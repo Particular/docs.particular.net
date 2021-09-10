b6db78893d0f660062d28653c0025dcd1da950e6
williambza <william.brander@gmail.com>
2021-09-10 10:25:56 +0200

    Add Core_8 sample for features

---
+++ samples/feature/Core_8/Sample/DiagnosticsFeature.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NServiceBus.Features;
+
+#region DiagnosticsFeature
+
+public class DiagnosticsFeature :
+    Feature
+{
+    internal DiagnosticsFeature()
+    {
+        EnableByDefault();
+    }
+
+    protected override void Setup(FeatureConfigurationContext context)
+    {
+        context.Services.AddSingleton<CustomLogger>();
+    }
+}
+
+#endregion
