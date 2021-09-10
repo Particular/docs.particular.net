edce173f3937c0cb736e81162a317de0c1f0ac35
williambza <william.brander@gmail.com>
2021-09-10 09:56:30 +0200

    Add Core_8 sample for consumer driven contracts

---
+++ samples/consumer-driven-contracts/Core_8/Consumer1/Contracts/Consumer1Contract.cs
@@ -0,0 +1,10 @@
+﻿namespace Subscriber1.Contracts
+{
+    using NServiceBus;
+
+    public interface Consumer1Contract :
+        IEvent
+    {
+        string Consumer1Property { get; set; }
+    }
+}
