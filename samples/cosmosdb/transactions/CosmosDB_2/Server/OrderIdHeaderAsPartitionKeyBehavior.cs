4636ddb5d5a9af57ef7ea1582eaa3f00f0359056
williambza <william.brander@gmail.com>
2021-09-10 10:01:17 +0200

    Add Core_8 sample for cosmos transactions

---
+++ samples/cosmosdb/transactions/CosmosDB_2/Server/OrderIdHeaderAsPartitionKeyBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+using NServiceBus.Logging;
+using NServiceBus.Pipeline;
+
+#region BehaviorUsingHeader
+class OrderIdHeaderAsPartitionKeyBehavior : Behavior<ITransportReceiveContext>
+{
+    public override Task Invoke(ITransportReceiveContext context, Func<Task> next)
+    {
+        if (context.Message.Headers.TryGetValue("Sample.CosmosDB.Transaction.OrderId", out var orderId))
+        {
+            Log.Info($"Found partition key '{orderId}' from header 'Sample.CosmosDB.Transaction'");
+
+            context.Extensions.Set(new PartitionKey(orderId));
+        }
+
+        return next();
+    }
+
+    static readonly ILog Log = LogManager.GetLogger<OrderIdAsPartitionKeyBehavior>();
+}
+#endregion
---
+++ samples/cosmosdb/transactions/CosmosDB_2/Server/ShipOrderHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+using NServiceBus;
+using NServiceBus.Logging;
+
+#region UseHeader
+public class ShipOrderHandler :
+    IHandleMessages<ShipOrder>
+{
+    public Task Handle(ShipOrder message, IMessageHandlerContext context)
+    {
+        var orderShippingInformation = new OrderShippingInformation
+        {
+            Id = Guid.NewGuid(),
+            OrderId = message.OrderId,
+            ShippedAt = DateTimeOffset.UtcNow,
+        };
+
+        Store(orderShippingInformation, context);
+
+        Log.Info($"Order Shipped. OrderId {message.OrderId}");
+
+        var options = new PublishOptions();
+        options.SetHeader("Sample.CosmosDB.Transaction.OrderId", message.OrderId.ToString());
+
+        return context.Publish(new OrderShipped { OrderId = orderShippingInformation.OrderId, ShippingDate = orderShippingInformation.ShippedAt }, options);
+    }
+
+    private static void Store(OrderShippingInformation orderShippingInformation, IMessageHandlerContext context)
+    {
+        var session = context.SynchronizedStorageSession.CosmosPersistenceSession();
+        var requestOptions = new TransactionalBatchItemRequestOptions
+        {
+            EnableContentResponseOnWrite = false,
+        };
+
+        session.Batch.CreateItem(orderShippingInformation, requestOptions);
+    }
+
+    static ILog Log = LogManager.GetLogger<ShipOrderHandler>();
+}
+#endregion
