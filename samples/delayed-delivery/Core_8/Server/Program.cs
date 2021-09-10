7536e9da446cdfa6ee0e895bf001d029ad19028f
williambza <william.brander@gmail.com>
2021-09-10 10:14:51 +0200

    Add Core_8 sample for delayed delivery

---
+++ samples/delayed-delivery/Core_8/Server/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.DelayedDelivery.Server";
+        var endpointConfiguration = new EndpointConfiguration("Samples.DelayedDelivery.Server");
+        endpointConfiguration.UsePersistence<LearningPersistence>();
+        endpointConfiguration.UseTransport(new LearningTransport());
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
