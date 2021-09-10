09469f3c458210fa4fe10c37b9b9a4771dd05b25
williambza <william.brander@gmail.com>
2021-09-10 15:11:17 +0200

    Add Core_8 sample for sender side distribution

---
+++ samples/scaleout/senderside/Core_8/Server2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Configuration;
+using System.Threading.Tasks;
+using NServiceBus;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.SenderSideScaleOut.Server2";
+        var endpointConfiguration = new EndpointConfiguration("Samples.SenderSideScaleOut.Server");
+        var discriminator = ConfigurationManager.AppSettings["InstanceId"];
+        endpointConfiguration.MakeInstanceUniquelyAddressable(discriminator);
+        endpointConfiguration.UsePersistence<NonDurablePersistence>();
+        endpointConfiguration.UseTransport(new MsmqTransport());
+        endpointConfiguration.EnableInstallers();
+        endpointConfiguration.SendFailedMessagesTo("error");
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        Console.WriteLine("Press enter to exit.");
+        Console.ReadLine();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
