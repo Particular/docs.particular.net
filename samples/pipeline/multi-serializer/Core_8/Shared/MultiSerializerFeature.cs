4b6120a0e427e5406ac433f101878c9d432bf9af
williambza <william.brander@gmail.com>
2021-09-10 14:07:38 +0200

    Add Core_8 multi-serializer sample

---
+++ samples/pipeline/multi-serializer/Core_8/Shared/MultiSerializerFeature.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NServiceBus;
+using NServiceBus.Features;
+
+#region behavior-configuration
+
+public class MultiSerializerFeature :
+    Feature
+{
+    internal MultiSerializerFeature()
+    {
+        EnableByDefault();
+    }
+
+    protected override void Setup(FeatureConfigurationContext context)
+    {
+        var pipeline = context.Pipeline;
+        pipeline.Replace("DeserializeLogicalMessagesConnector", typeof(DeserializeConnector));
+        pipeline.Replace("SerializeMessageConnector", typeof(SerializeConnector));
+        context.Services.AddSingleton<SerializationMapper>();
+    }
+}
+
+#endregion
+
