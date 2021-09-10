d463a2ec829feae24cd41283f0b1f16104e7fc6a
Jayanthi Sourirajan <Admin@DESKTOP-H8RCJI1>
2021-09-10 07:25:58 -0700

    Added V8 sample for serializers\multiple-deserializers

---
+++ samples/serializers/multiple-deserializers/Core_8/XmlEndpoint/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+static class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.MultipleDeserializers.XmlEndpoint";
+        #region configXml
+        var endpointConfiguration = new EndpointConfiguration("Samples.MultipleDeserializers.XmlEndpoint");
+        endpointConfiguration.UseSerialization<XmlSerializer>();
+        endpointConfiguration.RegisterOutgoingMessageLogger();
+        #endregion
+        endpointConfiguration.UseTransport(new LearningTransport());
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        var message = MesasgeBuilder.BuildMessage();
+        await endpointInstance.Send("Samples.MultipleDeserializers.ReceivingEndpoint", message)
+            .ConfigureAwait(false);
+        Console.WriteLine("Order Sent");
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
