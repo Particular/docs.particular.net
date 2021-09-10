12d0263d103ba857d2411b7ca38ca8b0b76e1db1
Szymon Pobiega <szymon.pobiega@gmail.com>
2021-09-10 08:42:56 +0200

    V8 version of unit-of-work sample

---
+++ samples/unit-of-work/Core_8/Sample/CustomManageUnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus.Logging;
+using NServiceBus.UnitOfWork;
+
+#pragma warning disable 618
+
+#region CustomManageUnitOfWork
+
+public class CustomManageUnitOfWork :
+    IManageUnitsOfWork
+{
+    static ILog log = LogManager.GetLogger("CustomManageUnitOfWork");
+
+    public Task Begin()
+    {
+        log.Info("Begin");
+        return Task.CompletedTask;
+    }
+
+    public Task End(Exception exception = null)
+    {
+        if (exception == null)
+        {
+            log.Info("End Success");
+        }
+        else
+        {
+            log.Error("End Fail", exception);
+        }
+        return Task.CompletedTask;
+    }
+}
+
+#endregion
+
+#pragma warning restore 618
