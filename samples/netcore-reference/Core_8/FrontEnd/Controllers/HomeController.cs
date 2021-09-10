abfa17e9c48c799fd34ef1aed2f441bf90ee8391
williambza <william.brander@gmail.com>
2021-09-10 13:30:35 +0200

    Add Core_8 netcore sample

---
+++ samples/netcore-reference/Core_8/FrontEnd/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using FrontEnd.Models;
+using Messages;
+using Microsoft.AspNetCore.Mvc;
+using NServiceBus;
+
+namespace FrontEnd
+{
+    #region front-end-controller
+    public class HomeController : Controller
+    {
+        IMessageSession messageSession;
+        
+        public HomeController(IMessageSession messageSession)
+        {
+            this.messageSession = messageSession;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public async Task<ActionResult> Index(ViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            
+            await messageSession.Send(new SomeMessage { Number = model.Number })
+                .ConfigureAwait(false);
+
+            ViewBag.Message = "Request was queued successfully.";
+            
+            return View(model);
+        }
+    }
+    #endregion
+}
