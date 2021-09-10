85d97863fbf79a087e9ef6710913e1be5df2bcee
williambza <william.brander@gmail.com>
2021-09-10 14:38:44 +0200

    Add Core_8 sample for plugins

---
+++ samples/plugin-based-config/Core_8/Shared/IRunAfterEndpointStop.cs
@@ -0,0 +1,6 @@
+﻿using System.Threading.Tasks;
+
+public interface IRunAfterEndpointStop
+{
+    Task Run();
+}
---
+++ samples/plugin-based-config/Core_8/Shared/IRunBeforeEndpointStart.cs
@@ -0,0 +1,6 @@
+using System.Threading.Tasks;
+
+public interface IRunBeforeEndpointStart
+{
+    Task Run();
+}
