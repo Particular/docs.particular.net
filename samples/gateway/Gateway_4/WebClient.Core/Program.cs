a7e171ea1d9910b9d319b52750d5afb99fcc107e
williambza <william.brander@gmail.com>
2021-09-10 10:35:39 +0200

    Add Core_8 sample for gateway

---
+++ samples/gateway/Gateway_4/WebClient.Core/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace WebClient.Core
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
