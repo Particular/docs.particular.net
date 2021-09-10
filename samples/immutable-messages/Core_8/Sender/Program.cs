3581171a815e0b69c0e391b207b4b713766d17fa
williambza <william.brander@gmail.com>
2021-09-10 10:51:51 +0200

    Add Core_8 sample for immutable messages

---
+++ samples/immutable-messages/Core_8/Sender/Program.cs
@@ -0,0 +1,35 @@
+﻿using Messages;
+using NServiceBus;
+using System;
+using System.Threading.Tasks;
+using UsingClasses.Messages;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.ImmutableMessages.UsingInterfaces.Sender";
+
+#region immutable-messages-endpoint-config
+
+        var endpointConfiguration = new EndpointConfiguration("Samples.ImmutableMessages.UsingInterfaces.Sender");
+        endpointConfiguration.UseSerialization<NewtonsoftSerializer>();
+        endpointConfiguration.UsePersistence<LearningPersistence>();
+        var routingConfiguration = endpointConfiguration.UseTransport(new LearningTransport());
+
+#endregion
+
+        routingConfiguration.RouteToEndpoint(typeof(MyMessageImpl), "Samples.ImmutableMessages.UsingInterfaces.Receiver");
+        routingConfiguration.RouteToEndpoint(typeof(MyMessage), "Samples.ImmutableMessages.UsingInterfaces.Receiver");
+
+        endpointConfiguration.ApplyCustomConventions();
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        await MessageSender.Start(endpointInstance)
+            .ConfigureAwait(false);
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
+
---
+++ samples/immutable-messages/Core_8/Shared/UsingInterfaces/IMyMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace UsingInterfaces.Messages
+{
+#region immutable-messages-as-interface
+    public interface IMyMessage
+    {
+        string Data { get; }
+    }
+#endregion
+}
