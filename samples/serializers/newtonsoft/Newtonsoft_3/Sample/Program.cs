3d39a7745aa7cc0d51ee0a8776f937aceb47300b
williambza <william.brander@gmail.com>
2021-09-13 09:06:38 +0200

    Add Core_8 sample for newtonsoft

---
+++ samples/serializers/newtonsoft/Newtonsoft_3/Sample/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using NServiceBus;
+
+static class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.Serialization.ExternalJson";
+
+        #region config
+
+        var endpointConfiguration = new EndpointConfiguration("Samples.Serialization.ExternalJson");
+        var settings = new JsonSerializerSettings
+        {
+            Formatting = Formatting.Indented
+        };
+        var serialization = endpointConfiguration.UseSerialization<NewtonsoftSerializer>();
+        serialization.Settings(settings);
+
+        #endregion
+
+        #region registermutator
+        
+        endpointConfiguration.RegisterComponents(
+            registration: components =>
+            {
+                components.AddTransient<MessageBodyWriter>();
+            });
+
+        #endregion
+
+        endpointConfiguration.UsePersistence<LearningPersistence>();
+        endpointConfiguration.UseTransport(new LearningTransport());
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+
+        #region message
+
+        var message = new CreateOrder
+        {
+            OrderId = 9,
+            Date = DateTime.Now,
+            CustomerId = 12,
+            OrderItems = new List<OrderItem>
+            {
+                new OrderItem
+                {
+                    ItemId = 6,
+                    Quantity = 2
+                },
+                new OrderItem
+                {
+                    ItemId = 5,
+                    Quantity = 4
+                },
+            }
+        };
+        await endpointInstance.SendLocal(message)
+            .ConfigureAwait(false);
+
+        #endregion
+
+        Console.WriteLine("Order Sent");
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
