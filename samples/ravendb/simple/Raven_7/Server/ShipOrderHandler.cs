a2aa2bbc682a442d56b61d8376b0a8ab8f1f0ba0
Szymon Pobiega <szymon.pobiega@gmail.com>
2021-09-14 10:44:53 +0200

    RavenDB simple sample for NSB v8

---
+++ samples/ravendb/simple/Raven_7/Server/ShipOrderHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+#region handler
+
+public class ShipOrderHandler :
+    IHandleMessages<ShipOrder>
+{
+    public Task Handle(ShipOrder message, IMessageHandlerContext context)
+    {
+        var session = context.SynchronizedStorageSession.RavenSession();
+        var orderShipped = new OrderShipped
+        {
+            OrderId = message.OrderId,
+            ShippingDate = DateTime.UtcNow,
+        };
+        return session.StoreAsync(orderShipped, context.CancellationToken);
+    }
+}
+
+#endregion
