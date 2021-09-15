d2efce38c6b8c3bee0b25000e64bc38bd7bbac0f
Jayanthi Sourirajan <Admin@DESKTOP-H8RCJI1>
2021-09-15 15:31:06 -0700

    update V8 samples for azure- web job host

---
+++ samples/azure/webjob-host/Core_8/Receiver/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Hosting;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var host = new HostBuilder()
+            .ConfigureHost()
+            .Build();
+
+        var cancellationToken = new WebJobsShutdownWatcher().Token;
+        using (host)
+        {
+            await host.RunAsync(cancellationToken)
+                .ConfigureAwait(false);
+        }
+    }
+}
