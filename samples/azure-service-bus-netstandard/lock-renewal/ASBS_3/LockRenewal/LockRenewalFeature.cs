29e2e53a9329e87d5cb448cfd7f4827acd19f6f6
Michał Wójcik <wojcik.mike@gmail.com>
2021-10-07 14:49:33 +0200

    Adding the lock-renewal sample for ASBS v3

---
+++ samples/azure-service-bus-netstandard/lock-renewal/ASBS_3/LockRenewal/LockRenewalFeature.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NServiceBus.Features;
+
+public class LockRenewalFeature : Feature
+{
+    internal LockRenewalFeature()
+    {
+        #region LockRenewalFeature
+
+        EnableByDefault();
+
+        Defaults(settings =>
+        {
+            settings.SetDefault<LockRenewalOptions>(new LockRenewalOptions
+            {
+                // NServiceBus.Transport.AzureServiceBus sets LockDuration to 5 minutes by default
+                LockDuration = TimeSpan.FromMinutes(5),
+                ExecuteRenewalBefore = TimeSpan.FromSeconds(10)
+            });
+        });
+
+        #endregion
+    }
+
+    protected override void Setup(FeatureConfigurationContext context)
+    {
+        var lockRenewalOptions = context.Settings.Get<LockRenewalOptions>();
+        var renewLockTokenIn = lockRenewalOptions.LockDuration - lockRenewalOptions.ExecuteRenewalBefore;
+
+        context.Pipeline.Register(
+            stepId: "LockRenewal",
+            factoryMethod: builder => new LockRenewalBehavior(renewLockTokenIn),
+            description: "Renew message lock token");
+    }
+}
