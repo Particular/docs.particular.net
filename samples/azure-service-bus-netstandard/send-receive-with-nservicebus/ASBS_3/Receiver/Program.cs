9cc331a37016c0875b43391f1de5b9fef5b30f6e
Michał Wójcik <wojcik.mike@gmail.com>
2021-10-08 17:12:04 +0200

    Adding a ASBS v3 sample send-receive-wtih-nservicebus

---
+++ samples/azure-service-bus-netstandard/send-receive-with-nservicebus/ASBS_3/Receiver/Program.cs
@@ -0,0 +1,41 @@
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+
+namespace Receiver
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var host = Host.CreateDefaultBuilder(args)
+                .ConfigureLogging((context, logging) =>
+                {
+                    logging.AddConfiguration(context.Configuration.GetSection("Logging"));
+
+                    logging.AddConsole();
+                })
+                .UseConsoleLifetime()
+                .UseNServiceBus(context =>
+                {
+                    var endpointConfiguration = new EndpointConfiguration("Receiver");
+
+                    var connectionString = context.Configuration.GetConnectionString("AzureServiceBusConnectionString");
+                    var transport = endpointConfiguration.UseTransport(new AzureServiceBusTransport(connectionString));
+
+                    endpointConfiguration.AuditProcessedMessagesTo("audit");
+                    endpointConfiguration.SendFailedMessagesTo("error");
+
+                    // Operational scripting: https://docs.particular.net/transports/azure-service-bus/operational-scripting
+                    endpointConfiguration.EnableInstallers();
+
+                    return endpointConfiguration;
+                })
+                .Build();
+
+            await host.RunAsync();
+        }
+    }
+}
