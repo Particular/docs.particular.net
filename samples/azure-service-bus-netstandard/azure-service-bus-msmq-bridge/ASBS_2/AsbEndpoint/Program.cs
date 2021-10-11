392fb73371a5790c739888ba7fd0b92c4e862393
Mauro Servienti <mauro.servienti@gmail.com>
2021-10-11 15:19:51 +0200

    Add azure-service-bus-msmq-bridge sample for v2

---
+++ samples/azure-service-bus-netstandard/azure-service-bus-msmq-bridge/ASBS_2/AsbEndpoint/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using NServiceBus;
+
+class Program
+{
+    static async Task Main()
+    {
+        const string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        var random = new Random();
+
+        Console.Title = "Samples.Azure.ServiceBus.AsbEndpoint";
+        var endpointConfiguration = new EndpointConfiguration("Samples.Azure.ServiceBus.AsbEndpoint");
+        endpointConfiguration.SendFailedMessagesTo("error");
+        endpointConfiguration.EnableInstallers();
+        endpointConfiguration.UsePersistence<InMemoryPersistence>();
+        endpointConfiguration.UseSerialization<NewtonsoftSerializer>();
+        endpointConfiguration.AddDeserializer<XmlSerializer>();
+
+        var transport = endpointConfiguration.UseTransport<AzureServiceBusTransport>();
+        var connectionString = Environment.GetEnvironmentVariable("AzureServiceBus.ConnectionString");
+        if (string.IsNullOrWhiteSpace(connectionString))
+        {
+            throw new Exception("Could not read the 'AzureServiceBus.ConnectionString' environment variable. Check the sample prerequisites.");
+        }
+        transport.ConnectionString(connectionString);
+
+        #region connect-asb-side-of-bridge
+
+        var routing = transport.Routing();
+        var bridge = routing.ConnectToBridge("Bridge");
+
+        #endregion
+
+        #region route-command-via-bridge
+
+        bridge.RouteToEndpoint(typeof(MyCommand), "Samples.Azure.ServiceBus.MsmqEndpoint");
+
+        #endregion
+
+        #region subscribe-to-event-via-bridge
+
+        bridge.RegisterPublisher(typeof(MyEvent), "Samples.Azure.ServiceBus.MsmqEndpoint");
+
+        #endregion
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+
+        Console.WriteLine("Press Enter to send a command");
+        Console.WriteLine("Press any other key to exit");
+
+        while (true)
+        {
+            var key = Console.ReadKey().Key;
+            if (key != ConsoleKey.Enter)
+            {
+                break;
+            }
+
+            var prop = new string(Enumerable.Range(0, 3).Select(i => letters[random.Next(letters.Length)]).ToArray());
+            await endpointInstance.Send(new MyCommand { Property = prop }).ConfigureAwait(false);
+            Console.WriteLine($"\nCommand with value '{prop}' sent");
+        }
+
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
---
+++ samples/azure-service-bus-netstandard/azure-service-bus-msmq-bridge/ASBS_2/Bridge/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Router;
+using NServiceBus.Routing;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.Title = "Samples.Azure.ServiceBus.Bridge";
+
+        var connectionString = Environment.GetEnvironmentVariable("AzureServiceBus.ConnectionString");
+        if (string.IsNullOrWhiteSpace(connectionString))
+        {
+            throw new Exception("Could not read the 'AzureServiceBus.ConnectionString' environment variable. Check the sample prerequisites.");
+        }
+
+        #region bridge-general-configuration
+
+        var bridgeConfiguration = new RouterConfiguration("Bridge");
+        var azureInterface = bridgeConfiguration.AddInterface<AzureServiceBusTransport>("ASB", transport =>
+        {
+            //Prevents ASB from using TransactionScope
+            transport.Transactions(TransportTransactionMode.ReceiveOnly);
+            transport.ConnectionString(connectionString);
+        });
+        var msmqInterface = bridgeConfiguration.AddInterface<MsmqTransport>("MSMQ", transport =>
+        {
+            transport.Transactions(TransportTransactionMode.ReceiveOnly);
+        });
+        msmqInterface.EnableMessageDrivenPublishSubscribe(new InMemorySubscriptionStorage());
+
+        //Configure the host of the MSMQ endpoint
+        msmqInterface.EndpointInstances.AddOrReplaceInstances("publishers", new List<EndpointInstance>
+        {
+            new EndpointInstance("Samples.Azure.ServiceBus.MsmqEndpoint").AtMachine(Environment.MachineName)
+        });
+
+        bridgeConfiguration.AutoCreateQueues();
+
+        var staticRouting = bridgeConfiguration.UseStaticRoutingProtocol();
+
+        staticRouting.AddForwardRoute(
+            incomingInterface: "MSMQ",
+            outgoingInterface: "ASB");
+
+        staticRouting.AddForwardRoute(
+            incomingInterface: "ASB",
+            outgoingInterface: "MSMQ");
+
+        #endregion
+
+        #region bridge-execution
+
+        var bridge = Router.Create(bridgeConfiguration);
+
+        await bridge.Start().ConfigureAwait(false);
+
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+
+        await bridge.Stop().ConfigureAwait(false);
+
+
+        #endregion
+    }
+}
