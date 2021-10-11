ae5962fc8a264a578c576c40fcce69a1f7c96a31
Mauro Servienti <mauro.servienti@gmail.com>
2021-10-11 14:28:55 +0200

    Add send-reply sample for ASBS 3.0 alpha

---
+++ samples/azure-service-bus-netstandard/send-reply/ASBS_3/Endpoint2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.ASBS.SendReply.Endpoint2";
+
+        var endpointConfiguration = new EndpointConfiguration("Samples.ASBS.SendReply.Endpoint2");
+        endpointConfiguration.SendFailedMessagesTo("error");
+        endpointConfiguration.EnableInstallers();
+
+
+        var connectionString = Environment.GetEnvironmentVariable("AzureServiceBus_ConnectionString");
+        if (string.IsNullOrWhiteSpace(connectionString))
+        {
+            throw new Exception("Could not read the 'AzureServiceBus_ConnectionString' environment variable. Check the sample prerequisites.");
+        }
+
+        var transport = new AzureServiceBusTransport(connectionString);
+        endpointConfiguration.UseTransport(transport);
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
