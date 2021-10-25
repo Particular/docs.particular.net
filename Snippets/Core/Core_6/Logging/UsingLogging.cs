e2a65d5540e44a0cacc0f63128639226f40f0ce0
Szymon Pobiega <szymon.pobiega@gmail.com>
2021-10-25 11:54:28 +0200

    Document *Format logging method behavior

--- Snippets/Core/Core_6/Logging/UsingLogging.cs
+++ Snippets/Core/Core_6/Logging/UsingLogging.cs
@@ -7,6 +7,7 @@
     public class ClassUsingLogging
     {
         static ILog log = LogManager.GetLogger<ClassUsingLogging>();
+        readonly int times = 2;
 
         public void SomeMethod()
         {
@@ -14,6 +15,7 @@
             if (log.IsDebugEnabled)
             {
                 log.Debug("Something expected happened.");
+                log.DebugFormat("Also, this other thing happened {0} times.", times);
             }
         }
     }
