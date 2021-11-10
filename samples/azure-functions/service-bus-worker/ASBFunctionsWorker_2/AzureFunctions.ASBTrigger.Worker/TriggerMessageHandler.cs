c5c6e0bc139557ec74117632069720f2b61b9b0a
Jens Pettersson <jens.pettersson@live.se>
2021-11-10 17:22:45 +0100

    Update Azure Functions Worker to v4 and .net6
    Add random exception in TriggerMessageHandler

--- samples/azure-functions/service-bus-worker/ASBFunctionsWorker_2/AzureFunctions.ASBTrigger.Worker/TriggerMessageHandler.cs
+++ samples/azure-functions/service-bus-worker/ASBFunctionsWorker_2/AzureFunctions.ASBTrigger.Worker/TriggerMessageHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using NServiceBus;
 using NServiceBus.Logging;
 
@@ -10,6 +11,12 @@ public class TriggerMessageHandler : IHandleMessages<TriggerMessage>
 
     public Task Handle(TriggerMessage message, IMessageHandlerContext context)
     {
+        var rnd = new Random().Next(10);
+        if (rnd % 2 != 0)
+        {
+            throw new Exception("BOOM!");
+        }
+
         Log.Warn($"Handling {nameof(TriggerMessage)} in {nameof(TriggerMessageHandler)}");
 
         return context.SendLocal(new FollowupMessage());
