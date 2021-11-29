71df4ef8f838b960b316627e438baed4523cdd19
Andreas Öhlund <andreas.ohlund@particular.net>
2021-11-29 09:32:50 +0100

    Fix prometheus sample

---
+++ samples/logging/prometheus-grafana/Metrics_4/Endpoint/Program.cs
@@ -0,0 +1,54 @@
+﻿using NServiceBus;
+using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.Metrics.Tracing.Endpoint";
+        var endpointConfiguration = new EndpointConfiguration("Samples.Metrics.Tracing.Endpoint");
+        endpointConfiguration.UseTransport<LearningTransport>();
+
+        #region prometheus-enable-nsb-metrics
+
+        endpointConfiguration.EnableMetrics();
+
+        #endregion
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+
+        #region prometheus-load-simulator
+
+        var simulator = new LoadSimulator(endpointInstance, TimeSpan.Zero, TimeSpan.FromSeconds(10));
+        simulator.Start();
+
+        #endregion
+
+        try
+        {
+            Console.WriteLine("Endpoint started. Press 'enter' to send a message");
+            Console.WriteLine("Press ESC key to quit");
+
+            while (true)
+            {
+                var key = Console.ReadKey(true);
+                if (key.Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+
+                await endpointInstance.SendLocal(new SomeCommand())
+                    .ConfigureAwait(false);
+            }
+        }
+        finally
+        {
+            await simulator.Stop()
+                .ConfigureAwait(false);
+            await endpointInstance.Stop()
+                .ConfigureAwait(false);
+        }
+    }
+}
---
+++ samples/logging/prometheus-grafana/Metrics_4/Endpoint/PrometheusFeature.cs
@@ -0,0 +1,129 @@
+﻿using NServiceBus;
+using NServiceBus.Features;
+using NServiceBus.Logging;
+using Prometheus;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+class PrometheusFeature : Feature
+{
+    static ILog log = LogManager.GetLogger(nameof(PrometheusFeature));
+
+    #region prometheus-name-mapping
+
+    Dictionary<string, string> nameMapping = new Dictionary<string, string>
+    {
+        // https://prometheus.io/docs/practices/naming/
+        {"# of msgs successfully processed / sec", "nservicebus_success_total"},
+        {"# of msgs pulled from the input queue /sec", "nservicebus_fetched_total"},
+        {"# of msgs failures / sec", "nservicebus_failure_total"},
+        {"Critical Time", "nservicebus_criticaltime_seconds"},
+        {"Processing Time", "nservicebus_processingtime_seconds"},
+        {"Retries", "nservicebus_retries_total"},
+    };
+
+    #endregion
+
+    public PrometheusFeature()
+    {
+        EnableByDefault();
+    }
+
+    protected override void Setup(FeatureConfigurationContext context)
+    {
+        var settings = context.Settings;
+
+        #region prometheus-register-probe
+
+        var labelValues = new[]
+        {
+            settings.EndpointName(),
+            Environment.MachineName,
+            Dns.GetHostName(),
+        };
+
+        var metricsOptions = settings.Get<MetricsOptions>();
+
+        metricsOptions.RegisterObservers(
+            register: probeContext =>
+            {
+                RegisterProbes(probeContext, labelValues);
+            });
+
+        #endregion
+
+        context.RegisterStartupTask(new MetricServerTask());
+    }
+
+    public void RegisterProbes(ProbeContext context, string[] labelValues)
+    {
+        #region prometheus-observers-registration
+
+        foreach (var duration in context.Durations)
+        {
+            if (!nameMapping.ContainsKey(duration.Name))
+            {
+                log.WarnFormat("Unsupported duration probe {0}", duration.Name);
+                continue;
+            }
+            var prometheusName = nameMapping[duration.Name];
+            var summary = Metrics.CreateSummary(prometheusName, duration.Description,
+                                                new SummaryConfiguration
+                                                {
+                                                    Objectives = new[]
+                                                                 {
+                                                                     new QuantileEpsilonPair(0.5, 0.05),
+                                                                     new QuantileEpsilonPair(0.9, 0.01),
+                                                                     new QuantileEpsilonPair(0.99, 0.001)
+                                                                 },
+                                                    LabelNames = Labels
+                                                });
+            duration.Register((ref DurationEvent @event) => summary.Labels(labelValues).Observe(@event.Duration.TotalSeconds));
+        }
+
+        foreach (var signal in context.Signals)
+        {
+            if (!nameMapping.ContainsKey(signal.Name))
+            {
+                log.WarnFormat("Unsupported signal probe {0}", signal.Name);
+                continue;
+            }
+            var prometheusName = nameMapping[signal.Name];
+            var counter = Metrics.CreateCounter(prometheusName, signal.Description, Labels);
+            signal.Register((ref SignalEvent @event) => counter.Labels(labelValues).Inc());
+        }
+
+        #endregion
+    }
+
+    #region prometheus-flush-probe
+
+    class MetricServerTask : FeatureStartupTask
+    {
+        MetricServer metricServer = new MetricServer(port: 3030);
+
+        protected override Task OnStart(IMessageSession session, CancellationToken cancellationToken = default)
+        {
+            metricServer.Start();
+            return Task.CompletedTask;
+        }
+
+        protected override Task OnStop(IMessageSession session, CancellationToken cancellationToken = default)
+        {
+            metricServer.Stop();
+            return Task.CompletedTask;
+        }
+    }
+
+    #endregion
+
+    static string[] Labels =
+    {
+        "endpoint",
+        "machinename",
+        "hostname"
+    };
+}
