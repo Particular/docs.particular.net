78a2c02382ec7f82d8b0880cd438592c1a45112e
danielmarbach <daniel.marbach@openplace.net>
2021-12-01 17:16:23 +0100

    V8 project

---
+++ Snippets/Raven/Raven_8/Outbox.cs
@@ -0,0 +1,17 @@
+﻿namespace Raven_6
+{
+    using System;
+    using NServiceBus;
+
+    class Outbox
+    {
+        Outbox(EndpointConfiguration endpointConfiguration)
+        {
+            #region OutboxRavendBTimeToKeep
+            var outboxSettings = endpointConfiguration.EnableOutbox();
+            outboxSettings.SetTimeToKeepDeduplicationData(TimeSpan.FromDays(7));
+            outboxSettings.SetFrequencyToRunDeduplicationDataCleanup(TimeSpan.FromMinutes(1));
+            #endregion
+        }
+    }
+}
