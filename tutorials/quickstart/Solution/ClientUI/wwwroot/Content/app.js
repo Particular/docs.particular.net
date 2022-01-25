edd79b65dd2af2bc58be28408a1cd4c760167629
Jayanthi Sourirajan <88632084+soujay@users.noreply.github.com>
2022-01-24 16:22:52 -0800

    fixed indentation

--- tutorials/quickstart/Solution/ClientUI/wwwroot/Content/app.js
+++ tutorials/quickstart/Solution/ClientUI/wwwroot/Content/app.js
@@ -10,12 +10,12 @@ ga('set', 'transport', 'beacon');
 var _kmq = _kmq || [];
 var _kmk = _kmk || '081ab96143b8f345362841db575656a8512960d3';
 function _kms(u){
-setTimeout(function () {
-var d = document, f = d.getElementsByTagName('script')[0],
-s = d.createElement('script');
-s.type = 'text/javascript'; s.async = true; s.src = u;
-f.parentNode.insertBefore(s, f);
-}, 1);
+    setTimeout(function () {
+        var d = document, f = d.getElementsByTagName('script')[0],
+        s = d.createElement('script');
+        s.type = 'text/javascript'; s.async = true; s.src = u;
+        f.parentNode.insertBefore(s, f);
+    }, 1);
 }
 _kms('//i.kissmetrics.io/i.js');
 _kms('//scripts.kissmetrics.io/' + _kmk + '.2.js');
