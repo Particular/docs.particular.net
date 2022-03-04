72b761073fb6bb8096379b8f21d90f62d79a92a4
David Boike <david.boike@gmail.com>
2022-03-04 09:23:56 -0600

    NHibernate [RowVersion] can't use ContainSagaData

--- Snippets/NHibernate/NHibernate_8/Concurrency/SagaDataWithRowVersion.cs
+++ Snippets/NHibernate/NHibernate_8/Concurrency/SagaDataWithRowVersion.cs
@@ -1,8 +1,9 @@
-﻿using System;
-using NServiceBus;
+﻿using NServiceBus;
 using NServiceBus.SagaPersisters.NHibernate;
+using System;
 
 #region NHibernateConcurrencyRowVersion
+#pragma warning disable NSB0012 // Saga data classes should inherit ContainSagaData - [RowVersion] requires non-derived class
 public class SagaDataWithRowVersion :
     IContainSagaData
 {
@@ -13,4 +14,5 @@ public class SagaDataWithRowVersion :
     public virtual string Originator { get; set; }
     public virtual Guid Id { get; set; }
 }
+#pragma warning restore NSB0012 // Saga data classes should inherit ContainSagaData
 #endregion
--- samples/nhibernate/custom-mappings/NHibernate_8/Sample.Default/OrderSagaData.cs
+++ samples/nhibernate/custom-mappings/NHibernate_8/Sample.Default/OrderSagaData.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using NServiceBus;
+﻿using NServiceBus;
 using NServiceBus.SagaPersisters.NHibernate;
+using System;
+
+#pragma warning disable NSB0012 // Saga data classes should inherit ContainSagaData - Required for [RowVersion]
 
 public class OrderSagaData :
-    ContainSagaData
+    IContainSagaData
 {
     public virtual string OrderId { get; set; }
     [RowVersion]
     public virtual int Version { get; set; }
+    public virtual string OriginalMessageId { get; set; }
+    public virtual string Originator { get; set; }
+    public virtual Guid Id { get; set; }
 }
 
+#pragma warning restore NSB0012 // Saga data classes should inherit ContainSagaData
