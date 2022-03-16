59a25f4606142235a60f30957675f4561325ea24
Hadi Eskandari <h.eskandari@gmail.com>
2022-03-16 12:31:35 +1100

    xml snippet

---
+++ Snippets/Xml/Core_8/XmlSerializerUsage.cs
@@ -0,0 +1,16 @@
+﻿namespace Core8
+{
+    using NServiceBus;
+
+    class XmlSerializerUsage
+    {
+        XmlSerializerUsage(EndpointConfiguration endpointConfiguration)
+        {
+            #region XmlSerialization
+
+            endpointConfiguration.UseSerialization<XmlSerializer>();
+
+            #endregion
+        }
+    }
+}
