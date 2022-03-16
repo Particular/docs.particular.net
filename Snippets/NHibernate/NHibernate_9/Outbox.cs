e9d71d4a1e4918a15ce1482ab41ef77cb453925c
Hadi Eskandari <h.eskandari@gmail.com>
2022-03-16 13:32:36 +1100

    nhibernate

---
+++ Snippets/NHibernate/NHibernate_9/Outbox.cs
@@ -0,0 +1,16 @@
+﻿using System.Data;
+using NServiceBus;
+
+public class Outbox
+{
+    public void TransactionScopeMode(EndpointConfiguration endpointConfiguration)
+    {
+        #region OutboxTransactionIsolation
+
+        var outboxSettings = endpointConfiguration.EnableOutbox();
+        outboxSettings.UseTransactionScope();
+        outboxSettings.TransactionIsolationLevel(IsolationLevel.ReadCommitted);
+
+        #endregion
+    }
+}
