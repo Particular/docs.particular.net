49cf1ffba72f8637eeec99321c6aab7b76fadc2f
Hadi Eskandari <h.eskandari@gmail.com>
2022-03-16 13:13:07 +1100

    property encryption

---
+++ Snippets/PropertyEncryption/PropertyEncryption_3/Conventions/Message.cs
@@ -0,0 +1,12 @@
+﻿namespace Core6.Encryption.Conventions
+{
+    using NServiceBus;
+
+    #region MessageForEncryptionConvention
+    public class MyMessage :
+        IMessage
+    {
+        public string MyEncryptedProperty { get; set; }
+    }
+    #endregion
+}
