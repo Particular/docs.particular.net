1d52f8a1295d6dfe3376814974b4e085b6ae4adc
Tim Bussmann <timbussmann@users.noreply.github.com>
2022-03-17 11:31:11 +0100

    add json sample for platform connector

---
+++ samples/platform-connector/json/PlatformConnector_2/Endpoint/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using NServiceBus;
+
+public class Program
+{
+    public static async Task Main()
+    {
+        Console.Title = "Endpoint";
+
+        var endpointConfiguration = new EndpointConfiguration("Endpoint");
+
+        endpointConfiguration.UseTransport<LearningTransport>();
+        endpointConfiguration.UsePersistence<NonDurablePersistence>();
+
+        #region loadConnectionDetails
+        var connectionPath = @".\platformConnection.json";
+        var json = File.ReadAllText(connectionPath);
+        var platformConnection = ServicePlatformConnectionConfiguration.Parse(json);
+        #endregion
+
+        #region configureConnection
+        endpointConfiguration.ConnectToServicePlatform(platformConnection);
+        #endregion
+
+        var endpoint = await Endpoint.Start(endpointConfiguration);
+
+        while (Console.ReadKey(true).Key != ConsoleKey.Escape)
+        {
+            await endpoint.SendLocal(new BusinessMessage { BusinessId = Guid.NewGuid() });
+        }
+
+        await endpoint.Stop();
+    }
+}
