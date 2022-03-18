67316b633ed99908bf1939227b3b97dde93faf6e
Tim Bussmann <timbussmann@users.noreply.github.com>
2022-03-18 11:55:35 +0100

    add v8 version of forwarding sample

---
+++ samples/routing/fowarding-address/Core_8/NServiceBus.ForwardingAddress/IncomingPhysicalMessageContextExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using NServiceBus.Extensibility;
+using NServiceBus.ObjectBuilder;
+using NServiceBus.Pipeline;
+using NServiceBus.Routing;
+using NServiceBus.Transport;
+
+static class IncomingPhysicalMessageContextExtensions
+{
+    public static IRoutingContext CreateRoutingContext(
+        this IIncomingPhysicalMessageContext context,
+        IReadOnlyCollection<RoutingStrategy> routingStrategies)
+    {
+        return new ForwardingRoutingContext(
+            new OutgoingMessage(context.MessageId, context.Message.Headers, context.Message.Body),
+            routingStrategies,
+            context.Builder,
+            context.CancellationToken,
+            context);
+    }
+
+    class ForwardingRoutingContext :
+        ContextBag,
+        IRoutingContext
+    {
+        public ForwardingRoutingContext(
+            OutgoingMessage message,
+            IReadOnlyCollection<RoutingStrategy> routingStrategies,
+            IServiceProvider builder,
+            CancellationToken cancellationToken,
+            IBehaviorContext parentContext) : base(parentContext?.Extensions)
+        {
+            Message = message;
+            RoutingStrategies = routingStrategies;
+            Builder = builder;
+            CancellationToken = cancellationToken;
+        }
+
+        public IServiceProvider Builder { get; }
+        public ContextBag Extensions => this;
+        public OutgoingMessage Message { get; }
+        public IReadOnlyCollection<RoutingStrategy> RoutingStrategies { get; set; }
+        public CancellationToken CancellationToken { get; }
+    }
+}
---
+++ samples/routing/fowarding-address/Core_8/NServiceBus.ForwardingAddress/LeaveForwardingAddressFeature.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NServiceBus;
+using NServiceBus.Features;
+using NServiceBus.Routing;
+using NServiceBus.Transport;
+
+#region forwarding-feature
+
+class LeaveForwardingAddressFeature :
+    Feature
+{
+    protected override void Setup(FeatureConfigurationContext context)
+    {
+        var settings = context.Settings;
+        var pipeline = context.Pipeline;
+        var forwardingAddressDirectory = settings.Get<ForwardingAddressDirectory>();
+
+        pipeline.Register(sp =>
+        {
+            var routeResolver = new UnicastRouteResolver(
+                sp.GetRequiredService<ITransportAddressResolver>(),
+                settings.Get<EndpointInstances>(),
+                settings.Get<DistributionPolicy>()
+            );
+
+            return new RerouteMessagesWithForwardingAddress(
+                forwardingAddressDirectory.ToLookup(),
+                routeResolver
+            );
+        }, "Finds forwarding addresses and resolves them");
+
+        pipeline.Register(new ForwardMessagesWithForwardingAddress(), "Forwards messages to their matching forwarding addresses");
+    }
+}
+
+#endregion
