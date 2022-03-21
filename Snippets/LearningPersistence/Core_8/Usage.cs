5a37901ad3651e0546400eab52fec99134f690c4
Hadi Eskandari <h.eskandari@gmail.com>
2022-03-22 09:15:06 +1100

    learning persistence

---
+++ Snippets/LearningPersistence/Core_8/Usage.cs
@@ -0,0 +1,25 @@
+﻿namespace Core7
+{
+    using NServiceBus;
+
+    class Usage
+    {
+        Usage(EndpointConfiguration endpointConfiguration)
+        {
+            #region LearningPersistence
+
+            endpointConfiguration.UsePersistence<LearningPersistence>();
+
+            #endregion
+        }
+        void SagaStorageDirectory(EndpointConfiguration endpointConfiguration)
+        {
+            #region LearningPersistenceSagaStorageDirectory
+
+            var persistence = endpointConfiguration.UsePersistence<LearningPersistence>();
+            persistence.SagaStorageDirectory("PathToStoreSagas");
+
+            #endregion
+        }
+    }
+}
