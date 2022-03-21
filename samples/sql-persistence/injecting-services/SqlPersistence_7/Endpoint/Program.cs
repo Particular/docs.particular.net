fdc9e1fcda16142310d36a114816501b92cfb2a6
Tim Bussmann <timbussmann@users.noreply.github.com>
2022-03-21 04:47:53 +0100

    Sql di sample for NSB v8 (#5681)
    * delete dotsettings file
    
    * add injecting-services sample for sql persistence v7
    
    * add prerelease marker
    
    Co-authored-by: Hadi Eskandari <h.eskandari@gmail.com>

---
+++ samples/sql-persistence/injecting-services/SqlPersistence_7/Endpoint/Program.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.DependencyInjection;
+using NServiceBus;
+using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.SqlPersistence.InjectingServices";
+
+        var endpointConfiguration = new EndpointConfiguration("Samples.SqlPersistence.InjectingServices");
+        endpointConfiguration.EnableInstallers();
+
+        var connectionString = @"Data Source=.\SqlExpress;Initial Catalog=NsbSamplesInjectedServices;Integrated Security=True";
+
+        endpointConfiguration.UseTransport(new SqlServerTransport(connectionString)
+        {
+            TransportTransactionMode = TransportTransactionMode.SendsAtomicWithReceive
+        });
+
+        var persistence = endpointConfiguration.UsePersistence<SqlPersistence>();
+        persistence.SqlDialect<SqlDialect.MsSqlServer>();
+        persistence.ConnectionBuilder(connectionBuilder: () => new SqlConnection(connectionString));
+        var subscriptions = persistence.SubscriptionSettings();
+        subscriptions.CacheFor(TimeSpan.FromMinutes(1));
+
+        #region BehaviorConfig
+
+        endpointConfiguration.Pipeline.Register(typeof(SqlConnectionBehavior),
+            "Forwards the NServiceBus SqlConnection/SqlTransaction to data services injected into message handlers.");
+
+        #endregion
+
+        #region DependencyInjectionConfig
+
+        endpointConfiguration.RegisterComponents(services =>
+        {
+            services.AddScoped<ConnectionHolder>();
+            services.AddScoped<IDataService, DataService>();
+        });
+
+        #endregion
+
+        SqlHelper.EnsureDatabaseExists(connectionString);
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+
+        Console.WriteLine("Press [S] to send a message, or [Enter] to exit");
+
+        await RunLoop(endpointInstance);
+
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+
+    static async Task RunLoop(IMessageSession messageSession)
+    {
+        while (true)
+        {
+            var key = Console.ReadKey(true);
+            switch (key.Key)
+            {
+                case ConsoleKey.Enter:
+                    return;
+
+                case ConsoleKey.S:
+                    await messageSession.SendLocal(new TestMsg { Id = Guid.NewGuid() });
+                    break;
+            }
+        }
+    }
+}
