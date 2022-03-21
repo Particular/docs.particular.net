6dab85b9b13560b00b00f12e1f977646e1a3cfb7
Hadi Eskandari <h.eskandari@gmail.com>
2022-03-22 09:31:52 +1100

    fileshare

---
+++ Snippets/FileShareDataBus/Core_8/CleanupStrategy/Define.cs
@@ -0,0 +1,17 @@
+namespace Core7.CleanupStrategy
+{
+    using NServiceBus;
+
+    class Define
+    {
+        Define(EndpointConfiguration endpointConfiguration)
+        {
+            #region DefineFileLocationForDatabusFiles
+
+            var dataBus = endpointConfiguration.UseDataBus<FileShareDataBus>();
+            dataBus.BasePath(@"\\share\databus_attachments\");
+
+            #endregion
+        }
+    }
+}
