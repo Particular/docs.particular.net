406ed8978429785941151c43a197c1ced5dfe9ed
Mike Minutillo <mike.minutillo@particular.net>
2022-03-22 10:28:11 +0800

    Fix saga mappings

--- samples/sql-persistence/saga-rename/SqlPersistence_7/EndpointVersion2/MyTimeoutSagaVersion2.cs
+++ samples/sql-persistence/saga-rename/SqlPersistence_7/EndpointVersion2/MyTimeoutSagaVersion2.cs
@@ -15,7 +15,9 @@ namespace MyNamespace2
 
         protected override void ConfigureHowToFindSaga(SagaPropertyMapper<SagaData> mapper)
         {
-            mapper.ConfigureMapping<StartTimeoutSaga>(msg => msg.TheId).ToSaga(saga => saga.TheId);
+            mapper.MapSaga(saga => saga.TheId)
+                .ToMessage<StartTimeoutSaga>(msg => msg.TheId)
+                ;
         }
 
         public Task Handle(StartTimeoutSaga message, IMessageHandlerContext context)
