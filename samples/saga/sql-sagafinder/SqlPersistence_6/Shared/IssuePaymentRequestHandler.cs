c1bcbcb78e1f09ee8e61a908033e937de48e6746
Mike Minutillo <mike.minutillo@particular.net>
2022-03-22 15:27:27 +0800

    Create v6 samples
    Based on the v5 samples

---
+++ samples/saga/sql-sagafinder/SqlPersistence_6/Shared/IssuePaymentRequestHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+
+class IssuePaymentRequestHandler :
+    IHandleMessages<IssuePaymentRequest>
+{
+    public Task Handle(IssuePaymentRequest message, IMessageHandlerContext context)
+    {
+        var completePayment = new CompletePaymentTransaction
+        {
+            PaymentTransactionId = message.PaymentTransactionId
+        };
+        return context.SendLocal(completePayment);
+    }
+}
