6ef870ffd7633e9186373451cc97269a155d8f54
Mike Minutillo <mike.minutillo@particular.net>
2022-03-23 17:54:12 +0800

    Switch to base class

--- samples/sqltransport-nhpersistence/Core_8/Receiver/OrderLifecycleSagaData.cs
+++ samples/sqltransport-nhpersistence/Core_8/Receiver/OrderLifecycleSagaData.cs
@@ -2,12 +2,7 @@
 using NServiceBus;
 
 public class OrderLifecycleSagaData :
-#pragma warning disable NSB0012 // Saga data classes should inherit ContainSagaData
-    IContainSagaData
-#pragma warning restore NSB0012 // Saga data classes should inherit ContainSagaData
+    ContainSagaData
 {
     public virtual Guid OrderId { get; set; }
-    public virtual Guid Id { get; set; }
-    public virtual string Originator { get; set; }
-    public virtual string OriginalMessageId { get; set; }
 }
