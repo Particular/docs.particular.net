e4ab51f7e9d4860abbc5bbcc6b4e2b70f0ec248f
Dennis <dvdstelt@gmail.com>
2022-04-16 13:52:12 +0200

    Finished sample documentation

--- samples/bridge/simple/Bridge_1/Bridge/Program.cs
+++ samples/bridge/simple/Bridge_1/Bridge/Program.cs
@@ -22,19 +22,35 @@ static class Program
             })
             .UseNServiceBusBridge((ctx, bridgeConfiguration) =>
             {
+                #region endpoint-adding-simple
+
                 var learningLeft = new BridgeTransport(new LearningTransport());
                 learningLeft.HasEndpoint("Samples.Bridge.Sender");
+
+                #endregion
+
+                #region endpoint-adding-register-publisher-by-type
 
                 var leftReceiver = new BridgeEndpoint("Samples.Bridge.LeftReceiver");
                 leftReceiver.RegisterPublisher(typeof(OrderReceived), "Samples.Bridge.Sender");
 
+                #endregion
+
                 var learningRight = new BridgeTransport(new LearningTransport());
 
-                var billing = new BridgeEndpoint("Samples.Bridge.RightReceiver");
-                billing.RegisterPublisher("Messages.Events.OrderPlaced", "Samples.Bridge.Sender");
+                #region endpoint-adding-register-publisher-by-string
+
+                var rightReceiver = new BridgeEndpoint("Samples.Bridge.RightReceiver");
+                rightReceiver.RegisterPublisher("Messages.Events.OrderPlaced", "Samples.Bridge.Sender");
+
+                #endregion
+
+                #region add-transports-to-bridge
 
                 bridgeConfiguration.AddTransport(learningLeft);
                 bridgeConfiguration.AddTransport(learningRight);
+
+                #endregion
             })
             .Build()
             .RunAsync().ConfigureAwait(false);
