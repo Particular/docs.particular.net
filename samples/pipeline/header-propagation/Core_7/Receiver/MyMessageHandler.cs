f3afcffdd6d3e7f0de62ee35742752842cc10346
Mike Minutillo <mike.minutillo@particular.net>
2022-05-04 12:30:09 +0800

    Create header propagation sample

---
+++ samples/pipeline/header-propagation/Core_7/Receiver/MyMessageHandler.cs
@@ -0,0 +1,24 @@
+﻿using NServiceBus;
+using System;
+using System.Threading.Tasks;
+
+class MyMessageHandler : IHandleMessages<ProcessOrder>, IHandleMessages<ShipOrder>
+{
+    #region handlers
+    public Task Handle(ProcessOrder message, IMessageHandlerContext context)
+    {
+        context.MessageHeaders.TryGetValue("CustomerId", out var customerId);
+        Console.WriteLine($"Recieved ProcessOrder for customer {customerId}. Shipping");
+
+        return context.SendLocal(new ShipOrder());
+    }
+
+    public Task Handle(ShipOrder message, IMessageHandlerContext context)
+    {
+        context.MessageHeaders.TryGetValue("CustomerId", out var customerId);
+        Console.WriteLine($"Shipping Order for customer {customerId}.");
+
+        return Task.CompletedTask;
+    }
+    #endregion
+}
---
+++ samples/pipeline/header-propagation/Core_8/Receiver/PropagateCustomerIdHeaderBehavior.cs
@@ -0,0 +1,21 @@
+﻿using NServiceBus.Pipeline;
+using System;
+using System.Threading.Tasks;
+
+#region behavior
+class PropagateCustomerIdHeaderBehavior : Behavior<IOutgoingLogicalMessageContext>
+{
+    public override Task Invoke(IOutgoingLogicalMessageContext context, Func<Task> next)
+    {
+        if(context.TryGetIncomingPhysicalMessage(out var incomingMessage))
+        {
+            if(incomingMessage.Headers.TryGetValue("CustomerId", out var incomingConversationId))
+            {
+                context.Headers["CustomerId"] = incomingConversationId;
+            }
+        }
+
+        return next();
+    }
+}
+#endregion
