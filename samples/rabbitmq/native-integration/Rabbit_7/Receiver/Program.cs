ccb7db7ff7ab000d47fdca6885b480321d649844
Andreas Öhlund <andreas.ohlund@particular.net>
2022-05-08 09:10:34 +0200

    Add v7 native integration sample

---
+++ samples/rabbitmq/native-integration/Rabbit_7/Receiver/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.RabbitMQ.NativeIntegration.Receiver";
+
+        #region ConfigureRabbitQueueName
+        var endpointConfiguration = new EndpointConfiguration("Samples.RabbitMQ.NativeIntegration");
+        var transport = endpointConfiguration.UseTransport<RabbitMQTransport>();
+        transport.UseConventionalRoutingTopology();
+        transport.ConnectionString("host=localhost");
+        #endregion
+
+        endpointConfiguration.EnableInstallers();
+        endpointConfiguration.UsePersistence<InMemoryPersistence>();
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
