6a6947d6b5d9dd68035c319b0cffdcb1c31a4ced
Andreas Öhlund <andreas.ohlund@particular.net>
2022-05-30 08:41:05 +0200

    Use beta4 and suppress

--- Snippets/BinaryDataBus/BinaryDataBus_1/Usage.cs
+++ Snippets/BinaryDataBus/BinaryDataBus_1/Usage.cs
@@ -6,7 +6,9 @@ class Usage
     Usage(EndpointConfiguration endpointConfiguration)
     {
         #region BinaryDataBusUsage
+#pragma warning disable CS0618 // Type or member is obsolete
         endpointConfiguration.UseDataBus<FileShareDataBus, BinaryFormatterDataBusSerializer>();
+#pragma warning restore CS0618 // Type or member is obsolete
         #endregion
     }
 }
