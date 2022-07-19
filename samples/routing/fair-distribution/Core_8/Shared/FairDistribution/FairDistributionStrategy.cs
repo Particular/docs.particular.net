e77344ed06433a2aeb891e91d81080a62375c22b
Tim Bussmann <timbussmann@users.noreply.github.com>
2022-07-19 11:01:52 +0200

    align v8 code

--- samples/routing/fair-distribution/Core_8/Shared/FairDistribution/FairDistributionStrategy.cs
+++ samples/routing/fair-distribution/Core_8/Shared/FairDistribution/FairDistributionStrategy.cs
@@ -6,14 +6,13 @@ using System;
 public class FairDistributionStrategy :
     DistributionStrategy
 {
-    IReadOnlySettings settings;
     Lazy<FlowManager> flowManager;
 
     public FairDistributionStrategy(IReadOnlySettings settings, string endpoint, DistributionStrategyScope scope)
         : base(endpoint, scope)
     {
-        this.settings = settings;
-        flowManager = new Lazy<FlowManager>(() => settings.Get<FlowManager>());
+        // FlowManager is created by the FairDistribution feature at endpoint start time
+        flowManager = new Lazy<FlowManager>(settings.Get<FlowManager>);
     }
 
     public override string SelectDestination(DistributionContext context)
