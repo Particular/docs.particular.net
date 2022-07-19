0a3026d5fff53b4ff063ef5b5022be059e76f707
Tim Bussmann <timbussmann@users.noreply.github.com>
2022-07-19 11:57:50 +0200

    Fix fair distribution sample for v7 (#5771)
    * fix fair distribution sample for v7
    
    * align v8 code

--- samples/routing/fair-distribution/Core_7/Shared/FairDistribution/FairDistributionStrategy.cs
+++ samples/routing/fair-distribution/Core_7/Shared/FairDistribution/FairDistributionStrategy.cs
@@ -1,23 +1,23 @@
-﻿using NServiceBus;
+﻿using System;
+using NServiceBus;
 using NServiceBus.Routing;
 using NServiceBus.Settings;
 
 public class FairDistributionStrategy :
     DistributionStrategy
 {
-    ReadOnlySettings settings;
-    FlowManager flowManager;
+    Lazy<FlowManager> flowManager;
 
     public FairDistributionStrategy(ReadOnlySettings settings, string endpoint, DistributionStrategyScope scope)
         : base(endpoint, scope)
     {
-        this.settings = settings;
-        flowManager = settings.Get<FlowManager>();
+        // FlowManager is created by the FairDistribution feature at endpoint start time
+        flowManager = new Lazy<FlowManager>(settings.Get<FlowManager>);
     }
 
     public override string SelectDestination(DistributionContext context)
     {
-        return flowManager
+        return flowManager.Value
             .FindShortestQueue(context.ReceiverAddresses);
     }
 }
