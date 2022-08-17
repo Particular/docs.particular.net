25bf00fdc7557c60bd6ffd5723d3b67ac3fbb1a3
kentdr <dan.kent@particular.net>
2022-08-17 13:28:49 -0400

    Rename Core_7.2 folder to Core_7

---
+++ samples/web/send-from-aspnetcore-webapi/Core_7/WebApplication/Startup.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+        app.UseRouting();
+        app.UseEndpoints(c => c.MapControllers());
+    }
+}
