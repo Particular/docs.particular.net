232a7d60136c992850fc250998904104bbaacc81
Travis Nickels <travis.nickels@particular.net>
2022-09-06 13:38:20 -0700

    Fixed Regex matching and error message grammar

--- tests/IntegrityTests/ValidateReferencedPartials.cs
+++ tests/IntegrityTests/ValidateReferencedPartials.cs
@@ -30,9 +30,9 @@ namespace IntegrityTests
 
                 var fileName = Path.GetFileNameWithoutExtension(articlePath);
                 string text = File.ReadAllText(articlePath);
-                string searchPattern = @"(?i)partial:\s*[a-zA-Z0-9-_]*";
+                string searchPattern = @"^(?i)partial:\s*[a-zA-Z0-9-]*";
                 string replacePattern = @"(?i)partial:\s*";
-                var matches = Regex.Matches(text, searchPattern, RegexOptions.IgnoreCase);
+                var matches = Regex.Matches(text, searchPattern, RegexOptions.IgnoreCase | RegexOptions.Multiline);
                 var errors = new List<string>();
 
                 foreach (Match match in matches)
@@ -54,11 +54,12 @@ namespace IntegrityTests
             if (allErrors.Count > 0)
             {
                 StringBuilder stringBuilder = new StringBuilder();
-                stringBuilder.Append($"There is ({allErrors.Count}) markdown file(s) that had references to partials that do not have a corresponding partial file to be used. Here are the file paths and partial references that were not used.\r\n");
+                stringBuilder.Append($"Found ({allErrors.Count}) markdown file(s) that had references to partials that do not have a corresponding partial file to be used. Here are the file paths and partial references that were not used.\r\n");
                 foreach (var error in allErrors)
                 {
-                    stringBuilder.Append($"\r\n  > {error.Key.ToString()}\r\n");
-                    stringBuilder.Append($"{string.Join($"\r\n", error.Value)}");
+                    stringBuilder.AppendLine()
+                        .AppendLine($"  > {error.Key.ToString()}")
+                        .AppendLine($"{string.Join(Environment.NewLine, error.Value)}");
                 }
                 Assert.Fail(stringBuilder.ToString());
             }
