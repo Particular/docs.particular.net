bac4c0c725b1dc30ff20c92847930a9cf6ceb408
Jayanthi Sourirajan <88632084+soujay@users.noreply.github.com>
2022-09-13 14:27:52 -0700

    add v8 sample

---
+++ samples/servicecontrol/events-subscription/ServiceControlContracts_4/EndpointsMonitor/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "EndpointsMonitor";
+        var endpointConfiguration = new EndpointConfiguration("EndpointsMonitor");
+        endpointConfiguration.UseSerialization<NewtonsoftJsonSerializer>();
+        endpointConfiguration.EnableInstallers();
+        endpointConfiguration.UsePersistence<InMemoryPersistence>();
+        endpointConfiguration.SendFailedMessagesTo("error");
+
+        var transport = endpointConfiguration.UseTransport<LearningTransport>();
+
+        var conventions = endpointConfiguration.Conventions();
+        conventions.DefiningEventsAs(
+            type =>
+            {
+                return typeof(IEvent).IsAssignableFrom(type) ||
+                       // include ServiceControl events
+                       type.Namespace != null &&
+                       type.Namespace.StartsWith("ServiceControl.Contracts");
+            });
+
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        Console.WriteLine("Press any key to finish.");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
