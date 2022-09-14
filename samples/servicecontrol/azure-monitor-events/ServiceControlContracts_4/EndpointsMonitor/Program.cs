af309b7a3427e0d3655c55c69f742e91f76a8301
awright18 <awright18@gmail.com>
2022-09-14 12:59:22 -0500

    added Service Control Contracts v4 sample

---
+++ samples/servicecontrol/azure-monitor-events/ServiceControlContracts_4/EndpointsMonitor/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Extensions.DependencyInjection;
+using NServiceBus;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "AzureMonitorConnector";
+        var endpointConfiguration = new EndpointConfiguration("AzureMonitorConnector");
+        endpointConfiguration.UseSerialization<NewtonsoftJsonSerializer>();
+        endpointConfiguration.EnableInstallers();
+        endpointConfiguration.UsePersistence<NonDurablePersistence>();
+        endpointConfiguration.SendFailedMessagesTo("error");
+
+        var transport = endpointConfiguration.UseTransport<LearningTransport>();
+
+        var conventions = endpointConfiguration.Conventions();
+        conventions.DefiningEventsAs(
+            type =>
+            {
+                return typeof(IEvent).IsAssignableFrom(type) ||
+                       // include ServiceControl events
+                       type.Namespace != null &&
+                       type.Namespace.StartsWith("ServiceControl.Contracts");
+            });
+
+        #region AppInsightsSdkSetup
+
+        var envInstrumentationKey = "ApplicationInsightKey";
+        var instrumentationKey = Environment.GetEnvironmentVariable(envInstrumentationKey);
+
+        if (string.IsNullOrEmpty(instrumentationKey))
+        {
+            throw new Exception($"Environment variable '{envInstrumentationKey}' required.");
+        }
+
+        Console.WriteLine("Using application insights application key: {0}", instrumentationKey);
+
+
+        var telemetryConfiguration = new TelemetryConfiguration(instrumentationKey);
+        var telemetryClient = new TelemetryClient(telemetryConfiguration);
+
+        endpointConfiguration.RegisterComponents(cc => cc.AddSingleton(telemetryClient));
+
+        #endregion
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        Console.WriteLine("Press any key to finish.");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
