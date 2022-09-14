95578993fc833f731c4a3f72cd1a538c6cae3e0b
Jayanthi Sourirajan <88632084+soujay@users.noreply.github.com>
2022-09-14 08:39:38 -0700

    add sample for monitoring 3rd party

---
+++ samples/servicecontrol/monitoring3rdparty/CustomChecks_4/Sample/Program.cs
@@ -0,0 +1,25 @@
+﻿using NServiceBus;
+using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.CustomChecks.Monitor3rdParty";
+        var endpointConfiguration = new EndpointConfiguration("Samples.CustomChecks.Monitor3rdParty");
+        endpointConfiguration.AuditProcessedMessagesTo("audit");
+        endpointConfiguration.SendFailedMessagesTo("error");
+        endpointConfiguration.UseSerialization<NewtonsoftJsonSerializer>();
+        endpointConfiguration.EnableInstallers();
+        endpointConfiguration.UsePersistence<NonDurablePersistence>();
+        endpointConfiguration.UseTransport<LearningTransport>();
+
+        endpointConfiguration.ReportCustomChecksTo("Particular.ServiceControl");
+
+        await Endpoint.Start(endpointConfiguration).ConfigureAwait(false);
+
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+    }
+}
