2caa818d2da600b1b07e34c984ae511147411c62
danielmarbach <daniel.marbach@openplace.net>
2022-09-14 16:56:29 +0200

    Fix analyzer warnings

--- Snippets/SqlPersistence/SqlPersistence_6/SagaFinder/MySagaData.cs
+++ Snippets/SqlPersistence/SqlPersistence_6/SagaFinder/MySagaData.cs
@@ -4,11 +4,8 @@
     using NServiceBus;
 
     public class MySagaData :
-        IContainSagaData
+        ContainSagaData
     {
-        public Guid Id { get; set; }
-        public string Originator { get; set; }
-        public string OriginalMessageId { get; set; }
         public string OrderId { get; set; }
         public string PaymentTransactionId { get; set; }
     }
