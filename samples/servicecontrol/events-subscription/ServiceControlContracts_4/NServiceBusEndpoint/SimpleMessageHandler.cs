900772df76a126aa1edaa2f9a0fb86bdd31153f2
awright18 <awright18@gmail.com>
2022-09-14 10:25:35 -0500

    Added new sample in event-subscription for ServiceControl v4

---
+++ samples/servicecontrol/events-subscription/ServiceControlContracts_4/NServiceBusEndpoint/SimpleMessageHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+
+public class SimpleMessageHandler :
+    IHandleMessages<SimpleMessage>
+{
+    static ILog log = LogManager.GetLogger<SimpleMessageHandler>();
+
+    public Task Handle(SimpleMessage message, IMessageHandlerContext context)
+    {
+        log.Info($"Received message with Id = {message.Id}.");
+        throw new Exception("BOOM!");
+    }
+}
