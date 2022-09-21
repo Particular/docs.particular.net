a9fea0167e85cfed00c322b48d836b34127c8316
Ramon Smits <ramon.smits@gmail.com>
2022-09-21 15:58:53 +0200

    Added missing spring 8 sample

---
+++ Snippets/Spring/Spring_8/Usage.cs
@@ -0,0 +1,38 @@
+﻿using NServiceBus;
+using Spring.Context.Support;
+
+class Usage
+{
+    Usage(EndpointConfiguration endpointConfiguration)
+    {
+#pragma warning disable CS0618 // Type or member is obsolete
+        #region Spring
+
+        endpointConfiguration.UseContainer<SpringBuilder>();
+
+        #endregion
+#pragma warning restore CS0618 // Type or member is obsolete
+    }
+
+    void Existing(EndpointConfiguration endpointConfiguration)
+    {
+#pragma warning disable CS0618 // Type or member is obsolete
+        #region Spring_Existing
+
+        var applicationContext = new GenericApplicationContext();
+        applicationContext.ObjectFactory
+            .RegisterSingleton("MyService", new MyService());
+        endpointConfiguration.UseContainer<SpringBuilder>(
+            customizations: customizations =>
+            {
+                customizations.ExistingApplicationContext(applicationContext);
+            });
+
+        #endregion
+#pragma warning restore CS0618 // Type or member is obsolete
+    }
+
+    class MyService
+    {
+    }
+}
