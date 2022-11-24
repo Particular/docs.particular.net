1c1c151fefb5ddd853b74c24d3739606ec8d59d6
danielmarbach <daniel.marbach@openplace.net>
2022-11-24 17:02:48 +0100

    Migration output

--- samples/azure-service-bus-netstandard/hierarchy-migration/ASBS_3/Migration/Program.cs
+++ samples/azure-service-bus-netstandard/hierarchy-migration/ASBS_3/Migration/Program.cs
@@ -6,6 +6,7 @@ using Azure.Messaging.ServiceBus.Administration;
 static class Program
 {
     const string Endpoint1QueueName = "samples.asbs.hierarchymigration.endpoint1";
+    const string Endpoint1SubscriptionName = "Samples.ASBS.HierarchyMigration.Endpoint1";
     const string Endpoint2QueueName = "samples.asbs.hierarchymigration.endpoint2";
     const string Endpoint2MigrationQueueName = "samples.asbs.hierarchymigration.endpoint2.migration";
     const string Endpoint2SubscriptionName = "Samples.ASBS.HierarchyMigration.Endpoint2";
@@ -15,6 +16,10 @@ static class Program
     static async Task Main()
     {
         Console.Title = "Samples.ASBS.HierarchyMigration.Migration";
+        Console.WriteLine("--------------------------------------------------------------------------------------------");
+        Console.WriteLine("| Endpoint 2 Migration Sample                                                              |");
+        Console.WriteLine("--------------------------------------------------------------------------------------------");
+
         var connectionString = Environment.GetEnvironmentVariable("AzureServiceBus_ConnectionString");
         if (string.IsNullOrWhiteSpace(connectionString))
         {
@@ -24,15 +29,44 @@ static class Program
         // Endpoint 2 will be gradually migrated
         var adminClient = new ServiceBusAdministrationClient(connectionString);
         Console.WriteLine("Make sure Endpoint1 and Endpoint2 are not running");
-        Console.WriteLine("Press any key to continue with the migration");
+        Console.WriteLine("Press any key to cleanup infrastructure from previous runs of this sample.");
         Console.ReadLine();
+
         await DeleteExistingInfrastructure(adminClient);
 
-        Console.WriteLine("Start Endpoint1 and Endpoint2 and wait a bit until some messages are published.");
-        Console.WriteLine("Press any key to continue with the migration");
-        Console.ReadLine();
+        Console.WriteLine("--------------------------------------------------------------------------------------------");
+        Console.WriteLine($@"Deleted topics:
+- {PublishBundleName}
+- {SubscriptionBundleName}
 
-        // Makes sure the new subscription bundle is there
+Deleted queues:
+- {Endpoint1QueueName}
+- {Endpoint2QueueName}
+- {Endpoint2MigrationQueueName}
+");
+        Console.WriteLine("--------------------------------------------------------------------------------------------");
+
+        Console.WriteLine("Start Endpoint1 and Endpoint2 and wait a bit until some messages are published.");
+
+        Console.WriteLine("--------------------------------------------------------------------------------------------");
+        Console.WriteLine($@"When the endpoints are running the topology should look like the following:
+{PublishBundleName}
+    |__{Endpoint1SubscriptionName}
+       |__ $default: 1=0
+       |__ Event2: [NServiceBus.EnclosedMessageTypes] LIKE '%Event2%'
+        --> {Endpoint1QueueName}
+    |__{Endpoint2SubscriptionName}
+       |__ $default: 1=0
+       |__ Event1: [NServiceBus.EnclosedMessageTypes] LIKE '%Event1%'
+        --> {Endpoint2QueueName}
+
+{Endpoint1QueueName}
+{Endpoint2QueueName}
+");
+        Console.WriteLine("--------------------------------------------------------------------------------------------");
+
+        Console.WriteLine("Press any key to setup the sample topology");
+        Console.ReadLine();
 
         await CreateEntityGracefully(async () => await adminClient.CreateTopicAsync(
             new CreateTopicOptions(SubscriptionBundleName)
@@ -40,6 +74,27 @@ static class Program
                 EnableBatchedOperations = true,
                 MaxSizeInMegabytes = 5120
             }));
+
+        Console.WriteLine();
+        Console.WriteLine("--------------------------------------------------------------------------------------------");
+        Console.WriteLine($@"The {SubscriptionBundleName} topic has been setup:
+{PublishBundleName}
+    |__{Endpoint1SubscriptionName}
+       |__ $default: 1=0
+       |__ Event2: [NServiceBus.EnclosedMessageTypes] LIKE '%Event2%'
+        --> {Endpoint1QueueName}
+    |__{Endpoint2SubscriptionName}
+       |__ $default: 1=0
+       |__ Event1: [NServiceBus.EnclosedMessageTypes] LIKE '%Event1%'
+        --> {Endpoint2QueueName}
+
+{SubscriptionBundleName}
+    |__
+
+{Endpoint1QueueName}
+{Endpoint2QueueName}
+");
+        Console.WriteLine("--------------------------------------------------------------------------------------------");
 
         await CreateEntityGracefully(async () =>
             await adminClient.CreateSubscriptionAsync(
@@ -54,6 +109,30 @@ static class Program
                 },
                 new CreateRuleOptions("$default", new TrueRuleFilter())));
 
+        Console.WriteLine();
+        Console.WriteLine("--------------------------------------------------------------------------------------------");
+        Console.WriteLine($@"The forward rule on topic {PublishBundleName} has been setup:
+{PublishBundleName}
+    |__{Endpoint1SubscriptionName}
+       |__ $default: 1=0
+       |__ Event2: [NServiceBus.EnclosedMessageTypes] LIKE '%Event2%'
+        --> {Endpoint1QueueName}
+    |__{Endpoint2SubscriptionName}
+       |__ $default: 1=0
+       |__ Event1: [NServiceBus.EnclosedMessageTypes] LIKE '%Event1%'
+        --> {Endpoint2QueueName}
+    |__forwardTo-{SubscriptionBundleName}
+       |__ $default: 1=1
+        --> {SubscriptionBundleName}
+
+{SubscriptionBundleName}
+    |__
+
+{Endpoint1QueueName}
+{Endpoint2QueueName}
+");
+        Console.WriteLine("--------------------------------------------------------------------------------------------");
+
         await CreateEntityGracefully(async () =>
             await adminClient.CreateQueueAsync(
                 new CreateQueueOptions(Endpoint2MigrationQueueName)
@@ -61,6 +140,31 @@ static class Program
                     RequiresDuplicateDetection = true,
                     DuplicateDetectionHistoryTimeWindow = TimeSpan.FromMinutes(5),
                 }));
+
+        Console.WriteLine();
+        Console.WriteLine("--------------------------------------------------------------------------------------------");
+        Console.WriteLine($@"The migration queue {Endpoint2MigrationQueueName} has been setup:
+{PublishBundleName}
+    |__{Endpoint1SubscriptionName}
+       |__ $default: 1=0
+       |__ Event2: [NServiceBus.EnclosedMessageTypes] LIKE '%Event2%'
+        --> {Endpoint1QueueName}
+    |__{Endpoint2SubscriptionName}
+       |__ $default: 1=0
+       |__ Event1: [NServiceBus.EnclosedMessageTypes] LIKE '%Event1%'
+        --> {Endpoint2QueueName}
+    |__forwardTo-{SubscriptionBundleName}
+       |__ $default: 1=1
+        --> {SubscriptionBundleName}
+
+{SubscriptionBundleName}
+    |__
+
+{Endpoint1QueueName}
+{Endpoint2QueueName}
+{Endpoint2MigrationQueueName}
+");
+        Console.WriteLine("--------------------------------------------------------------------------------------------");
 
         SubscriptionProperties publishBundleEndpoint2Subscription = await adminClient.GetSubscriptionAsync(PublishBundleName, Endpoint2SubscriptionName);
 
@@ -76,9 +180,32 @@ static class Program
         publishBundleEndpoint2Subscription.ForwardTo = Endpoint2MigrationQueueName;
         await adminClient.UpdateSubscriptionAsync(publishBundleEndpoint2Subscription);
 
-        Console.WriteLine("Original subscription forwarding to the migration queue setup");
-        Console.WriteLine("Press any key to continue with the migration");
-        Console.ReadLine();
+        Console.WriteLine();
+        Console.WriteLine("--------------------------------------------------------------------------------------------");
+        Console.WriteLine($@"The subscription {Endpoint2SubscriptionName} on topic {SubscriptionBundleName} has been setup:
+{PublishBundleName}
+    |__{Endpoint1SubscriptionName}
+       |__ $default: 1=0
+       |__ Event2: [NServiceBus.EnclosedMessageTypes] LIKE '%Event2%'
+        --> {Endpoint1QueueName}
+    |__{Endpoint2SubscriptionName}
+       |__ $default: 1=0
+       |__ Event1: [NServiceBus.EnclosedMessageTypes] LIKE '%Event1%'
+        --> {Endpoint2QueueName}
+    |__forwardTo-{SubscriptionBundleName}
+       |__ $default: 1=1
+        --> {SubscriptionBundleName}
+
+{SubscriptionBundleName}
+    |__{Endpoint2SubscriptionName}
+       |__ $default: 1=0
+        --> {Endpoint2MigrationQueueName}
+
+{Endpoint1QueueName}
+{Endpoint2QueueName}
+{Endpoint2MigrationQueueName}
+");
+        Console.WriteLine("--------------------------------------------------------------------------------------------");
 
         await foreach (var rule in adminClient.GetRulesAsync(PublishBundleName, Endpoint2SubscriptionName))
         {
@@ -96,30 +223,106 @@ static class Program
                 });
         }
 
-        Console.WriteLine("New subscription setup with all the necessary rules of the original one");
+        Console.WriteLine();
+        Console.WriteLine("--------------------------------------------------------------------------------------------");
+        Console.WriteLine($@"The subscription {Endpoint2SubscriptionName} on topic {SubscriptionBundleName} has been setup:
+{PublishBundleName}
+    |__{Endpoint1SubscriptionName}
+       |__ $default: 1=0
+       |__ Event2: [NServiceBus.EnclosedMessageTypes] LIKE '%Event2%'
+        --> {Endpoint1QueueName}
+    |__{Endpoint2SubscriptionName}
+       |__ $default: 1=0
+       |__ Event1: [NServiceBus.EnclosedMessageTypes] LIKE '%Event1%'
+        --> {Endpoint2QueueName}
+    |__forwardTo-{SubscriptionBundleName}
+       |__ $default: 1=1
+        --> {SubscriptionBundleName}
+
+{SubscriptionBundleName}
+    |__{Endpoint2SubscriptionName}
+       |__ $default: 1=0
+       |__ Event1: [NServiceBus.EnclosedMessageTypes] LIKE '%Event1%'
+        --> {Endpoint2MigrationQueueName}
+
+{Endpoint1QueueName}
+{Endpoint2QueueName}
+{Endpoint2MigrationQueueName}
+");
+        Console.WriteLine("--------------------------------------------------------------------------------------------");
+
         Console.WriteLine("Press any key to continue with the migration");
         Console.ReadLine();
 
         await adminClient.DeleteSubscriptionAsync(PublishBundleName, Endpoint2SubscriptionName);
 
+        Console.WriteLine();
+        Console.WriteLine("--------------------------------------------------------------------------------------------");
+        Console.WriteLine($@"The subscription {Endpoint2SubscriptionName} on topic {PublishBundleName} has been deleted:
+{PublishBundleName}
+    |__{Endpoint1SubscriptionName}
+       |__ $default: 1=0
+       |__ Event2: [NServiceBus.EnclosedMessageTypes] LIKE '%Event2%'
+        --> {Endpoint1QueueName}
+    |__forwardTo-{SubscriptionBundleName}
+       |__ $default: 1=1
+        --> {SubscriptionBundleName}
+
+{SubscriptionBundleName}
+    |__{Endpoint2SubscriptionName}
+       |__ $default: 1=0
+       |__ Event1: [NServiceBus.EnclosedMessageTypes] LIKE '%Event1%'
+        --> {Endpoint2MigrationQueueName}
+
+{Endpoint1QueueName}
+{Endpoint2QueueName}
+{Endpoint2MigrationQueueName}
+");
+        Console.WriteLine("--------------------------------------------------------------------------------------------");
+
         QueueRuntimeProperties migrationQueueRuntimeProperties = await adminClient.GetQueueRuntimePropertiesAsync(Endpoint2MigrationQueueName);
-        Console.WriteLine(migrationQueueRuntimeProperties.ActiveMessageCount);
+        Console.WriteLine($"There are currently {migrationQueueRuntimeProperties.ActiveMessageCount} messages in the migration queue.");
 
         QueueProperties migrationQueueProperties = await adminClient.GetQueueAsync(Endpoint2MigrationQueueName);
         migrationQueueProperties.ForwardTo = Endpoint2QueueName;
         await adminClient.UpdateQueueAsync(migrationQueueProperties);
+
+        Console.WriteLine();
+        Console.WriteLine("--------------------------------------------------------------------------------------------");
+        Console.WriteLine($@"The queue {Endpoint2MigrationQueueName} forwarding to {Endpoint2QueueName} has been setup:
+{PublishBundleName}
+    |__{Endpoint1SubscriptionName}
+       |__ $default: 1=0
+       |__ Event2: [NServiceBus.EnclosedMessageTypes] LIKE '%Event2%'
+        --> {Endpoint1QueueName}
+    |__forwardTo-{SubscriptionBundleName}
+       |__ $default: 1=1
+        --> {SubscriptionBundleName}
+
+{SubscriptionBundleName}
+    |__{Endpoint2SubscriptionName}
+       |__ $default: 1=0
+       |__ Event1: [NServiceBus.EnclosedMessageTypes] LIKE '%Event1%'
+        --> {Endpoint2MigrationQueueName}
+
+{Endpoint1QueueName}
+{Endpoint2QueueName}
+{Endpoint2MigrationQueueName}
+--> {Endpoint2QueueName}
+");
+        Console.WriteLine("--------------------------------------------------------------------------------------------");
 
         do
         {
             migrationQueueRuntimeProperties = await adminClient.GetQueueRuntimePropertiesAsync(Endpoint2MigrationQueueName);
             if(migrationQueueRuntimeProperties.ActiveMessageCount > 0)
             {
-                Console.WriteLine(migrationQueueRuntimeProperties.ActiveMessageCount);
+                Console.WriteLine($"There are still {migrationQueueRuntimeProperties.ActiveMessageCount} messages in the migration queue.");
                 await Task.Delay(2000);
             }
             else
             {
-                Console.WriteLine("Everything forwarded");
+                Console.WriteLine("Everything forwarded. Moving on");
             }
         } while (migrationQueueRuntimeProperties.ActiveMessageCount > 0);
 
@@ -127,14 +330,71 @@ static class Program
         subscriptionBundleEndpoint2Subscription.ForwardTo = Endpoint2SubscriptionName;
         await adminClient.UpdateSubscriptionAsync(subscriptionBundleEndpoint2Subscription);
 
-        Console.WriteLine("New subscription changed to forward to the endpoint queue");
+        Console.WriteLine();
+        Console.WriteLine("--------------------------------------------------------------------------------------------");
+        Console.WriteLine($@"The subscription {Endpoint2SubscriptionName} on topic {SubscriptionBundleName} has been changed to forward to {Endpoint2QueueName}:
+{PublishBundleName}
+    |__{Endpoint1SubscriptionName}
+       |__ $default: 1=0
+       |__ Event2: [NServiceBus.EnclosedMessageTypes] LIKE '%Event2%'
+        --> {Endpoint1QueueName}
+    |__forwardTo-{SubscriptionBundleName}
+       |__ $default: 1=1
+        --> {SubscriptionBundleName}
+
+{SubscriptionBundleName}
+    |__{Endpoint2SubscriptionName}
+       |__ $default: 1=0
+       |__ Event1: [NServiceBus.EnclosedMessageTypes] LIKE '%Event1%'
+        --> {Endpoint2SubscriptionName}
+
+{Endpoint1QueueName}
+{Endpoint2QueueName}
+{Endpoint2MigrationQueueName}
+--> {Endpoint2QueueName}
+");
+        Console.WriteLine("--------------------------------------------------------------------------------------------");
+
+
         Console.WriteLine("Press any key to continue with the migration");
         Console.ReadLine();
 
         await adminClient.DeleteQueueAsync(Endpoint2MigrationQueueName);
-        Console.WriteLine("Migration queue deleted.");
+
+        Console.WriteLine();
+        Console.WriteLine("--------------------------------------------------------------------------------------------");
+        Console.WriteLine($@"The queue {Endpoint2MigrationQueueName} was removed:
+{PublishBundleName}
+    |__{Endpoint1SubscriptionName}
+       |__ $default: 1=0
+       |__ Event2: [NServiceBus.EnclosedMessageTypes] LIKE '%Event2%'
+        --> {Endpoint1QueueName}
+    |__forwardTo-{SubscriptionBundleName}
+       |__ $default: 1=1
+        --> {SubscriptionBundleName}
+
+{SubscriptionBundleName}
+    |__{Endpoint2SubscriptionName}
+       |__ $default: 1=0
+       |__ Event1: [NServiceBus.EnclosedMessageTypes] LIKE '%Event1%'
+        --> {Endpoint2SubscriptionName}
+
+{Endpoint1QueueName}
+{Endpoint2QueueName}
+");
+        Console.WriteLine("--------------------------------------------------------------------------------------------");
 
         Console.WriteLine("Migration done.");
+
+        Console.WriteLine($@"Stop Endpoint 2 and change:
+transport.Topology = TopicTopology.Single(""{PublishBundleName}"");
+
+to
+
+transport.Topology = TopicTopology.Hierarchy(""{PublishBundleName}"", ""{SubscriptionBundleName}"");
+
+and then start Endpoint 2.
+");
     }
 
     static async Task DeleteExistingInfrastructure(ServiceBusAdministrationClient adminClient)
