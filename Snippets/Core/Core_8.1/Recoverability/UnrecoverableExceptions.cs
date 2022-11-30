8a0a682b81ac95bb7b5fc7f3725f49e0f3878069
lailabougria <laila.bougria@particular.net>
2022-11-30 18:15:22 +0100

    unrecoverable exception docs

---
+++ Snippets/Core/Core_8.1/Recoverability/UnrecoverableExceptions.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+
+namespace Core8.Recoverability
+{
+    using System;
+    using NServiceBus;
+    using NServiceBus.Settings;
+
+    public class UnrecoverableExceptions
+    {
+        void Configure(EndpointConfiguration endpointConfiguration)
+        {
+            #region UnrecoverableExceptions
+
+            var recoverability = endpointConfiguration.Recoverability();
+            recoverability.AddUnrecoverableException<ValidationException>();
+            recoverability.AddUnrecoverableException(typeof(ArgumentException));
+
+            #endregion
+        }
+
+        void Configure(SettingsHolder settings)
+        {
+            #region UnrecoverableExceptionsSettings
+
+            settings.AddUnrecoverableException(typeof(ValidationException));
+
+            #endregion
+        }
+
+        class ValidationException : Exception
+        {
+        }
+
+        class SomeMessage : IMessage
+        {
+        }
+
+        class SomeMessageHandler : IHandleMessages<SomeMessage>
+        {
+            #region ThrowUnrecoverableException
+            public Task Handle(SomeMessage message, IMessageHandlerContext context)
+            {
+                throw new UnrecoverableException("Something went wrong, don't retry");
+            }
+            #endregion
+        }
+    }
+}
