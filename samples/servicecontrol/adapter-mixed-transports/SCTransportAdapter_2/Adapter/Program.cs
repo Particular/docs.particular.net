b2cb8f821a15ab7f300f7923417a142d4b8e0cfa
Tomasz Masternak <tomasz.masternak@particular.net>
2022-12-20 07:23:49 +0000

    remove preview

--- samples/servicecontrol/adapter-mixed-transports/SCTransportAdapter_2/Adapter/Program.cs
+++ samples/servicecontrol/adapter-mixed-transports/SCTransportAdapter_2/Adapter/Program.cs
@@ -11,7 +11,7 @@ class Program
         #region AdapterTransport
 
         var transportAdapterConfig = new TransportAdapterConfig<SqlServerTransport, LearningTransport>("Samples.ServiceControl.Adapter");
-
+        
         #endregion
 
 #pragma warning disable 618
