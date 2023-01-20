855073ca70bbc5548d1d679d70db8968f8bd8d22
Travis Nickels <travis.nickels@particular.net>
2023-01-20 15:46:17 -0800

    Update samples/web/blazor-server-application/Core_8/WebApp/Program.cs
    Co-authored-by: Mauro Servienti <mauro.servienti@gmail.com>

--- samples/web/blazor-server-application/Core_8/WebApp/Program.cs
+++ samples/web/blazor-server-application/Core_8/WebApp/Program.cs
@@ -20,11 +20,6 @@ builder.Services.AddServerSideBlazor();
 
 var app = builder.Build();
 
-if (!app.Environment.IsDevelopment())
-{
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
 
 app.UseHttpsRedirection();
 
