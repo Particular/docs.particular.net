7de7bc10376700bbfb0c6c388370b49e47d9d016
Travis Nickels <travis.nickels@particular.net>
2023-01-25 13:21:31 -0800

    Use NServiceBus in a Blazor server app sample (#5956)

---
+++ samples/web/blazor-server-application/Core_8/Server/Program.cs
@@ -0,0 +1,18 @@
+﻿using NServiceBus;
+using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    public static async Task Main()
+    {
+        var endpointConfiguration = new EndpointConfiguration(Console.Title = "Samples.Blazor.Server");
+        endpointConfiguration.EnableCallbacks(makesRequests: false);
+        endpointConfiguration.UseTransport(new LearningTransport());
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration);
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop();
+    }
+}
