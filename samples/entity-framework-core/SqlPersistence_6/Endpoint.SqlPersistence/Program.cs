1db5cc5a1c359f696dc63e74f72c12743fad8887
Jo Palac <j_palac@hotmail.com>
2023-03-31 12:50:18 +1000

    Updated EF core samples

--- samples/entity-framework-core/SqlPersistence_6/Endpoint.SqlPersistence/Program.cs
+++ samples/entity-framework-core/SqlPersistence_6/Endpoint.SqlPersistence/Program.cs
@@ -1,25 +1,27 @@
 ﻿using System;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using NServiceBus;
 using NServiceBus.Faults;
 using NServiceBus.Persistence.Sql;
-using NServiceBus.Transport.SQLServer;
+using NServiceBus.Transport.SqlServer;
+using System.Transactions;
 
 class Program
 {
     static async Task Main()
     {
-        var connection = @"server=(local)\sqlexpress;database=nservicebus;Integrated Security=True;Max Pool Size=100";
+        // for SqlExpress use Data Source=.\SqlExpress;Initial Catalog=nservicebus;Integrated Security=True;Encrypt=false
+        var connectionString = @"Server=localhost,1433;Initial Catalog=nservicebus;User Id=SA;Password=yourStrong(!)Password;Encrypt=false";
         Console.Title = "Samples.EntityFrameworkUnitOfWork.SQL";
 
         using (var receiverDataContext = new ReceiverDataContext(new DbContextOptionsBuilder<ReceiverDataContext>()
-            .UseSqlServer(new SqlConnection(connection))
+            .UseSqlServer(new SqlConnection(connectionString))
             .Options))
         {
             await receiverDataContext.Database.EnsureCreatedAsync().ConfigureAwait(false);
-        }
+        }        
 
         var endpointConfiguration = new EndpointConfiguration("Samples.EntityFrameworkUnitOfWork.SQL");
         endpointConfiguration.EnableInstallers();
@@ -27,12 +29,12 @@ class Program
         endpointConfiguration.ExecuteTheseHandlersFirst(typeof(CreateOrderHandler), typeof(OrderLifecycleSaga), typeof(CreateShipmentHandler));
 
         var transport = endpointConfiguration.UseTransport<SqlServerTransport>();
-        transport.ConnectionString(connection);
-        transport.NativeDelayedDelivery().DisableTimeoutManagerCompatibility();
+        transport.ConnectionString(connectionString);        
         transport.SubscriptionSettings().DisableSubscriptionCache();
+        transport.Transactions(TransportTransactionMode.SendsAtomicWithReceive);
 
         var persistence = endpointConfiguration.UsePersistence<SqlPersistence>();
-        persistence.ConnectionBuilder(() => new SqlConnection(connection));
+        persistence.ConnectionBuilder(() => new SqlConnection(connectionString));
         var dialect = persistence.SqlDialect<SqlDialect.MsSqlServer>();
 
         #region UnitOfWork_SQL
