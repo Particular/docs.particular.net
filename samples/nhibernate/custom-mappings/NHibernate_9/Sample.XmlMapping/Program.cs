c9abccdca56c58336676b8529b0f0dfe1b326da1
Jo Palac <j_palac@hotmail.com>
2023-04-04 15:41:32 +1000

    Update Nhibernate custom mappings samples

--- samples/nhibernate/custom-mappings/NHibernate_9/Sample.XmlMapping/Program.cs
+++ samples/nhibernate/custom-mappings/NHibernate_9/Sample.XmlMapping/Program.cs
@@ -2,6 +2,8 @@
 using System.IO;
 using System.Threading.Tasks;
 using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Driver;
 using NServiceBus;
 using NServiceBus.Persistence;
 using Environment = NHibernate.Cfg.Environment;
@@ -11,21 +13,25 @@ class Program
     static async Task Main()
     {
         Console.Title = "Samples.CustomNhMappings.XmlMapping";
-        var nhConfiguration = new Configuration();
-
-        nhConfiguration.SetProperty(Environment.ConnectionProvider, "NHibernate.Connection.DriverConnectionProvider");
-        nhConfiguration.SetProperty(Environment.ConnectionDriver, "NHibernate.Driver.Sql2008ClientDriver");
-        nhConfiguration.SetProperty(Environment.Dialect, "NHibernate.Dialect.MsSql2008Dialect");
-        nhConfiguration.SetProperty(Environment.ConnectionStringName, "NServiceBus/Persistence");
-
-        AddMappingsFromFilesystem(nhConfiguration);
 
         var endpointConfiguration = new EndpointConfiguration("Samples.CustomNhMappings.XmlMapping");
         endpointConfiguration.EnableInstallers();
         endpointConfiguration.UseTransport(new LearningTransport());
 
+        // for SqlExpress use Data Source=.\SqlExpress;Initial Catalog=Samples.CustomNhMappings;Integrated Security=True;Max Pool Size=100;Encrypt=false
+        var connectionString = @"Server=localhost,1433;Initial Catalog=Samples.CustomNhMappings;User Id=SA;Password=yourStrong(!)Password;Max Pool Size=100;Encrypt=false";
+        var hibernateConfig = new Configuration();
+        hibernateConfig.DataBaseIntegration(x =>
+        {
+            x.ConnectionString = connectionString;
+            x.Dialect<MsSql2012Dialect>();
+            x.Driver<MicrosoftDataSqlClientDriver>();
+        });
+
+        AddMappingsFromFilesystem(hibernateConfig);
+
         var persistence = endpointConfiguration.UsePersistence<NHibernatePersistence>();
-        persistence.UseConfiguration(nhConfiguration);
+        persistence.UseConfiguration(hibernateConfig);
 
         var endpointInstance = await Endpoint.Start(endpointConfiguration)
             .ConfigureAwait(false);
