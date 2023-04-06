a6ff765cef10b249801118b931bc4a7163085de7
Jo Palac <jpalac@users.noreply.github.com>
2023-04-06 11:22:02 +1000

    Update samples/bridge/sql-multi-instance/Bridge_1/Receiver/Program.cs
    Co-authored-by: Daniel Marbach <daniel.marbach@openplace.net>

--- samples/bridge/sql-multi-instance/Bridge_1/Receiver/Program.cs
+++ samples/bridge/sql-multi-instance/Bridge_1/Receiver/Program.cs
@@ -5,8 +5,8 @@ using NServiceBus;
 
 class Program
 {    
-    // for SqlExpress use Data Source=.\SqlExpress;Initial Catalog=NsbSamplesSqlMultiInstanceReceiver;Integrated Security=True;Encrypt=false
-    const string ConnectionString = @"Server=localhost,1433;Initial Catalog=NsbSamplesSqlMultiInstanceReceiver;User Id=SA;Password=yourStrong(!)Password;Encrypt=false";
+    // for SqlExpress use Data Source=.\SqlExpress;Initial Catalog=NsbSamplesSqlMultiInstanceReceiver;Integrated Security=True;Max Pool Size=100;Encrypt=false
+    const string ConnectionString = @"Server=localhost,1433;Initial Catalog=NsbSamplesSqlMultiInstanceReceiver;User Id=SA;Password=yourStrong(!)Password;Max Pool Size=100;Encrypt=false";
 
     static async Task Main()
     {
