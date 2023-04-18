1ed9a410dab9be7fec4523ce452009b6843f5f7b
Christian <christian.quiros@particular.net>
2023-04-18 13:56:26 +0200

    adding google analytics tracking to quickstart

--- tutorials/quickstart/Solution/ClientUI/wwwroot/Content/app.js
+++ tutorials/quickstart/Solution/ClientUI/wwwroot/Content/app.js
@@ -1,4 +1,9 @@
-﻿(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+﻿window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-GMZ1FS541B');
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
@@ -30,6 +35,10 @@ _kms('//scripts.kissmetrics.io/' + _kmk + '.2.js');
         var gaEvt = function (category, action, label) {
             console.log('GA', category, action, label);
             window.ga('send', 'event', category, action, label);
+            gtag('event', category, {
+                'event_action': action,
+                'event_label': label
+            });
         };
        
         gaEvt('QuickStart', 'Running', SOLUTION_VERSION); 
