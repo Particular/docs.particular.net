c26849ede48c83fb47c5f9c388b06ec8e815f9e2
kentdr <dan.kent@particular.net>
2023-05-02 14:37:01 -0400

    Make the TriggerMessage handler an async method

--- samples/previews/aws-lambda/sqs/SQSLambda_1/AwsLambda.SQSTrigger/TriggerMessageHandler.cs
+++ samples/previews/aws-lambda/sqs/SQSLambda_1/AwsLambda.SQSTrigger/TriggerMessageHandler.cs
@@ -8,10 +8,10 @@ public class TriggerMessageHandler : IHandleMessages<TriggerMessage>
 {
     static readonly ILog Log = LogManager.GetLogger<TriggerMessageHandler>();
 
-    public Task Handle(TriggerMessage message, IMessageHandlerContext context)
+    public async Task Handle(TriggerMessage message, IMessageHandlerContext context)
     {
         Log.Info($"Handling {nameof(TriggerMessage)} in {nameof(TriggerMessageHandler)}");
-        return context.SendLocal(new FollowupMessage());
+        await context.SendLocal(new FollowupMessage());
     }
 }
 
