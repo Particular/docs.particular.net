08f2df440d9c5324305fb8780dca655d05e94afc
Andreas Öhlund <andreas.ohlund@particular.net>
2023-06-12 12:32:59 +0200

    Add snippets

---
+++ Snippets/SystemJson/Core_8.1/SystemJsonSerializerUsage.cs
@@ -0,0 +1,16 @@
+﻿namespace Core8
+{
+    using NServiceBus;
+
+    class SystemJsonSerializerUsage
+    {
+        SystemJsonSerializerUsage(EndpointConfiguration endpointConfiguration)
+        {
+            #region SystemJsonSerialization
+
+            endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+
+            #endregion
+        }
+    }
+}
