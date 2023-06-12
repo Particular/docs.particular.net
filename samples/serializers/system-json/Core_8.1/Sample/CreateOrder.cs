db58b061aa5f2257ec113ba76405a6f77ead1e40
Andreas Öhlund <andreas.ohlund@particular.net>
2023-06-12 13:14:13 +0200

    Add sample

---
+++ samples/serializers/system-json/Core_8.1/Sample/CreateOrder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using NServiceBus;
+
+public class CreateOrder :
+    IMessage
+{
+    public int OrderId { get; set; }
+    public DateTime Date { get; set; }
+    public int CustomerId { get; set; }
+    public List<OrderItem> OrderItems { get; set; }
+}
---
+++ samples/serializers/system-json/Core_8.1/Sample/Program.cs
@@ -0,0 +1,57 @@
+﻿using NServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+static class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.Serialization.SystemJson";
+
+        var endpointConfiguration = new EndpointConfiguration("Samples.Serialization.ExternalJson");
+        #region config
+
+        endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+
+        #endregion
+
+        endpointConfiguration.UsePersistence<LearningPersistence>();
+        endpointConfiguration.UseTransport(new LearningTransport());
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+
+        #region message
+
+        var message = new CreateOrder
+        {
+            OrderId = 9,
+            Date = DateTime.Now,
+            CustomerId = 12,
+            OrderItems = new List<OrderItem>
+            {
+                new OrderItem
+                {
+                    ItemId = 6,
+                    Quantity = 2
+                },
+                new OrderItem
+                {
+                    ItemId = 5,
+                    Quantity = 4
+                },
+            }
+        };
+        await endpointInstance.SendLocal(message)
+            .ConfigureAwait(false);
+
+        #endregion
+
+        Console.WriteLine("Order Sent");
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
