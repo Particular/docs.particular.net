878ad9b654370b8a9817882c5ee9454dd8972b64
Daniel Marbach <daniel.marbach@openplace.net>
2023-06-15 12:59:11 +0200

    Document DI for behaviors (#6092)
    * Start documenting DI for behaviors
    
    * Update manipulate-with-behaviors.md
    
    * Fix snippet

---
+++ Snippets/Core/Core_7/Pipeline/InjectingDependencies.cs
@@ -0,0 +1,39 @@
+namespace Core7.Pipeline
+{
+    using System;
+    using System.Threading.Tasks;
+    using NServiceBus.Pipeline;
+
+    #region InjectingDependencies
+
+    public class BehaviorUsingDependencyInjection :
+        Behavior<IIncomingLogicalMessageContext>
+    {
+        // Dependencies injected into the constructor are singletons and cached for the lifetime
+        // of the endpoint
+        public BehaviorUsingDependencyInjection(SingletonDependency singletonDependency)
+        {
+            this.singletonDependency = singletonDependency;
+        }
+
+        public override async Task Invoke(IIncomingLogicalMessageContext context, Func<Task> next)
+        {
+            var scopedDependency = context.Builder.Build<ScopedDependency>();
+            // do something with the scoped dependency before
+            await next();
+            // do something with the scoped dependency after
+        }
+
+        SingletonDependency singletonDependency;
+    }
+
+#endregion
+
+    public class SingletonDependency
+    {
+    }
+
+    public class ScopedDependency
+    {
+    }
+}
