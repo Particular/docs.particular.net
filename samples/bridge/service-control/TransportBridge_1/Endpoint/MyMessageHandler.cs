9199c086d4fc610f1d6b444ff1e7c412654d8ca3
Kyle Baley <kyle@baley.org>
2023-06-30 22:38:45 -0500

    Add legacy transport bridge docs

---
+++ samples/bridge/service-control/TransportBridge_1/Endpoint/MyMessageHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+
+class MyMessageHandler :
+    IHandleMessages<MyMessage>
+{
+    static ILog log = LogManager.GetLogger<MyMessageHandler>();
+
+    public Task Handle(MyMessage message, IMessageHandlerContext context)
+    {
+        log.Info($"Processing message {message.Id}");
+        return FailureSimulator.Invoke();
+    }
+}
---
+++ samples/bridge/sql-multi-instance/TransportBridge_1/Receiver/Program.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Threading.Tasks;
+using NServiceBus;
+#pragma warning disable 618
+
+class Program
+{    
+    // for SqlExpress use Data Source=.\SqlExpress;Initial Catalog=NsbSamplesSqlMultiInstanceReceiver;Integrated Security=True;Max Pool Size=100;Encrypt=false
+    const string ConnectionString = @"Server=localhost,1433;Initial Catalog=NsbSamplesSqlMultiInstanceReceiver;User Id=SA;Password=yourStrong(!)Password;Max Pool Size=100;Encrypt=false";
+
+    static async Task Main()
+    {
+        Console.Title = "Samples.SqlServer.MultiInstanceReceiver";
+
+        #region ReceiverConfiguration
+
+        var endpointConfiguration = new EndpointConfiguration("Samples.SqlServer.MultiInstanceReceiver");
+        var transport = endpointConfiguration.UseTransport<SqlServerTransport>();
+        transport.ConnectionString(ConnectionString);
+        endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+        endpointConfiguration.EnableInstallers();
+
+        #endregion
+
+        SqlHelper.EnsureDatabaseExists(ConnectionString);
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+
+        Console.WriteLine("Press any key to exit");
+        Console.WriteLine("Waiting for Order messages from the Sender");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
