d5bf5c72b4c843253c134bf271581e8d0136b4ac
Jayanthi <88632084+soujay@users.noreply.github.com>
2023-07-13 11:26:53 -0700

    add in serialization

--- tutorials/quickstart/Snippets/Core_7/ShippingProgram.cs
+++ tutorials/quickstart/Snippets/Core_7/ShippingProgram.cs
@@ -23,6 +23,8 @@ namespace Shipping
                            // Define the endpoint name
                            var endpointConfiguration = new EndpointConfiguration("Shipping");
 
+                           endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+
                            // Select the learning (filesystem-based) transport to communicate
                            // with other endpoints
                            endpointConfiguration.UseTransport<LearningTransport>();
@@ -35,7 +37,7 @@ namespace Shipping
 
                            // Enable monitoring endpoint performance
                            var metrics = endpointConfiguration.EnableMetrics();
-                           metrics.SendMetricDataToServiceControl("Particular.Monitoring", 
+                           metrics.SendMetricDataToServiceControl("Particular.Monitoring",
                                TimeSpan.FromMilliseconds(500));
 
                            // Return the completed configuration
