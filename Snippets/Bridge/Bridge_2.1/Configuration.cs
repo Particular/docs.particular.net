82f0a07949bd5533045572fe17d334c05f89fb45
Ramon Smits <ramon.smits@gmail.com>
2023-07-25 13:46:16 +0200

    Added Bridge_2.1.csproj, migrating DoNotEnforceBestPractices from 2.0 to 2.1

---
+++ Snippets/Bridge/Bridge_2.1/Configuration.cs
@@ -0,0 +1,15 @@
+﻿using NServiceBus;
+
+public class Configuration
+{
+    public void DoNotEnforceBestPractices()
+    {
+        var bridgeConfiguration = new BridgeConfiguration();
+
+        #region do-not-enforce-best-practices
+
+        bridgeConfiguration.DoNotEnforceBestPractices();
+
+        #endregion
+    }
+}
