5f1bafd27c5b792a45f0545b2ca0091f1cffde5d
Ramon Smits <ramon.smits@gmail.com>
2023-07-27 19:36:59 +0000

    Optimized outbox cleanup query

--- Snippets/SqlPersistence/SqlPersistence_4/MsSqlServer_OutboxCleanup.sql
+++ Snippets/SqlPersistence/SqlPersistence_4/MsSqlServer_OutboxCleanup.sql
@@ -1,7 +1,8 @@
 startcode MsSqlServer_OutboxCleanupSql
 WHILE 1 = 1
 BEGIN
-	DELETE TOP (@BatchSize) FROM [dbo].[EndpointNameOutboxData]
+	-- Avoid batch sizes over 4.000 to prevent lock escalation
+	DELETE TOP (@BatchSize) FROM [dbo].[EndpointNameOutboxData] WITH (ROWLOCK)
 	WHERE Dispatched = 'true' AND DispatchedAt < @DispatchedBefore;
 	IF @@ROWCOUNT < @BatchSize -- Important that @@ROWCOUNT is read immediately after the DELETE
 		BREAK
