e53033d83886c6e0ba575233d11c5035a6728153
Christian Quirós <christian.qiros+github@gmail.com>
2023-07-28 01:35:10 +0000

    Dispatching a message outside of a message handler

--- samples/hosting/aws-lambda-sqs/SQSLambda_2/AwsLambda.SQSTrigger/Function.cs
+++ samples/hosting/aws-lambda-sqs/SQSLambda_2/AwsLambda.SQSTrigger/Function.cs
@@ -2,6 +2,8 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 
+using Amazon.Lambda.Annotations;
+using Amazon.Lambda.Annotations.APIGateway;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.SQSEvents;
 
@@ -17,8 +19,7 @@ namespace SQSTrigger
         public Function()
         {
         }
-
-        #region FunctionHandler
+        #region FunctionHandler for SQS Events
         public async Task FunctionHandler(SQSEvent evnt, ILambdaContext context)
         {
             using var cancellationTokenSource = new CancellationTokenSource(context.RemainingTime.Subtract(DefaultRemainingTimeGracePeriod));
@@ -30,6 +31,24 @@ namespace SQSTrigger
 
         static readonly TimeSpan DefaultRemainingTimeGracePeriod = TimeSpan.FromSeconds(10);
 
+        #region FunctionHandler for HTTP API calls
+        [LambdaFunction()]
+        [HttpApi(LambdaHttpMethod.Get, "/")]
+        public async Task<string> Default(ILambdaContext context)
+        {
+            //TODO: remove try catch
+            try
+            {
+                await serverlessEndpoint.Send(new TriggerMessage(), context);
+                return $"{nameof(TriggerMessage)} sent.";
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
+        }
+        #endregion
+
         #region EndpointSetup
 
         private static readonly IAwsLambdaSQSEndpoint serverlessEndpoint = new AwsLambdaSQSEndpoint(context =>
@@ -40,6 +59,7 @@ namespace SQSTrigger
             var transport = endpointConfiguration.Transport;
             var routing = endpointConfiguration.RoutingSettings;
 
+            routing.RouteToEndpoint(typeof(TriggerMessage), "AwsLambdaSQSTrigger");
             routing.RouteToEndpoint(typeof(BackToSenderMessage), "AwsLambda.Sender");
 
             var advanced = endpointConfiguration.AdvancedConfiguration;
