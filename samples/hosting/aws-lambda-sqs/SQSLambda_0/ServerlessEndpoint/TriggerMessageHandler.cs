18eb76dab06d8e79ffa9e9267f572a6bc5a1ee5a
Travis Nickels <travis.nickels@particular.net>
2023-08-03 14:58:36 -0700

    Fixing the prerelease sample of AwsLambda.SQS

--- samples/hosting/aws-lambda-sqs/SQSLambda_0/ServerlessEndpoint/TriggerMessageHandler.cs
+++ samples/hosting/aws-lambda-sqs/SQSLambda_0/ServerlessEndpoint/TriggerMessageHandler.cs
@@ -3,16 +3,14 @@ using NServiceBus;
 using NServiceBus.Logging;
 
 #region TriggerMessageHandler
-
 public class TriggerMessageHandler : IHandleMessages<TriggerMessage>
 {
     static readonly ILog Log = LogManager.GetLogger<TriggerMessageHandler>();
 
-    public Task Handle(TriggerMessage message, IMessageHandlerContext context)
+    public async Task Handle(TriggerMessage message, IMessageHandlerContext context)
     {
-        Log.Info($"Handling {nameof(TriggerMessage)} in {nameof(TriggerMessageHandler)}");
-        return context.SendLocal(new FollowupMessage());
+        Log.Info($"Handling {nameof(TriggerMessage)} in ServerlessEndpoint.");
+        await context.Send(new ResponseMessage());
     }
 }
-
 #endregion
