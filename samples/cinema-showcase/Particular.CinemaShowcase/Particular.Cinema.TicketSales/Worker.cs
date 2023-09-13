bd480b69803efd5a4fd82ed7f1469b737257ad07
Mike Minutillo <mike.minutillo@particular.net>
2023-09-13 10:47:26 +0800

    Remove unused usings

--- samples/cinema-showcase/Particular.CinemaShowcase/Particular.Cinema.TicketSales/Worker.cs
+++ samples/cinema-showcase/Particular.CinemaShowcase/Particular.Cinema.TicketSales/Worker.cs
@@ -1,11 +1,5 @@
 ﻿using Microsoft.Extensions.Hosting;
 using Particular.Cinema.Messages;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Particular.Cinema.TicketSales
 {
