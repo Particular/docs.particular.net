ed82c745320609b9d684f0a270a5b98e4426bb95
Andreas Öhlund <andreas.ohlund@particular.net>
2023-10-04 13:18:10 +0200

    Make remoting section versioned to 8 and below

--- Snippets/Core/Core_9/Handlers/IntegrationWithNonTaskBasedAPI.cs
+++ Snippets/Core/Core_9/Handlers/IntegrationWithNonTaskBasedAPI.cs
@@ -40,7 +40,6 @@
 
     #endregion
 
-
     public class DependencyWhichRaisedEvent
     {
         public event EventHandler MyEvent;
@@ -50,79 +49,4 @@
             MyEvent?.Invoke(this, EventArgs.Empty);
         }
     }
-
-    #region HandlerWhichIntegratesWithAPM
-
-    public class HandlerWhichIntegratesWithAPM :
-        IHandleMessages<MyMessage>
-    {
-        public async Task Handle(MyMessage message, IMessageHandlerContext context)
-        {
-            var dependency = new DependencyWhichUsesAPM();
-
-            var result = await Task.Factory.FromAsync(
-                    beginMethod: (callback, state) =>
-                    {
-                        var d = (DependencyWhichUsesAPM) state;
-                        return d.BeginCall(callback, state);
-                    },
-                    endMethod: asyncResult =>
-                    {
-                        var d = (DependencyWhichUsesAPM) asyncResult.AsyncState;
-                        return d.EndCall(asyncResult);
-                    },
-                    state: dependency)
-                .ConfigureAwait(false);
-
-            // Use the result in some way
-            Trace.WriteLine(result);
-        }
-    }
-
-    #endregion
-
-    public class DependencyWhichUsesAPM
-    {
-        public IAsyncResult BeginCall(AsyncCallback callback, object state)
-        {
-            return null;
-        }
-
-        public string EndCall(IAsyncResult callback)
-        {
-            return null;
-        }
-    }
-
-    #region HandlerWhichIntegratesWithRemotingWithTask
-
-    public class HandlerWhichIntegratesWithRemotingWithTask :
-        IHandleMessages<MyMessage>
-    {
-        public async Task Handle(MyMessage message, IMessageHandlerContext context)
-        {
-            var result = await Task.Run(
-                function: () =>
-                {
-                    var remoteService = new RemoteService();
-                    return remoteService.TimeConsumingRemoteCall();
-                })
-                .ConfigureAwait(false);
-            // Use the result in some way
-            Trace.WriteLine(result);
-        }
-    }
-
-    #endregion
-
-    public class RemoteService :
-        MarshalByRefObject
-    {
-        public string TimeConsumingRemoteCall()
-        {
-            Thread.Sleep(1000);
-            return "Hello from remote.";
-        }
-    }
-
 }
---
+++ nservicebus/handlers/async-handlers_apm_core_[,9).partial.md
@@ -0,0 +1,17 @@
+### Asynchronous programming model (APM) pattern
+
+For existing code which uses the [Asynchronous Programming Model (APM)](https://msdn.microsoft.com/en-us/library/ms228963.aspx), it is best to use [`Task.Factory.FromAsync`](https://msdn.microsoft.com/en-us/library/system.threading.tasks.taskfactory.fromasync.aspx) to wrap the `BeginOperationName` and `EndOperationName` methods with a task object.
+
+snippet: HandlerWhichIntegratesWithAPM
+
+### Asynchronous RPC calls
+
+The APM approach described above can be used to integrate with remote procedure calls as shown in this snippet:
+
+snippet: HandlerWhichIntegratesWithRemotingWithAPM
+
+or use [`Task.Run`](https://msdn.microsoft.com/en-us/library/system.threading.tasks.task.run.aspx) directly in a message handler:
+
+snippet: HandlerWhichIntegratesWithRemotingWithTask
+
+NOTE: [`Task.Run`](https://msdn.microsoft.com/en-us/library/system.threading.tasks.task.run.aspx) can have significantly less overhead than using a delegate with `BeginInvoke`/`EndInvoke`. By default, both APIs will use the worker thread pool as the underlying scheduling engine. Analyze and measure for the business scenarios involved.
