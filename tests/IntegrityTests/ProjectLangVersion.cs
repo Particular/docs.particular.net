283447d27de340f528aaaf0881475d3f99422c49
Andreas Öhlund <andreas.ohlund@particular.net>
2023-10-06 09:22:49 +0200

    Fix lang version test

--- tests/IntegrityTests/ProjectLangVersion.cs
+++ tests/IntegrityTests/ProjectLangVersion.cs
@@ -1,13 +1,12 @@
-﻿using System.Xml.Linq;
+﻿using NUnit.Framework;
+using System.Xml.Linq;
 using System.Xml.XPath;
-using NUnit.Framework;
 
 namespace IntegrityTests
 {
     public class ProjectLangVersion
     {
-        //TODO-Do we need this?`
-        //[Test]
+        [Test]
         public void ShouldUseLangVersion10()
         {
             // Also reflected in https://docs.particular.net/samples/#technology-choices-c-language-level
@@ -30,6 +29,15 @@ namespace IntegrityTests
                     // Ignore if not an Sdk-style project
                     var sdk = xdoc.Root.Attribute("Sdk");
                     if (sdk == null)
+                    {
+                        return true;
+                    }
+
+                    // Ignore if targeting .net8 only
+                    var firstTargetFrameworkElement = xdoc.XPathSelectElement("/Project/PropertyGroup/TargetFramework");
+                    var tfm = firstTargetFrameworkElement?.Value;
+
+                    if (tfm == "net8.0")
                     {
                         return true;
                     }
