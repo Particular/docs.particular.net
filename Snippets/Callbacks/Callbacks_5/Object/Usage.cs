e7e9c0502309b60419ef2929b78dc8b9a7a04cf2
Andreas Öhlund <andreas.ohlund@particular.net>
2023-10-18 14:04:53 +0200

    Add v5 snippets for callbacks and callbacks testing

---
+++ Snippets/Callbacks/Callbacks_5/Object/Usage.cs
@@ -0,0 +1,28 @@
+﻿namespace Callbacks.Object
+{
+    using System.Threading.Tasks;
+    using NServiceBus;
+    using NServiceBus.Logging;
+
+    class Usage
+    {
+        async Task Simple(EndpointConfiguration endpointConfiguration, IEndpointInstance endpoint, ILog log)
+        {
+            #region Callbacks-InstanceId
+
+            endpointConfiguration.MakeInstanceUniquelyAddressable("uniqueId");
+
+            #endregion
+
+            #region ObjectCallback
+
+            var message = new Message();
+            var response = await endpoint.Request<ResponseMessage>(message)
+                .ConfigureAwait(false);
+            log.Info($"Callback received with response:{response.Property}");
+
+            #endregion
+        }
+    }
+
+}
