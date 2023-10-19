0c636a32c602048148239cada9933cbdeda5dca0
Andreas Öhlund <andreas.ohlund@particular.net>
2023-10-19 21:43:17 +0200

    Add snippets for extension hosting v3 (#6250)
    * Add snippets for extension hosting v3
    
    * Tweaks
    
    ---------
    
    Co-authored-by: Brandon Ording <bording@gmail.com>

---
+++ Snippets/Extensions.Hosting/Extensions.Hosting_3/WebApiControllerUsage.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NServiceBus;
+
+public class WebApiControllerUsage
+{
+    #region web-api-usage
+    [ApiController]
+    class WebApiController : ControllerBase
+    {
+        IMessageSession messageSession;
+
+        public WebApiController(IMessageSession messageSession)
+        {
+            this.messageSession = messageSession;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            await messageSession.Send(new MessageFromWebApi());
+            return Ok("message was sent successfully");
+        }
+    }
+    #endregion
+
+    class MessageFromWebApi
+    {
+    }
+}
