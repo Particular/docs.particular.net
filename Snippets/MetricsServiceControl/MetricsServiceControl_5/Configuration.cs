070dca6581b652df1daf51f81b6b931df7f5749f
Andreas Öhlund <andreas.ohlund@particular.net>
2023-10-26 00:35:29 +0200

    Add snippets for metrics servicecontrol v5 (#6262)

---
+++ Snippets/MetricsServiceControl/MetricsServiceControl_5/Configuration.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using NServiceBus;
+
+class Configuration
+{
+    void ConfigureEndpoint(EndpointConfiguration endpointConfiguration)
+    {
+        #region SendMetricDataToServiceControl
+        const string SERVICE_CONTROL_METRICS_ADDRESS = "particular.monitoring";
+
+        var metrics = endpointConfiguration.EnableMetrics();
+
+        metrics.SendMetricDataToServiceControl(
+            serviceControlMetricsAddress: SERVICE_CONTROL_METRICS_ADDRESS,
+            interval: TimeSpan.FromMinutes(1),
+            instanceId: "INSTANCE_ID_OPTIONAL");
+        #endregion
+
+        #region ServiceControlTTBR
+
+        metrics.SetServiceControlMetricsMessageTTBR(TimeSpan.FromHours(1));
+
+        #endregion
+    }
+    void SendMetricDataToServiceControlHostId(EndpointConfiguration endpointConfiguration)
+    {
+        #region SendMetricDataToServiceControlHostId
+        const string SERVICE_CONTROL_METRICS_ADDRESS = "particular.monitoring";
+
+        var endpointName = "MyEndpoint";
+        var machineName = $"{Dns.GetHostName()}.{IPGlobalProperties.GetIPGlobalProperties().DomainName}";
+        var instanceIdentifier = $"{endpointName}@{machineName}";
+
+        var metrics = endpointConfiguration.EnableMetrics();
+
+        metrics.SendMetricDataToServiceControl(
+            serviceControlMetricsAddress: SERVICE_CONTROL_METRICS_ADDRESS,
+            interval: TimeSpan.FromMinutes(1),
+            instanceId: instanceIdentifier);
+        #endregion
+    }
+}
