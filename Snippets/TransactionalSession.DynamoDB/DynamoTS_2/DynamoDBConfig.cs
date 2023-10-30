04e3d801f4c57a0d38da4c864f1eecab67423398
kentdr <dan.kent@particular.net>
2023-10-30 13:48:29 -0400

    Add v2 DynamoDB transactional session

---
+++ Snippets/TransactionalSession.DynamoDB/DynamoTS_2/DynamoDBConfig.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using NServiceBus;
+using NServiceBus.TransactionalSession;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+public class DynamoDBConfig
+{
+    public void Configure(EndpointConfiguration config)
+    {
+        #region enabling-transactional-session-dynamo
+
+        var persistence = config.UsePersistence<DynamoPersistence>();
+        persistence.EnableTransactionalSession();
+
+        #endregion
+    }
+
+    public async Task OpenDefault(IServiceProvider serviceProvider)
+    {
+        #region open-transactional-session-dynamo
+
+        using var childScope = serviceProvider.CreateScope();
+        var session = childScope.ServiceProvider.GetService<ITransactionalSession>();
+        await session.Open(new DynamoOpenSessionOptions())
+            .ConfigureAwait(false);
+
+        // use the session
+
+        await session.Commit()
+            .ConfigureAwait(false);
+
+        #endregion
+    }
+
+    public async Task UseSession(ITransactionalSession session)
+    {
+        #region use-transactional-session-dynamo
+        await session.Open(new DynamoOpenSessionOptions())
+            .ConfigureAwait(false);
+
+        // add messages to the transaction:
+        await session.Send(new MyMessage())
+            .ConfigureAwait(false);
+
+        // access the database:
+        var dynamoSession = session.SynchronizedStorageSession.DynamoPersistenceSession();
+
+        await session.Commit()
+            .ConfigureAwait(false);
+        #endregion
+    }
+}
