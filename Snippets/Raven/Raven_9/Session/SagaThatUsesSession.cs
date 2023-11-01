b060f7856482c5adf43a27584e22e05621831996
Andreas Öhlund <andreas.ohlund@particular.net>
2023-11-01 11:17:14 +0100

    Add snippets for RavenDB v9

---
+++ Snippets/Raven/Raven_9/Session/SagaThatUsesSession.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+
+#region ravendb-persistence-shared-session-for-saga
+
+public class SagaThatUsesSession :
+    Saga<SagaThatUsesSession.SagaData>,
+    IHandleMessages<MyMessage>
+{
+    public Task Handle(MyMessage message, IMessageHandlerContext context)
+    {
+        var document = new MyDocument();
+        var ravenSession = context.SynchronizedStorageSession.RavenSession();
+        return ravenSession.StoreAsync(document, context.CancellationToken);
+    }
+
+    #endregion
+
+    protected override void ConfigureHowToFindSaga(SagaPropertyMapper<SagaData> mapper)
+    {
+    }
+
+    public class SagaData :
+        ContainSagaData
+    {
+    }
+}
