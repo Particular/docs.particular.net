d3ce3d0a229628f44e95b71d19ffea023842b8c7
Andreas Öhlund <andreas.ohlund@particular.net>
2023-11-01 20:52:12 +0100

    Add snippets for saga audit v5 (#6287)

---
+++ Snippets/SagaAudit/SagaAudit_5/API.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using NServiceBus;
+
+class API
+{
+    public void Enable()
+    {
+        #region SagaAuditNew_Enable
+
+        var endpointConfiguration = new EndpointConfiguration("MyEndpoint");
+
+        //For ServiceControl 4.13.0 and later
+        endpointConfiguration.AuditSagaStateChanges(
+            serviceControlQueue: "audit");
+
+        //For previous versions of ServiceControl
+        endpointConfiguration.AuditSagaStateChanges(
+            serviceControlQueue: "particular.servicecontrol");
+
+        #endregion
+    }
+
+    public void CustomSerialization()
+    {
+        #region SagaAuditNew_CustomSerialization
+
+        var endpointConfiguration = new EndpointConfiguration("MyEndpoint");
+        endpointConfiguration.AuditSagaStateChanges(
+            serviceControlQueue: "particular.servicecontrol",
+            customSagaEntitySerialization: saga =>
+            {
+                var data = (MySagaData) saga;
+                var dictionary = new Dictionary<string, string>
+                {
+                    ["prop1"] = data.Name,
+                    ["prop2"] = data.Address
+                };
+                return dictionary;
+            });
+
+        #endregion
+    }
+
+    class MySagaData
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
