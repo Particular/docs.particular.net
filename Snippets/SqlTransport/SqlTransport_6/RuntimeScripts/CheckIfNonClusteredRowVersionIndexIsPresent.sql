0529907c2f0497900d6b0cafd7aa17e4abf0d3d6
Andreas Öhlund <andreas.ohlund@particular.net>
2023-11-01 12:27:30 +0100

    Remove unused snippets

--- Snippets/SqlTransport/SqlTransport_6/RuntimeScripts/CheckIfNonClusteredRowVersionIndexIsPresent.sql
+++
@@ -1,8 +0,0 @@
-startcode CheckIfNonClusteredRowVersionIndexIsPresentSql
-
-SELECT COUNT(*)
-FROM sys.indexes
-WHERE name = 'Index_RowVersion'
-    AND object_id = OBJECT_ID('{0}')
-    AND type = 2
-endcode
--- Snippets/SqlTransport/SqlTransport_8/RuntimeScripts/CheckIfTableHasRecoverableText.sql
+++
@@ -1,3 +0,0 @@
-startcode CheckIfTableHasRecoverableTextSql
-SELECT TOP (0) * FROM {0} WITH (NOLOCK);
-endcode
