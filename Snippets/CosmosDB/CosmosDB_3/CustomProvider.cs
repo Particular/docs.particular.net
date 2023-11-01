3c9a4b68d227d8cc1c2379ae84c3f9a8ed18c962
Andreas Öhlund <andreas.ohlund@particular.net>
2023-11-01 10:31:15 +0100

    Add cosmos v3 snippets

---
+++ Snippets/CosmosDB/CosmosDB_3/CustomProvider.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.DependencyInjection;
+using NServiceBus;
+using NServiceBus.Persistence.CosmosDB;
+
+#region CosmosDBCustomClientProvider
+
+class CustomCosmosClientProvider
+    : IProvideCosmosClient
+{
+    // get fully configured via DI
+    public CustomCosmosClientProvider(CosmosClient cosmosClient)
+    {
+        Client = cosmosClient;
+    }
+    public CosmosClient Client { get; }
+}
+#endregion
+
+class CosmosDBCustomClientProviderRegistration
+{
+    public CosmosDBCustomClientProviderRegistration(EndpointConfiguration endpointConfiguration)
+    {
+        #region CosmosDBCustomClientProviderRegistration
+
+        endpointConfiguration.RegisterComponents(c => c.AddTransient<IProvideCosmosClient, CustomCosmosClientProvider>());
+
+        #endregion
+    }
+}
