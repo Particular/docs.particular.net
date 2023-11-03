b8fa62b7f6264c3458b3086164ccd7b79fe881c4
Dan Kent <83468000+kentdr@users.noreply.github.com>
2023-11-03 17:11:04 -0400

    Add snippets for Mongo v4 (#6293)
    * Add v4 snippets
    
    * Remove namespaces

---
+++ Snippets/mongodb/MongoDB_4/DocumentVersion.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+class DocumentVersion
+{
+    async Task UpdateWithVersion(IMongoCollection<BsonDocument> collection, string versionFieldName, UpdateDefinitionBuilder<BsonDocument> updateBuilder, int currentVersion, Guid documentId)
+    {
+        #region MongoDBUpdateWithVersion
+
+        UpdateDefinition<BsonDocument> updateDefinition = updateBuilder.Inc(versionFieldName, 1);
+
+        //Define other update operations on the document
+
+        var modifiedDocument = await collection.FindOneAndUpdateAsync<BsonDocument>(
+            filter: document => document["_id"].AsGuid == documentId && document["_version"] == currentVersion,
+            update: updateDefinition,
+            options: new FindOneAndUpdateOptions<BsonDocument, BsonDocument> { IsUpsert = false, ReturnDocument = ReturnDocument.After });
+
+        if (modifiedDocument == null)
+        {
+            //The document was not updated because the version was already incremented.
+        }
+
+        #endregion
+    }
+}
