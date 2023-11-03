2c80a47443cd7ffecfbacf6fd1c62efccc65d81e
Dan Kent <83468000+kentdr@users.noreply.github.com>
2023-11-03 17:09:29 -0400

    Add TransactionalSession v3 snippets (#6297)
    * Add TransactionalSession v3 snippets
    
    * Use file scoped namespaces
    
    * Formatting
    
    ---------
    
    Co-authored-by: Brandon Ording <bording@gmail.com>

---
+++ Snippets/TransactionalSession/TransactionalSession_3/MyPersistenceConfigExtensions.cs
@@ -0,0 +1,10 @@
+namespace NServiceBus.MyPersistence;
+
+using NServiceBus;
+
+static class MyPersistenceConfigExtensions
+{
+    public static void EnableTransactionalSession(this PersistenceExtensions<MyPersistence> persistence)
+    {
+    }
+}
