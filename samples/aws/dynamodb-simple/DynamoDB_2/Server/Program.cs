8b55837f747a1659f2d5c2972a57336ad65b14ff
kentdr <dan.kent@particular.net>
2023-11-07 15:02:58 -0500

    Add DynamoDB simple sample v2

---
+++ samples/aws/dynamodb-simple/DynamoDB_2/Server/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Amazon.DynamoDBv2;
+using Amazon.Runtime;
+
+using NServiceBus;
+using NServiceBus.Persistence.DynamoDB;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.DynamoDB.Simple.Server";
+
+        #region DynamoDBConfig
+
+        var amazonDynamoDbClient = new AmazonDynamoDBClient(
+            new BasicAWSCredentials("localdb", "localdb"),
+            new AmazonDynamoDBConfig
+            {
+                ServiceURL = "http://localhost:8000"
+            });
+
+        var endpointConfiguration = new EndpointConfiguration("Samples.DynamoDB.Simple.Server");
+
+        var persistence = endpointConfiguration.UsePersistence<DynamoPersistence>();
+        persistence.DynamoClient(amazonDynamoDbClient);
+        persistence.UseSharedTable(new TableConfiguration
+        {
+            TableName = "Samples.DynamoDB.Simple"
+        });
+
+        #endregion
+
+        endpointConfiguration.UseTransport<LearningTransport>();
+        endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+
+        endpointConfiguration.EnableInstallers();
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
---
+++ samples/aws/dynamodb-simple/DynamoDB_2/Server/ShipOrderHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+
+public class ShipOrderHandler :
+    IHandleMessages<ShipOrder>
+{
+    static ILog log = LogManager.GetLogger<ShipOrderHandler>();
+
+    public Task Handle(ShipOrder message, IMessageHandlerContext context)
+    {
+        log.Info($"Order Shipped. OrderId {message.OrderId}");
+        return Task.CompletedTask;
+    }
+}
