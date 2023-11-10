786f100ca5bab5917c6c5da69525bbefd0158af1
Andreas Öhlund <andreas.ohlund@particular.net>
2023-11-10 13:40:27 +0100

    Update async pages sample

---
+++ samples/web/asp-mvc-application/Core_9/AsyncPagesMVC/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class HomeController :
+    Controller
+{
+    public ActionResult SendLinks()
+    {
+        return View();
+    }
+}
---
+++ samples/web/asp-mvc-application/Core_9/Server/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.Mvc.Server";
+        var endpointConfiguration = new EndpointConfiguration("Samples.Mvc.Server");
+        endpointConfiguration.EnableCallbacks(makesRequests: false);
+        endpointConfiguration.UseTransport(new LearningTransport());
+        endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
