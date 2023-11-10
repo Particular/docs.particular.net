6156f8beca9946d3c921219274c53e0c259b1b65
Andreas Öhlund <andreas.ohlund@particular.net>
2023-11-10 13:59:44 +0100

    Add send sample for v9

---
+++ samples/web/send-from-aspnetcore-webapi/Core_9/Endpoint/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+static class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.ASPNETCore.Endpoint";
+        var endpointConfiguration = new EndpointConfiguration("Samples.ASPNETCore.Endpoint");
+        endpointConfiguration.UsePersistence<LearningPersistence>();
+        endpointConfiguration.UseTransport(new LearningTransport());
+        endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration);
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop();
+    }
+}
