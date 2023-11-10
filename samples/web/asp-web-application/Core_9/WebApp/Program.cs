c7aa517df09aff8d84864af7d8d700bb1fc21464
Andreas Öhlund <andreas.ohlund@particular.net>
2023-11-10 13:54:35 +0100

    Add asp web app sample for v9

---
+++ samples/web/asp-web-application/Core_9/WebApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using NServiceBus;
+
+class Program
+{
+    public static void Main()
+    {
+        #region ApplicationStart
+        var builder = WebApplication.CreateBuilder();
+
+        builder.Host.UseNServiceBus(context =>
+        {
+            var endpointConfiguration = new EndpointConfiguration("Samples.AsyncPages.WebApplication");
+            endpointConfiguration.MakeInstanceUniquelyAddressable("1");
+            endpointConfiguration.EnableCallbacks();
+            endpointConfiguration.UseTransport(new LearningTransport());
+            endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+
+            return endpointConfiguration;
+        });
+        #endregion
+
+        builder.Services.AddRazorPages();
+
+        var app = builder.Build();
+
+        app.MapRazorPages();
+
+        app.Run();
+    }
+}
