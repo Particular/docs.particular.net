00161fc3afcc5726e5bbb6bdee92910cb32eb4b9
kentdr <dan.kent@particular.net>
2023-11-14 10:16:47 -0500

    Add simple azure table v6 sample

---
+++ samples/azure/azure-table/simple/ASTP_6/Server/Program.cs
@@ -0,0 +1,35 @@
+using System;
+
+using Azure.Data.Tables;
+
+using NServiceBus;
+
+Console.Title = "Samples.AzureTable.Simple.Server";
+
+#region AzureTableConfig
+
+var useStorageTable = true;
+var endpointConfiguration = new EndpointConfiguration("Samples.AzureTable.Simple.Server");
+
+var persistence = endpointConfiguration.UsePersistence<AzureTablePersistence>();
+
+var connection = useStorageTable ? "UseDevelopmentStorage=true" :
+    "TableEndpoint=https://localhost:8081/;AccountName=AzureTableSamples;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+
+var tableServiceClient = new TableServiceClient(connection);
+persistence.UseTableServiceClient(tableServiceClient);
+
+#endregion
+
+endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+endpointConfiguration.UseTransport(new LearningTransport());
+endpointConfiguration.EnableInstallers();
+
+var endpointInstance = await Endpoint.Start(endpointConfiguration)
+    .ConfigureAwait(false);
+
+Console.WriteLine("Press any key to exit");
+Console.ReadKey();
+
+await endpointInstance.Stop()
+    .ConfigureAwait(false);
