c3a0eca7e991add55a3fe4177ef4e0c71b6e0f49
Brandon Ording <bording@gmail.com>
2023-11-14 17:46:18 -0500

    Add NServiceBus.Encryption.MessageProperty v5 samples

---
+++ samples/encryption/encryption-conventions/PropertyEncryption_5/Shared/EncryptionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NServiceBus;
+using NServiceBus.Encryption.MessageProperty;
+
+public static class EncryptionExtensions
+{
+    #region ConfigureEncryption
+
+    public static void ConfigurationEncryption(this EndpointConfiguration endpointConfiguration)
+    {
+        var encryptionService = new AesEncryptionService(
+            encryptionKeyIdentifier: "2015-10",
+            key: Convert.FromBase64String("v96c7+e1aqFeNhMdZneHAQITEcgJu1z3ENSFUIr+FoM="));
+        endpointConfiguration.EnableMessagePropertyEncryption(encryptionService,
+            encryptedPropertyConvention: info => info.Name.StartsWith("Encrypted"));
+    }
+
+    #endregion
+}
