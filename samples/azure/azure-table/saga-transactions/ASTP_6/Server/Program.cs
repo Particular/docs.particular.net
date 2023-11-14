240809a147ad358aa7f12b7730071dff8354e21e
kentdr <dan.kent@particular.net>
2023-11-14 10:57:16 -0500

    Add saga transactions sample v6

---
+++ samples/azure/azure-table/saga-transactions/ASTP_6/Server/Program.cs
@@ -0,0 +1,45 @@
+using System;
+
+using Azure.Data.Tables;
+
+using NServiceBus;
+
+Console.Title = "Samples.AzureTable.Transactions.Server";
+
+#region AzureTableConfig
+
+var endpointConfiguration = new EndpointConfiguration("Samples.AzureTable.Transactions.Server");
+endpointConfiguration.EnableOutbox();
+
+var useStorageTable = true;
+var persistence = endpointConfiguration.UsePersistence<AzureTablePersistence>();
+
+var connection = useStorageTable ? "UseDevelopmentStorage=true" :
+    "TableEndpoint=https://localhost:8081/;AccountName=AzureTableSamples;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+
+var tableServiceClient = new TableServiceClient(connection);
+persistence.UseTableServiceClient(tableServiceClient);
+
+persistence.DefaultTable("Server");
+
+#endregion
+
+#region BehaviorRegistration
+
+endpointConfiguration.Pipeline.Register(new OrderIdAsPartitionKeyBehavior.Registration());
+
+#endregion
+
+endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+var transport = endpointConfiguration.UseTransport<LearningTransport>();
+transport.Transactions(TransportTransactionMode.ReceiveOnly);
+endpointConfiguration.EnableInstallers();
+
+var endpointInstance = await Endpoint.Start(endpointConfiguration)
+    .ConfigureAwait(false);
+
+Console.WriteLine("Press any key to exit");
+Console.ReadKey();
+
+await endpointInstance.Stop()
+    .ConfigureAwait(false);
