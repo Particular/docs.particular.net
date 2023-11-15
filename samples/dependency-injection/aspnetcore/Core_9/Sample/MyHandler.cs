cc46d3001eb1b4c38fa8ef1ce02e07fc0a54f280
Brandon Ording <bording@gmail.com>
2023-11-15 18:30:12 -0500

    Add NServiceBus 9 dependency injection samples

---
+++ samples/dependency-injection/aspnetcore/Core_9/Sample/MyHandler.cs
@@ -0,0 +1,17 @@
+﻿#region InjectingDependency
+public class MyHandler : IHandleMessages<MyMessage>
+{
+    MyService myService;
+
+    public MyHandler(MyService myService)
+    {
+        this.myService = myService;
+    }
+
+    public Task Handle(MyMessage message, IMessageHandlerContext context)
+    {
+        myService.WriteHello();
+        return Task.CompletedTask;
+    }
+}
+#endregion
---
+++ samples/dependency-injection/externally-managed-mode/Core_9/Sample/Program.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using NServiceBus;
+
+static class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.NServiceBus.ExternallyManagedContainer";
+
+        var endpointConfiguration = new EndpointConfiguration("Sample");
+        endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+        endpointConfiguration.UseTransport(new LearningTransport());
+
+        #region ContainerConfiguration
+
+        // ServiceCollection is provided by Microsoft.Extensions.DependencyInjection
+        var serviceCollection = new ServiceCollection();
+
+        // most dependencies may now be registered
+        serviceCollection.AddSingleton<Greeter>();
+        serviceCollection.AddSingleton<MessageSender>();
+
+        // EndpointWithExternallyManagedContainer.Create accepts an IServiceCollection,
+        // which is inherited by ServiceCollection
+        var endpointWithExternallyManagedContainer = EndpointWithExternallyManagedContainer
+            .Create(endpointConfiguration, serviceCollection);
+
+        // if IMessageSession is required as dependency, it may now be registered
+        serviceCollection.AddSingleton(p => endpointWithExternallyManagedContainer.MessageSession.Value);
+
+        #endregion
+
+        using (var serviceProvider = serviceCollection.BuildServiceProvider())
+        {
+            var endpoint = await endpointWithExternallyManagedContainer.Start(serviceProvider)
+                .ConfigureAwait(false);
+
+            var sender = serviceProvider.GetRequiredService<MessageSender>();
+            await sender.SendMessage()
+                .ConfigureAwait(false);
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+            await endpoint.Stop()
+                .ConfigureAwait(false);
+        }
+    }
+}
