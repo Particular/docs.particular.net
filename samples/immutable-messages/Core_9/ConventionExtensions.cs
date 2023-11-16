ee47ddbb51e028ad4b78a4c3208b0764143e3c70
Brandon Ording <bording@gmail.com>
2023-11-16 17:46:16 -0500

    Add NServiceBus 9 Immutable Messages sample

---
+++ samples/immutable-messages/Core_9/ConventionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using NServiceBus;
+
+public static class ConventionExtensions
+{
+    public static void ApplyCustomConventions(this EndpointConfiguration endpointConfiguration)
+    {
+        var conventions = endpointConfiguration.Conventions();
+        conventions.DefiningCommandsAs(
+            type =>
+            {
+                return type.Namespace != null &&
+                       type.Namespace.EndsWith("Commands");
+            });
+        conventions.DefiningEventsAs(
+            type =>
+            {
+                return type.Namespace != null &&
+                       type.Namespace.EndsWith("Events");
+            });
+        conventions.DefiningMessagesAs(
+            type =>
+            {
+                return type.Namespace != null &&
+                       type.Namespace.EndsWith("Messages");
+            });
+    }
+}
