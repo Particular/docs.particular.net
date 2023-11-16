b1258a11f23f711c63f701d822d673d206b0304a
Brandon Ording <bording@gmail.com>
2023-11-16 10:29:07 -0500

    Add NServiceBus 9 dependency injection samples (#6315)

---
+++ samples/dependency-injection/aspnetcore/Core_9/Sample/MyService.cs
@@ -0,0 +1,14 @@
+﻿public class MyService
+{
+    readonly ILogger logger;
+
+    public MyService(ILogger<MyService> logger)
+    {
+        this.logger = logger;
+    }
+
+    public void WriteHello()
+    {
+        logger.LogInformation("Hello from MyService.");
+    }
+}
---
+++ samples/dependency-injection/aspnetcore/Core_9/SampleAutofac/MyHandler.cs
@@ -0,0 +1,15 @@
+﻿public class MyHandler : IHandleMessages<MyMessage>
+{
+    MyService myService;
+
+    public MyHandler(MyService myService)
+    {
+        this.myService = myService;
+    }
+
+    public Task Handle(MyMessage message, IMessageHandlerContext context)
+    {
+        myService.WriteHello();
+        return Task.CompletedTask;
+    }
+}
