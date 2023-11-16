a07e91246dc9c97fcc0d33c3220bb898c72b8e5a
Brandon Ording <bording@gmail.com>
2023-11-16 17:06:41 -0500

    Add NServiceBus.Gateway 5 sample

---
+++ samples/gateway/Gateway_5/RemoteSite/Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Gateway;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.Gateway.RemoteSite";
+        var endpointConfiguration = new EndpointConfiguration("Samples.Gateway.RemoteSite");
+        endpointConfiguration.EnableInstallers();
+        endpointConfiguration.UseSerialization<XmlSerializer>();
+        endpointConfiguration.UseTransport(new LearningTransport());
+
+        #region RemoteSiteGatewayConfig
+        var gatewayConfig = endpointConfiguration.Gateway(new NonDurableDeduplicationConfiguration());
+        gatewayConfig.AddReceiveChannel("http://localhost:25899/RemoteSite/");
+        #endregion
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        Console.WriteLine("\r\nPress any key to stop program\r\n");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
