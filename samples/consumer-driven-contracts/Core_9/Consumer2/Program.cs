797698cd424091a824c6813a8ee8ce5b49394025
Brandon Ording <bording@gmail.com>
2023-11-16 19:01:45 -0500

    Add NServiceBus 9 Consumer-driven Contracts sample

---
+++ samples/consumer-driven-contracts/Core_9/Consumer2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.ConsumerDrivenContracts.Consumer2";
+        var endpointConfiguration = new EndpointConfiguration("Samples.ConsumerDrivenContracts.Consumer2");
+        endpointConfiguration.UsePersistence<NonDurablePersistence>();
+        var transport = endpointConfiguration.UseTransport(new LearningTransport());
+        endpointConfiguration.UseSerialization<XmlSerializer>();
+
+        // uncomment below to demonstrate json
+        //endpointConfiguration.UseSerialization<NewtonsoftJsonSerializer>();
+
+        endpointConfiguration.SendFailedMessagesTo("error");
+        endpointConfiguration.EnableInstallers();
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+
+        Console.WriteLine("Press any key to exit");
+
+        Console.ReadKey();
+
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
