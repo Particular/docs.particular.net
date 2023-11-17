a5c56c311b3f6ea74f997199efd925aa2a1807fc
Brandon Ording <bording@gmail.com>
2023-11-17 14:25:21 -0500

    Add NServiceBus 9 Custom Exception Handling sample (#6323)

---
+++ samples/message-error-handling/Core_9/CustomErrorHandling/MyCustomException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class MyCustomException :
+    Exception
+{
+    public MyCustomException()
+    {
+    }
+
+    public MyCustomException(string message) :
+        base(message)
+    {
+    }
+
+    public MyCustomException(string message, Exception innerException) :
+        base(message, innerException)
+    {
+    }
+}
