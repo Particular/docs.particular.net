ef11672872d726be821c91643ddac40956423808
Brandon Ording <bording@gmail.com>
2023-11-20 11:57:44 -0500

    Add NServiceBus.Gateway 5 sample (#6320)

---
+++ samples/gateway/Gateway_5/Shared/PriceUpdateAcknowledged.cs
@@ -0,0 +1,10 @@
+﻿using NServiceBus;
+
+namespace Shared
+{
+    public class PriceUpdateAcknowledged :
+    IMessage
+    {
+        public string BranchOffice { get; set; }
+    }
+}
---
+++ samples/gateway/Gateway_5/Shared/UpdatePrice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NServiceBus;
+
+namespace Shared
+{
+    public class UpdatePrice :
+        IMessage
+    {
+        public int ProductId { get; set; }
+        public double NewPrice { get; set; }
+        public DateTime ValidFrom { get; set; }
+    }
+}
