44eff4900ec6c050be23f77c73d400baa13086ab
Brandon Ording <bording@gmail.com>
2023-11-20 10:48:41 -0500

    Add NServiceBus 9 custom feature sample (#6318)

---
+++ samples/feature/Core_9/Sample/CustomLogger.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Diagnostics;
+using System.Text.Json;
+using NServiceBus;
+using NServiceBus.Logging;
+
+#region CustomLogger
+public class CustomLogger
+{
+    static ILog log = LogManager.GetLogger<CustomLogger>();
+    static readonly JsonSerializerOptions options = new() { WriteIndented = true };
+
+    public IDisposable StartTimer(string name)
+    {
+        return new Log(name);
+    }
+
+    public void WriteSaga(IContainSagaData sagaData)
+    {
+        var serialized = JsonSerializer.Serialize(sagaData, options);
+        log.Warn($"Saga State: \r\n{serialized}");
+    }
+
+    class Log :
+        IDisposable
+    {
+        string name;
+        Stopwatch stopwatch;
+
+        public Log(string name)
+        {
+            stopwatch = Stopwatch.StartNew();
+            this.name = name;
+        }
+
+        public void Dispose()
+        {
+            log.Warn($"{name} took {stopwatch.ElapsedMilliseconds}ms to process");
+        }
+    }
+}
+
+#endregion
