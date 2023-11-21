d64dd4065a0a76fb512de3433c114c143efab76b
Brandon Ording <bording@gmail.com>
2023-11-21 16:04:41 -0500

    Add NServiceBus 9 on-premises sample

---
+++ samples/showcase/on-premises/Core_9/Store.ECommerce/Program.cs
@@ -0,0 +1,30 @@
+﻿using NServiceBus;
+using Store.Messages.Commands;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddSignalR(c => c.EnableDetailedErrors = true);
+
+builder.UseNServiceBus(() =>
+{
+    var endpointConfiguration = new EndpointConfiguration("Store.ECommerce");
+    endpointConfiguration.PurgeOnStartup(true);
+    endpointConfiguration.ApplyCommonConfiguration(routing =>
+    {
+        routing.RouteToEndpoint(typeof(SubmitOrder).Assembly, "Store.Messages.Commands", "Store.Sales");
+    });
+
+    return endpointConfiguration;
+});
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapHub<OrdersHub>("/ordershub");
+app.MapControllers();
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ samples/showcase/on-premises/Core_9/Store.Messages/RequestResponse/ProvisionDownloadResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Store.Messages.RequestResponse
+{
+    using NServiceBus;
+    public class ProvisionDownloadResponse :
+        IMessage
+    {
+        public int OrderNumber { get; set; }
+        public string[] ProductIds { get; set; }
+        public string ClientId { get; set; }
+    }
+}
