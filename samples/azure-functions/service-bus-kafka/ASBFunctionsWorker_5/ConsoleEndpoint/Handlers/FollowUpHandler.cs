3643dc774720497e97be8c7814cd681b91239918
kentdr <dan.kent@particular.net>
2023-11-21 14:34:10 -0500

    Remove reference to Newtonsoft

---
+++ samples/azure-functions/service-bus-kafka/ASBFunctionsWorker_5/ConsoleEndpoint/Handlers/FollowUpHandler.cs
@@ -0,0 +1,14 @@
+﻿using AzureFunctions.Messages.NServiceBusMessages;
+using NServiceBus;
+
+namespace ConsoleEndpoint.Handlers;
+
+public class FollowUpHandler : IHandleMessages<FollowUp>
+{
+    public Task Handle(FollowUp message, IMessageHandlerContext context)
+    {
+        Console.WriteLine($"User [{message.CustomerId}] for unit [{message.UnitId}] has message: {message.Description}");
+
+        return Task.CompletedTask;
+    }
+}
