50c40630948508f73c59316af1090a54739e8568
Brandon Ording <bording@gmail.com>
2023-11-29 15:19:03 -0500

    Add NServiceBus 9 Unobtrusive sample (#6336)

---
+++ samples/unobtrusive/Core_9/Client/MyEventHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Events;
+using NServiceBus;
+using NServiceBus.Logging;
+
+public class MyEventHandler :
+    IHandleMessages<MyEvent>
+{
+    static ILog log = LogManager.GetLogger<MyEventHandler>();
+
+    public Task Handle(MyEvent message, IMessageHandlerContext context)
+    {
+        log.Info($"MyEvent received from server with id:{message.EventId}");
+        return Task.CompletedTask;
+    }
+}
