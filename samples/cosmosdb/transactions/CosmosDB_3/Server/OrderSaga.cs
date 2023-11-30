003d561bb02f38caa6f21c0cbc90b9dd42d458ce
kentdr <dan.kent@particular.net>
2023-11-30 11:12:49 -0500

    Add v3 of cosmos transaction sample

---
+++ samples/cosmosdb/transactions/CosmosDB_3/Server/OrderSaga.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+
+#region thesaga
+
+public class OrderSaga :
+    Saga<OrderSagaData>,
+    IAmStartedByMessages<StartOrder>,
+    IHandleMessages<OrderShipped>,
+    IHandleTimeouts<CompleteOrder>
+{
+    static readonly ILog Log = LogManager.GetLogger<OrderSaga>();
+
+    protected override void ConfigureHowToFindSaga(SagaPropertyMapper<OrderSagaData> mapper)
+    {
+        mapper.MapSaga(saga => saga.OrderId)
+            .ToMessage<StartOrder>(msg => msg.OrderId)
+            .ToMessageHeader<OrderShipped>("Sample.CosmosDB.Transaction.OrderId");
+    }
+
+    public Task Handle(StartOrder message, IMessageHandlerContext context)
+    {
+        var orderDescription = $"The saga for order {message.OrderId}";
+        Data.OrderDescription = orderDescription;
+        Log.Info($"Received StartOrder message {Data.OrderId}. Starting Saga");
+
+        var shipOrder = new ShipOrder
+        {
+            OrderId = message.OrderId
+        };
+
+        Log.Info("Order will complete in 5 seconds");
+        var timeoutData = new CompleteOrder
+        {
+            OrderDescription = orderDescription,
+            OrderId = Data.OrderId,
+        };
+
+        return Task.WhenAll(
+            context.SendLocal(shipOrder),
+            RequestTimeout(context, TimeSpan.FromSeconds(5), timeoutData)
+        );
+    }
+
+    public Task Handle(OrderShipped message, IMessageHandlerContext context)
+    {
+        Log.Info($"Order with OrderId {Data.OrderId} shipped on {message.ShippingDate}");
+        return Task.CompletedTask;
+    }
+
+    public Task Timeout(CompleteOrder state, IMessageHandlerContext context)
+    {
+        Log.Info($"Saga with OrderId {Data.OrderId} completed");
+        MarkAsComplete();
+        var orderCompleted = new OrderCompleted
+        {
+            OrderId = Data.OrderId
+        };
+        return context.Publish(orderCompleted);
+    }
+}
+
+#endregion
---
+++ samples/cosmosdb/transactions/CosmosDB_3/Server/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+using NServiceBus;
+using NServiceBus.Logging;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.CosmosDB.Transactions.Server";
+
+        #region CosmosDBConfig
+
+        var endpointConfiguration = new EndpointConfiguration("Samples.CosmosDB.Transactions.Server");
+        endpointConfiguration.EnableOutbox();
+
+        var persistence = endpointConfiguration.UsePersistence<CosmosPersistence>();
+        var connection = @"AccountEndpoint = https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+        persistence.DatabaseName("Samples.CosmosDB.Transactions");
+        persistence.CosmosClient(new CosmosClient(connection));
+        persistence.DefaultContainer("Server", "/OrderId");
+
+        #endregion
+
+        #region TransactionInformationFromLogicalMessage
+
+        var transactionInformation = persistence.TransactionInformation();
+        transactionInformation.ExtractPartitionKeyFromMessage<IProvideOrderId>(provideOrderId =>
+        {
+            Log.Info($"Found partition key '{provideOrderId.OrderId}' from '{nameof(IProvideOrderId)}'");
+            return new PartitionKey(provideOrderId.OrderId.ToString());
+        });
+        #endregion
+
+        #region TransactionInformationFromHeader
+        transactionInformation.ExtractPartitionKeyFromHeader("Sample.CosmosDB.Transaction.OrderId", orderId =>
+        {
+            Log.Info($"Found partition key '{orderId}' from header 'Sample.CosmosDB.Transaction'");
+            return orderId;
+        });
+
+        #endregion
+
+        var transport = new LearningTransport
+        {
+            TransportTransactionMode = TransportTransactionMode.ReceiveOnly
+        };
+        endpointConfiguration.UseTransport(transport);
+        endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+
+        endpointConfiguration.EnableInstallers();
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+
+    static ILog Log = LogManager.GetLogger<Program>();
+}
