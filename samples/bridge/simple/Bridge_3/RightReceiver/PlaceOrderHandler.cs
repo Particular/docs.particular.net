28ed217db8ca2350731dc27cadc307764c338df5
kentdr <dan.kent@particular.net>
2023-12-04 09:49:58 -0500

    Add v3 simple bridge sample

---
+++ samples/bridge/simple/Bridge_3/RightReceiver/PlaceOrderHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+public class PlaceOrderHandler : IHandleMessages<PlaceOrder>
+{
+    public async Task Handle(PlaceOrder message, IMessageHandlerContext context)
+    {
+        Console.WriteLine($"Received PlaceOrder Command with Id {message.OrderId}");
+
+        await context.Reply(new OrderResponse { OrderId = message.OrderId }).ConfigureAwait(false);
+    }
+}
