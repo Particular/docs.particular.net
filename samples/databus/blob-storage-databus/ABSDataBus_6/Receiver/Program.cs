a7c9ad6d26256980abe73e6c6ca493521da1ff10
kentdr <dan.kent@particular.net>
2023-12-05 09:56:31 -0500

    Add databus blob storage v6 sample

---
+++ samples/databus/blob-storage-databus/ABSDataBus_6/Receiver/Program.cs
@@ -0,0 +1,29 @@
+using Azure.Storage.Blobs;
+using NServiceBus;
+using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.AzureBlobStorageDataBus.Receiver";
+        var endpointConfiguration = new EndpointConfiguration("Samples.AzureBlobStorageDataBus.Receiver");
+
+        var blobServiceClient = new BlobServiceClient("UseDevelopmentStorage=true");
+        var dataBus = endpointConfiguration.UseDataBus<AzureDataBus, SystemJsonDataBusSerializer>()
+            .Container("testcontainer")
+            .UseBlobServiceClient(blobServiceClient);
+
+        endpointConfiguration.UseSerialization<XmlSerializer>();
+        endpointConfiguration.UseTransport(new LearningTransport());
+        endpointConfiguration.EnableInstallers();
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
