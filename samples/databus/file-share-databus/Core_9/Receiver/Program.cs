5122da4a3f79eca18a6d8c4c6e73a9a339ea04c9
kentdr <dan.kent@particular.net>
2023-12-06 11:39:13 -0500

    Add v9 of file share databus sample

---
+++ samples/databus/file-share-databus/Core_9/Receiver/Program.cs
@@ -0,0 +1,23 @@
+using NServiceBus;
+using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.DataBus.Receiver";
+        var endpointConfiguration = new EndpointConfiguration("Samples.DataBus.Receiver");
+        var dataBus = endpointConfiguration.UseDataBus<FileShareDataBus, SystemJsonDataBusSerializer>();
+        dataBus.BasePath(@"..\..\..\..\storage");
+        endpointConfiguration.UsePersistence<LearningPersistence>();
+        endpointConfiguration.UseSerialization<XmlSerializer>();
+        endpointConfiguration.UseTransport(new LearningTransport());
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
---
+++ samples/databus/file-share-databus/Core_9/Shared/AnotherMessageWithLargePayload.cs
@@ -0,0 +1,11 @@
+﻿using NServiceBus;
+
+#region AnotherMessageWithLargePayload
+
+public class AnotherMessageWithLargePayload :
+    ICommand
+{
+    public byte[] LargeBlob { get; set; }
+}
+
+#endregion
