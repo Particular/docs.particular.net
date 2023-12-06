3609a835478f90bd20c367da7502bd319629018b
kentdr <dan.kent@particular.net>
2023-12-06 10:18:52 -0500

    Add databus customer serializer sample v9

---
+++ samples/databus/custom-serializer/Core_9/Receiver/Program.cs
@@ -0,0 +1,29 @@
+using NServiceBus;
+using Shared;
+using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.DataBus.Receiver";
+        var endpointConfiguration = new EndpointConfiguration("Samples.DataBus.Receiver");
+
+        #region ConfigureReceiverCustomDataBusSerializer
+
+        var dataBus = endpointConfiguration.UseDataBus<FileShareDataBus, BsonDataBusSerializer>();
+        dataBus.BasePath(@"..\..\..\..\storage");
+
+        #endregion
+
+        endpointConfiguration.UseSerialization<XmlSerializer>();
+        endpointConfiguration.UseTransport(new LearningTransport());
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
