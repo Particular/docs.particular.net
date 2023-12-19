fe17696fcc9c946591597c97429d324200d78d83
Dan Kent <83468000+kentdr@users.noreply.github.com>
2023-12-19 10:14:14 -0500

    Add Databus v6 samples (#6352)
    * Add databus blob storage v6 sample
    
    * Bump TFMs
    
    * Remove newtonsoft reference
    
    * Bump dependencies
    
    * Switch to SystemJsonSerializer
    
    * Add databus customer serializer sample v9
    
    * Bump TFMs
    
    * Bump dependencies
    
    * Add v9 of customer serializer converter sample
    
    * Bump TFMs
    
    * Bump references
    
    * Add v9 of file share databus sample
    
    * Bump TFMs
    
    * Bump references
    
    * Fix databus cleanup function sample
    
    * Add v6 of databus cleanup function sample
    
    * Revert "Add v6 of databus cleanup function sample"
    
    This reverts commit 501f296978ca0fe26ea6270f5c60457a51bf8f08.
    
    * Use SystemJsonSerializer instead of XML
    
    * Remove LearningTransport
    
    * Remove annoying extra line
    
    * Use SystemJsonSerializer instead of XML
    
    * Remove annoying extra line

---
+++ samples/databus/databus-custom-serializer-converter/Core_9/Shared/DatabusPropertyConverterFactory.cs
@@ -0,0 +1,44 @@
+﻿using NServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Shared
+{
+    #region DatabusPropertyConverterFactory
+    public class DatabusPropertyConverterFactory : JsonConverterFactory
+    {
+        public override bool CanConvert(Type typeToConvert)
+        {
+            if (!typeToConvert.IsGenericType)
+            {
+                return false;
+            }
+            if (typeToConvert.GetGenericTypeDefinition() != typeof(DataBusProperty<>))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+        {
+            Type keyType = typeToConvert.GetGenericArguments()[0];
+
+            JsonConverter converter = (JsonConverter)Activator.CreateInstance(
+                typeof(DatabusPropertyConverter<>).MakeGenericType(
+                    new Type[] { keyType }),
+                BindingFlags.Instance | BindingFlags.Public,
+                binder: null,
+                args: new object[] { options },
+                culture: null)!;
+
+            return converter;
+        }
+    }
+    #endregion
+}
