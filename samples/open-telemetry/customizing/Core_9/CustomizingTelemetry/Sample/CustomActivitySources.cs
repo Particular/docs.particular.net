79b63d34ebf2309a0fc2741cd103c44fd778fb19
Andreas Öhlund <andreas.ohlund@particular.net>
2024-01-08 12:33:03 +0100

    Add v9 customizing sample

---
+++ samples/open-telemetry/customizing/Core_9/CustomizingTelemetry/Sample/CustomActivitySources.cs
@@ -0,0 +1,9 @@
+﻿using System.Diagnostics;
+
+#region custom-activity-source
+static class CustomActivitySources
+{
+    public const string Name = "Sample.ActivitySource";
+    public static ActivitySource Main = new ActivitySource(Name);
+}
+#endregion
