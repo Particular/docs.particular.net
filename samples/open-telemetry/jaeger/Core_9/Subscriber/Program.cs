c8216f0368d0db9e3739540d75aa0b049c5589b1
Andreas Öhlund <andreas.ohlund@particular.net>
2024-01-09 08:47:52 +0100

    Add v9 sample for jaeger

---
+++ samples/open-telemetry/jaeger/Core_9/Subscriber/Program.cs
@@ -0,0 +1,37 @@
+using NServiceBus;
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    const string EndpointName = "Samples.OpenTelemetry.Subscriber";
+
+    public static async Task Main()
+    {
+        Console.Title = EndpointName;
+
+        var tracerProvider = Sdk.CreateTracerProviderBuilder()
+            .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(EndpointName))
+            .AddSource("NServiceBus.Core")
+            .AddOtlpExporter() // The exporter defaults to gRPC on over port 4317 - https://github.com/open-telemetry/opentelemetry-dotnet/blob/main/src/OpenTelemetry.Exporter.OpenTelemetryProtocol/README.md#otlpexporteroptions
+            .Build();
+
+        var endpointConfiguration = new EndpointConfiguration(EndpointName);
+
+        endpointConfiguration.EnableOpenTelemetry();
+
+        endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+        endpointConfiguration.UseTransport(new LearningTransport());
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration);
+
+        Console.WriteLine("Press any key to exit");
+        _ = Console.ReadKey();
+
+        await endpointInstance.Stop();
+        tracerProvider.ForceFlush();
+    }
+}
