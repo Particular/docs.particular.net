4efc8dd5613d4b9ec52b486673a1398db128b8c4
Andreas Öhlund <andreas.ohlund@particular.net>
2024-01-11 12:33:18 +0100

    Add metrics 5 app insight sample

---
+++ samples/logging/application-insights/Metrics_5/Endpoint/ApplicationInsightsConfigurationExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.ApplicationInsights.Extensibility;
+using NServiceBus;
+using NServiceBus.Configuration.AdvancedExtensibility;
+
+public static class ApplicationInsightsConfigurationExtensions
+{
+    public static void EnableApplicationInsights(this EndpointConfiguration configuration, TelemetryConfiguration telemetryConfiguration)
+    {
+        #region enable-nsb-metrics
+        configuration.EnableMetrics();
+        #endregion
+        configuration.EnableFeature<ApplicationInsightsFeature>();
+        configuration.GetSettings().Set(telemetryConfiguration);
+    }
+}
