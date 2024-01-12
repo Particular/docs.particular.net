f6e6e043359a4c8cc00f7f73671a1551a44255bd
Dan Kent <83468000+kentdr@users.noreply.github.com>
2024-01-12 14:22:09 -0500

    Add MongoDB v4 samples (#6395)
    * Specify serializer in client program
    
    * Add MongoDB v4 sample
    
    * Bump TFMS and LangVer
    
    * Bump NSB and persister
    
    * Remove LearningPersister
    
    * Tweaks
    
    ---------
    
    Co-authored-by: Brandon Ording <bording@gmail.com>

---
+++ samples/mongodb/simple/MongoDB_4/Server/Program.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.MongoDB.Server";
+
+        #region mongoDbConfig
+
+        var endpointConfiguration = new EndpointConfiguration("Samples.MongoDB.Server");
+        var persistence = endpointConfiguration.UsePersistence<MongoPersistence>();
+        persistence.DatabaseName("Samples_MongoDB_Server");
+
+        #endregion
+
+        endpointConfiguration.EnableInstallers();
+        endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+        endpointConfiguration.UseTransport(new LearningTransport());
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
