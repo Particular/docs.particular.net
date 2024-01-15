90e18fe9a3005e6e39957e2cef8087ca6e422422
lailabougria <laila.bougria@particular.net>
2024-01-15 21:19:19 +0100

    add sample for v9

---
+++ samples/saga/migration/Core_9/Server/NotFoundHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Sagas;
+
+#region Forwarder
+class NotFoundHandler :
+    IHandleSagaNotFound
+{
+    public Task Handle(object message, IMessageProcessingContext context)
+    {
+        return context.ForwardCurrentMessageTo("Samples.SagaMigration.Server.New");
+    }
+}
+#endregion
