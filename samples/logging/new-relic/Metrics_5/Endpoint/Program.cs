f76bd90749727292f5d2c5fd0be053ad7a8b18c1
Andreas Öhlund <andreas.ohlund@particular.net>
2024-01-17 09:10:52 +0100

    Add newrelic sample for metrics 5

---
+++ samples/logging/new-relic/Metrics_5/Endpoint/Program.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Threading.Tasks;
+using Endpoint;
+using NServiceBus;
+
+class Program
+{
+    static async Task Main()
+    {
+        var endpointConfiguration = new EndpointConfiguration(Console.Title = "Samples.Metrics.Tracing.Endpoint");
+
+        endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+        endpointConfiguration.UseTransport<LearningTransport>();
+
+        NewRelicMetrics.Setup(endpointConfiguration);
+
+        var endpointInstance = await NServiceBus.Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+
+        #region newrelic-load-simulator
+
+        var simulator = new LoadSimulator(endpointInstance, TimeSpan.Zero, TimeSpan.FromSeconds(10));
+        simulator.Start();
+
+        #endregion
+
+        try
+        {
+            Console.WriteLine("Endpoint started.");
+            Console.WriteLine("Press [ENTER] to send additional messages.");
+            Console.WriteLine("Press [ESC] to quit.");
+
+            while (true)
+            {
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.Escape:
+                        return;
+                    case ConsoleKey.Enter:
+                        await endpointInstance.SendLocal(new SomeCommand())
+                            .ConfigureAwait(false);
+                        break;
+                }
+            }
+        }
+        finally
+        {
+            await simulator.Stop()
+                .ConfigureAwait(false);
+            await endpointInstance.Stop()
+                .ConfigureAwait(false);
+        }
+    }
+}
