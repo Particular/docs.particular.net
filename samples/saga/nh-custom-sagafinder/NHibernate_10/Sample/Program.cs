be343ca8f92ef31d2d1cc59ce3d77e565a5ff9d6
lailabougria <laila.bougria@particular.net>
2024-01-19 18:06:29 +0100

    add sample for core v8

---
+++ samples/saga/nh-custom-sagafinder/NHibernate_10/Sample/Program.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Threading.Tasks;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Driver;
+using NServiceBus;
+using NServiceBus.Persistence;
+
+class Program
+{
+    static async Task Main()
+    {
+        var endpointName = "Samples.NHibernateCustomSagaFinder";
+        Console.Title = endpointName;
+        var endpointConfiguration = new EndpointConfiguration(endpointName);
+        endpointConfiguration.EnableInstallers();
+        endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+        endpointConfiguration.UseTransport(new LearningTransport());
+
+        #region config
+
+        // for SqlExpress use Data Source=.\SqlExpress;Initial Catalog=NsbSamplesNhCustomSagaFinder;Integrated Security=True;Max Pool Size=100;Encrypt=false
+        var connectionString = @"Server=localhost,1433;Initial Catalog=NsbSamplesNhCustomSagaFinder;User Id=SA;Password=yourStrong(!)Password;Max Pool Size=100;Encrypt=false";
+        var hibernateConfig = new Configuration();
+        hibernateConfig.DataBaseIntegration(x =>
+        {
+            x.ConnectionString = connectionString;
+            x.Dialect<MsSql2012Dialect>();
+            x.Driver<MicrosoftDataSqlClientDriver>();
+        });
+
+        var persistence = endpointConfiguration.UsePersistence<NHibernatePersistence>()
+            .UseConfiguration(hibernateConfig);
+
+        persistence.ConnectionString(connectionString);
+
+        #endregion
+
+        SqlHelper.EnsureDatabaseExists(connectionString);
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        var startOrder = new StartOrder
+        {
+            OrderId = "123"
+        };
+        await endpointInstance.SendLocal(startOrder)
+            .ConfigureAwait(false);
+
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
