1893f878e1f4cb5b706c87ce35196f769a480fda
David Boike <david.boike@gmail.com>
2024-01-19 12:08:26 -0600

    Dispatch notifications NSB9

---
+++ samples/pipeline/dispatch-notifications/Core_9/SampleApp/ConfigExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using NServiceBus;
+using NServiceBus.Configuration.AdvancedExtensibility;
+using NServiceBus.Features;
+
+#region config-extensions
+static class ConfigExtensions
+{
+    public static void NotifyDispatch(this EndpointConfiguration endpointConfiguration, IWatchDispatches watch)
+    {
+        var settings = endpointConfiguration.GetSettings();
+        settings.EnableFeatureByDefault<DispatchNotificationFeature>();
+        settings.GetOrCreate<List<IWatchDispatches>>().Add(watch);
+    }
+}
+#endregion
---
+++ samples/pipeline/dispatch-notifications/Core_9/SampleApp/IWatchDispatches.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using NServiceBus.Transport;
+using System.Collections.Generic;
+
+#region watch-interface
+interface IWatchDispatches
+{
+    Task Notify(IEnumerable<TransportOperation> operations);
+}
+#endregion
