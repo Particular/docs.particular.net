ed7ae881188d62ddbb3f2edbc8017adac97abd98
kentdr <dan.kent@particular.net>
2024-01-26 11:13:20 -0500

    Create v9 of RavenDB simple sample

---
+++ samples/ravendb/simple/Raven_9/Client/Program.cs
@@ -0,0 +1,37 @@
+using System;
+using NServiceBus;
+
+Console.Title = "Samples.RavenDB.Client";
+var endpointConfiguration = new EndpointConfiguration("Samples.RavenDB.Client");
+endpointConfiguration.EnableInstallers();
+endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+endpointConfiguration.UseTransport(new LearningTransport());
+
+var endpointInstance = await Endpoint.Start(endpointConfiguration)
+    .ConfigureAwait(false);
+
+Console.WriteLine("Press 'enter' to send a StartOrder messages");
+Console.WriteLine("Press any other key to exit");
+
+while (true)
+{
+    var key = Console.ReadKey();
+    Console.WriteLine();
+
+    if (key.Key != ConsoleKey.Enter)
+    {
+        break;
+    }
+
+    var orderId = Guid.NewGuid();
+    var startOrder = new StartOrder
+    {
+        OrderId = orderId
+    };
+    await endpointInstance.Send("Samples.RavenDB.Server", startOrder)
+        .ConfigureAwait(false);
+    Console.WriteLine($"StartOrder Message sent with OrderId {orderId}");
+}
+
+await endpointInstance.Stop()
+    .ConfigureAwait(false);
