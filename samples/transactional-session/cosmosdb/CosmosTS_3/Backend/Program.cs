b10f9305c42a64e3c9058375dfd2f02c4f129775
Andreas Öhlund <andreas.ohlund@particular.net>
2024-01-29 10:15:47 +0100

    Add v9 cosmos sample

---
+++ samples/transactional-session/cosmosdb/CosmosTS_3/Backend/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Hosting;
+using NServiceBus;
+
+Console.Title = "Backend";
+
+var builder = Host.CreateApplicationBuilder(args);
+
+var endpointConfiguration = new EndpointConfiguration("Samples.TransactionalSession.Backend");
+endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+endpointConfiguration.UseTransport(new LearningTransport { TransportTransactionMode = TransportTransactionMode.ReceiveOnly });
+endpointConfiguration.EnableOutbox();
+
+#region cosmos-txsession-backend-persistence
+var persistence = endpointConfiguration.UsePersistence<CosmosPersistence>();
+persistence.CosmosClient(new CosmosClient(Configuration.CosmosDBConnectionString));
+persistence.DefaultContainer("Orders", "/CustomerId");
+persistence.TransactionInformation().ExtractPartitionKeyFromMessage<OrderReceived>(message => new PartitionKey(message.CustomerId));
+#endregion
+
+builder.UseNServiceBus(endpointConfiguration);
+
+await builder.Build().RunAsync();
