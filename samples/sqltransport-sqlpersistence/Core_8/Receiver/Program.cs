cc9a1843a899776704e3d7c8e99400892f255c92
Andreas Öhlund <andreas.ohlund@particular.net>
2024-01-30 09:32:52 +0100

    Cleanup v8 sample

--- samples/sqltransport-sqlpersistence/Core_8/Receiver/Program.cs
+++ samples/sqltransport-sqlpersistence/Core_8/Receiver/Program.cs
@@ -1,58 +1,51 @@
 using System;
-using Microsoft.Data.SqlClient;
 using System.IO;
-using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 using NServiceBus;
 using NServiceBus.Transport.SqlServer;
 
-public static class Program
+Console.Title = "Samples.Sql.Receiver";
+
+#region ReceiverConfiguration
+
+var endpointConfiguration = new EndpointConfiguration("Samples.Sql.Receiver");
+endpointConfiguration.SendFailedMessagesTo("error");
+endpointConfiguration.AuditProcessedMessagesTo("audit");
+endpointConfiguration.EnableInstallers();
+// for SqlExpress use Data Source=.\SqlExpress;Initial Catalog=NsbSamplesSql;Integrated Security=True;Max Pool Size=100;Encrypt=false
+var connectionString = @"Server=localhost,1433;Initial Catalog=NsbSamplesSql;User Id=SA;Password=yourStrong(!)Password;Max Pool Size=100;Encrypt=false";
+
+var transport = new SqlServerTransport(connectionString)
 {
-    static async Task Main()
-    {
-        Console.Title = "Samples.Sql.Receiver";
-
-        #region ReceiverConfiguration
-
-        var endpointConfiguration = new EndpointConfiguration("Samples.Sql.Receiver");
-        endpointConfiguration.SendFailedMessagesTo("error");
-        endpointConfiguration.AuditProcessedMessagesTo("audit");
-        endpointConfiguration.EnableInstallers();
-        // for SqlExpress use Data Source=.\SqlExpress;Initial Catalog=NsbSamplesSql;Integrated Security=True;Max Pool Size=100;Encrypt=false
-        var connectionString = @"Server=localhost,1433;Initial Catalog=NsbSamplesSql;User Id=SA;Password=yourStrong(!)Password;Max Pool Size=100;Encrypt=false";
-
-        var transport = new SqlServerTransport(connectionString)
-        {
-            DefaultSchema = "receiver",
-            TransportTransactionMode = TransportTransactionMode.SendsAtomicWithReceive,
-            Subscriptions =
+    DefaultSchema = "receiver",
+    TransportTransactionMode = TransportTransactionMode.SendsAtomicWithReceive,
+    Subscriptions =
             {
                 CacheInvalidationPeriod = TimeSpan.FromMinutes(1),
                 SubscriptionTableName = new SubscriptionTableName(table: "Subscriptions", schema: "dbo")
             }
-        };
+};
 
-        transport.SchemaAndCatalog.UseSchemaForQueue("error", "dbo");
-        transport.SchemaAndCatalog.UseSchemaForQueue("audit", "dbo");
-        transport.SchemaAndCatalog.UseSchemaForQueue("Samples.Sql.Sender", "sender");
+transport.SchemaAndCatalog.UseSchemaForQueue("error", "dbo");
+transport.SchemaAndCatalog.UseSchemaForQueue("audit", "dbo");
+transport.SchemaAndCatalog.UseSchemaForQueue("Samples.Sql.Sender", "sender");
 
-        endpointConfiguration.UseSerialization<SystemJsonSerializer>();
-        var routing = endpointConfiguration.UseTransport(transport);
-        routing.RouteToEndpoint(typeof(OrderAccepted), "Samples.Sql.Sender");
+endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+var routing = endpointConfiguration.UseTransport(transport);
+routing.RouteToEndpoint(typeof(OrderAccepted), "Samples.Sql.Sender");
 
-        var persistence = endpointConfiguration.UsePersistence<SqlPersistence>();
-        var dialect = persistence.SqlDialect<SqlDialect.MsSqlServer>();
-        dialect.Schema("receiver");
-        persistence.ConnectionBuilder(() => new SqlConnection(connectionString));
-        persistence.TablePrefix("");
+var persistence = endpointConfiguration.UsePersistence<SqlPersistence>();
+var dialect = persistence.SqlDialect<SqlDialect.MsSqlServer>();
+dialect.Schema("receiver");
+persistence.ConnectionBuilder(() => new SqlConnection(connectionString));
+persistence.TablePrefix("");
 
-        #endregion
+#endregion
 
-        await SqlHelper.CreateSchema(connectionString, "receiver");
-        var allText = File.ReadAllText("Startup.sql");
-        await SqlHelper.ExecuteSql(connectionString, allText);
-        var endpointInstance = await Endpoint.Start(endpointConfiguration).ConfigureAwait(false);
-        Console.WriteLine("Press any key to exit");
-        Console.ReadKey();
-        await endpointInstance.Stop().ConfigureAwait(false);
-    }
-}
+await SqlHelper.CreateSchema(connectionString, "receiver");
+var allText = File.ReadAllText("Startup.sql");
+await SqlHelper.ExecuteSql(connectionString, allText);
+var endpointInstance = await Endpoint.Start(endpointConfiguration);
+Console.WriteLine("Press any key to exit");
+Console.ReadKey();
+await endpointInstance.Stop();
--- samples/sqltransport-sqlpersistence/Core_8/Sender/Program.cs
+++ samples/sqltransport-sqlpersistence/Core_8/Sender/Program.cs
@@ -1,73 +1,65 @@
 using System;
-using System.Threading.Tasks;
 using NServiceBus;
 using NServiceBus.Transport.SqlServer;
 
-public static class Program
+Console.Title = "Samples.Sql.Sender";
+
+var endpointConfiguration = new EndpointConfiguration("Samples.Sql.Sender");
+endpointConfiguration.SendFailedMessagesTo("error");
+endpointConfiguration.EnableInstallers();
+
+#region SenderConfiguration
+
+// for SqlExpress use Data Source=.\SqlExpress;Initial Catalog=NsbSamplesSql;Integrated Security=True;Max Pool Size=100;Encrypt=false
+var connectionString = @"Server=localhost,1433;Initial Catalog=NsbSamplesSql;User Id=SA;Password=yourStrong(!)Password;Max Pool Size=100;Encrypt=false";
+var transport = new SqlServerTransport(connectionString)
 {
-    static Random random;
-
-    public static async Task Main()
-    {
-        random = new Random();
-
-        Console.Title = "Samples.Sql.Sender";
-        var endpointConfiguration = new EndpointConfiguration("Samples.Sql.Sender");
-        endpointConfiguration.SendFailedMessagesTo("error");
-        endpointConfiguration.EnableInstallers();
-
-        #region SenderConfiguration
-
-        // for SqlExpress use Data Source=.\SqlExpress;Initial Catalog=NsbSamplesSql;Integrated Security=True;Max Pool Size=100;Encrypt=false
-        var connectionString = @"Server=localhost,1433;Initial Catalog=NsbSamplesSql;User Id=SA;Password=yourStrong(!)Password;Max Pool Size=100;Encrypt=false";
-        var transport = new SqlServerTransport(connectionString)
-        {
-            DefaultSchema = "sender",
-            Subscriptions =
+    DefaultSchema = "sender",
+    Subscriptions =
             {
                 SubscriptionTableName = new SubscriptionTableName(
                         table: "Subscriptions",
                         schema: "dbo")
             },
-            TransportTransactionMode = TransportTransactionMode.SendsAtomicWithReceive
-        };
-        transport.SchemaAndCatalog.UseSchemaForQueue("error", "dbo");
-        transport.SchemaAndCatalog.UseSchemaForQueue("audit", "dbo");
+    TransportTransactionMode = TransportTransactionMode.SendsAtomicWithReceive
+};
+transport.SchemaAndCatalog.UseSchemaForQueue("error", "dbo");
+transport.SchemaAndCatalog.UseSchemaForQueue("audit", "dbo");
 
-        endpointConfiguration.UsePersistence<NonDurablePersistence>();
+endpointConfiguration.UsePersistence<NonDurablePersistence>();
 
-        endpointConfiguration.UseSerialization<SystemJsonSerializer>();
-        endpointConfiguration.UseTransport(transport);
+endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+endpointConfiguration.UseTransport(transport);
 
-        #endregion
+#endregion
 
-        await SqlHelper.CreateSchema(connectionString, "sender");
+await SqlHelper.CreateSchema(connectionString, "sender");
 
-        var endpointInstance = await Endpoint.Start(endpointConfiguration)
-            .ConfigureAwait(false);
-        Console.WriteLine("Press enter to send a message");
-        Console.WriteLine("Press any key to exit");
+var endpointInstance = await Endpoint.Start(endpointConfiguration);
 
-        while (true)
-        {
-            var key = Console.ReadKey();
-            Console.WriteLine();
+Console.WriteLine("Press enter to send a message");
+Console.WriteLine("Press any key to exit");
 
-            if (key.Key != ConsoleKey.Enter)
-            {
-                break;
-            }
+var random = new Random();
 
-            var orderSubmitted = new OrderSubmitted
-            {
-                OrderId = Guid.NewGuid(),
-                Value = random.Next(100)
-            };
-            await endpointInstance.Publish(orderSubmitted)
-                .ConfigureAwait(false);
-            Console.WriteLine("Published OrderSubmitted message");
-        }
-        await endpointInstance.Stop()
-            .ConfigureAwait(false);
+while (true)
+{
+    var key = Console.ReadKey();
+    Console.WriteLine();
+
+    if (key.Key != ConsoleKey.Enter)
+    {
+        break;
     }
+
+    var orderSubmitted = new OrderSubmitted
+    {
+        OrderId = Guid.NewGuid(),
+        Value = random.Next(100)
+    };
+    await endpointInstance.Publish(orderSubmitted)
+        .ConfigureAwait(false);
+    Console.WriteLine("Published OrderSubmitted message");
 }
+
+await endpointInstance.Stop();
