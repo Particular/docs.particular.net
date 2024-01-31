cf3114bbec501653fea8ab8b035d38edb8365455
Andreas Öhlund <andreas.ohlund@particular.net>
2024-01-31 12:58:23 +0100

    Cleanup v7 sample

--- samples/sqltransport/simple/SqlTransport_7/Receiver/Program.cs
+++ samples/sqltransport/simple/SqlTransport_7/Receiver/Program.cs
@@ -1,32 +1,25 @@
 using System;
-using System.Threading.Tasks;
 using NServiceBus;
 
-class Program
+
+Console.Title = "Samples.SqlServer.SimpleReceiver";
+var endpointConfiguration = new EndpointConfiguration("Samples.SqlServer.SimpleReceiver");
+
+// for SqlExpress use Data Source=.\SqlExpress;Initial Catalog=SqlServerSimple;Integrated Security=True;Max Pool Size=100;Encrypt=false
+var connectionString = @"Server=localhost,1433;Initial Catalog=SqlServerSimple;User Id=SA;Password=yourStrong(!)Password;Max Pool Size=100;Encrypt=false";
+
+endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+endpointConfiguration.UseTransport(new SqlServerTransport(connectionString)
 {
-    static async Task Main()
-    {
-        Console.Title = "Samples.SqlServer.SimpleReceiver";
-        var endpointConfiguration = new EndpointConfiguration("Samples.SqlServer.SimpleReceiver");
+    TransportTransactionMode = TransportTransactionMode.SendsAtomicWithReceive
+});
 
-        // for SqlExpress use Data Source=.\SqlExpress;Initial Catalog=SqlServerSimple;Integrated Security=True;Max Pool Size=100;Encrypt=false
-        var connectionString = @"Server=localhost,1433;Initial Catalog=SqlServerSimple;User Id=SA;Password=yourStrong(!)Password;Max Pool Size=100;Encrypt=false";
+endpointConfiguration.EnableInstallers();
 
-        endpointConfiguration.UseSerialization<SystemJsonSerializer>();
-        endpointConfiguration.UseTransport(new SqlServerTransport(connectionString)
-        {
-            TransportTransactionMode = TransportTransactionMode.SendsAtomicWithReceive
-        });
-
-        endpointConfiguration.EnableInstallers();
-
-        await SqlHelper.EnsureDatabaseExists(connectionString);
-        var endpointInstance = await Endpoint.Start(endpointConfiguration)
-            .ConfigureAwait(false);
-        Console.WriteLine("Press any key to exit");
-        Console.WriteLine("Waiting for message from the Sender");
-        Console.ReadKey();
-        await endpointInstance.Stop()
-            .ConfigureAwait(false);
-    }
-}
+await SqlHelper.EnsureDatabaseExists(connectionString);
+var endpointInstance = await Endpoint.Start(endpointConfiguration)
+    .ConfigureAwait(false);
+Console.WriteLine("Press any key to exit");
+Console.WriteLine("Waiting for message from the Sender");
+Console.ReadKey();
+await endpointInstance.Stop();
--- samples/sqltransport/simple/SqlTransport_7/Sender/Program.cs
+++ samples/sqltransport/simple/SqlTransport_7/Sender/Program.cs
@@ -2,55 +2,51 @@ using System;
 using System.Threading.Tasks;
 using NServiceBus;
 
-class Program
+Console.Title = "Samples.SqlServer.SimpleSender";
+var endpointConfiguration = new EndpointConfiguration("Samples.SqlServer.SimpleSender");
+endpointConfiguration.EnableInstallers();
+
+#region TransportConfiguration
+// for SqlExpress use Data Source=.\SqlExpress;Initial Catalog=SqlServerSimple;Integrated Security=True;Max Pool Size=100;Encrypt=false
+var connectionString = @"Server=localhost,1433;Initial Catalog=SqlServerSimple;User Id=SA;Password=yourStrong(!)Password;Max Pool Size=100;Encrypt=false";
+var routing = endpointConfiguration.UseTransport(new SqlServerTransport(connectionString)
 {
-    static async Task Main()
+    TransportTransactionMode = TransportTransactionMode.SendsAtomicWithReceive
+});
+
+routing.RouteToEndpoint(typeof(MyCommand), "Samples.SqlServer.SimpleReceiver");
+
+#endregion
+
+endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+
+await SqlHelper.EnsureDatabaseExists(connectionString);
+
+var endpointInstance = await Endpoint.Start(endpointConfiguration);
+
+await SendMessages(endpointInstance);
+
+await endpointInstance.Stop();
+
+static async Task SendMessages(IMessageSession messageSession)
+{
+    Console.WriteLine("Press [c] to send a command, or [e] to publish an event. Press [Esc] to exit.");
+    while (true)
     {
-        Console.Title = "Samples.SqlServer.SimpleSender";
-        var endpointConfiguration = new EndpointConfiguration("Samples.SqlServer.SimpleSender");
-        endpointConfiguration.EnableInstallers();
+        var input = Console.ReadKey();
+        Console.WriteLine();
 
-        #region TransportConfiguration
-        // for SqlExpress use Data Source=.\SqlExpress;Initial Catalog=SqlServerSimple;Integrated Security=True;Max Pool Size=100;Encrypt=false
-        var connectionString = @"Server=localhost,1433;Initial Catalog=SqlServerSimple;User Id=SA;Password=yourStrong(!)Password;Max Pool Size=100;Encrypt=false";
-        var routing = endpointConfiguration.UseTransport(new SqlServerTransport(connectionString)
+        switch (input.Key)
         {
-            TransportTransactionMode = TransportTransactionMode.SendsAtomicWithReceive
-        });
-
-        routing.RouteToEndpoint(typeof(MyCommand), "Samples.SqlServer.SimpleReceiver");
-
-        #endregion
-
-        endpointConfiguration.UseSerialization<SystemJsonSerializer>();
-
-        await SqlHelper.EnsureDatabaseExists(connectionString);
-        var endpointInstance = await Endpoint.Start(endpointConfiguration)
-            .ConfigureAwait(false);
-        await SendMessages(endpointInstance);
-        await endpointInstance.Stop()
-            .ConfigureAwait(false);
-    }
-
-    static async Task SendMessages(IMessageSession messageSession)
-    {
-        Console.WriteLine("Press [c] to send a command, or [e] to publish an event. Press [Esc] to exit.");
-        while (true)
-        {
-            var input = Console.ReadKey();
-            Console.WriteLine();
-
-            switch (input.Key)
-            {
-                case ConsoleKey.C:
-                    await messageSession.Send(new MyCommand());
-                    break;
-                case ConsoleKey.E:
-                    await messageSession.Publish(new MyEvent());
-                    break;
-                case ConsoleKey.Escape:
-                    return;
-            }
+            case ConsoleKey.C:
+                await messageSession.Send(new MyCommand());
+                break;
+            case ConsoleKey.E:
+                await messageSession.Publish(new MyEvent());
+                break;
+            case ConsoleKey.Escape:
+                return;
         }
     }
 }
+
