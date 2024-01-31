f2514375340ef052ad83d1dc40884fe762423130
Andreas Öhlund <andreas.ohlund@particular.net>
2024-01-31 13:01:36 +0100

    Add v8 simple sample

---
+++ samples/sqltransport/simple/SqlTransport_8/Receiver/Program.cs
@@ -0,0 +1,25 @@
+using System;
+using NServiceBus;
+
+
+Console.Title = "Samples.SqlServer.SimpleReceiver";
+var endpointConfiguration = new EndpointConfiguration("Samples.SqlServer.SimpleReceiver");
+
+// for SqlExpress use Data Source=.\SqlExpress;Initial Catalog=SqlServerSimple;Integrated Security=True;Max Pool Size=100;Encrypt=false
+var connectionString = @"Server=localhost,1433;Initial Catalog=SqlServerSimple;User Id=SA;Password=yourStrong(!)Password;Max Pool Size=100;Encrypt=false";
+
+endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+endpointConfiguration.UseTransport(new SqlServerTransport(connectionString)
+{
+    TransportTransactionMode = TransportTransactionMode.SendsAtomicWithReceive
+});
+
+endpointConfiguration.EnableInstallers();
+
+await SqlHelper.EnsureDatabaseExists(connectionString);
+var endpointInstance = await Endpoint.Start(endpointConfiguration)
+    .ConfigureAwait(false);
+Console.WriteLine("Press any key to exit");
+Console.WriteLine("Waiting for message from the Sender");
+Console.ReadKey();
+await endpointInstance.Stop();
---
+++ samples/sqltransport/simple/SqlTransport_8/Shared/MyCommand.cs
@@ -0,0 +1,5 @@
+﻿using NServiceBus;
+
+public class MyCommand : IMessage
+{
+}
