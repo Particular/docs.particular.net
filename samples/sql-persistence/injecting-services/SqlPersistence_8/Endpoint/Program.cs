f64c125fb1d0e461538ded932c46b32f8fcad331
kentdr <dan.kent@particular.net>
2024-01-31 11:59:04 -0500

    Create v8 sample

---
+++ samples/sql-persistence/injecting-services/SqlPersistence_8/Endpoint/Program.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.DependencyInjection;
+using NServiceBus;
+using System;
+using System.Threading.Tasks;
+
+
+Console.Title = "Samples.SqlPersistence.InjectingServices";
+
+var endpointConfiguration = new EndpointConfiguration("Samples.SqlPersistence.InjectingServices");
+endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+
+endpointConfiguration.EnableInstallers();
+
+// for SqlExpress use Data Source=.\SqlExpress;Initial Catalog=NsbSamplesInjectedServices;Integrated Security=True;Encrypt=false
+var connectionString = @"Server=localhost,1433;Initial Catalog=NsbSamplesInjectedServices;User Id=SA;Password=yourStrong(!)Password;Encrypt=false;Max Pool Size=100";
+
+endpointConfiguration.UseTransport(new SqlServerTransport(connectionString)
+{
+    TransportTransactionMode = TransportTransactionMode.SendsAtomicWithReceive
+});
+
+var persistence = endpointConfiguration.UsePersistence<SqlPersistence>();
+persistence.SqlDialect<SqlDialect.MsSqlServer>();
+persistence.ConnectionBuilder(connectionBuilder: () => new SqlConnection(connectionString));
+
+var subscriptions = persistence.SubscriptionSettings();
+subscriptions.CacheFor(TimeSpan.FromMinutes(1));
+
+#region BehaviorConfig
+
+endpointConfiguration.Pipeline.Register(typeof(SqlConnectionBehavior),
+    "Forwards the NServiceBus SqlConnection/SqlTransaction to data services injected into message handlers.");
+
+#endregion
+
+#region DependencyInjectionConfig
+
+endpointConfiguration.RegisterComponents(services =>
+{
+    services.AddScoped<ConnectionHolder>();
+    services.AddScoped<IDataService, DataService>();
+});
+
+#endregion
+
+SqlHelper.EnsureDatabaseExists(connectionString);
+
+var endpointInstance = await Endpoint.Start(endpointConfiguration)
+    .ConfigureAwait(false);
+
+Console.WriteLine("Press [S] to send a message, or [Enter] to exit");
+
+await RunLoop(endpointInstance);
+
+await endpointInstance.Stop()
+    .ConfigureAwait(false);
+
+static async Task RunLoop(IMessageSession messageSession)
+{
+    while (true)
+    {
+        var key = Console.ReadKey(true);
+        switch (key.Key)
+        {
+            case ConsoleKey.Enter:
+                return;
+
+            case ConsoleKey.S:
+                await messageSession.SendLocal(new TestMsg { Id = Guid.NewGuid() });
+                break;
+        }
+    }
+}
