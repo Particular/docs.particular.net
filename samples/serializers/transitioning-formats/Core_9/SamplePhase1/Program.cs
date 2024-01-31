e0a6bb5efd17435d973dd4c13e92c4eb0eb198b4
kentdr <dan.kent@particular.net>
2024-01-31 10:29:01 -0500

    Create v9 of transitioning formats sample

---
+++ samples/serializers/transitioning-formats/Core_9/SamplePhase1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Newtonsoft.Json;
+using NServiceBus;
+
+Console.Title = "Samples.Serialization.TransitionPhase1";
+
+var endpointConfiguration = new EndpointConfiguration("Samples.Serialization.TransitionPhase1");
+endpointConfiguration.SharedConfig();
+
+#region Phase1
+
+var settingsV1 = new JsonSerializerSettings
+{
+    Formatting = Formatting.Indented
+};
+
+var serializationV1 = endpointConfiguration.UseSerialization<NewtonsoftJsonSerializer>();
+serializationV1.Settings(settingsV1);
+serializationV1.ContentTypeKey("jsonv1");
+
+#endregion
+
+var endpointInstance = await Endpoint.Start(endpointConfiguration)
+    .ConfigureAwait(false);
+
+var message = MessageCreator.NewOrder();
+
+await endpointInstance.SendLocal(message)
+    .ConfigureAwait(false);
+
+await endpointInstance.Send("Samples.Serialization.TransitionPhase2", message)
+    .ConfigureAwait(false);
+
+Console.WriteLine("Order Sent");
+Console.WriteLine("Press any key to exit");
+Console.ReadKey();
+
+await endpointInstance.Stop()
+    .ConfigureAwait(false);
