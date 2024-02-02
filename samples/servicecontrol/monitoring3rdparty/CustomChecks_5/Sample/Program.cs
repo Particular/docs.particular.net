81a7317ce1f5b6a9b3626d4de03abe09cc3a3ac4
Brandon Ording <bording@gmail.com>
2024-02-02 16:54:48 -0500

    Add CustomChecks 5 monitoring3rdparty sample

---
+++ samples/servicecontrol/monitoring3rdparty/CustomChecks_5/Sample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.CustomChecks.Monitor3rdParty";
+        var endpointConfiguration = new EndpointConfiguration("Samples.CustomChecks.Monitor3rdParty");
+        endpointConfiguration.AuditProcessedMessagesTo("audit");
+        endpointConfiguration.SendFailedMessagesTo("error");
+        endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+        endpointConfiguration.UseTransport<LearningTransport>();
+
+        endpointConfiguration.ReportCustomChecksTo("Particular.ServiceControl");
+
+        await Endpoint.Start(endpointConfiguration).ConfigureAwait(false);
+
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+    }
+}
