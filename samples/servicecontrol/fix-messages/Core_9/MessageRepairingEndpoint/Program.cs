aa6275c5cff16b1d21bf66f21f9a718388f74944
Brandon Ording <bording@gmail.com>
2024-02-02 16:35:16 -0500

    Add NServiceBus 9 fix messages sample

---
+++ samples/servicecontrol/fix-messages/Core_9/MessageRepairingEndpoint/Program.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "MessageRepairingEndpoint";
+        var endpointConfiguration = new EndpointConfiguration("FixMalformedMessages.MessageRepairingEndpoint");
+        endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+        endpointConfiguration.UseTransport<LearningTransport>();
+        endpointConfiguration.EnableInstallers();
+        endpointConfiguration.SendFailedMessagesTo("error");
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+
+        Console.WriteLine("Press 'Enter' to finish.");
+        Console.ReadLine();
+
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
