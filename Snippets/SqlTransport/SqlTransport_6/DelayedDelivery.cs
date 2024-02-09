e76111b19056100d425c19d1dcbe00438ef65595
David Boike <david.boike@gmail.com>
2024-02-09 15:33:47 -0600

    SQL Transport snippets (#6447)
    * Adjust snippets for moving delayed messages in SQL Transport
    
    * Remove minor version snippets, only 6.3.x is supported now
    
    * Resolving snippets & partials for merged minors

--- Snippets/SqlTransport/SqlTransport_6/DelayedDelivery.cs
+++ Snippets/SqlTransport/SqlTransport_6/DelayedDelivery.cs
@@ -4,19 +4,13 @@ using NServiceBus;
 class DelayedDelivery
 {
     void Configure(EndpointConfiguration endpointConfiguration)
-    {        
+    {
         var transport = endpointConfiguration.UseTransport<SqlServerTransport>();
         var delayedDeliverySettings = transport.NativeDelayedDelivery();
 
         #region DelayedDeliveryTableSuffix
 
         delayedDeliverySettings.TableSuffix("Delayed");
-
-        #endregion
-
-        #region DelayedDeliveryProcessingInterval
-
-        delayedDeliverySettings.ProcessingInterval(TimeSpan.FromSeconds(5));
 
         #endregion
 
--- Snippets/SqlTransport/SqlTransport_6/RuntimeScripts/CreateQueueText.sql
+++ Snippets/SqlTransport/SqlTransport_6/RuntimeScripts/CreateQueueText.sql
@@ -19,33 +19,39 @@ BEGIN
     RETURN
 END
 
-CREATE TABLE {0} (
-    Id uniqueidentifier NOT NULL,
-    CorrelationId varchar(255),
-    ReplyToAddress varchar(255),
-    Recoverable bit NOT NULL,
-    Expires datetime,
-    Headers nvarchar(max) NOT NULL,
-    Body varbinary(max),
-    RowVersion bigint IDENTITY(1,1) NOT NULL
-);
+BEGIN TRY
+    CREATE TABLE {0} (
+        Id uniqueidentifier NOT NULL,
+        CorrelationId varchar(255),
+        ReplyToAddress varchar(255),
+        Recoverable bit NOT NULL,
+        Expires datetime,
+        Headers nvarchar(max) NOT NULL,
+        Body varbinary(max),
+        RowVersion bigint IDENTITY(1,1) NOT NULL
+    );
 
-CREATE NONCLUSTERED INDEX Index_RowVersion ON {0}
-(
-	[RowVersion] ASC
-)
+    CREATE NONCLUSTERED INDEX Index_RowVersion ON {0}
+    (
+	    [RowVersion] ASC
+    )
 
-CREATE NONCLUSTERED INDEX Index_Expires ON {0}
-(
-    Expires
-)
-INCLUDE
-(
-    Id,
-    RowVersion
-)
-WHERE
-    Expires IS NOT NULL
+    CREATE NONCLUSTERED INDEX Index_Expires ON {0}
+    (
+        Expires
+    )
+    INCLUDE
+    (
+        Id,
+        RowVersion
+    )
+    WHERE
+        Expires IS NOT NULL
+END TRY
+BEGIN CATCH
+    EXEC sp_releaseapplock @Resource = '{0}_lock';
+    THROW;
+END CATCH;
 
 EXEC sp_releaseapplock @Resource = '{0}_lock'
 endcode
