1f66ee935d32dcb0c689546d23e765697db2bf37
kentdr <dan.kent@particular.net>
2024-02-12 17:21:39 -0500

    Add v6 of databus cleanup sample

---
+++ samples/databus/blob-storage-databus-cleanup-function/ABSDataBus_6/SenderAndReceiver/MessageWithLargePayload.cs
@@ -0,0 +1,9 @@
+﻿using NServiceBus;
+
+//[TimeToBeReceived("00:00:10")]
+public class MessageWithLargePayload :
+    ICommand
+{
+    public DataBusProperty<byte[]> LargePayload { get; set; }
+    public string Description { get; set; }
+}
