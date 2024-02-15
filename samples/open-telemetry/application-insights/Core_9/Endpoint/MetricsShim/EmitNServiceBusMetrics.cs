9e88e531ff6717539d0e03c7edd41e8f5cea857b
Andreas Öhlund <andreas.ohlund@particular.net>
2024-02-15 09:44:10 +0100

    Add critical time shim

---
+++ samples/open-telemetry/application-insights/Core_9/Endpoint/MetricsShim/EmitNServiceBusMetrics.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics.Metrics;
+
+class EmitNServiceBusMetrics
+{
+    static readonly Meter NServiceBusMeter = new Meter(
+           "NServiceBus.Core",
+           "0.1.0");
+
+    static readonly Histogram<decimal> CriticalTime =
+        NServiceBusMeter.CreateHistogram<decimal>("nservicebus.messaging.criticaltime", description: "The critical time for messages processed by the endpoint.");
+}
