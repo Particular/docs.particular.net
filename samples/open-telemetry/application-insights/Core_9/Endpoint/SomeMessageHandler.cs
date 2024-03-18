a1a43c2da84968562cab6e8c984b5c929968fe01
Andreas Öhlund <andreas.ohlund@particular.net>
2024-03-18 12:58:58 +0100

    Update v9 sample

--- samples/open-telemetry/application-insights/Core_9/Endpoint/SomeMessageHandler.cs
+++ samples/open-telemetry/application-insights/Core_9/Endpoint/SomeMessageHandler.cs
@@ -1,22 +1,23 @@
 ﻿using System;
 using System.Threading.Tasks;
 using NServiceBus;
-using NServiceBus.Logging;
 
 class SomeMessageHandler : IHandleMessages<SomeMessage>
 {
-    static ILog log = LogManager.GetLogger<SomeMessageHandler>();
-    static Random random = new Random();
-
     public async Task Handle(SomeMessage message, IMessageHandlerContext context)
     {
-        await Task.Delay(random.Next(50, 250), context.CancellationToken);
+        await Task.Delay(Random.Shared.Next(50, 250), context.CancellationToken);
 
-        if (random.Next(10) <= 1)
+        if (context.MessageHeaders.ContainsKey("simulate-failure"))
         {
-            throw new Exception("Random 10% chaos!");
+            throw new Exception("Simulated failure");
         }
 
-        log.Info("Message handled");
+        if (context.MessageHeaders.ContainsKey("simulate-immediate-retry") && !context.MessageHeaders.ContainsKey(Headers.ImmediateRetries))
+        {
+            throw new Exception("Simulated immediate retry");
+        }
+
+        Console.WriteLine("Message processed");
     }
 }
