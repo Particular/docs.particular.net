752043196c0207bc10f2caab4e9d0613ff516373
Phil Bastian <155411597+PhilBastian@users.noreply.github.com>
2024-06-29 15:29:26 +0800

    entra connection samples for postgresql (#6729)
    * entra connection sample for postgresql persistence
    
    * Apply suggestions from code review
    
    * entra connection sample for postgresql transport
    
    * fix snippets build
    
    * Apply suggestions from code review
    
    Co-authored-by: Phil Bastian <155411597+PhilBastian@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Daniel Marbach <daniel.marbach@openplace.net>

--- Snippets/PostgreSqlTransport/PostgreSqlTransport_8/ConnectionStrings.cs
+++ Snippets/PostgreSqlTransport/PostgreSqlTransport_8/ConnectionStrings.cs
@@ -1,3 +1,6 @@
+using System;
+using Azure.Core;
+using Azure.Identity;
 using Npgsql;
 using NServiceBus;
 
@@ -12,6 +15,40 @@ class ConnectionStrings
         #endregion
     }
 
+    void ConnectionEntra(EndpointConfiguration endpointConfiguration)
+    {
+        #region postgresql-config-entra
+
+        var connection = "Server=test.postgres.database.azure.com;Database=postgres;Port=5432;User Id=<entra user id>;Ssl Mode=Require;";
+        var dataSourceBuilder = new NpgsqlDataSourceBuilder(connection);
+        if (string.IsNullOrEmpty(dataSourceBuilder.ConnectionStringBuilder.Password))
+        {
+            dataSourceBuilder.UsePeriodicPasswordProvider(async (_, ct) =>
+            {
+                var credentials = new DefaultAzureCredential();
+                var token = await credentials.GetTokenAsync(new TokenRequestContext(["https://ossrdbms-aad.database.windows.net/.default"]), ct);
+                return token.Token;
+            }, TimeSpan.FromHours(24), TimeSpan.FromSeconds(10));
+        }
+        var builder = dataSourceBuilder.Build();
+        var transport = new PostgreSqlTransport(async cancellationToken =>
+        {
+            var connection = builder.CreateConnection();
+            try 
+            {
+                await connection.OpenAsync(cancellationToken);
+                return connection;
+            }
+            catch
+            {
+                connection.Dispose();
+                throw;
+            }
+        });
+
+        #endregion
+    }
+
     void ConnectionFactory(EndpointConfiguration endpointConfiguration)
     {
         #region postgresql-custom-connection-factory
@@ -22,7 +59,7 @@ class ConnectionStrings
                 var connection = new NpgsqlConnection("SomeConnectionString");
                 try
                 {
-                    await connection.OpenAsync();
+                    await connection.OpenAsync(cancellationToken);
 
                     // perform custom operations
 
