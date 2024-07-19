61b54dc4142b9da8b8d070b661633c07f6f37b96
David Boike <david.boike@gmail.com>
2024-07-19 10:59:56 -0500

    Fix snippets

--- Snippets/MsmqTransport/MsmqTransport_2/PlatformRemoteQueues.cs
+++ Snippets/MsmqTransport/MsmqTransport_2/PlatformRemoteQueues.cs
@@ -1,16 +1,59 @@
-﻿
+﻿using System;
+using System.Threading.Tasks;
 using NServiceBus;
 
 class PlatformRemoteQueues
 {
-    PlatformRemoteQueues(EndpointConfiguration endpointConfiguration)
+    async Task Snippets(EndpointConfiguration endpointConfiguration, IEndpointInstance endpoint)
     {
-/*         #region ConfigMsmqErrorWithCode
+        #region ConfigureErrorRemoteQueue
 
-        endpointConfiguration.SendFailedMessagesTo("targetErrorQueue@errormachinename");
+        endpointConfiguration.SendFailedMessagesTo("Error_Queue@machinename");
 
-        endpointConfiguration.AuditProcessedMessagesTo("targetAuditQueue@auditmachinename");
+        #endregion
 
-        #endregion */
+        #region ConfigureAuditRemoteQueue
+
+        endpointConfiguration.AuditProcessedMessagesTo("Audit_Queue@machinename");
+
+        #endregion
+
+        #region ConfigureMetricsRemoteQueue
+
+        var metrics = endpointConfiguration.EnableMetrics();
+        metrics.SendMetricDataToServiceControl(
+            serviceControlMetricsAddress: "ServiceControl_Metrics_Queue@machinename",
+            interval: TimeSpan.FromMinutes(1),
+            instanceId: "INSTANCE_ID_OPTIONAL");
+
+        #endregion
+
+        #region ConfigureHeartbeatsRemoteQueue
+
+        endpointConfiguration.SendHeartbeatTo(
+            serviceControlQueue: "ServiceControl_Queue@machinename",
+            frequency: TimeSpan.FromSeconds(15),
+            timeToLive: TimeSpan.FromSeconds(30));
+
+        #endregion
+
+        #region ConfigureCustomChecksRemoteQueue
+
+        endpointConfiguration.ReportCustomChecksTo(
+            serviceControlQueue: "ServiceControl_Queue@machinename",
+            timeToLive: TimeSpan.FromSeconds(10));
+
+        #endregion
+
+        #region OverridingPhysicalRouting
+
+        var options = new SendOptions();
+        options.SetDestination("MyDestination@machinename");
+        options.RouteReplyTo("MyDestination@machinename");
+        await endpoint.Send(new MyMessage(), options);
+
+        #endregion
     }
+
+    class MyMessage : ICommand { }
 }
