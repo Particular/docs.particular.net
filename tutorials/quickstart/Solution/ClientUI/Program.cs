43d9c2941d838d0347bf46b4a33c45d92b840121
Phil Bastian <phil.bastian@particular.net>
2024-07-24 15:23:46 +0800

    convert quickstart tutorials to Host.CreateApplicationBuilder

--- tutorials/quickstart/Solution/ClientUI/Program.cs
+++ tutorials/quickstart/Solution/ClientUI/Program.cs
@@ -1,6 +1,8 @@
 using System;
 using Messages;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NServiceBus;
 
@@ -11,35 +13,35 @@ namespace ClientUI
         public static void Main(string[] args)
         {
             Console.Title = "ClientUI";
-            CreateHostBuilder(args).Build().Run();
-        }
 
-        public static IHostBuilder CreateHostBuilder(string[] args)
-        {
-            return Host.CreateDefaultBuilder(args)
-                        .UseNServiceBus(context =>
-                        {
-                            var endpointConfiguration = new EndpointConfiguration("ClientUI");
-                            endpointConfiguration.UseSerialization<SystemJsonSerializer>();
-                            var transport = endpointConfiguration.UseTransport<LearningTransport>();
+            var builder = WebApplication.CreateBuilder(args);
 
-                            var routing = transport.Routing();
-                            routing.RouteToEndpoint(typeof(PlaceOrder), "Sales");
+            var endpointConfiguration = new EndpointConfiguration("ClientUI");
+            endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+            var transport = endpointConfiguration.UseTransport<LearningTransport>();
 
-                            endpointConfiguration.SendFailedMessagesTo("error");
-                            endpointConfiguration.AuditProcessedMessagesTo("audit");
-                            endpointConfiguration.SendHeartbeatTo("Particular.ServiceControl");
+            var routing = transport.Routing();
+            routing.RouteToEndpoint(typeof(PlaceOrder), "Sales");
 
-                            var metrics = endpointConfiguration.EnableMetrics();
-                            metrics.SendMetricDataToServiceControl("Particular.Monitoring", TimeSpan.FromMilliseconds(500));
+            endpointConfiguration.SendFailedMessagesTo("error");
+            endpointConfiguration.AuditProcessedMessagesTo("audit");
+            endpointConfiguration.SendHeartbeatTo("Particular.ServiceControl");
 
-                            return endpointConfiguration;
+            var metrics = endpointConfiguration.EnableMetrics();
+            metrics.SendMetricDataToServiceControl("Particular.Monitoring", TimeSpan.FromMilliseconds(500));
 
-                        })
-                       .ConfigureWebHostDefaults(webBuilder =>
-                        {
-                            webBuilder.UseStartup<Startup>();
-                        });
+            builder.UseNServiceBus(endpointConfiguration);
+
+            builder.Services.AddControllers();
+            builder.Services.AddMvc();
+
+            var app = builder.Build();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.MapControllers();
+
+            app.Run();
         }
     }
 }
--- tutorials/quickstart/Solution/Sales/Program.cs
+++ tutorials/quickstart/Solution/Sales/Program.cs
@@ -10,38 +10,34 @@ namespace Sales
         static async Task Main(string[] args)
         {
             Console.Title = "Sales";
-            await CreateHostBuilder(args).RunConsoleAsync();
-        }
 
-        public static IHostBuilder CreateHostBuilder(string[] args)
-        {
-            return Host.CreateDefaultBuilder(args)
-                       .UseNServiceBus(context =>
-                       {
-                           var endpointConfiguration = new EndpointConfiguration("Sales");
+            var builder = Host.CreateApplicationBuilder(args);
 
-                           endpointConfiguration.UseSerialization<SystemJsonSerializer>();
-                           endpointConfiguration.UseTransport<LearningTransport>();
+            var endpointConfiguration = new EndpointConfiguration("Sales");
 
-                           endpointConfiguration.SendFailedMessagesTo("error");
-                           endpointConfiguration.AuditProcessedMessagesTo("audit");
-                           endpointConfiguration.SendHeartbeatTo("Particular.ServiceControl");
+            endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+            endpointConfiguration.UseTransport<LearningTransport>();
 
-                           // So that when we test recoverability, we don't have to wait so long
-                           // for the failed message to be sent to the error queue
-                           var recoverability = endpointConfiguration.Recoverability();
-                           recoverability.Delayed(
-                               delayed =>
-                               {
-                                   delayed.TimeIncrease(TimeSpan.FromSeconds(2));
-                               }
-                           );
+            endpointConfiguration.SendFailedMessagesTo("error");
+            endpointConfiguration.AuditProcessedMessagesTo("audit");
+            endpointConfiguration.SendHeartbeatTo("Particular.ServiceControl");
 
-                           var metrics = endpointConfiguration.EnableMetrics();
-                           metrics.SendMetricDataToServiceControl("Particular.Monitoring", TimeSpan.FromMilliseconds(500));
+            // So that when we test recoverability, we don't have to wait so long
+            // for the failed message to be sent to the error queue
+            var recoverability = endpointConfiguration.Recoverability();
+            recoverability.Delayed(
+                delayed =>
+                {
+                    delayed.TimeIncrease(TimeSpan.FromSeconds(2));
+                }
+            );
 
-                           return endpointConfiguration;
-                       });
+            var metrics = endpointConfiguration.EnableMetrics();
+            metrics.SendMetricDataToServiceControl("Particular.Monitoring", TimeSpan.FromMilliseconds(500));
+
+            builder.UseNServiceBus(endpointConfiguration);
+
+            await builder.Build().RunAsync();
         }
     }
 }
