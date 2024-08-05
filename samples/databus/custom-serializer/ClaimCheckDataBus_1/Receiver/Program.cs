bef80d3eee52e64a7b14df4e4c229e7cfb3c2043
WilliamBZA <william.brander@gmail.com>
2024-08-05 20:21:59 +0200

    Add claimcheck samples

---
+++ samples/databus/custom-serializer/ClaimCheckDataBus_1/Receiver/Program.cs
@@ -0,0 +1,27 @@
+using NServiceBus;
+using Shared;
+using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Receiver";
+        var endpointConfiguration = new EndpointConfiguration("Samples.DataBus.Receiver");
+
+        #region ConfigureReceiverCustomDataBusSerializer
+
+        var dataBus = endpointConfiguration.UseDataBus<FileShareDataBus, BsonDataBusSerializer>();
+        dataBus.BasePath(@"..\..\..\..\storage");
+
+        #endregion
+
+        endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+        endpointConfiguration.UseTransport(new LearningTransport());
+        var endpointInstance = await Endpoint.Start(endpointConfiguration);
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop();
+    }
+}
