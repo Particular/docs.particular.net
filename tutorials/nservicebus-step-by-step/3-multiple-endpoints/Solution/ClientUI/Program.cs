882e239931acbfb9f6125372f172198a58390b0d
Poornima Nayar <poornima.nayar@particular.net>
2024-10-10 10:45:18 +0100

    Updates to version on step 3

--- tutorials/nservicebus-step-by-step/3-multiple-endpoints/Solution/ClientUI/Program.cs
+++ tutorials/nservicebus-step-by-step/3-multiple-endpoints/Solution/ClientUI/Program.cs
@@ -22,7 +22,7 @@ class Program
 
         builder.UseNServiceBus(endpointConfiguration);
 
-        builder.Services.AddHostedService<Worker>();
+        builder.Services.AddHostedService<InputLoopService>();
 
         await builder.Build().RunAsync();
     }
