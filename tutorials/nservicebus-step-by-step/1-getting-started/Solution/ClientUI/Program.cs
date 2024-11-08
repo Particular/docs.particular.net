3ba69a2d225a7c8b791d68eb9b30d64cf3bc98b4
Andreas Öhlund <andreas.ohlund@particular.net>
2024-11-08 19:46:22 +0100

    Cleanup lesson 1 solution

--- tutorials/nservicebus-step-by-step/1-getting-started/Solution/ClientUI/Program.cs
+++ tutorials/nservicebus-step-by-step/1-getting-started/Solution/ClientUI/Program.cs
@@ -1,26 +1,17 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.Hosting;
 using NServiceBus;
 using System;
 
-namespace ClientUI;
-class Program
-{
-    static async Task Main(string[] args)
-    {
-        Console.Title = "ClientUI";
+Console.Title = "ClientUI";
 
-        var builder = Host.CreateApplicationBuilder(args);
+var builder = Host.CreateApplicationBuilder(args);
 
-        var endpointConfiguration = new EndpointConfiguration("ClientUI");
+var endpointConfiguration = new EndpointConfiguration("ClientUI");
 
-        endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+endpointConfiguration.UseSerialization<SystemJsonSerializer>();
 
-        var transport = endpointConfiguration.UseTransport(new LearningTransport());
+var transport = endpointConfiguration.UseTransport(new LearningTransport());
 
-        builder.UseNServiceBus(endpointConfiguration);
+builder.UseNServiceBus(endpointConfiguration);
 
-        await builder.Build().RunAsync();
-    }
-}
+await builder.Build().RunAsync();
