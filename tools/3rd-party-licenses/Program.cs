3d4960cb65a8fdc6d8cb23295c2b35491372cdf3
David Boike <david.boike@gmail.com>
2024-12-18 08:50:43 -0600

    Adjust current working directory when running from an IDE

--- tools/3rd-party-licenses/Program.cs
+++ tools/3rd-party-licenses/Program.cs
@@ -6,6 +6,12 @@ const string servicePulseSln = "../../checkout/ServicePulse/src/ServicePulse.sln
 const string servicePulseNpm = "../../checkout/ServicePulse/src/Frontend";
 const string serviceControlSln = "../../checkout/ServiceControl/src/ServiceControl.sln";
 
+// Support running from IDE when current directory relative to repo is /tools/3rd-party-licenses/bin/Debug/net...
+while (Regex.IsMatch(Environment.CurrentDirectory, @"[/\\]bin([/\\]|$)"))
+{
+    Environment.CurrentDirectory = Path.GetDirectoryName(Environment.CurrentDirectory);
+}
+
 await using var output = new StreamWriter(includePath, append: false);
 
 var nuGetPackages = new NuGetPackages(componentsPath, [Tuple.Create("ServicePulse", servicePulseSln), Tuple.Create("ServiceControl", serviceControlSln)]);
