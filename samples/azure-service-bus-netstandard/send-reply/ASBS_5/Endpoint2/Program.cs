dd9a8383522a0a74809c08a40892a4f4f5d6c7b5
Daniel Marbach <danielmarbach@users.noreply.github.com>
2025-02-14 10:15:12 +0100

    Update send-reply sample

--- samples/azure-service-bus-netstandard/send-reply/ASBS_5/Endpoint2/Program.cs
+++ samples/azure-service-bus-netstandard/send-reply/ASBS_5/Endpoint2/Program.cs
@@ -14,7 +14,7 @@ if (string.IsNullOrWhiteSpace(connectionString))
     throw new Exception("Could not read the 'AzureServiceBus_ConnectionString' environment variable. Check the sample prerequisites.");
 }
 
-var transport = new AzureServiceBusTransport(connectionString);
+var transport = new AzureServiceBusTransport(connectionString, TopicTopology.Default);
 endpointConfiguration.UseTransport(transport);
 endpointConfiguration.UseSerialization<SystemJsonSerializer>();
 
