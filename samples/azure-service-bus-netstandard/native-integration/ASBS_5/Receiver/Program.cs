9f2db512107c8c5653dc520e36a09f0f2a60d9b2
Daniel Marbach <danielmarbach@users.noreply.github.com>
2025-02-14 09:17:56 +0100

    Update native integration sample

--- samples/azure-service-bus-netstandard/native-integration/ASBS_5/Receiver/Program.cs
+++ samples/azure-service-bus-netstandard/native-integration/ASBS_5/Receiver/Program.cs
@@ -14,14 +14,13 @@ endpointConfiguration.EnableInstallers();
 endpointConfiguration.UseSerialization<SystemJsonSerializer>();
 endpointConfiguration.Conventions().DefiningMessagesAs(type => type.Name == "NativeMessage");
 
-
 var connectionString = Environment.GetEnvironmentVariable("AzureServiceBus_ConnectionString");
 if (string.IsNullOrWhiteSpace(connectionString))
 {
     throw new Exception("Could not read the 'AzureServiceBus_ConnectionString' environment variable. Check the sample prerequisites.");
 }
 
-var transport = new AzureServiceBusTransport(connectionString);
+var transport = new AzureServiceBusTransport(connectionString, TopicTopology.Default);
 endpointConfiguration.UseTransport(transport);
 
 var endpointInstance = await Endpoint.Start(endpointConfiguration);
