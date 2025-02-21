14cf48b24b0991a131324856ee7110adeaabe60a
Brandon Ording <bording@gmail.com>
2025-02-21 14:04:29 -0500

    Update Snippets and Samples for NServiceBus.AzureFunctions.InProcess.ServiceBus 5.0.0 (#7011)

---
+++ Snippets/ASBFunctions/ASBFunctions_5/TriggerDefinition.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.ServiceBus;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+using ExecutionContext = Microsoft.Azure.WebJobs.ExecutionContext;
+
+#region custom-trigger-definition
+class CustomTriggerDefinition
+{
+    IFunctionEndpoint functionEndpoint;
+
+    public CustomTriggerDefinition(IFunctionEndpoint functionEndpoint)
+    {
+        this.functionEndpoint = functionEndpoint;
+    }
+
+    [FunctionName("MyCustomTrigger")]
+    public Task Run(
+        [ServiceBusTrigger("MyFunctionsEndpoint")]
+        ServiceBusReceivedMessage message,
+        ServiceBusClient client,
+        ServiceBusMessageActions messageActions,
+        ILogger logger,
+        ExecutionContext executionContext)
+    {
+        return functionEndpoint.ProcessAtomic(message, executionContext, client, messageActions, logger);
+    }
+}
+#endregion
