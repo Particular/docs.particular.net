601cf31767a41e0219408e4bae54c486ef99ff63
Irina Scurtu <scurtuirina@gmail.com>
2025-02-25 22:44:58 +0200

    added more examples

--- samples/pubsub/native/Core_9/Subscriber/OrderReceivedHandler.cs
+++ samples/pubsub/native/Core_9/Subscriber/OrderReceivedHandler.cs
@@ -1,15 +1,18 @@
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using NServiceBus;
-using NServiceBus.Logging;
-
 public class OrderReceivedHandler :
     IHandleMessages<OrderReceived>
 {
-    static ILog log = LogManager.GetLogger<OrderReceivedHandler>();
+    private static readonly ILogger<OrderReceivedHandler> logger =
+    LoggerFactory.Create(builder =>
+    {
+        builder.AddConsole();
+    }).CreateLogger<OrderReceivedHandler>();
 
     public Task Handle(OrderReceived message, IMessageHandlerContext context)
     {
-        log.Info($"Subscriber has received OrderReceived event with OrderId {message.OrderId}.");
+        logger.LogInformation($"Subscriber has received OrderReceived event with OrderId {message.OrderId}.");
         return Task.CompletedTask;
     }
 }
