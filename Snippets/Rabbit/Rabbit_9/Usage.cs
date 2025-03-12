cc1d2997d68b6a4229d5ba7cea0daa536688b4ae
Brandon Ording <bording@gmail.com>
2025-03-12 16:04:17 -0400

    Fix snippets & samples (#7041)
    * Fix snippets
    
    * Fix samples

--- Snippets/Rabbit/Rabbit_9/Usage.cs
+++ Snippets/Rabbit/Rabbit_9/Usage.cs
@@ -136,7 +136,7 @@ class Usage
         #region rabbitmq-client-certificate
 
         var transport = endpointConfiguration.UseTransport<RabbitMQTransport>();
-        transport.SetClientCertificate(X509CertificateLoader.LoadCertificateFromFile("/path/to/certificate"));
+        transport.SetClientCertificate(new X509Certificate2("/path/to/certificate"));
 
         #endregion
     }
