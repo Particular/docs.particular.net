77ca5025e1991134e8369f6dd043b150aa499ea1
Irina Dominte <scurtuirina@gmail.com>
2025-03-13 15:36:14 +0200

    fixed some same issues

--- samples/databus/file-share-databus/Core_9/Receiver/MessageWithLargePayloadHandler.cs
+++ samples/databus/file-share-databus/Core_9/Receiver/MessageWithLargePayloadHandler.cs
@@ -1,17 +1,17 @@
 using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
 using NServiceBus;
+using NServiceBus.Logging;
 
 #region MessageWithLargePayloadHandler
 
-public class MessageWithLargePayloadHandler(ILogger<MessageWithLargePayloadHandler> logger) :
+public class MessageWithLargePayloadHandler :
     IHandleMessages<MessageWithLargePayload>
 {
+    static ILog log = LogManager.GetLogger<MessageWithLargePayloadHandler>();
 
     public Task Handle(MessageWithLargePayload message, IMessageHandlerContext context)
     {
-        //ToDo check this
-        logger.LogInformation($"Message received, size of blob property: {message.LargeBlob} Bytes");
+        log.Info($"Message received, size of blob property: {message.LargeBlob.Value.Length} Bytes");
         return Task.CompletedTask;
     }
 }
--- samples/databus/file-share-databus/Core_9/Receiver/Program.cs
+++ samples/databus/file-share-databus/Core_9/Receiver/Program.cs
@@ -1,21 +1,20 @@
-using Microsoft.Extensions.Hosting;
 using NServiceBus;
 using System;
 using System.Threading.Tasks;
 
-
-Console.Title = "Receiver";
-
-var builder = Host.CreateApplicationBuilder(args);
-var endpointConfiguration = new EndpointConfiguration("Samples.DataBus.Receiver");
-var claimCheck = endpointConfiguration.UseClaimCheck<FileShareClaimCheck, SystemJsonClaimCheckSerializer>();
-claimCheck.BasePath(@"..\..\..\..\storage");
-endpointConfiguration.UseSerialization<SystemJsonSerializer>();
-endpointConfiguration.UseTransport(new LearningTransport());
-Console.WriteLine("Press any key, the application is starting");
-Console.ReadKey();
-Console.WriteLine("Starting...");
-
-builder.UseNServiceBus(endpointConfiguration);
-
-await builder.Build().RunAsync();
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Receiver";
+        var endpointConfiguration = new EndpointConfiguration("Samples.DataBus.Receiver");
+        var dataBus = endpointConfiguration.UseDataBus<FileShareDataBus, SystemJsonDataBusSerializer>();
+        dataBus.BasePath(@"..\..\..\..\storage");
+        endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+        endpointConfiguration.UseTransport(new LearningTransport());
+        var endpointInstance = await Endpoint.Start(endpointConfiguration);
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop();
+    }
+}
