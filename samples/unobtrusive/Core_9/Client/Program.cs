b493bb36f98f0aa66892fb4761002a53e7bf0b0c
Irina Dominte <scurtuirina@gmail.com>
2025-03-13 15:48:39 +0200

    fixed obsolete

--- samples/unobtrusive/Core_9/Client/Program.cs
+++ samples/unobtrusive/Core_9/Client/Program.cs
@@ -1,8 +1,10 @@
+using Client;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 Console.Title = "Client";
 var builder = Host.CreateApplicationBuilder(args);
-
+builder.Services.AddHostedService<InputLoopService>();
 var endpointConfiguration = new EndpointConfiguration("Samples.Unobtrusive.Client");
 endpointConfiguration.UseSerialization<SystemJsonSerializer>();
 endpointConfiguration.UseTransport(new LearningTransport());
@@ -12,10 +14,6 @@ dataBus.BasePath(@"..\..\..\..\DataBusShare\");
 #pragma warning restore CS0618 // Type or member is obsolete
 
 endpointConfiguration.ApplyCustomConventions();
-
-var endpointInstance = await Endpoint.Start(endpointConfiguration);
-await CommandSender.Start(endpointInstance);
-await endpointInstance.Stop();
 
 builder.UseNServiceBus(endpointConfiguration);
 
--- samples/unobtrusive/Core_9/Server/Program.cs
+++ samples/unobtrusive/Core_9/Server/Program.cs
@@ -1,7 +1,10 @@
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Server;
 
 Console.Title = "Server";
 var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<InputLoopService>();
 
 var endpointConfiguration = new EndpointConfiguration("Samples.Unobtrusive.Server");
 endpointConfiguration.UseSerialization<SystemJsonSerializer>();
@@ -13,9 +16,6 @@ endpointConfiguration.UseDataBus<FileShareDataBus, SystemJsonDataBusSerializer>(
 
 endpointConfiguration.ApplyCustomConventions();
 
-var endpointInstance = await Endpoint.Start(endpointConfiguration);
-await CommandSender.Start(endpointInstance);
-await endpointInstance.Stop();
 builder.UseNServiceBus(endpointConfiguration);
 
 await builder.Build().RunAsync();
