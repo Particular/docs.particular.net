cf92be227622e309e1adc44cf633648cf3662108
Udi Dahan <udidahan@users.noreply.github.com>
2025-04-03 19:08:38 +0300

    Update ShippingProgram.cs

--- tutorials/quickstart/Snippets/Core_9/ShippingProgram.cs
+++ tutorials/quickstart/Snippets/Core_9/ShippingProgram.cs
@@ -26,19 +26,20 @@ namespace Shipping
                            // Choose JSON to serialize and deserialize messages
                            endpointConfig.UseSerialization<SystemJsonSerializer>();
 
-                           // Select the learning (filesystem-based) transport to communicate
-                           // with other endpoints
+                           // Select the learning (filesystem-based) transport to 
+                           // communicate with other endpoints
                            endpointConfig.UseTransport<LearningTransport>();
 
-                           // Enable monitoring errors, auditing, and heartbeats with the
-                           // Particular Service Platform tools
+                           // Enable monitoring errors, auditing, and heartbeats
+                           // with the Particular Service Platform tools
                            endpointConfig.SendFailedMessagesTo("error");
                            endpointConfig.AuditProcessedMessagesTo("audit");
                            endpointConfig.SendHeartbeatTo("Particular.ServiceControl");
 
                            // Enable monitoring endpoint performance
                            var metrics = endpointConfig.EnableMetrics();
-                           metrics.SendMetricDataToServiceControl("Particular.Monitoring",
+                           metrics.SendMetricDataToServiceControl(
+                               "Particular.Monitoring",
                                TimeSpan.FromMilliseconds(500));
 
                            // Return the completed configuration
