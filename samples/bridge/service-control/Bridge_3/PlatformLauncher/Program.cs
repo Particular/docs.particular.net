a19ef286551825540c9788b679724ed5ee9d1914
Ramon Smits <ramon@ramonsmits.com>
2025-05-19 01:04:52 +0200

    Remove class Program

--- samples/bridge/service-control/Bridge_3/PlatformLauncher/Program.cs
+++ samples/bridge/service-control/Bridge_3/PlatformLauncher/Program.cs
@@ -1,14 +1,4 @@
-﻿namespace PlatformLauncher
-{
-    using System;
-    using System.Threading.Tasks;
+﻿using System;
 
-    class Program
-    {
-        static async Task Main(string[] args)
-        {
-            Console.Title = "PlatformLauncher";
-            await Particular.PlatformLauncher.Launch();
-        }
-    }
-}
+Console.Title = "PlatformLauncher";
+await Particular.PlatformLauncher.Launch();
