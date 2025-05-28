7ea50e82f902b9eeb292024888fcc70f26260458
Jo Palac <jo.palac@particular.net>
2025-05-28 11:01:06 +1000

    add warning about missing connection string

--- samples/azure/custom-audit-transport/Core_9/CustomAuditTransport/Program.cs
+++ samples/azure/custom-audit-transport/Core_9/CustomAuditTransport/Program.cs
@@ -11,8 +11,13 @@ var endpointConfiguration = new EndpointConfiguration("Samples.CustomAuditTransp
 
 endpointConfiguration.UsePersistence<LearningPersistence>();
 endpointConfiguration.UseSerialization<SystemJsonSerializer>();
-var asbConnectionString = Environment.GetEnvironmentVariable("AzureServiceBus_ConnectionString");
-var transport = new AzureServiceBusTransport(asbConnectionString, TopicTopology.Default);
+
+var connectionString = Environment.GetEnvironmentVariable("AzureServiceBus_ConnectionString");
+if (string.IsNullOrWhiteSpace(connectionString))
+{
+    throw new Exception("Could not read the 'AzureServiceBus_ConnectionString' environment variable. Check the sample prerequisites.");
+}
+var transport = new AzureServiceBusTransport(connectionString, TopicTopology.Default);
 endpointConfiguration.UseTransport(transport);
 
 endpointConfiguration.AuditProcessedMessagesTo("audit");
