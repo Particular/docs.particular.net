62d715c5b62204a0716fc44c52f1ed60c470adf7
Andreas Öhlund <andreas.ohlund@particular.net>
2025-06-02 13:04:39 +0200

    Cleanup otel trace sample

--- samples/open-telemetry/logging/Core_9/GenericHost/MyMessageHandler.cs
+++ samples/open-telemetry/logging/Core_9/GenericHost/MyMessageHandler.cs
@@ -2,15 +2,8 @@
 using Microsoft.Extensions.Logging;
 using NServiceBus;
 
-public class MyMessageHandler : IHandleMessages<MyMessage>
+public class MyMessageHandler(ILogger<MyMessageHandler> logger) : IHandleMessages<MyMessage>
 {
-    private readonly ILogger<MyMessageHandler> logger;
-
-    public MyMessageHandler(ILogger<MyMessageHandler> logger)
-    {
-        this.logger = logger;
-    }
-
     public Task Handle(MyMessage message, IMessageHandlerContext context)
     {
         #region log-statement
