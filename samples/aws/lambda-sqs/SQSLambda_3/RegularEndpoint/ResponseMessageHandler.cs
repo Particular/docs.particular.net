2ac168a28bc81a7fe56ee2e22545576b6ca2bac8
Daniel Marbach <danielmarbach@users.noreply.github.com>
2025-06-05 15:55:51 +0200

    Lambda SQS v3: Samples, Snippets and upgrade guide

---
+++ samples/aws/lambda-sqs/SQSLambda_3/RegularEndpoint/ResponseMessageHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+
+public class ResponseMessageHandler(ILogger<ResponseMessageHandler> logger) : IHandleMessages<ResponseMessage>
+{
+
+    public Task Handle(ResponseMessage message, IMessageHandlerContext context)
+    {
+        logger.LogInformation($"Handling {nameof(ResponseMessage)} in RegularEndpoint");
+        return Task.CompletedTask;
+    }
+}
