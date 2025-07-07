5fa5cd07a9d9b0566b56958fee043aac147a21e6
Dharmishtha Patel <dharmishthapatel@Dharmishthas-MacBook-Pro.local>
2025-07-07 09:34:54 -0700

    Refactor logger calls in samples to use structured logging

--- samples/ravendb/simple/Raven_9/Server/OrderSaga.cs
+++ samples/ravendb/simple/Raven_9/Server/OrderSaga.cs
@@ -20,7 +20,7 @@ public class OrderSaga(ILogger<OrderSaga> logger) :
         var orderDescription = $"The saga for order {message.OrderId}";
         Data.OrderDescription = orderDescription;
 
-        logger.LogInformation($"Received StartOrder message {Data.OrderId}. Starting Saga");
+        logger.LogInformation("Received StartOrder message {OrderId}. Starting Saga", Data.OrderId);
 
         var shipOrder = new ShipOrder
         {
@@ -42,7 +42,7 @@ public class OrderSaga(ILogger<OrderSaga> logger) :
 
     public Task Timeout(CompleteOrder state, IMessageHandlerContext context)
     {
-        logger.LogInformation($"Saga with OrderId {Data.OrderId} completed");
+        logger.LogInformation("Saga with OrderId {OrderId} completed", Data.OrderId);
         var orderCompleted = new OrderCompleted
         {
             OrderId = Data.OrderId
