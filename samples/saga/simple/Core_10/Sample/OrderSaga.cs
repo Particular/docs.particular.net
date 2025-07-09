e9b4c22b874153d8778a5a95454eaf6f7e24c335
Ryan Thomas <ryan.thomas@particular.net>
2025-07-09 11:53:38 -0500

    Include structured logging fixes for logger calls.

--- samples/saga/simple/Core_10/Sample/OrderSaga.cs
+++ samples/saga/simple/Core_10/Sample/OrderSaga.cs
@@ -20,37 +20,46 @@ public class OrderSaga(ILogger<OrderSaga> logger) :
     public async Task Handle(StartOrder message, IMessageHandlerContext context)
     {
         // Correlation property Data.OrderId is automatically assigned with the value from message.OrderId;
-        logger.LogInformation($"StartOrder received with OrderId {message.OrderId}");
+        logger.LogInformation("StartOrder received with OrderId {MessageOrderId}", message.OrderId);
 
-        logger.LogInformation($@"Sending a CompleteOrder that will be delayed by 10 seconds
-Stop the endpoint now to see the saga data in:
-{LearningLocationHelper.GetSagaLocation<OrderSaga>(Data.Id)}");
+        logger.LogInformation(@"Sending a CompleteOrder that will be delayed by 10 seconds
+                                Stop the endpoint now to see the saga data in:
+                                {GetSagaLocation}",
+                                LearningLocationHelper.GetSagaLocation<OrderSaga>(Data.Id));
+
         var completeOrder = new CompleteOrder
         {
             OrderId = Data.OrderId
         };
+
         var sendOptions = new SendOptions();
         sendOptions.DelayDeliveryWith(TimeSpan.FromSeconds(10));
         sendOptions.RouteToThisEndpoint();
+
         await context.Send(completeOrder, sendOptions);
 
         var timeout = DateTimeOffset.UtcNow.AddSeconds(30);
-        logger.LogInformation($@"Requesting a CancelOrder that will be executed in 30 seconds.
-Stop the endpoint now to see the timeout data in the delayed directory
-{LearningLocationHelper.TransportDelayedDirectory(timeout)}");
+
+        logger.LogInformation(@"Requesting a CancelOrder that will be executed in 30 seconds.
+                                Stop the endpoint now to see the timeout data in the delayed directory
+                                {TransportDelayedDirectory}",
+                                LearningLocationHelper.TransportDelayedDirectory(timeout));
+
         await RequestTimeout<CancelOrder>(context, timeout);
     }
 
     public Task Handle(CompleteOrder message, IMessageHandlerContext context)
     {
-        logger.LogInformation($"CompleteOrder received with OrderId {message.OrderId}");
+        logger.LogInformation("CompleteOrder received with OrderId {MessageOrderId}", message.OrderId);
         MarkAsComplete();
         return Task.CompletedTask;
     }
 
     public Task Timeout(CancelOrder state, IMessageHandlerContext context)
     {
-        logger.LogInformation($"CompleteOrder not received soon enough OrderId {Data.OrderId}. Calling MarkAsComplete");
+        logger.LogInformation(@"CompleteOrder not received soon enough OrderId {DataOrderId}.
+                                Calling MarkAsComplete",
+                                Data.OrderId);
         MarkAsComplete();
         return Task.CompletedTask;
     }
