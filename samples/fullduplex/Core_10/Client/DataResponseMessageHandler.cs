e7fa056088f224990d348f5263a572bb247757e0
Dharmishtha Patel <dharmishthapatel@Dharmishthas-MacBook-Pro.local>
2025-07-15 11:14:34 -0700

    Add fullduplex Core_10 sample with .NET 10 and modern C# features

---
+++ samples/fullduplex/Core_10/Client/DataResponseMessageHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using Microsoft.Extensions.Logging;
+
+#region DataResponseMessageHandler
+class DataResponseMessageHandler(ILogger<DataResponseMessageHandler> logger) :
+    IHandleMessages<DataResponseMessage>
+#endregion
+{
+    public Task Handle(DataResponseMessage message, IMessageHandlerContext context)
+    {
+        logger.LogInformation("Response received with description: {Description}", message.String);
+        return Task.CompletedTask;
+    }
+}
