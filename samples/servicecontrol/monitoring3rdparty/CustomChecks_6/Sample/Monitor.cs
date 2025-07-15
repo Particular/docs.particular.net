d2ce0745b49a783543477e20dc964c934d6c7f15
Tomek Masternak <tomasz.masternak@particular.net>
2025-07-15 13:22:09 +0200

    new version of the sample

---
+++ samples/servicecontrol/monitoring3rdparty/CustomChecks_6/Sample/Monitor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using NServiceBus.CustomChecks;
+
+
+#region thecustomcheck
+
+class ThirdPartyMonitor : CustomCheck
+{
+    const string url = "http://localhost:57789";
+    static readonly HttpClient client = new() { Timeout = TimeSpan.FromSeconds(3) };
+    readonly ILogger<ThirdPartyMonitor> logger;
+
+    public ThirdPartyMonitor(ILogger<ThirdPartyMonitor> logger)
+        : base(
+            id: $"Monitor {url}",
+            category: "Monitor 3rd Party ",
+            repeatAfter: TimeSpan.FromSeconds(10))
+    {
+        this.logger = logger;
+    }
+
+    public override async Task<CheckResult> PerformCheck(CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            using var response = await client.GetAsync(url, cancellationToken);
+
+            if (response.IsSuccessStatusCode)
+            {
+                logger.LogInformation("Succeeded in contacting {Url}", url);
+                return CheckResult.Pass;
+            }
+
+            var error = $"Failed to contact '{url}'. HttpStatusCode: {response.StatusCode}";
+            logger.LogInformation(error);
+            return CheckResult.Failed(error);
+        }
+        catch (Exception exception)
+        {
+            var error = $"Failed to contact '{url}'. Error: {exception.Message}";
+            logger.LogInformation(error);
+            return CheckResult.Failed(error);
+        }
+    }
+}
+
+#endregion
