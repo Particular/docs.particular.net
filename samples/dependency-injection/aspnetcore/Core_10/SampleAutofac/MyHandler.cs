58c31c3903d432f056d55d271354fbc762ae15ea
David Boike <david.boike@gmail.com>
2025-07-15 11:39:45 -0500

    NSB10 version of AspNetCore DI integration sample (#7465)

---
+++ samples/dependency-injection/aspnetcore/Core_10/SampleAutofac/MyHandler.cs
@@ -0,0 +1,8 @@
+﻿public class MyHandler(MyService myService) : IHandleMessages<MyMessage>
+{
+    public Task Handle(MyMessage message, IMessageHandlerContext context)
+    {
+        myService.WriteHello();
+        return Task.CompletedTask;
+    }
+}
