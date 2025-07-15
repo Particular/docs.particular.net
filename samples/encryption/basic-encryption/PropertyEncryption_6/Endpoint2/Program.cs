801c122a5704c2d6c4dc4c4264fd4e9bc0331879
David Boike <david.boike@gmail.com>
2025-07-15 11:46:40 -0500

    NSB10 version

---
+++ samples/encryption/basic-encryption/PropertyEncryption_6/Endpoint2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Extensions.Hosting;
+using NServiceBus;
+
+Console.Title = "Endpoint2";
+var builder = Host.CreateApplicationBuilder(args);
+
+var endpointConfiguration = new EndpointConfiguration("Samples.Encryption.Endpoint2");
+endpointConfiguration.ConfigurationEncryption();
+endpointConfiguration.UsePersistence<LearningPersistence>();
+endpointConfiguration.UseTransport(new LearningTransport());
+endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+
+builder.UseNServiceBus(endpointConfiguration);
+
+await builder.Build().RunAsync();
