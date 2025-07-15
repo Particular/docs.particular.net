24d326606bdd000d1ec841e00da6bc4c8bb18b39
David Boike <david.boike@gmail.com>
2025-07-15 12:28:02 -0500

    Nsb10 basic encryption sample (#7466)
    * Fix NSB9 sample
    
    * NSB10 version

---
+++ samples/encryption/basic-encryption/PropertyEncryption_6/Endpoint2/Handler.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+
+public class Handler(ILogger<Handler> logger) :
+    IHandleMessages<MessageWithSecretData>
+{
+
+    public Task Handle(MessageWithSecretData message, IMessageHandlerContext context)
+    {
+        logger.LogInformation("I know the secret - it's '{Secret}'", message.Secret.Value);
+        logger.LogInformation("SubSecret: {SubSecret}", message.SubProperty.Secret.Value);
+        foreach (var creditCard in message.CreditCards)
+        {
+            logger.LogInformation("CreditCard: {Number} is valid to {ValidTo}", creditCard.Number.Value, creditCard.ValidTo);
+        }
+        return Task.CompletedTask;
+    }
+}
