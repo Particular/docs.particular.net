da49a3167f22e2338631fd90887ff0c2485c4382
Irina Dominte <scurtuirina@gmail.com>
2025-07-16 15:46:53 +0300

    Updated the asp pages sample

---
+++ samples/web/asp-web-application/Core_10/WebApp/Program.cs
@@ -0,0 +1,19 @@
+﻿#region ApplicationStart
+var builder = WebApplication.CreateBuilder(args);
+
+var endpointConfiguration = new EndpointConfiguration("Samples.AsyncPages.WebApplication");
+endpointConfiguration.MakeInstanceUniquelyAddressable("1");
+endpointConfiguration.EnableCallbacks();
+endpointConfiguration.UseTransport(new LearningTransport());
+endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+
+builder.UseNServiceBus(endpointConfiguration);
+#endregion
+
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+app.MapRazorPages();
+
+app.Run();
