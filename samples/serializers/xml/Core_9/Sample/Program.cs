480874b72a88fba934e6b5bab0f74617072918c6
Tomek Masternak <tomasz.masternak@particular.net>
2025-07-16 11:14:59 +0200

    fixing existing samples

--- samples/serializers/xml/Core_9/Sample/Program.cs
+++ samples/serializers/xml/Core_9/Sample/Program.cs
@@ -4,13 +4,11 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NServiceBus;
 using NServiceBus.MessageMutator;
-using Sample;
 
 
 Console.Title = "Xml";
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<InputLoopService>();
 
 #region config
 var endpointConfiguration = new EndpointConfiguration("Samples.Serialization.Xml");
@@ -35,4 +33,38 @@ Console.ReadKey();
 Console.WriteLine("Starting...");
 
 builder.UseNServiceBus(endpointConfiguration);
-await builder.Build().RunAsync();
+
+var host = builder.Build();
+
+await host.StartAsync();
+
+var messageSession = host.Services.GetRequiredService<IMessageSession>();
+
+#region message
+var message = new CreateOrder
+{
+    OrderId = 9,
+    Date = DateTime.Now,
+    CustomerId = 12,
+    OrderItems = new List<OrderItem>
+    {
+        new OrderItem
+        {
+            ItemId = 6,
+            Quantity = 2
+        },
+        new OrderItem
+        {
+            ItemId = 5,
+            Quantity = 4
+        },
+    }
+};
+
+await messageSession.SendLocal(message);
+
+#endregion
+
+Console.WriteLine("Order Sent");
+
+await host.StopAsync();
