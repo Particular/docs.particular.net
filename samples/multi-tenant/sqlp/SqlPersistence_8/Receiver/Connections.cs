98d4397fcb01e1db03d6ff756740f2698f6561e3
Jason Taylor <hello@jasontaylor.dev>
2025-07-16 15:24:31 +1000

    Clean-up Connections

--- samples/multi-tenant/sqlp/SqlPersistence_8/Receiver/Connections.cs
+++ samples/multi-tenant/sqlp/SqlPersistence_8/Receiver/Connections.cs
@@ -2,6 +2,8 @@
 
 public static class Connections
 {
+    // For SQL Server Express use:         Data Source=.\SqlExpress;Integrated Security=True;Encrypt=false;Max Pool Size=100
+    // For SQL Server Express LocalDb use: Data Source=(localdb)\MSSQLLocalDB;Integrated Security=True;Encrypt=false;Max Pool Size=100
     private const string Base = @"Data Source=.;User Id=SA;Password=yourStrong(!)Password;Encrypt=false;Max Pool Size=100";
 
     public static readonly string TenantA = $"{Base};Initial Catalog=SqlMultiTenantA";
