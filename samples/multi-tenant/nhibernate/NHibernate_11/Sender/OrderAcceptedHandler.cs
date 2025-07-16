56590ee51be4cd370e8e0397363a11fac38a67b6
Jason Taylor <hello@jasontaylor.dev>
2025-07-16 17:08:21 +1000

    Create NHibernate_11 Sample

---
+++ samples/multi-tenant/nhibernate/NHibernate_11/Sender/OrderAcceptedHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+
+
+public class OrderAcceptedHandler(ILogger<OrderAcceptedHandler> logger) :
+    IHandleMessages<OrderAccepted>
+{
+
+    public Task Handle(OrderAccepted message, IMessageHandlerContext context)
+    {
+        context.MessageHeaders.TryGetValue("tenant_id", out var tenantId);
+
+        logger.LogInformation("Tenant {TenantId} order {OrderId} was accepted.", tenantId, message.OrderId);
+        return Task.CompletedTask;
+    }
+}
---
+++ samples/multi-tenant/nhibernate/NHibernate_11/Sender/Program.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+Console.Title = "Sender";
+
+var builder = Host.CreateApplicationBuilder(args);
+
+var endpointConfiguration = new EndpointConfiguration("Samples.MultiTenant.Sender");
+endpointConfiguration.UseTransport(new LearningTransport());
+endpointConfiguration.EnableInstallers();
+endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+
+Console.WriteLine("Press any key, the application is starting");
+Console.ReadKey();
+Console.WriteLine("Starting...");
+
+builder.UseNServiceBus(endpointConfiguration);
+
+var host = builder.Build();
+await host.StartAsync();
+
+var messageSession = host.Services.GetRequiredService<IMessageSession>();
+
+const string letters = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+var random = new Random();
+
+Console.WriteLine("Press A or B to publish a message (A and B are tenant IDs)");
+Console.WriteLine("Press Escape to exit");
+var acceptableInput = new List<char> { 'A', 'B' };
+
+while (true)
+{
+    var key = Console.ReadKey(true);
+
+    if (key.Key == ConsoleKey.Escape)
+    {
+        break;
+    }
+    var uppercaseKey = char.ToUpperInvariant(key.KeyChar);
+
+    if (acceptableInput.Contains(uppercaseKey))
+    {
+        var orderId = new string(Enumerable.Range(0, 4).Select(x => letters[random.Next(letters.Length)]).ToArray());
+        var message = new OrderSubmitted
+        {
+            OrderId = orderId,
+            Value = random.Next(100)
+        };
+
+        var options = new PublishOptions();
+        options.SetHeader("tenant_id", uppercaseKey.ToString());
+
+        await messageSession.Publish(message, options);
+
+        Console.WriteLine($"Submitted order {message.OrderId} for tenant {uppercaseKey}");
+    }
+    else
+    {
+        Console.WriteLine($"[{uppercaseKey}] is not a valid tenant identifier.");
+    }
+}
+
+await host.StopAsync();
