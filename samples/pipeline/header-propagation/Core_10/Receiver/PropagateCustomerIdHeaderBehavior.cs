069cdfda22dc6fafdd188234d22886c0ec96a41b
Phil Bastian <phil.bastian@particular.net>
2025-07-17 07:10:36 +0800

    update header propogation sample for NSB 10

---
+++ samples/pipeline/header-propagation/Core_10/Receiver/PropagateCustomerIdHeaderBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus.Pipeline;
+
+#region behavior
+class PropagateCustomerIdHeaderBehavior : Behavior<IOutgoingLogicalMessageContext>
+{
+    public override Task Invoke(IOutgoingLogicalMessageContext context, Func<Task> next)
+    {
+        if (context.TryGetIncomingPhysicalMessage(out var incomingMessage))
+        {
+            if (incomingMessage.Headers.TryGetValue("CustomerId", out var incomingConversationId))
+            {
+                context.Headers["CustomerId"] = incomingConversationId;
+            }
+        }
+
+        return next();
+    }
+}
+#endregion
