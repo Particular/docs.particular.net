15849faaa10fec9c47528fef94069e1ba89f43ef
Tomek Masternak <tomasz.masternak@particular.net>
2025-07-16 09:52:57 +0200

    core 10 version of the sample

---
+++ samples/servicecontrol/events-subscription/ServiceControlContracts_6/EndpointsMonitor/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Hosting;
+using NServiceBus;
+using System;
+
+await Host.CreateDefaultBuilder(args)
+ .ConfigureServices((hostContext, services) =>
+ {
+ }).UseNServiceBus(x =>
+ {
+     Console.Title = "EndpointsMonitor";
+     var endpointConfiguration = new EndpointConfiguration("EndpointsMonitor");
+     endpointConfiguration.UseSerialization<NewtonsoftJsonSerializer>();
+     endpointConfiguration.EnableInstallers();
+     endpointConfiguration.UsePersistence<NonDurablePersistence>();
+
+     #region ServiceControlEventsMonitorCustomErrorQueue
+     endpointConfiguration.SendFailedMessagesTo("error-monitoring");
+     #endregion
+
+     var transport = endpointConfiguration.UseTransport<LearningTransport>();
+
+     var conventions = endpointConfiguration.Conventions();
+     conventions.DefiningEventsAs(
+         type =>
+         {
+             return typeof(IEvent).IsAssignableFrom(type) ||
+                    // include ServiceControl events
+                    type.Namespace != null &&
+                    type.Namespace.StartsWith("ServiceControl.Contracts");
+         });
+     return endpointConfiguration;
+ }).Build().RunAsync();
