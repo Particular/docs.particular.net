386e5dcdbee5024408ae809347587076add32d47
Adam Furmanek <adam.furmanek@particular.net>
2025-07-17 15:47:21 +0200

    Updating Azure Table transaction sample to .NET 10 (#7497)

---
+++ samples/azure/azure-table/transactions/ASTP_7/Client/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NServiceBus;
+
+
+Console.Title = "Client";
+
+var builder = Host.CreateApplicationBuilder(args);
+
+var endpointConfiguration = new EndpointConfiguration("Samples.AzureTable.Transactions.Client");
+endpointConfiguration.UsePersistence<LearningPersistence>();
+endpointConfiguration.UseTransport(new LearningTransport());
+endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+
+
+
+Console.WriteLine("Press any key, the application is starting");
+Console.TreatControlCAsInput = true;
+var input = Console.ReadKey();
+if (input.Key == ConsoleKey.C && (input.Modifiers & ConsoleModifiers.Control) != 0)
+{
+    Environment.Exit(0);
+}
+Console.WriteLine("Starting...");
+
+builder.UseNServiceBus(endpointConfiguration);
+var host = builder.Build();
+
+await host.StartAsync();
+
+var messageSession = host.Services.GetRequiredService<IMessageSession>();
+
+Console.WriteLine("Press 'S' to send a message");
+while (true)
+{
+    var key = Console.ReadKey();
+    Console.WriteLine();
+
+    if (key.Key != ConsoleKey.S)
+    {
+        break;
+    }
+
+    var orderId = Guid.NewGuid();
+    var startOrder = new StartOrder
+    {
+        OrderId = orderId
+    };
+
+    await messageSession.Send("Samples.AzureTable.Transactions.Server", startOrder);
+    Console.WriteLine($"StartOrder Message sent to Server with OrderId {orderId}");
+}
+
+await host.StopAsync();
---
+++ samples/azure/azure-table/transactions/ASTP_7/Server/OrderIdHeaderAsPartitionKeyBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+using NServiceBus.Pipeline;
+
+#region BehaviorUsingHeader
+class OrderIdHeaderAsPartitionKeyBehavior(ILogger<OrderIdHeaderAsPartitionKeyBehavior> logger) : Behavior<ITransportReceiveContext>
+{
+    public override Task Invoke(ITransportReceiveContext context, Func<Task> next)
+    {
+        if (context.Message.Headers.TryGetValue("Sample.AzureTable.Transaction.OrderId", out var orderId))
+        {
+            logger.LogInformation("Found partition key '{PartitionKey}' from header 'Sample.AzureTable.Transaction'", orderId);
+
+            context.Extensions.Set(new TableEntityPartitionKey(orderId));
+        }
+
+        return next();
+    }
+
+}
+#endregion
