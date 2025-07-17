db0e031e93c79d96c81acd88bda2ddae9f1060d5
Adam Furmanek <adam.furmanek@particular.net>
2025-07-17 15:47:10 +0200

    Updating Azure Table sample to .NET 10 (#7495)

---
+++ samples/azure/azure-table/table/ASTP_7/Server/BehaviorProvidingDynamicTable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+using NServiceBus.Pipeline;
+
+#region BehaviorAddingTableInfo
+
+class BehaviorProvidingDynamicTable(ILogger<BehaviorProvidingDynamicTable> logger) : Behavior<IIncomingLogicalMessageContext>
+{
+    public override Task Invoke(IIncomingLogicalMessageContext context, Func<Task> next)
+    {
+        if (context.Message.Instance is ShipOrder ||
+            context.Headers.TryGetValue(Headers.SagaType, out var sagaTypeHeader) && sagaTypeHeader.Contains(nameof(ShipOrderSaga)))
+        {
+
+            logger.LogInformation("Message '{MessageType}' destined to be handled by '{SagaType}' will use '{TableName}' table.", context.Message.MessageType.FullName, nameof(ShipOrderSaga), "ShipOrderSagaData");
+
+            context.Extensions.Set(new TableInformation("ShipOrderSagaData"));
+        }
+        else
+        {
+            logger.LogInformation("Message '{MessageType}' destined to be handled by '{SagaType}' will use the default table.", context.Message.MessageType.FullName, nameof(OrderSaga));
+        }
+
+        return next();
+    }
+
+}
+#endregion
