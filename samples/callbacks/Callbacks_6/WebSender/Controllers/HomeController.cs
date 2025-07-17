1e07a1e9967a052443957a96bb65748eb8ca2c5f
Bartek Wasielak <bartek.wasielak@particular.net>
2025-07-17 15:18:44 -0400

    Add NServiceBus 10 Callbacks sample (#7501)

---
+++ samples/callbacks/Callbacks_6/WebSender/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NServiceBus;
+
+public class HomeController(IMessageSession messageSession) : Controller
+{
+    public ActionResult Index()
+    {
+        return View();
+    }
+
+    #region Web_SendEnumMessage
+
+    public async Task<ActionResult> SendEnumMessage()
+    {
+        var message = new EnumMessage();
+        var sendOptions = new SendOptions();
+        sendOptions.SetDestination("Samples.Callbacks.Receiver");
+        var status = await messageSession.Request<Status>(message, sendOptions);
+        return View("SendEnumMessage", status);
+    }
+
+    #endregion
+
+    #region Web_SendIntMessage
+
+    public async Task<ActionResult> SendIntMessage()
+    {
+        var message = new IntMessage();
+        var sendOptions = new SendOptions();
+        sendOptions.SetDestination("Samples.Callbacks.Receiver");
+        var status = await messageSession.Request<int>(message, sendOptions);
+        return View("SendIntMessage", status);
+    }
+
+    #endregion
+
+    #region Web_SendObjectMessage
+
+    public async Task<ActionResult> SendObjectMessage()
+    {
+        var message = new ObjectMessage();
+        var sendOptions = new SendOptions();
+        sendOptions.SetDestination("Samples.Callbacks.Receiver");
+        var status = await messageSession.Request<ObjectResponseMessage>(message, sendOptions);
+        return View("SendObjectMessage", status);
+    }
+
+    #endregion
+}
