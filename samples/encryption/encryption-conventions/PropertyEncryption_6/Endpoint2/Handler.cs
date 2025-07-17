739aff922d2698e741a8112fb97579945b68b325
David Boike <david.boike@gmail.com>
2025-07-17 13:18:08 -0500

    NSB10 version of encryption-conventions sample (#7502)
    * NSB10 version of encryption-conventions sample
    
    * Prerelease file

---
+++ samples/encryption/encryption-conventions/PropertyEncryption_6/Endpoint2/Handler.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+
+public class Handler(ILogger<Handler> logger) :
+    IHandleMessages<MessageWithSecretData>
+{
+    public Task Handle(MessageWithSecretData message, IMessageHandlerContext context)
+    {
+        logger.LogInformation("I know the secret - it's '{EncryptedSecret}'", message.EncryptedSecret);
+        logger.LogInformation("SubSecret: {SubSecret}", message.SubProperty.EncryptedSecret);
+        foreach (var creditCard in message.CreditCards)
+        {
+            logger.LogInformation("CreditCard: {EncryptedNumber} is valid to {ValidTo}", creditCard.EncryptedNumber, creditCard.ValidTo);
+        }
+        return Task.CompletedTask;
+    }
+}
---
+++ samples/encryption/encryption-conventions/PropertyEncryption_6/Endpoint2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using NServiceBus;
+
+Console.Title = "Endpoint2";
+var builder = Host.CreateApplicationBuilder(args);
+
+var endpointConfiguration = new EndpointConfiguration("Samples.Encryption.Endpoint2");
+endpointConfiguration.Conventions().DefiningMessagesAs(type => type.Name.Contains("Message"));
+endpointConfiguration.ConfigurationEncryption();
+endpointConfiguration.UsePersistence<LearningPersistence>();
+endpointConfiguration.UseTransport(new LearningTransport());
+endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+
+builder.UseNServiceBus(endpointConfiguration);
+await builder.Build().RunAsync();
