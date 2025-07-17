c98b10ac176450b335ef0233bfbf7dc86c0c47c5
Adam Furmanek <adam.furmanek@particular.net>
2025-07-17 15:47:28 +0200

    Updating Azure Table saga sample to .NET 10 (#7498)

---
+++ samples/azure/azure-table/saga-transactions/ASTP_7/Server/ShipOrderHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Data.Tables;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+using NServiceBus.Persistence.AzureTable;
+
+public class ShipOrderHandler(ILogger<ShipOrderHandler> logger) :
+    IHandleMessages<ShipOrder>
+{
+    public Task Handle(ShipOrder message, IMessageHandlerContext context)
+    {
+        var orderShippingInformation = new OrderShippingInformation
+        {
+            Id = Guid.NewGuid(),
+            OrderId = message.OrderId,
+            ShippedAt = DateTimeOffset.UtcNow,
+        };
+
+        Store(orderShippingInformation, context);
+
+
+        logger.LogInformation("Order Shipped. OrderId {OrderId}", message.OrderId);
+
+        return context.Reply(new OrderShipped { OrderId = orderShippingInformation.OrderId, ShippingDate = orderShippingInformation.ShippedAt });
+    }
+
+    private static void Store(OrderShippingInformation orderShippingInformation, IMessageHandlerContext context)
+    {
+        var session = context.SynchronizedStorageSession.AzureTablePersistenceSession();
+
+        orderShippingInformation.PartitionKey = session.PartitionKey;
+
+        session.Batch.Add(new TableTransactionAction(TableTransactionActionType.Add, orderShippingInformation));
+    }
+
+}
