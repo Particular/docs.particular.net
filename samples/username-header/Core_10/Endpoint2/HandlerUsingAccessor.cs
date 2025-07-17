ccd63eadb59f05c032504f53bd1f980e083947e2
Irina Scurtu <scurtuirina@gmail.com>
2025-07-17 15:51:51 +0300

    Username-heaer sample to .net 10

---
+++ samples/username-header/Core_10/Endpoint2/HandlerUsingAccessor.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+
+#region handler-using-custom-header
+
+public class HandlerUsingAccessor :
+    IHandleMessages<MyMessage>
+{
+    readonly IPrincipalAccessor principalAccessor;
+    private readonly ILogger<HandlerUsingAccessor> logger;
+
+    public HandlerUsingAccessor(IPrincipalAccessor principalAccessor, ILogger<HandlerUsingAccessor> logger)
+    {
+        this.principalAccessor = principalAccessor;
+        this.logger = logger;
+    }
+
+    public Task Handle(MyMessage message, IMessageHandlerContext context)
+    {
+        var headers = context.MessageHeaders;
+        var usernameFromHeader = headers["UserName"];
+        var usernameFromAccessor = principalAccessor?.CurrentPrincipal?.Identity?.Name ?? "null";
+        logger.LogInformation("Username extracted from header: {UsernameFromHeader}", usernameFromHeader);
+        logger.LogInformation("Username extracted from accessor: {UsernameFromAccessor}", usernameFromAccessor);
+        return Task.CompletedTask;
+    }
+}
+
+#endregion
