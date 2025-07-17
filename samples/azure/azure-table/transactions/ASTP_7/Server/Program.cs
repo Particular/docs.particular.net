20e60279a0d7c471cdfc661607a73fb6ffa33c16
Adam Furmanek <adam.furmanek@particular.net>
2025-07-17 15:10:05 +0200

    Updating Azure Table transaction sample to .NET 10

---
+++ samples/azure/azure-table/transactions/ASTP_7/Server/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+using Azure.Data.Tables;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+
+
+Console.Title = "Server";
+
+var builder = Host.CreateApplicationBuilder(args);
+
+#region AzureTableConfig
+
+var endpointConfiguration = new EndpointConfiguration("Samples.AzureTable.Transactions.Server");
+endpointConfiguration.EnableOutbox();
+
+var useStorageTable = true;
+var persistence = endpointConfiguration.UsePersistence<AzureTablePersistence>();
+
+var connection = useStorageTable ? "UseDevelopmentStorage=true" :
+    "TableEndpoint=https://localhost:8081/;AccountName=AzureTableSamples;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+
+var tableServiceClient = new TableServiceClient(connection);
+persistence.UseTableServiceClient(tableServiceClient);
+
+persistence.DefaultTable("Server");
+
+#endregion
+
+
+#region BehaviorRegistration
+
+var serviceProvider = builder.Services.BuildServiceProvider();
+var logger = serviceProvider.GetRequiredService<ILogger<OrderIdHeaderAsPartitionKeyBehavior>>();
+endpointConfiguration.Pipeline.Register(new OrderIdHeaderAsPartitionKeyBehavior(logger), "Extracts a partition key from a header");
+endpointConfiguration.Pipeline.Register(new OrderIdAsPartitionKeyBehavior.Registration());
+
+#endregion
+
+endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+var transport = endpointConfiguration.UseTransport<LearningTransport>();
+transport.Transactions(TransportTransactionMode.ReceiveOnly);
+endpointConfiguration.EnableInstallers();
+
+
+Console.WriteLine("Press any key, the application is starting");
+Console.TreatControlCAsInput = true;
+var input = Console.ReadKey();
+Console.TreatControlCAsInput = false;
+if (input.Key == ConsoleKey.C && (input.Modifiers & ConsoleModifiers.Control) != 0)
+{
+    Environment.Exit(0);
+}
+Console.WriteLine("Starting...");
+
+builder.UseNServiceBus(endpointConfiguration);
+await builder.Build().RunAsync();
---
+++ samples/azure/azure-table/transactions/ASTP_7/Server/ShipOrderHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Data.Tables;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+using NServiceBus.Persistence.AzureTable;
+
+#region UseHeader
+public class ShipOrderHandler (ILogger<ShipOrderHandler> logger):
+    IHandleMessages<ShipOrder>
+{
+    public Task Handle(ShipOrder message, IMessageHandlerContext context)
+    {
+        var orderShippingInformation = new OrderShippingInformation
+        {
+            Id = Guid.NewGuid(),
+            OrderId = message.OrderId,
+            ShippedAt = DateTimeOffset.UtcNow,
+        };
+
+        Store(orderShippingInformation, context);
+
+
+        logger.LogInformation("Order Shipped. OrderId {OrderId}", message.OrderId);
+
+        var options = new PublishOptions();
+        options.SetHeader("Sample.AzureTable.Transaction.OrderId", message.OrderId.ToString());
+
+        return context.Publish(new OrderShipped { OrderId = orderShippingInformation.OrderId, ShippingDate = orderShippingInformation.ShippedAt }, options);
+    }
+
+    private static void Store(OrderShippingInformation orderShippingInformation, IMessageHandlerContext context)
+    {
+        var session = context.SynchronizedStorageSession.AzureTablePersistenceSession();
+
+        orderShippingInformation.PartitionKey = session.PartitionKey;
+
+        session.Batch.Add(new TableTransactionAction(TableTransactionActionType.Add, orderShippingInformation));
+    }
+}
+#endregion
