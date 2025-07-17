ce6279649be1fb759cd89e145ad828e4f7e16cac
Irina Scurtu <scurtuirina@gmail.com>
2025-07-17 15:19:15 +0300

    Added sample for .net 10

---
+++ samples/unit-testing/Testing_10/Sample/MyControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using NServiceBus.Testing;
+using NUnit.Framework;
+
+[TestFixture]
+public class MyControllerTests
+{
+    #region EndpointInstanceTest
+    [Test]
+    public async Task ShouldSendMessage()
+    {
+        var endpointInstance = new TestableEndpointInstance();
+        var handler = new MyController(endpointInstance);
+
+        await handler.HandleRequest();
+
+        var sentMessages = endpointInstance.SentMessages;
+        Assert.That(sentMessages, Has.Length.EqualTo(1));
+        Assert.That(sentMessages[0].Message, Is.InstanceOf<MyMessage>());
+    }
+    #endregion
+}
