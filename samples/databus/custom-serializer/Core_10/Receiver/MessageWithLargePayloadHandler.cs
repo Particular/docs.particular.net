96dd5c40d48de7ed8258da3f2adf1f8cec175da1
Christian <christian.quiros+github@gmail.com>
2025-07-17 21:49:48 -0600

    Add baseline Core_10 copy from Core_9 for databus custom-serializer

---
+++ samples/databus/custom-serializer/Core_10/Receiver/MessageWithLargePayloadHandler.cs
@@ -0,0 +1,17 @@
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+using NServiceBus.Logging;
+
+public class MessageWithLargePayloadHandler(ILogger<MessageWithLargePayloadHandler> logger) :
+    IHandleMessages<MessageWithLargePayload>
+{
+
+    public Task Handle(MessageWithLargePayload message, IMessageHandlerContext context)
+    {
+        #pragma warning disable CS0618 // Type or member is obsolete
+        logger.LogInformation("Message received containing {MeasurementCount} measurements", message.LargeData.Value.Length);
+        #pragma warning restore CS0618 // Type or member is obsolete
+        return Task.CompletedTask;
+    }
+}
