77e87ba12e802d576ca5b5de88d24d1bd42fc845
Christian <christian.quiros+github@gmail.com>
2025-07-17 23:01:45 -0600

    Add baseline Bridge_4 copy from Bridge_3

---
+++ samples/bridge/service-control/Bridge_4/Endpoint/Program.cs
@@ -0,0 +1,42 @@
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NServiceBus;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((hostContext, services) =>
+    {
+        Console.Title = "Endpoint";
+        services.AddHostedService<InputLoopService>();
+    })
+    .UseNServiceBus(x =>
+    {
+        var endpointConfiguration = new EndpointConfiguration(
+            "Samples.Bridge.Endpoint");
+
+        endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+        endpointConfiguration.UseTransport(new LearningTransport
+        {
+            StorageDirectory = $"{LearningTransportInfrastructure.FindStoragePath()}2"
+        });
+
+        var recoverability = endpointConfiguration.Recoverability();
+        recoverability.Immediate(
+            customizations: immediate => { immediate.NumberOfRetries(0); });
+        recoverability.Delayed(delayed => delayed.NumberOfRetries(0));
+
+        endpointConfiguration.SendFailedMessagesTo("error");
+        endpointConfiguration.AuditProcessedMessagesTo("audit");
+        endpointConfiguration.SendHeartbeatTo("Particular.ServiceControl");
+        endpointConfiguration.EnableInstallers();
+        endpointConfiguration.EnableMetrics()
+            .SendMetricDataToServiceControl("Particular.Monitoring", TimeSpan.FromSeconds(1));
+
+        var routing = endpointConfiguration.UseTransport(new LearningTransport());
+        routing.RouteToEndpoint(typeof(MyMessage), "Endpoint");
+
+        return endpointConfiguration;
+    })
+    .Build();
+
+await host.RunAsync();
