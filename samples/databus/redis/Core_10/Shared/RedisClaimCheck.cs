70af321c35d3859cda9fe9a47c7b70d33a2fa3a9
Christian <christian.quiros+github@gmail.com>
2025-07-18 00:08:38 -0600

    Add baseline Core_10 copy from Core_9

---
+++ samples/databus/redis/Core_10/Shared/RedisClaimCheck.cs
@@ -0,0 +1,45 @@
+﻿namespace Shared;
+
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using NServiceBus.ClaimCheck;
+using StackExchange.Redis;
+
+#region claim-check
+public class RedisClaimCheck(string configuration) : IClaimCheck
+{
+    private IConnectionMultiplexer redis;
+
+    public async Task<Stream> Get(
+        string key,
+        CancellationToken cancellationToken = default)
+    {
+        var db = redis.GetDatabase();
+        byte[] redisValue = await db.StringGetAsync(key);
+        return new MemoryStream(redisValue);
+    }
+
+    public async Task<string> Put(
+        Stream stream,
+        TimeSpan timeToBeReceived,
+        CancellationToken cancellationToken = default)
+    {
+        var db = redis.GetDatabase();
+        var key = $"particular:sample:claimcheck:{Guid.NewGuid()}";
+        var value = ((MemoryStream)stream).ToArray();
+        await db.StringSetAsync(
+            key,
+            value,
+            timeToBeReceived == TimeSpan.MaxValue ? null : timeToBeReceived
+        );
+        return key;
+    }
+
+    public async Task Start(CancellationToken cancellationToken = default)
+    {
+        redis = await ConnectionMultiplexer.ConnectAsync(configuration);
+    }
+}
+#endregion
