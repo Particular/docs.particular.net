39d89b76d669ef1e0a4187bdb56651ac932f2bd8
Christian <christian.quiros+github@gmail.com>
2025-07-17 21:55:50 -0600

    Migrate databus custom-serializer to NServiceBus 10
    - Updated all .csproj files to target .NET 10 and NServiceBus 10.0.0-alpha.1
    - Added NServiceBus.ClaimCheck 2.0.0-alpha.1 package
    - Migrated from DataBusProperty<T> to regular array properties
    - Updated BsonDataBusSerializer to BsonClaimCheckSerializer implementing IClaimCheckSerializer
    - Replaced UseDataBus with UseClaimCheck<FileShareClaimCheck, BsonClaimCheckSerializer>
    - Added prerelease.txt marker file
    - Removed InputLoopService pattern and moved logic to Program.cs
    - Updated message handler to work with regular arrays instead of DataBusProperty<T>
    - Added claim check conventions for properties starting with 'Large'
    
    Fixes: #7289
    
    a

--- samples/databus/custom-serializer/Core_10/Receiver/Program.cs
+++ samples/databus/custom-serializer/Core_10/Receiver/Program.cs
@@ -1,21 +1,21 @@
+using System;
 using Microsoft.Extensions.Hosting;
 using NServiceBus;
+using NServiceBus.ClaimCheck;
 using Shared;
-using System;
-using System.Threading.Tasks;
 
 Console.Title = "Receiver";
 var builder = Host.CreateApplicationBuilder(args);
 var endpointConfiguration = new EndpointConfiguration("Samples.DataBus.Receiver");
 
-#pragma warning disable CS0618 // Type or member is obsolete
 #region ConfigureReceiverCustomDataBusSerializer
 
-var dataBus = endpointConfiguration.UseDataBus<FileShareDataBus, BsonDataBusSerializer>();
-dataBus.BasePath(@"..\..\..\..\storage");
+var claimCheck = endpointConfiguration.UseClaimCheck<FileShareClaimCheck, BsonClaimCheckSerializer>();
+claimCheck.BasePath(@"..\..\..\..\storage");
 
 #endregion
-#pragma warning restore CS0618 // Type or member is obsolete
+
+endpointConfiguration.Conventions().DefiningClaimCheckPropertiesAs(prop => prop.Name.StartsWith("Large"));
 
 endpointConfiguration.UseSerialization<SystemJsonSerializer>();
 endpointConfiguration.UseTransport(new LearningTransport());
--- samples/databus/custom-serializer/Core_10/Sender/Program.cs
+++ samples/databus/custom-serializer/Core_10/Sender/Program.cs
@@ -1,30 +1,76 @@
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using NServiceBus;
-using Sender;
-using Shared;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NServiceBus;
+using NServiceBus.ClaimCheck;
+using Shared;
 
 Console.Title = "Sender";
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<InputLoopService>();
 var endpointConfiguration = new EndpointConfiguration("Samples.DataBus.Sender");
 
-#pragma warning disable CS0618 // Type or member is obsolete
 #region ConfigureSenderCustomDataBusSerializer
 
-var dataBus = endpointConfiguration.UseDataBus<FileShareDataBus, BsonDataBusSerializer>();
-dataBus.BasePath(@"..\..\..\..\storage");
+var claimCheck = endpointConfiguration.UseClaimCheck<FileShareClaimCheck, BsonClaimCheckSerializer>();
+claimCheck.BasePath(@"..\..\..\..\storage");
 
 #endregion
-#pragma warning restore CS0618 // Type or member is obsolete
+
+endpointConfiguration.Conventions().DefiningClaimCheckPropertiesAs(prop => prop.Name.StartsWith("Large"));
 
 endpointConfiguration.UseSerialization<SystemJsonSerializer>();
 endpointConfiguration.UseTransport(new LearningTransport());
 builder.UseNServiceBus(endpointConfiguration);
 
-await builder.Build().RunAsync();
+var app = builder.Build();
+
+await app.StartAsync();
+
+var messageSession = app.Services.GetRequiredService<IMessageSession>();
+
+Console.WriteLine("Press Enter to send a large message with claim check");
+Console.WriteLine("Press any other key to exit");
+
+while (true)
+{
+    var key = Console.ReadKey(true);
+    Console.WriteLine();
+
+    if (key.Key == ConsoleKey.Enter)
+    {
+        await SendMessageLargePayload(messageSession);
+        continue;
+    }
+    break;
+}
+
+await app.StopAsync();
+
+static Task SendMessageLargePayload(IMessageSession messageSession)
+{
+    var measurements = GetMeasurements().ToArray();
+
+    var message = new MessageWithLargePayload
+    {
+        SomeProperty = "This message contains a large collection that will be sent on the claim check",
+        LargeData = measurements
+    };
+    Console.WriteLine(@"Message sent, the payload is stored in: ..\..\..\storage");
+    return messageSession.Send("Samples.DataBus.Receiver", message);
+}
+
+static IEnumerable<Measurement> GetMeasurements()
+{
+    for (var i = 0; i < 10000; i++)
+    {
+        yield return new Measurement
+        {
+            Timestamp = DateTimeOffset.UtcNow,
+            MeasurementName = $"Instrument {i}",
+            MeasurementValue = i * 10m
+        };
+    }
+}
