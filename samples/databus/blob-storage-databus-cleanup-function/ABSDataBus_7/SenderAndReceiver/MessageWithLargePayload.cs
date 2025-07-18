6aa1b154455917cbe632f8ce3b1935ad5d5161e1
Christian <christian.quiros+github@gmail.com>
2025-07-18 00:04:25 -0600

    Add baseline ABSDataBus_7 copy from ABSDataBus_6

---
+++ samples/databus/blob-storage-databus-cleanup-function/ABSDataBus_7/SenderAndReceiver/MessageWithLargePayload.cs
@@ -0,0 +1,10 @@
+﻿using NServiceBus;
+
+[TimeToBeReceived("00:00:10")]
+public class MessageWithLargePayload : ICommand
+{
+#pragma warning disable CS0618 // Type or member is obsolete
+    public DataBusProperty<byte[]> LargePayload { get; set; }
+#pragma warning restore CS0618 // Type or member is obsolete
+    public string Description { get; set; }
+}
