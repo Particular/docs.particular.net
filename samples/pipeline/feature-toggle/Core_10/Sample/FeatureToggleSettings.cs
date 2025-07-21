8a795e94dd821c72b1351df6e5e6bc460a19229a
Phil Bastian <155411597+PhilBastian@users.noreply.github.com>
2025-07-21 10:52:11 +0800

    upgrade feature toggle sample to nsb10 (#7511)
    * move inputloop to program to allow showdown, refactor to primary constructors and use logger for log messages
    
    * upgrade sample to nsb10

---
+++ samples/pipeline/feature-toggle/Core_10/Sample/FeatureToggleSettings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using NServiceBus.Pipeline;
+
+public class FeatureToggleSettings
+{
+    public void AddToggle(Func<IInvokeHandlerContext, bool> toggle) => Toggles.Add(toggle);
+
+    internal IList<Func<IInvokeHandlerContext, bool>> Toggles { get; } = new List<Func<IInvokeHandlerContext, bool>>();
+}
