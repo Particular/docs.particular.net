8d3fb07cf30b214bbc966d9ce388172cc88d5555
Jason Taylor <hello@jasontaylor.dev>
2025-07-21 13:52:00 +1000

    Remove unnecessary usings

--- samples/mongodb/simple/MongoDB_6/Client/OrderCompletedHandler.cs
+++ samples/mongodb/simple/MongoDB_6/Client/OrderCompletedHandler.cs
@@ -1,6 +1,4 @@
-﻿using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
-using NServiceBus;
+﻿using Microsoft.Extensions.Logging;
 
 public class OrderCompletedHandler(ILogger<OrderCompletedHandler> logger) : IHandleMessages<OrderCompleted>
 {
--- samples/mongodb/simple/MongoDB_6/Server/OrderSagaData.cs
+++ samples/mongodb/simple/MongoDB_6/Server/OrderSagaData.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using NServiceBus;
-
-#region sagadata
+﻿#region sagadata
 
 public class OrderSagaData : ContainSagaData
 {
