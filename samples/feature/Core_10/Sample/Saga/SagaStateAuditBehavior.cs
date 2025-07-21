2b178ad26902dc251960c1944040c4d71eb7db8f
Dharmishtha Patel <dharmishthapatel@Dharmishthas-MacBook-Pro.local>
2025-07-21 07:20:48 -0700

    Upgrade Feature sample to .NET 10 with NServiceBus 10 and modern C# features

---
+++ samples/feature/Core_10/Sample/Saga/SagaStateAuditBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus.Pipeline;
+using NServiceBus.Sagas;
+
+#region SagaStateAuditBehavior
+class SagaStateAuditBehavior(CustomLogger logger) :
+    Behavior<IInvokeHandlerContext>
+{
+    public override async Task Invoke(IInvokeHandlerContext context, Func<Task> next)
+    {
+        await next();
+        if (context.Extensions.TryGet(out ActiveSagaInstance activeSagaInstance))
+        {
+            var instance = activeSagaInstance.Instance.Entity;
+            logger.WriteSaga(instance);
+        }
+    }
+}
+#endregion
+
+
