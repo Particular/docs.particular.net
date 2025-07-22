50a57951c5d0539a79d5a22e87adba20c10fcad3
Dharmishtha Patel <dharmishthapatel@Dharmishthas-MacBook-Pro.local>
2025-07-21 19:17:34 -0700

    Add ErrorHandling sample for .NET 10 with NServiceBus 10 and modern C# features

---
+++ samples/errorhandling/Core_10/WithDelayedRetries/Program.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+Console.Title = "WithDelayedRetries";
+
+var endpointConfiguration = new EndpointConfiguration("Samples.ErrorHandling.WithDelayedRetries");
+endpointConfiguration.UsePersistence<LearningPersistence>();
+endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+endpointConfiguration.UseTransport(new LearningTransport());
+
+var endpointInstance = await Endpoint.Start(endpointConfiguration);
+
+Console.WriteLine("Press enter to send a message that will throw an exception.");
+Console.WriteLine("Press any key to exit");
+
+while (true)
+{
+    var key = Console.ReadKey();
+    if (key.Key != ConsoleKey.Enter)
+    {
+        break;
+    }
+
+    var myMessage = new MyMessage
+    {
+        Id = Guid.NewGuid()
+    };
+
+    await endpointInstance.SendLocal(myMessage);
+}
+
+await endpointInstance.Stop();
