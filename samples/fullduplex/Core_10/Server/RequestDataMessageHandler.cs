f0e5a547a2e30cc58f3fbadc158dbdce9051f9ae
Dharmishtha <dharmishtha.patel@particular.net>
2025-07-22 06:10:36 -0700

    Add fullduplex Core_10 sample with .NET 10 and modern C# features (#7468)

---
+++ samples/fullduplex/Core_10/Server/RequestDataMessageHandler.cs
@@ -0,0 +1,23 @@
+using System.Threading.Tasks;
+using NServiceBus;
+using Microsoft.Extensions.Logging;
+
+#region RequestDataMessageHandler
+public class RequestDataMessageHandler(ILogger<RequestDataMessageHandler> logger) :
+    IHandleMessages<RequestDataMessage>
+#endregion
+{
+    public async Task Handle(RequestDataMessage message, IMessageHandlerContext context)
+    {
+        logger.LogInformation("Received request {DataId}.", message.DataId);
+        logger.LogInformation("String received: {Description}.", message.String);
+
+        #region DataResponseReply
+
+        var response = new DataResponseMessage(message.DataId, message.String);
+
+        await context.Reply(response);
+
+        #endregion
+    }
+}
