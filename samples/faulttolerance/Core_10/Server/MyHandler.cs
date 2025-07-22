cb500fba82c199fb2971cb0da0bb0b0cf044139b
Dharmishtha Patel <dharmishthapatel@Dharmishthas-MacBook-Pro.local>
2025-07-21 18:08:48 -0700

    Add FaultTolerance sample for .NET 10 with NServiceBus 10 and modern C# features

---
+++ samples/faulttolerance/Core_10/Server/MyHandler.cs
@@ -0,0 +1,16 @@
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+
+#region MyHandler
+public class MyHandler(ILogger<MyHandler> logger) :
+    IHandleMessages<MyMessage>
+{
+    public Task Handle(MyMessage message, IMessageHandlerContext context)
+    {
+        logger.LogInformation("Message received. Id: {Id}", message.Id);
+        // throw new Exception("Uh oh - something went wrong....");
+        return Task.CompletedTask;
+    }
+}
+#endregion
