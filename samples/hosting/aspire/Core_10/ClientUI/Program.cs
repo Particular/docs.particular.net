3421236f7feebb2d5f1083b5481e84f84ae76959
Hazel Bohon <hazel.bohon@particular.net>
2025-07-23 09:57:23 -0500

    Aspire sample net10 (#7545)
    * Fix samples/hosting/aspire/Core_9/
    
    The ServiceControlMonitor does not accept the amqp:// format connection string.
    
    * Copied Core_9 to Core_10
    
    * Update NET10 projects
    
    * TargetFrameworks: NET10.0
    * LangVersion: preview
    * NServiceBus.* dependencies updated to `alpha.1`.
    
    * Added prerelease.txt
    
    * Changed TargetFrameworks to TargetFramework
    
    * Change targetframework in AspireDemo.AppHost.csproj
    
    * Updated Preview packages for Billing.csproj

---
+++ samples/hosting/aspire/Core_10/ClientUI/Program.cs
@@ -0,0 +1,31 @@
+﻿using ClientUI;
+using Messages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder();
+
+builder.AddServiceDefaults();
+
+var endpointConfiguration = new EndpointConfiguration("ClientUI");
+endpointConfiguration.EnableOpenTelemetry();
+
+var connectionString = builder.Configuration.GetConnectionString("transport");
+var transport = new RabbitMQTransport(RoutingTopology.Conventional(QueueType.Quorum), connectionString);
+var routing = endpointConfiguration.UseTransport(transport);
+routing.RouteToEndpoint(typeof(PlaceOrder), "Sales");
+
+endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+endpointConfiguration.SendHeartbeatTo("Particular.ServiceControl");
+
+var metrics = endpointConfiguration.EnableMetrics();
+metrics.SendMetricDataToServiceControl("Particular.Monitoring", TimeSpan.FromSeconds(1));
+
+endpointConfiguration.EnableInstallers();
+
+builder.UseNServiceBus(endpointConfiguration);
+
+builder.Services.AddHostedService<MessageSenderService>();
+
+await builder.Build().RunAsync();
