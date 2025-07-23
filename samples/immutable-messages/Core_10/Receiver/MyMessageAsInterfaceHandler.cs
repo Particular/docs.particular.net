9147ef7c3973337cc73bd5e9947ab00a170cf63d
Hazel Bohon <hazel.bohon@particular.net>
2025-07-23 09:57:04 -0500

    Update Immutable-Messages to NET10 (#7549)
    * Update Immutable-Messages to NET10
    
    - Copied the `core_9` dir to `core_10`
    - Updated to .NET 10
      - Updated TargetFramework to NET10.0
      - Updated LangVersion to preview
      - Updated package references to latest alphas.
      - Added prerelease.txt
      - Removed an unsued variable reference @ program.cs:10.
    - Fixes #7548
    
    * Update samples/immutable-messages/Core_10/Shared/Shared.csproj
    
    Co-authored-by: Adam Wright <awright18@gmail.com>
    
    * Update samples/immutable-messages/Core_10/Shared/Shared.csproj
    
    Co-authored-by: Adam Wright <awright18@gmail.com>
    
    ---------
    
    Co-authored-by: Adam Wright <awright18@gmail.com>

---
+++ samples/immutable-messages/Core_10/Receiver/MyMessageAsInterfaceHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+using UsingInterfaces.Messages;
+
+#region immutable-messages-as-interface-handling
+public class MyMessageAsInterfaceHandler(ILogger<MyMessageAsInterfaceHandler> logger) :
+    IHandleMessages<IMyMessage>
+{
+    public Task Handle(IMyMessage message, IMessageHandlerContext context)
+    {
+        logger.LogInformation("IMyMessage (as interface) received from server with data: {Data}", message.Data);
+        return Task.CompletedTask;
+    }
+}
+#endregion
