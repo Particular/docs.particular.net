0ff7b00ebd9530e971926c8a33e1f2d793057b46
Hazel Bohon <hazel.bohon@particular.net>
2025-07-23 07:07:55 -0500

    Added Core_10 version of HeaderManipulation
    - Copied Core_9 to Core_10
    - Updated to NET10
      - Changed TargetFrameworks from `net9.0;net8.0` to `net10.0`
      - Changed LangVersion to `preview`
      - Updated Package references to alphas.
      - Added prerelease.txt
    - Fixes #7313

---
+++ samples/header-manipulation/Core_10/Sample/MyHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+#region handler
+
+public class MyHandler(ILogger<MyHandler> logger) :
+    IHandleMessages<MyMessage>
+{
+
+    public Task Handle(MyMessage message, IMessageHandlerContext context)
+    {
+        logger.LogInformation("Hello from MyHandler");
+        var headers = context.MessageHeaders;
+        foreach (var header in headers.OrderBy(x => x.Key))
+        {
+            logger.LogInformation("Header Key={Key}, Value={Value}", header.Key, header.Value);
+        }
+        return Task.CompletedTask;
+    }
+}
+
+#endregion
