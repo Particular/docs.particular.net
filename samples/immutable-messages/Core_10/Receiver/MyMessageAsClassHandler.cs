418a186126fdb44543afcb2ee364897ca97fa6d4
Hazel Bohon <hazel.bohon@particular.net>
2025-07-23 07:28:56 -0500

    Update Immutable-Messages to NET10
    - Copied the `core_9` dir to `core_10`
    - Updated to .NET 10
      - Updated TargetFramework to NET10.0
      - Updated LangVersion to preview
      - Updated package references to latest alphas.
      - Added prerelease.txt
      - Removed an unsued variable reference @ program.cs:10.
    - Fixes #7548

---
+++ samples/immutable-messages/Core_10/Receiver/MyMessageAsClassHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+using UsingClasses.Messages;
+
+#region immutable-messages-as-class-handling
+public class MyMessageAsClassHandler(ILogger<MyMessageAsClassHandler> logger) :
+    IHandleMessages<MyMessage>
+{
+     public Task Handle(MyMessage message, IMessageHandlerContext context)
+    {
+        logger.LogInformation("MyMessage (as class) received from server with data: {Data}", message.Data);
+        return Task.CompletedTask;
+    }
+}
+#endregion
