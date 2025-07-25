2c1db4c8fc51423ca377e21de7ea8dc6765f7931
awright18 <awright18@gmail.com>
2025-07-25 15:55:24 -0500

    Add ASBS_6 to asb-native-integration sample

---
+++ samples/azure-service-bus-netstandard/native-integration/ASBS_6/Receiver/NativeMessageHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+
+#region NativeMessageHandler
+
+public class NativeMessageHandler(ILogger<NativeMessageHandler> logger) :
+    IHandleMessages<NativeMessage>
+{
+
+    public Task Handle(NativeMessage message, IMessageHandlerContext context)
+    {
+        logger.LogInformation("Message content: {Content}", message.Content);
+        logger.LogInformation("Received native message sent on {SentOnUtc} UTC", message.SentOnUtc);
+        return Task.CompletedTask;
+    }
+}
+
+#endregion
---
+++ samples/azure-service-bus-netstandard/native-integration/ASBS_6/Receiver/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using NServiceBus;
+
+Console.Title = "NativeIntegration";
+
+var builder = Host.CreateApplicationBuilder(args);
+
+#region EndpointName
+
+var endpointConfiguration = new EndpointConfiguration("Samples.ASB.NativeIntegration");
+
+#endregion
+
+endpointConfiguration.EnableInstallers();
+endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+endpointConfiguration.Conventions().DefiningMessagesAs(type => type.Name == "NativeMessage");
+
+var connectionString = Environment.GetEnvironmentVariable("AzureServiceBus_ConnectionString");
+if (string.IsNullOrWhiteSpace(connectionString))
+{
+    throw new Exception("Could not read the 'AzureServiceBus_ConnectionString' environment variable. Check the sample prerequisites.");
+}
+
+var transport = new AzureServiceBusTransport(connectionString, TopicTopology.Default);
+endpointConfiguration.UseTransport(transport);
+
+
+Console.WriteLine("Press any key, the application is starting");
+Console.ReadKey();
+Console.WriteLine("Starting...");
+
+builder.UseNServiceBus(endpointConfiguration);
+await builder.Build().RunAsync();
