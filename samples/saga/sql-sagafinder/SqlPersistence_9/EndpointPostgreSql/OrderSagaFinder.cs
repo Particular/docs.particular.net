6c2e172910926f0564bccfd9a5d0470662e2e2c0
Jayanthi <jayanthi.sourirajan@particular.net>
2025-07-25 17:15:00 -0700

    replace input loop service, upgrade to net 10 , nsb 10 preview

---
+++ samples/saga/sql-sagafinder/SqlPersistence_9/EndpointPostgreSql/OrderSagaFinder.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Extensibility;
+using NServiceBus.Persistence;
+using NServiceBus.Sagas;
+
+#region PostgreSqlFinder
+class OrderSagaFinder :
+    ISagaFinder<OrderSagaData, CompletePaymentTransaction>
+{
+    public Task<OrderSagaData> FindBy(CompletePaymentTransaction message, ISynchronizedStorageSession storageSession, IReadOnlyContextBag context, CancellationToken cancellationToken = default)
+    {
+        return storageSession.GetSagaData<OrderSagaData>(
+            context: context,
+            whereClause: @"""Data""->>'PaymentTransactionId' = @propertyValue",
+            appendParameters: (builder, append) =>
+            {
+                var parameter = builder();
+                parameter.ParameterName = "propertyValue";
+                parameter.Value = message.PaymentTransactionId;
+                append(parameter);
+            });
+    }
+}
+#endregion
---
+++ samples/saga/sql-sagafinder/SqlPersistence_9/EndpointPostgreSql/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.Extensions.Hosting;
+using Npgsql;
+using NpgsqlTypes;
+using NServiceBus;
+
+
+Console.Title = "PostgreSql";
+var builder = Host.CreateApplicationBuilder(args);
+var endpointConfiguration = new EndpointConfiguration("Samples.SqlSagaFinder.PostgreSql");
+endpointConfiguration.UseTransport(new LearningTransport());
+endpointConfiguration.UseSerialization<XmlSerializer>();
+endpointConfiguration.SendFailedMessagesTo("error");
+endpointConfiguration.EnableInstallers();
+#region PostgreSqlConfig
+
+var persistence = endpointConfiguration.UsePersistence<SqlPersistence>();
+var password = Environment.GetEnvironmentVariable("PostgreSqlPassword");
+password = "yourStrong(!)Password";
+if (string.IsNullOrWhiteSpace(password))
+{
+    throw new Exception("Could not extract 'PostgreSqlPassword' from Environment variables.");
+}
+var username = Environment.GetEnvironmentVariable("PostgreSqlUserName");
+username = "postgres";
+if (string.IsNullOrWhiteSpace(username))
+{
+    throw new Exception("Could not extract 'PostgreSqlUserName' from Environment variables.");
+}
+var connection = $"Host=localhost;Username={username};Password={password};Database=NsbSamplesSqlSagaFinder";
+persistence.TablePrefix("Finder");
+var dialect = persistence.SqlDialect<SqlDialect.PostgreSql>();
+dialect.JsonBParameterModifier(
+    modifier: parameter =>
+    {
+        var npgsqlParameter = (NpgsqlParameter)parameter;
+        npgsqlParameter.NpgsqlDbType = NpgsqlDbType.Jsonb;
+    });
+persistence.ConnectionBuilder(
+    connectionBuilder: () =>
+    {
+        return new NpgsqlConnection(connection);
+    });
+
+var subscriptions = persistence.SubscriptionSettings();
+subscriptions.CacheFor(TimeSpan.FromMinutes(1));
+
+#endregion
+
+var endpointInstance = await Endpoint.Start(endpointConfiguration);
+var startOrder = new StartOrder
+{
+    OrderId = "123"
+};
+await endpointInstance.SendLocal(startOrder);
+
+Console.WriteLine("Press any key to exit");
+Console.ReadKey();
+await endpointInstance.Stop();
+builder.UseNServiceBus(endpointConfiguration);
+
+await builder.Build().RunAsync();
