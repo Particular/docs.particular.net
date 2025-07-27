c3881c8a9decba00e75e610422716eb1018d02a5
Christian <christian.quiros+github@gmail.com>
2025-07-27 17:48:04 -0600

    receiver program.cs line re-grouping

--- samples/databus/file-share-databus/DataBus_1/Receiver/Program.cs
+++ samples/databus/file-share-databus/DataBus_1/Receiver/Program.cs
@@ -1,24 +1,26 @@
-using NServiceBus;
-using Shared;
 using System;
+using Shared;
 using Microsoft.Extensions.Hosting;
+using NServiceBus;
 
 Console.Title = "Receiver";
+
 var builder = Host.CreateApplicationBuilder(args);
+
 var endpointConfiguration = new EndpointConfiguration("Samples.ClaimCheck.Receiver");
-var claimCheck = endpointConfiguration.UseClaimCheck<FileShareClaimCheck, SystemJsonClaimCheckSerializer>();
-var storagePath = new SolutionDirectoryFinder().GetDirectory("storage");
-claimCheck.BasePath(storagePath);
 endpointConfiguration.UseSerialization<SystemJsonSerializer>();
 endpointConfiguration.UseTransport(new LearningTransport());
 
-builder.UseNServiceBus(endpointConfiguration);
-var host = builder.Build();
+var claimCheck = endpointConfiguration.UseClaimCheck<FileShareClaimCheck, SystemJsonClaimCheckSerializer>();
+var storagePath = new SolutionDirectoryFinder().GetDirectory("storage");
+claimCheck.BasePath(storagePath);
 
+builder.UseNServiceBus(endpointConfiguration);
+
+var host = builder.Build();
 await host.StartAsync();
 
 Console.WriteLine("Press any key to exit");
 Console.ReadKey();
 
 await host.StopAsync();
-
