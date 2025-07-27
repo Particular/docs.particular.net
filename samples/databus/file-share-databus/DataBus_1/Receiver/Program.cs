59f20840b4dddc5cf417fe87b53347e74c03920b
Christian <christian.quiros+github@gmail.com>
2025-07-27 17:16:46 -0600

    Remove InputLoopService from DataBus_1

--- samples/databus/file-share-databus/DataBus_1/Receiver/Program.cs
+++ samples/databus/file-share-databus/DataBus_1/Receiver/Program.cs
@@ -3,7 +3,6 @@ using Shared;
 using System;
 using Microsoft.Extensions.Hosting;
 
-
 Console.Title = "Receiver";
 var builder = Host.CreateApplicationBuilder(args);
 var endpointConfiguration = new EndpointConfiguration("Samples.ClaimCheck.Receiver");
@@ -13,9 +12,13 @@ claimCheck.BasePath(storagePath);
 endpointConfiguration.UseSerialization<SystemJsonSerializer>();
 endpointConfiguration.UseTransport(new LearningTransport());
 
-Console.WriteLine("Press any key, the application is starting");
-Console.ReadKey();
-Console.WriteLine("Starting...");
-
 builder.UseNServiceBus(endpointConfiguration);
-await builder.Build().RunAsync();
+var host = builder.Build();
+
+await host.StartAsync();
+
+Console.WriteLine("Press any key to exit");
+Console.ReadKey();
+
+await host.StopAsync();
+
