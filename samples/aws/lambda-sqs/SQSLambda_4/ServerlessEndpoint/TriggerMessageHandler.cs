16f730239d995ccf66091385efb81351eb9e9925
Jason Taylor <hello@jasontaylor.dev>
2025-07-30 10:33:01 +1000

    Update to .NET 10

---
+++ samples/aws/lambda-sqs/SQSLambda_4/ServerlessEndpoint/TriggerMessageHandler.cs
@@ -0,0 +1,14 @@
+﻿using NServiceBus.Logging;
+
+#region TriggerMessageHandler
+public class TriggerMessageHandler : IHandleMessages<TriggerMessage>
+{
+    static readonly ILog Log = LogManager.GetLogger<TriggerMessageHandler>();
+
+    public async Task Handle(TriggerMessage message, IMessageHandlerContext context)
+    {
+        Log.Info($"Handling {nameof(TriggerMessage)} in ServerlessEndpoint.");
+        await context.Send(new ResponseMessage());
+    }
+}
+#endregion
