725b38ac92d5316407216fcd5a0c8315ae57f486
Hope Svotwa <hope@geratin.net>
2025-07-31 12:32:43 +0200

    Datadog Logging sample for NServiceBus 10 #7320

---
+++ samples/logging/datadog/Core_10/Endpoint/LoadSimulator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using NServiceBus;
+
+// Simulates busy (almost no delay) / quiet time in a sine wave
+class LoadSimulator(IEndpointInstance endpointInstance, TimeSpan minimumDelay, TimeSpan idleDuration)
+{
+    CancellationTokenSource tokenSource = new();
+    TimeSpan idleDuration = TimeSpan.FromTicks(idleDuration.Ticks / 2);
+    Task fork;
+
+    public Task Start()
+    {
+        fork = Loop();
+        return Task.CompletedTask;
+    }
+
+    async Task Loop()
+    {
+        try
+        {
+            while (true)
+            {
+                await Work();
+                var delay = NextDelay();
+                await Task.Delay(delay, tokenSource.Token);
+            }
+        }
+        catch (OperationCanceledException)
+        {
+        }
+    }
+
+    int index;
+
+    TimeSpan NextDelay()
+    {
+        var angleInRadians = Math.PI / 180.0 * ++index;
+        var delay = TimeSpan.FromMilliseconds(idleDuration.TotalMilliseconds * Math.Sin(angleInRadians));
+        delay += idleDuration;
+        delay += minimumDelay;
+        return delay;
+    }
+
+    Task Work()
+    {
+        return endpointInstance.SendLocal(new SomeCommand());
+    }
+
+    public Task Stop()
+    {
+        tokenSource.Cancel();
+        return fork;
+    }
+}
