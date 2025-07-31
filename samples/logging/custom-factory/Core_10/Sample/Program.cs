c0d1a96102251b4d269365f6eb78e38d23e51858
Hope Svotwa <hope@geratin.net>
2025-07-31 12:30:31 +0200

    Custom Factory Logging sample for NServiceBus 10 #7319

---
+++ samples/logging/custom-factory/Core_10/Sample/Program.cs
@@ -0,0 +1,30 @@
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NServiceBus;
+using NServiceBus.Logging;
+
+var host = Host.CreateDefaultBuilder(args)
+    .UseConsoleLifetime()
+    .UseNServiceBus(_ =>
+    {
+        var loggerDefinition = LogManager.Use<ConsoleLoggerDefinition>();
+        loggerDefinition.Level(LogLevel.Info);
+
+        var endpointConfiguration = new EndpointConfiguration("Samples.Logging.CustomFactory");
+        endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+        endpointConfiguration.UseTransport<LearningTransport>();
+
+        return endpointConfiguration;
+    })
+    .Build();
+
+await host.StartAsync();
+
+var endpointInstance = host.Services.GetRequiredService<IMessageSession>();
+await endpointInstance.SendLocal(new MyMessage());
+
+Console.WriteLine("Press any key to exit");
+Console.ReadKey();
+
+await host.StopAsync();
