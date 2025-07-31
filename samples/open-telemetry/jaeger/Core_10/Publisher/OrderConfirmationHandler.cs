d0822fc3ec4125c9d9af1e99f7cbb48ae5a5f6e8
Jason Taylor <hello@jasontaylor.dev>
2025-07-31 11:51:44 +1000

    Create Core_10 Sample

---
+++ samples/open-telemetry/jaeger/Core_10/Publisher/OrderConfirmationHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using Shared;
+
+public class OrderConfirmationHandler : IHandleMessages<ConfirmOrderMessage>, IHandleMessages<DeclineOrderMessage>
+{
+    public async Task Handle(ConfirmOrderMessage message, IMessageHandlerContext context)
+    {
+        await Task.Delay(25, context.CancellationToken);
+        Console.WriteLine($"Order {message.OrderId} was confirmed.");
+    }
+
+    public async Task Handle(DeclineOrderMessage message, IMessageHandlerContext context)
+    {
+        await Task.Delay(25, context.CancellationToken);
+        Console.WriteLine($"Order {message.OrderId} was declined.");
+    }
+}
