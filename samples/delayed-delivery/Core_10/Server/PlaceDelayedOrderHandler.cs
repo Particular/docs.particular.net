14a5e04999c725cfb228187ed7fd7744448824c5
Bob Langley <bob.langley@particular.net>
2025-07-31 15:57:00 -0700

    Add delayed-delivery Core_10 sample for NServiceBus 10 with .NET 10 and alpha packages

---
+++ samples/delayed-delivery/Core_10/Server/PlaceDelayedOrderHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using Microsoft.Extensions.Logging;
+#region PlaceDelayedOrderHandler
+
+public class PlaceDelayedOrderHandler(ILogger<PlaceDelayedOrderHandler> logger) :
+    IHandleMessages<PlaceDelayedOrder>
+{
+
+    public Task Handle(PlaceDelayedOrder message, IMessageHandlerContext context)
+    {
+        logger.LogInformation("[Defer Message Delivery] Order for Product: {Product} placed with id: {Id}", message.Product, message.Id);
+        return Task.CompletedTask;
+    }
+}
+
+#endregion
