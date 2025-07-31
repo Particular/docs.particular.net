92736f8d6e9fc326ecf9a1209cfcf520ff6d241d
Jason Taylor <hello@jasontaylor.dev>
2025-07-31 10:44:31 +1000

    Cloned Core_9

---
+++ samples/open-telemetry/logging/Core_10/GenericHost/MyMessageHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+
+public class MyMessageHandler(ILogger<MyMessageHandler> logger) : IHandleMessages<MyMessage>
+{
+    public Task Handle(MyMessage message, IMessageHandlerContext context)
+    {
+        #region log-statement
+
+        logger.LogInformation("Received message #{Number}", message.Number);
+
+        #endregion
+
+        return Task.CompletedTask;
+    }
+}
