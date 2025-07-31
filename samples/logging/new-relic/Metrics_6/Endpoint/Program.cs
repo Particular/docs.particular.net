44a1de17f56893b5374e92d745911ae012e14733
Hope Svotwa <hope@geratin.net>
2025-07-31 12:27:47 +0200

    New Relic Logging sample for NServiceBus 10 #7325

---
+++ samples/logging/new-relic/Metrics_6/Endpoint/Program.cs
@@ -0,0 +1,43 @@
+using System;
+using Endpoint;
+using NServiceBus;
+
+var endpointConfiguration = new EndpointConfiguration(Console.Title = "TracingEndpoint");
+
+endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+endpointConfiguration.UseTransport<LearningTransport>();
+
+NewRelicMetrics.Setup(endpointConfiguration);
+
+var endpointInstance = await NServiceBus.Endpoint.Start(endpointConfiguration);
+
+#region newrelic-load-simulator
+
+var simulator = new LoadSimulator(endpointInstance, TimeSpan.Zero, TimeSpan.FromSeconds(10));
+simulator.Start();
+
+#endregion
+
+try
+{
+    Console.WriteLine("Endpoint started.");
+    Console.WriteLine("Press [ENTER] to send additional messages.");
+    Console.WriteLine("Press [ESC] to quit.");
+
+    while (true)
+    {
+        switch (Console.ReadKey(true).Key)
+        {
+            case ConsoleKey.Escape:
+                return;
+            case ConsoleKey.Enter:
+                await endpointInstance.SendLocal(new SomeCommand());
+                break;
+        }
+    }
+}
+finally
+{
+    await simulator.Stop();
+    await endpointInstance.Stop();
+}
