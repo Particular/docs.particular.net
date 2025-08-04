124e408f7543c949152c65bf835a9a5bf04bf2d4
Bob Langley <bob.langley@particular.net>
2025-08-04 12:27:28 -0700

    Create CosmosDB_4 version for NServiceBus v10
    - Updated to target .NET 10 with preview language version
    - Upgraded to latest alpha packages:
      • NServiceBus 10.0.0-alpha.2
      • NServiceBus.Extensions.Hosting 4.0.0-alpha.2
      • NServiceBus.Persistence.CosmosDB 4.0.0-alpha.2
    - Added prerelease.txt marker file
    - Updated Dockerfile for .NET 10 with multi-stage build
    - Set DOTNET_SYSTEM_NET_SECURITY_NOREVOCATIONCHECKBYDEFAULT environment variable for .NET 10 preview compatibility
    - Validated complete sample functionality with containerized testing and real Azure CosmosDB

---
+++ samples/cosmosdb/transactions/CosmosDB_4/Client/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Client";
+
+        var hostBuilder = Host.CreateDefaultBuilder()
+            .ConfigureServices(services =>
+            {
+                services.AddLogging(logging => logging.AddConsole());
+            })
+            .UseNServiceBus(context =>
+            {
+                var endpointConfiguration = new EndpointConfiguration("Samples.CosmosDB.Transactions.Client");
+                endpointConfiguration.UsePersistence<LearningPersistence>();
+                endpointConfiguration.UseTransport(new LearningTransport());
+                endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+                return endpointConfiguration;
+            });
+
+        var host = hostBuilder.Build();
+        await host.StartAsync();
+
+        var messageSession = host.Services.GetRequiredService<IMessageSession>();
+
+        Console.WriteLine("Press 'S' to send a StartOrder message to the server endpoint");
+        Console.WriteLine("Press any other key to exit");
+
+        while (true)
+        {
+            var key = Console.ReadKey();
+            Console.WriteLine();
+
+            var orderId = Guid.NewGuid();
+            var startOrder = new StartOrder
+            {
+                OrderId = orderId
+            };
+            if (key.Key == ConsoleKey.S)
+            {
+                await messageSession.Send("Samples.CosmosDB.Transactions.Server", startOrder);
+                Console.WriteLine($"StartOrder Message sent to Server with OrderId {orderId}");
+                continue;
+            }
+            break;
+        }
+
+        await host.StopAsync();
+    }
+}
