a975b477e471ff3daede2516b6c684774000221b
Mauro Servienti <mauro.servienti@gmail.com>
2025-08-05 17:32:07 +0200

    Update outbox/sql sample to NServiceBus v10 (#7610)
    * Remove the imput loop service from the v9 sample
    
    * Add v10 sample
    
    * It cannot be plural
    
    * tweaks

---
+++ samples/outbox/sql/Core_10/Receiver/OrderSubmittedHandler.cs
@@ -0,0 +1,44 @@
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using NServiceBus;
+using Microsoft.Extensions.Logging;
+
+sealed class OrderSubmittedHandler(ILogger<OrderSubmittedHandler> logger) :
+    IHandleMessages<OrderSubmitted>
+{
+    public async Task Handle(OrderSubmitted message, IMessageHandlerContext context)
+    {
+        logger.LogInformation("Order {OrderId} worth {OrderValue} submitted",
+            message.OrderId,
+            message.Value
+        );
+
+        #region StoreUserData
+
+        var session = context.SynchronizedStorageSession.SqlPersistenceSession();
+
+        var sql = @"insert into receiver.SubmittedOrder
+                                    (Id, Value)
+                        values      (@Id, @Value)";
+
+        await using (var command = new SqlCommand(
+                         cmdText: sql,
+                         connection: (SqlConnection)session.Connection,
+                         transaction: (SqlTransaction)session.Transaction))
+        {
+            var parameters = command.Parameters;
+            parameters.AddWithValue("Id", message.OrderId);
+            parameters.AddWithValue("Value", message.Value);
+            await command.ExecuteNonQueryAsync(context.CancellationToken);
+        }
+
+        #endregion
+
+        #region Reply
+
+        var orderAccepted = new OrderAccepted(OrderId: message.OrderId);
+        await context.Reply(orderAccepted);
+
+        #endregion
+    }
+}
---
+++ samples/outbox/sql/Core_10/Shared/OrderSubmitted.cs
@@ -0,0 +1,4 @@
+﻿using System;
+using NServiceBus;
+
+public record OrderSubmitted(Guid OrderId, decimal Value) : IEvent;
