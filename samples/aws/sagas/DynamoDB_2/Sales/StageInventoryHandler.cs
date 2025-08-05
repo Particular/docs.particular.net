b6ca01bb3aa2e08bb6ba769141db8c1f984c37bb
Christian <christian.quiros+github@gmail.com>
2025-08-05 14:20:42 -0600

    Add baseline DynamoDB_2 copy from DynamoDB_1

---
+++ samples/aws/sagas/DynamoDB_2/Sales/StageInventoryHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+
+public class StageInventoryHandler : IHandleMessages<OrderReceived>
+{
+  ILog log = LogManager.GetLogger<StageInventoryHandler>();
+
+  public async Task Handle(OrderReceived message, IMessageHandlerContext context)
+  {
+    log.Info($"Staging inventory for order {message.OrderId}.");
+
+    await Task.Delay(TimeSpan.FromSeconds(Random.Shared.Next(2, 5)), context.CancellationToken);
+
+    await context.Publish(new InventoryStaged { OrderId = message.OrderId });
+  }
+}
