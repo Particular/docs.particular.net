92953df0f9e60ab6d979802db4ea2cbce70cc4d7
Nick Gallegos <nick.gallegos@particular.net>
2025-08-06 16:57:30 -0600

    Added nsb10 sample

---
+++ samples/azure/webjob-host/Core_10/Receiver/SimulateWorkHostedService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using NServiceBus;
+
+class SimulateWorkHostedService : IHostedService
+{
+    public SimulateWorkHostedService(IMessageSession messageSession)
+    {
+        this.messageSession = messageSession;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        worker = SimulateWork(cancellationTokenSource.Token);
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        cancellationTokenSource.Cancel();
+        return worker;
+    }
+
+    async Task SimulateWork(CancellationToken cancellationToken)
+    {
+        try
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                // sending here to simulate work
+                await messageSession.SendLocal(new MyMessage(), cancellationToken: cancellationToken);
+
+                await Task.Delay(1000, cancellationToken);
+            }
+        }
+        catch (OperationCanceledException)
+        {
+            // graceful shutdown
+        }
+    }
+
+    readonly IMessageSession messageSession;
+    readonly CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+    Task worker;
+}
