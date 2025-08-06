ef83cac9dfccc04e3314c86e207c810bff84a7f4
Mauro Servienti <mauro.servienti@gmail.com>
2025-08-06 17:22:27 +0200

    Add NServiceBus 10 sample (#7615)

---
+++ samples/outbox/cosmosdb/Core_10/Sample/MyHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+public class MyHandler : IHandleMessages<MyMessage>
+{
+    #region Handler
+    public async Task Handle(MyMessage message, IMessageHandlerContext context)
+    {
+        Console.WriteLine($"Processing MessageId {context.MessageId}");
+        await Task.CompletedTask;
+    }
+    #endregion
+}
