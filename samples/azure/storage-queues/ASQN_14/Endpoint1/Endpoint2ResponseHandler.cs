df591e2a02647d656af5762a84ad03f52d7e3e08
Nick Gallegos <nick.gallegos@particular.net>
2025-08-06 15:49:03 -0600

    Add nsb10 sample

---
+++ samples/azure/storage-queues/ASQN_14/Endpoint1/Endpoint2ResponseHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using Microsoft.Extensions.Logging;
+using Shared;
+
+public sealed class Endpoint2ResponseHandler(ILogger<Endpoint2ResponseHandler> logger)
+    : IHandleMessages<MyResponse>
+{
+    public Task Handle(MyResponse message, IMessageHandlerContext context)
+    {
+        logger.LogInformation("Received MyResponse: {Property}", message.Property);
+        return Task.CompletedTask;
+    }
+}
---
+++ samples/azure/storage-queues/ASQN_14/Endpoint2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Extensions.Hosting;
+using NServiceBus;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+const string endpointName = "Samples-Azure-StorageQueues-Endpoint2";
+Console.Title = endpointName;
+
+var endpointConfiguration = new EndpointConfiguration(endpointName);
+endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+endpointConfiguration.EnableInstallers();
+
+var transport = new AzureStorageQueueTransport("UseDevelopmentStorage=true")
+{
+    QueueNameSanitizer = BackwardsCompatibleQueueNameSanitizer.WithMd5Shortener
+};
+
+var routingSettings = endpointConfiguration.UseTransport(transport);
+routingSettings.DisablePublishing();
+
+endpointConfiguration.UsePersistence<LearningPersistence>();
+builder.UseNServiceBus(endpointConfiguration);
+
+await builder.Build().RunAsync();
