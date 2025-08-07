74806e9d19711540dca642ff824b921dae6567c4
Nick Gallegos <nick.gallegos@particular.net>
2025-08-07 09:41:28 -0600

    Created nsb10 sample

---
+++ samples/azure-functions/service-bus-worker/ASBFunctionsWorker_7/AzureFunctions.ASBTrigger.Worker/FollowupMessageHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+
+#region FollowupMessageHandler
+
+public class FollowupMessageHandler(ILogger<FollowupMessageHandler> logger) : IHandleMessages<FollowupMessage>
+{
+
+    public Task Handle(FollowupMessage message, IMessageHandlerContext context)
+    {
+        logger.LogWarning("Handling {MessageType} in {HandlerType}.", nameof(FollowupMessage), nameof(FollowupMessageHandler));
+        return Task.CompletedTask;
+    }
+}
+
+#endregion
---
+++ samples/azure-functions/service-bus-worker/ASBFunctionsWorker_7/AzureFunctions.ASBTrigger.Worker/TriggerMessageHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+
+#region TriggerMessageHandler
+
+public class TriggerMessageHandler(ILogger<TriggerMessageHandler> logger) : IHandleMessages<TriggerMessage>
+{
+
+    public Task Handle(TriggerMessage message, IMessageHandlerContext context)
+    {
+        logger.LogWarning("Handling {MessageType} in {HandlerType}", nameof(TriggerMessage), nameof(TriggerMessageHandler));
+
+        return context.SendLocal(new FollowupMessage());
+    }
+}
+
+#endregion
