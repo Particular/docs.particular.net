fd73c2f638019c4a9b5ec752957fefa1b427cb49
Warwick Schroeder <warwick.schroeder@particular.net>
2025-08-08 14:53:20 +0800

    - Update to use NSB10 - Refactor shared messages - add 'prerelease' file - review and update sample.md file - update tfm-mappings.txt file to include .net10.0-windows

---
+++ samples/bridge/azure-service-bus-msmq-bridge/Bridge_4/MsmqEndpoint/OtherEventHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+using Shared;
+
+namespace MsmqEndpoint;
+
+public class OtherEventHandler : IHandleMessages<OtherEvent>
+{
+    static ILog log = LogManager.GetLogger<OtherEventHandler>();
+
+    public Task Handle(OtherEvent message, IMessageHandlerContext context)
+    {
+        log.Info($"Received OtherEvent: {message.Property}");
+        log.Info("Conversation Done");
+        return Task.CompletedTask;
+    }
+}
