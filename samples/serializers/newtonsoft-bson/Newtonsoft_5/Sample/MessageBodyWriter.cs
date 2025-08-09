c7a8b18f922d4c31f2639cfd11f44cf6f435835f
Tamara Rivera <tamita.rivera@gmail.com>
2025-08-08 18:42:22 -0700

    Add NSB 10 version of the sample

---
+++ samples/serializers/newtonsoft-bson/Newtonsoft_5/Sample/MessageBodyWriter.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using NServiceBus.MessageMutator;
+
+#region mutator
+public class MessageBodyWriter(ILogger<MessageBodyWriter> logger) :
+    IMutateIncomingTransportMessages
+{
+    public Task MutateIncoming(MutateIncomingTransportMessageContext context)
+    {
+        var bodyAsString = Encoding.UTF8
+            .GetString(context.Body.ToArray());
+
+        logger.LogInformation   ("Serialized Message Body:");
+        logger.LogInformation(bodyAsString);
+
+        return Task.CompletedTask;
+    }
+}
+#endregion
---
+++ samples/serializers/newtonsoft-bson/Newtonsoft_5/Sample/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json.Bson;
+using NServiceBus;
+using NServiceBus.MessageMutator;
+
+Console.Title = "ExternalBson";
+
+var builder = Host.CreateApplicationBuilder(args);
+
+#region config
+
+var endpointConfiguration = new EndpointConfiguration("Samples.Serialization.ExternalBson");
+
+var serialization = endpointConfiguration.UseSerialization<NewtonsoftJsonSerializer>();
+serialization.ContentTypeKey("application/bson");
+serialization.ReaderCreator(stream => new BsonDataReader(stream));
+serialization.WriterCreator(stream => new BsonDataWriter(stream));
+
+#endregion
+
+#region registermutator
+builder.Services.AddSingleton<MessageBodyWriter>();
+
+// Then later get it from the service provider when needed
+var serviceProvider = builder.Services.BuildServiceProvider();
+var messageBodyWriter = serviceProvider.GetRequiredService<MessageBodyWriter>();
+endpointConfiguration.RegisterMessageMutator(messageBodyWriter);
+
+#endregion
+
+endpointConfiguration.UseTransport(new LearningTransport());
+builder.UseNServiceBus(endpointConfiguration);
+
+var host = builder.Build();
+await host.StartAsync();
+
+var messageSession = host.Services.GetRequiredService<IMessageSession>();
+
+#region message
+
+var message = new CreateOrder
+{
+    OrderId = 9,
+    Date = DateTime.Now,
+    CustomerId = 12,
+    OrderItems =
+    [
+        new OrderItem
+        {
+            ItemId = 6,
+            Quantity = 2
+        },
+        new OrderItem
+        {
+            ItemId = 5,
+            Quantity = 4
+        }
+
+    ]
+};
+
+#endregion
+
+await messageSession.SendLocal(message);
+Console.WriteLine("Order Sent");
+
+await host.StopAsync();
