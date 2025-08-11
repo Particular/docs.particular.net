d7c4a3733b2c71ea0c9e577ff55d170c1be282f4
Nick Gallegos <nick.gallegos@particular.net>
2025-08-11 10:51:52 -0600

    Add NSB 10 RabbitMQ sample

---
+++ samples/rabbitmq/simple/Rabbit_11/Receiver/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Hosting;
+
+Console.Title = "SimpleReceiver";
+var builder = Host.CreateApplicationBuilder(args);
+
+var endpointConfiguration = new EndpointConfiguration("Samples.RabbitMQ.SimpleReceiver");
+var transport = endpointConfiguration.UseTransport<RabbitMQTransport>();
+endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+transport.UseConventionalRoutingTopology(QueueType.Quorum);
+transport.ConnectionString("host=localhost");
+endpointConfiguration.EnableInstallers();
+
+builder.UseNServiceBus(endpointConfiguration);
+await builder.Build().RunAsync();
---
+++ samples/rabbitmq/simple/Rabbit_11/Shared/MyEvent.cs
@@ -0,0 +1,5 @@
+﻿namespace Shared;
+
+public class MyEvent : IEvent
+{
+}
