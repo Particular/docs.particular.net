d42914e013d8d83b6a9755141f599ce0fa5f8319
Nick Gallegos <nick.gallegos@particular.net>
2025-08-11 10:54:49 -0600

    Add NServiceBus 10 RabbitMQ Transport sample (#7711)

---
+++ samples/rabbitmq/simple/Rabbit_11/Receiver/MyCommandHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace Receiver;
+
+using Microsoft.Extensions.Logging;
+using Shared;
+
+public class MyCommandHandler(ILogger<MyCommandHandler> logger) : IHandleMessages<MyCommand>
+{
+    public Task Handle(MyCommand commandMessage, IMessageHandlerContext context)
+    {
+        logger.LogInformation("Hello from {HandlerType}", nameof(MyCommandHandler));
+
+        return Task.CompletedTask;
+    }
+}
---
+++ samples/rabbitmq/simple/Rabbit_11/Receiver/MyEventHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace Receiver;
+
+using Microsoft.Extensions.Logging;
+using Shared;
+
+public class MyEventHandler(ILogger<MyEventHandler> logger) : IHandleMessages<MyEvent>
+{
+    public Task Handle(MyEvent eventMessage, IMessageHandlerContext context)
+    {
+        logger.LogInformation("Hello from {HandlerType}", nameof(MyEventHandler));
+
+        return Task.CompletedTask;
+    }
+}
