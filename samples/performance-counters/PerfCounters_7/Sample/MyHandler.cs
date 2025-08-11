fddc83edba5dcc7527c17155aab87ff5c06a2273
Jayanthi <jayanthi.sourirajan@particular.net>
2025-08-11 13:45:41 -0700

    Nsb10 samples performance counters (#7707)
    Updated sample to nsb10
    
    Co-authored-by: Tamara Rivera <tamita.rivera@gmail.com>

---
+++ samples/performance-counters/PerfCounters_7/Sample/MyHandler.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Logging;
+
+#region handler
+
+public class MyHandler(ILogger<MyHandler> logger) : IHandleMessages<MyMessage>
+{
+    public async Task Handle(MyMessage message, IMessageHandlerContext context)
+    {
+        var sleepTime = Random.Shared.Next(1, 1000);
+        await Task.Delay(sleepTime, context.CancellationToken);
+        logger.LogInformation("Hello from MyHandler. Slept for {SleepTime} ms", sleepTime);
+    }
+}
+
+#endregion
---
+++ samples/performance-counters/PerfCounters_7/Sample/Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+Console.Title = "PerfCounters";
+var builder = Host.CreateApplicationBuilder(args);
+var endpointConfiguration = new EndpointConfiguration("Samples.PerfCounters");
+endpointConfiguration.EnableInstallers();
+endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+endpointConfiguration.UseTransport(new LearningTransport());
+endpointConfiguration.SendFailedMessagesTo("error");
+
+#region enable-counters
+var performanceCounters = endpointConfiguration.EnableWindowsPerformanceCounters();
+performanceCounters.EnableSLAPerformanceCounters(TimeSpan.FromSeconds(100));
+#endregion
+
+Console.WriteLine("Press any key to send 10 messages with random sleep");
+Console.ReadKey();
+
+builder.UseNServiceBus(endpointConfiguration);
+var host = builder.Build();
+await host.StartAsync();
+
+var messageSession = host.Services.GetRequiredService<IMessageSession>();
+
+while (true)
+{
+    var key = Console.ReadKey();
+    Console.WriteLine();
+
+    if (key.Key != ConsoleKey.Enter)
+    {
+        break;
+    }
+
+    for (var i = 0; i < 10; i++)
+    {
+        var myMessage = new MyMessage();
+        await messageSession.SendLocal(myMessage);
+    }
+
+    Console.WriteLine("10 messages sent.");
+}
+
+await host.StopAsync();
+
