f361ede2cbbb78c465e1392a9fd02c6c83d17012
Tamara Rivera <tamita.rivera@gmail.com>
2025-08-11 12:33:56 -0700

    Add NSB 10 version of the sample

---
+++ samples/serializers/system-json/Core_10/Sample/CreateOrderHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+public class CreateOrderHandler (ILogger<CreateOrderHandler> logger):
+    IHandleMessages<CreateOrder>
+{
+    public Task Handle(CreateOrder message, IMessageHandlerContext context)
+    {
+        logger.LogInformation("Order received");
+        return Task.CompletedTask;
+    }
+}
---
+++ samples/serializers/system-json/Core_10/Sample/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NServiceBus;
+using System;
+
+Console.Title = "SystemJson";
+
+var builder = Host.CreateApplicationBuilder(args);
+
+var endpointConfiguration = new EndpointConfiguration("Samples.Serialization.SystemJson");
+
+#region config
+
+endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+
+#endregion
+
+endpointConfiguration.UseTransport(new LearningTransport());
+builder.UseNServiceBus(endpointConfiguration);
+
+var host = builder.Build();
+await host.StartAsync();
+
+var messageSession = host.Services.GetRequiredService<IMessageSession>();
+
+#region message
+
+var message = new CreateOrder
+{
+    OrderId = 9,
+    Date = DateTime.Now,
+    CustomerId = 12,
+    OrderItems =
+    [
+        new OrderItem()
+        {
+            ItemId = 6,
+            Quantity = 2
+        },
+
+        new OrderItem()
+        {
+            ItemId = 5,
+            Quantity = 4
+        }
+
+    ]
+};
+await messageSession.SendLocal(message);
+
+#endregion
+
+Console.WriteLine("Order Sent");
+
+await host.StopAsync();
