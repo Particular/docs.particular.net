c6d4a6cdca6c94d44fdd243ae2867c957d884f9b
Ryan Thomas <ryan.thomas@particular.net>
2025-08-12 14:21:41 -0500

    Add NSB10 snippets for NServiceBus.Callbacks.Testing

---
+++ Snippets/CallbacksTesting/CallbacksTesting_6/Int/Message.cs
@@ -0,0 +1,9 @@
+﻿namespace CallbacksTesting.Int
+{
+    using NServiceBus;
+
+    public class Message :
+        IMessage
+    {
+    }
+}
---
+++ Snippets/CallbacksTesting/CallbacksTesting_6/Object/Usage.cs
@@ -0,0 +1,58 @@
+﻿namespace CallbacksTesting.Object
+{
+    using System.Threading.Tasks;
+    using NServiceBus;
+    using NServiceBus.Callbacks.Testing;
+
+    class Usage
+    {
+        async Task ObjectCallbackTesting()
+        {
+            #region ObjectCallbackTesting
+
+            var request = new Message();
+            var simulatedResponse = new ResponseMessage();
+
+            var session = new TestableCallbackAwareSession();
+            session.When(
+                matcher: (Message message) =>
+                {
+                    return message == request;
+                },
+                response: simulatedResponse);
+
+            var result = await session.Request<ResponseMessage>(request);
+
+            Assert.AreEqual(simulatedResponse, result);
+
+            #endregion
+
+        }
+
+        async Task ObjectCallbackTestingWithOptions()
+        {
+            #region ObjectCallbackTestingWithOptions
+
+            var request = new Message();
+            var simulatedResponse = new ResponseMessage();
+
+            var session = new TestableCallbackAwareSession();
+            session.When(
+                matcher: (Message message, SendOptions options) =>
+                {
+                    return message == request && options.GetHeaders()
+                               .ContainsKey("Simulated.Header");
+                },
+                response: simulatedResponse);
+
+            var sendOptions = new SendOptions();
+            sendOptions.SetHeader("Simulated.Header", "value");
+            var result = await session.Request<ResponseMessage>(request, sendOptions);
+
+            Assert.AreEqual(simulatedResponse, result);
+
+            #endregion
+        }
+    }
+
+}
