a6be96a454154b45a24110b7a5e3dd9445aafccd
Ryan Thomas <ryan.thomas@particular.net>
2025-08-13 17:51:34 -0500

    Remove namespace declaration to make it consistent with rest of snippets project. (#7733)

--- Snippets/ASQ/ASQN_14/AccessToNativeMessage.cs
+++ Snippets/ASQ/ASQN_14/AccessToNativeMessage.cs
@@ -3,27 +3,24 @@ using System.Threading.Tasks;
 using Azure.Storage.Queues.Models;
 using NServiceBus.Pipeline;
 
-namespace ASQN_13
+public class AccessToNativeMessage
 {
-    public class AccessToNativeMessage
+    #region access-native-incoming-message
+
+    class DoNotAttemptMessageProcessingIfMessageIsNotLocked : Behavior<ITransportReceiveContext>
     {
-        #region access-native-incoming-message
-
-        class DoNotAttemptMessageProcessingIfMessageIsNotLocked : Behavior<ITransportReceiveContext>
+        public override Task Invoke(ITransportReceiveContext context, Func<Task> next)
         {
-            public override Task Invoke(ITransportReceiveContext context, Func<Task> next)
+            var NextVisibleOnUtc = context.Extensions.Get<QueueMessage>().NextVisibleOn;
+
+            if (NextVisibleOnUtc <= DateTime.UtcNow)
             {
-                var NextVisibleOnUtc = context.Extensions.Get<QueueMessage>().NextVisibleOn;
-
-                if (NextVisibleOnUtc <= DateTime.UtcNow)
-                {
-                    return next();
-                }
-
-                throw new Exception($"Message lock lost for MessageId {context.Message.MessageId} and it cannot be processed.");
+                return next();
             }
-        }
 
-        #endregion
+            throw new Exception($"Message lock lost for MessageId {context.Message.MessageId} and it cannot be processed.");
+        }
     }
+
+    #endregion
 }
