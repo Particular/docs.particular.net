d20e8ee55b5f47c2abb99f8202ff7ff2db14efd9
Ryan Thomas <ryan.thomas@particular.net>
2025-08-14 13:28:58 -0500

    Add NSB10 Version of CosmosDB Snippets

---
+++ Snippets/CosmosDB/CosmosDB_4/SharedTransactions.cs
@@ -0,0 +1,94 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+using NServiceBus;
+
+class MyMessage
+{
+    public string ItemId { get; set; }
+}
+
+class UsageHandler : IHandleMessages<MyMessage>
+{
+    ToDoActivity test1 = new ToDoActivity();
+    ToDoActivity test2 = new ToDoActivity();
+    ToDoActivity test3 = new ToDoActivity();
+
+    public class ToDoActivity
+    {
+        public string id { get; set; }
+    }
+
+    #region CosmosDBHandlerSharedTransaction
+    public Task Handle(MyMessage message, IMessageHandlerContext context)
+    {
+        //setup the items for the batch...
+
+        var session = context.SynchronizedStorageSession.CosmosPersistenceSession();
+
+        session.Batch
+                .CreateItem(test1)
+                .ReplaceItem(test2.id, test2)
+                .UpsertItem(test3)
+                .DeleteItem("/item/id");
+
+        return Task.CompletedTask;
+    }
+    #endregion
+}
+
+#region CosmosDB-TransactionalBatchRegisteredWithDependencyInjectionResolvedInHandler
+
+class MyHandler : IHandleMessages<MyMessage>
+{
+    public MyHandler(ICosmosStorageSession storageSession)
+    {
+        transactionalBatch = storageSession.Batch;
+    }
+
+    public Task Handle(MyMessage message, IMessageHandlerContext context)
+    {
+        transactionalBatch.DeleteItem(message.ItemId);
+
+        return Task.CompletedTask;
+    }
+
+    private readonly TransactionalBatch transactionalBatch;
+}
+
+#endregion
+
+#region CosmosDB-TransactionalBatchRegisteredWithDependencyInjectionResolvedInCustomType
+
+class MyCustomDependency
+{
+    public MyCustomDependency(ICosmosStorageSession storageSession)
+    {
+        transactionalBatch = storageSession.Batch;
+    }
+
+    public void DeleteItemInCosmos(string itemId)
+    {
+        transactionalBatch.DeleteItem(itemId);
+    }
+
+    private readonly TransactionalBatch transactionalBatch;
+}
+
+class MyHandlerWithCustomDependency : IHandleMessages<MyMessage>
+{
+    public MyHandlerWithCustomDependency(MyCustomDependency customDependency)
+    {
+        this.customDependency = customDependency;
+    }
+
+    public Task Handle(MyMessage message, IMessageHandlerContext context)
+    {
+        customDependency.DeleteItemInCosmos(message.ItemId);
+
+        return Task.CompletedTask;
+    }
+
+    private readonly MyCustomDependency customDependency;
+}
+
+#endregion
---
+++ Snippets/CosmosDB/CosmosDB_4/Usage.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Fluent;
+using NServiceBus;
+
+class Usage
+{
+    Usage(EndpointConfiguration endpointConfiguration)
+    {
+        #region CosmosDBUsage
+
+        endpointConfiguration.UsePersistence<CosmosPersistence>()
+            .CosmosClient(new CosmosClient("ConnectionString"));
+
+        #endregion
+
+        #region CosmosDBDatabaseName
+
+        endpointConfiguration.UsePersistence<CosmosPersistence>()
+            .CosmosClient(new CosmosClient("ConnectionString"))
+            .DatabaseName("DatabaseName");
+
+        #endregion
+
+        #region CosmosDBDefaultContainer
+
+        endpointConfiguration.UsePersistence<CosmosPersistence>()
+            .CosmosClient(new CosmosClient("ConnectionString"))
+            .DefaultContainer(
+                containerName: "ContainerName",
+                partitionKeyPath: "/partition/key/path");
+
+        #endregion
+
+        #region CosmosDBDisableContainerCreation
+
+        endpointConfiguration.UsePersistence<CosmosPersistence>()
+            .CosmosClient(new CosmosClient("ConnectionString"))
+            .DefaultContainer(
+                containerName: "ContainerName",
+                partitionKeyPath: "/partition/key/path")
+            .DisableContainerCreation();
+
+        #endregion
+
+        #region CosmosDBOutboxCleanup
+
+        var outbox = endpointConfiguration.EnableOutbox();
+        outbox.TimeToKeepOutboxDeduplicationData(TimeSpan.FromDays(7));
+
+        #endregion
+
+        #region CosmosDBMigrationMode
+
+        var persistence = endpointConfiguration.UsePersistence<CosmosPersistence>();
+        var sagas = persistence.Sagas();
+        sagas.EnableMigrationMode();
+
+        #endregion
+
+        #region CosmosDBConfigureThrottlingWithClientOptions
+
+        endpointConfiguration.UsePersistence<CosmosPersistence>()
+            .CosmosClient(new CosmosClient("ConnectionString", new CosmosClientOptions
+            {
+                MaxRetryWaitTimeOnRateLimitedRequests = TimeSpan.FromSeconds(30),
+                MaxRetryAttemptsOnRateLimitedRequests = 9
+            }));
+
+        #endregion
+
+        #region CosmosDBConfigureThrottlingWithBuilder
+
+        var cosmosClientBuilder = new CosmosClientBuilder("ConnectionString")
+           .WithThrottlingRetryOptions(
+               maxRetryWaitTimeOnThrottledRequests: TimeSpan.FromSeconds(30),
+               maxRetryAttemptsOnThrottledRequests: 9
+           );
+
+        endpointConfiguration.UsePersistence<CosmosPersistence>()
+            .CosmosClient(cosmosClientBuilder.Build());
+
+        #endregion
+
+        #region UsePessimisticLocking
+
+        var sagaPersistenceConfiguration = persistence.Sagas();
+        sagaPersistenceConfiguration.UsePessimisticLocking();
+
+        #endregion
+
+        #region PessimisticLeaseLockDuration
+
+        var pessimisticLockingConfiguration = sagaPersistenceConfiguration.UsePessimisticLocking();
+        pessimisticLockingConfiguration.SetLeaseLockTime(TimeSpan.FromMilliseconds(500));
+
+        #endregion
+
+        #region PessimisticLeaseLockAcquisitionTimeout
+
+        pessimisticLockingConfiguration.SetLeaseLockAcquisitionTimeout(TimeSpan.FromMilliseconds(500));
+
+        #endregion
+
+        #region PessimisticLeaseLockAcquisitionMinMaxRefreshDelay
+
+        pessimisticLockingConfiguration.SetLeaseLockAcquisitionMinimumRefreshDelay(TimeSpan.FromMilliseconds(50));
+        pessimisticLockingConfiguration.SetLeaseLockAcquisitionMaximumRefreshDelay(TimeSpan.FromMilliseconds(100));
+
+        #endregion
+    }
+}
