85a11c28673fdb50c31738f4b7e9e9b995a57a06
Ryan Thomas <ryan.thomas@particular.net>
2025-08-14 14:44:27 -0500

    Add NSB10 snippets for FileShareDataBus

---
+++ Snippets/FileShareDataBus/DataBus_2/Usage.cs
@@ -0,0 +1,14 @@
+﻿using NServiceBus;
+
+class Usage
+{
+    Usage(NServiceBus.EndpointConfiguration endpointConfiguration, string databusPath)
+    {
+        #region FileShareDataBus
+
+        var claimCheck = endpointConfiguration.UseClaimCheck<FileShareClaimCheck, SystemJsonClaimCheckSerializer>();
+        claimCheck.BasePath(databusPath);
+
+        #endregion
+    }
+}
