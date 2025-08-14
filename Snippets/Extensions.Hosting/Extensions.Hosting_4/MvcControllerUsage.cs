a902e6a2617f7a2af40bfbe866f859aa2383bc6b
Ryan Thomas <ryan.thomas@particular.net>
2025-08-14 14:36:41 -0500

    Add NSB10 snippets for Extensions.Hosting

---
+++ Snippets/Extensions.Hosting/Extensions.Hosting_4/MvcControllerUsage.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NServiceBus;
+
+public class MvcControllerUsage
+{
+    #region mvc-controller-usage
+    public class MvcController(IMessageSession messageSession) : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ViewResult> SendMessage()
+        {
+            await messageSession.Send(new MessageFromMvc());
+            return View();
+        }
+    }
+    #endregion
+
+    class MessageFromMvc
+    {
+    }
+}
---
+++ Snippets/Extensions.Hosting/Extensions.Hosting_4/WebApiControllerUsage.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NServiceBus;
+
+public class WebApiControllerUsage
+{
+    #region web-api-usage
+    [ApiController]
+    class WebApiController(IMessageSession messageSession) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            await messageSession.Send(new MessageFromWebApi());
+            return Ok("message was sent successfully");
+        }
+    }
+    #endregion
+
+    class MessageFromWebApi
+    {
+    }
+}
