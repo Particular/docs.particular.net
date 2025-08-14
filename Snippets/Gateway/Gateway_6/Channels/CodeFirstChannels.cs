4e907919c0a2ff5b857585235623d1681d59ce82
Ryan Thomas <ryan.thomas@particular.net>
2025-08-14 14:48:11 -0500

    Add NSB10 snippets for Gateway

---
+++ Snippets/Gateway/Gateway_6/Channels/CodeFirstChannels.cs
@@ -0,0 +1,20 @@
+﻿using NServiceBus;
+using NServiceBus.Gateway;
+
+public class CodeFirstChannels
+{
+    public CodeFirstChannels()
+    {
+        var endpointConfiguration = new EndpointConfiguration("MyEndpoint");
+
+        #region CodeFirstChannels
+
+        var config = new NonDurableDeduplicationConfiguration();
+        var gatewayConfig = endpointConfiguration.Gateway(config);
+
+        gatewayConfig.AddReceiveChannel("http://Headquarter.mycorp.com/", isDefault: true);
+        gatewayConfig.AddReceiveChannel("http://Headquarter.myotherdomain.com/", maxConcurrency: 10);
+
+        #endregion
+    }
+}
