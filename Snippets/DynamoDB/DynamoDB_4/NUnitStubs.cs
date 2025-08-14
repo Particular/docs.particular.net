8388a663c3d2fc3c5161bd55e428722c211056f8
Ryan Thomas <ryan.thomas@particular.net>
2025-08-14 14:31:36 -0500

    Add NSB10 snippets for DynamoDB

---
+++ Snippets/DynamoDB/DynamoDB_4/NUnitStubs.cs
@@ -0,0 +1,28 @@
+﻿namespace DynamoDB_4
+{
+    using Amazon.DynamoDBv2.Model;
+
+    internal class Assert
+    {
+        internal static void That(IReadOnlyCollection<TransactWriteItem> transactWriteItems, object value)
+        {
+            return;
+        }
+    }
+    internal class Has
+    {
+        public static Count Count { get; internal set; }
+    }
+
+    internal class Count
+    {
+        internal bool EqualTo(int v)
+        {
+            return true;
+        }
+    }
+
+    internal class TestAttribute : Attribute
+    {
+    }
+}
---
+++ Snippets/DynamoDB/DynamoDB_4/OutboxConfig.cs
@@ -0,0 +1,31 @@
+﻿namespace DynamoDB_4;
+
+using NServiceBus;
+
+public class OutboxConfig
+{
+    void ConfigureSagaTable(EndpointConfiguration endpointConfiguration)
+    {
+        #region DynamoOutboxTableConfiguration
+
+        var outboxConfiguration = endpointConfiguration.EnableOutbox();
+        outboxConfiguration.UseTable(new TableConfiguration
+        {
+            TableName = "MyOutboxTable",
+            PartitionKeyName = "MyOutboxPartitionKey",
+            SortKeyName = "MyOutboxSortKey",
+            TimeToLiveAttributeName = "MyOutboxTtlAttribute"
+        });
+        #endregion
+    }
+
+    void CleanupConfig(EndpointConfiguration endpointConfiguration)
+    {
+        #region DynamoDBOutboxCleanup
+
+        var outboxConfiguration = endpointConfiguration.EnableOutbox();
+        outboxConfiguration.SetTimeToKeepDeduplicationData(TimeSpan.FromDays(14));
+
+        #endregion
+    }
+}
