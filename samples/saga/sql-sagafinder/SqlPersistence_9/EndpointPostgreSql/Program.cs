c22b9ff4cba155941f5d8684968ebb0bd0d978ad
Jayanthi <jayanthi.sourirajan@particular.net>
2025-08-15 20:15:49 -0700

    Nsb10 samples sql sagafinder (#7601)
    Update to nsb10

---
+++ samples/saga/sql-sagafinder/SqlPersistence_9/EndpointPostgreSql/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Npgsql;
+using NpgsqlTypes;
+using NServiceBus;
+
+
+Console.Title = "PostgreSql";
+var builder = Host.CreateApplicationBuilder(args);
+var endpointConfiguration = new EndpointConfiguration("Samples.SqlSagaFinder.PostgreSql");
+endpointConfiguration.UseTransport(new LearningTransport());
+endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+endpointConfiguration.SendFailedMessagesTo("error");
+endpointConfiguration.EnableInstallers();
+#region PostgreSqlConfig
+
+var persistence = endpointConfiguration.UsePersistence<SqlPersistence>();
+var password = Environment.GetEnvironmentVariable("PostgreSqlPassword");
+if (string.IsNullOrWhiteSpace(password))
+{
+    throw new Exception("Could not extract 'PostgreSqlPassword' from Environment variables.");
+}
+var username = Environment.GetEnvironmentVariable("PostgreSqlUserName");
+if (string.IsNullOrWhiteSpace(username))
+{
+    throw new Exception("Could not extract 'PostgreSqlUserName' from Environment variables.");
+}
+var connection = $"Host=localhost;Username={username};Password={password};Database=NsbSamplesSqlSagaFinder";
+persistence.TablePrefix("Finder");
+var dialect = persistence.SqlDialect<SqlDialect.PostgreSql>();
+dialect.JsonBParameterModifier(
+    modifier: parameter =>
+    {
+        var npgsqlParameter = (NpgsqlParameter)parameter;
+        npgsqlParameter.NpgsqlDbType = NpgsqlDbType.Jsonb;
+    });
+persistence.ConnectionBuilder(
+    connectionBuilder: () =>
+    {
+        return new NpgsqlConnection(connection);
+    });
+
+var subscriptions = persistence.SubscriptionSettings();
+subscriptions.CacheFor(TimeSpan.FromMinutes(1));
+
+#endregion
+
+builder.UseNServiceBus(endpointConfiguration);
+
+
+var host = builder.Build();
+
+await host.StartAsync();
+
+var messageSession = host.Services.GetRequiredService<IMessageSession>();
+
+Console.WriteLine("Press 'enter' to send a message");
+while (true)
+{
+    var key = Console.ReadKey();
+    Console.WriteLine();
+
+    if (key.Key != ConsoleKey.Enter)
+    {
+        break;
+    }
+
+    var startOrder = new StartOrder
+    {
+        OrderId = "123"
+    };
+    await messageSession.SendLocal(startOrder);
+
+    Console.WriteLine($"StartOrder sent: {startOrder.OrderId}");
+}
+
+await host.StopAsync();
---
+++ samples/saga/sql-sagafinder/SqlPersistence_9/EndpointSqlServer/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NServiceBus;
+
+
+Console.Title = "SqlServer";
+var builder = Host.CreateApplicationBuilder(args);
+
+var endpointConfiguration = new EndpointConfiguration("Samples.SqlSagaFinder.SqlServer");
+endpointConfiguration.UseTransport(new LearningTransport());
+endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+endpointConfiguration.EnableInstallers();
+
+#region sqlServerConfig
+
+//for local instance or SqlExpress
+//var connectionString = @"Data Source=(localdb)\mssqllocaldb;Database=NsbSamplesSqlSagaFinder;Trusted_Connection=True;MultipleActiveResultSets=true";
+var connectionString = @"Server=localhost,1433;Initial Catalog=NsbSamplesSqlSagaFinder;User Id=SA;Password=yourStrong(!)Password;Encrypt=false";
+var persistence = endpointConfiguration.UsePersistence<SqlPersistence>();
+persistence.SqlDialect<SqlDialect.MsSqlServer>();
+persistence.ConnectionBuilder(
+    connectionBuilder: () =>
+    {
+        return new SqlConnection(connectionString);
+    });
+var subscriptions = persistence.SubscriptionSettings();
+subscriptions.CacheFor(TimeSpan.FromMinutes(1));
+
+#endregion
+
+await SqlHelper.EnsureDatabaseExists(connectionString);
+
+builder.UseNServiceBus(endpointConfiguration);
+
+var host = builder.Build();
+
+await host.StartAsync();
+
+var messageSession = host.Services.GetRequiredService<IMessageSession>();
+
+Console.WriteLine("Press 'enter' to send a message");
+while (true)
+{
+    var key = Console.ReadKey();
+    Console.WriteLine();
+
+    if (key.Key != ConsoleKey.Enter)
+    {
+        break;
+    }
+
+    var startOrder = new StartOrder
+    {
+        OrderId = "123"
+    };
+    await messageSession.SendLocal(startOrder);
+
+    Console.WriteLine($"StartOrder sent: {startOrder.OrderId}");
+}
+
+await host.StopAsync();
+
