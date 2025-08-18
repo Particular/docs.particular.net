f1ce3cbf3b7a3cfe00735b53aca8de7e0b67e0c2
Tamara Rivera <tamita.rivera@gmail.com>
2025-08-18 14:04:29 -0700

    Add NServiceBus 10 version of the LearningPersistence snippets

---
+++ Snippets/LearningPersistence/Core_10/Usage.cs
@@ -0,0 +1,23 @@
+﻿using NServiceBus;
+
+class Usage
+{
+    Usage(EndpointConfiguration endpointConfiguration)
+    {
+        #region LearningPersistence
+
+        endpointConfiguration.UsePersistence<LearningPersistence>();
+
+        #endregion
+    }
+
+    void SagaStorageDirectory(EndpointConfiguration endpointConfiguration)
+    {
+        #region LearningPersistenceSagaStorageDirectory
+
+        var persistence = endpointConfiguration.UsePersistence<LearningPersistence>();
+        persistence.SagaStorageDirectory("PathToStoreSagas");
+
+        #endregion
+    }
+}
