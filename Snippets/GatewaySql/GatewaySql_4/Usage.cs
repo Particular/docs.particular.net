51bb7e06830d5df1cb3046e9d08a98668d266d39
Ryan Thomas <ryan.thomas@particular.net>
2025-08-19 10:18:08 -0500

    Add NSB10 snippets for GatewaySql

---
+++ Snippets/GatewaySql/GatewaySql_4/Usage.cs
@@ -0,0 +1,42 @@
+﻿using NServiceBus;
+using Microsoft.Data.SqlClient;
+
+class Usage
+{
+    public void StandardUsage(EndpointConfiguration endpointConfiguration, string connectionString)
+    {
+        #region DefaultUsage
+        var gatewayConfiguration = new SqlGatewayDeduplicationConfiguration();
+        gatewayConfiguration.ConnectionBuilder(
+            connectionBuilder: () =>
+            {
+                return new SqlConnection(connectionString);
+            });
+
+        var gatewaySettings = endpointConfiguration.Gateway(gatewayConfiguration);
+        #endregion
+    }
+
+    public void SchemaAndTableName(EndpointConfiguration endpointConfiguration, string connectionString)
+    {
+        #region CustomizeSchemaAndTableName
+        var gatewayConfiguration = new SqlGatewayDeduplicationConfiguration();
+        gatewayConfiguration.Schema = "custom_schema";
+        gatewayConfiguration.TableName = "CustomTableName";
+        gatewayConfiguration.ConnectionBuilder(
+            connectionBuilder: () =>
+            {
+                return new SqlConnection(connectionString);
+            });
+
+        var gatewaySettings = endpointConfiguration.Gateway(gatewayConfiguration);
+        #endregion
+    }
+
+    public void WithEndpointName(SqlGatewayDeduplicationConfiguration gatewayConfiguration, string endpointName)
+    {
+        #region WithEndpointName
+        gatewayConfiguration.TableName = $"{endpointName}_GatewayDeduplication";
+        #endregion
+    }
+}
