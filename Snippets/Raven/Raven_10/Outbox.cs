b0d56e65e9f0137c95f6ea0f7dbdfb0b1a427896
Ryan Thomas <ryan.thomas@particular.net>
2025-08-19 12:11:57 -0500

    Add NSB10 Snippets for Raven

---
+++ Snippets/Raven/Raven_10/Outbox.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using NServiceBus;
+
+class Outbox
+{
+    Outbox(EndpointConfiguration endpointConfiguration)
+    {
+        #region OutboxRavendBTimeToKeep
+        var outboxSettings = endpointConfiguration.EnableOutbox();
+        outboxSettings.SetTimeToKeepDeduplicationData(TimeSpan.FromDays(7));
+        #endregion
+    }
+
+    void OutboxDisableCleanup(EndpointConfiguration endpointConfiguration)
+    {
+        #region OutboxRavendBDisableCleanup
+        var outbox = endpointConfiguration.EnableOutbox();
+        outbox.SetFrequencyToRunDeduplicationDataCleanup(Timeout.InfiniteTimeSpan);
+        #endregion
+    }
+
+    void Cleaner(EndpointConfiguration endpointConfiguration)
+    {
+        #region OutboxRavendBEnableCleaner
+        var outboxSettings = endpointConfiguration.EnableOutbox();
+        outboxSettings.SetFrequencyToRunDeduplicationDataCleanup(TimeSpan.FromMinutes(1));
+        #endregion
+    }
+}
