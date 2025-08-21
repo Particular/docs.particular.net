ad2cb65ca47c6cb18ab71ce7d61dad0d498e58d2
Tamara Rivera <tamita.rivera@gmail.com>
2025-08-20 17:51:11 -0700

    Add Add NServiceBus 10 version of the SystemJson snippets

---
+++ Snippets/SystemJson/Core_10/SystemJsonSerializerUsage.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+using NServiceBus;
+
+class SystemJsonSerializerUsage
+{
+    SystemJsonSerializerUsage(EndpointConfiguration endpointConfiguration)
+    {
+        #region SystemJsonSerialization
+
+        endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+
+        #endregion
+
+
+        #region SystemJsonContentType
+
+        endpointConfiguration.UseSerialization<SystemJsonSerializer>()
+            .ContentType("application/json; systemjson");
+
+        #endregion
+
+        #region SystemJsonOptions
+
+        var jsonSerializerOptions = new JsonSerializerOptions
+        {
+            WriteIndented = true
+        };
+
+        endpointConfiguration.UseSerialization<SystemJsonSerializer>()
+            .Options(jsonSerializerOptions);
+
+        #endregion
+    }
+}
