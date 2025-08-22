f0a477e55ada903b59d489c00b8575711df50ab4
Brandon Ording <bording@gmail.com>
2025-08-22 17:03:41 -0400

    Add UniformSession_5 project

---
+++ Snippets/UniformSession/UniformSession_5/EnableUniformSession.cs
@@ -0,0 +1,13 @@
+﻿using NServiceBus;
+
+public class EnableUniformSession
+{
+    public void Configure(EndpointConfiguration endpointConfiguration)
+    {
+        #region enable-uniformsession
+
+        endpointConfiguration.EnableUniformSession();
+
+        #endregion
+    }
+}
