8c1200039832793ed40e6a1de908c04c29ce7614
Brandon Ording <bording@gmail.com>
2025-08-22 17:32:01 -0400

    Add Xml/Core_10 project

---
+++ Snippets/Xml/Core_10/XmlSerializationIntegration.cs
@@ -0,0 +1,32 @@
+﻿using System.Xml.Linq;
+using NServiceBus;
+
+class XmlSerializationIntegration
+{
+    public void RawXml(EndpointConfiguration endpointConfiguration)
+    {
+        #region ConfigureRawXmlSerialization
+
+        var serialization = endpointConfiguration.UseSerialization<XmlSerializer>();
+        serialization.DontWrapRawXml();
+        #endregion
+    }
+
+    #region MessageWithXDocument
+
+    public class MessageWithXDocument : IMessage
+    {
+        // name and casing must match the root node
+        public XDocument nutrition { get; set; }
+    }
+    #endregion
+
+    #region MessageWithXElement
+
+    public class MessageWithXElement : IMessage
+    {
+        // name and casing must match the root node
+        public XElement nutrition { get; set; }
+    }
+    #endregion
+}
---
+++ Snippets/Xml/Core_10/XmlSerializerUsage.cs
@@ -0,0 +1,13 @@
+﻿using NServiceBus;
+
+class XmlSerializerUsage
+{
+    public XmlSerializerUsage(EndpointConfiguration endpointConfiguration)
+    {
+        #region XmlSerialization
+
+        endpointConfiguration.UseSerialization<XmlSerializer>();
+
+        #endregion
+    }
+}
