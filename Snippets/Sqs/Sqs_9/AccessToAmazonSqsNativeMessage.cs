65ca9107482b1e82e44e78ff6b318d40d91d1c29
Tamara Rivera <tamita.rivera@gmail.com>
2025-08-22 10:59:27 -0700

    Add NServiceBus 10 version of the Sqs snippets (#7760)
    * Add NServiceBus 10 version of the Sqs snippets
    
    * Remove the reference to a previous version

---
+++ Snippets/Sqs/Sqs_9/AccessToAmazonSqsNativeMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon.SQS.Model;
+using NServiceBus.Pipeline;
+
+#region sqs-access-to-native-message
+class AccessToAmazonSqsNativeMessage : Behavior<IIncomingContext>
+{
+    public override Task Invoke(IIncomingContext context, Func<Task> next)
+    {
+        // get the native Amazon SQS message
+        var message = context.Extensions.Get<Message>();
+
+        //do something useful
+
+        return next();
+    }
+}
+#endregion
--- Snippets/Sqs/Sqs_9/Usage.cs
+++ Snippets/Sqs/Sqs_9/Usage.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Net;
 using Amazon;
 using Amazon.Runtime;
@@ -7,7 +6,6 @@ using Amazon.S3;
 using Amazon.SimpleNotificationService;
 using Amazon.SQS;
 using NServiceBus;
-using NServiceBus.Transport.SQS;
 
 
 class Usage
