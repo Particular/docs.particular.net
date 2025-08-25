dd91825aae927367f98408b8c5bcb7cb30dc50ef
Tamara Rivera <tamita.rivera@gmail.com>
2025-08-25 13:49:06 -0700

    Add NServiceBus 10 version of the Transactional session NHibernate snippets (#7777)
    * Add snippets for previously released versions
    
    * Add NServiceBus 10 version of the Transactional session NHibernate snippets

---
+++ Snippets/TransactionalSession.NHibernate/NHibernateTS_10/NHibernateConfig.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NServiceBus;
+using NServiceBus.TransactionalSession;
+using System;
+using System.Threading.Tasks;
+
+public class NHibernateConfig
+{
+    public void Configure(EndpointConfiguration config)
+    {
+        #region enabling-transactional-session-nhibernate
+
+        var persistence = config.UsePersistence<NHibernatePersistence>();
+        persistence.EnableTransactionalSession();
+
+        #endregion
+    }
+
+    public async Task OpenDefault(IServiceProvider serviceProvider)
+    {
+        #region open-transactional-session-nhibernate
+
+        using var childScope = serviceProvider.CreateScope();
+        var session = childScope.ServiceProvider.GetService<ITransactionalSession>();
+        await session.Open();
+
+        // use the session
+
+        await session.Commit();
+
+        #endregion
+    }
+
+    public async Task UseSession(ITransactionalSession session)
+    {
+        #region use-transactional-session-nhibernate
+        await session.Open();
+
+        // add messages to the transaction:
+        await session.Send(new MyMessage());
+
+        // access the database:
+        var nhibernateSession = session.SynchronizedStorageSession.Session();
+
+        await session.Commit();
+        #endregion
+    }
+}
---
+++ Snippets/TransactionalSession.NHibernate/NHibernateTS_11/MyMessage.cs
@@ -0,0 +1,5 @@
+﻿using NServiceBus;
+
+public class MyMessage : ICommand
+{
+}
