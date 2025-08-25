fb55ed8d909b9a7ca5907cdf3a846d8bd76e089e
Tamara Rivera <tamita.rivera@gmail.com>
2025-08-25 12:21:55 -0700

    Add NServiceBus 10 version of the Transactional session DynamoDB snippets (#7775)
    * Add the previously released versions of the snippets
    
    * Add NServiceBus 10 version of the Transactional session DynamoDB snippets

---
+++ Snippets/TransactionalSession.DynamoDB/DynamoTS_4/DynamoDBConfig.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using NServiceBus;
+using NServiceBus.TransactionalSession;
+
+public class DynamoDBConfig
+{
+    public void Configure(EndpointConfiguration config)
+    {
+        #region enabling-transactional-session-dynamo
+
+        var persistence = config.UsePersistence<DynamoPersistence>();
+        persistence.EnableTransactionalSession();
+
+        #endregion
+    }
+
+    public async Task OpenDefault(IServiceProvider serviceProvider)
+    {
+        #region open-transactional-session-dynamo
+
+        using var childScope = serviceProvider.CreateScope();
+        var session = childScope.ServiceProvider.GetService<ITransactionalSession>();
+        await session.Open(new DynamoOpenSessionOptions());
+
+        // use the session
+
+        await session.Commit();
+
+        #endregion
+    }
+
+    public async Task UseSession(ITransactionalSession session)
+    {
+        #region use-transactional-session-dynamo
+        await session.Open(new DynamoOpenSessionOptions());
+
+        // add messages to the transaction:
+        await session.Send(new MyMessage());
+
+        // access the database:
+        var dynamoSession = session.SynchronizedStorageSession.DynamoPersistenceSession();
+
+        await session.Commit();
+        #endregion
+    }
+}
