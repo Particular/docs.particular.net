1108a542a7733a38c90eedd9a59ea1ac78891055
Daniel Marbach <danielmarbach@users.noreply.github.com>
2025-09-03 17:37:40 +0200

    Outbox Sample

---
+++ samples/outbox/mongodb/Core_9/Sample/Helper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using NServiceBus;
+public static class Helper
+{
+    public static Task SendDuplicates<TMessage>(IMessageSession context, TMessage message, int totalCount)
+    {
+        var duplicatedMessageId = Guid.NewGuid().ToString();
+
+        var tasks = Enumerable.Range(0, totalCount)
+            .Select(i =>
+            {
+                var options = new SendOptions();
+                options.RouteToThisEndpoint();
+                options.SetMessageId(duplicatedMessageId);
+
+                return context.Send(message, options);
+            });
+
+        return Task.WhenAll(tasks);
+    }
+}
