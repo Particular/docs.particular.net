5cd1a9ff5c794ae464ef1b28b31fca2c2cbef4f4
David Boike <david.boike@gmail.com>
2025-09-03 16:01:08 -0500

    Merge NHibernate minor snippets

--- Snippets/NHibernate/NHibernate_8/Outbox.cs
+++ Snippets/NHibernate/NHibernate_8/Outbox.cs
@@ -58,5 +58,52 @@
 
         #endregion
 
+        public void CustomTableName(EndpointConfiguration endpointConfiguration)
+        {
+            #region OutboxNHibernateCustomTableNameConfig
+
+            var persistence = endpointConfiguration.UsePersistence<NHibernatePersistence>();
+            persistence.CustomizeOutboxTableName(
+                outboxTableName: "MyEndpointOutbox",
+                outboxSchemaName: "MySchema");
+
+            #endregion
+        }
+
+        public void PessimisticMode(EndpointConfiguration endpointConfiguration)
+        {
+            #region OutboxPessimisticMode
+
+            var outboxSettings = endpointConfiguration.EnableOutbox();
+            outboxSettings.UsePessimisticConcurrencyControl();
+
+            #endregion
+        }
+
+        public void TransactionScopeMode(EndpointConfiguration endpointConfiguration)
+        {
+            #region OutboxTransactionScopeMode
+
+            var outboxSettings = endpointConfiguration.EnableOutbox();
+            outboxSettings.UseTransactionScope();
+
+            #endregion
+        }
+
+
+
+
+
+
+        public void TransactionScopeMode(EndpointConfiguration endpointConfiguration)
+        {
+            #region OutboxTransactionIsolation
+
+            var outboxSettings = endpointConfiguration.EnableOutbox();
+            outboxSettings.UseTransactionScope();
+            outboxSettings.TransactionIsolationLevel(IsolationLevel.ReadCommitted);
+
+            #endregion
+        }
     }
 }
