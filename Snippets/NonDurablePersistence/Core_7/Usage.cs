8b37c198a14557148d100a103be825d2fd0a595e
David Boike <david.boike@gmail.com>
2025-09-03 16:07:29 -0500

    Merge non-durable persistence minor snippets

--- Snippets/NonDurablePersistence/Core_7/Usage.cs
+++ Snippets/NonDurablePersistence/Core_7/Usage.cs
@@ -15,5 +15,15 @@
 
             #endregion
         }
+
+        public Usage(EndpointConfiguration endpointConfiguration)
+        {
+            #region GatewayDeduplicationCacheSize
+
+            var persistence = endpointConfiguration.UsePersistence<InMemoryPersistence, StorageType.GatewayDeduplication>();
+            persistence.GatewayDeduplicationCacheSize(50000);
+
+            #endregion
+        }
     }
 }
--- persistence/non-durable/index_gatewaydedupe_core_[7,7.2).partial.md
+++
@@ -1,6 +0,0 @@
-## Gateway deduplication
-
-> [!NOTE]
-> It is advised to use the [in-memory persister that is part of the gateway package](/persistence/non-durable/gateway-deduplication.md).
-
-In-memory gateway deduplication persistence has no built-in cleanup.
