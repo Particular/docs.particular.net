5253bdec2c716babfbbb8e2dd8ef42559ffa1ab6
Brandon Ording <bording@gmail.com>
2025-09-18 15:25:44 -0400

    Add NSB10 project

---
+++ Snippets/ASBFunctionsWorker/ASBFunctionsWorker_7/Configuration.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Microsoft.Extensions.Hosting;
+using NServiceBus;
+
+#region asb-function-isolated-configuration
+[assembly: NServiceBusTriggerFunction("WorkerDemoEndpoint")]
+
+public class Program
+{
+    public static Task Main()
+    {
+        var host = new HostBuilder()
+            .ConfigureFunctionsWorkerDefaults()
+            .UseNServiceBus()
+            .Build();
+
+        return host.RunAsync();
+    }
+}
+#endregion asb-function-isolated-configuration
+
+class EnableDiagnostics
+{
+    #region asb-function-isolated-enable-diagnostics
+    public static Task Main()
+    {
+        var host = new HostBuilder()
+            .ConfigureFunctionsWorkerDefaults()
+            .UseNServiceBus(configuration =>
+            {
+                configuration.LogDiagnostics();
+            })
+            .Build();
+
+        return host.RunAsync();
+    }
+    #endregion
+}
+
+class EnableDiagnosticsBlob
+{
+    public static Task Main()
+    {
+        var endpointName = "ASBWorkerEndpoint";
+
+        #region asb-function-iso-diagnostics-blob
+        var host = new HostBuilder()
+            .ConfigureFunctionsWorkerDefaults()
+            .UseNServiceBus(configuration =>
+            {
+                configuration.AdvancedConfiguration.CustomDiagnosticsWriter(
+                    async (diagnostics, cancellationToken) =>
+                    {
+                        var connectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+                        var blobServiceClient = new BlobServiceClient(connectionString);
+
+                        var containerClient = blobServiceClient.GetBlobContainerClient("diagnostics");
+                        await containerClient.CreateIfNotExistsAsync(cancellationToken: cancellationToken);
+
+                        var blobName = $"{endpointName}-configuration.txt";
+                        var blobClient = containerClient.GetBlobClient(blobName);
+                        await blobClient.UploadAsync(BinaryData.FromString(diagnostics), cancellationToken);
+                    });
+            })
+            .Build();
+        #endregion
+        return host.RunAsync();
+    }
+    
+}
+
+class ConfigureErrorQueue
+{
+    #region asb-function-isolated-configure-error-queue
+    public static Task Main()
+    {
+        var host = new HostBuilder()
+            .ConfigureFunctionsWorkerDefaults()
+            .UseNServiceBus(configuration =>
+            {
+                // Change the error queue name:
+                configuration.AdvancedConfiguration.SendFailedMessagesTo("my-custom-error-queue");
+
+                // Or disable the error queue to let ASB native dead-lettering handle repeated failures:
+                configuration.DoNotSendMessagesToErrorQueue();
+            })
+            .Build();
+
+        return host.RunAsync();
+    }
+    #endregion
+}
