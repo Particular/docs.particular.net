83d4046eea9819cc147bc08f7feffb7cc9cfda3b
Ryan Thomas <ryan.thomas@particular.net>
2025-10-06 14:22:35 -0500

    Add Core 10 version of the Avro sample

---
+++ samples/serializers/avro/Core_10/Sample/Program.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+Console.Title = "Avro";
+
+#region config
+
+var endpointConfiguration = new EndpointConfiguration("Samples.Serialization.Avro");
+
+endpointConfiguration.UseSerialization<AvroSerializer>();
+
+#endregion
+
+endpointConfiguration.UseTransport(new LearningTransport());
+
+endpointConfiguration.Pipeline.Register(typeof(MessageBodyLogger), "Logs the message body received");
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.UseNServiceBus(endpointConfiguration);
+
+var host = builder.Build();
+
+await host.StartAsync();
+
+var messageSession = host.Services.GetRequiredService<IMessageSession>();
+
+Console.WriteLine();
+Console.WriteLine("Press 'Enter' to send a message");
+Console.WriteLine("Press any other key to exit");
+
+while (true)
+{
+    if (Console.ReadKey().Key != ConsoleKey.Enter)
+    {
+        break;
+    }
+
+    #region message
+
+    var message = new CreateOrder
+    {
+        OrderId = 9,
+        Date = DateTime.Now,
+        CustomerId = 12,
+        OrderItems =
+        [
+            new OrderItem
+            {
+                ItemId = 6,
+                Quantity = 2
+            },
+
+            new OrderItem
+            {
+                ItemId = 5,
+                Quantity = 4
+            }
+        ]
+    };
+
+    await messageSession.SendLocal(message);
+
+    #endregion
+
+    Console.WriteLine("Message Sent");
+}
+
+await host.StopAsync();
