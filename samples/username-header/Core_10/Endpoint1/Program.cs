7b25b56c812e799ba118f0fdf54b2051beab1c9a
Ryan Thomas <ryan.thomas@particular.net>
2025-10-06 14:55:28 -0500

    Remove InputLoopService from username-header sample, clean up Core 10 version

--- samples/username-header/Core_10/Endpoint1/Program.cs
+++ samples/username-header/Core_10/Endpoint1/Program.cs
@@ -1,47 +1,48 @@
 using System;
 using System.Security.Principal;
-using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using NServiceBus;
 using NServiceBus.MessageMutator;
 
+Console.Title = "Endpoint1";
 
-var principalAccessor = new PrincipalAccessor();
+var endpointConfiguration = new EndpointConfiguration("Samples.UsernameHeader.Endpoint1");
+endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+endpointConfiguration.UseTransport(new LearningTransport());
 
 var builder = Host.CreateApplicationBuilder(args);
-var host = Host.CreateDefaultBuilder(args)
-    .UseNServiceBus(x =>
-    {
-        var endpointConfiguration = new EndpointConfiguration("Samples.UsernameHeader.Endpoint1");
-        endpointConfiguration.UseSerialization<SystemJsonSerializer>();
-        endpointConfiguration.UseTransport(new LearningTransport());
 
+#region component-registration-sender
 
-        #region component-registration-sender
+// Register both services
 
-        builder.Services.AddSingleton<IPrincipalAccessor>(principalAccessor);
-        var serviceProvider = builder.Services.BuildServiceProvider();
+var principalAccessor = new PrincipalAccessor();
+builder.Services.AddSingleton<IPrincipalAccessor>(principalAccessor);
 
-        var logger = serviceProvider.GetRequiredService<ILogger<AddUserNameToOutgoingHeadersMutator>>();
-        var mutator = new AddUserNameToOutgoingHeadersMutator(principalAccessor, logger);
-        endpointConfiguration.RegisterMessageMutator(mutator);
+var serviceProvider = builder.Services.BuildServiceProvider();
 
-        #endregion
-        return endpointConfiguration;
-    }).Build();
+var logger = serviceProvider.GetRequiredService<ILogger<AddUserNameToOutgoingHeadersMutator>>();
+var mutator = new AddUserNameToOutgoingHeadersMutator(principalAccessor, logger);
+endpointConfiguration.RegisterMessageMutator(mutator);
+#endregion
 
+Console.WriteLine("Press any key, the application is starting");
+Console.ReadKey();
+Console.WriteLine("Starting...");
+
+builder.UseNServiceBus(endpointConfiguration);
+
+var host = builder.Build();
 
 await host.StartAsync();
-
 
 #region send-message
 
 async Task SendMessage(int userNumber)
 {
     var identity = new GenericIdentity($"FakeUser{userNumber}");
-    principalAccessor.CurrentPrincipal = new GenericPrincipal(identity, new string[0]);
+    principalAccessor.CurrentPrincipal = new GenericPrincipal(identity, []);
 
     var messageSession = host.Services.GetRequiredService<IMessageSession>();
 
