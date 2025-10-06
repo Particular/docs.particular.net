ff28c179ad56e226d63f66c290ee71ccd5a0e361
Ryan Thomas <ryan.thomas@particular.net>
2025-10-06 15:14:06 -0500

    Remove InputLoopService implementation from ASTP_6

--- samples/azure/azure-table/transactions/ASTP_6/Client/Program.cs
+++ samples/azure/azure-table/transactions/ASTP_6/Client/Program.cs
@@ -3,22 +3,52 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NServiceBus;
 
-
 Console.Title = "Client";
-
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<InputLoopService>();
 
 var endpointConfiguration = new EndpointConfiguration("Samples.AzureTable.Transactions.Client");
 endpointConfiguration.UsePersistence<LearningPersistence>();
 endpointConfiguration.UseTransport(new LearningTransport());
 endpointConfiguration.UseSerialization<SystemJsonSerializer>();
 
-
-
 Console.WriteLine("Press any key, the application is starting");
-Console.ReadKey();
+Console.TreatControlCAsInput = true;
+var input = Console.ReadKey();
+if (input.Key == ConsoleKey.C && (input.Modifiers & ConsoleModifiers.Control) != 0)
+{
+    Environment.Exit(0);
+}
 Console.WriteLine("Starting...");
 
+var builder = Host.CreateApplicationBuilder(args);
 builder.UseNServiceBus(endpointConfiguration);
-await builder.Build().RunAsync();
+
+var host = builder.Build();
+
+await host.StartAsync();
+
+var messageSession = host.Services.GetRequiredService<IMessageSession>();
+
+Console.WriteLine("Press 'S' to send a message");
+Console.WriteLine("Press any other key to exit");
+
+while (true)
+{
+    var key = Console.ReadKey();
+    Console.WriteLine();
+
+    if (key.Key != ConsoleKey.S)
+    {
+        break;
+    }
+
+    var orderId = Guid.NewGuid();
+    var startOrder = new StartOrder
+    {
+        OrderId = orderId
+    };
+
+    await messageSession.Send("Samples.AzureTable.Transactions.Server", startOrder);
+    Console.WriteLine($"StartOrder Message sent to Server with OrderId {orderId}");
+}
+
+await host.StopAsync();
