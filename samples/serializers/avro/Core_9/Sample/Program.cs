8e2e8d771dacb2568282c52f84019014f22baebc
Ryan Thomas <ryan.thomas@particular.net>
2025-10-06 14:23:10 -0500

    Remove InputLoopService implementation from Avro Core 9 sample

--- samples/serializers/avro/Core_9/Sample/Program.cs
+++ samples/serializers/avro/Core_9/Sample/Program.cs
@@ -3,9 +3,6 @@ using Microsoft.Extensions.Hosting;
 
 Console.Title = "Avro";
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<InputLoopService>();
-
 #region config
 
 var endpointConfiguration = new EndpointConfiguration("Samples.Serialization.Avro");
@@ -18,5 +15,54 @@ endpointConfiguration.UseTransport(new LearningTransport());
 
 endpointConfiguration.Pipeline.Register(typeof(MessageBodyLogger), "Logs the message body received");
 
+var builder = Host.CreateApplicationBuilder(args);
 builder.UseNServiceBus(endpointConfiguration);
-await builder.Build().RunAsync();
+
+var host = builder.Build();
+
+await host.StartAsync();
+
+var messageSession = host.Services.GetRequiredService<IMessageSession>();
+
+Console.WriteLine();
+Console.WriteLine("Press 'Enter' to send a message");
+Console.WriteLine("Press any other key to exit");
+
+while (true)
+{
+    if (Console.ReadKey().Key != ConsoleKey.Enter)
+    {
+        break;
+    }
+
+    #region message
+
+    var message = new CreateOrder
+    {
+        OrderId = 9,
+        Date = DateTime.Now,
+        CustomerId = 12,
+        OrderItems =
+        [
+            new OrderItem
+            {
+                ItemId = 6,
+                Quantity = 2
+            },
+
+            new OrderItem
+            {
+                ItemId = 5,
+                Quantity = 4
+            }
+        ]
+    };
+
+    await messageSession.SendLocal(message);
+
+    #endregion
+
+    Console.WriteLine("Message Sent");
+}
+
+await host.StopAsync();
