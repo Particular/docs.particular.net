60efd53149ec9640c91958948646d53e53c4e8cd
Ryan Thomas <ryan.thomas@particular.net>
2025-10-13 11:13:53 -0500

    Remove InputLoopService from SQSLambda samples

--- samples/aws/lambda-sqs/SQSLambda_2/RegularEndpoint/Program.cs
+++ samples/aws/lambda-sqs/SQSLambda_2/RegularEndpoint/Program.cs
@@ -5,18 +5,38 @@ using NServiceBus;
 
 Console.Title = "RegularEndpoint";
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<InputLoopService>();
-
 var endpointConfiguration = new EndpointConfiguration("RegularEndpoint");
 
 endpointConfiguration.UseSerialization<SystemJsonSerializer>();
 endpointConfiguration.UseTransport<SqsTransport>();
 
-
-Console.WriteLine("Press any key, the application is starting");
-Console.ReadKey();
 Console.WriteLine("Starting...");
 
+var builder = Host.CreateApplicationBuilder(args);
+
 builder.UseNServiceBus(endpointConfiguration);
-await builder.Build().RunAsync();
+
+var host = builder.Build();
+
+await host.StartAsync();
+
+var messageSession = host.Services.GetRequiredService<IMessageSession>();
+
+Console.WriteLine("Press [ENTER] to send a message to the serverless endpoint queue.");
+Console.WriteLine("Press any other key to exit.");
+
+while (true)
+{
+    var key = Console.ReadKey();
+    Console.WriteLine();
+
+    if (key.Key != ConsoleKey.Enter)
+    {
+        break;
+    }
+
+    await messageSession.Send("ServerlessEndpoint", new TriggerMessage());
+    Console.WriteLine("Message sent to the serverless endpoint queue.");
+}
+
+await host.StopAsync();
