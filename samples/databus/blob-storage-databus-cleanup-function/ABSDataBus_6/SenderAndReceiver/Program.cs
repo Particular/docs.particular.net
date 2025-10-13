54e26949b131630042381793f8e851ec5625b3e4
Ryan Thomas <ryan.thomas@particular.net>
2025-10-13 13:09:33 -0500

    Remove InputLoopService from blob-storage-databus-cleanup-function sample

--- samples/databus/blob-storage-databus-cleanup-function/ABSDataBus_6/SenderAndReceiver/Program.cs
+++ samples/databus/blob-storage-databus-cleanup-function/ABSDataBus_6/SenderAndReceiver/Program.cs
@@ -3,12 +3,9 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NServiceBus;
-using SenderAndReceiver;
 
 Console.Title = "SenderAndReceiver";
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<InputLoopService>();
 var endpointConfiguration = new EndpointConfiguration("Samples.AzureDataBusCleanupWithFunctions.SenderAndReceiver");
 
 #pragma warning disable CS0618 // Type or member is obsolete
@@ -19,6 +16,48 @@ dataBus.ConnectionString("UseDevelopmentStorage=true");
 endpointConfiguration.UseSerialization<SystemJsonSerializer>();
 endpointConfiguration.UseTransport<LearningTransport>();
 endpointConfiguration.EnableInstallers();
+
+var builder = Host.CreateApplicationBuilder(args);
+
 builder.UseNServiceBus(endpointConfiguration);
 
-await builder.Build().RunAsync();
+var app = builder.Build();
+await app.StartAsync();
+
+var messageSession = app.Services.GetRequiredService<IMessageSession>();
+
+Console.WriteLine("Press 'Enter' to send a large message (>4MB)");
+Console.WriteLine("Press any other key to exit");
+
+while (true)
+{
+    var key = Console.ReadKey();
+
+    if (key.Key == ConsoleKey.Enter)
+    {
+        await SendMessageLargePayload(messageSession);
+    }
+    else
+    {
+        break;
+    }
+}
+
+await app.StopAsync();
+return;
+
+static async Task SendMessageLargePayload(IMessageSession messageSession)
+{
+    Console.WriteLine("Sending message...");
+
+#pragma warning disable CS0618 // Type or member is obsolete
+    var message = new MessageWithLargePayload
+    {
+        Description = "This message contains a large payload that will be sent on the Azure data bus",
+        LargePayload = new DataBusProperty<byte[]>(new byte[1024 * 1024 * 5]) // 5MB
+    };
+#pragma warning restore CS0618 // Type or member is obsolete
+    await messageSession.SendLocal(message);
+
+    Console.WriteLine("Message sent.");
+}
