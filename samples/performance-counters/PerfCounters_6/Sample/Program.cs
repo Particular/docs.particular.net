29fe782a67a48103599c6b50a710df602184b606
Ryan Thomas <ryan.thomas@particular.net>
2025-10-13 13:30:32 -0500

    Remove InputLoopService from performance-counters sample

--- samples/performance-counters/PerfCounters_6/Sample/Program.cs
+++ samples/performance-counters/PerfCounters_6/Sample/Program.cs
@@ -1,11 +1,8 @@
-
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Sample;
 
 Console.Title = "PerfCounters";
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<InputLoopService>();
+
 var endpointConfiguration = new EndpointConfiguration("Samples.PerfCounters");
 endpointConfiguration.EnableInstallers();
 endpointConfiguration.UseSerialization<SystemJsonSerializer>();
@@ -17,10 +14,35 @@ var performanceCounters = endpointConfiguration.EnableWindowsPerformanceCounters
 performanceCounters.EnableSLAPerformanceCounters(TimeSpan.FromSeconds(100));
 #endregion
 
-Console.WriteLine("Press enter to send 10 messages with random sleep");
-Console.WriteLine("Press any key, the application is starting");
-Console.ReadKey();
-Console.WriteLine("Starting...");
+var builder = Host.CreateApplicationBuilder(args);
 
 builder.UseNServiceBus(endpointConfiguration);
-await builder.Build().RunAsync();
+
+var host = builder.Build();
+await host.StartAsync();
+
+var messageSession = host.Services.GetRequiredService<IMessageSession>();
+
+Console.WriteLine("Press [Enter] to send 10 messages with random sleep");
+Console.WriteLine("Press any other key to exit");
+
+while (true)
+{
+    var key = Console.ReadKey();
+    Console.WriteLine();
+
+    if (key.Key != ConsoleKey.Enter)
+    {
+        break;
+    }
+
+    for (var i = 0; i < 10; i++)
+    {
+        var myMessage = new MyMessage();
+        await messageSession.SendLocal(myMessage);
+    }
+
+    Console.WriteLine("10 messages sent.");
+}
+
+await host.StopAsync();
