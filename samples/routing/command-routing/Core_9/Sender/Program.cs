e3d39fef44739db706054182ea5fd36acc752797
Ryan Thomas <ryan.thomas@particular.net>
2025-10-13 13:52:11 -0500

    Remove InputLoopService from command-routing samples

--- samples/routing/command-routing/Core_9/Sender/Program.cs
+++ samples/routing/command-routing/Core_9/Sender/Program.cs
@@ -3,12 +3,8 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NServiceBus;
-using Sender;
 
 Console.Title = "Sender";
-
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<InputLoopService>();
 
 var endpointConfiguration = new EndpointConfiguration("Samples.CommandRouting.Sender");
 
@@ -23,10 +19,71 @@ routing.RouteToEndpoint(
 
 endpointConfiguration.UseSerialization<SystemJsonSerializer>();
 
-
-Console.WriteLine("Press any key, the application is starting");
-Console.ReadKey();
 Console.WriteLine("Starting...");
 
+var builder = Host.CreateApplicationBuilder(args);
+
 builder.UseNServiceBus(endpointConfiguration);
-await builder.Build().RunAsync();
+
+var host = builder.Build();
+
+await host.StartAsync();
+
+var messageSession = host.Services.GetRequiredService<IMessageSession>();
+
+Console.WriteLine("Press [S] to send an order");
+Console.WriteLine("Press [C] to cancel an order");
+Console.WriteLine("Press any other key to exit");
+
+while (true)
+{
+    var key = Console.ReadKey();
+    Console.WriteLine();
+
+    if (key.Key != ConsoleKey.C && key.Key != ConsoleKey.S)
+    {
+        break;
+    }
+
+    switch (key.Key)
+    {
+        case ConsoleKey.S:
+            await PlaceOrder(messageSession, Guid.NewGuid().ToString(), 25m);
+            break;
+        case ConsoleKey.C:
+            await CancelOrder(messageSession, Guid.NewGuid().ToString());
+            break;
+        default:
+            continue;
+    }
+}
+
+await host.StopAsync();
+return;
+
+static async Task PlaceOrder(IMessageSession messageSession, string orderId, decimal value)
+{
+    #region send-command-with-configured-route
+    var command = new PlaceOrder
+    {
+        OrderId = orderId,
+        Value = value
+    };
+
+    await messageSession.Send(command);
+    Console.WriteLine("Order placed");
+    #endregion
+}
+
+static async Task CancelOrder(IMessageSession messageSession, string orderId)
+{
+    #region send-command-without-configured-route
+    var command = new CancelOrder
+    {
+        OrderId = orderId
+    };
+
+    await messageSession.Send("Samples.CommandRouting.Receiver", command);
+    Console.WriteLine("Order canceled");
+    #endregion
+}
