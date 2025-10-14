a386527c391cb189e875d84704253ee5a719d905
Brandon Ording <bording@gmail.com>
2025-10-14 11:09:10 -0400

    Remove ServiceControl URL reservation references (#7864)
    * Remove ServiceControl URL reservation references
    
    * Remove old uninstalling RavenDB 2.0 page

--- Snippets/ServiceControl/ServiceControl_All/powershell-basics.ps1
+++ Snippets/ServiceControl/ServiceControl_All/powershell-basics.ps1
@@ -22,18 +22,6 @@ Test-IfPortIsAvailable -Port 33333
 33330..33339 | Test-IfPortIsAvailable | ? Available
 # endcode
 
-# startcode ps-urlacls
-Get-UrlAcls | ? Port -eq 33333
-# endcode
-
-# startcode ps-removeurlacl
-Get-UrlAcls | ? Port -eq 33335 | Remove-UrlAcl
-# endcode
-
-# startcode ps-addurlacl
-Add-UrlAcl -Url http://servicecontrol.mycompany.com:33333/api/ -Users Users
-# endcode
-
 # startcode ps-importlicense
 Import-ServiceControlLicense License.xml
 # endcode
