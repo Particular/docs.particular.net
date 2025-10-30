9a8d1e9515b330ca36b5a1b045eff7075838e17c
Ryan Thomas <ryan.thomas@particular.net>
2025-10-30 12:14:10 -0500

    Remove MessageSenderService from hosting aspire samples

--- samples/hosting/aspire/Core_9/ClientUI/Program.cs
+++ samples/hosting/aspire/Core_9/ClientUI/Program.cs
@@ -1,8 +1,9 @@
-﻿using ClientUI;
-using Messages;
+﻿using Messages;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
+Console.Title = "Client UI";
 
 var builder = Host.CreateApplicationBuilder();
 
@@ -27,6 +28,39 @@ endpointConfiguration.EnableInstallers();
 
 builder.UseNServiceBus(endpointConfiguration);
 
-builder.Services.AddHostedService<MessageSenderService>();
+var host = builder.Build();
 
-await builder.Build().RunAsync();
+await host.StartAsync();
+
+var messageSession = host.Services.GetRequiredService<IMessageSession>();
+
+Console.WriteLine("Sending messages on an increasing interval. Press [CTRL] + [C] to exit");
+
+using (var cts = new CancellationTokenSource())
+{
+    Console.CancelKeyPress += (s, e) =>
+    {
+        Console.WriteLine("Cancellation Requested...");
+        cts.Cancel();
+        e.Cancel = true;
+    };
+
+    try
+    {
+        var i = 1;
+
+        while (true)
+        {
+            await messageSession.Send(new PlaceOrder { OrderId = Guid.NewGuid().ToString() }, cts.Token);
+            Console.WriteLine("Sent a message");
+
+            await Task.Delay(i++ * 500, cts.Token);
+        }
+    }
+    catch (OperationCanceledException) when (cts.Token.IsCancellationRequested)
+    {
+        // graceful shutdown
+    }
+}
+
+await host.StopAsync();
