6c2874b25336697683d17ab22b7daffcc52fc5d3
Mauro Servienti <mauro.servienti@gmail.com>
2025-10-31 10:38:07 +0100

    Create the authorization gateway

---
+++ samples/servicecontrol/secure-with-entra-id/Core_9/AuthGateway/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Identity.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+#region add-authentication-support
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
+builder.Services.AddAuthorization();
+#endregion
+
+#region add-yarp
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+#endregion
+
+var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapReverseProxy();
+
+app.Run();
