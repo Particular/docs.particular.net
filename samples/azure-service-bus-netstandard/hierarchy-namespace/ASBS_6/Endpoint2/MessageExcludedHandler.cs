fd3807a8d8fe6fd3ca02ff72ca7268c12bbc3ed6
Bartek Wasielak <bartek.wasielak@particular.net>
2026-02-04 12:46:25 -0500

    adding a sample for hierarchy namespace

---
+++ samples/azure-service-bus-netstandard/hierarchy-namespace/ASBS_6/Endpoint2/MessageExcludedHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+
+public class MessageExcludedHandler (ILogger<MessageExcludedHandler> logger):
+    IHandleMessages<MessageExcluded>
+{
+
+    public Task Handle(MessageExcluded message, IMessageHandlerContext context)
+    {
+        logger.LogInformation("Received MessageExcluded: {Property}", message.Property);
+
+        return Task.CompletedTask;
+    }
+}
