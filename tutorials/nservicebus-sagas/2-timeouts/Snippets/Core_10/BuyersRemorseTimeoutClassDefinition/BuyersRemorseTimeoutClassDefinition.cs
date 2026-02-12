f73bcb4f9feef61297631b8a1976d9ae98979792
Tamara Rivera <tamita.rivera@gmail.com>
2026-02-11 17:11:36 -0800

    Remove unnecessary sections

--- tutorials/nservicebus-sagas/2-timeouts/Snippets/Core_10/BuyersRemorseTimeoutClassDefinition/BuyersRemorseTimeoutClassDefinition.cs
+++ tutorials/nservicebus-sagas/2-timeouts/Snippets/Core_10/BuyersRemorseTimeoutClassDefinition/BuyersRemorseTimeoutClassDefinition.cs
@@ -2,30 +2,8 @@
 
 #region BuyersRemorseTimeoutClassDefinition
 
-class BuyersRemorsePolicy : Saga<BuyersRemorseData>,
-    IAmStartedByMessages<PlaceOrder>
-{
-    // ...
-}
-
 class BuyersRemorseIsOver
 {
 }
 
 #endregion
-
-internal class Saga<T>
-{
-}
-
-internal class BuyersRemorseData
-{
-}
-
-internal class PlaceOrder
-{
-}
-
-internal interface IAmStartedByMessages<T>
-{
-}
