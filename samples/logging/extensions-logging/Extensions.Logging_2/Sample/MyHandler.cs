19f84c98a431dff03baaeeb824e5c56506525189
Brandon Ording <bording@gmail.com>
2026-03-19 18:19:37 -0400

    Fix logging sample (#8142)

--- samples/logging/extensions-logging/Extensions.Logging_2/Sample/MyHandler.cs
+++ samples/logging/extensions-logging/Extensions.Logging_2/Sample/MyHandler.cs
@@ -1,11 +1,8 @@
-﻿using System.Threading.Tasks;
-using NServiceBus;
-using NServiceBus.Logging;
+﻿using NServiceBus.Logging;
 
-public class MyHandler :
-    IHandleMessages<MyMessage>
+public class MyHandler : IHandleMessages<MyMessage>
 {
-    static ILog log = LogManager.GetLogger<MyHandler>();
+    static readonly ILog log = LogManager.GetLogger<MyHandler>();
 
     public Task Handle(MyMessage message, IMessageHandlerContext context)
     {
