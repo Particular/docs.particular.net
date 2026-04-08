52254af623387e7928ae0616cdebb5f25d24d389
Irina Dominte <scurtuirina@gmail.com>
2026-04-08 16:06:37 +0300

    Samples for delayed delivery

---
+++ samples/rabbitmq/delayed-delivery/Rabbit_11/Receiver/MyCommandHandler.cs
@@ -0,0 +1,14 @@
+namespace Receiver;
+
+using Microsoft.Extensions.Logging;
+using Shared;
+
+public class MyCommandHandler(ILogger<MyCommandHandler> logger) : IHandleMessages<MyCommand>
+{
+    public Task Handle(MyCommand commandMessage, IMessageHandlerContext context)
+    {
+        logger.LogInformation("Received MyCommand");
+
+        return Task.CompletedTask;
+    }
+}
---
+++ samples/rabbitmq/delayed-delivery/Rabbit_11/Shared/MyCommand.cs
@@ -0,0 +1,5 @@
+namespace Shared;
+
+public class MyCommand : ICommand
+{
+}
