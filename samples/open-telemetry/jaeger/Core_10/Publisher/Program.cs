928eb5d0fe1054e62ca0b025e75e8c23c7131413
Brandon Ording <bording@gmail.com>
2026-04-30 15:53:09 -0400

    Weekly legacy docs review (#8228)
    * Update legacy docs
    
    * Update sample

--- samples/open-telemetry/jaeger/Core_10/Publisher/Program.cs
+++ samples/open-telemetry/jaeger/Core_10/Publisher/Program.cs
@@ -1,54 +1,58 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using OpenTelemetry;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 using Shared;
 
-class Program
+const string EndpointName = "Samples.OpenTelemetry.Publisher";
+
+Console.Title = EndpointName;
+var builder = Host.CreateApplicationBuilder(args);
+
+#region jaeger-exporter-configuration
+var tracerProvider = Sdk.CreateTracerProviderBuilder()
+    .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(EndpointName))
+    .AddSource("NServiceBus.Core*")
+    .AddOtlpExporter() // The exporter defaults to gRPC on over port 4317 - https://github.com/open-telemetry/opentelemetry-dotnet/blob/main/src/OpenTelemetry.Exporter.OpenTelemetryProtocol/README.md#otlpexporteroptions
+    .Build();
+#endregion
+
+var endpointConfiguration = new EndpointConfiguration(EndpointName);
+endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+endpointConfiguration.UseTransport(new LearningTransport());
+
+builder.UseNServiceBus(endpointConfiguration);
+
+var host = builder.Build();
+
+await host.StartAsync();
+
+var messageSession = host.Services.GetRequiredService<IMessageSession>();
+
+Console.WriteLine("Press '1' to publish the OrderReceived event");
+Console.WriteLine("Press any other key to exit");
+
+while (true)
 {
-    const string EndpointName = "Samples.OpenTelemetry.Publisher";
+    var key = Console.ReadKey();
+    Console.WriteLine();
 
-    public static async Task Main()
+    if (key.Key == ConsoleKey.D1)
     {
-        Console.Title = EndpointName;
-
-        #region jaeger-exporter-configuration
-        var tracerProvider = Sdk.CreateTracerProviderBuilder()
-            .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(EndpointName))
-            .AddSource("NServiceBus.Core*")
-            .AddOtlpExporter() // The exporter defaults to gRPC on over port 4317 - https://github.com/open-telemetry/opentelemetry-dotnet/blob/main/src/OpenTelemetry.Exporter.OpenTelemetryProtocol/README.md#otlpexporteroptions
-            .Build();
-        #endregion
-
-        var endpointConfiguration = new EndpointConfiguration(EndpointName);
-        endpointConfiguration.UseSerialization<SystemJsonSerializer>();
-        endpointConfiguration.UseTransport(new LearningTransport());
-
-        var endpointInstance = await Endpoint.Start(endpointConfiguration);
-
-        Console.WriteLine("Press '1' to publish the OrderReceived event");
-        Console.WriteLine("Press any other key to exit");
-        while (true)
+        var orderReceived = new OrderReceived
         {
-            var key = Console.ReadKey();
-            Console.WriteLine();
+            OrderId = Guid.NewGuid()
+        };
 
-            var orderReceivedId = Guid.NewGuid();
-            if (key.Key == ConsoleKey.D1)
-            {
-                var orderReceived = new OrderReceived
-                {
-                    OrderId = orderReceivedId
-                };
-                await endpointInstance.Publish(orderReceived);
-                Console.WriteLine($"Published OrderReceived Event with Id {orderReceivedId}.");
-            }
-            else
-            {
-                break;
-            }
-        }
-
-        await endpointInstance.Stop();
-        tracerProvider.ForceFlush();
+        await messageSession.Publish(orderReceived);
+        Console.WriteLine($"Published OrderReceived Event with Id {orderReceived.OrderId}.");
+    }
+    else
+    {
+        break;
     }
 }
+
+await host.StopAsync();
+tracerProvider.ForceFlush();
--- samples/open-telemetry/jaeger/Core_10/Subscriber/Program.cs
+++ samples/open-telemetry/jaeger/Core_10/Subscriber/Program.cs
@@ -1,32 +1,26 @@
+using Microsoft.Extensions.Hosting;
 using OpenTelemetry;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 
-class Program
-{
-    const string EndpointName = "Samples.OpenTelemetry.Subscriber";
+const string EndpointName = "Samples.OpenTelemetry.Subscriber";
 
-    public static async Task Main()
-    {
-        Console.Title = EndpointName;
+Console.Title = EndpointName;
+var builder = Host.CreateApplicationBuilder(args);
 
-        var tracerProvider = Sdk.CreateTracerProviderBuilder()
-            .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(EndpointName))
-            .AddSource("NServiceBus.Core*")
-            .AddOtlpExporter() // The exporter defaults to gRPC on over port 4317 - https://github.com/open-telemetry/opentelemetry-dotnet/blob/main/src/OpenTelemetry.Exporter.OpenTelemetryProtocol/README.md#otlpexporteroptions
-            .Build();
+var tracerProvider = Sdk.CreateTracerProviderBuilder()
+    .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(EndpointName))
+    .AddSource("NServiceBus.Core*")
+    .AddOtlpExporter() // The exporter defaults to gRPC on over port 4317 - https://github.com/open-telemetry/opentelemetry-dotnet/blob/main/src/OpenTelemetry.Exporter.OpenTelemetryProtocol/README.md#otlpexporteroptions
+    .Build();
 
-        var endpointConfiguration = new EndpointConfiguration(EndpointName);
+var endpointConfiguration = new EndpointConfiguration(EndpointName);
+endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+endpointConfiguration.UseTransport(new LearningTransport());
 
-        endpointConfiguration.UseSerialization<SystemJsonSerializer>();
-        endpointConfiguration.UseTransport(new LearningTransport());
+builder.UseNServiceBus(endpointConfiguration);
 
-        var endpointInstance = await Endpoint.Start(endpointConfiguration);
+var host = builder.Build();
 
-        Console.WriteLine("Press any key to exit");
-        _ = Console.ReadKey();
-
-        await endpointInstance.Stop();
-        tracerProvider.ForceFlush();
-    }
-}
+await host.RunAsync();
+tracerProvider.ForceFlush();
