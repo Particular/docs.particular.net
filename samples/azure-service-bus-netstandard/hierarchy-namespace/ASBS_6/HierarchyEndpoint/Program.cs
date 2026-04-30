c4c7ad0c8809a74d380c5bb49e933951f06039fe
Brandon Ording <bording@gmail.com>
2026-04-30 17:57:05 -0400

    Clean up sample

--- samples/azure-service-bus-netstandard/hierarchy-namespace/ASBS_6/HierarchyEndpoint/Program.cs
+++ samples/azure-service-bus-netstandard/hierarchy-namespace/ASBS_6/HierarchyEndpoint/Program.cs
@@ -16,8 +16,10 @@ if (string.IsNullOrWhiteSpace(connectionString))
     throw new Exception("Could not read the 'AzureServiceBus_ConnectionString' environment variable. Check the sample prerequisites.");
 }
 
-var transport = new AzureServiceBusTransport(connectionString, TopicTopology.Default);
-transport.HierarchyNamespaceOptions = new HierarchyNamespaceOptions { HierarchyNamespace = "my-hierarchy" };
+var transport = new AzureServiceBusTransport(connectionString, TopicTopology.Default)
+{
+    HierarchyNamespaceOptions = new HierarchyNamespaceOptions { HierarchyNamespace = "my-hierarchy" }
+};
 
 endpointConfiguration.UseTransport(transport);
 endpointConfiguration.UseSerialization<SystemJsonSerializer>();
