738e0c3d9781175ec3ee8a5a7e3fb6a54302436b
David Boike <david.boike@gmail.com>
2026-05-04 13:57:34 -0500

    Couple more

--- samples/saga/batching/Core_10/WorkGenerator/Program.cs
+++ samples/saga/batching/Core_10/WorkGenerator/Program.cs
@@ -1,53 +1,62 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using NServiceBus;
 
-public static class Program
+var config = new EndpointConfiguration(EndpointNames.WorkGenerator);
+
+config.UsePersistence<LearningPersistence>();
+config.UseSerialization<SystemJsonSerializer>();
+config.AuditProcessedMessagesTo("audit");
+config.SendFailedMessagesTo("error");
+
+var transport = config.UseTransport<LearningTransport>();
+var routing = transport.Routing();
+RoutingHelper.ApplyDefaultRouting(routing);
+
+var builder = Host.CreateApplicationBuilder();
+builder.Services.AddNServiceBusEndpoint(config);
+var host = builder.Build();
+var messageSession = host.Services.GetRequiredService<IMessageSession>();
+await host.StartAsync();
+
+Console.WriteLine("Started.");
+Console.WriteLine("Press 'S' to start a new process or [Enter] to exit.");
+
+await RunInputLoop(messageSession);
+
+await host.StopAsync();
+
+static async Task RunInputLoop(IMessageSession messageSession)
 {
-    public static async Task Main()
+    while (true)
     {
-        var config = new EndpointConfiguration(EndpointNames.WorkGenerator);
-
-        config.UsePersistence<LearningPersistence>();
-        config.UseSerialization<SystemJsonSerializer>();
-        config.AuditProcessedMessagesTo("audit");
-        config.SendFailedMessagesTo("error");
-
-        var transport = config.UseTransport<LearningTransport>();
-        var routing = transport.Routing();
-        var endpoint = await config.StartWithDefaultRoutes(routing);
-
-        Console.WriteLine("Started.");
-        Console.WriteLine("Press 'S' to start a new process or [Enter] to exit.");
-
-        while (true)
+        var pressedKey = Console.ReadKey();
+        switch (pressedKey.Key)
         {
-            var pressedKey = Console.ReadKey();
-            switch (pressedKey.Key)
+            case ConsoleKey.Enter:
             {
-                case ConsoleKey.Enter:
-                    {
-                        return;
-                    }
-                case ConsoleKey.S:
-                    {
-                        await StartSaga(endpoint);
-                        break;
-                    }
+                return;
+            }
+            case ConsoleKey.S:
+            {
+                await StartSaga(messageSession);
+                break;
             }
         }
     }
+}
 
-    static async Task StartSaga(IMessageSession session)
+static async Task StartSaga(IMessageSession session)
+{
+    var id = Guid.NewGuid();
+    var count = Random.Shared.Next(500, 1000);
+
+    await session.SendLocal(new StartProcessing
     {
-        var id = Guid.NewGuid();
-        var count = Random.Shared.Next(500, 1000);
-
-        await session.SendLocal(new StartProcessing
-        {
-            ProcessId = id,
-            WorkCount = count
-        });
-        Console.WriteLine($"Started process '{id}' with '{count}' work orders.");
-    }
+        ProcessId = id,
+        WorkCount = count
+    });
+    Console.WriteLine($"Started process '{id}' with '{count}' work orders.");
 }
