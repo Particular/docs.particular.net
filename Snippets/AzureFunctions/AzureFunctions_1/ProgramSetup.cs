a59719b39f9e23676149c5ad3f209e4525551fa1
Andreas Öhlund <andreas.ohlund@particular.net>
2026-05-25 15:58:11 +0200

    Refactor ProgramSetup.cs to reorganize region directives for improved code readability

--- Snippets/AzureFunctions/AzureFunctions_1/ProgramSetup.cs
+++ Snippets/AzureFunctions/AzureFunctions_1/ProgramSetup.cs
@@ -4,16 +4,16 @@ using System.Threading.Tasks;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.Hosting;
 
-#region azure-functions-program-builder
 class ProgramSetup
 {
     public static async Task Main(string[] args)
     {
+        #region azure-functions-program-builder
         var builder = FunctionsApplication.CreateBuilder(args);
         builder.AddNServiceBusFunctions();
 
         var host = builder.Build();
         await host.RunAsync();
+        #endregion
     }
 }
-#endregion
