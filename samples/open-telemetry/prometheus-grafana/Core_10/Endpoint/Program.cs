225febde95291188dd02dbceecf7680a71e7b9c6
Tamara Rivera <tamita.rivera@gmail.com>
2026-06-11 13:42:23 -0700

    Fix v10 of the Prometheus sample

--- samples/open-telemetry/prometheus-grafana/Core_10/Endpoint/Program.cs
+++ samples/open-telemetry/prometheus-grafana/Core_10/Endpoint/Program.cs
@@ -26,7 +26,7 @@ public class Program
         #endregion
 
         #region enable-prometheus-http-listener
-        meterProviderBuilder.AddPrometheusHttpListener(options => options.UriPrefixes = new[] { "http://*:9464/" });
+        meterProviderBuilder.AddPrometheusHttpListener();
         #endregion
 
         var meterProvider = meterProviderBuilder.Build();
