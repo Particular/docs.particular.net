118cc63a41cf86a55ee0b212f7f36fb611da2455
Nick Gallegos <nick.gallegos@particular.net>
2026-06-15 15:36:09 -0600

    Review /samples/cosmosdb/transactions

--- samples/cosmosdb/transactions/CosmosDB_3/Server/Program.cs
+++ samples/cosmosdb/transactions/CosmosDB_3/Server/Program.cs
@@ -27,9 +27,11 @@ class Program
 
              var persistence = endpointConfiguration.UsePersistence<CosmosPersistence>();
              var connection = Environment.GetEnvironmentVariable("COSMOS_CONNECTION_STRING") ??
-                             @"AccountEndpoint = https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+                             """AccountEndpoint = http://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==""";
              persistence.DatabaseName("Samples.CosmosDB.Transactions");
-             persistence.CosmosClient(new CosmosClient(connection));
+             persistence.CosmosClient(new CosmosClient(connection, new CosmosClientOptions {
+                 ConnectionMode = ConnectionMode.Gateway // prevents a BadRequest error on CosmosDB installation to local emulators
+             }));
              persistence.DefaultContainer("Server", "/OrderId");
 
              #endregion
@@ -47,7 +49,7 @@ class Program
              #region TransactionInformationFromHeader
              transactionInformation.ExtractPartitionKeyFromHeader("Sample.CosmosDB.Transaction.OrderId", orderId =>
              {
-                 logger.LogInformation($"Found partition key '{orderId}' from header 'Sample.CosmosDB.Transaction'");
+                 logger.LogInformation($"Found partition key '{orderId}' from header 'Sample.CosmosDB.Transaction.OrderId'");
                  return orderId;
              });
 
