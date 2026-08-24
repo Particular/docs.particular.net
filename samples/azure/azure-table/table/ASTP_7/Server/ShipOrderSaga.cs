634d784feb1c283f8d98d8b27189be1a314b855c
Daniel Marbach <danielmarbach@users.noreply.github.com>
2026-08-24 22:09:51 +0200

    ReplyToOriginator changes due to obsoletion and warnings as errors

--- samples/azure/azure-table/table/ASTP_7/Server/ShipOrderSaga.cs
+++ samples/azure/azure-table/table/ASTP_7/Server/ShipOrderSaga.cs
@@ -32,7 +32,7 @@ public class ShipOrderSaga(ILogger<ShipOrderSaga> logger) :
 
         state.OrderId = Data.OrderId;
 
-        return ReplyToOriginator(context, state);
+        return ReplyToOriginator<CompleteOrder>(context, state);
     }
 }
 
