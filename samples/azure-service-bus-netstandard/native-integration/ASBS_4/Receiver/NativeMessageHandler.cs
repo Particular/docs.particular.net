c2eb12470cbb1a4b615150da7d26bb1e6fec7261
Jayanthi <jayanthi.sourirajan@particular.net>
2026-09-21 13:15:29 -0700

    Revert "remove unsupported verion of transport "NServiceBus.Transport.AzureServiceBus" Version="4.*""
    This reverts commit fb3f3dc51a3f273c82b4a8a86a7f57945a02d40e.

---
+++ samples/azure-service-bus-netstandard/native-integration/ASBS_4/Receiver/NativeMessageHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+
+#region NativeMessageHandler
+
+public class NativeMessageHandler :
+    IHandleMessages<NativeMessage>
+{
+    static readonly ILog Log = LogManager.GetLogger<NativeMessageHandler>();
+
+    public Task Handle(NativeMessage message, IMessageHandlerContext context)
+    {
+        Log.Info($"Message content: {message.Content}");
+        Log.Info($"Received native message sent on {message.SentOnUtc} UTC");
+        return Task.CompletedTask;
+    }
+}
+
+#endregion
